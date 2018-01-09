5e8591c2d259c96f10b3fda5a3d00fe5d61e549b
Gav <gavin@parity.io>
2018-01-09 15:05:41 +0100

    More fleshing out on runtime.

--- wasm-runtime/target/wasm32-unknown-unknown/release/libpwasm_alloc.d
+++ wasm-runtime/target/wasm32-unknown-unknown/release/libpwasm_alloc.d
@@ -1,1 +1,1 @@
-/Users/gav/Core/polkadot/wasm-runtime/target/wasm32-unknown-unknown/release/libpwasm_alloc.rlib: /Users/gav/Core/polkadot/wasm-runtime/pwasm-alloc/src/lib.rs /Users/gav/Core/polkadot/wasm-runtime/pwasm-libc/src/lib.rs
+/Users/gav/Core/polkadot/wasm-runtime/target/wasm32-unknown-unknown/release/libpwasm_alloc.rlib: /Users/gav/Core/polkadot/wasm-runtime/pwasm-libc/src/lib.rs /Users/gav/Core/polkadot/wasm-runtime/pwasm-alloc/src/lib.rs
