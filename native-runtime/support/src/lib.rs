f0075fad153000abac48989a9cf2209f012f5019
Gav <gavin@parity.io>
2018-01-09 15:07:55 +0100

    Update native support.

--- native-runtime/support/src/lib.rs
+++ native-runtime/support/src/lib.rs
@@ -1,4 +1,8 @@
 pub use std::vec::Vec;
+pub use std::rc::Rc;
+pub use std::cell::RefCell;
+pub use std::boxed::Box;
+pub use std::mem::{size_of, transmute};
 
 pub fn storage(_key: &[u8]) -> Vec<u8> { vec![] }
 pub fn storage_into<T: Sized>(_key: &[u8]) -> Option<T> { None }
