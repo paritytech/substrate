f829866f19fb833667349d9bee6a48f8dd3cb411
Gav <gavin@parity.io>
2018-01-21 22:57:50 +0100

    Fix some semantics. Add Storable::take.

--- wasm-runtime/polkadot/src/runtime/governance.rs
+++ wasm-runtime/polkadot/src/runtime/governance.rs
@@ -16,6 +16,13 @@
 
 //! Governance system: Handles administration and dispatch of sensitive operations including
 //! setting new code, minting new tokens and changing parameters.
+//!
+//! For now this is limited to a simple qualified majority vote (whose parameter is retrieved from
+//! storage) between validators. A single vote may be proposed per era, and at most one approval
+//! vote may be cast by each validator. The tally is maintained
+//!
+//!
+//!
 
 use runtime_support::Vec;
 use keyedvec::KeyedVec;
@@ -34,26 +41,28 @@ pub fn propose(transactor: &AccountID, proposal: &Proposal) {
 	approve(transactor, staking::current_era());
 }
 
-pub fn approve(transactor: &AccountID, era_index: BlockNumber) {
+pub fn approve(validator: &AccountID, era_index: BlockNumber) {
 	if era_index != staking::current_era() {
 		panic!("approval vote applied on non-current era.")
 	}
 	if Proposal::lookup(b"gov:pro").is_none() {
 		panic!("there must be a proposal in order to approve.");
 	}
-	let key = transactor.to_keyed_vec(b"gov:app:");
+	if session::validators().into_iter().position(|v| &v == validator).is_none() {
+		panic!("transactor must be a validator to approve.");
+	}
+	let key = validator.to_keyed_vec(b"gov:app:");
 	if bool::lookup(&key).is_some() {
 		panic!("transactor may not approve a proposal twice in one era.");
 	}
 	true.store(&key);
-	(approval_count() + 1).store(b"gov:app");
+}
+
+pub fn set_approval_ppm_required(ppm: u32) {
+	ppm.store(b"gov:apr");
 }
 
 // INSPECTION API
-
-pub fn approval_count() -> u32 {
-	Storable::lookup_default(b"gov:app")
-}
 
 pub fn approval_ppm_required() -> u32 {
 	Storable::lookup(b"gov:apr").unwrap_or(1000)
@@ -69,26 +78,17 @@ pub fn approvals_required() -> u32 {
 pub fn end_of_an_era() {
 	// tally up votes for the current proposal, if any. enact if there are sufficient approvals.
 	if let Some(proposal) = Proposal::lookup(b"gov:pro") {
-		let enact = approval_count() >= approvals_required();
-
-		// clear proposal
-		reset_proposal();
-
-		if enact {
+		kill(b"gov:pro");
+		let approved: u32 = session::validators().into_iter()
+			.map(|v| bool::take(&v.to_keyed_vec(b"gov:app:")).map(|_| 1).unwrap_or(0))
+			.sum();
+		if approved >= approvals_required() {
 			proposal.enact();
 		}
 	}
 }
 
 // PRIVATE API
-
-fn reset_proposal() {
-	session::validators().into_iter().for_each(|v| {
-		kill(&v.to_keyed_vec(b"gov:app:"));
-	});
-	kill(b"gov:pro");
-	kill(b"gov:app");
-}
 
 #[cfg(test)]
 mod tests {
