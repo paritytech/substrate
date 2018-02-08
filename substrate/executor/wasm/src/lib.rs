5b7f037ef91c3d9581bc7e63e22aceefdee3ee19
Robert Habermeier <rphmeier@gmail.com>
2018-02-08 22:36:24 +0100

    fix runtime_test

--- substrate/executor/wasm/src/lib.rs
+++ substrate/executor/wasm/src/lib.rs
@@ -13,57 +13,42 @@ use runtime_io::{
 	twox_128, twox_256, ed25519_verify, enumerated_trie_root
 };
 
-fn test_blake2_256(input: &[u8]) -> Vec<u8> {
-	blake2_256(&input).to_vec()
-}
+impl_stubs!(
+	test_data_in NO_DECODE => |input| {
+		print("set_storage");
+		set_storage(b"input", input);
 
-fn test_twox_256(input: &[u8]) -> Vec<u8> {
-	twox_256(&input).to_vec()
-}
+		print("storage");
+		let foo = storage(b"foo");
 
-fn test_twox_128(input: &[u8]) -> Vec<u8> {
-	twox_128(&input).to_vec()
-}
+		print("set_storage");
+		set_storage(b"baz", &foo);
 
-fn test_ed25519_verify(input: &[u8]) -> Vec<u8> {
-	let sig = &input[0..64];
-	let pubkey = &input[64..96];
-	let msg = b"all ok!";
-	[ed25519_verify(sig, &msg[..], pubkey) as u8].to_vec()
-}
+		print("finished!");
+		b"all ok!".to_vec()
+	},
+	test_empty_return NO_DECODE => |_| Vec::new(),
+	test_panic NO_DECODE => |_| panic!("test panic"),
+	test_conditional_panic NO_DECODE => |input: &[u8]| {
+		if input.len() > 0 {
+			panic!("test panic")
+		}
+		input.to_vec()
+	},
+	test_blake2_256 NO_DECODE => |input| blake2_256(input).to_vec(),
+	test_twox_256 NO_DECODE => |input| twox_256(input).to_vec(),
+	test_twox_128 NO_DECODE => |input| twox_128(input).to_vec(),
+	test_ed25519_verify NO_DECODE => |input: &[u8]| {
+		let mut pubkey = [0; 32];
+		let mut sig = [0; 64];
 
-fn test_enumerated_trie_root(_input: &[u8]) -> Vec<u8> {
-	enumerated_trie_root(&[&b"zero"[..], &b"one"[..], &b"two"[..]]).to_vec()
-}
+		pubkey.copy_from_slice(&input[0..32]);
+		sig.copy_from_slice(&input[32..96]);
 
-fn test_data_in(input: &[u8]) -> Vec<u8> {
-	print("set_storage");
-	set_storage(b"input", &input);
-
-	print("storage");
-	let foo = storage(b"foo");
-
-	print("set_storage");
-	set_storage(b"baz", &foo);
-
-	print("finished!");
-	b"all ok!".to_vec()
-}
-
-fn test_empty_return(_input: &[u8]) -> Vec<u8> {
-	Vec::new()
-}
-
-fn test_panic(_input: &[u8]) -> Vec<u8> {
-	panic!("test panic");
-}
-
-fn test_conditional_panic(input: &[u8]) -> Vec<u8> {
-	if input.len() > 0 {
-		panic!("test panic");
+		let msg = b"all ok!";
+		[ed25519_verify(&sig, &msg[..], &pubkey) as u8].to_vec()
+	},
+	test_enumerated_trie_root NO_DECODE => |_| {
+		enumerated_trie_root(&[&b"zero"[..], &b"one"[..], &b"two"[..]]).to_vec()
 	}
-	input.to_vec()
-}
-
-impl_stubs!(test_data_in, test_empty_return, test_panic, test_conditional_panic,
-	test_blake2_256, test_twox_256, test_twox_128, test_ed25519_verify, test_enumerated_trie_root);
+);
