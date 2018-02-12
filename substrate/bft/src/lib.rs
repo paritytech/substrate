fc18524404cc9b952422caa7c62a96dbeec921df
Robert Habermeier <rphmeier@gmail.com>
2018-02-12 16:01:11 +0100

    bft service implementation without I/O

--- substrate/bft/src/lib.rs
+++ substrate/bft/src/lib.rs
@@ -22,20 +22,31 @@ pub mod generic;
 extern crate substrate_codec as codec;
 extern crate substrate_client as client;
 extern crate substrate_primitives as primitives;
+extern crate substrate_state_machine as state_machine;
 extern crate ed25519;
 extern crate tokio_timer;
+extern crate parking_lot;
 
-#[cfg_attr(test, macro_use)]
+#[macro_use]
 extern crate futures;
 
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+
 use client::Client;
+use client::backend::Backend;
 use codec::Slicable;
 use ed25519::Signature;
 use primitives::block::{Block, Header, HeaderHash};
 use primitives::AuthorityId;
+use state_machine::CodeExecutor;
 
-use futures::{Stream, Future};
+use futures::{stream, task, Async, Sink, Future};
+use futures::future::{Executor, ExecuteErrorKind};
+use futures::sync::oneshot;
 use tokio_timer::Timer;
+use parking_lot::Mutex;
 
 pub use generic::InputStreamConcluded;
 
@@ -89,7 +100,7 @@ pub trait Proposer: Sized {
 
     /// Initialize the proposal logic on top of a specific header.
     // TODO: provide state context explicitly?
-    fn init(parent_header: &Header, sign_with: ed25519::Pair) -> Self;
+    fn init(parent_header: &Header, sign_with: Arc<ed25519::Pair>) -> Self;
 
     /// Create a proposal.
     fn propose(&self) -> Self::CreateProposal;
@@ -99,8 +110,8 @@ pub trait Proposer: Sized {
 }
 
 /// Instance of BFT agreement.
-pub struct BftInstance<P> {
-	key: ed25519::Pair, // TODO (now): key changing over time.
+struct BftInstance<P> {
+	key: Arc<ed25519::Pair>,
 	authorities: Vec<AuthorityId>,
 	parent_hash: HeaderHash,
 	timer: Timer,
@@ -184,5 +195,167 @@ impl<P: Proposer> generic::Context for BftInstance<P> {
 
 		Box::new(self.timer.sleep(Duration::from_secs(timeout))
 			.map_err(|_| Error::FaultyTimer))
+	}
+}
+
+type Input = stream::Empty<Communication, Error>;
+
+// "black hole" output sink.
+struct Output;
+
+impl Sink for Output {
+	type SinkItem = Communication;
+	type SinkError = Error;
+
+	fn start_send(&mut self, _item: Communication) -> ::futures::StartSend<Communication, Error> {
+		Ok(::futures::AsyncSink::Ready)
+	}
+
+	fn poll_complete(&mut self) -> ::futures::Poll<(), Error> {
+		Ok(Async::Ready(()))
+	}
+}
+
+/// A future that resolves either when canceled (witnessing a block from the network at same height)
+/// or when agreement completes.
+pub struct BftFuture<P: Proposer, B: Backend, X: CodeExecutor> {
+	inner: generic::Agreement<BftInstance<P>, Input, Output>,
+	cancel: Arc<AtomicBool>,
+	send_task: Option<oneshot::Sender<task::Task>>,
+	client: Arc<Client<B, X>>,
+}
+
+impl<P, B, X> Future for BftFuture<P, B, X>
+	where
+		P: Proposer,
+		B: Backend,
+		X: CodeExecutor,
+		client::error::Error: From<<B::State as state_machine::backend::Backend>::Error>,
+{
+	type Item = ();
+	type Error = ();
+
+	fn poll(&mut self) -> ::futures::Poll<(), ()> {
+		// send the task to the bft service so this can be cancelled.
+		if let Some(sender) = self.send_task.take() {
+			let _ = sender.send(task::current());
+		}
+
+		// service has canceled the future. bail
+		if self.cancel.load(Ordering::Acquire) {
+			return Ok(Async::Ready(()))
+		}
+
+		// TODO: handle this error, at least by logging.
+		let committed = try_ready!(self.inner.poll().map_err(|_| ()));
+
+		// If we didn't see the justification (very unlikely),
+		// we will get the block from the network later.
+		if let Some(justified_block) = committed.candidate {
+			// TODO: import justification alongside.xw
+			let _ = self.client.import_block(
+				justified_block.header,
+				Some(justified_block.transactions),
+			);
+		}
+
+		Ok(Async::Ready(()))
+	}
+}
+
+struct AgreementHandle {
+	cancel: Arc<AtomicBool>,
+	task: Option<oneshot::Receiver<task::Task>>,
+}
+
+impl Drop for AgreementHandle {
+	fn drop(&mut self) {
+		let task = match self.task.take() {
+			Some(t) => t,
+			None => return,
+		};
+
+		// if this fails, the task is definitely not live anyway.
+		if let Ok(task) = task.wait() {
+			self.cancel.store(true, Ordering::Release);
+			task.notify();
+		}
+	}
+}
+
+/// The BftService kicks off the agreement process on top of any blocks it
+/// is notified of.
+pub struct BftService<P, E, B: Backend, X: CodeExecutor> {
+	client: Arc<Client<B, X>>,
+	executor: E,
+	live_agreements: Mutex<HashMap<HeaderHash, AgreementHandle>>,
+	timer: Timer,
+	round_timeout_multiplier: u64,
+	key: Arc<ed25519::Pair>, // TODO: key changing over time.
+	_marker: ::std::marker::PhantomData<P>,
+}
+
+impl<P, E, B, X> BftService<P, E, B, X>
+	where
+		P: Proposer,
+		E: Executor<BftFuture<P, B, X>>,
+		B: Backend,
+		X: CodeExecutor,
+		client::error::Error: From<<B::State as state_machine::backend::Backend>::Error>,
+{
+	/// Signal that a valid block with the given header has been imported.
+	///
+	/// This will begin the consensus process to build a block on top of it.
+	/// If the executor fails to run the future, an error will be returned.
+	pub fn build_upon(&self, header: &Header) -> Result<(), ExecuteErrorKind> {
+		let parent_hash = header.parent_hash.clone();
+		let hash = header.hash();
+		let mut _preempted_consensus = None;
+
+		let proposer = P::init(header, self.key.clone());
+
+		// TODO: check key is one of the authorities.
+		let authorities = Vec::new();
+		let n = authorities.len();
+		let max_faulty = n.saturating_sub(1) / 3;
+
+		let bft_instance = BftInstance {
+			proposer,
+			parent_hash,
+			round_timeout_multiplier: self.round_timeout_multiplier,
+			timer: self.timer.clone(),
+			key: self.key.clone(),
+			authorities: authorities,
+		};
+
+		let agreement = generic::agree(
+			bft_instance,
+			n,
+			max_faulty,
+			stream::empty(),
+			Output,
+		);
+
+		let cancel = Arc::new(AtomicBool::new(false));
+		let (tx, rx) = oneshot::channel();
+
+		self.executor.execute(BftFuture {
+			inner: agreement,
+			cancel: cancel.clone(),
+			send_task: Some(tx),
+			client: self.client.clone(),
+		}).map_err(|e| e.kind())?;
+
+		{
+			let mut live = self.live_agreements.lock();
+			live.insert(hash, AgreementHandle {
+				task: Some(rx),
+				cancel,
+			});
+
+			_preempted_consensus = live.remove(&parent_hash);
+		}
+
+		Ok(())
 	}
 }
