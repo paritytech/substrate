702bf9595f0366e37029eccf93a8620cdcdb21b9
Robert Habermeier <rphmeier@gmail.com>
2018-04-12 13:59:17 +0200

    delay before voting on proposals

--- polkadot/consensus/src/error.rs
+++ polkadot/consensus/src/error.rs
@@ -24,6 +24,11 @@ error_chain! {
 		Bft(::bft::Error, ::bft::ErrorKind);
 	}
 
+	foreign_links {
+		Io(::std::io::Error);
+		SharedIo(::futures::future::SharedError<::std::io::Error>);
+	}
+
 	errors {
 		InvalidDutyRosterLength(expected: usize, got: usize) {
 			description("Duty Roster had invalid length"),
