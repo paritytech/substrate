bb39328ea10dd11994a1d2a6edd318b8425abf30
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-04-18 18:39:53 +0300

    Fix `panic_fmt` signature by adding `_col`
    Wrong `panic_fmt` signature can inhibit some optimizations in LTO mode.

--- polkadot/parachain/test-chains/basic_add/src/wasm.rs
+++ polkadot/parachain/test-chains/basic_add/src/wasm.rs
@@ -25,7 +25,8 @@ use super::{HeadData, BlockData};
 pub extern fn panic_fmt(
     _args: ::core::fmt::Arguments,
     _file: &'static str,
-    _line: u32
+    _line: u32,
+	_col: u32,
 ) -> ! {
     use core::intrinsics;
     unsafe {
