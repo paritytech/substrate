319a93aa8e0b41726788ebee87f8c347bc3007de
Robert Habermeier <rphmeier@gmail.com>
2018-05-08 16:26:25 +0200

    import cached BFT messages into the produced stream

--- substrate/network/src/test/mod.rs
+++ substrate/network/src/test/mod.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
+mod consensus;
 mod sync;
 
 use std::collections::{VecDeque, HashSet, HashMap};
