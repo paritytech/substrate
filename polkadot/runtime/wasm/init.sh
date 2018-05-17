33b68f338f4dee15aec7d219673428b2f412fde8
Robert Habermeier <rphmeier@gmail.com>
2018-05-17 14:26:11 +0200

    install wasm-gc using nightly (#167)

--- polkadot/runtime/wasm/init.sh
+++ polkadot/runtime/wasm/init.sh
@@ -3,5 +3,5 @@
 rustup update nightly
 rustup target add wasm32-unknown-unknown --toolchain nightly
 rustup update stable
-cargo install --git https://github.com/alexcrichton/wasm-gc
-cargo install --git https://github.com/pepyakin/wasm-export-table.git
+cargo +nightly install --git https://github.com/alexcrichton/wasm-gc
+cargo +nightly install --git https://github.com/pepyakin/wasm-export-table.git
