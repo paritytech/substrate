CarLiveChain/polkadot

light client structure + remote call requests

polkadot/api/src/light.rs
@@ -1,0 +1,89 @@
+// Copyright 2017 Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Polkadot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Strongly typed API for light Polkadot client.
+
+use std::sync::Arc;
+use client::backend::Backend;
+use client::{Client, CallExecutor};
+use codec::Slicable;
+use state_machine;
+use primitives::{AccountId, BlockId, Hash, Index, SessionKey, Timestamp};
+use primitives::parachain::DutyRoster;
+use runtime::{Block, UncheckedExtrinsic};
+use {PolkadotApi, BlockBuilder, CheckedBlockId, CheckedId, Result, ErrorKind};
+
+/// Remote polkadot API implementation.
+pub struct RemotePolkadotApi<B: Backend, E: CallExecutor>(pub Arc<Client<B, E>>);
+
+/// Block builder for light client.
+pub struct LightBlockBuilder;
+
+impl<B: Backend, E: CallExecutor> PolkadotApi for RemotePolkadotApi<B, E>
+	where ::client::error::Error: From<<<B as Backend>::State as state_machine::backend::Backend>::Error>
+{
+	type CheckedBlockId = CheckedId;
+	type BlockBuilder = LightBlockBuilder;
+
+	fn check_id(&self, _id: BlockId) -> Result<CheckedId> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn session_keys(&self, at: &CheckedId) -> Result<Vec<SessionKey>> {
+		self.0.executor().call(at.block_id(), "authorities", &[])
+			.and_then(|r| Vec::<SessionKey>::decode(&mut &r.return_data[..])
+				.ok_or("error decoding session keys".into()))
+			.map_err(Into::into)
+	}
+
+	fn validators(&self, _at: &CheckedId) -> Result<Vec<AccountId>> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn random_seed(&self, _at: &Self::CheckedBlockId) -> Result<Hash> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn duty_roster(&self, _at: &CheckedId) -> Result<DutyRoster> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn timestamp(&self, _at: &CheckedId) -> Result<Timestamp> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn evaluate_block(&self, _at: &CheckedId, _block: Block) -> Result<()> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn index(&self, _at: &CheckedId, _account: AccountId) -> Result<Index> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn build_block(&self, _parent: &CheckedId, _timestamp: Timestamp) -> Result<Self::BlockBuilder> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+}
+
+impl BlockBuilder for LightBlockBuilder {
+	fn push_extrinsic(&mut self, _extrinsic: UncheckedExtrinsic) -> Result<()> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn bake(self) -> Block {
+		unimplemented!()
+	}
+}

substrate/client/db/src/lib.rs
@@ -53,14 +53,15 @@ pub struct DatabaseSettings {
 pub fn new_client<E, F>(
 	settings: DatabaseSettings,
 	executor: E,
-	build_genesis: F
-) -> Result<client::Client<Backend, E>, client::error::Error>
+	genesis_builder: F,
+) -> Result<client::Client<Backend, client::LocalCallExecutor<Backend, E>>, client::error::Error>
 	where
 		E: CodeExecutor,
-		F: FnOnce() -> (block::Header, Vec<(Vec<u8>, Vec<u8>)>)
+		F: client::GenesisBuilder,
 {
-	let backend = Backend::new(&settings)?;
-	Ok(client::Client::new(backend, executor, build_genesis)?)
+	let backend = Arc::new(Backend::new(&settings)?);
+	let executor = client::LocalCallExecutor::new(backend.clone(), executor);
+	Ok(client::Client::new(backend, executor, genesis_builder)?)
 }
 
 mod columns {
@@ -252,8 +253,8 @@ impl client::blockchain::Backend for BlockchainDb {
 impl client::backend::BlockImportOperation for BlockImportOperation {
 	type State = DbState;
 
-	fn state(&self) -> Result<&Self::State, client::error::Error> {
-		Ok(&self.pending_state)
+	fn state(&self) -> Result<Option<&Self::State>, client::error::Error> {
+		Ok(Some(&self.pending_state))
 	}
 
 	fn set_block_data(&mut self, header: block::Header, body: Option<block::Body>, justification: Option<primitives::bft::Justification>, is_best: bool) -> Result<(), client::error::Error> {
@@ -396,4 +397,3 @@ impl client::backend::Backend for Backend {
 		}
 	}
 }
-

substrate/client/src/in_mem.rs
@@ -27,7 +27,8 @@ use primitives::block::{self, Id as BlockId, HeaderHash};
 use blockchain::{self, BlockStatus};
 use state_machine::backend::Backend as StateBackend;
 
-fn header_hash(header: &block::Header) -> block::HeaderHash {
+/// Compute block header hash.
+pub fn header_hash(header: &block::Header) -> block::HeaderHash {
 	header.blake2_256().into()
 }
 
@@ -72,14 +73,8 @@ impl Clone for Blockchain {
 }
 
 impl Blockchain {
-	fn id(&self, id: BlockId) -> Option<HeaderHash> {
-		match id {
-			BlockId::Hash(h) => Some(h),
-			BlockId::Number(n) => self.storage.read().hashes.get(&n).cloned(),
-		}
-	}
-
-	fn new() -> Blockchain {
+	/// Create new in-memory blockchain storage.
+	pub fn new() -> Blockchain {
 		Blockchain {
 			storage: RwLock::new(
 				BlockchainStorage {
@@ -92,7 +87,16 @@ impl Blockchain {
 		}
 	}
 
-	fn insert(&self, hash: HeaderHash, header: block::Header, justification: Option<primitives::bft::Justification>, body: Option<block::Body>, is_new_best: bool) {
+	/// Get header hash of given block.
+	pub fn id(&self, id: BlockId) -> Option<HeaderHash> {
+		match id {
+			BlockId::Hash(h) => Some(h),
+			BlockId::Number(n) => self.storage.read().hashes.get(&n).cloned(),
+		}
+	}
+
+	/// Insert block.
+	pub fn insert(&self, hash: HeaderHash, header: block::Header, justification: Option<primitives::bft::Justification>, body: Option<block::Body>, is_new_best: bool) {
 		let number = header.number;
 		let mut storage = self.storage.write();
 		storage.blocks.insert(hash, Block {
@@ -120,7 +124,7 @@ impl Blockchain {
 		let this = self.storage.read();
 		let other = other.storage.read();
 			this.hashes == other.hashes
-		    && this.best_hash == other.best_hash
+			&& this.best_hash == other.best_hash
 			&& this.best_number == other.best_number
 			&& this.genesis_hash == other.genesis_hash
 	}
@@ -163,8 +167,8 @@ impl blockchain::Backend for Blockchain {
 impl backend::BlockImportOperation for BlockImportOperation {
 	type State = state_machine::backend::InMemory;
 
-	fn state(&self) -> error::Result<&Self::State> {
-		Ok(&self.pending_state)
+	fn state(&self) -> error::Result<Option<&Self::State>> {
+		Ok(Some(&self.pending_state))
 	}
 
 	fn set_block_data(&mut self, header: block::Header, body: Option<block::Body>, justification: Option<primitives::bft::Justification>, is_new_best: bool) -> error::Result<()> {

paritytech/substrate

polkadot/api/src/light.rs
@@ -1,0 +1,89 @@
+// Copyright 2017 Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Polkadot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Strongly typed API for light Polkadot client.
+
+use std::sync::Arc;
+use client::backend::Backend;
+use client::{Client, CallExecutor};
+use codec::Slicable;
+use state_machine;
+use primitives::{AccountId, BlockId, Hash, Index, SessionKey, Timestamp};
+use primitives::parachain::DutyRoster;
+use runtime::{Block, UncheckedExtrinsic};
+use {PolkadotApi, BlockBuilder, CheckedBlockId, CheckedId, Result, ErrorKind};
+
+/// Remote polkadot API implementation.
+pub struct RemotePolkadotApi<B: Backend, E: CallExecutor>(pub Arc<Client<B, E>>);
+
+/// Block builder for light client.
+pub struct LightBlockBuilder;
+
+impl<B: Backend, E: CallExecutor> PolkadotApi for RemotePolkadotApi<B, E>
+	where ::client::error::Error: From<<<B as Backend>::State as state_machine::backend::Backend>::Error>
+{
+	type CheckedBlockId = CheckedId;
+	type BlockBuilder = LightBlockBuilder;
+
+	fn check_id(&self, id: BlockId) -> Result<CheckedId> {
+		Ok(CheckedId(id))
+	}
+
+	fn session_keys(&self, at: &CheckedId) -> Result<Vec<SessionKey>> {
+		self.0.executor().call(at.block_id(), "authorities", &[])
+			.and_then(|r| Vec::<SessionKey>::decode(&mut &r.return_data[..])
+				.ok_or("error decoding session keys".into()))
+			.map_err(Into::into)
+	}
+
+	fn validators(&self, _at: &CheckedId) -> Result<Vec<AccountId>> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn random_seed(&self, _at: &Self::CheckedBlockId) -> Result<Hash> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn duty_roster(&self, _at: &CheckedId) -> Result<DutyRoster> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn timestamp(&self, _at: &CheckedId) -> Result<Timestamp> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn evaluate_block(&self, _at: &CheckedId, _block: Block) -> Result<()> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn index(&self, _at: &CheckedId, _account: AccountId) -> Result<Index> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn build_block(&self, _parent: &CheckedId, _timestamp: Timestamp) -> Result<Self::BlockBuilder> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+}
+
+impl BlockBuilder for LightBlockBuilder {
+	fn push_extrinsic(&mut self, _extrinsic: UncheckedExtrinsic) -> Result<()> {
+		Err(ErrorKind::UnknownRuntime.into())
+	}
+
+	fn bake(self) -> Block {
+		unimplemented!()
+	}
+}

substrate/client/db/src/lib.rs
@@ -65,14 +65,15 @@ pub struct DatabaseSettings {
 pub fn new_client<E, F>(
 	settings: DatabaseSettings,
 	executor: E,
-	build_genesis: F
-) -> Result<client::Client<Backend, E>, client::error::Error>
+	genesis_builder: F,
+) -> Result<client::Client<Backend, client::LocalCallExecutor<Backend, E>>, client::error::Error>
 	where
 		E: CodeExecutor,
-		F: FnOnce() -> (block::Header, Vec<(Vec<u8>, Vec<u8>)>)
+		F: client::GenesisBuilder,
 {
-	let backend = Backend::new(&settings)?;
-	Ok(client::Client::new(backend, executor, build_genesis)?)
+	let backend = Arc::new(Backend::new(&settings)?);
+	let executor = client::LocalCallExecutor::new(backend.clone(), executor);
+	Ok(client::Client::new(backend, executor, genesis_builder)?)
 }
 
 mod columns {
@@ -265,8 +266,8 @@ pub struct BlockImportOperation {
 impl client::backend::BlockImportOperation for BlockImportOperation {
 	type State = DbState;
 
-	fn state(&self) -> Result<&Self::State, client::error::Error> {
-		Ok(&self.old_state)
+	fn state(&self) -> Result<Option<&Self::State>, client::error::Error> {
+		Ok(Some(&self.old_state))
 	}
 
 	fn set_block_data(&mut self, header: block::Header, body: Option<block::Body>, justification: Option<primitives::bft::Justification>, is_best: bool) -> Result<(), client::error::Error> {

substrate/client/src/in_mem.rs
@@ -26,7 +26,8 @@ use primitives::block::{self, Id as BlockId, HeaderHash};
 use blockchain::{self, BlockStatus};
 use state_machine::backend::{Backend as StateBackend, InMemory};
 
-fn header_hash(header: &block::Header) -> block::HeaderHash {
+/// Compute block header hash.
+pub fn header_hash(header: &block::Header) -> block::HeaderHash {
 	header.blake2_256().into()
 }
 
@@ -65,14 +66,8 @@ impl Clone for Blockchain {
 }
 
 impl Blockchain {
-	fn id(&self, id: BlockId) -> Option<HeaderHash> {
-		match id {
-			BlockId::Hash(h) => Some(h),
-			BlockId::Number(n) => self.storage.read().hashes.get(&n).cloned(),
-		}
-	}
-
-	fn new() -> Blockchain {
+	/// Create new in-memory blockchain storage.
+	pub fn new() -> Blockchain {
 		Blockchain {
 			storage: RwLock::new(
 				BlockchainStorage {
@@ -85,7 +80,16 @@ impl Blockchain {
 		}
 	}
 
-	fn insert(&self, hash: HeaderHash, header: block::Header, justification: Option<primitives::bft::Justification>, body: Option<block::Body>, is_new_best: bool) {
+	/// Get header hash of given block.
+	pub fn id(&self, id: BlockId) -> Option<HeaderHash> {
+		match id {
+			BlockId::Hash(h) => Some(h),
+			BlockId::Number(n) => self.storage.read().hashes.get(&n).cloned(),
+		}
+	}
+
+	/// Insert block.
+	pub fn insert(&self, hash: HeaderHash, header: block::Header, justification: Option<primitives::bft::Justification>, body: Option<block::Body>, is_new_best: bool) {
 		let number = header.number;
 		let mut storage = self.storage.write();
 		storage.blocks.insert(hash, Block {
@@ -113,7 +117,7 @@ impl Blockchain {
 		let this = self.storage.read();
 		let other = other.storage.read();
 			this.hashes == other.hashes
-		    && this.best_hash == other.best_hash
+			&& this.best_hash == other.best_hash
 			&& this.best_number == other.best_number
 			&& this.genesis_hash == other.genesis_hash
 	}
@@ -163,8 +167,8 @@ pub struct BlockImportOperation {
 impl backend::BlockImportOperation for BlockImportOperation {
 	type State = InMemory;
 
-	fn state(&self) -> error::Result<&Self::State> {
-		Ok(&self.old_state)
+	fn state(&self) -> error::Result<Option<&Self::State>> {
+		Ok(Some(&self.old_state))
 	}
 
 	fn set_block_data(&mut self, header: block::Header, body: Option<block::Body>, justification: Option<primitives::bft::Justification>, is_new_best: bool) -> error::Result<()> {