512931b8cecd0aa1d9df5424015b5694400f72e2
Gav Wood <gavin@parity.io>
2018-05-31 18:31:03 +0200

    Backport of #191 (#193)
    * Merge branch 'master' into gav-result-dispatch
    
    * Fix merge.

--- substrate/network/src/protocol.rs
+++ substrate/network/src/protocol.rs
@@ -114,13 +114,13 @@ impl Protocol {
 	/// Create a new instance.
 	pub fn new(config: ProtocolConfig, chain: Arc<Client>, transaction_pool: Arc<TransactionPool>) -> error::Result<Protocol>  {
 		let info = chain.info()?;
-		let best_hash = info.chain.best_hash;
+		let sync = ChainSync::new(&info);
 		let protocol = Protocol {
 			config: config,
 			chain: chain,
 			genesis_hash: info.chain.genesis_hash,
-			sync: RwLock::new(ChainSync::new(&info)),
-			consensus: Mutex::new(Consensus::new(best_hash)),
+			sync: RwLock::new(sync),
+			consensus: Mutex::new(Consensus::new()),
 			peers: RwLock::new(HashMap::new()),
 			handshaking_peers: RwLock::new(HashMap::new()),
 			transaction_pool: transaction_pool,
@@ -520,7 +520,7 @@ impl Protocol {
 			}
 		}
 
-		self.consensus.lock().collect_garbage(Some((hash, &header)));
+		self.consensus.lock().collect_garbage(Some(&header));
 	}
 
 	pub fn transactions_stats(&self) -> BTreeMap<ExtrinsicHash, TransactionStats> {
