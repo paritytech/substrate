0a500cefc6bbba5404925740e7327e47ac7c4e43
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-06-07 17:49:54 +0300

    Fix build of test-chains.

--- polkadot/parachain/test-chains/build_test_chains.sh
+++ polkadot/parachain/test-chains/build_test_chains.sh
@@ -7,8 +7,7 @@ do
   i=${i%/}
   cd $i
 
-  # TODO: stop using exact nightly when wee-alloc works on normal nightly.
-  RUSTFLAGS="-C link-arg=--import-memory" cargo +nightly-2018-03-07 build --target=wasm32-unknown-unknown --release --no-default-features
+  RUSTFLAGS="-C link-arg=--import-memory" cargo +nightly build --target=wasm32-unknown-unknown --release --no-default-features
   wasm-gc target/wasm32-unknown-unknown/release/$i.wasm ../../tests/res/$i.wasm
   cd ..
 done
