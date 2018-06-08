28b4443462618d9447c06d483d3487ae2f850aeb
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-06-08 18:07:15 +0300

    Refactor build scripts.

--- build.sh
+++ build.sh
@@ -1,13 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-# NOTE `cargo install wasm-gc` before running this script.
-# NOTE `cargo install --git https://github.com/pepyakin/wasm-export-table.git`
+# This script assumes that all pre-requisites are installed.
 
 set -e
+
+source `dirname "$0"`/common.sh
+
 export CARGO_INCREMENTAL=0
 
-cd demo/runtime/wasm && ./build.sh && cd ../../..
-cd substrate/executor/wasm && ./build.sh && cd ../../..
-cd substrate/test-runtime/wasm && ./build.sh && cd ../../..
-cd polkadot/runtime/wasm && ./build.sh && cd ../../..
-cd polkadot/parachain/test-chains && ./build.sh && cd ../../..
+# Save current directory.
+pushd .
+
+cd $ROOT
+
+for SRC in "${SRCS[@]}"
+do
+  echo "*** Building wasm binaries in $SRC"
+  cd $SRC
+
+  ./build.sh
+
+  cd - >> /dev/null
+done
+
+# Restore initial directory.
+popd
---
+++ common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ROOT=`dirname "$0"`
+
+# A list of directories which contain wasm projects.
+SRCS=(
+	"polkadot/runtime/wasm"
+	"substrate/executor/wasm"
+	"demo/runtime/wasm"
+	"substrate/test-runtime/wasm"
+	"polkadot/parachain/test-chains/basic_add"
+)
+
+# Make pushd/popd silent.
+
+pushd () {
+	command pushd "$@" > /dev/null
+}
+
+popd () {
+	command popd "$@" > /dev/null
+}
