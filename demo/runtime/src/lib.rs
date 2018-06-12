584d3e15dcf9fb08f762285d87e883ee7865fa3e
Gav <gavin@parity.io>
2018-06-12 15:16:35 +0200

    --all builds

--- demo/runtime/src/lib.rs
+++ demo/runtime/src/lib.rs
@@ -163,13 +163,13 @@ pub type Header = generic::Header<BlockNumber, BlakeTwo256, Vec<u8>>;
 /// Block type as expected by this runtime.
 pub type Block = generic::Block<Header, UncheckedExtrinsic>;
 /// Unchecked extrinsic type as expected by this runtime.
-pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Index, Call, Signature, Staking>;
+pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Index, Call, Signature>;
 /// Extrinsic type as expected by this runtime. This is not the type that is signed.
 pub type Extrinsic = generic::Extrinsic<Address, Index, Call>;
 /// Extrinsic type that is signed.
 pub type BareExtrinsic = generic::Extrinsic<AccountId, Index, Call>;
 /// Executive: handles dispatch to the various modules.
-pub type Executive = executive::Executive<Concrete, Block, Staking,
+pub type Executive = executive::Executive<Concrete, Block, Staking, Staking,
 	(((((), Council), Democracy), Staking), Session)>;
 
 impl_outer_config! {
--- substrate/test-runtime/src/lib.rs
+++ substrate/test-runtime/src/lib.rs
@@ -51,7 +51,7 @@ pub mod system;
 use rstd::prelude::*;
 use codec::Slicable;
 
-use runtime_primitives::traits::{Checkable, BlakeTwo256};
+use runtime_primitives::traits::{BlindCheckable, BlakeTwo256};
 use runtime_primitives::Ed25519Signature;
 pub use primitives::hash::H256;
 
@@ -101,9 +101,13 @@ impl Slicable for Extrinsic {
 	}
 }
 
-impl Checkable for Extrinsic {
+impl BlindCheckable for Extrinsic {
 	type Checked = Self;
+	type Address = AccountId;
 
+	fn sender(&self) -> &Self::Address {
+		&self.transfer.from
+	}
 	fn check(self) -> Result<Self, &'static str> {
 		if ::runtime_primitives::verify_encoded_lazy(&self.signature, &self.transfer, &self.transfer.from) {
 			Ok(self)
