7c6f9be7af12b6af4297b1c534510ba0f59caaed
Gav Wood <gavin@parity.io>
2018-07-11 13:01:16 +0200

    PoC-2 tweaks (#293)
    - Rename poc-2 to staging
    - Make telemetry default for global chains

--- polkadot/service/src/chain_spec.rs
+++ polkadot/service/src/chain_spec.rs
@@ -136,7 +136,7 @@ impl ChainSpec {
 		Self::from_embedded(include_bytes!("../res/poc-1.json"))
 	}
 
-	fn poc_2_testnet_config_genesis() -> Genesis {
+	fn staging_testnet_config_genesis() -> Genesis {
 		let initial_authorities = vec![
 			hex!["82c39b31a2b79a90f8e66e7a77fdb85a4ed5517f2ae39f6a80565e8ecae85cf5"].into(),
 			hex!["4de37a07567ebcbf8c64568428a835269a566723687058e017b6d69db00a77e7"].into(),
@@ -200,16 +200,16 @@ impl ChainSpec {
 			}),
 		})
 	}
-	/// PoC-2 testnet config.
-	pub fn poc_2_testnet_config() -> Self {
+	/// Staging testnet config.
+	pub fn staging_testnet_config() -> Self {
 		let boot_nodes = vec![
 			"enode://a93a29fa68d965452bf0ff8c1910f5992fe2273a72a1ee8d3a3482f68512a61974211ba32bb33f051ceb1530b8ba3527fc36224ba6b9910329025e6d9153cf50@104.211.54.233:30333".into(),
 			"enode://051b18f63a316c4c5fef4631f8c550ae0adba179153588406fac3e5bbbbf534ebeda1bf475dceda27a531f6cdef3846ab6a010a269aa643a1fec7bff51af66bd@104.211.48.51:30333".into(),
 			"enode://c831ec9011d2c02d2c4620fc88db6d897a40d2f88fd75f47b9e4cf3b243999acb6f01b7b7343474650b34eeb1363041a422a91f1fc3850e43482983ee15aa582@104.211.48.247:30333".into(),
 		];
 		ChainSpec {
-			spec: ChainSpecFile { name: "PoC-2 Testnet".to_owned(), boot_nodes },
-			genesis: GenesisSource::Factory(Self::poc_2_testnet_config_genesis),
+			spec: ChainSpecFile { name: "Staging Testnet".to_owned(), boot_nodes },
+			genesis: GenesisSource::Factory(Self::staging_testnet_config_genesis),
 		}
 	}
 
