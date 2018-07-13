ab89ef968b56690b37b1cd81bc7ac637a936a600
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-07-13 14:49:20 +0200

    Fix test year

--- substrate/network-libp2p/tests/tests.rs
+++ substrate/network-libp2p/tests/tests.rs
@@ -1,4 +1,4 @@
-// Copyright 2015-2017 Parity Technologies (UK) Ltd.
+// Copyright 2018 Parity Technologies (UK) Ltd.
 // This file is part of Parity.
 
 // Parity is free software: you can redistribute it and/or modify
