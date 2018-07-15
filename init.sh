1369641406994d32443086aede110d66b3a425f0
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-07-15 14:09:29 +0200

    Use --export-table from the latest LLD (#316)
    * Remove unused build.sh
    
    * Don't use wasm-export-table anymore.
    
    * Rebuild binaries.
    
    * Fix build script.

--- init.sh
+++ init.sh
@@ -11,9 +11,3 @@ rustup update stable
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
 	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc
-
-# At the moment of writing, rustc still uses LLD 6 which produces wasm binaries
-# that don't export a table. Meanwhile, we are waiting for LLD 7 to come
-# in rustc we could use this handy little tool.
-command -v wasm-export-table || \
-	cargo +nightly install --git https://github.com/pepyakin/wasm-export-table.git
--- substrate/executor/wasm/build.sh
+++ substrate/executor/wasm/build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-cargo +nightly build --target=wasm32-unknown-unknown --release
+RUSTFLAGS="-C link-arg=--export-table" cargo +nightly build --target=wasm32-unknown-unknown --release
 for i in test
 do
-	# Add export of the default table under name 'table'.
-	wasm-export-table target/wasm32-unknown-unknown/release/runtime_$i.wasm target/wasm32-unknown-unknown/release/runtime_$i.table.wasm
-	cp target/wasm32-unknown-unknown/release/runtime_$i.table.wasm target/wasm32-unknown-unknown/release/runtime_$i.wasm
 	wasm-gc target/wasm32-unknown-unknown/release/runtime_$i.wasm target/wasm32-unknown-unknown/release/runtime_$i.compact.wasm
 done
