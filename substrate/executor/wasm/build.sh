606fa87be456cbd630bc1dfd4680423f1898e902
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-07-15 10:20:00 +0200

    Fix build script.

--- substrate/executor/wasm/build.sh
+++ substrate/executor/wasm/build.sh
@@ -4,6 +4,5 @@ set -e
 RUSTFLAGS="-C link-arg=--export-table" cargo +nightly build --target=wasm32-unknown-unknown --release
 for i in test
 do
-	cp target/wasm32-unknown-unknown/release/runtime_$i.table.wasm target/wasm32-unknown-unknown/release/runtime_$i.wasm
 	wasm-gc target/wasm32-unknown-unknown/release/runtime_$i.wasm target/wasm32-unknown-unknown/release/runtime_$i.compact.wasm
 done
