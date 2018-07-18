1e418445d140cc87d7a9d9bb97b81cb3c2fa109c
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-07-18 13:15:04 +0200

    Explicitely connect to bootnodes

--- substrate/network-libp2p/src/service.rs
+++ substrate/network-libp2p/src/service.rs
@@ -480,6 +480,26 @@ fn init_thread(
 		}
 	}
 
+	// Explicitely connect to the boostrap nodes as a temporary measure.
+	for bootnode in shared.config.boot_nodes.iter() {
+		// TODO: this code is copy-pasted from `network_state`, but it is
+		// temporary anyway
+		let mut addr: Multiaddr = bootnode.parse()
+			.map_err(|_| ErrorKind::AddressParse)?;
+		let p2p_component = addr.pop().ok_or(ErrorKind::AddressParse)?;
+		let peer_id = match p2p_component {
+			AddrComponent::P2P(key) | AddrComponent::IPFS(key) =>
+				PeerstorePeerId::from_bytes(key).map_err(|_| ErrorKind::AddressParse)?,
+			_ => return Err(ErrorKind::BadProtocol.into()),
+		};
+
+		trace!(target: "sub-libp2p", "Dialing bootnode {:?}", peer_id);
+		for proto in shared.protocols.read().0.clone().into_iter() {
+			open_peer_custom_proto(shared.clone(), transport.clone(),
+				proto, peer_id.clone(), &swarm_controller)
+		}
+	}
+
 	// Start connecting to nodes now.
 	connect_to_nodes(shared.clone(), transport.clone(), &swarm_controller);
 
