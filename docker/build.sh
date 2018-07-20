26605da2b285bcaaa1d7934c0337662cd7ffb702
chevdor <chevdor@gmail.com>
2018-07-20 09:18:51 +0200

    Add PORT, VOLUME and reduce size of the docker image significantly
    Fix doc and reduce image size
    
    Fix #375

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+# Find the current version from Cargo.toml
 VERSION=`grep "^version" ../Cargo.toml | egrep -o "([0-9\.]+)"`
+GITUSER=chevdor
+GITREPO=polkadot
 
-echo "Building polkadot:$VERSION docker image, hang on!"
-docker build --build-arg PROFILE=release -t polkadot:$VERSION .
+# Build the image
+echo "Building ${GITREPO}:$VERSION docker image, hang on!"
+time docker build --build-arg PROFILE=release -t ${GITUSER}/${GITREPO}:$VERSION .
 
+# Show the list of available images for this repo
 echo "Image is ready"
-docker images | grep polkadot
+docker images | grep ${GITREPO}
+
+echo -e "\nIf you just built the latest, you may want to update your tag:"
+echo " $ docker tag ${GITUSER}/${GITREPO}:$VERSION ${GITUSER}/${GITREPO}:latest"
