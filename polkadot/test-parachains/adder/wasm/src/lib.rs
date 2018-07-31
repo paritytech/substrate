b393ae9fafdb06758e9727f6180c7e67483bf7fe
Robert Habermeier <rphmeier@gmail.com>
2018-07-31 15:26:01 +0200

    final grumbles

--- polkadot/test-parachains/adder/wasm/src/lib.rs
+++ polkadot/test-parachains/adder/wasm/src/lib.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! WASM validation for basic-add parachain.
+//! WASM validation for adder parachain.
 
 #![no_std]
 
--- polkadot/test-parachains/build.sh
+++ polkadot/test-parachains/build.sh
@@ -6,7 +6,7 @@ export RUSTFLAGS="-C link-arg=--import-memory -C lto=fat -C panic=abort"
 
 for i in adder
 do
-    cd $i/wasm
+	cd $i/wasm
 	cargo +nightly build --target=wasm32-unknown-unknown --release --no-default-features --target-dir target
 	wasm-gc target/wasm32-unknown-unknown/release/$i'_'wasm.wasm target/wasm32-unknown-unknown/release/$i.wasm
 	cp target/wasm32-unknown-unknown/release/$i.wasm ../../../parachain/tests/res/
