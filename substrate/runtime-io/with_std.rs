9e4266897687b67c2978f1a2be3e6bc27d0b0c90
David Palm <dvdplm@gmail.com>
2018-08-01 19:53:20 +0200

    Use BlakeHasher in runtime-io

--- substrate/runtime-io/with_std.rs
+++ substrate/runtime-io/with_std.rs
@@ -23,10 +23,9 @@ extern crate substrate_primitives as primitives;
 extern crate substrate_state_machine;
 extern crate triehash;
 extern crate ed25519;
-extern crate hashdb;
-extern crate plain_hasher;
-extern crate rlp;
 
+#[cfg(test)]
+extern crate rlp;
 #[cfg(test)]
 extern crate heapsize;
 
@@ -35,26 +34,14 @@ pub extern crate substrate_codec as codec;
 // re-export hashing functions.
 pub use primitives::{blake2_256, twox_128, twox_256};
 
+pub use primitives::BlakeHasher;
 pub use substrate_state_machine::{Externalities, TestExternalities};
 use primitives::hexdisplay::HexDisplay;
 use primitives::H256;
-use hashdb::Hasher;
-use plain_hasher::PlainHasher;
 
 // TODO: use the real error, not NoError.
 
-// TODO: This is just a sanity check toy impl of `Hasher` – this needs to go somewhere else and possibly be renamed to `Hash`?
-pub struct MyHasher;
-impl Hasher for MyHasher {
-	type Out = H256;
-	type StdHasher = PlainHasher;
-	const LENGTH:usize = 32;
-	fn hash(x: &[u8]) -> Self::Out {
-		blake2_256(x).into()
-	}
-}
-
-environmental!(ext: trait Externalities<MyHasher>);
+environmental!(ext: trait Externalities<BlakeHasher>);
 
 /// Get `key` from storage and return a `Vec`, empty if there's a problem.
 pub fn storage(key: &[u8]) -> Option<Vec<u8>> {
@@ -147,7 +134,7 @@ pub fn ed25519_verify<P: AsRef<[u8]>>(sig: &[u8; 64], msg: &[u8], pubkey: P) -> 
 /// Execute the given closure with global function available whose functionality routes into the
 /// externalities `ext`. Forwards the value that the closure returns.
 // TODO: need a concrete hasher here due to limitations of the `environmental!` macro, otherwise a type param would have been fine I think.
-pub fn with_externalities<R, F: FnOnce() -> R>(ext: &mut Externalities<MyHasher>, f: F) -> R {
+pub fn with_externalities<R, F: FnOnce() -> R>(ext: &mut Externalities<BlakeHasher>, f: F) -> R {
 	ext::using(ext, f)
 }
 
@@ -240,7 +227,7 @@ mod std_tests {
 
 	#[test]
 	fn storage_works() {
-		let mut t = TestExternalities::<MyHasher>::new();
+		let mut t = TestExternalities::<BlakeHasher>::new();
 		assert!(with_externalities(&mut t, || {
 			assert_eq!(storage(b"hello"), None);
 			set_storage(b"hello", b"world");
@@ -261,7 +248,7 @@ mod std_tests {
 
 	#[test]
 	fn read_storage_works() {
-		let mut t: TestExternalities<MyHasher> = map![
+		let mut t: TestExternalities<BlakeHasher> = map![
 			b":test".to_vec() => b"\x0b\0\0\0Hello world".to_vec()
 		];
 
@@ -277,7 +264,7 @@ mod std_tests {
 
 	#[test]
 	fn clear_prefix_works() {
-		let mut t: TestExternalities<MyHasher> = map![
+		let mut t: TestExternalities<BlakeHasher> = map![
 			b":a".to_vec() => b"\x0b\0\0\0Hello world".to_vec(),
 			b":abcd".to_vec() => b"\x0b\0\0\0Hello world".to_vec(),
 			b":abc".to_vec() => b"\x0b\0\0\0Hello world".to_vec(),
