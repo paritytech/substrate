d7c09357027b9e6748264b2e3e12b703be6eced5
David Palm <dvdplm@gmail.com>
2018-08-03 15:12:07 +0200

    Fix service

--- substrate/service/src/components.rs
+++ substrate/service/src/components.rs
@@ -28,6 +28,7 @@ use substrate_executor::{NativeExecutor, NativeExecutionDispatch};
 use extrinsic_pool::{txpool::Options as ExtrinsicPoolOptions, api::ExtrinsicPool as ExtrinsicPoolApi};
 use runtime_primitives::{traits::Block as BlockT, traits::Header as HeaderT, generic::BlockId, BuildStorage};
 use config::Configuration;
+use primitives::{BlakeHasher, BlakeRlpCodec};
 
 // Type aliases.
 // These exist mainly to avoid typing `<F as Factory>::Foo` all over the code.
@@ -152,9 +153,9 @@ pub trait Components {
 	/// Associated service factory.
 	type Factory: ServiceFactory;
 	/// Client backend.
-	type Backend: 'static + client::backend::Backend<FactoryBlock<Self::Factory>>;
+	type Backend: 'static + client::backend::Backend<FactoryBlock<Self::Factory>, BlakeHasher, BlakeRlpCodec>;
 	/// Client executor.
-	type Executor: 'static + client::CallExecutor<FactoryBlock<Self::Factory>> + Send + Sync;
+	type Executor: 'static + client::CallExecutor<FactoryBlock<Self::Factory>, BlakeHasher, BlakeRlpCodec> + Send + Sync;
 	/// Extrinsic pool type.
 	type ExtrinsicPool: ExtrinsicPool<FactoryBlock<Self::Factory>>;
 
