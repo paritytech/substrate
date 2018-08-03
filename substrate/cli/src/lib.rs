a88c4af88b703156eb42ccea0e0894f1035cbdb8
Simon Littlejohns <39836665+simonljs@users.noreply.github.com>
2018-08-03 15:21:57 +0100

    Update lib.rs

--- substrate/cli/src/lib.rs
+++ substrate/cli/src/lib.rs
@@ -163,8 +163,7 @@ where
 {
 	panic_hook::set();
 
-	let full_version = service::Configuration::<<F>::Configuration, <F>::Genesis>
-		::full_version_from_strs(version.version, version.commit);
+	let full_version = service::config::full_version_from_strs(version.version, version.commit);
 
 	let yaml = format!(include_str!("./cli.yml"),
 		name = version.executable_name,
