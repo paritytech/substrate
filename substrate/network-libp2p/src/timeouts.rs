5a77b2a1ab0a9e3839b4aeb67d55f53d37b9db26
Guanqun Lu <guanqun.lu@gmail.com>
2018-08-06 19:27:14 +0800

    remove the extra ? in the license header (#500)

--- substrate/network-libp2p/src/timeouts.rs
+++ substrate/network-libp2p/src/timeouts.rs
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.?
+// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
 use futures::{Async, future, Future, Poll, stream, Stream, sync::mpsc};
 use std::io::{Error as IoError, ErrorKind as IoErrorKind};
