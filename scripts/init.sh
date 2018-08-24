8e2622c5a195c17bb76f3d6a6d340a71e96c0396
ddorgan <david@peersystems.co.uk>
2018-08-24 14:43:49 +0200

    cargo --force to allow CI to build. (#599)

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,4 +13,4 @@ rustup target add wasm32-unknown-unknown --toolchain nightly
 
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
-	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc
+	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
