aca00b28ad0f3c8a806247fd59ed0712734f185b
Guanqun Lu <guanqun.lu@gmail.com>
2018-08-27 16:42:50 +0800

    rename Polkadot to Substrate in the license header via following four commands (#614)
    git grep -l 'This file is part of Polkadot' | xargs sed -i 's/This file is part of Polkadot/This file is part of Substrate/g'
        git grep -l 'Polkadot is free software' | xargs sed -i 's/Polkadot is free software/Substrate is free software/g'
        git grep -l 'Polkadot is distributed in the hope' | xargs sed -i 's/Polkadot is distributed in the hope/Substrate is distributed in the hope/g'
        git grep -l 'along with Polkadot' | xargs sed -i 's/along with Polkadot/along with Substrate/g'

--- substrate/network/src/service.rs
+++ substrate/network/src/service.rs
@@ -1,5 +1,5 @@
 // Copyright 2017 Parity Technologies (UK) Ltd.
-// This file is part of Polkadot.
+// This file is part of Substrate.
 
 // Substrate is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
