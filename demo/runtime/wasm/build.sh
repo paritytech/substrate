d5a02709ca8b0ee40704941822937f362fdfc4e6
arkpar <arkady.paronyan@gmail.com>
2018-09-04 17:02:38 +0200

    Reverted toolchain change

--- demo/runtime/wasm/build.sh
+++ demo/runtime/wasm/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-cargo +nightly-2018-08-27 build --target=wasm32-unknown-unknown --release
+cargo +nightly build --target=wasm32-unknown-unknown --release
 for i in demo_runtime
 do
 	wasm-gc target/wasm32-unknown-unknown/release/$i.wasm target/wasm32-unknown-unknown/release/$i.compact.wasm
