1c0b385898870f8f4f8dd25cefe4428811d8585c
David Palm <dvdplm@gmail.com>
2018-09-05 12:17:53 +0200

    Missing bounds

--- substrate/runtime-io/without_std.rs
+++ substrate/runtime-io/without_std.rs
@@ -177,7 +177,7 @@ pub fn enumerated_trie_root<H: Hasher + ExternTrieCrypto>(values: &[&[u8]]) -> [
 
 /// A trie root formed from the iterated items.
 pub fn trie_root<
-	H,
+	H: Hasher + ExternTrieCrypto,
 	I: IntoIterator<Item = (A, B)>,
 	A: AsRef<[u8]> + Ord,
 	B: AsRef<[u8]>,
@@ -189,7 +189,7 @@ pub fn trie_root<
 
 /// A trie root formed from the enumerated items.
 pub fn ordered_trie_root<
-	H,
+	H: Hasher + ExternTrieCrypto,
 	I: IntoIterator<Item = A>,
 	A: AsRef<[u8]>
 >(_input: I) -> [u8; 32] {
