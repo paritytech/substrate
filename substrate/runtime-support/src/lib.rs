3d027c3c26ddbafef7dbc6df98060904831b9d0d
Svyatoslav Nikolsky <svyatonik@gmail.com>
2018-09-05 14:36:23 +0300

    DigestItem trait (v2) (#650)
    * DigestItem trait
    
    * removed autoimpl in impl_outer_log
    
    * StubDigestItem -> ()

--- substrate/runtime-support/src/lib.rs
+++ substrate/runtime-support/src/lib.rs
@@ -121,3 +121,27 @@ macro_rules! impl_outer_event {
 		)*
 	}
 }
+
+#[macro_export]
+macro_rules! impl_outer_log {
+
+	($(#[$attr:meta])* pub enum $name:ident for $trait:ident { $( $module:ident ),* }) => {
+		// Workaround for https://github.com/rust-lang/rust/issues/26925 . Remove when sorted.
+		#[derive(Clone, PartialEq, Eq, Encode, Decode)]
+		#[cfg_attr(feature = "std", derive(Debug, Serialize, Deserialize))]
+		$(#[$attr])*
+		#[allow(non_camel_case_types)]
+		pub enum $name {
+			$(
+				$module($module::Log<$trait>),
+			)*
+		}
+		$(
+			impl From<$module::Log<$trait>> for $name {
+				fn from(x: $module::Log<$trait>) -> Self {
+					$name::$module(x)
+				}
+			}
+		)*
+	};
+}
