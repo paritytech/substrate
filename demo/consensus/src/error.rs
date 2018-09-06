dbf6d0ca1e9549e69be298e8753bf5e9b6ebf412
arkpar <arkady.paronyan@gmail.com>
2018-09-06 12:25:08 +0200

    2017->2018

--- demo/consensus/src/error.rs
+++ demo/consensus/src/error.rs
@@ -1,4 +1,4 @@
-// Copyright 2017 Parity Technologies (UK) Ltd.
+// Copyright 2018 Parity Technologies (UK) Ltd.
 // This file is part of Substrate Demo.
 
 // Substrate Demo is free software: you can redistribute it and/or modify
