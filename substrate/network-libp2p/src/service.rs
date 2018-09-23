9b506ec881b1e605be88177b2b6d84d6d159aa00
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-09-23 16:36:49 +0200

    Fix destroying nodes when we shouldn't

--- substrate/network-libp2p/src/service.rs
+++ substrate/network-libp2p/src/service.rs
@@ -372,10 +372,18 @@ fn init_thread(
 					local_address: None,	// TODO: fill
 				});
 			},
-			ServiceEvent::NodeClosed { node_index, closed_custom_protocols: protocols } |
-			ServiceEvent::ClosedCustomProtocols { node_index, protocols } => {
+			ServiceEvent::NodeClosed { node_index, closed_custom_protocols } => {
 				let old = peers.lock().remove(&node_index);
 				debug_assert!(old.is_some());
+				for protocol in protocols {
+					registered_custom.find_protocol(protocol)
+					.expect("we passed a list of protocols when building the service, and never \
+						modify that list ; therefore all the reported ids should always be valid")
+						.custom_data()
+						.disconnected(&ctxt!(protocol, node_index), &node_index);
+				}
+			},
+			ServiceEvent::ClosedCustomProtocols { node_index, protocols } => {
 				for protocol in protocols {
 					registered_custom.find_protocol(protocol)
 					.expect("we passed a list of protocols when building the service, and never \
