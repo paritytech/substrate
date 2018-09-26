9cdd3fc181ddde1966c208736865c20ef892ca9c
Gav Wood <github@gavwood.com>
2018-09-26 17:57:19 +0100

    Whitespace

--- node/consensus/src/lib.rs
+++ node/consensus/src/lib.rs
@@ -1,10 +1,8 @@
 // Copyright 2018 Parity Technologies (UK) Ltd.
 // This file is part of Substrate.
 
-
 // Substrate is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
