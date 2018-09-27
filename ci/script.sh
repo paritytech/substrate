282aca861d113c3a3a46946a38f3cb40b919baf0
Bastian Köcher <git@kchr.de>
2018-09-27 17:21:30 +0200

    Enable warnings about unused crates in the CI

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+# Enable warnings about unused extern crates
+export RUSTFLAGS=" -W unused-extern-crates"
+
 # Install rustup and the specified rust toolchain.
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=$RUST_TOOLCHAIN -y
 
