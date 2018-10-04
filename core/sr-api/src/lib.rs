23461a866681d920451bd2b98fa01fc72bd3f0e9
Bastian Köcher <git@kchr.de>
2018-10-04 15:47:50 +0200

    Adds `impl_apis!` macro for implementing the new API traits

--- core/sr-api/src/lib.rs
+++ core/sr-api/src/lib.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-//! API's for interfacing with the runtime.
+//! API's for interfacing with the runtime via native/wasm.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
@@ -23,11 +23,46 @@ extern crate sr_primitives as primitives;
 extern crate parity_codec as codec;
 extern crate sr_version as runtime_version;
 
-use primitives::{ApplyResult, traits::Block as BlockT, generic::BlockId};
+use primitives::{ApplyResult, traits::Block as BlockT};
+#[cfg(feature = "api-for-client")]
+use primitives::generic::BlockId;
 use runtime_version::RuntimeVersion;
 use rstd::vec::Vec;
+#[cfg(feature = "api-for-client")]
 use codec::{Encode, Decode};
 
+/// Declare an API trait.
+///
+/// # Example:
+///
+/// ```
+/// decl_api!{
+///     pub trait Test<Event> {
+///         fn test<AccountId>(event: Event) -> AccountId;
+///     }
+/// }
+/// ```
+///
+/// Will result in the following declaration:
+///
+/// ```
+/// pub trait Test<Event, AccountId> {
+///     fn test(event: Event) -> AccountId;
+/// }
+/// ```
+///
+/// By enabling the `api-for-client` feature, the declaration will change to the following:
+///
+/// ```
+/// pub trait Test<Block: BlockT, Event> {
+///     type Error;
+///     fn test<AccountId: Encode + Decode>(&self, at: &BlockId<Block>, event: Event) -> Result<Event, Self::Error>;
+/// }
+/// ```
+///
+/// The declaration generated with the `api-for-client` feature enabled, should be used by the client
+/// in core. Without the feature being enabled, the resulting declaration should be used in
+/// conjunction with the `impl_apis!`.
 macro_rules! decl_api {
 	(
 		$( #[$attr:meta] )*
@@ -249,6 +284,7 @@ macro_rules! decl_api {
 			};
 			$( $generic_param_parsed $( : $generic_bound_parsed )* ),*;
 			{ $( $result_return_ty; )* };
+			$( $( $generic_param_orig $( : $generic_bound_orig )*, )* )* $( $( $( $fn_generic, )* )* )*;
 		);
 	};
 	(@generate_traits
@@ -262,6 +298,7 @@ macro_rules! decl_api {
 		};
 		$( $generic_param_parsed:ident $( : $generic_bound_parsed:ident )* ),*;
 		{ $( $result_return_ty:ty; )* };
+		$( $generic_param_joined:ident $( : $generic_bound_joined:ident )*, )*;
 	) => {
 		#[cfg(feature = "api-for-client")]
 		$( #[$attr] )*
@@ -277,11 +314,9 @@ macro_rules! decl_api {
 
 		#[cfg(not(feature = "api-for-client"))]
 		$( #[$attr] )*
-		pub trait $name $( < $( $generic_param_orig $( : $generic_bound_orig )* ),* > )* {
+		pub trait $name < $( $generic_param_joined $( : $generic_bound_joined )* ),* > {
 			$(
-				fn $fn_name $( < $( $fn_generic: Encode + Decode ),* > )* (
-					$( $param_name: $param_type ),*
-				) $( -> $return_ty )*;
+				fn $fn_name ($( $param_name: $param_type ),*) $( -> $return_ty )*;
 			)*
 		}
 	};
@@ -318,7 +353,7 @@ decl_api! {
 	/// The `OldTxQueue` api trait for interfering with the old transaction queue.
 	pub trait OldTxQueue {
 		fn account_nonce<AccountId, Index>(account: AccountId) -> Index;
-		fn lookup_address<Address, AccountId>(address: Address) -> Option<AccountId>;
+		fn lookup_address<Address, LookupId>(address: Address) -> Option<LookupId>;
 	}
 }
 
@@ -336,4 +371,169 @@ decl_api! {
 		fn validators<AccountId>() -> Vec<AccountId>;
 		fn timestamp<Moment>() -> Moment;
 	}
+}
+
+/// Implement the given API's for the given runtime.
+/// All desired API's need to be implemented in one `impl_apis!` call.
+/// Besides generating the implementation for the runtime, there will be also generated an
+/// auxiliary module named `api` that contains function for inferring with the API in native/wasm.
+///
+/// # Example:
+///
+/// ```nocompile
+/// impl_apis! {
+///     impl Core<Block, AccountId> for Runtime {
+///         fn version() -> RuntimeVersion { 1 }
+///         fn authorities() -> Vec<AuthorityId> { vec![1] }
+///         fn execute_block(block: Block) {
+///             //comment
+///             let block = call_arbitrary_code(block);
+///             execute(block);
+///         }
+///     }
+///
+///     impl OldTxQueue<AccountId, Index, Address, LookupId> for Runtime {
+///         fn account_nonce(account: AccountId) -> Index {
+///             0
+///         }
+///         fn lookup_address(address: Address) -> Option<LookupId> {
+///             None
+///         }
+///     }
+/// }
+/// ```
+#[macro_export]
+macro_rules! impl_apis {
+	(
+		impl $trait_name:ident $( < $( $generic:ident ),* > )* for $runtime:ident {
+			$(
+				fn $fn_name:ident ( $( $arg_name:ident : $arg_ty:ty ),* ) $( -> $return_ty:ty )* {
+					$( $impl:tt )*
+				}
+			)*
+		}
+		$( $rest:tt )*
+	) => {
+		impl $trait_name $( < $( $generic ),* > )* for $runtime {
+			$(
+				fn $fn_name ( $( $arg_name : $arg_ty ),* ) $( -> $return_ty )* {
+					$( $impl )*
+				}
+			)*
+		}
+		impl_apis! {
+			$runtime;
+			$( $fn_name ( $( $arg_name: $arg_ty ),* ); )*;
+			$( $rest )*
+		}
+	};
+	(
+		$runtime:ident;
+		$( $fn_name_parsed:ident ( $( $arg_name_parsed:ident : $arg_ty_parsed:ty ),* ); )*;
+		impl $trait_name:ident $( < $( $generic:ident ),* > )* for $runtime_ignore:ident {
+			$(
+				fn $fn_name:ident ( $( $arg_name:ident : $arg_ty:ty ),* ) $( -> $return_ty:ty )* {
+					$( $impl:tt )*
+				}
+			)*
+		}
+		$( $rest:tt )*
+	) => {
+		impl $trait_name $( < $( $generic ),* > )* for $runtime {
+			$(
+				fn $fn_name ( $( $arg_name : $arg_ty ),* ) $( -> $return_ty )* {
+					$( $impl )*
+				}
+			)*
+		}
+		impl_apis! {
+			$runtime;
+			$( $fn_name_parsed ( $( $arg_name_parsed: $arg_ty_parsed )* ); )*
+			$( $fn_name ( $( $arg_name: $arg_ty ),* ); )*;
+			$( $rest )*
+		}
+	};
+	(
+		$runtime:ident;
+		$( $fn_name:ident ( $( $arg_name:ident : $arg_ty:ty ),* ); )*;
+	) => {
+		mod api {
+			use super::*;
+
+			#[cfg(feature = "std")]
+			pub fn dispatch(method: &str, mut data: &[u8]) -> Option<Vec<u8>> {
+				match method {
+					$(
+						stringify!($fn_name) => {
+							Some({impl_apis! {
+								@GENERATE_IMPL_CALL
+								$runtime;
+								$fn_name;
+								$( $arg_name : $arg_ty ),*;
+								data;
+							}})
+						}
+					)*
+						_ => None,
+				}
+			}
+
+			$(
+				#[cfg(not(feature = "std"))]
+				#[no_mangle]
+				pub fn $fn_name(input_data: *mut u8, input_len: usize) -> u64 {
+					let mut input = if input_len == 0 {
+						&[0u8; 0]
+					} else {
+						unsafe {
+							$crate::slice::from_raw_parts(input_data, input_len)
+						}
+					};
+
+					let output = { impl_apis! {
+						@GENERATE_IMPL_CALL
+						$runtime;
+						$fn_name;
+						$( $arg_name : $arg_ty ),*;
+						input;
+					}};
+					let res = output.as_ptr() as u64 + ((output.len() as u64) << 32);
+
+					// Leak the output vector to avoid it being freed.
+					// This is fine in a WASM context since the heap
+					// will be discarded after the call.
+					::core::mem::forget(output);
+					res
+				}
+			)*
+		}
+	};
+	(@GENERATE_IMPL_CALL
+		$runtime:ident;
+		$fn_name:ident;
+		$arg_name:ident : $arg_ty:ty;
+		$input:ident;
+	) => {
+		let $arg_name : $arg_ty = match $crate::codec::Decode::decode(&mut $input) {
+			Some(input) => input,
+			None => panic!("Bad input data provided to {}", stringify!($fn_name)),
+		};
+
+		let output = $runtime::$fn_name($arg_name);
+		$crate::codec::Encode::encode(&output)
+	};
+	(@GENERATE_IMPL_CALL
+		$runtime:ident;
+		$fn_name:ident;
+		$( $arg_name:ident : $arg_ty:ty ),*;
+		$input:ident;
+	) => {
+		let ( $( $arg_name ),* ) : ($( $arg_ty ),*) = match $crate::codec::Decode::decode(&mut $input) {
+			Some(input) => input,
+			None => panic!("Bad input data provided to {}", stringify!($fn_name)),
+		};
+
+		let output = $runtime::$fn_name($( $arg_name ),*);
+		$crate::codec::Encode::encode(&output)
+	};
 }
