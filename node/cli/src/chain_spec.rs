644d14e1bb52ca1ec37c17629d56906aba794088
haydn dufrene <haydn.dufrene@gmail.com>
2018-11-02 10:32:28 -0700

    style changes, mostly indentation

--- node/cli/src/chain_spec.rs
+++ node/cli/src/chain_spec.rs
@@ -19,8 +19,8 @@
 use primitives::{AuthorityId, ed25519};
 use node_primitives::AccountId;
 use node_runtime::{ConsensusConfig, CouncilSeatsConfig, CouncilVotingConfig, DemocracyConfig,
-	SessionConfig, StakingConfig, TimestampConfig, BalancesConfig, TreasuryConfig, UpgradeKeyConfig,
-	ContractConfig, Permill, Perbill};
+				   SessionConfig, StakingConfig, TimestampConfig, BalancesConfig, TreasuryConfig,
+				   UpgradeKeyConfig, ContractConfig, Permill, Perbill};
 pub use node_runtime::GenesisConfig;
 use substrate_service;
 
@@ -45,7 +45,7 @@ fn staging_testnet_config_genesis() -> GenesisConfig {
 		hex!["f295940fa750df68a686fcf4abd4111c8a9c5a5a5a83c4c8639c451a94a7adfd"].into(),
 	];
 	const MILLICENTS: u128 = 1_000_000_000;
-	const CENTS: u128 = 1_000 * MILLICENTS;	// assume this is worth about a cent.
+	const CENTS: u128 = 1_000 * MILLICENTS;    // assume this is worth about a cent.
 	const DOLLARS: u128 = 100 * CENTS;
 
 	const SECS_PER_BLOCK: u64 = 4;
@@ -55,12 +55,12 @@ fn staging_testnet_config_genesis() -> GenesisConfig {
 
 	GenesisConfig {
 		consensus: Some(ConsensusConfig {
-			code: include_bytes!("../../runtime/wasm/target/wasm32-unknown-unknown/release/node_runtime.compact.wasm").to_vec(),	// TODO change
+			code: include_bytes!("../../runtime/wasm/target/wasm32-unknown-unknown/release/node_runtime.compact.wasm").to_vec(),    // TODO change
 			authorities: initial_authorities.clone(),
 		}),
 		system: None,
 		balances: Some(BalancesConfig {
-			balances: endowed_accounts.iter().map(|&k|(k, 10_000_000 * DOLLARS)).collect(),
+			balances: endowed_accounts.iter().map(|&k| (k, 10_000_000 * DOLLARS)).collect(),
 			transaction_base_fee: 1 * CENTS,
 			transaction_byte_fee: 10 * MILLICENTS,
 			existential_deposit: 1 * DOLLARS,
@@ -70,7 +70,7 @@ fn staging_testnet_config_genesis() -> GenesisConfig {
 		}),
 		session: Some(SessionConfig {
 			validators: initial_authorities.iter().cloned().map(Into::into).collect(),
-			session_length: 5 * MINUTES
+			session_length: 5 * MINUTES,
 		}),
 		staking: Some(StakingConfig {
 			current_era: 0,
@@ -86,9 +86,9 @@ fn staging_testnet_config_genesis() -> GenesisConfig {
 			minimum_validator_count: 4,
 		}),
 		democracy: Some(DemocracyConfig {
-			launch_period: 5 * MINUTES,	// 1 day per public referendum
-			voting_period: 5 * MINUTES,	// 3 days to discuss & vote on an active referendum
-			minimum_deposit: 50 * DOLLARS,	// 12000 as the minimum deposit for a referendum
+			launch_period: 5 * MINUTES,    // 1 day per public referendum
+			voting_period: 5 * MINUTES,    // 3 days to discuss & vote on an active referendum
+			minimum_deposit: 50 * DOLLARS,    // 12000 as the minimum deposit for a referendum
 		}),
 		council_seats: Some(CouncilSeatsConfig {
 			active_council: vec![],
@@ -100,8 +100,7 @@ fn staging_testnet_config_genesis() -> GenesisConfig {
 			approval_voting_period: 2 * DAYS,
 			term_duration: 28 * DAYS,
 			desired_seats: 0,
-			inactive_grace_period: 1,	// one additional vote should go by before an inactive voter can be reaped.
-
+			inactive_grace_period: 1,    // one additional vote should go by before an inactive voter can be reaped.
 		}),
 		council_voting: Some(CouncilVotingConfig {
 			cooloff_period: 4 * DAYS,
@@ -132,8 +131,7 @@ fn staging_testnet_config_genesis() -> GenesisConfig {
 
 /// Staging testnet config.
 pub fn staging_testnet_config() -> ChainSpec {
-	let boot_nodes = vec![
-	];
+	let boot_nodes = vec![];
 	ChainSpec::from_genesis(
 		"Staging Testnet",
 		"staging_testnet",
@@ -156,19 +154,19 @@ pub fn get_authority_id_from_seed(seed: &str) -> AuthorityId {
 
 /// Helper function to create GenesisConfig for testing
 pub fn testnet_genesis(initial_authorities: Vec<AuthorityId>,
-                   upgrade_key: AccountId,
-                   endowed_accounts: Option<Vec<AuthorityId>>
-                   ) -> GenesisConfig {
-    let endowed_accounts = endowed_accounts.unwrap_or_else(|| {
-        vec![
+					   upgrade_key: AccountId,
+					   endowed_accounts: Option<Vec<AuthorityId>>,
+) -> GenesisConfig {
+	let endowed_accounts = endowed_accounts.unwrap_or_else(|| {
+		vec![
 			get_authority_id_from_seed("Alice"),
 			get_authority_id_from_seed("Bob"),
 			get_authority_id_from_seed("Charlie"),
 			get_authority_id_from_seed("Dave"),
 			get_authority_id_from_seed("Eve"),
 			get_authority_id_from_seed("Ferdie"),
-        ]
-    });
+		]
+	});
 	GenesisConfig {
 		consensus: Some(ConsensusConfig {
 			code: include_bytes!("../../runtime/wasm/target/wasm32-unknown-unknown/release/node_runtime.compact.wasm").to_vec(),
@@ -182,7 +180,7 @@ pub fn testnet_genesis(initial_authorities: Vec<AuthorityId>,
 			transfer_fee: 0,
 			creation_fee: 0,
 			reclaim_rebate: 0,
-			balances: endowed_accounts.iter().map(|&k|(k.into(), (1 << 60))).collect(),
+			balances: endowed_accounts.iter().map(|&k| (k.into(), (1 << 60))).collect(),
 		}),
 		session: Some(SessionConfig {
 			validators: initial_authorities.iter().cloned().map(Into::into).collect(),
@@ -225,7 +223,7 @@ pub fn testnet_genesis(initial_authorities: Vec<AuthorityId>,
 			voting_period: 20,
 		}),
 		timestamp: Some(TimestampConfig {
-			period: 5,					// 5 second block time.
+			period: 5,                    // 5 second block time.
 		}),
 		treasury: Some(TreasuryConfig {
 			proposal_bond: Permill::from_percent(5),
@@ -248,11 +246,12 @@ pub fn testnet_genesis(initial_authorities: Vec<AuthorityId>,
 }
 
 fn development_config_genesis() -> GenesisConfig {
-	testnet_genesis(vec![
-		get_authority_id_from_seed("Alice"),
-	],
+	testnet_genesis(
+		vec![
+			get_authority_id_from_seed("Alice"),
+		],
 		get_authority_id_from_seed("Alice").into(),
-        None
+		None,
 	)
 }
 
@@ -262,12 +261,13 @@ pub fn development_config() -> ChainSpec {
 }
 
 fn local_testnet_genesis() -> GenesisConfig {
-	testnet_genesis(vec![
-		get_authority_id_from_seed("Alice"),
-		get_authority_id_from_seed("Bob"),
-	],
+	testnet_genesis(
+		vec![
+			get_authority_id_from_seed("Alice"),
+			get_authority_id_from_seed("Bob"),
+		],
 		get_authority_id_from_seed("Alice").into(),
-        None
+		None,
 	)
 }
 
--- test-utils/chain-spec-builder/src/main.rs
+++ test-utils/chain-spec-builder/src/main.rs
@@ -7,45 +7,45 @@ extern crate node_cli;
 extern crate substrate_service;
 extern crate substrate_primitives;
 
-use node_cli::chain_spec::{ChainSpec, GenesisConfig, get_authority_id_from_seed, testnet_genesis};
+use node_cli::chain_spec;
 use substrate_service::chain_ops::build_spec;
 
-fn genesis_constructor() -> GenesisConfig {
-    let yaml = load_yaml!("./cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
-    let authorities = matches.values_of("initial_authority_seed")
-        .unwrap()
-        .map(get_authority_id_from_seed)
-        .collect();
+fn genesis_constructor() -> chain_spec::GenesisConfig {
+	let yaml = load_yaml!("./cli.yml");
+	let matches = App::from_yaml(yaml).get_matches();
+	let authorities = matches.values_of("initial_authority_seed")
+		.unwrap()
+		.map(chain_spec::get_authority_id_from_seed)
+		.collect();
 
-    let endowed_accounts = matches.values_of("endowed_account_seed")
-        .unwrap()
-        .map(get_authority_id_from_seed)
-        .collect();
+	let endowed_accounts = matches.values_of("endowed_account_seed")
+		.unwrap()
+		.map(chain_spec::get_authority_id_from_seed)
+		.collect();
 
-    let upgrade_key_seed = matches.value_of("upgrade_key_seed").unwrap();
-    let upgrade_key = get_authority_id_from_seed(upgrade_key_seed);
-    testnet_genesis(
-        authorities,
-        upgrade_key.into(),
-        Some(endowed_accounts),
-    )
+	let upgrade_key_seed = matches.value_of("upgrade_key_seed").unwrap();
+	let upgrade_key = chain_spec::get_authority_id_from_seed(upgrade_key_seed);
+	chain_spec::testnet_genesis(
+		authorities,
+		upgrade_key.into(),
+		Some(endowed_accounts),
+	)
 }
 
-fn generate_chain_spec()-> String {
-    let chain_spec = ChainSpec::from_genesis(
-        "Custom",
-        "custom",
-        genesis_constructor,
-        vec![],
-        None,
-        None,
-        None,
-    );
-    build_spec(chain_spec, false).unwrap()
+fn generate_chain_spec() -> String {
+	let chain_spec = chain_spec::ChainSpec::from_genesis(
+		"Custom",
+		"custom",
+		genesis_constructor,
+		vec![],
+		None,
+		None,
+		None,
+	);
+	build_spec(chain_spec, false).unwrap()
 }
 
 fn main() {
-    let json = generate_chain_spec();
-    println!("{}", json);
+	let json = generate_chain_spec();
+	println!("{}", json);
 }
