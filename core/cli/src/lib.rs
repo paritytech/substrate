609ee60216f2ad0675a2930f7e73b67be3c46c70
Pierre Krieger <pierre.krieger1708@gmail.com>
2018-11-13 16:26:53 +0100

    Remove dependency on libp2p from cli (#1107)

--- core/cli/src/lib.rs
+++ core/cli/src/lib.rs
@@ -34,7 +34,6 @@ extern crate sysinfo;
 
 extern crate substrate_client as client;
 extern crate substrate_network as network;
-extern crate substrate_network_libp2p as network_libp2p;
 extern crate sr_primitives as runtime_primitives;
 extern crate substrate_service as service;
 extern crate substrate_primitives as primitives;
@@ -56,13 +55,12 @@ pub mod error;
 pub mod informant;
 mod panic_hook;
 
-use network_libp2p::Protocol;
 use runtime_primitives::traits::As;
 use service::{
 	ServiceFactory, FactoryFullConfiguration, RuntimeGenesis,
 	FactoryGenesis, PruningMode, ChainSpec,
 };
-use network::NonReservedPeerMode;
+use network::{NonReservedPeerMode, Protocol};
 use primitives::H256;
 
 use std::io::{Write, Read, stdin, stdout};
