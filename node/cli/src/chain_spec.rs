754f28d8505af2f6ec10cf1d267785195dd2dc8a
Gav Wood <github@gavwood.com>
2019-01-07 16:07:58 +0100

    Invulnerabiity from slashing for certain validators (#1355)
    * Invulnerabiity from slashing
    
    * Update srml/staking/src/lib.rs
    
    Co-Authored-By: gavofyork <github@gavwood.com>

--- node/cli/src/chain_spec.rs
+++ node/cli/src/chain_spec.rs
@@ -86,6 +86,7 @@ fn staging_testnet_config_genesis() -> GenesisConfig {
 			bonding_duration: 60 * MINUTES,
 			offline_slash_grace: 4,
 			minimum_validator_count: 4,
+			invulnerables: initial_authorities.iter().cloned().map(Into::into).collect(),
 		}),
 		democracy: Some(DemocracyConfig {
 			launch_period: 10 * MINUTES,    // 1 day per public referendum
@@ -211,6 +212,7 @@ pub fn testnet_genesis(
 			current_offline_slash: 0,
 			current_session_reward: 0,
 			offline_slash_grace: 0,
+			invulnerables: initial_authorities.iter().cloned().map(Into::into).collect(),
 		}),
 		democracy: Some(DemocracyConfig {
 			launch_period: 9,
