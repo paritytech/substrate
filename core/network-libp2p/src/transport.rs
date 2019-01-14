75c3b51b67f0efd30ec8484c4098fe4cb3a9f5d0
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-01-14 13:38:36 +0100

    Update to latest libp2p (#1386)
    * Update to latest libp2p
    
    * Fix indentations
    
    * Add basic test
    
    * Apply suggestions from code review
    
    Co-Authored-By: tomaka <pierre.krieger1708@gmail.com>
    
    * Remove Mutex from topology
    
    * Remove unused method
    
    * Fix concerns

--- core/network-libp2p/src/transport.rs
+++ core/network-libp2p/src/transport.rs
@@ -14,33 +14,33 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-use libp2p::{self, PeerId, Transport, mplex, secio, yamux};
-use libp2p::core::{either, upgrade, transport::boxed::Boxed, muxing::StreamMuxerBox};
-use libp2p::transport_timeout::TransportTimeout;
-use std::time::Duration;
-use std::usize;
+use futures::prelude::*;
+use libp2p::{self, InboundUpgradeExt, OutboundUpgradeExt, PeerId, Transport, mplex, secio, yamux};
+use libp2p::core::{self, transport::boxed::Boxed, muxing::StreamMuxerBox};
+use std::{io, time::Duration, usize};
 
 /// Builds the transport that serves as a common ground for all connections.
 pub fn build_transport(
 	local_private_key: secio::SecioKeyPair
-) -> Boxed<(PeerId, StreamMuxerBox)> {
+) -> Boxed<(PeerId, StreamMuxerBox), io::Error> {
 	let mut mplex_config = mplex::MplexConfig::new();
 	mplex_config.max_buffer_len_behaviour(mplex::MaxBufferBehaviour::Block);
 	mplex_config.max_buffer_len(usize::MAX);
 
-	let base = libp2p::CommonTransport::new()
+	// TODO: rework the transport creation (https://github.com/libp2p/rust-libp2p/issues/783)
+	libp2p::tcp::TcpConfig::new()
 		.with_upgrade(secio::SecioConfig::new(local_private_key))
 		.and_then(move |out, endpoint| {
-			let upgrade = upgrade::or(
-				upgrade::map(yamux::Config::default(), either::EitherOutput::First),
-				upgrade::map(mplex_config, either::EitherOutput::Second),
-			);
 			let peer_id = out.remote_key.into_peer_id();
-			let upgrade = upgrade::map(upgrade, move |muxer| (peer_id, muxer));
-			upgrade::apply(out.stream, upgrade, endpoint.into())
-		})
-		.map(|(id, muxer), _| (id, StreamMuxerBox::new(muxer)));
+			let peer_id2 = peer_id.clone();
+			let upgrade = core::upgrade::SelectUpgrade::new(yamux::Config::default(), mplex_config)
+				.map_inbound(move |muxer| (peer_id, muxer))
+				.map_outbound(move |muxer| (peer_id2, muxer));
 
-	TransportTimeout::new(base, Duration::from_secs(20))
+			core::upgrade::apply(out.stream, upgrade, endpoint)
+				.map(|(id, muxer)| (id, core::muxing::StreamMuxerBox::new(muxer)))
+		})
+		.with_timeout(Duration::from_secs(20))
+		.map_err(|err| io::Error::new(io::ErrorKind::Other, err))
 		.boxed()
 }
