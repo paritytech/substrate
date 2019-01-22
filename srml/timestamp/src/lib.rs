a0dc5ccb283bd6a2d58c5bea233c41611126286b
Bastian Köcher <git@kchr.de>
2019-01-22 17:11:11 +0100

    Last fix

--- srml/timestamp/src/lib.rs
+++ srml/timestamp/src/lib.rs
@@ -227,7 +227,7 @@ impl<T: Trait> Module<T> {
 
 fn extract_inherent_data(data: &InherentData) -> Result<InherentType, RuntimeString> {
 	data.get_data::<InherentType>(&INHERENT_IDENTIFIER)
-		.map_err(|_| "Invalid timestamp inherent data encoding.".into())?
+		.map_err(|_| RuntimeString::from("Invalid timestamp inherent data encoding."))?
 		.ok_or_else(|| "Timestamp inherent data is not provided.".into())
 }
 
