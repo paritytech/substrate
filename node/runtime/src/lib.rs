6c0f7135c161f88dce6839923926577178b10492
Sergey Pepyakin <sergei@parity.io>
2019-01-29 13:24:12 +0100

    Bump the runtime version to 17.

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -94,8 +94,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 16,
-	impl_version: 16,
+	spec_version: 17,
+	impl_version: 17,
 	apis: RUNTIME_API_VERSIONS,
 };
 
