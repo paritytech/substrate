2056e4cd99b96408367f89ccd90c4107687b5ec0
Wei Tang <accounts@that.world>
2019-02-06 14:50:52 +0100

    Update trie to v0.11 (#1704)
    * Update trie to v0.11
    
    * Update wasm runtimes

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -66,8 +66,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 21,
-	impl_version: 21,
+	spec_version: 22,
+	impl_version: 22,
 	apis: RUNTIME_API_VERSIONS,
 };
 
