8a69e23fcbe89acb63e9f55ea75a9a2cb1ade81f
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2019-02-08 14:55:15 +0100

    Fixed a bunch of warnings (#1740)

--- core/primitives/src/lib.rs
+++ core/primitives/src/lib.rs
@@ -21,6 +21,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(not(feature = "std"), feature(alloc))]
 
+/// Initalise a key-value collection from array.
+///
 /// Creates a vector of given pairs and calls `collect` on the iterator from it.
 /// Can be used to create a `HashMap`.
 #[macro_export]
