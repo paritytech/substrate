9e533782ccf9b3ac28a6f6c549c2ad7e4e499445
thiolliere <gui.thiolliere@gmail.com>
2019-02-08 17:17:14 +0100

    update runtime

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -65,8 +65,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 24,
-	impl_version: 24,
+	spec_version: 25,
+	impl_version: 25,
 	apis: RUNTIME_API_VERSIONS,
 };
 
