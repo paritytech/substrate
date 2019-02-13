70a14dc647da9dd46a62bce0ac7b891ac1bc09d7
Shaopeng Wang <git@spwang.net>
2019-02-14 00:17:56 +1300

    Bump spec and impl version.

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -60,8 +60,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 28,
-	impl_version: 28,
+	spec_version: 29,
+	impl_version: 29,
 	apis: RUNTIME_API_VERSIONS,
 };
 
