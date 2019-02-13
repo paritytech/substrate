278da1481227cf904c4be1bf6bd9615a91934440
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2019-02-13 08:52:52 +0100

    EnumerableStorageMap (#1763)
    * Refactor decl_storage a bit to allow easier impl of linked map.
    
    * A bunch of refactorings for storage generation.
    
    - Rename StorageMap and ChildrenStorageMap to avoid confusion with generator::StorageMap.
    - Separate implementation from the procedural macro code to clean it up.
    - Make sure that genesis is initialised using the `StorageValue/StorageMap`
      generated implementations instead of going RAW.
    
    * WiP: Writing test.
    
    * Basic implementation.
    
    * Implement enumeration.
    
    * Fix non-std issues.
    
    * fix warning
    
    * Fix test-client.
    
    * Address review grumbles - part 1
    
    * Avoid cloning the key, relax Storage requirements.
    
    * Rebuild runtime.
    
    * Remove dangling todo.

--- srml/consensus/src/lib.rs
+++ srml/consensus/src/lib.rs
@@ -168,7 +168,7 @@ decl_storage! {
 		#[serde(with = "substrate_primitives::bytes")]
 		config(code): Vec<u8>;
 
-		build(|storage: &mut primitives::StorageMap, _: &mut primitives::ChildrenStorageMap, config: &GenesisConfig<T>| {
+		build(|storage: &mut primitives::StorageOverlay, _: &mut primitives::ChildrenStorageOverlay, config: &GenesisConfig<T>| {
 			use codec::{Encode, KeyedVec};
 
 			let auth_count = config.authorities.len() as u32;
