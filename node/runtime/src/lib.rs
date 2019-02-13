1ef8d63e3515cec9fe8db44859a66891982ff575
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-02-13 18:59:22 +0100

    Adding vesting tests to Balances module (#1786)
    * Adding vesting tests to Balances module
    
    * Fix gitlab message, bump impl

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -61,7 +61,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
 	spec_version: 28,
-	impl_version: 28,
+	impl_version: 29,
 	apis: RUNTIME_API_VERSIONS,
 };
 
