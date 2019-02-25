ad17dd8bf27604f5cfd15d8c2c7bf3852378b48c
Bastian Köcher <bkchr@users.noreply.github.com>
2019-02-25 11:25:47 +0100

    Update srml/support/src/runtime.rs
    Co-Authored-By: thiolliere <gui.thiolliere@gmail.com>

--- srml/support/src/runtime.rs
+++ srml/support/src/runtime.rs
@@ -744,7 +744,7 @@ macro_rules! __decl_runtime_metadata {
 	(
 		$runtime:ident;
 		$( $parsed_modules:ident { $( $withs:ident )* } )*;
-		$( {leading_module: $( $leading_module:ident )* } )?
+		$( { leading_module: $( $leading_module:ident )* } )?
 		$name:ident: $module:ident::{
 			$other_module:ident $( $modules:ident )*
 		}
