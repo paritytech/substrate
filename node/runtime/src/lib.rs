2f9e37a287f865ea8f9b125f303799af0ba471ca
Marcio Diaz <marcio.diaz@gmail.com>
2019-03-14 18:39:59 +0100

    bump spec and impl versions

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -60,8 +60,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 35,
-	impl_version: 35,
+	spec_version: 36,
+	impl_version: 37,
 	apis: RUNTIME_API_VERSIONS,
 };
 
