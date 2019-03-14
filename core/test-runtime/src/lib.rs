27dd576c0d8a73633618438ae48b12874bdf40c8
Michael Mueller <mich@elmueller.net>
2019-03-14 16:34:26 +0100

    Revert unnecessary version bump

--- core/test-runtime/src/lib.rs
+++ core/test-runtime/src/lib.rs
@@ -50,7 +50,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("parity-test"),
 	authoring_version: 1,
 	spec_version: 1,
-	impl_version: 2,
+	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 };
 
