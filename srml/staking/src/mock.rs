9b026cc50ac18c929d1fde0cc32100c12c83397b
kianenigma <kian.peymani@gmail.com>
2019-03-15 12:25:19 +0100

    Remove unused test parameters

--- srml/staking/src/mock.rs
+++ srml/staking/src/mock.rs
@@ -79,7 +79,6 @@ pub struct ExtBuilder {
 	session_length: u64,
 	sessions_per_era: u64,
 	current_era: u64,
-	monied: bool,
 	reward: u64,
 	validator_pool: bool,
 	nominate: bool,
@@ -94,7 +93,6 @@ impl Default for ExtBuilder {
 			session_length: 1,
 			sessions_per_era: 1,
 			current_era: 0,
-			monied: true,
 			reward: 10,
 			validator_pool: false,
 			nominate: true,
@@ -121,16 +119,7 @@ impl ExtBuilder {
 		self.current_era = current_era;
 		self
 	}
-	pub fn _monied(mut self, monied: bool) -> Self {
-		self.monied = monied;
-		self
-	}
-	pub fn reward(mut self, reward: u64) -> Self {
-		self.reward = reward;
-		self
-	}
 	pub fn validator_pool(mut self, validator_pool: bool) -> Self { 
-		// NOTE: this should only be set to true with monied = false.
 		self.validator_pool = validator_pool;
 		self
 	}
@@ -165,44 +154,22 @@ impl ExtBuilder {
 			keys: vec![],
 		}.assimilate_storage(&mut t, &mut c);
 		let _ = balances::GenesisConfig::<Test>{
-			balances: if self.monied {
-				if self.reward > 0 {
-					vec![
-						(1, 10 * balance_factor),
-						(2, 20 * balance_factor),
-						(3, 300 * balance_factor),
-						(4, 400 * balance_factor),
-						(10, balance_factor),
-						(11, balance_factor * 1000),
-						(20, balance_factor),
-						(21, balance_factor * 2000),
-						(30, balance_factor),
-						(31, balance_factor * 3000),
-						(40, balance_factor),
-						(41, balance_factor * 4000),
-						(100, 2000 * balance_factor),
-						(101, 2000 * balance_factor),
-					]
-				} else {
-					vec![
-						(1, 10 * balance_factor),
-						(2, 20 * balance_factor),
-						(3, 300 * balance_factor),
-						(4, 400 * balance_factor),
-					]
-				}
-			} else {
-				vec![
+			balances: vec![
+					(1, 10 * balance_factor),
+					(2, 20 * balance_factor),
+					(3, 300 * balance_factor),
+					(4, 400 * balance_factor),
 					(10, balance_factor),
-					(11, balance_factor * 10),
+					(11, balance_factor * 1000),
 					(20, balance_factor),
-					(21, balance_factor * 20),
+					(21, balance_factor * 2000),
 					(30, balance_factor),
-					(31, balance_factor * 30),
+					(31, balance_factor * 3000),
 					(40, balance_factor),
-					(41, balance_factor * 40),
-				]
-			},
+					(41, balance_factor * 4000),
+					(100, 2000 * balance_factor),
+					(101, 2000 * balance_factor),
+			],
 			existential_deposit: self.existential_deposit,
 			transfer_fee: 0,
 			creation_fee: 0,
@@ -232,7 +199,7 @@ impl ExtBuilder {
 			minimum_validator_count: self.minimum_validator_count,
 			bonding_duration: self.sessions_per_era * self.session_length * 3,
 			session_reward: Perbill::from_millionths((1000000 * self.reward / balance_factor) as u32),
-			offline_slash: if self.monied { Perbill::from_percent(40) } else { Perbill::zero() },
+			offline_slash: Perbill::from_percent(40),
 			current_session_reward: self.reward,
 			current_offline_slash: 20,
 			offline_slash_grace: 0,
