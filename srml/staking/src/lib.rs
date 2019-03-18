771bf0e921aa0a8a8be3d44c57132a35149c07fb
Gav Wood <gavin@parity.io>
2019-03-18 10:16:23 +0100

    Remove rev causing upset

--- srml/staking/src/lib.rs
+++ srml/staking/src/lib.rs
@@ -6,8 +6,6 @@
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-
-
 // Substrate is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
