9feab5c970b3e2acb99c6252db356638bfe5e28d
thiolliere <gui.thiolliere@gmail.com>
2019-03-25 15:19:15 +0100

    impl MakePayment for balances instances (#2055)

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -59,7 +59,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
 	spec_version: 41,
-	impl_version: 41,
+	impl_version: 42,
 	apis: RUNTIME_API_VERSIONS,
 };
 
