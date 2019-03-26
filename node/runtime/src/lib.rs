8e2411fc04aeb660bf32a1a38a02e0f71a15a70c
André Silva <andre.beat@gmail.com>
2019-03-26 15:01:52 +0000

    runtime: bump impl_version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -59,7 +59,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
 	spec_version: 45,
-	impl_version: 45,
+	impl_version: 46,
 	apis: RUNTIME_API_VERSIONS,
 };
 
