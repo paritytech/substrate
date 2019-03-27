533d8ce75cb0d68d05794e21a2c8697b3a16cd36
joe petrowski <25483142+joepetrowski@users.noreply.github.com>
2019-03-27 10:55:16 +0100

    Update srml/consensus/src/lib.rs
    Co-Authored-By: AmarRSingh <asinghchrony@protonmail.com>

--- srml/consensus/src/lib.rs
+++ srml/consensus/src/lib.rs
@@ -90,7 +90,7 @@
 //! 
 //! ## Related Modules
 //! 
-//! - [`staking`](https://crates.parity.io/srml_staking/index.html): this module uses `consensus` to monitor offline reporting amongst validators
+//! - [`staking`](https://crates.parity.io/srml_staking/index.html): This module uses `consensus` to monitor offline reporting amongst validators.
 //! - [`aura`](https://crates.parity.io/srml_aura/index.html): This module does not relate directly to `consensus`, but serves to manage offline reporting for the Aura consensus algorithm with its own `handle_report` method.
 //! - [`grandpa`](https://crates.parity.io/srml_grandpa/index.html): although GRANDPA does its own voter-set managing, it has a mode where it can track `consensus`, if desired
 //! 
