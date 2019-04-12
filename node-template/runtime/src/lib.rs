8d735db20e896f807bb421270685d9e44b5a29ee
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-04-12 11:53:35 +0200

    Update `template-node` to `node-template` (#2265)
    * Update `template-node` to `node-template`
    
    For substrate-up scripts
    
    * Remove random artifact

--- node-template/runtime/src/lib.rs
+++ node-template/runtime/src/lib.rs
@@ -92,8 +92,8 @@ pub mod opaque {
 
 /// This runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("template-node"),
-	impl_name: create_runtime_str!("template-node"),
+	spec_name: create_runtime_str!("node-template"),
+	impl_name: create_runtime_str!("node-template"),
 	authoring_version: 3,
 	spec_version: 3,
 	impl_version: 0,
