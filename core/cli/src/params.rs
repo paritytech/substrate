36f5aa55209d2e15dc8ca41e57b6439e366da4ea
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2019-04-16 12:57:51 +0200

    Disable CORS when running in --dev (#2291)
    * Disable cors in dev mode,
    
    * Whitelist substrate-ui.
    
    * Fix build.
    
    * Update docs.

--- core/cli/src/params.rs
+++ core/cli/src/params.rs
@@ -332,7 +332,8 @@ pub struct RunCmd {
 	/// Specify browser Origins allowed to access the HTTP & WS RPC servers.
 	/// It's a comma-separated list of origins (protocol://domain or special `null` value).
 	/// Value of `all` will disable origin validation.
-	/// Default is to allow localhost and https://polkadot.js.org origin.
+	/// Default is to allow localhost, https://polkadot.js.org and https://substrate-ui.parity.io origins.
+	/// When running in --dev mode the default is to allow all origins.
 	#[structopt(long = "rpc-cors", value_name = "ORIGINS", parse(try_from_str = "parse_cors"))]
 	pub rpc_cors: Option<Option<Vec<String>>>,
 
