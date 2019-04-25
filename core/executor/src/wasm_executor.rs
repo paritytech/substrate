7cca029d6ae93c5849b50edfcc6d2c313ba3e5bf
Tomasz Drwięga <tomasz@parity.io>
2019-04-25 12:26:07 +0200

    Remove HTTP stuff.

--- core/executor/src/wasm_executor.rs
+++ core/executor/src/wasm_executor.rs
@@ -529,7 +529,6 @@ impl_function_executor!(this: FunctionExecutor<'e, E>,
 
 		Ok(())
 	},
-    // TODO [ToDr] http externalities.
 	ext_sandbox_instantiate(
 		dispatch_thunk_idx: usize,
 		wasm_ptr: *const u8,
