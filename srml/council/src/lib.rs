e97fb08c9a483d599b2516a21d6a0f6fd9725078
Luke Schoen <ltfschoen@gmail.com>
2019-04-26 18:44:18 +0200

    docs: Update example

--- srml/council/src/lib.rs
+++ srml/council/src/lib.rs
@@ -204,12 +204,42 @@
 //! - `tally` - The count of the yay and nay votes associated with voting on a council proposal.
 //!
 //!
-//! ### Snippet: Get the next seats tally block number.
+//! ### Snippet: Approve all candidates when additional empty seats are required
 //!
-//!	The next tally of the seats voting will be scheduled based on the condition of needing
-//! more councillors or not. If no councillor is needed, the associated function to get the next tally,
-//! namely `next_tally()`, will return `None`.  Else, it will return an `Option` containing
-//! the next scheduled tally’s block number.
+//! #### Existing Council Module Functionality Related to this Code Snippet
+//!
+//! In the Council module at the end of each block in the `end_block` function it checks if we're ready to call
+//! `start_tally`, which ends the voting period, and starts tallying the approval votes that were submitted for
+//! the election of candidates to council seat(s).
+//!
+//! We know we're ready to call `start_tally` when the result from calling `voting_period` indicates that the
+//! current block number is when the council voting period ends, and when the result from calling `next_tally`
+//! indicates that the current block number is when a tally for the next election is scheduled to occur.
+//!
+//! `start_tally` starts a new presentation period from the current block number if it finds that there are any
+//! empty seats available. It only does this if the desired council member count is greater than the amount of seats
+//! to be retained (after excluding any seats that expire at the current block number).
+//!
+//! A snapshot of the final voting stake that was locked up from each voter's bond at the end of the voting period
+//! is recorded.
+//!
+//! It also initializes a leaderboard. The size of the leaderboard is the amount of empty seats available,
+//! plus the amount of the top runners-up that are already registered in a candidate slot by locking up a bond.
+//!
+//! During the presentation period the candidates that believe they obtained sufficient votes (based on the recorded
+//! snapshot of the final stake corresponding to their candidature) to be elected will present themselves for
+//! election on the council.
+//!
+//! Additionally in `end_block` we also call `next_finalize` to check if there are any accounts that currently
+//! hold a seat that will become free on the current block number. If there are, then we call `finalize_tally`,
+//! which elects the candidates with the most approval votes into council seats with a term duration and returns
+//! the bond that was staked by them and their voters.
+//!
+//! #### Purpose of the Code Snippet
+//!
+//! This code snippet includes an `approve_all` public function that could be called by a signed account to approve
+//! the eligibility of all candidates when there are empty council seats and when the tally for the next election
+//! occurs at the current or a future block number.
 //!
 //! ```
 //! use srml_support::{decl_module, dispatch::Result};
@@ -225,20 +255,26 @@
 //! 		pub fn approve_all(origin) -> Result {
 //! 			let _origin = ensure_signed(origin)?;
 //!
+//! 			// Get the current block number
+//!			let current_block_number = <system::Module<T>>::block_number();
+//!
 //! 			// Get the number of seats that we want the council to have
 //! 			let desired = <seats::Module<T>>::desired_seats() as usize;
 //!
 //! 			// Get the number of seats occupied by the current council.
-//! 			let occupied  = <seats::Module<T>>::active_council().len();
+//! 			let occupied = <seats::Module<T>>::active_council().len();
 //!
 //! 			// Get the appropriate block number to schedule the next tally.
 //! 			let maybe_next_tally = <seats::Module<T>>::next_tally();
 //!
-//! 			if (desired == occupied) {
-//! 				assert_eq!(maybe_next_tally, None);
-//! 			} else {
-//!					assert!(maybe_next_tally.is_some());
-//! 			}
+//! 			assert!(desired > occupied, "Unable to approval all candidates when there are no empty seats");
+//!
+//! 			if let Some(next_tally_block_number) = <seats::Module<T>>::next_tally() {
+//!         			if current_block_number <= next_tally_block_number {
+//!					assert!(maybe_next_tally.is_some(),
+//! 						"Unable to approval all candidates when the block number of the next tally has past");
+//!				}
+//!			}
 //!
 //! 			Ok(())
 //! 		}
