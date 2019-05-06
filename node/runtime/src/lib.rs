6dc693cb805590e7a67f7150c74bb243575f9ec7
thiolliere <gui.thiolliere@gmail.com>
2019-05-06 08:44:36 +0200

    optimize double map first key hash (#2451)
    * fix double map encoding it is now encoded as specified in the doc

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -58,8 +58,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 70,
-	impl_version: 70,
+	spec_version: 71,
+	impl_version: 71,
 	apis: RUNTIME_API_VERSIONS,
 };
 
