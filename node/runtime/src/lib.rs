0d4e5a1017867923575594101cd802aad1e142e0
thiolliere <gui.thiolliere@gmail.com>
2019-05-06 13:14:13 +0200

    backport fix doublemapkey (#2480)
    * fix double map encoding
    
    * decl_storage doc (#2341)
    
    * doc
    
    * other doc cleanup and fixing links
    
    * one more typo
    
    * decl_storage document hasher choice (#2387)
    
    * doc
    
    * fix
    
    * grammar and line wrapping
    
    * small changes to end
    
    * Update lib.rs
    
    * update after demi's review
    
    * bump runtime
    
    * update doc

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -58,8 +58,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 59,
-	impl_version: 59,
+	spec_version: 60,
+	impl_version: 60,
 	apis: RUNTIME_API_VERSIONS,
 };
 
