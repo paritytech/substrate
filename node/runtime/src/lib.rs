3b6dba5b71223bd6d6e5f69a5b353a466007bac4
Xiliang Chen <xlchen1291@gmail.com>
2019-05-09 19:40:28 +1200

    Remove As trait bound from Currency::Balance (#2205)
    * remove As<64> bond
    
    * Currency trait refactor
    
    * bump spec version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -58,8 +58,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 72,
-	impl_version: 74,
+	spec_version: 73,
+	impl_version: 73,
 	apis: RUNTIME_API_VERSIONS,
 };
 
