cc16ab8a088a037c361bfa3b47f7dfec0d2fa66a
gabriel <gabriel@parity.io>
2019-05-09 15:03:50 +0200

    print file names and line numbers

--- scripts/gitlab/check_line_width.sh
+++ scripts/gitlab/check_line_width.sh
@@ -8,13 +8,26 @@ BASE_BRANCH="origin/master"
 LINE_WIDTH="101"
 
 
-if git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} \
-  | grep "^.\{${LINE_WIDTH}\}" ${file}
-then
-  echo "|"
-  echo "| newly added lines longer than ${LINE_WIDTH} characters."
-  echo "|"
-  exit 1
-fi
+FAIL=""
 
+git diff --name-only ${BASE_BRANCH}...${CI_COMMIT_SHA} | while read file
+do
+  if git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} \
+    | grep -q "^.\{${LINE_WIDTH}\}" ${file}
+  then
+    if [ -z "${FAIL}" ]
+    then
+      echo "|"
+      echo "| newly added changes contain lines longer than ${LINE_WIDTH} characters."
+      echo "|"
+      FAIL="true"
+    fi
+    echo "| file: ${file}"
+    git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} \
+      | grep -n "^.\{${LINE_WIDTH}\}" ${file}
+    echo "|"
+  fi
+done
+
+test "${FAIL}" && exit 1
 
