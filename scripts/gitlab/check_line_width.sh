0a7c987053449cea8c1c5581c703019cfd4b4531
Gavin Wood <gavin@parity.io>
2019-05-10 14:26:31 +0200

    Update check_line_width.sh

--- scripts/gitlab/check_line_width.sh
+++ scripts/gitlab/check_line_width.sh
@@ -5,21 +5,20 @@
 
 
 BASE_BRANCH="origin/master"
-LINE_WIDTH="101"
+LINE_WIDTH="121"
+GOOD_LINE_WIDTH="101"
 
 
 FAIL=""
 
 git diff --name-only ${BASE_BRANCH}...${CI_COMMIT_SHA} \*.rs | while read file
 do
-  if git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} \
-    | grep -q "^.\{${LINE_WIDTH}\}" ${file}
+  if git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} | grep -q "^.\{${LINE_WIDTH}\}" ${file}
   then
     if [ -z "${FAIL}" ]
     then
       echo "| warning!"
-	  echo "| Lines should be longer than 100 characters long only in exceptional circumstances and\
- certainly no longer than 120."
+	  echo "| Lines should not be longer than 120 characters."
 	  echo "| "
 	  echo "| see more https://wiki.parity.io/Substrate-Style-Guide"
       echo "|"
@@ -29,6 +28,23 @@ do
     git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} \
       | grep -n "^.\{${LINE_WIDTH}\}" ${file}
     echo "|"
+  else
+  if git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} | grep -q "^.\{${GOOD_LINE_WIDTH}\}" ${file}
+  then
+    if [ -z "${FAIL}" ]
+    then
+      echo "| warning!"
+	  echo "| Lines should be longer than 100 characters only in exceptional circumstances!"
+	  echo "| "
+	  echo "| see more https://wiki.parity.io/Substrate-Style-Guide"
+      echo "|"
+    fi
+    echo "| file: ${file}"
+    git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} \
+      | grep -n "^.\{${LINE_WIDTH}\}" ${file}
+    echo "|"
+  fi
+  
   fi
 done
 
