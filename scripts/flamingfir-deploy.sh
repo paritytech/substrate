b5c9ac506defe624b253668249c02bed088f06b9
David Dorgan <david@parity.io>
2019-05-15 19:36:27 +0100

    Correct wget syntax

--- scripts/flamingfir-deploy.sh
+++ scripts/flamingfir-deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-set -x
 
-export download_url="https://releases.parity.io/substrate/x86_64-debian:stretch/2.0.0-${CI_COMMIT_SHORT_SHA}/substrate"
+DOWNLOAD_URL="https://releases.parity.io/substrate/x86_64-debian:stretch/2.0.0-${CI_COMMIT_SHORT_SHA}/substrate"
+POST_DATA='{"extra_vars":{"artifact_path":"'${DOWNLOAD_URL}'"}}'
 
-wget -O -  --header "Authorization: Bearer ${AWX_TOKEN}" --header "Content-type: application/json" --post-data '{"extra_vars":{"artifact_path":"${download_url}"}' https://ansible-awx.parity.io/api/v2/job_templates/32/launch/
-
-
+wget -O -  --header "Authorization: Bearer ${AWX_TOKEN}" --header "Content-type: application/json" --post-data "${POST_DATA}" https://ansible-awx.parity.io/api/v2/job_templates/32/launch/
