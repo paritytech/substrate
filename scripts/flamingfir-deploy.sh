fb345be62ff77be793b3ee7a3dce171a15ba17f4
David Dorgan <david@parity.io>
2019-05-15 19:00:18 +0100

    Fix formatting

--- scripts/flamingfir-deploy.sh
+++ scripts/flamingfir-deploy.sh
@@ -3,6 +3,6 @@ set -x
 
 export download_url="https://releases.parity.io/substrate/x86_64-debian:stretch/2.0.0-${CI_COMMIT_SHORT_SHA}/substrate"
 
-wget -O -  --header "Authorization: Bearer ${AWX_TOKEN}" --header "Content-type: application/json" --post-data '{"extra_vars":{"artifact_path":"${download_url"}}' https://ansible-awx.parity.io/api/v2/job_templates/32/launch/
+wget -O -  --header "Authorization: Bearer ${AWX_TOKEN}" --header "Content-type: application/json" --post-data '{"extra_vars":{"artifact_path":"${download_url}"}' https://ansible-awx.parity.io/api/v2/job_templates/32/launch/
 
 
