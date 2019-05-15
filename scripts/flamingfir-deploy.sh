71418b3e441f653e8120de9f5df97ad3c39d8c97
David Dorgan <david@parity.io>
2019-05-15 20:30:45 +0100

    Use 8 char commit id

--- scripts/flamingfir-deploy.sh
+++ scripts/flamingfir-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DOWNLOAD_URL="https://releases.parity.io/substrate/x86_64-debian:stretch/2.0.0-${CI_COMMIT_SHORT_SHA}/substrate"
+COMMIT=$(echo `expr substr ${CI_COMMIT_SHORT_SHA} 0 8)
+DOWNLOAD_URL="https://releases.parity.io/substrate/x86_64-debian:stretch/2.0.0-${COMMIT}/substrate"
 POST_DATA='{"extra_vars":{"artifact_path":"'${DOWNLOAD_URL}'"}}'
 
-wget -O -  --header "Authorization: Bearer ${AWX_TOKEN}" --header "Content-type: application/json" --post-data "${POST_DATA}" https://ansible-awx.parity.io/api/v2/job_templates/32/launch/
+wget -O - --header "Authorization: Bearer ${AWX_TOKEN}" --header "Content-type: application/json" --post-data "${POST_DATA}" https://ansible-awx.parity.io/api/v2/job_templates/32/launch/
