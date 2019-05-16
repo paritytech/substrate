0d0045cf4e6b6802bb8bc78024f9ae7adc001b14
David Dorgan <david@parity.io>
2019-05-16 13:00:55 +0100

    Uncomment wget

--- scripts/flamingfir-deploy.sh
+++ scripts/flamingfir-deploy.sh
@@ -6,4 +6,4 @@ COMMIT=$(echo ${CI_BUILD_REF} | cut -c -9)
 DOWNLOAD_URL="https://releases.parity.io/substrate/x86_64-debian:stretch/2.0.0-${COMMIT}/substrate"
 POST_DATA='{"extra_vars":{"artifact_path":"'${DOWNLOAD_URL}'","target_host":"'${TARGET_HOST}'"}}'
 
-#wget -O - --header "Authorization: Bearer ${AWX_TOKEN}" --header "Content-type: application/json" --post-data "${POST_DATA}" https://ansible-awx.parity.io/api/v2/job_templates/32/launch/
+wget -O - --header "Authorization: Bearer ${AWX_TOKEN}" --header "Content-type: application/json" --post-data "${POST_DATA}" https://ansible-awx.parity.io/api/v2/job_templates/32/launch/
