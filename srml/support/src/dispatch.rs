f5909a905809f33dd72f412bcfa184c934f6c24b
thiolliere <gui.thiolliere@gmail.com>
2019-05-20 10:38:03 +0200

    add test

--- srml/support/src/dispatch.rs
+++ srml/support/src/dispatch.rs
@@ -1391,3 +1391,32 @@ mod tests {
 		<Module<TraitImpl> as OnFinalize<u32>>::on_finalize(42);
 	}
 }
+
+#[cfg(test)]
+mod reserved_keyword {
+	/// ```compile_fail
+	/// pub use srml_support::dispatch::Result;
+	///
+	/// pub trait Trait {
+	/// 	type Origin;
+	/// 	type BlockNumber: Into<u32>;
+	/// }
+	///
+	/// pub mod system {
+	/// 	use srml_support::dispatch::Result;
+	///
+	/// 	pub fn ensure_root<R>(_: R) -> Result {
+	/// 		Ok(())
+	/// 	}
+	/// }
+	///
+	/// srml_support::decl_module! {
+	/// 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+	/// 		/// Hi, this is a comment.
+	/// 		fn on_initialize() -> Result { unreachable!() }
+	/// 	}
+	/// }
+	/// ```
+	#[allow(unused)]
+	struct TestDeclModule;
+}
