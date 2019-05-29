fac15fdcb2a02094c9e9f0502066abf1c4b936ee
thiolliere <gui.thiolliere@gmail.com>
2019-05-29 14:16:37 +0200

    derive codec for inherent data (#2714)
    * derive codec for inherent data
    
    * increase spec version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -58,7 +58,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 86,
+	spec_version: 87,
 	impl_version: 87,
 	apis: RUNTIME_API_VERSIONS,
 };
