c9a67c727ee40b4c6249e2d2be262dc7b339c2b2
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2019-05-30 17:30:21 +0200

    Bump structopt and fix compilation. (#2736)

--- core/cli/src/lib.rs
+++ core/cli/src/lib.rs
@@ -50,7 +50,7 @@ pub use structopt::clap::App;
 use params::{
 	RunCmd, PurgeChainCmd, RevertCmd, ImportBlocksCmd, ExportBlocksCmd, BuildSpecCmd,
 	NetworkConfigurationParams, MergeParameters, TransactionPoolParams,
-	NodeKeyParams, NodeKeyType
+	NodeKeyParams, NodeKeyType, Cors,
 };
 pub use params::{NoCustom, CoreParams, SharedParams};
 pub use traits::{GetLogFilter, AugmentClap};
@@ -485,9 +485,9 @@ where
 	config.rpc_ws_max_connections = cli.ws_max_connections;
 	config.rpc_cors = cli.rpc_cors.unwrap_or_else(|| if is_dev {
 		log::warn!("Running in --dev mode, RPC CORS has been disabled.");
-		None
+		Cors::All
 	} else {
-		Some(vec![
+		Cors::List(vec![
 			"http://localhost:*".into(),
 			"http://127.0.0.1:*".into(),
 			"https://localhost:*".into(),
@@ -495,7 +495,7 @@ where
 			"https://polkadot.js.org".into(),
 			"https://substrate-ui.parity.io".into(),
 		])
-	});
+	}).into();
 
 	// Override telemetry
 	if cli.no_telemetry {
