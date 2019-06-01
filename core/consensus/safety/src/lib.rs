3f39ae0496c27a323d5895b97aaba3e52f3c133b
Marcio Diaz <marcio.diaz@gmail.com>
2019-06-01 20:36:13 +0200

    Add trace

--- core/consensus/safety/src/lib.rs
+++ core/consensus/safety/src/lib.rs
@@ -25,6 +25,7 @@ use parity_codec::{Encode, Decode};
 use std::sync::Arc;
 use runtime_primitives::traits::Block;
 use runtime_primitives::generic::BlockId;
+use log::{error, warn, debug, info, trace};
 
 /// Submit report call to the transaction pool.
 /// TODO: Ask how to do submit an unsigned in the proper way.
@@ -37,12 +38,12 @@ pub fn submit_report_call<A, B, C>(
 	A: txpool::ChainApi<Block=B>,
 	C: client::blockchain::HeaderBackend<B>,
 {
-	println!("SUBMIT_REPORT_CALL");
+	info!(target: "accountable-safety", "Submitting report call to tx pool");
 	let extrinsic = UncheckedExtrinsic::new_unsigned(report_call);
 	let uxt = Decode::decode(&mut extrinsic.encode().as_slice())
 		.expect("Encoded extrinsic is valid");
 	let block_id = BlockId::<B>::number(client.info().unwrap().best_number);
 	if let Err(e) = transaction_pool.submit_one(&block_id, uxt) {
-		println!("Error importing misbehavior report: {:?}", e);
+		info!(target: "accountable-safety", "Error importing misbehavior report: {:?}", e);
 	}
 }
