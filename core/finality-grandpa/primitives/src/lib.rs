8e8a9c6b54fead7ecfe956ebb2e322640d4eaffa
Marcio Diaz <marcio.diaz@gmail.com>
2019-06-05 08:56:46 +0200

    Bring prevote, precommit and Equivocation from grandpa primitives

--- core/finality-grandpa/primitives/src/lib.rs
+++ core/finality-grandpa/primitives/src/lib.rs
@@ -28,6 +28,7 @@ use client::decl_runtime_apis;
 use rstd::vec::Vec;
 
 use ed25519::Public as AuthorityId;
+pub use grandpa::{Precommit, Prevote, Equivocation};
 
 /// A scheduled change of authority set.
 #[cfg_attr(feature = "std", derive(Debug, PartialEq))]
