a0215fd4f7d64879d6c8461169bc39bf5dbf1fae
Marcio Diaz <marcio.diaz@gmail.com>
2019-06-05 14:50:32 +0200

    Add ValidateUnsigned to grandpa

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -234,7 +234,7 @@ construct_runtime!(
 		CouncilMotions: council_motions::{Module, Call, Storage, Event<T>, Origin},
 		CouncilSeats: council_seats::{Config<T>},
 		FinalityTracker: finality_tracker::{Module, Call, Inherent},
-		Grandpa: grandpa::{Module, Call, Storage, Config<T>, Log(), Event<T>},
+		Grandpa: grandpa::{Module, Call, Storage, Config<T>, Log(), Event<T>, ValidateUnsigned},
 		Treasury: treasury,
 		Contract: contract::{Module, Call, Storage, Config<T>, Event<T>},
 		Sudo: sudo,
