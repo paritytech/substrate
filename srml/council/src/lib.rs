e598f50aa687168dd38045b7646b3788e36c9ff9
Kian Peymani <Kianenigma@users.noreply.github.com>
2019-06-07 15:27:32 +0200

    Enhanced Council. (#2407)
    * first --unclean-- working version of locks and weight decay
    
    * Refactor voter activity history.
    
    * Tuning offset calculation and tests
    
    * Remove print.
    
    * Configurable decay ratio.
    
    * Remove spaces.
    
    * Name for activity.
    
    * Fix some style
    
    * Re-write voters as chunked map-vec.
    
    * Adds panic todo.
    
    * Fix some of the grumbles.
    
    * Fix some of the grumbles.
    
    * Chunked approvals vector.
    
    * Introduce voter_index for set_approvas in favor of complexity.
    
    * Add a bit more docs
    
    * Add boolean approvals as scalar.
    
    * Some cleanups TODO removes.
    
    * enhance some error checking.
    
    * No more double_map
    
    * Combined voter info and fee.
    
    * Fix global tests
    
    * Bump.
    
    * Fix line length
    
    * Fix error message.
    
    * Kill As<T>.
    
    * Final fix.
    
    * _Further_ kill As.
    
    * Proper imbalance for fee.
    
    * Bump.
    
    * Fix spacing.
    
    * Update
    
    * Address grumbles.
    
    * Line width.

--- srml/council/src/lib.rs
+++ srml/council/src/lib.rs
@@ -116,6 +116,8 @@ mod tests {
 		type Event = Event;
 		type BadPresentation = ();
 		type BadReaper = ();
+		type BadVoterIndex = ();
+		type LoserCandidate = ();
 		type OnMembersChanged = CouncilMotions;
 	}
 	impl motions::Trait for Test {
@@ -124,35 +126,91 @@ mod tests {
 		type Event = Event;
 	}
 
-	pub fn new_test_ext(with_council: bool) -> runtime_io::TestExternalities<Blake2Hasher> {
-		let mut t = system::GenesisConfig::<Test>::default().build_storage().unwrap().0;
-		t.extend(balances::GenesisConfig::<Test>{
-			transaction_base_fee: 0,
-			transaction_byte_fee: 0,
-			balances: vec![(1, 10), (2, 20), (3, 30), (4, 40), (5, 50), (6, 60)],
-			existential_deposit: 0,
-			transfer_fee: 0,
-			creation_fee: 0,
-			vesting: vec![],
-		}.build_storage().unwrap().0);
-		t.extend(democracy::GenesisConfig::<Test>::default().build_storage().unwrap().0);
-		t.extend(seats::GenesisConfig::<Test> {
-			candidacy_bond: 9,
-			voter_bond: 3,
-			present_slash_per_voter: 1,
-			carry_count: 2,
-			inactive_grace_period: 1,
-			active_council: if with_council { vec![
-				(1, 10),
-				(2, 10),
-				(3, 10)
-			] } else { vec![] },
-			approval_voting_period: 4,
-			presentation_duration: 2,
-			desired_seats: 2,
-			term_duration: 5,
-		}.build_storage().unwrap().0);
-		runtime_io::TestExternalities::new(t)
+	pub struct ExtBuilder {
+		balance_factor: u64,
+		decay_ratio: u32,
+		voting_fee: u64,
+		voter_bond: u64,
+		bad_presentation_punishment: u64,
+		with_council: bool,
+	}
+
+	impl Default for ExtBuilder {
+		fn default() -> Self {
+			Self {
+				balance_factor: 1,
+				decay_ratio: 24,
+				voting_fee: 0,
+				voter_bond: 0,
+				bad_presentation_punishment: 1,
+				with_council: false,
+			}
+		}
+	}
+
+	impl ExtBuilder {
+		pub fn with_council(mut self, council: bool) -> Self {
+			self.with_council = council;
+			self
+		}
+		pub fn balance_factor(mut self, factor: u64) -> Self {
+			self.balance_factor = factor;
+			self
+		}
+		pub fn decay_ratio(mut self, ratio: u32) -> Self {
+			self.decay_ratio = ratio;
+			self
+		}
+		pub fn voting_fee(mut self, fee: u64) -> Self {
+			self.voting_fee = fee;
+			self
+		}
+		pub fn bad_presentation_punishment(mut self, fee: u64) -> Self {
+			self.bad_presentation_punishment = fee;
+			self
+		}
+		pub fn voter_bond(mut self, fee: u64) -> Self {
+			self.voter_bond = fee;
+			self
+		}
+		pub fn build(self) -> runtime_io::TestExternalities<Blake2Hasher> {
+			let mut t = system::GenesisConfig::<Test>::default().build_storage().unwrap().0;
+			t.extend(balances::GenesisConfig::<Test>{
+				transaction_base_fee: 0,
+				transaction_byte_fee: 0,
+				balances: vec![
+					(1, 10 * self.balance_factor),
+					(2, 20 * self.balance_factor),
+					(3, 30 * self.balance_factor),
+					(4, 40 * self.balance_factor),
+					(5, 50 * self.balance_factor),
+					(6, 60 * self.balance_factor)
+				],
+				existential_deposit: 0,
+				transfer_fee: 0,
+				creation_fee: 0,
+				vesting: vec![],
+			}.build_storage().unwrap().0);
+			t.extend(seats::GenesisConfig::<Test> {
+				candidacy_bond: 3,
+				voter_bond: self.voter_bond,
+				present_slash_per_voter: self.bad_presentation_punishment,
+				carry_count: 2,
+				inactive_grace_period: 1,
+				active_council: if self.with_council { vec![
+					(1, 10),
+					(2, 10),
+					(3, 10)
+				] } else { vec![] },
+				approval_voting_period: 4,
+				presentation_duration: 2,
+				desired_seats: 2,
+				decay_ratio: self.decay_ratio,
+				voting_fee: self.voting_fee,
+				term_duration: 5,
+			}.build_storage().unwrap().0);
+			runtime_io::TestExternalities::new(t)
+		}
 	}
 
 	pub type System = system::Module<Test>;
