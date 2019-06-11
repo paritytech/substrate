7b339ef80d4bb076337b86ee6f01482a1d5e9889
David Craven <david@craven.ch>
2019-06-11 10:14:34 +0200

    Fix build.

--- core/finality-grandpa/src/communication/mod.rs
+++ core/finality-grandpa/src/communication/mod.rs
@@ -37,7 +37,6 @@ use log::{debug, trace};
 use parity_codec::{Encode, Decode};
 use substrate_primitives::{ed25519, Pair};
 use substrate_telemetry::{telemetry, CONSENSUS_DEBUG, CONSENSUS_INFO};
-use runtime_primitives::ConsensusEngineId;
 use runtime_primitives::traits::{Block as BlockT, Hash as HashT, Header as HeaderT};
 use network::{consensus_gossip as network_gossip, NetworkService};
 use network_gossip::ConsensusMessage;
