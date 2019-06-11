b2d7540001886918b37f618e1d3b97e87d9c96ad
Denis P <denis.pisarev@parity.io>
2019-06-11 22:23:28 +0200

    simplification

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,25 +4,25 @@
 
 set -e
 
-PROJECT_ROOT=`git rev-parse --show-toplevel`
-source "`dirname \"$0\"`/common.sh"
+PROJECT_ROOT=`git rev-parse --show-toplevel`    # /home/tripleight/code/github.com/paritytech/substrate
+source "`dirname \"$0\"`/common.sh"             # source scripts/common.sh
 
 export CARGO_INCREMENTAL=0
 
 # Save current directory.
-pushd .
+pushd .                                         # PROJECT_ROOT
 
-cd -- "$ROOT"
+cd -- "$ROOT"                                   # scripts
 
-for SRC in "${SRCS[@]}"
+for SRC in "${SRCS[@]}"                         # SRCS=("core/executor/wasm" "node/runtime/wasm" "node-template/runtime/wasm"	"core/test-runtime/wasm")
 do
   echo "*** Building wasm binaries in $SRC"
-  cd "$PROJECT_ROOT/$SRC"
-
-  ./build.sh
+  cd "$PROJECT_ROOT/$SRC"                       # 
+  
+  ./build.sh "$@"
 
   cd - >> /dev/null
 done
 
 # Restore initial directory.
-popd
+popd                                            # cd $PROJECT_ROOT
