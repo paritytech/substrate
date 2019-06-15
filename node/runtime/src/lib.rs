16ffaf76de4c5061cb7636c06ad902372f90d0cc
Wei Tang <hi@that.world>
2019-06-15 10:26:22 +0200

    Maintain TotalIssurance integrity in case of sudo set_balance (#2871)
    * Maintain TotalInsurance integrity in case of sudo set_balance
    
    * Fix set_balance docs
    
    * Update impl_version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -59,7 +59,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
 	spec_version: 94,
-	impl_version: 94,
+	impl_version: 95,
 	apis: RUNTIME_API_VERSIONS,
 };
 
