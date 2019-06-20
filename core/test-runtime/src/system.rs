90bc2614ba100913ee57a7e045ea55229b523df6
cheme <emericchevalier.pro@gmail.com>
2019-06-20 16:53:34 +0200

    Fix test runtime (was changing state after root calculation).

--- core/test-runtime/src/system.rs
+++ core/test-runtime/src/system.rs
@@ -124,7 +124,7 @@ fn execute_block_with_state_root_handler(
 	if let Some(storage_changes_root) = storage_changes_root(header.parent_hash.into()) {
 		digest.push(generic::DigestItem::ChangesTrieRoot(storage_changes_root.into()));
 	}
-	if let Some(new_authorities) = <NewAuthorities>::take() {
+	if let Some(new_authorities) = <NewAuthorities>::get() {
 		digest.push(generic::DigestItem::Consensus(*b"aura", new_authorities.encode()));
 		digest.push(generic::DigestItem::Consensus(*b"babe", new_authorities.encode()));
 	}
