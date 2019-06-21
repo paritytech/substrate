b5996431eb707f8eaef015ef081a5cf2166d451a
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2019-06-21 22:50:33 +0200

    feat: add `GrandpaUnjusitifed` type

--- srml/slashing/src/misconduct.rs
+++ srml/slashing/src/misconduct.rs
@@ -2,7 +2,6 @@ use crate::{CheckpointMisconduct, Misconduct, Fraction};
 
 /// An actor taking too long to respond
 /// Slash after each era, 0.05 * min(3(k-1) / n, 1)
-#[derive(Copy, Clone)]
 pub struct Unresponsive;
 
 impl Misconduct for Unresponsive {
@@ -22,6 +21,25 @@ impl CheckpointMisconduct for Unresponsive {
 	}
 }
 
+/// Grandpa misconducts
+pub mod grandpa {
+	use crate::{CheckpointMisconduct, Misconduct, Fraction};
+
+	/// Unjustified vote(s) from only one validator in the same era then slash 10%
+	// assumption: this is called in the end of the era otherwise it would be impossible to know
+	// that only one validator had performed a culprit in the era.
+	pub struct UnjustifiedVote;
+
+	impl Misconduct for UnjustifiedVote {
+		type Severity = u64;
+	}
+
+	impl CheckpointMisconduct for UnjustifiedVote {
+		fn severity(&self, _k: u64, _n: u64) -> Fraction<Self::Severity> {
+			Fraction::new(1, 10)
+		}
+	}
+}
 
 #[cfg(test)]
 mod tests {
@@ -43,5 +61,12 @@ mod tests {
 		let s = CheckpointMisconduct::severity(&Unresponsive, 34, 100);
 		let rate = s.denominator() as f64 / s.numerator() as f64;
 		assert_eq!(rate, 0.0495);
+	}
+
+	#[test]
+	fn grandpa_unjustified_vote() {
+		let s = CheckpointMisconduct::severity(&grandpa::UnjustifiedVote, 0, 0);
+		let rate = s.denominator() as f64 / s.numerator() as f64;
+		assert_eq!(rate, 0.10);
 	}
 }
