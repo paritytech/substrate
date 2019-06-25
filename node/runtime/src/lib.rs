9024a55b0554fd74433641ca7a7217fddc24785d
Sergey Pepyakin <sergei@parity.io>
2019-06-25 15:53:53 +0200

    Bump node runtime version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -58,8 +58,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 97,
-	impl_version: 99,
+	spec_version: 98,
+	impl_version: 100,
 	apis: RUNTIME_API_VERSIONS,
 };
 
