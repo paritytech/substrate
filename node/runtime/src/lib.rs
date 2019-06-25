0aff8a4bc83b5ec618c3d2bfac8d82f2f0d0d600
Robert Habermeier <rphmeier@gmail.com>
2019-06-25 18:06:08 +0200

    bump impl_version of node

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -59,7 +59,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
 	spec_version: 95,
-	impl_version: 95,
+	impl_version: 96,
 	apis: RUNTIME_API_VERSIONS,
 };
 
