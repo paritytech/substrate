18e68fcb8c6ecc79f063a7ec62ee19f7d409437f
cheme <emericchevalier.pro@gmail.com>
2019-06-25 13:04:30 +0200

    bum spec runtime version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -58,7 +58,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node"),
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
-	spec_version: 97,
+	spec_version: 98,
 	impl_version: 99,
 	apis: RUNTIME_API_VERSIONS,
 };
