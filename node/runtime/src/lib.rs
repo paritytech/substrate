6b343e3a21a22db86590ea3eb8d75ab1afb7262b
Jim Posen <jim.posen@gmail.com>
2019-06-27 17:35:49 +0200

    Bump node runtime spec/impl versions.

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -67,8 +67,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 103,
-	impl_version: 103,
+	spec_version: 104,
+	impl_version: 104,
 	apis: RUNTIME_API_VERSIONS,
 };
 
