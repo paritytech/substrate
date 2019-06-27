59d68d9a9c7f13d276a9dafc5d6541d309b358fe
kianenigma <kian.peymani@gmail.com>
2019-06-27 18:22:15 +0200

    Bump.

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -59,7 +59,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
 	spec_version: 98,
-	impl_version: 100,
+	impl_version: 101,
 	apis: RUNTIME_API_VERSIONS,
 };
 
