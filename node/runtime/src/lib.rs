ef07ffd8e0f1b145c93857c9c778c8109c2f1ee9
Kian Peymani <Kianenigma@users.noreply.github.com>
2019-06-28 14:36:46 +0200

    Enable staking equalise. (#2886)
    * Enable equalise.
    
    * Bump.
    
    * Line-width
    
    * Add benchmarks for equalise.
    
    * Line-width
    
    * Re-trigger CI.
    
    * Fix test.
    
    * Some nits.
    
    * Rename.
    
    * Bump.

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -59,7 +59,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("substrate-node"),
 	authoring_version: 10,
 	spec_version: 99,
-	impl_version: 104,
+	impl_version: 105,
 	apis: RUNTIME_API_VERSIONS,
 };
 
