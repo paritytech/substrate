e5a71e54c56efbad732d9e52e5b386b93f1d979d
Kian Peymani <Kianenigma@users.noreply.github.com>
2019-06-28 14:36:46 +0200

    Enable staking equalise. (#2886)
    * Enable equalise.
    
    * Bump.
    
    * Line-width
    
    * Add benchmarks for equalise.
    
    * Line-width
    
    * Re-trigger CI.
    
    * Fix test.
    
    * Some nits.
    
    * Rename.
    
    * Bump.

--- srml/staking/src/benches.rs
+++ srml/staking/src/benches.rs
@@ -35,7 +35,15 @@ const EDGES: u64 = 2;
 const TO_ELECT: usize = 100;
 const STAKE: u64 = 1000;
 
-fn do_phragmen(b: &mut Bencher, num_vals: u64, num_noms: u64, count: usize, votes_per: u64) {
+fn do_phragmen(
+	b: &mut Bencher,
+	num_vals: u64,
+	num_noms: u64,
+	count: usize,
+	votes_per: u64,
+	eq_iters: usize,
+	eq_tolerance: u128,
+) {
 	with_externalities(&mut ExtBuilder::default().nominate(false).build(), || {
 		assert!(num_vals > votes_per);
 		let rr = |a, b| rand::thread_rng().gen_range(a as usize, b as usize) as u64;
@@ -53,62 +61,135 @@ fn do_phragmen(b: &mut Bencher, num_vals: u64, num_noms: u64, count: usize, vote
 				let mut stashes_to_vote = (1 ..= 2*num_vals)
 					.step_by(2)
 					.map(|ctrl| ctrl + 1)
-					.collect::<Vec<AccountIdType>>();
+					.collect::<Vec<AccountId>>();
 				let votes = (0 .. votes_per)
 					.map(|_| {
 						stashes_to_vote.remove(rr(0, stashes_to_vote.len()) as usize)
 					})
-					.collect::<Vec<AccountIdType>>();
+					.collect::<Vec<AccountId>>();
 				bond_nominator(acc, STAKE + rr(10, 50), votes);
 			});
 
 		b.iter(|| {
-			let _ = phragmen::elect::<Test, _, _, _>(
+			let r = phragmen::elect::<Test, _, _, _>(
 				count,
 				1_usize,
 				<Validators<Test>>::enumerate(),
 				<Nominators<Test>>::enumerate(),
 				Staking::slashable_balance_of
-			);
+			).unwrap();
+
+			// Do the benchmarking with equalize.
+			if eq_iters > 0 {
+				let elected_stashes = r.0;
+				let assignments = r.1;
+
+				let to_balance = |b: ExtendedBalance|
+				<<mock::Test as Trait>::CurrencyToVote as Convert<ExtendedBalance, Balance>>::convert(b);
+				let to_votes = |b: Balance|
+					<<mock::Test as Trait>::CurrencyToVote as Convert<Balance, u64>>::convert(b) as ExtendedBalance;
+				let ratio_of = |b, p| (p as ExtendedBalance).saturating_mul(to_votes(b)) / ACCURACY;
+
+				let assignments_with_stakes = assignments.into_iter().map(|(n, a)|(
+					n,
+					Staking::slashable_balance_of(&n),
+					a.into_iter().map(|(acc, r)| (
+						acc.clone(),
+						r,
+						to_balance(ratio_of(Staking::slashable_balance_of(&n), r)),
+					))
+					.collect::<Vec<Assignment<Test>>>()
+				)).collect::<Vec<(AccountId, Balance, Vec<Assignment<Test>>)>>();
+
+				let mut exposures = <ExpoMap<Test>>::new();
+				elected_stashes
+					.into_iter()
+					.map(|e| (e, Staking::slashable_balance_of(&e)))
+					.for_each(|(e, s)| {
+						let item = Exposure { own: s, total: s, ..Default::default() };
+						exposures.insert(e, item);
+					});
+
+				for (n, _, assignment) in &assignments_with_stakes {
+					for (c, _, s) in assignment {
+						if let Some(expo) = exposures.get_mut(c) {
+							expo.total = expo.total.saturating_add(*s);
+							expo.others.push( IndividualExposure { who: n.clone(), value: *s } );
+						}
+					}
+				}
+
+				let mut assignments_with_votes = assignments_with_stakes.into_iter()
+					.map(|a| (
+						a.0, a.1,
+						a.2.into_iter()
+							.map(|e| (e.0, e.1, to_votes(e.2)))
+							.collect::<Vec<(AccountId, ExtendedBalance, ExtendedBalance)>>()
+					))
+					.collect::<Vec<(
+						AccountId,
+						Balance,
+						Vec<(AccountId, ExtendedBalance, ExtendedBalance)>
+					)>>();
+				equalize::<Test>(&mut assignments_with_votes, &mut exposures, eq_tolerance, eq_iters);
+			}
 		})
 	})
 }
 
 macro_rules! phragmen_benches {
 	($($name:ident: $tup:expr,)*) => {
-    $(
+	$(
         #[bench]
         fn $name(b: &mut Bencher) {
-			let (v, n, t, e) = $tup;
+			let (v, n, t, e, eq_iter, eq_tol) = $tup;
 			println!("");
 			println!(
-				"++ Benchmark: {} Validators // {} Nominators // {} Edges-per-nominator // {} total edges // electing {}",
-				v, n, e, e * n, t
+				r#"
+++ Benchmark: {} Validators // {} Nominators // {} Edges-per-nominator // {} total edges //
+electing {} // Equalize: {} iterations -- {} tolerance"#,
+				v, n, e, e * n, t, eq_iter, eq_tol,
 			);
-			do_phragmen(b, v, n, t, e);
+			do_phragmen(b, v, n, t, e, eq_iter, eq_tol);
         }
     )*
 	}
 }
 
 phragmen_benches! {
-	bench_1_1: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES),
-	bench_1_2: (VALIDATORS*2, NOMINATORS, TO_ELECT, EDGES),
-	bench_1_3: (VALIDATORS*4, NOMINATORS, TO_ELECT, EDGES),
-	bench_1_4: (VALIDATORS*8, NOMINATORS, TO_ELECT, EDGES),
+	bench_1_1: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES, 0, 0),
+	bench_1_2: (VALIDATORS*2, NOMINATORS, TO_ELECT, EDGES, 0, 0),
+	bench_1_3: (VALIDATORS*4, NOMINATORS, TO_ELECT, EDGES, 0, 0),
+	bench_1_4: (VALIDATORS*8, NOMINATORS, TO_ELECT, EDGES, 0, 0),
+	bench_1_1_eq: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES, 2, 0),
+	bench_1_2_eq: (VALIDATORS*2, NOMINATORS, TO_ELECT, EDGES, 2, 0),
+	bench_1_3_eq: (VALIDATORS*4, NOMINATORS, TO_ELECT, EDGES, 2, 0),
+	bench_1_4_eq: (VALIDATORS*8, NOMINATORS, TO_ELECT, EDGES, 2, 0),
 
-	bench_0_1: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES),
-	bench_0_2: (VALIDATORS, NOMINATORS, TO_ELECT * 4, EDGES),
-	bench_0_3: (VALIDATORS, NOMINATORS, TO_ELECT * 8, EDGES),
-	bench_0_4: (VALIDATORS, NOMINATORS, TO_ELECT * 16, EDGES),
+	bench_0_1: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES, 0, 0),
+	bench_0_2: (VALIDATORS, NOMINATORS, TO_ELECT * 4, EDGES, 0, 0),
+	bench_0_3: (VALIDATORS, NOMINATORS, TO_ELECT * 8, EDGES, 0, 0),
+	bench_0_4: (VALIDATORS, NOMINATORS, TO_ELECT * 16, EDGES , 0, 0),
+	bench_0_1_eq: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES, 2, 0),
+	bench_0_2_eq: (VALIDATORS, NOMINATORS, TO_ELECT * 4, EDGES, 2, 0),
+	bench_0_3_eq: (VALIDATORS, NOMINATORS, TO_ELECT * 8, EDGES, 2, 0),
+	bench_0_4_eq: (VALIDATORS, NOMINATORS, TO_ELECT * 16, EDGES , 2, 0),
 
-	bench_2_1: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES),
-	bench_2_2: (VALIDATORS, NOMINATORS*2, TO_ELECT, EDGES),
-	bench_2_3: (VALIDATORS, NOMINATORS*4, TO_ELECT, EDGES),
-	bench_2_4: (VALIDATORS, NOMINATORS*8, TO_ELECT, EDGES),
+	bench_2_1: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES, 0, 0),
+	bench_2_2: (VALIDATORS, NOMINATORS*2, TO_ELECT, EDGES, 0, 0),
+	bench_2_3: (VALIDATORS, NOMINATORS*4, TO_ELECT, EDGES, 0, 0),
+	bench_2_4: (VALIDATORS, NOMINATORS*8, TO_ELECT, EDGES, 0, 0),
+	bench_2_1_eq: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES, 2, 0),
+	bench_2_2_eq: (VALIDATORS, NOMINATORS*2, TO_ELECT, EDGES, 2, 0),
+	bench_2_3_eq: (VALIDATORS, NOMINATORS*4, TO_ELECT, EDGES, 2, 0),
+	bench_2_4_eq: (VALIDATORS, NOMINATORS*8, TO_ELECT, EDGES, 2, 0),
 
-	bench_3_1: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES),
-	bench_3_2: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES*2),
-	bench_3_3: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES*4),
-	bench_3_4: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES*8),
+	bench_3_1: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES, 0, 0 ),
+	bench_3_2: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES*2, 0, 0),
+	bench_3_3: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES*4, 0, 0),
+	bench_3_4: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES*8, 0, 0),
+	bench_3_1_eq: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES, 2, 0),
+	bench_3_2_eq: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES*2, 2, 0),
+	bench_3_3_eq: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES*4, 2, 0),
+	bench_3_4_eq: (VALIDATORS, NOMINATORS, TO_ELECT, EDGES*8, 2, 0),
 }
--- srml/staking/src/lib.rs
+++ srml/staking/src/lib.rs
@@ -290,7 +290,7 @@ use primitives::traits::{
 use primitives::{Serialize, Deserialize};
 use system::ensure_signed;
 
-use phragmen::{elect, ACCURACY, ExtendedBalance};
+use phragmen::{elect, ACCURACY, ExtendedBalance, equalize};
 
 const RECENT_OFFLINE_COUNT: usize = 32;
 const DEFAULT_MINIMUM_VALIDATOR_COUNT: u32 = 4;
@@ -1077,7 +1077,7 @@ impl<T: Trait> Module<T> {
 			let ratio_of = |b, p| (p as ExtendedBalance).saturating_mul(to_votes(b)) / ACCURACY;
 
 			// Compute the actual stake from nominator's ratio.
-			let mut assignments_with_stakes = assignments.iter().map(|(n, a)|(
+			let assignments_with_stakes = assignments.iter().map(|(n, a)|(
 				n.clone(),
 				Self::slashable_balance_of(n),
 				a.iter().map(|(acc, r)| (
@@ -1111,17 +1111,22 @@ impl<T: Trait> Module<T> {
 				}
 			}
 
-			// This optimization will most likely be only applied off-chain.
-			let do_equalize = false;
-			if do_equalize {
-				let tolerance = 10 as u128;
-				let iterations = 10 as usize;
-				phragmen::equalize::<T>(
-					&mut assignments_with_stakes,
-					&mut exposures,
-					tolerance,
-					iterations
-				);
+			if cfg!(feature = "equalize") {
+				let tolerance = 0_u128;
+				let iterations = 2_usize;
+				let mut assignments_with_votes = assignments_with_stakes.iter()
+					.map(|a| (
+						a.0.clone(), a.1,
+						a.2.iter()
+							.map(|e| (e.0.clone(), e.1, to_votes(e.2)))
+							.collect::<Vec<(T::AccountId, ExtendedBalance, ExtendedBalance)>>()
+					))
+					.collect::<Vec<(
+						T::AccountId,
+						BalanceOf<T>,
+						Vec<(T::AccountId, ExtendedBalance, ExtendedBalance)>
+					)>>();
+				equalize::<T>(&mut assignments_with_votes, &mut exposures, tolerance, iterations);
 			}
 
 			// Clear Stakers and reduce their slash_count.
@@ -1141,9 +1146,11 @@ impl<T: Trait> Module<T> {
 				}
 				<Stakers<T>>::insert(c.clone(), e.clone());
 			}
+
+			// Update slot stake.
 			<SlotStake<T>>::put(&slot_stake);
 
-			// Set the new validator set.
+			// Set the new validator set in sessions.
 			<CurrentElected<T>>::put(&elected_stashes);
 			let validators = elected_stashes.into_iter()
 				.map(|s| Self::bonded(s).unwrap_or_default())
