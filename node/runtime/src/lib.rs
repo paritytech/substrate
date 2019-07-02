4f73fe991bd64c23fd484871d70d0d12b306507f
Jim Posen <jim.posen@gmail.com>
2019-07-02 20:07:00 +0200

    srml-contract: Validate that modules do not declare oversized tables. (#2969)
    * srml-contract: Validate that modules do not declare oversized tables.
    
    * Bump node runtime spec/impl versions.

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -66,8 +66,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// Per convention: if the runtime behavior changes, increment spec_version and set impl_version
 	// to equal spec_version. If only runtime implementation changes and behavior does not, then
 	// leave spec_version as is and increment impl_version.
-	spec_version: 101,
-	impl_version: 101,
+	spec_version: 102,
+	impl_version: 102,
 	apis: RUNTIME_API_VERSIONS,
 };
 
