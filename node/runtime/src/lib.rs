9c48cb847c186ddd9ac46de7aa34ab3ed3806bec
Gav Wood <gavin@parity.io>
2019-07-10 20:01:48 +0200

    Bump runtime

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -69,8 +69,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 108,
-	impl_version: 108,
+	spec_version: 109,
+	impl_version: 109,
 	apis: RUNTIME_API_VERSIONS,
 };
 
