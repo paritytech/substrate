05b215ee33abaac9d66763fded26324aaafade5d
Demi M. Obenour <demi@parity.io>
2019-07-11 12:58:32 -0400

    Fix run.sh and move it to scripts/

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash --
-set -euo pipefail
-PATH=~/.local/bin:~/.cargo/bin:/usr/local/bin:/usr/bin
-cargo check --all
-cargo test --all --release
-cargo run -- purge-chain --dev
-cargo run -- --dev
