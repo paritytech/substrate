c7e163a81fd7da9a3bec2cb0ab08e7130187bb5f
David Craven <david@craven.ch>
2019-07-12 14:25:35 +0200

    Rewrite keystore.

--- core/keystore/src/lib.rs
+++ core/keystore/src/lib.rs
@@ -14,185 +14,176 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate. If not, see <http://www.gnu.org/licenses/>.
 
-//! Keystore (and session key management) for ed25519 based chains like Polkadot.
+//! Session key store is an in memory key store.
 
 #![warn(missing_docs)]
 
+use parking_lot::Mutex;
+use substrate_primitives::crypto::{KeyTypeId, Pair};
 use std::collections::HashMap;
-use std::path::PathBuf;
-use std::fs::{self, File};
-use std::io::{self, Write};
+use std::sync::Arc;
+use std::time::{Duration, Instant};
 
-use substrate_primitives::crypto::{KeyTypeId, Pair, Public};
+// 24h in seconds
+const INACTIVITY_PERIOD: u64 = 60 * 60 * 24;
 
 /// Keystore error.
 #[derive(Debug, derive_more::Display, derive_more::From)]
 pub enum Error {
-	/// IO error.
-	Io(io::Error),
-	/// JSON error.
-	Json(serde_json::Error),
-	/// Invalid password.
-	#[display(fmt="Invalid password")]
-	InvalidPassword,
-	/// Invalid BIP39 phrase
-	#[display(fmt="Invalid recovery phrase (BIP39) data")]
-	InvalidPhrase,
 	/// Invalid seed
 	#[display(fmt="Invalid seed")]
 	InvalidSeed,
 }
 
+impl std::error::Error for Error {}
+
 /// Keystore Result
 pub type Result<T> = std::result::Result<T, Error>;
 
-impl std::error::Error for Error {
-	fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-		match self {
-			Error::Io(ref err) => Some(err),
-			Error::Json(ref err) => Some(err),
-			_ => None,
-		}
-	}
+/// Handler for `Store` events.
+trait StoreHandler: Send {
+	/// Called when a new session key is added to the store.
+	fn on_add_session_key(&mut self, pair: &[u8]);
 }
 
-/// Key store.
+/// Store for session keys.
 pub struct Store {
-	path: PathBuf,
-	additional: HashMap<(KeyTypeId, Vec<u8>), Vec<u8>>,
+	handlers: Mutex<Vec<(KeyTypeId, Box<dyn StoreHandler>)>>,
 }
 
 impl Store {
-	/// Create a new store at the given path.
-	pub fn open(path: PathBuf) -> Result<Self> {
-		fs::create_dir_all(&path)?;
-		Ok(Store { path, additional: HashMap::new() })
-	}
-
-	fn get_pair<TPair: Pair>(&self, public: &TPair::Public) -> Result<Option<TPair>> {
-		let key = (TPair::KEY_TYPE, public.to_raw_vec());
-		if let Some(bytes) = self.additional.get(&key) {
-			let pair = TPair::from_seed_slice(bytes)
-				.map_err(|_| Error::InvalidSeed)?;
-			return Ok(Some(pair));
+	/// Create a new store.
+	pub fn new() -> Self {
+		Self {
+			handlers: Default::default(),
 		}
-		Ok(None)
 	}
 
-	fn insert_pair<TPair: Pair>(&mut self, pair: &TPair) {
-		let key = (TPair::KEY_TYPE, pair.public().to_raw_vec());
-		self.additional.insert(key, pair.to_raw_vec());
-	}
-
-	/// Generate a new key, placing it into the store.
-	pub fn generate<TPair: Pair>(&self, password: &str) -> Result<TPair> {
-		let (pair, phrase, _) = TPair::generate_with_phrase(Some(password));
-		let mut file = File::create(self.key_file_path::<TPair>(&pair.public()))?;
-		::serde_json::to_writer(&file, &phrase)?;
-		file.flush()?;
-		Ok(pair)
-	}
-
-	/// Create a new key from seed. Do not place it into the store.
-	pub fn generate_from_seed<TPair: Pair>(&mut self, seed: &str) -> Result<TPair> {
-		let pair = TPair::from_string(seed, None)
-			.ok().ok_or(Error::InvalidSeed)?;
-		self.insert_pair(&pair);
-		Ok(pair)
-	}
-
-	/// Load a key file with given public key.
-	pub fn load<TPair: Pair>(&self, public: &TPair::Public, password: &str) -> Result<TPair> {
-		if let Some(pair) = self.get_pair(public)? {
-			return Ok(pair)
-		}
-
-		let path = self.key_file_path::<TPair>(public);
-		let file = File::open(path)?;
-
-		let phrase: String = ::serde_json::from_reader(&file)?;
-		let (pair, _) = TPair::from_phrase(&phrase, Some(password))
-			.ok().ok_or(Error::InvalidPhrase)?;
-		if &pair.public() != public {
-			return Err(Error::InvalidPassword);
-		}
-		Ok(pair)
-	}
-
-	/// Get public keys of all stored keys.
-	pub fn contents<TPublic: Public>(&self) -> Result<Vec<TPublic>> {
-		let mut public_keys: Vec<TPublic> = self.additional.keys()
-			.filter_map(|(ty, public)| {
-				if *ty != TPublic::KEY_TYPE {
-					return None
-				}
-				Some(TPublic::from_slice(public))
-			})
-			.collect();
-
-		let key_type: [u8; 4] = TPublic::KEY_TYPE.to_le_bytes();
-		for entry in fs::read_dir(&self.path)? {
-			let entry = entry?;
-			let path = entry.path();
-
-			// skip directories and non-unicode file names (hex is unicode)
-			if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
-				match hex::decode(name) {
-					Ok(ref hex) => {
-						if hex[0..4] != key_type { continue	}
-						let public = TPublic::from_slice(&hex[4..]);
-						public_keys.push(public);
-					}
-					_ => continue,
-				}
+	/// Adds a session key pair.
+	pub fn add_key<TPair: Pair>(&self, pair: &TPair) {
+		let value = pair.to_raw_vec();
+		let mut handlers = self.handlers.lock();
+		for (key_type, handler) in handlers.iter_mut() {
+			if *key_type == TPair::KEY_TYPE {
+				handler.on_add_session_key(&value[..]);
 			}
 		}
-
-		Ok(public_keys)
 	}
 
-	fn key_file_path<TPair: Pair>(&self, public: &TPair::Public) -> PathBuf {
-		let mut buf = self.path.clone();
-		let bytes: [u8; 4] = TPair::KEY_TYPE.to_le_bytes();
-		let key_type = hex::encode(bytes);
-		let key = hex::encode(public.as_slice());
-		buf.push(key_type + key.as_str());
-		buf
+	/// Adds a session key pair from a seed. Used for testing.
+	pub fn add_key_from_seed<TPair: Pair>(&self, seed: &str) -> Result<()> {
+		let pair = TPair::from_string(seed, None)
+			.map_err(|_| Error::InvalidSeed)?;
+		self.add_key(&pair);
+		Ok(())
+	}
+
+	/// Creates a local session store.
+	pub fn create_local_store<TPair>(&self) -> Arc<LocalStore<TPair>>
+	where
+		TPair: Pair + Send + Clone,
+		TPair::Public: Send + Clone,
+	{
+		let session_store = Arc::new(LocalStore::new());
+		self.handlers.lock().push((TPair::KEY_TYPE, Box::new(session_store.clone())));
+		session_store
+	}
+}
+
+/// A local session store supporting a single crypto algorithm.
+pub struct LocalStore<TPair: Pair> {
+	keys: Mutex<HashMap<TPair::Public, (TPair, Instant)>>,
+}
+
+impl<TPair> LocalStore<TPair>
+where
+	TPair: Pair + Send + Clone,
+	TPair::Public: Send + Clone,
+{
+	/// Creates a new `LocalStore`.
+	pub fn new() -> Self {
+		Self {
+			keys: Default::default(),
+		}
+	}
+
+	/// Gets the key matching the public key.
+	pub fn get_key(&self, public: &TPair::Public) -> Option<TPair> {
+		let pair = {
+			let mut keys = self.keys.lock();
+			if let Some(value) = keys.get_mut(public) {
+				value.1 = Instant::now();
+				Some(value.0.clone())
+			} else {
+				None
+			}
+		};
+		self.clean();
+		pair
+	}
+
+	/// Find keys matching a predicate.
+	pub fn get_keys(&self, f: impl Fn(&TPair::Public) -> bool) -> Vec<TPair> {
+		let filtered_keys = {
+			self.keys.lock().iter_mut()
+				.filter(|(public, _)| f(public))
+				.map(|(_, value)| {
+					value.1 = Instant::now();
+					value.0.clone()
+				})
+				.collect::<Vec<_>>()
+		};
+		self.clean();
+		filtered_keys
+	}
+
+	/// Cleanup session store.
+	fn clean(&self) {
+		let mut keys = self.keys.lock();
+		if keys.len() > 1 {
+			let duration = Duration::new(INACTIVITY_PERIOD, 0);
+			let new_keys = keys.iter().filter(|(_, (_, instant))| {
+				Instant::now().duration_since(instant.clone()) < duration
+			}).map(|(k, v)| (k.clone(), v.clone())).collect::<HashMap<_, _>>();
+			// If a validator gets unstaked but then restaked some sessions later
+			// prevent the validator from getting slashed.
+			if new_keys.len() > 0 {
+				*keys = new_keys;
+			}
+		}
+	}
+}
+
+impl<TPair> StoreHandler for Arc<LocalStore<TPair>>
+where
+	TPair: Pair + Send,
+	TPair::Public: Send,
+{
+	fn on_add_session_key(&mut self, pair: &[u8]) {
+		let pair = TPair::from_seed_slice(pair)
+			.expect("store contains valid bytes");
+		let mut keys = self.keys.lock();
+		keys.insert(pair.public(), (pair, Instant::now()));
 	}
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use tempdir::TempDir;
 	use substrate_primitives::ed25519;
-	use substrate_primitives::crypto::Ss58Codec;
 
 	#[test]
-	fn basic_store() {
-		let temp_dir = TempDir::new("keystore").unwrap();
-		let store = Store::open(temp_dir.path().to_owned()).unwrap();
+	fn test_handler() {
+		let store = Store::new();
+		let local_store = store.create_local_store::<ed25519::Pair>();
 
-		assert!(store.contents::<ed25519::Public>().unwrap().is_empty());
-
-		let key: ed25519::Pair = store.generate("thepassword").unwrap();
-		let key2: ed25519::Pair = store.load(&key.public(), "thepassword").unwrap();
-
-		assert!(store.load::<ed25519::Pair>(&key.public(), "notthepassword").is_err());
-
-		assert_eq!(key.public(), key2.public());
-
-		assert_eq!(store.contents::<ed25519::Public>().unwrap()[0], key.public());
-	}
-
-	#[test]
-	fn test_generate_from_seed() {
-		let temp_dir = TempDir::new("keystore").unwrap();
-		let mut store = Store::open(temp_dir.path().to_owned()).unwrap();
-
-		let pair: ed25519::Pair = store
-			.generate_from_seed("0x3d97c819d68f9bafa7d6e79cb991eebcd77d966c5334c0b94d9e1fa7ad0869dc")
-			.unwrap();
-		assert_eq!("5DKUrgFqCPV8iAXx9sjy1nyBygQCeiUYRFWurZGhnrn3HJCA", pair.public().to_ss58check());
+		let key_a1 = ed25519::Pair::from_seed(&[0; 32]);
+		let key_b1 = ed25519::Pair::from_string("//Alice", None).unwrap();
+		store.set_key(&key_a1);
+		store.set_key_from_seed::<ed25519::Pair>("//Alice").unwrap();
+		let key_a2 = local_store.get_key(&key_a1.public()).unwrap();
+		let key_b2 = local_store.get_key(&key_b1.public()).unwrap();
+		assert_eq!(key_a1.to_raw_vec(), key_a2.to_raw_vec());
+		assert_eq!(key_b1.to_raw_vec(), key_b2.to_raw_vec());
 	}
 }
