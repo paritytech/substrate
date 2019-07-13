73bb12fa2f2e4cc2d051f49ac299913a5ba8e156
Demi M. Obenour <demi@parity.io>
2019-07-12 23:10:56 -0400

    Bump `impl_version`

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -70,7 +70,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
 	spec_version: 110,
-	impl_version: 110,
+	impl_version: 111,
 	apis: RUNTIME_API_VERSIONS,
 };
 
