166bc5c3e55270cc293e2c1f1d6edcb7eaca25a5
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2019-07-15 13:03:03 +0200

    nits

--- srml/rolling-window/src/lib.rs
+++ srml/rolling-window/src/lib.rs
@@ -68,15 +68,15 @@ decl_module! {
 impl<T: Trait> Module<T> {
 	/// Remove items that doesn't fit into the rolling window
 	pub fn on_session_end() {
-		let session = match SessionIndex::get().wrapping_add(1);
+		let session = SessionIndex::get().wrapping_add(1);
 
 		for (kind, _) in <MisconductReports<T>>::enumerate() {
 			<MisconductReports<T>>::mutate(kind, |window| {
 				// it is guaranteed that `reported_session` happened before `session`
 				window.retain(|(reported_session, window_length, _)| {
 
-					let diff = session.wrapping_sub(reported_session);
-					diff < window_length
+					let diff = session.wrapping_sub(*reported_session);
+					diff < *window_length
 				});
 			});
 		}
