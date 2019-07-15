4bd0fbc79eeffdb207471a4621588f722d1627f3
cheme <emericchevalier.pro@gmail.com>
2019-07-15 17:54:14 +0200

    Fix pruning test.

--- core/state-machine/src/changes_trie/prune.rs
+++ core/state-machine/src/changes_trie/prune.rs
@@ -230,18 +230,17 @@ mod tests {
 	fn prune_works() {
 		fn prepare_storage() -> InMemoryStorage<Blake2Hasher, u64> {
 
-			let child_prefix = ChildIndex::key_neutral_prefix(67u64);
+			let child_key = ChildIndex { block: 67u64, storage_key: b"1".to_vec() }.encode();
 			let mut mdb1 = MemoryDB::<Blake2Hasher>::default();
 			let root1 = insert_into_memory_db::<Blake2Hasher, _>(&mut mdb1, vec![(vec![10], vec![20])]).unwrap();
 			let mut mdb2 = MemoryDB::<Blake2Hasher>::default();
 			let root2 = insert_into_memory_db::<Blake2Hasher, _>(&mut mdb2, vec![(vec![11], vec![21]), (vec![12], vec![22])]).unwrap();
-			let mut ch_mdb3 = MemoryDB::<Blake2Hasher>::default();
-			let ch_root3 = insert_into_memory_db::<Blake2Hasher, _>(&mut ch_mdb3, vec![(vec![110], vec![120])]).unwrap();
 			let mut mdb3 = MemoryDB::<Blake2Hasher>::default();
+			let ch_root3 = insert_into_memory_db::<Blake2Hasher, _>(&mut mdb3, vec![(vec![110], vec![120])]).unwrap();
 			let root3 = insert_into_memory_db::<Blake2Hasher, _>(&mut mdb3, vec![
 				(vec![13], vec![23]),
 				(vec![14], vec![24]),
-				(child_prefix, ch_root3.as_ref().encode()),
+				(child_key, ch_root3.as_ref().encode()),
 			]).unwrap();
 			let mut mdb4 = MemoryDB::<Blake2Hasher>::default();
 			let root4 = insert_into_memory_db::<Blake2Hasher, _>(&mut mdb4, vec![(vec![15], vec![25])]).unwrap();
