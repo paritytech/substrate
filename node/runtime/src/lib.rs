8d53c9b59496439a40e33650c86d3d20ada28fbf
Amaury Martiny <amaury.martiny@protonmail.com>
2019-07-16 11:02:14 +0200

    Bump impl_version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -70,7 +70,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
 	spec_version: 111,
-	impl_version: 111,
+	impl_version: 112,
 	apis: RUNTIME_API_VERSIONS,
 };
 
