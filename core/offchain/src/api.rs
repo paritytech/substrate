bfa775c7fb8583ccb763d4ab662ddf8a94362c82
Gautam Dhameja <gautam@parity.io>
2019-07-18 16:56:45 +0200

    timestamp offchain api impl

--- core/offchain/src/api.rs
+++ core/offchain/src/api.rs
@@ -15,6 +15,8 @@
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
 use std::sync::Arc;
+use std::time::SystemTime;
+use std::convert::TryInto;
 use client::backend::OffchainStorage;
 use crate::AuthorityKeyProvider;
 use futures::{Stream, Future, sync::mpsc};
@@ -188,7 +190,19 @@ impl<Storage, KeyProvider> OffchainExt for Api<Storage, KeyProvider> where
 	}
 
 	fn timestamp(&mut self) -> Timestamp {
-		unavailable_yet("timestamp")
+		let now = SystemTime::now();
+		let epoch_duration = now.duration_since(SystemTime::UNIX_EPOCH);
+		match epoch_duration {
+			Err(_) => {
+				warn!("Current time is earlier than UNIX_EPOCH.");
+				Timestamp::from_unix_millis(0)
+				},
+			Ok(d) => {
+				let duration = d.as_millis();
+				// Assuming overflow won't happen for a few hundred years.
+				Timestamp::from_unix_millis(duration.try_into().unwrap())
+			}
+		}
 	}
 
 	fn sleep_until(&mut self, _deadline: Timestamp) {
@@ -367,6 +381,22 @@ mod tests {
 		);
 
 		AsyncApi::new(pool, db, "pass".to_owned().into(), TestProvider::default(), BlockId::Number(0))
+	}
+
+	#[test]
+	fn should_get_timestamp() {
+		let mut api = offchain_api().0;
+		
+		// Get timestamp from std.
+		let now = SystemTime::now();
+		let d: u64 = now.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis().try_into().unwrap();
+
+		// Get timestamp from offchain api.
+		let timestamp = api.timestamp();
+		
+		// Compare.
+		assert_eq!(timestamp.unix_millis() > 0, true);
+		assert_eq!(timestamp.unix_millis(), d);
 	}
 
 	#[test]
