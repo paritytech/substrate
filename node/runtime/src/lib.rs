77d0cb24385f2241b72d993b11d4c19478e38ae0
André Silva <andre.beat@gmail.com>
2019-07-19 10:30:59 +0100

    srml: grandpa: Pause/resume current authority set (#3068)
    * grandpa: add pause/resume signals to runtime module
    
    * grandpa: add tests for srml pause/resume transitions
    
    * node: bump spec_version
    
    * Apply suggestions from code review
    
    * Update core/finality-grandpa/primitives/src/lib.rs
    
    * Update core/finality-grandpa/primitives/src/lib.rs

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -69,7 +69,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 113,
+	spec_version: 114,
 	impl_version: 114,
 	apis: RUNTIME_API_VERSIONS,
 };
