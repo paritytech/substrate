5c46276be831cb25c4bf9f26091d1d6d230b3e8e
4meta5 <asinghchrony@protonmail.com>
2019-07-21 11:54:55 +0200

    change back impl

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -75,7 +75,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
 	spec_version: 115,
-	impl_version: 116,
+	impl_version: 115,
 	apis: RUNTIME_API_VERSIONS,
 };
 
