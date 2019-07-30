c77cf8e0274c07d77f6d21ed0a6e5445d7944da8
Bastian Köcher <git@kchr.de>
2019-07-30 11:39:29 +0200

    Fix test

--- core/state-machine/src/basic.rs
+++ core/state-machine/src/basic.rs
@@ -186,7 +186,7 @@ mod tests {
 		ext.set_storage(b"doe".to_vec(), b"reindeer".to_vec());
 		ext.set_storage(b"dog".to_vec(), b"puppy".to_vec());
 		ext.set_storage(b"dogglesworth".to_vec(), b"cat".to_vec());
-		const ROOT: [u8; 32] = hex!("0b33ed94e74e0f8e92a55923bece1ed02d16cf424e124613ddebc53ac3eeeabe");
+		const ROOT: [u8; 32] = hex!("0b41e488cccbd67d1f1089592c2c235f5c5399b053f7fe9152dd4b5f279914cd");
 		assert_eq!(ext.storage_root(), H256::from(ROOT));
 	}
 
