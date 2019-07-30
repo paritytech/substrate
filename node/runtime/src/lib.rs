76d64b5fcee771ba348d1cd1255f5c08da997e56
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2019-07-31 00:07:33 +0200

    Revert "Update schnorrkel to `0.7.0` (#3261)"
    This reverts commit fb28e11573f013adb3dc34bfadd796f9d081166b.

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -79,8 +79,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 124,
-	impl_version: 124,
+	spec_version: 123,
+	impl_version: 125,
 	apis: RUNTIME_API_VERSIONS,
 };
 
