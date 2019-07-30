af317719466e9ed7503507ab32db802e293b5621
Marcio Diaz <marcio.diaz@gmail.com>
2019-07-30 12:46:41 +0200

    Fix tests, node-template

--- core/consensus/aura/src/lib.rs
+++ core/consensus/aura/src/lib.rs
@@ -809,7 +809,8 @@ mod tests {
 						inherent_data_providers,
 						transaction_pool: Default::default(),
 						phantom: Default::default(),
-					}
+						transaction_pool: Default::default(),
+					})
 				},
 				PeersClient::Light(_) => unreachable!("No (yet) tests for light client + Aura"),
 			}
