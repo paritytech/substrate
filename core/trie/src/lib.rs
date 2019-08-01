d4024f97f4f7dcb04d2475ee47deb57399f09e4b
cheme <emericchevalier.pro@gmail.com>
2019-08-01 16:36:38 +0200

    Restoring test to 10 000.

--- core/trie/src/lib.rs
+++ core/trie/src/lib.rs
@@ -519,8 +519,7 @@ mod tests {
 	#[test]
 	fn random_should_work() {
 		let mut seed = <Blake2Hasher as Hasher>::Out::zero();
-		//for test_i in 0..10000 {
-		for test_i in 0..1000 {
+		for test_i in 0..10000 {
 			if test_i % 50 == 0 {
 				println!("{:?} of 10000 stress tests done", test_i);
 			}
