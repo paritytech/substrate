81d42d1cddefa87dcebe0467b3b93db035d0fc47
Demi M. Obenour <demi@parity.io>
2019-08-05 20:20:43 -0400

    Testing infrastructure for BABE
    Also includes a trivial additional test.

--- srml/babe/src/tests.rs
+++ srml/babe/src/tests.rs
@@ -17,9 +17,18 @@
 //! Consensus extension module tests for BABE consensus.
 
 use super::*;
+use runtime_io::with_externalities;
+use mock::new_test_ext;
 
 #[test]
-fn randomness_is_computed_correctly() {
+fn empty_randomness_is_correct() {
 	let s = compute_randomness([0; RANDOMNESS_LENGTH], 0, std::iter::empty(), None);
-	assert_eq!(s, [74, 25, 49, 128, 53, 97, 244, 49, 222, 202, 176, 2, 231, 66, 95, 10, 133, 49, 213, 228, 86, 161, 164, 127, 217, 153, 138, 37, 48, 192, 248, 0])
+	assert_eq!(s, [74, 25, 49, 128, 53, 97, 244, 49, 222, 202, 176, 2, 231, 66, 95, 10, 133, 49, 213, 228, 86, 161, 164, 127, 217, 153, 138, 37, 48, 192, 248, 0]);
+}
+
+#[test]
+fn check_module() {
+	with_externalities(&mut new_test_ext(vec![0, 1, 2, 3]), || {
+		<Module<mock::Test>>::randomness_change_epoch(1);
+	})
 }
