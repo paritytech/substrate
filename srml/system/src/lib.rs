16821cdccd50e51831a0a6b909884c8b41818904
Juan Aguilar <mhpoin@gmail.com>
2019-08-12 16:13:02 +0200

    Remove enumerate_trie_root in favour of ordered_trie_root #2382 (#3360)

--- srml/system/src/lib.rs
+++ srml/system/src/lib.rs
@@ -147,7 +147,7 @@ pub fn extrinsics_root<H: Hash, E: codec::Encode>(extrinsics: &[E]) -> H::Output
 /// Compute the trie root of a list of extrinsics.
 pub fn extrinsics_data_root<H: Hash>(xts: Vec<Vec<u8>>) -> H::Output {
 	let xts = xts.iter().map(Vec::as_slice).collect::<Vec<_>>();
-	H::enumerated_trie_root(&xts)
+	H::ordered_trie_root(&xts)
 }
 
 pub trait Trait: 'static + Eq + Clone {
