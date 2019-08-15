a1732fb1909d9adeccdc5000a3bbe34912ec121b
Svyatoslav Nikolsky <svyatonik@gmail.com>
2019-08-15 12:10:43 +0300

    intrduce && use configuration range in CT

--- core/state-machine/src/changes_trie/storage.rs
+++ core/state-machine/src/changes_trie/storage.rs
@@ -17,7 +17,7 @@
 //! Changes trie storage utilities.
 
 use std::collections::BTreeMap;
-use hash_db::{Hasher, Prefix};
+use hash_db::{Hasher, Prefix, EMPTY_PREFIX};
 use trie::DBValue;
 use trie::MemoryDB;
 use parking_lot::RwLock;
@@ -37,8 +37,8 @@ pub struct InMemoryStorage<H: Hasher, Number: BlockNumber> {
 }
 
 /// Adapter for using changes trie storage as a TrieBackendEssence' storage.
-pub struct TrieBackendAdapter<'a, H: Hasher, Number: BlockNumber, S: 'a + Storage<H, Number>> {
-	storage: &'a S,
+pub struct TrieBackendAdapter<'a, H: Hasher, Number: BlockNumber> {
+	storage: &'a dyn Storage<H, Number>,
 	_hasher: ::std::marker::PhantomData<(H, Number)>,
 }
 
@@ -48,7 +48,7 @@ struct InMemoryStorageData<H: Hasher, Number: BlockNumber> {
 }
 
 impl<H: Hasher, Number: BlockNumber> InMemoryStorage<H, Number> {
-	/// Create the storage from given in-memory database.
+	/// Creates storage from given in-memory database.
 	pub fn with_db(mdb: MemoryDB<H>) -> Self {
 		Self {
 			data: RwLock::new(InMemoryStorageData {
@@ -58,9 +58,20 @@ impl<H: Hasher, Number: BlockNumber> InMemoryStorage<H, Number> {
 		}
 	}
 
-	/// Create the storage with empty database.
+	/// Creates storage with empty database.
 	pub fn new() -> Self {
 		Self::with_db(Default::default())
+	}
+
+	/// Creates storage with given proof.
+	pub fn with_proof(proof: Vec<Vec<u8>>) -> Self {
+		use hash_db::HashDB;
+
+ 		let mut proof_db = MemoryDB::<H>::default();
+		for item in proof {
+			proof_db.insert(EMPTY_PREFIX, &item);
+		}
+		Self::with_db(proof_db)
 	}
 
 	/// Create the storage with given blocks.
@@ -132,20 +143,23 @@ impl<H: Hasher, Number: BlockNumber> RootsStorage<H, Number> for InMemoryStorage
 }
 
 impl<H: Hasher, Number: BlockNumber> Storage<H, Number> for InMemoryStorage<H, Number> {
+	fn as_roots_storage(&self) -> &dyn RootsStorage<H, Number> {
+		self
+	}
+
 	fn get(&self, key: &H::Out, prefix: Prefix) -> Result<Option<DBValue>, String> {
 		MemoryDB::<H>::get(&self.data.read().mdb, key, prefix)
 	}
 }
 
-impl<'a, H: Hasher, Number: BlockNumber, S: 'a + Storage<H, Number>> TrieBackendAdapter<'a, H, Number, S> {
-	pub fn new(storage: &'a S) -> Self {
+impl<'a, H: Hasher, Number: BlockNumber> TrieBackendAdapter<'a, H, Number> {
+	pub fn new(storage: &'a dyn Storage<H, Number>) -> Self {
 		Self { storage, _hasher: Default::default() }
 	}
 }
 
-impl<'a, H, Number, S> TrieBackendStorage<H> for TrieBackendAdapter<'a, H, Number, S>
+impl<'a, H, Number> TrieBackendStorage<H> for TrieBackendAdapter<'a, H, Number>
 	where
-		S: 'a + Storage<H, Number>,
 		Number: BlockNumber,
 		H: Hasher,
 {
