20ae48759df5ab035ed92e8e2e06fb5aa3c461fb
Svyatoslav Nikolsky <svyatonik@gmail.com>
2019-08-16 12:51:49 +0300

    Fix light client synchronization on master (#3301)
    * value ranges in consensus cache
    
    * skip values in cache
    
    * read epoch0 + epoch1 data from genesis in babe
    
    * sync authorities + session validators at genesis
    
    * removed some debug printlns
    
    * fixed cache encoding
    
    * Revert "skip values in cache"
    
    This reverts commit ce451c32823aaa4b67d99ca5b58f1bf3984df4db.
    
    * Revert "value ranges in consensus cache"
    
    This reverts commit 9062f9434cddd14a01275ddbfcd904b04282e63b.
    
    * get rid of cache::AUTHORITIES in Babe
    
    * cleaning up
    
    * cleaning up
    
    * update spec version
    
    * lost changes
    
    * fixed tests
    
    * Update node/runtime/src/lib.rs
    
    Co-Authored-By: DemiMarie-parity <48690212+DemiMarie-parity@users.noreply.github.com>
    
    * fix once-per-block condition
    
    * fix standalone babe + temp_storage in BuildGenesis
    
    * fix benhes compilation
    
    * fixed comment
    
    * re-added light nodes to integration tests
    
    * finalize_with_ancestors from extra_requests
    
    * post-merge fix
    
    * aaand removed debug code
    
    * (another one)
    
    * fix warn in logs (do not call ForkTree::finalize twice for the same block)
    
    * sync digest.next_authorities with actual next authorities
    
    * more docs
    
    * reverting all commits affecting storage
    
    * also remove keys from babe trait
    
    * fixed warnings
    
    * post-merge fixes
    
    * reverted some redundant changes
    
    * reverted more changes

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -80,8 +80,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 143,
-	impl_version: 143,
+	spec_version: 144,
+	impl_version: 144,
 	apis: RUNTIME_API_VERSIONS,
 };
 
