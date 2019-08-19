50123d2287f834ff6181babd1e49ea51eb356169
Max Inden <mail@max-inden.de>
2019-08-19 16:46:37 +0200

    {core,srml}/authority-discovery: Fix indentation

--- core/authority-discovery/primitives/src/lib.rs
+++ core/authority-discovery/primitives/src/lib.rs
@@ -25,32 +25,39 @@ use rstd::vec::Vec;
 decl_runtime_apis! {
 	/// The authority discovery api.
 	///
-	/// This api is used by the `core/authority-discovery` module to retrieve our own authority identifier, to retrieve
-	/// identifiers of the current authority set, as well as sign and verify Kademlia Dht external address payloads from
-	/// and to other authorities.
+	/// This api is used by the `core/authority-discovery` module to retrieve our
+	/// own authority identifier, to retrieve identifiers of the current authority
+	/// set, as well as sign and verify Kademlia Dht external address payloads
+	/// from and to other authorities.
 	///
 	/// # Note
 	///
-	/// The underlying cryptography key can change at any moment in time. This can for example be problematic when a key
-	/// change happens in between calling `public_key` and `sign`, resulting in a signature that is not signed by the
-	/// key corresponding to the public key retrieved in the first call. To prevent races like the one above, one can
-	/// verify that the retrieved signature corresponds to the public key by calling `verify` as a third step.
+	/// The underlying cryptography key can change at any moment in time. This can
+	/// for example be problematic when a key change happens in between calling
+	/// `public_key` and `sign`, resulting in a signature that is not signed by
+	/// the key corresponding to the public key retrieved in the first call. To
+	/// prevent races like the one above, one can verify that the retrieved
+	/// signature corresponds to the public key by calling `verify` as a third
+	/// step.
 	///
 	pub trait AuthorityDiscoveryApi<AuthorityId: Codec> {
-		/// Returns own authority identifier iff it is part of the current authority set, otherwise this function
-		/// returns None. The restriction might be softened in the future in case a consumer needs to learn own
-		/// authority identifier in any case.
+		/// Returns own authority identifier iff it is part of the current authority
+		/// set, otherwise this function returns None. The restriction might be
+		/// softened in the future in case a consumer needs to learn own authority
+		/// identifier in any case.
 		fn public_key() -> Option<AuthorityId>;
 
 		/// Retrieve authority identifiers of the current authority set.
 		fn authorities() -> Vec<AuthorityId>;
 
-		/// Sign the given payload with one of our authority keys. This key will correspond to the public key returned
-		/// by `public_key` unless the underlying key is changed in between calls. If own authority key is not part of
-		/// the current set of authorities, this function returns None.
+		/// Sign the given payload with one of our authority keys. This key will
+		/// correspond to the public key returned by `public_key` unless the
+		/// underlying key is changed in between calls. If own authority key is not
+		/// part of the current set of authorities, this function returns None.
 		fn sign(payload: Vec<u8>) -> Option<Vec<u8>>;
 
-		/// Verify the given signature for the given payload with the given authority identifier.
+		/// Verify the given signature for the given payload with the given
+		/// authority identifier.
 		fn verify(payload: Vec<u8>, signature: Vec<u8>, public_key: AuthorityId) -> bool;
 	}
 }
--- srml/authority-discovery/src/lib.rs
+++ srml/authority-discovery/src/lib.rs
@@ -36,275 +36,275 @@ use srml_support::{decl_module, decl_storage, StorageValue};
 pub trait Trait: system::Trait + session::Trait {}
 
 decl_storage! {
-    trait Store for Module<T: Trait> as AuthorityDiscovery {
-        /// The current set of keys that may issue a heartbeat.
-        Keys get(keys): Vec<im_online::AuthorityId>;
-    }
-    add_extra_genesis {
-        config(keys): Vec<im_online::AuthorityId>;
-        build(|
-              storage: &mut (sr_primitives::StorageOverlay, sr_primitives::ChildrenStorageOverlay),
-              config: &GenesisConfig
-              | {
-                  sr_io::with_storage(
-                      storage,
-                      || Module::<T>::initialize_keys(&config.keys),
-                  );
-              })
-    }
+	trait Store for Module<T: Trait> as AuthorityDiscovery {
+		/// The current set of keys that may issue a heartbeat.
+		Keys get(keys): Vec<im_online::AuthorityId>;
+	}
+	add_extra_genesis {
+		config(keys): Vec<im_online::AuthorityId>;
+		build(|
+			  storage: &mut (sr_primitives::StorageOverlay, sr_primitives::ChildrenStorageOverlay),
+			  config: &GenesisConfig
+			  | {
+				  sr_io::with_storage(
+					  storage,
+					  || Module::<T>::initialize_keys(&config.keys),
+				  );
+			  })
+	}
 }
 
 decl_module! {
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-    }
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+	}
 }
 
 impl<T: Trait> Module<T> {
-    /// Returns own authority identifier iff it is part of the current authority
-    /// set, otherwise this function returns None. The restriction might be
-    /// softened in the future in case a consumer needs to learn own authority
-    /// identifier in any case.
-    pub fn public_key() -> Option<im_online::AuthorityId> {
-        let authorities = Keys::get();
+	/// Returns own authority identifier iff it is part of the current authority
+	/// set, otherwise this function returns None. The restriction might be
+	/// softened in the future in case a consumer needs to learn own authority
+	/// identifier in any case.
+	pub fn public_key() -> Option<im_online::AuthorityId> {
+		let authorities = Keys::get();
 
-        let local_keys = im_online::AuthorityId::all();
+		let local_keys = im_online::AuthorityId::all();
 
-        let mut intersect: Vec<im_online::AuthorityId> = authorities
-            .into_iter()
-            .filter_map(|authority| {
-                if local_keys.contains(&authority) {
-                    Some(authority)
-                } else {
-                    None
-                }
-            })
-            .collect();
+		let mut intersect: Vec<im_online::AuthorityId> = authorities
+			.into_iter()
+			.filter_map(|authority| {
+				if local_keys.contains(&authority) {
+					Some(authority)
+				} else {
+					None
+				}
+			})
+			.collect();
 
-        intersect.pop()
-    }
+		intersect.pop()
+	}
 
-    /// Retrieve authority identifiers of the current authority set.
-    pub fn authorities() -> Vec<im_online::AuthorityId> {
-        Keys::get()
-    }
+	/// Retrieve authority identifiers of the current authority set.
+	pub fn authorities() -> Vec<im_online::AuthorityId> {
+		Keys::get()
+	}
 
-    /// Sign the given payload with one of our authority keys. This key will
-    /// correspond to the public key returned by `public_key` unless the
-    /// underlying key is changed in between calls. If own authority key is not
-    /// part of the current set of authorities, this function returns None.
-    pub fn sign(payload: Vec<u8>) -> Option<Vec<u8>> {
-        let pub_key = Module::<T>::public_key()?;
+	/// Sign the given payload with one of our authority keys. This key will
+	/// correspond to the public key returned by `public_key` unless the
+	/// underlying key is changed in between calls. If own authority key is not
+	/// part of the current set of authorities, this function returns None.
+	pub fn sign(payload: Vec<u8>) -> Option<Vec<u8>> {
+		let pub_key = Module::<T>::public_key()?;
 
-        pub_key.sign(&payload).map(|s| s.encode())
-    }
+		pub_key.sign(&payload).map(|s| s.encode())
+	}
 
-    /// Verify the given signature for the given payload with the given
-    /// authority identifier.
-    pub fn verify(
-        payload: Vec<u8>,
-        signature: Vec<u8>,
-        public_key: im_online::AuthorityId,
-    ) -> bool {
-        let sig: Result<im_online::AuthoritySignature, _> = Decode::decode(&mut &signature[..]);
+	/// Verify the given signature for the given payload with the given
+	/// authority identifier.
+	pub fn verify(
+		payload: Vec<u8>,
+		signature: Vec<u8>,
+		public_key: im_online::AuthorityId,
+	) -> bool {
+		let sig: Result<im_online::AuthoritySignature, _> = Decode::decode(&mut &signature[..]);
 
-        match sig {
-            Ok(sig) => public_key.verify(&payload, &sig),
-            Err(_e) => false,
-        }
-    }
+		match sig {
+			Ok(sig) => public_key.verify(&payload, &sig),
+			Err(_e) => false,
+		}
+	}
 
-    fn initialize_keys(keys: &[im_online::AuthorityId]) {
-        if !keys.is_empty() {
-            assert!(Keys::get().is_empty(), "Keys are already initialized!");
-            Keys::put_ref(keys);
-        }
-    }
+	fn initialize_keys(keys: &[im_online::AuthorityId]) {
+		if !keys.is_empty() {
+			assert!(Keys::get().is_empty(), "Keys are already initialized!");
+			Keys::put_ref(keys);
+		}
+	}
 }
 
 impl<T: Trait> session::OneSessionHandler<T::AccountId> for Module<T> {
-    type Key = im_online::AuthorityId;
+	type Key = im_online::AuthorityId;
 
-    fn on_genesis_session<'a, I: 'a>(validators: I)
-    where
-        I: Iterator<Item = (&'a T::AccountId, im_online::AuthorityId)>,
-    {
-        let keys = validators.map(|x| x.1).collect::<Vec<_>>();
-        Self::initialize_keys(&keys);
-    }
+	fn on_genesis_session<'a, I: 'a>(validators: I)
+	where
+		I: Iterator<Item = (&'a T::AccountId, im_online::AuthorityId)>,
+	{
+		let keys = validators.map(|x| x.1).collect::<Vec<_>>();
+		Self::initialize_keys(&keys);
+	}
 
-    fn on_new_session<'a, I: 'a>(_changed: bool, _validators: I, next_validators: I)
-    where
-        I: Iterator<Item = (&'a T::AccountId, im_online::AuthorityId)>,
-    {
-        // Remember who the authorities are for the new session.
-        Keys::put(next_validators.map(|x| x.1).collect::<Vec<_>>());
-    }
+	fn on_new_session<'a, I: 'a>(_changed: bool, _validators: I, next_validators: I)
+	where
+		I: Iterator<Item = (&'a T::AccountId, im_online::AuthorityId)>,
+	{
+		// Remember who the authorities are for the new session.
+		Keys::put(next_validators.map(|x| x.1).collect::<Vec<_>>());
+	}
 
-    fn on_disabled(_i: usize) {
-        // ignore
-    }
+	fn on_disabled(_i: usize) {
+		// ignore
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use app_crypto::Pair;
-    use primitives::testing::KeyStore;
-    use primitives::{crypto::key_types, sr25519, traits::BareCryptoStore, H256};
-    use sr_io::{with_externalities, TestExternalities};
-    use sr_primitives::testing::{Header, UintAuthorityId};
-    use sr_primitives::traits::{ConvertInto, IdentityLookup, OpaqueKeys};
-    use sr_primitives::Perbill;
-    use srml_support::{impl_outer_origin, parameter_types};
+	use super::*;
+	use app_crypto::Pair;
+	use primitives::testing::KeyStore;
+	use primitives::{crypto::key_types, sr25519, traits::BareCryptoStore, H256};
+	use sr_io::{with_externalities, TestExternalities};
+	use sr_primitives::testing::{Header, UintAuthorityId};
+	use sr_primitives::traits::{ConvertInto, IdentityLookup, OpaqueKeys};
+	use sr_primitives::Perbill;
+	use srml_support::{impl_outer_origin, parameter_types};
 
-    type AuthorityDiscovery = Module<Test>;
-    type SessionIndex = u32;
+	type AuthorityDiscovery = Module<Test>;
+	type SessionIndex = u32;
 
-    #[derive(Clone, Eq, PartialEq)]
-    pub struct Test;
-    impl Trait for Test {}
+	#[derive(Clone, Eq, PartialEq)]
+	pub struct Test;
+	impl Trait for Test {}
 
-    pub struct TestOnSessionEnding;
-    impl session::OnSessionEnding<im_online::AuthorityId> for TestOnSessionEnding {
-        fn on_session_ending(
-            _: SessionIndex,
-            _: SessionIndex,
-        ) -> Option<Vec<im_online::AuthorityId>> {
-            None
-        }
-    }
+	pub struct TestOnSessionEnding;
+	impl session::OnSessionEnding<im_online::AuthorityId> for TestOnSessionEnding {
+		fn on_session_ending(
+			_: SessionIndex,
+			_: SessionIndex,
+		) -> Option<Vec<im_online::AuthorityId>> {
+			None
+		}
+	}
 
-    impl session::Trait for Test {
-        type OnSessionEnding = TestOnSessionEnding;
-        type Keys = UintAuthorityId;
-        type ShouldEndSession = session::PeriodicSessions<Period, Offset>;
-        type SessionHandler = TestSessionHandler;
-        type Event = ();
-        type ValidatorId = im_online::AuthorityId;
-        type ValidatorIdOf = ConvertInto;
-        type SelectInitialValidators = ();
-    }
+	impl session::Trait for Test {
+		type OnSessionEnding = TestOnSessionEnding;
+		type Keys = UintAuthorityId;
+		type ShouldEndSession = session::PeriodicSessions<Period, Offset>;
+		type SessionHandler = TestSessionHandler;
+		type Event = ();
+		type ValidatorId = im_online::AuthorityId;
+		type ValidatorIdOf = ConvertInto;
+		type SelectInitialValidators = ();
+	}
 
-    pub type BlockNumber = u64;
+	pub type BlockNumber = u64;
 
-    parameter_types! {
-        pub const Period: BlockNumber = 1;
-        pub const Offset: BlockNumber = 0;
-        pub const UncleGenerations: u64 = 0;
-        pub const BlockHashCount: u64 = 250;
-        pub const MaximumBlockWeight: u32 = 1024;
-        pub const MaximumBlockLength: u32 = 2 * 1024;
-        pub const AvailableBlockRatio: Perbill = Perbill::one();
-    }
+	parameter_types! {
+		pub const Period: BlockNumber = 1;
+		pub const Offset: BlockNumber = 0;
+		pub const UncleGenerations: u64 = 0;
+		pub const BlockHashCount: u64 = 250;
+		pub const MaximumBlockWeight: u32 = 1024;
+		pub const MaximumBlockLength: u32 = 2 * 1024;
+		pub const AvailableBlockRatio: Perbill = Perbill::one();
+	}
 
-    impl system::Trait for Test {
-        type Origin = Origin;
-        type Index = u64;
-        type BlockNumber = BlockNumber;
-        type Call = ();
-        type Hash = H256;
-        type Hashing = ::sr_primitives::traits::BlakeTwo256;
-        type AccountId = im_online::AuthorityId;
-        type Lookup = IdentityLookup<Self::AccountId>;
-        type Header = Header;
-        type WeightMultiplierUpdate = ();
-        type Event = ();
-        type BlockHashCount = BlockHashCount;
-        type MaximumBlockWeight = MaximumBlockWeight;
-        type AvailableBlockRatio = AvailableBlockRatio;
-        type MaximumBlockLength = MaximumBlockLength;
-	      type Version = ();
-    }
+	impl system::Trait for Test {
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = BlockNumber;
+		type Call = ();
+		type Hash = H256;
+		type Hashing = ::sr_primitives::traits::BlakeTwo256;
+		type AccountId = im_online::AuthorityId;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type WeightMultiplierUpdate = ();
+		type Event = ();
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type MaximumBlockLength = MaximumBlockLength;
+		type Version = ();
+	}
 
-    impl_outer_origin! {
-        pub enum Origin for Test {}
-    }
+	impl_outer_origin! {
+		pub enum Origin for Test {}
+	}
 
-    pub struct TestSessionHandler;
-    impl session::SessionHandler<im_online::AuthorityId> for TestSessionHandler {
-        fn on_new_session<Ks: OpaqueKeys>(
-            _changed: bool,
-            _validators: &[(im_online::AuthorityId, Ks)],
-            _queued_validators: &[(im_online::AuthorityId, Ks)],
-        ) {
-        }
+	pub struct TestSessionHandler;
+	impl session::SessionHandler<im_online::AuthorityId> for TestSessionHandler {
+		fn on_new_session<Ks: OpaqueKeys>(
+			_changed: bool,
+			_validators: &[(im_online::AuthorityId, Ks)],
+			_queued_validators: &[(im_online::AuthorityId, Ks)],
+		) {
+		}
 
-        fn on_disabled(_validator_index: usize) {}
+		fn on_disabled(_validator_index: usize) {}
 
-        fn on_genesis_session<Ks: OpaqueKeys>(_validators: &[(im_online::AuthorityId, Ks)]) {}
-    }
+		fn on_genesis_session<Ks: OpaqueKeys>(_validators: &[(im_online::AuthorityId, Ks)]) {}
+	}
 
-    #[test]
-    fn public_key_fn_returns_intersection_of_current_authorities_and_keys_in_key_store() {
-        // Create keystore and generate key.
-        let key_store = KeyStore::new();
-        key_store
-            .write()
-            .sr25519_generate_new(key_types::IM_ONLINE, None)
-            .expect("Generates key.");
+	#[test]
+	fn public_key_fn_returns_intersection_of_current_authorities_and_keys_in_key_store() {
+		// Create keystore and generate key.
+		let key_store = KeyStore::new();
+		key_store
+			.write()
+			.sr25519_generate_new(key_types::IM_ONLINE, None)
+			.expect("Generates key.");
 
-        // Retrieve key to later check if we got the right one.
-        let public_key = key_store
-            .read()
-            .sr25519_public_keys(key_types::IM_ONLINE)
-            .pop()
-            .unwrap();
-        let authority_id = im_online::AuthorityId::from(public_key);
+		// Retrieve key to later check if we got the right one.
+		let public_key = key_store
+			.read()
+			.sr25519_public_keys(key_types::IM_ONLINE)
+			.pop()
+			.unwrap();
+		let authority_id = im_online::AuthorityId::from(public_key);
 
-        // Build genesis.
-        let mut t = system::GenesisConfig::default()
-            .build_storage::<Test>()
-            .unwrap();
+		// Build genesis.
+		let mut t = system::GenesisConfig::default()
+			.build_storage::<Test>()
+			.unwrap();
 
-        GenesisConfig {
-            keys: vec![authority_id.clone()],
-        }
-        .assimilate_storage::<Test>(&mut t)
-        .unwrap();
+		GenesisConfig {
+			keys: vec![authority_id.clone()],
+		}
+		.assimilate_storage::<Test>(&mut t)
+		.unwrap();
 
-        // Create externalities.
-        let mut externalities = TestExternalities::new(t);
-        externalities.set_keystore(key_store);
+		// Create externalities.
+		let mut externalities = TestExternalities::new(t);
+		externalities.set_keystore(key_store);
 
-        with_externalities(&mut externalities, || {
-            assert_eq!(
-                authority_id,
-                AuthorityDiscovery::public_key().expect("Retrieving public key.")
-            );
-        });
-    }
+		with_externalities(&mut externalities, || {
+			assert_eq!(
+				authority_id,
+				AuthorityDiscovery::public_key().expect("Retrieving public key.")
+			);
+		});
+	}
 
-    #[test]
-    fn public_key_fn_does_not_return_key_outside_current_authority_set() {
-        // Create keystore and generate key.
-        let key_store = KeyStore::new();
-        key_store
-            .write()
-            .sr25519_generate_new(key_types::IM_ONLINE, None)
-            .expect("Generates key.");
+	#[test]
+	fn public_key_fn_does_not_return_key_outside_current_authority_set() {
+		// Create keystore and generate key.
+		let key_store = KeyStore::new();
+		key_store
+			.write()
+			.sr25519_generate_new(key_types::IM_ONLINE, None)
+			.expect("Generates key.");
 
-        // Build genesis.
-        let mut t = system::GenesisConfig::default()
-            .build_storage::<Test>()
-            .unwrap();
+		// Build genesis.
+		let mut t = system::GenesisConfig::default()
+			.build_storage::<Test>()
+			.unwrap();
 
-        // Generate random authority set.
-        let keys = vec![(); 5]
-            .iter()
-            .map(|_x| sr25519::Pair::generate_with_phrase(None).0.public())
-            .map(im_online::AuthorityId::from)
-            .collect();
+		// Generate random authority set.
+		let keys = vec![(); 5]
+			.iter()
+			.map(|_x| sr25519::Pair::generate_with_phrase(None).0.public())
+			.map(im_online::AuthorityId::from)
+			.collect();
 
-        GenesisConfig { keys: keys }
-            .assimilate_storage::<Test>(&mut t)
-            .unwrap();
+		GenesisConfig { keys: keys }
+			.assimilate_storage::<Test>(&mut t)
+			.unwrap();
 
-        // Create externalities.
-        let mut externalities = TestExternalities::new(t);
-        externalities.set_keystore(key_store);
+		// Create externalities.
+		let mut externalities = TestExternalities::new(t);
+		externalities.set_keystore(key_store);
 
-        with_externalities(&mut externalities, || {
-            assert_eq!(None, AuthorityDiscovery::public_key());
-        });
-    }
+		with_externalities(&mut externalities, || {
+			assert_eq!(None, AuthorityDiscovery::public_key());
+		});
+	}
 }
