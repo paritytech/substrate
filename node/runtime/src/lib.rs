fd315bc1dac850322a40751ce24c2e9178967bc5
Gavin Wood <gavin@parity.io>
2019-08-24 20:12:29 +0200

    Allow root to force transfers (#3475)
    * Allow root to force transfers
    
    * Bump version
    
    * Avoid changing pre-existing encodings

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -79,7 +79,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 150,
+	spec_version: 151,
 	impl_version: 151,
 	apis: RUNTIME_API_VERSIONS,
 };
