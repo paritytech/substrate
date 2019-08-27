223770a8abc645ff87b50e4226d236aeb48067d5
Bastian Köcher <bkchr@users.noreply.github.com>
2019-08-27 11:57:35 +0200

    Make sure that `on_before_session_ending` is called (#3487)
    * Make sure that `on_before_session_ending` is called
    
    * Move the call above the validtor set being set
    
    * Bump spec_version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -79,8 +79,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 151,
-	impl_version: 153,
+	spec_version: 152,
+	impl_version: 152,
 	apis: RUNTIME_API_VERSIONS,
 };
 
