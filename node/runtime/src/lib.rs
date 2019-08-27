3211a8d4a7a9cc4e3185bc5dd5bfc1a39a65d2ed
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2019-08-27 14:07:43 +0200

    Fixed uncle pruning (#3491)
    * Fixed uncle pruning
    
    * Version bump

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -79,8 +79,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 152,
-	impl_version: 152,
+	spec_version: 153,
+	impl_version: 153,
 	apis: RUNTIME_API_VERSIONS,
 };
 
