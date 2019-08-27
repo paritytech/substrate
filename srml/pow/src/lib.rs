c68cee49c20ffcd094d4813f34c58645daf46ba5
Wei Tang <hi@that.world>
2019-08-27 21:51:47 +0200

    srml-pow: add support for authorship

--- srml/pow/src/lib.rs
+++ srml/pow/src/lib.rs
@@ -3,3 +3,24 @@
 
 pub mod diffs;
 pub mod algos;
+
+use core::marker::PhantomData;
+use codec::Codec;
+use pow_primitives::POW_ENGINE_ID;
+use srml_support::ConsensusEngineId;
+
+pub struct FindAuthor<V: Codec>(PhantomData<V>);
+
+impl<V: Codec> srml_support::traits::FindAuthor<V> for FindAuthor<V> {
+	fn find_author<'a, I>(digests: I) -> Option<V> where
+		I: 'a + IntoIterator<Item=(ConsensusEngineId, &'a [u8])>
+	{
+		for (engine_id, mut value) in digests {
+			if engine_id == POW_ENGINE_ID {
+				return V::decode(&mut value).ok()
+			}
+		}
+
+		None
+	}
+}
