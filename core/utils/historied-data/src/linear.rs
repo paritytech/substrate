8dbd48d859f1463f13744af8a04f68793ff8f1a5
cheme <emericchevalier.pro@gmail.com>
2019-08-28 11:38:00 +0200

    Remove serialized variant.

--- core/utils/historied-data/src/linear.rs
+++ core/utils/historied-data/src/linear.rs
@@ -25,7 +25,6 @@
 use crate::State as TransactionState;
 use rstd::vec::Vec;
 use rstd::vec;
-use rstd::borrow::Cow;
 
 /// Array like buffer for in memory storage.
 /// By in memory we expect that this will
@@ -234,218 +233,12 @@ impl States {
 
 }
 
-
-#[derive(Debug, Clone)]
-#[cfg_attr(any(test, feature = "test"), derive(PartialEq))]
-/// Arraylike buffer with in place byte data.
-/// Can be written as is in underlying
-/// storage.
-/// Could be use for direct access memory to.
-pub struct Serialized<'a>(Cow<'a, [u8]>);
-
-// Basis implementation to be on par with implementation using
-// vec like container. Those method could be move to a trait
-// implementation.
-// Those function requires checked index.
-impl<'a> Serialized<'a> {
-
-	fn len(&self) -> usize {
-		let len = self.0.len();
-		self.read_le_usize(len - 4)
-	}
-
-	fn clear(&mut self) {
-		self.write_le_usize(0, 0);
-		self.0.to_mut().truncate(4);
-	}
-
-	fn truncate(&mut self, index: usize) {
-		if index == 0 {
-			self.clear();
-			return;
-		}
-		let len = self.len();
-		if index >= len {
-			return;
-		}
-		let start_ix = self.index_start();
-		let new_start = self.index_element(index);
-		let len_ix = index * 4;
-		self.slice_copy(start_ix, new_start, len_ix);
-		self.write_le_usize(new_start + len_ix - 4, index);
-		self.0.to_mut().truncate(new_start + len_ix);
-	}
-
-	fn pop(&mut self) -> Option<(Vec<u8>, usize)> {
-		let len = self.len();
-		if len == 0 {
-			return None;
-		}
-		let start_ix = self.index_element(len - 1);
-		let end_ix = self.index_start();
-		let state = self.read_le_usize(start_ix);
-		let value = self.0[start_ix + 4..end_ix].to_vec();
-		if len - 1 == 0 {
-			self.clear();
-			return Some((value, state))	
-		} else {
-			self.write_le_usize(self.0.len() - 8, len - 1);
-		};
-		let ix_size = (len * 4) - 4;
-		self.slice_copy(end_ix, start_ix, ix_size);
-		self.0.to_mut().truncate(start_ix + ix_size);
-		Some((value, state))
-	}
-
-	fn push(&mut self, val: (&[u8], usize)) {
-		let len = self.len();
-		let start_ix = self.index_start();
-		let end_ix = self.0.len();
-		// A sized buffer and multiple index to avoid to big copy
-		// should be use here.
-		let mut new_ix = self.0[start_ix..end_ix].to_vec();
-		// truncate here can be bad
-		self.0.to_mut().truncate(start_ix + 4);
-		self.write_le_usize(start_ix, val.1);
-		self.0.to_mut().extend_from_slice(val.0);
-		self.0.to_mut().append(&mut new_ix);
-		if len > 0 {
-			self.write_le_usize(self.0.len() - 4, start_ix);
-			self.append_le_usize(len + 1);
-		} else {
-			self.write_le_usize(self.0.len() - 4, 1);
-		}
-	}
-
-	fn remove(&mut self, index: usize) {
-		let len = self.len();
-		if len == 1 && index == 0 {
-			self.clear();
-			return;
-		}
-		// eager removal is costy, running some gc impl
-		// can be interesting (would be malleable serializing).
-		let elt_start = self.index_element(index);
-		let start_ix = self.index_start();
-		let elt_end = if index == len - 1 {
-			start_ix
-		} else {
-			self.index_element(index + 1) 
-		};
-		let delete_size = elt_end - elt_start;
-		for _ in elt_start..elt_end {
-			let _ = self.0.to_mut().remove(elt_start);
-		}
-		let start_ix = start_ix - delete_size;
-		for i in 1..len - index - 1 {
-			let old_value = self.read_le_usize(start_ix + i * 4);
-			self.write_le_usize(start_ix + (i - 1) * 4, old_value - delete_size);
-		}
-		let len = len - 1;
-		let end_index = start_ix + len * 4;
-		self.write_le_usize(end_index - 4, len);
-		self.0.to_mut().truncate(end_index);
-
-	}
-
-	fn get_state(&self, index: usize) -> (&[u8], usize) {
-		let start_ix = self.index_element(index);
-		let len = self.len();
-		let end_ix = if index == len - 1 {
-			self.index_start()
-		} else {
-			self.index_element(index + 1)
-		};
-		let state = self.read_le_usize(start_ix);
-		(&self.0[start_ix + 4..end_ix], state)
-	}
-
-}
-
-impl<'a> Default for Serialized<'a> {
-	fn default() -> Self {
-		Serialized(Cow::Borrowed(&[0u8; 4][..]))
-	}
-}
-
-// Utility function for basis implementation.
-impl<'a> Serialized<'a> {
-	
-	// Index at end, also contains the encoded size
-	fn index_start(&self) -> usize {
-		let nb_ix = self.len();
-		if nb_ix ==0 { return 0; }
-		let end = self.0.len();
-		end - (nb_ix * 4)
-	}
-
-	fn index_element(&self, position: usize) -> usize {
-		if position == 0 {
-			return 0;
-		}
-		let i = self.index_start() + (position - 1) * 4;
-		self.read_le_usize(i)
-	}
-
-	// move part of array that can overlap
-	// This is a memory inefficient implementation.
-	fn slice_copy(&mut self, start_from: usize, start_to: usize, size: usize) {
-		let buffer = self.0[start_from..start_from + size].to_vec();
-		self.0.to_mut()[start_to..start_to + size].copy_from_slice(&buffer[..]);
-	}
-
-	// Usize encoded as le u32.
-	fn read_le_usize(&self, pos: usize) -> usize {
-		let mut buffer = [0u8; 4];
-		buffer.copy_from_slice(&self.0[pos..pos + 4]);
-		u32::from_le_bytes(buffer) as usize
-	}
-
-	// Usize encoded as le u32.
-	fn write_le_usize(&mut self, pos: usize, value: usize) {
-		let buffer = (value as u32).to_le_bytes();
-		self.0.to_mut()[pos..pos + 4].copy_from_slice(&buffer[..]);
-	}
-
-	// Usize encoded as le u32.
-	fn append_le_usize(&mut self, value: usize) {
-		let buffer = (value as u32).to_le_bytes();
-		self.0.to_mut().extend_from_slice(&buffer[..]);
-	}
-
-}
-
-impl<'a> Serialized<'a> {
-
-	/// Set a value, it uses a state history as parameter.
-	/// This method uses `get_mut` and do remove pending
-	/// dropped value.
-	pub fn set(&mut self, history: &[TransactionState], val: &[u8]) {
-		if let Some((_v, index)) = self.get_mut(history) {
-			if index == history.len() - 1 {
-				self.pop();
-				self.push((val, index));
-				return;
-			}
-		}
-		self.push((val, history.len() - 1));
-	}
-
-	fn mut_ref(&mut self, _index: usize) -> () {
-		()
-	}
-
-}
-
-
-
-// share implementation, trait would be better.
-macro_rules! history_impl(( $read: ty, $owned: ty, $mut: ty ) => {
+impl<V> History<V> {
 
 	/// Access to latest pending value (non dropped state in history).
 	/// When possible please prefer `get_mut` as it can free
 	/// some memory.
-	pub fn get(&self, history: &[TransactionState]) -> Option<$read> {
+	pub fn get(&self, history: &[TransactionState]) -> Option<&V> {
 		// index is never 0, 
 		let mut index = self.len();
 		if index == 0 {
@@ -468,7 +261,7 @@ macro_rules! history_impl(( $read: ty, $owned: ty, $mut: ty ) => {
 	}
 
 	#[cfg(any(test, feature = "test"))]
-	pub fn get_prospective(&self, history: &[TransactionState]) -> Option<$read> {
+	pub fn get_prospective(&self, history: &[TransactionState]) -> Option<&V> {
 		// index is never 0, 
 		let mut index = self.len();
 		if index == 0 {
@@ -491,7 +284,7 @@ macro_rules! history_impl(( $read: ty, $owned: ty, $mut: ty ) => {
 	}
 
 	#[cfg(any(test, feature = "test"))]
-	pub fn get_committed(&self, history: &[TransactionState]) -> Option<$read> {
+	pub fn get_committed(&self, history: &[TransactionState]) -> Option<&V> {
 		// index is never 0, 
 		let mut index = self.len();
 		if index == 0 {
@@ -513,7 +306,7 @@ macro_rules! history_impl(( $read: ty, $owned: ty, $mut: ty ) => {
 		None
 	}
 
-	pub fn into_committed(mut self, history: &[TransactionState]) -> Option<$owned> {
+	pub fn into_committed(mut self, history: &[TransactionState]) -> Option<V> {
 		// index is never 0, 
 		let mut index = self.len();
 		if index == 0 {
@@ -543,7 +336,7 @@ macro_rules! history_impl(( $read: ty, $owned: ty, $mut: ty ) => {
 	/// Access to latest pending value (non dropped state in history).
 	///
 	/// This method remove latest dropped value up to the latest valid value.
-	pub fn get_mut(&mut self, history: &[TransactionState]) -> Option<($mut, usize)> {
+	pub fn get_mut(&mut self, history: &[TransactionState]) -> Option<(&mut V, usize)> {
 
 		let mut index = self.len();
 		if index == 0 {
@@ -582,7 +375,7 @@ macro_rules! history_impl(( $read: ty, $owned: ty, $mut: ty ) => {
 		&mut self,
 		history: &[TransactionState],
 		tx_index: Option<&[usize]>,
-	) -> Option<($mut, usize)> {
+	) -> Option<(&mut V, usize)> {
 		if let Some(tx_index) = tx_index {
 			let mut tx_index = &tx_index[..];
 			let mut index = self.len();
@@ -649,64 +442,6 @@ macro_rules! history_impl(( $read: ty, $owned: ty, $mut: ty ) => {
 			return self.get_mut(history);
 		}
 	}
-
-});
-
-impl<'a> Serialized<'a> {
-  history_impl!(&[u8], Vec<u8>, ());
 }
 
-impl<V> History<V> {
-  history_impl!(&V, V, &mut V);
-}
 
-mod test {
-  use super::*;
-
-  #[test]
-  fn serialized_basis() {
-    // test basis unsafe function similar to a simple vec
-    // without index checking.
-	//fn get_state(&self, index: usize) -> (&V, usize) {
-	//fn len(&self) -> usize {
-	//fn truncate(&mut self, index: usize) {
-	//fn push(&mut self, val: (&[u8], usize)) {
-	//fn pop(&mut self) -> Option<(Vec<u8>, usize)> {
-	//fn remove(&mut self, index: usize) {
-	//fn get_state(&self, index: usize) -> (&[u8], usize) {
-    let v1 = &b"val1"[..];
-    let v2 = &b"value_2"[..];
-    let v3 = &b"a third value 3"[..];
-    let mut ser = Serialized::default();
-    assert_eq!(ser.len(), 0);
-    assert_eq!(ser.pop(), None);
-    ser.push((v1, 1));
-    assert_eq!(ser.get_state(0), (v1, 1));
-    assert_eq!(ser.pop(), Some((v1.to_vec(), 1)));
-	assert_eq!(ser.len(), 0);
-    ser.push((v1, 1));
-    ser.push((v2, 2));
-    ser.push((v3, 3));
- 	assert_eq!(ser.get_state(0), (v1, 1));
-    assert_eq!(ser.get_state(1), (v2, 2));
-    assert_eq!(ser.get_state(2), (v3, 3));
-    assert_eq!(ser.pop(), Some((v3.to_vec(), 3)));
-    assert_eq!(ser.len(), 2);
-    ser.push((v3, 3));
-    assert_eq!(ser.get_state(2), (v3, 3));
-	ser.remove(0);
-    assert_eq!(ser.len(), 2);
-	assert_eq!(ser.get_state(0), (v2, 2));
-    assert_eq!(ser.get_state(1), (v3, 3));
-    ser.push((v1, 1));
-	ser.remove(1);
-    assert_eq!(ser.len(), 2);
-	assert_eq!(ser.get_state(0), (v2, 2));
-    assert_eq!(ser.get_state(1), (v1, 1));
-	ser.push((v1, 1));
-	ser.truncate(1);
-    assert_eq!(ser.len(), 1);
-	assert_eq!(ser.get_state(0), (v2, 2));
-	
-  }
-}
