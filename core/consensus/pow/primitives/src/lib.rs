fb6344e969a7868f70b57767d7f299db0b2f1223
Wei Tang <hi@that.world>
2019-08-29 23:54:59 +0200

    Update core/consensus/pow/primitives/src/lib.rs
    Co-Authored-By: DemiMarie-parity <48690212+DemiMarie-parity@users.noreply.github.com>

--- core/consensus/pow/primitives/src/lib.rs
+++ core/consensus/pow/primitives/src/lib.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Primitives for Substrate PoW consensus.
+//! Primitives for Substrate Proof-of-Work (PoW) consensus.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
