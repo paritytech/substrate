f470a21a875beb529104c51cdf935de0ccba15cc
thiolliere <gui.thiolliere@gmail.com>
2019-08-29 16:21:13 +0200

    fix doc

--- srml/support/src/storage/generator/mod.rs
+++ srml/support/src/storage/generator/mod.rs
@@ -26,7 +26,7 @@ mod map;
 mod double_map;
 mod value;
 
-pub use linked_map::StorageLinkedMap;
+pub use linked_map::{StorageLinkedMap, Enumerator};
 pub use map::StorageMap;
 pub use double_map::StorageDoubleMap;
 pub use value::StorageValue;
