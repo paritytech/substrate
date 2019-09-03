8a7fbc3fd87b5d8164e9f68f312901913bc750f3
Bastian Köcher <bkchr@users.noreply.github.com>
2019-09-03 15:13:32 +0200

    Fix `build-only-wasm` script (#3536)

--- scripts/build-only-wasm.sh
+++ scripts/build-only-wasm.sh
@@ -23,7 +23,7 @@ if [ -d $WASM_BUILDER_RUNNER ]; then
   export DEBUG=false
   export OUT_DIR="$PROJECT_ROOT/target/release/build"
   cargo run --release --manifest-path="$WASM_BUILDER_RUNNER/Cargo.toml" \
-    | grep -vE "cargo:rerun-if-|Executing build command"                  \
+    | grep -vE "cargo:rerun-if-|Executing build command"
 else
   cargo build --release -p $1
 fi
