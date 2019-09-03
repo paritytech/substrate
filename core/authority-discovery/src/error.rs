1a376bf6058308b7857e2d7dbc829f8dae2a1137
Max Inden <mail@max-inden.de>
2019-09-03 16:26:48 +0200

    core/authority-discovery: Add doc comments to Error enum

--- core/authority-discovery/src/error.rs
+++ core/authority-discovery/src/error.rs
@@ -19,18 +19,27 @@
 /// AuthorityDiscovery Result.
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// Error type for the authority discovery module.
 #[derive(Debug, derive_more::Display, derive_more::From)]
 pub enum Error {
+	/// Failed to verify a dht payload with the given signature.
 	VerifyingDhtPayload,
+	/// Failed to hash the authority id to be used as a dht key.
 	HashingAuthorityId(libp2p::core::multiaddr::multihash::EncodeError),
+	/// Failed calling into the Substrate runtime.
 	CallingRuntime(client::error::Error),
+	/// Failed signing the dht payload via the Substrate runtime.
 	SigningDhtPayload,
 	/// From the Dht we only get the hashed authority id. In order to retrieve the actual authority id and to ensure it
 	/// is actually an authority, we match the hash against the hash of the authority id of all other authorities. This
 	/// error is the result of the above failing.
 	MatchingHashedAuthorityIdWithAuthorityId,
+	/// Failed to set the authority discovery peerset priority group in the peerset module.
 	SettingPeersetPriorityGroup(String),
+	/// Failed to encode a dht payload.
 	Encoding(prost::EncodeError),
+	/// Failed to decode a dht payload.
 	Decoding(prost::DecodeError),
+	/// Failed to parse a libp2p multi address.
 	ParsingMultiaddress(libp2p::core::multiaddr::Error),
 }
