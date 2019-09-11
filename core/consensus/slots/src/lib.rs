aa429f5eaf2f13c6422d9ed330d073978767803c
Robert Habermeier <rphmeier@gmail.com>
2019-09-11 15:25:36 +0200

    slots passes header to epoch_data

--- core/consensus/slots/src/lib.rs
+++ core/consensus/slots/src/lib.rs
@@ -78,7 +78,7 @@ pub trait SimpleSlotWorker<B: BlockT> {
 	fn block_import(&self) -> Arc<Mutex<Self::BlockImport>>;
 
 	/// Returns the epoch data necessary for authoring.
-	fn epoch_data(&self, block: &B::Hash) -> Result<Self::EpochData, consensus_common::Error>;
+	fn epoch_data(&self, header: &B::Header) -> Result<Self::EpochData, consensus_common::Error>;
 
 	/// Returns the number of authorities given the epoch data.
 	fn authorities_len(&self, epoch_data: &Self::EpochData) -> usize;
@@ -120,7 +120,7 @@ pub trait SimpleSlotWorker<B: BlockT> {
 		let (timestamp, slot_number, slot_duration) =
 			(slot_info.timestamp, slot_info.number, slot_info.duration);
 
-		let epoch_data = match self.epoch_data(&chain_head.hash()) {
+		let epoch_data = match self.epoch_data(&chain_head) {
 			Ok(epoch_data) => epoch_data,
 			Err(err) => {
 				warn!("Unable to fetch epoch data at block {:?}: {:?}", chain_head.hash(), err);
