707d2e4c036039c39ff0dd2ffe3c4ca26c1763b6
Tomasz Drwięga <tomasz@parity.io>
2019-09-11 14:51:48 +0200

    Remove dumbness.

--- srml/im-online/src/tests.rs
+++ srml/im-online/src/tests.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Tests for the offences module.
+//! Tests for the im-online module.
 
 #![cfg(test)]
 
