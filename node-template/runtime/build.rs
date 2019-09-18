c1eeb04af13ead7c92842ff39dc5aab88e991eed
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-09-18 16:42:33 +0200

    Address review again

--- node-template/runtime/build.rs
+++ node-template/runtime/build.rs
@@ -19,11 +19,7 @@ use wasm_builder_runner::{build_current_project_with_rustflags, WasmBuilderSourc
 fn main() {
 	build_current_project_with_rustflags(
 		"wasm_binary.rs",
-		// Note: replace with `WasmBuilderSource::Crates("1.0.6")` if you copy-paste this template.
-		WasmBuilderSource::CratesOrPath {
-			path: "../../core/utils/wasm-builder",
-			version: "1.0.6",
-		},
+		WasmBuilderSource::Crates("1.0.7"),
 		// This instructs LLD to export __heap_base as a global variable, which is used by the
 		// external memory allocator.
 		"-Clink-arg=--export=__heap_base",
