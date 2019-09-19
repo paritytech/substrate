09eb2ed766ad3cab6c2c4071ec8fb937c51669c6
Expenses <expenses@airmail.cc>
2019-09-19 19:36:51 +1200

    increased spec_version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -82,8 +82,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 158,
-	impl_version: 160,
+	spec_version: 159,
+	impl_version: 159,
 	apis: RUNTIME_API_VERSIONS,
 };
 
