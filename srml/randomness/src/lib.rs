907a29acb28758a86b65098341eac4333df51566
Ashley <ashley.ruglys@gmail.com>
2019-09-28 11:41:49 +1200

    Add tests

--- srml/randomness/src/lib.rs
+++ srml/randomness/src/lib.rs
@@ -49,18 +49,18 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use rstd::prelude::*;
-use sr_primitives::traits::{Hash, UniqueSaturatedInto};
+use rstd::{prelude::*, convert::TryInto};
+use sr_primitives::traits::Hash;
 use support::{decl_module, decl_storage};
 use safe_mix::TripletMix;
 use codec::Encode;
 use system::Trait;
 
 
-// TODO: this is less than ideal and needs testing
 fn block_number_to_index<T: Trait>(block_number: T::BlockNumber) -> usize {
-	let index = block_number % 81.into();
-	index.unique_saturated_into() as usize
+	// '- 2' because on_initialize is called on the first block after genesis - block 2
+	let index = (block_number - 2.into()) % 81.into();
+	index.try_into().ok().expect("something % 81 should always be less than usize")
 }
 
 decl_module! {
@@ -152,5 +152,127 @@ impl<T: Trait> Module<T> {
 		} else {
 			T::Hash::default()
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use primitives::{H256, Blake2Hasher};
+	use sr_primitives::{Perbill, traits::{BlakeTwo256, OnInitialize, Header as _, IdentityLookup}, testing::Header};
+	use support::{impl_outer_origin, parameter_types};
+	use runtime_io::with_externalities;
+
+	#[derive(Clone, PartialEq, Eq)]
+	pub struct Test;
+
+	impl_outer_origin! {
+		pub enum Origin for Test {}
+	}
+
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+		pub const MaximumBlockWeight: u32 = 1024;
+		pub const MaximumBlockLength: u32 = 2 * 1024;
+		pub const AvailableBlockRatio: Perbill = Perbill::one();
+	}
+
+	impl system::Trait for Test {
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = u64;
+		type Call = ();
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = u64;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type WeightMultiplierUpdate = ();
+		type Event = ();
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type MaximumBlockLength = MaximumBlockLength;
+		type Version = ();
+	}
+
+	type System = system::Module<Test>;
+	type Randomness = Module<Test>;
+
+	fn new_test_ext() -> runtime_io::TestExternalities<Blake2Hasher> {
+		let t = system::GenesisConfig::default().build_storage::<Test>().unwrap();
+		t.into()
+	}
+
+	#[test]
+	fn test_block_number_to_index() {
+		for i in 2 .. 1000 {
+			assert_eq!((i - 2) as usize % 81, block_number_to_index::<Test>(i));
+		}
+	}
+
+	#[test]
+	fn test_random_material_parital() {
+		with_externalities(&mut new_test_ext(), || {
+			let genesis_hash = System::parent_hash();
+			let mut parent_hash = genesis_hash;
+
+			for i in 2..40 {
+				System::initialize(&i, &parent_hash, &Default::default(), &Default::default());
+				Randomness::on_initialize(i);
+
+				let header = System::finalize();
+				parent_hash = header.hash();
+			}
+
+			let random_material = Randomness::random_material();
+
+			assert_eq!(random_material.len(), 38);
+			assert_eq!(random_material[0], genesis_hash);
+		});
+	}
+
+	#[test]
+	fn test_random_material_filled() {
+		with_externalities(&mut new_test_ext(), || {
+			let genesis_hash = System::parent_hash();
+			let mut parent_hash = genesis_hash;
+
+			for i in 2..83 {
+				System::initialize(&i, &parent_hash, &Default::default(), &Default::default());
+				Randomness::on_initialize(i);
+
+				let header = System::finalize();
+				parent_hash = header.hash();
+			}
+
+			let random_material = Randomness::random_material();
+
+			assert_eq!(random_material.len(), 81);
+			assert_ne!(random_material[0], random_material[1]);
+			assert_eq!(random_material[0], genesis_hash);
+		});
+	}
+
+	#[test]
+	fn test_random_material_filled_twice() {
+		with_externalities(&mut new_test_ext(), || {
+			let genesis_hash = System::parent_hash();
+			let mut parent_hash = genesis_hash;
+
+			for i in 2..164 {
+				System::initialize(&i, &parent_hash, &Default::default(), &Default::default());
+				Randomness::on_initialize(i);
+
+				let header = System::finalize();
+				parent_hash = header.hash();
+			}
+
+			let random_material = Randomness::random_material();
+
+			assert_eq!(random_material.len(), 81);
+			assert_ne!(random_material[0], random_material[1]);
+			assert_ne!(random_material[0], genesis_hash);
+		});
 	}
 }
