f8559f20b2f5ef3c820deefdd78db45770083b3b
Bastian Köcher <bkchr@users.noreply.github.com>
2019-09-27 12:08:11 +0200

    Support trailing commas in `decl_module` (#3712)

--- srml/support/src/storage/storage_items.rs
+++ srml/support/src/storage/storage_items.rs
@@ -256,7 +256,6 @@ mod tests {
 	use crate::metadata::*;
 	use crate::metadata::StorageHasher;
 	use crate::rstd::marker::PhantomData;
-	use crate::storage::{StorageValue, StorageMap};
 
 	storage_items! {
 		Value: b"a" => u32;
