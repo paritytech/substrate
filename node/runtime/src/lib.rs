048952a2a214dd3ba4725353ba1c9d8e90fe87b0
André Silva <andre.beat@gmail.com>
2019-09-30 17:03:40 +0100

    srml: im-online: fix received heartbeats pruning (#3724)
    * srml: im-online: fix pruning of received heartbeats
    
    * srml: im-online: add test for received heartbeats pruning
    
    * srml: im-online: remove unused variables from test
    
    * node: bump spec_version

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -84,8 +84,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 165,
-	impl_version: 167,
+	spec_version: 166,
+	impl_version: 166,
 	apis: RUNTIME_API_VERSIONS,
 };
 
