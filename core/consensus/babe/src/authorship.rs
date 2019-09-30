237fc0a641730a6607db0e83a2bad9a2d41c9f25
Demi M. Obenour <demi@parity.io>
2019-09-30 12:13:34 -0400

    improve docs

--- core/consensus/babe/src/authorship.rs
+++ core/consensus/babe/src/authorship.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-//! BABE authorship code
+//! BABE authority selection and slot claiming.
 
 use merlin::Transcript;
 use babe_primitives::{AuthorityId, BabeAuthorityWeight, BABE_ENGINE_ID, BABE_VRF_PREFIX};
