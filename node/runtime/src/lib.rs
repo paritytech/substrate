461c9b05b9ccdd6e489504dd4bf2947176c1ad41
Bastian Köcher <bkchr@users.noreply.github.com>
2019-10-05 13:06:38 +0200

    srml-utility: Store errors as `DispatchError` to make the decodable (#3766)

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -84,8 +84,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 171,
-	impl_version: 171,
+	spec_version: 172,
+	impl_version: 172,
 	apis: RUNTIME_API_VERSIONS,
 };
 
@@ -497,7 +497,7 @@ construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic
 	{
 		System: system::{Module, Call, Storage, Config, Event},
-		Utility: utility::{Module, Call, Event<T>},
+		Utility: utility::{Module, Call, Event},
 		Babe: babe::{Module, Call, Storage, Config, Inherent(Timestamp)},
 		Timestamp: timestamp::{Module, Call, Storage, Inherent},
 		Authorship: authorship::{Module, Call, Storage, Inherent},
