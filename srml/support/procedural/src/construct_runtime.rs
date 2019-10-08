8f023cb5179666120f25a553cfbcef71d16a6b89
AlexeyKarasev <karasev.alexey@gmail.com>
2019-10-08 09:23:42 +0300

    finished parsing

--- srml/support/procedural/src/construct_runtime.rs
+++ srml/support/procedural/src/construct_runtime.rs
@@ -21,7 +21,7 @@ struct RuntimeDefinition {
 	pub enum_token: Token![enum],
 	pub name: Ident,
 	pub where_section: WhereSection,
-	pub modules: ext::Braces<ext::Punctuated<DeclModulesLine, Token![;]>>,
+	pub modules: ext::Braces<ext::Punctuated<DeclModulesLine, Token![,]>>,
 }
 
 #[derive(Parse, ToTokens, Debug)]
@@ -43,6 +43,49 @@ struct WhereSection {
 #[derive(Parse, ToTokens, Debug)]
 struct DeclModulesLine {
 	pub name: Ident,
+	pub name_colon: Token![:],
+	pub module: Ident,
+	pub module_instance: ext::Opt<ModuleInstance>,
+	pub module_details: ext::Opt<ModuleDetails>,
+}
+
+#[derive(Parse, ToTokens, Debug)]
+struct ModuleInstance {
+	pub colons: Token![::],
+	pub lt: Token![<],
+	pub name: Ident,
+	pub gt: Token![>],
+}
+
+#[derive(Parse, ToTokens, Debug)]
+struct ModuleDetails {
+	pub colons: Token![::],
+	pub entries: ext::Braces<ext::Punctuated<ModuleEntryWrapper, Token![,]>>
+}
+
+#[derive(ToTokens, Debug)]
+struct ModuleEntryWrapper {
+	pub inner: ModuleEntry,
+}
+
+impl Parse for ModuleEntryWrapper {
+	fn parse(input: ParseStream) -> Result<Self> {
+		ModuleEntry::parse(input).map(|inner| ModuleEntryWrapper { inner } ).map_err(|_| input.error("Expected `default` or module export name (e.g. Call, Event, etc.)"))
+	}
+}
+
+#[derive(Parse, ToTokens, Debug)]
+enum ModuleEntry {
+	Default(Token![default]),
+	Part(ModulePart),
+}
+
+#[derive(Parse, ToTokens, Debug)]
+struct ModulePart {
+	pub name: Ident,
+	// This deviates from macro $( <$modules_generic:ident> )*
+	pub generics: ext::Opt<syn::Generics>,
+	pub args: ext::Opt<ext::Parens<ext::Punctuated<Ident, Token![,]>>>,
 }
 
 pub fn construct_runtime(input: TokenStream) -> TokenStream {
