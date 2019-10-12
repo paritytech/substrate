458edbd23d4e19a507c794300f7f98781f50cb96
Ashley <ashley.ruglys@gmail.com>
2019-10-13 01:12:09 +1300

    Clean up biguint fuzzer

--- core/sr-arithmetic/fuzzer/src/biguint.rs
+++ core/sr-arithmetic/fuzzer/src/biguint.rs
@@ -10,22 +10,17 @@ type S = u128;
 
 fn main() {
 	loop {
-		fuzz!(|data: (usize, usize, Vec<Single>, Vec<Single>)| {
-			let data = Data {
-				random_limb_len_1: data.0,
-				random_limb_len_2: data.1,
-				digits_1: data.2,
-				digits_2: data.3,
-			};
+		fuzz!(|data: (Vec<Single>, Vec<Single>)| {
+			let (digits_u, digits_v) = data;
 
-			run_with_data_set(4, 4, false, &data, |u, v| {
+			run_with_data_set(4, &digits_u, &digits_v, |u, v| {
 				let ue = S::try_from(u.clone()).unwrap();
 				let ve = S::try_from(v.clone()).unwrap();
 				assert_eq!(u.cmp(&v), ue.cmp(&ve));
 				assert_eq!(u.eq(&v), ue.eq(&ve));
 			});
 
-			run_with_data_set(3, 3, false, &data, |u, v| {
+			run_with_data_set(3, &digits_u, &digits_v, |u, v| {
 				let expected = S::try_from(u.clone()).unwrap() + S::try_from(v.clone()).unwrap();
 				let t = u.clone().add(&v);
 				assert_eq!(
@@ -34,7 +29,7 @@ fn main() {
 				);
 			});
 
-			run_with_data_set(4, 4, false, &data, |u, v| {
+			run_with_data_set(4, &digits_u, &digits_v, |u, v| {
 				let expected = S::try_from(u.clone())
 					.unwrap()
 					.checked_sub(S::try_from(v.clone()).unwrap());
@@ -51,7 +46,7 @@ fn main() {
 				}
 			});
 
-			run_with_data_set(2, 2, false, &data, |u, v| {
+			run_with_data_set(2, &digits_u, &digits_v, |u, v| {
 				let expected = S::try_from(u.clone()).unwrap() * S::try_from(v.clone()).unwrap();
 				let t = u.clone().mul(&v);
 				assert_eq!(
@@ -60,7 +55,7 @@ fn main() {
 				);
 			});
 
-			run_with_data_set(4, 4, false, &data, |u, v| {
+			run_with_data_set(4, &digits_u, &digits_v, |u, v| {
 				let ue = S::try_from(u.clone()).unwrap();
 				let ve = S::try_from(v.clone()).unwrap();
 				if ve == 0 {
@@ -90,47 +85,22 @@ fn main() {
 	}
 }
 
-struct Data {
-	random_limb_len_1: usize,
-	random_limb_len_2: usize,
-	digits_1: Vec<Single>,
-	digits_2: Vec<Single>,
-}
-
 fn run_with_data_set<F>(
-	limbs_ub_1: usize,
-	limbs_ub_2: usize,
-	exact: bool,
-	data: &Data,
+	max_limbs: usize,
+	digits_u: &[Single], digits_v: &[Single],
 	assertion: F,
 ) -> Option<()>
 	where
 		F: Fn(BigUint, BigUint) -> (),
 {
-	let digits_len_1 = if exact {
-		limbs_ub_1
-	} else {
-		value_between(data.random_limb_len_1, 1, limbs_ub_1)?
-	};
+	// Ensure that `1 <= num_digits < max_limbs`.
+	value_between(digits_u.len(), 1, max_limbs)?;
+	value_between(digits_v.len(), 1, max_limbs)?;
 
-	let digits_len_2 = if exact {
-		limbs_ub_2
-	} else {
-		value_between(data.random_limb_len_2, 1, limbs_ub_2)?
-	};
-
-	let u = big_uint_of_size(digits_len_1, &data.digits_1)?;
-	let v = big_uint_of_size(digits_len_2, &data.digits_2)?;
+	let u = BigUint::from_limbs(digits_u);
+	let v = BigUint::from_limbs(digits_v);
 	// this is easier than using lldb
 	println!("u: {:?}, v: {:?}", u, v);
 	assertion(u, v);
 	Some(())
-}
-
-fn big_uint_of_size(size: usize, digits: &[Single]) -> Option<BigUint> {
-	if digits.len() != size {
-		None
-	} else {
-		Some(BigUint::from_limbs(digits))
-	}
 }
