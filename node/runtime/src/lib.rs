6b22ce1e89db473e0eb7aeaacdf01fbd9b957362
circlespainter <6113296+circlespainter@users.noreply.github.com>
2019-10-13 13:24:02 +0300

    Bump `impl_version`

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -85,7 +85,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
 	spec_version: 175,
-	impl_version: 175,
+	impl_version: 176,
 	apis: RUNTIME_API_VERSIONS,
 };
 
