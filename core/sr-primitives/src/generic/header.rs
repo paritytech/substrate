1cadf86c5706e5a8deefb93c81591ecebe595be6
Ashley <ashley.ruglys@gmail.com>
2019-10-16 09:35:07 +1300

    more code review

--- core/sr-primitives/src/generic/header.rs
+++ core/sr-primitives/src/generic/header.rs
@@ -21,9 +21,8 @@ use serde::{Deserialize, Serialize};
 #[cfg(feature = "std")]
 use log::debug;
 use crate::codec::{Decode, Encode, Codec, Input, Output, HasCompact, EncodeAsRef, Error};
-use arithmetic::traits::SimpleArithmetic;
 use crate::traits::{
-	self, Member, SimpleBitOps, MaybeDisplay, Hash as HashT, MaybeSerializeDebug,
+	self, Member, SimpleArithmetic, SimpleBitOps, MaybeDisplay, Hash as HashT, MaybeSerializeDebug,
 	MaybeSerializeDebugButNotDeserialize
 };
 use crate::generic::Digest;
