41cfa3a01e9a628a43293b37682dfa1c03fe8f15
Ashley <ashley.ruglys@gmail.com>
2019-10-15 19:02:46 +0100

    Update core/sr-arithmetic/src/traits.rs
    Co-Authored-By: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- core/sr-arithmetic/src/traits.rs
+++ core/sr-arithmetic/src/traits.rs
@@ -18,7 +18,6 @@
 
 use rstd::prelude::*;
 use rstd::{self, convert::{TryFrom, TryInto}};
-//use crate::weights::DispatchInfo;
 use codec::HasCompact;
 pub use integer_sqrt::IntegerSquareRoot;
 pub use num_traits::{
