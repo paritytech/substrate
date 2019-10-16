074e3a9b992f0c4200ca81e050d12157f2574ea1
thiolliere <gui.thiolliere@gmail.com>
2019-10-16 15:47:46 +0200

    Refactor decl storage (#3765)
    * split implementation in multiple files:
      * transformation -> genesis_config/  getters.rs  instance_trait.rs  metadata.rs  mod.rs  store_trait.rs
      * mod.rs -> parser.rs
      * impl.rs -> storage_struct.rs
    * parser is isolated into parse module, it could be improved as well but this can be done in another PR
    * modules contains a defintion of decl_storage input which must be ok to work with.
    * implementation change:
      * T: Trait might be more often bound to 'static (anyway we only use static one and it is needed for metadata current implementation).
      * GenesisConfig no longer requires its fields to be Clone (possible since to EncodeLike feature)
      * builder for map storages must return precise type Vec<(key, value)>

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -84,8 +84,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 176,
-	impl_version: 176,
+	spec_version: 177,
+	impl_version: 177,
 	apis: RUNTIME_API_VERSIONS,
 };
 
