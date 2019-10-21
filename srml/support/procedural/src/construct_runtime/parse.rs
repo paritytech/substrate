7b98eab9aeea85c0bf94bf4a8b5565603f80dafb
AlexeyKarasev <karasev.alexey@gmail.com>
2019-10-21 11:21:25 +0300

    cleanup

--- srml/support/procedural/src/construct_runtime/parse.rs
+++ srml/support/procedural/src/construct_runtime/parse.rs
@@ -1,3 +1,19 @@
+// Copyright 2019 Parity Technologies (UK) Ltd.
+// This file is part of Substrate.
+
+// Substrate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Substrate is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
+
 use proc_macro2::Span;
 use srml_support_procedural_tools::{syn_ext as ext, Parse, ToTokens};
 use std::collections::BTreeMap;
