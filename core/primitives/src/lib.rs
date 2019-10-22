de5e13280f79a3fe204a5df40b80d3e5904e5e8f
yjh <yjh465402634@gmail.com>
2019-10-22 17:37:52 +0800

    refactor map macro for more general use (#3850)
    * refactor map macro for more general use
    
    Signed-off-by: yjhmelody <465402634@qq.com>
    
    * use $(,)?
    
    Signed-off-by: yjhmelody <465402634@qq.com>
    
    * Update core/primitives/src/lib.rs
    
    Co-Authored-By: thiolliere <gui.thiolliere@gmail.com>
    
    * Update core/primitives/src/lib.rs
    
    Co-Authored-By: thiolliere <gui.thiolliere@gmail.com>

--- core/primitives/src/lib.rs
+++ core/primitives/src/lib.rs
@@ -26,9 +26,9 @@
 /// Can be used to create a `HashMap`.
 #[macro_export]
 macro_rules! map {
-	($( $name:expr => $value:expr ),*) => (
+	($( $name:expr => $value:expr ),* $(,)? ) => (
 		vec![ $( ( $name, $value ) ),* ].into_iter().collect()
-	)
+	);
 }
 
 use rstd::prelude::*;
