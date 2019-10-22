51f04e8584248c140285c42e33df9e363ea80fa0
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-10-22 12:40:37 +0200

    Apply breaking changes of new libp2p versions

--- core/network/src/transport.rs
+++ core/network/src/transport.rs
@@ -90,7 +90,7 @@ pub fn build_transport(
 	#[cfg(not(target_os = "unknown"))]
 	let transport = transport.and_then(move |stream, endpoint| {
 		let upgrade = core::upgrade::SelectUpgrade::new(noise_config, secio_config);
-		core::upgrade::apply(stream, upgrade, endpoint)
+		core::upgrade::apply(stream, upgrade, endpoint, upgrade::Version::V1)
 			.and_then(|out| match out {
 				// We negotiated noise
 				EitherOutput::First((remote_id, out)) => {
@@ -101,8 +101,8 @@ pub fn build_transport(
 					Ok((EitherOutput::First(out), remote_key.into_peer_id()))
 				}
 				// We negotiated secio
-				EitherOutput::Second(out) =>
-					Ok((EitherOutput::Second(out.stream), out.remote_key.into_peer_id()))
+				EitherOutput::Second((remote_id, out)) =>
+					Ok((EitherOutput::Second(out), remote_id))
 			})
 	});
 
@@ -120,11 +120,11 @@ pub fn build_transport(
 				.map_inbound(move |muxer| (peer_id, muxer))
 				.map_outbound(move |muxer| (peer_id2, muxer));
 
-			core::upgrade::apply(stream, upgrade, endpoint)
+			core::upgrade::apply(stream, upgrade, endpoint, upgrade::Version::V1)
 				.map(|(id, muxer)| (id, core::muxing::StreamMuxerBox::new(muxer)))
 		})
 
-		.with_timeout(Duration::from_secs(20))
+		.timeout(Duration::from_secs(20))
 		.map_err(|err| io::Error::new(io::ErrorKind::Other, err))
 		.boxed();
 
