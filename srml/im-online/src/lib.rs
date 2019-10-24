3631e3fff3daeacbe0c36cc6dba789163d7123ff
Tomasz Drwięga <tomasz@parity.io>
2019-10-24 15:49:31 +0200

    Bring back SubmitSignedTransaction.

--- srml/im-online/src/lib.rs
+++ srml/im-online/src/lib.rs
@@ -96,12 +96,6 @@ pub mod sr25519 {
 	mod app_sr25519 {
 		use app_crypto::{app_crypto, key_types::IM_ONLINE, sr25519};
 		app_crypto!(sr25519, IM_ONLINE);
-
-		impl From<Signature> for sr_primitives::AnySignature {
-			fn from(sig: Signature) -> Self {
-				sr25519::Signature::from(sig).into()
-			}
-		}
 	}
 
 	/// An i'm online keypair using sr25519 as its crypto.
@@ -119,12 +113,6 @@ pub mod ed25519 {
 	mod app_ed25519 {
 		use app_crypto::{app_crypto, key_types::IM_ONLINE, ed25519};
 		app_crypto!(ed25519, IM_ONLINE);
-
-		impl From<Signature> for sr_primitives::AnySignature {
-			fn from(sig: Signature) -> Self {
-				ed25519::Signature::from(sig).into()
-			}
-		}
 	}
 
 	/// An i'm online keypair using ed25519 as its crypto.
