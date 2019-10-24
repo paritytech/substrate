49faba4b4145326058ba742cc884b950f1d382d8
Ashley <ashley.ruglys@gmail.com>
2019-10-24 21:26:36 +1300

    Test on variable limb lengths

--- core/sr-arithmetic/benches/bench.rs
+++ core/sr-arithmetic/benches/bench.rs
@@ -1,4 +1,18 @@
-use criterion::{Criterion, criterion_group, criterion_main};
+// Copyright 2019 Parity Technologies
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use criterion::{Criterion, Throughput, BenchmarkId, criterion_group, criterion_main};
 use sr_arithmetic::biguint::{BigUint, Single};
 
 fn random_big_uint(size: usize) -> BigUint {
@@ -8,69 +22,47 @@ fn random_big_uint(size: usize) -> BigUint {
 	BigUint::from_limbs(&digits)
 }
 
-fn bench_addition_2_digit(c: &mut Criterion) {
-	let a = random_big_uint(2);
-	let b = random_big_uint(2);
-	c.bench_function("addition 2 digit", |bencher| bencher.iter(|| {
-		let _ = a.clone().add(&b);
-	}));
+fn bench_op<F: Fn(&BigUint, &BigUint)>(c: &mut Criterion, name: &str, op: F) {
+	let mut group = c.benchmark_group(name);
+
+	for size in [2, 4, 6, 8, 10].iter() {
+		group.throughput(Throughput::Elements(*size));
+		group.bench_with_input(BenchmarkId::from_parameter(size), size, |bencher, &size| {
+			let a = random_big_uint(size as usize);
+			let b = random_big_uint(size as usize);
+
+			bencher.iter(|| op(&a, &b));
+		});
+	}
 }
 
-fn bench_addition_4_digit(c: &mut Criterion) {
-	let a = random_big_uint(4);
-	let b = random_big_uint(4);
-	c.bench_function("addition 4 digit", |bencher| bencher.iter(|| {
-		let _ = a.clone().add(&b);
-	}));
+fn bench_addition(c: &mut Criterion) {
+	bench_op(c, "addition", |a, b| {
+		a.clone().add(&b);
+	});
 }
 
-fn bench_subtraction_2_digit(c: &mut Criterion) {
-	let a = random_big_uint(2);
-	let b = random_big_uint(2);
-	c.bench_function("subtraction 2 digit", |bencher| bencher.iter(|| {
+fn bench_subtraction(c: &mut Criterion) {
+	bench_op(c, "subtraction", |a, b| {
 		let _ = a.clone().sub(&b);
-	}));
+	});
 }
 
-fn bench_subtraction_4_digit(c: &mut Criterion) {
-	let a = random_big_uint(4);
-	let b = random_big_uint(4);
-	c.bench_function("subtraction 4 digit", |bencher| bencher.iter(|| {
-		let _ = a.clone().sub(&b);
-	}));
+fn bench_multiplication(c: &mut Criterion) {
+	bench_op(c, "multiplication", |a, b| {
+		a.clone().mul(&b);
+	});
 }
 
-fn bench_multiplication_2_digit(c: &mut Criterion) {
-	let a = random_big_uint(2);
-	let b = random_big_uint(2);
-	c.bench_function("multiplication 2 digit", |bencher| bencher.iter(|| {
-		let _ = a.clone().mul(&b);
-	}));
-}
-
-fn bench_multiplication_4_digit(c: &mut Criterion) {
-	let a = random_big_uint(4);
-	let b = random_big_uint(4);
-	c.bench_function("multiplication 4 digit", |bencher| bencher.iter(|| {
-		let _ = a.clone().mul(&b);
-	}));
-}
-
-fn bench_division_4_digit(c: &mut Criterion) {
-	let a = random_big_uint(4);
-	let b = random_big_uint(2);
-	c.bench_function("division 4 digit", |bencher| bencher.iter(|| {
-		let _ = a.clone().div(&b, true);
-	}));
+fn bench_division(c: &mut Criterion) {
+	bench_op(c, "division", |a, b| {
+		a.clone().div(&b, true);
+	});
 }
 
 criterion_group!{
     name = benches;
     config = Criterion::default();
-    targets =
-        bench_addition_2_digit, bench_addition_4_digit,
-        bench_subtraction_2_digit, bench_subtraction_4_digit,
-        bench_multiplication_2_digit, bench_multiplication_4_digit,
-        bench_division_4_digit
+    targets = bench_addition, bench_subtraction, bench_multiplication, bench_division
 }
 criterion_main!(benches);
