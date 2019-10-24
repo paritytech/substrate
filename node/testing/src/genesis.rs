81618967efc309fbbdd8dec3e8c28b21027861d1
thiolliere <gui.thiolliere@gmail.com>
2019-10-24 17:26:26 +0200

    Fix treasury kept and spend when emptied (#3880)
    * Now construct_runtime must include treasury config so account is created at genesis.
    * if it doesn't though it is ok, account will be created when the amount put is more than existential deposit.

--- node/testing/src/genesis.rs
+++ node/testing/src/genesis.rs
@@ -96,5 +96,6 @@ pub fn config(support_changes_trie: bool, code: Option<&[u8]>) -> GenesisConfig 
 		membership_Instance1: Some(Default::default()),
 		elections_phragmen: Some(Default::default()),
 		sudo: Some(Default::default()),
+		treasury: Some(Default::default()),
 	}
 }
