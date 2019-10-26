f2465e1e69f1286ee8a05d3708d16d659fd7d4a5
NikVolf <nikvolf@gmail.com>
2019-10-26 11:02:18 +0300

    configured benches

--- core/transaction-pool/graph/benches/basics.rs
+++ core/transaction-pool/graph/benches/basics.rs
@@ -42,7 +42,6 @@ impl ChainApi for TestApi {
 	type Error = error::Error;
 	type ValidationFuture = futures::future::Ready<error::Result<TransactionValidity>>;
 
-	/// Verify extrinsic at given block.
 	fn validate_transaction(
 		&self,
 		at: &BlockId<Self::Block>,
@@ -79,7 +78,6 @@ impl ChainApi for TestApi {
 		})
 	}
 
-	/// Returns a block number given the block id.
 	fn block_id_to_number(&self, at: &BlockId<Self::Block>) -> Result<Option<NumberFor<Self>>, Self::Error> {
 		Ok(match at {
 			BlockId::Number(num) => Some(*num),
@@ -87,7 +85,6 @@ impl ChainApi for TestApi {
 		})
 	}
 
-	/// Returns a block hash given the block id.
 	fn block_id_to_hash(&self, at: &BlockId<Self::Block>) -> Result<Option<BlockHash<Self>>, Self::Error> {
 		Ok(match at {
 			BlockId::Number(num) => Some(H256::from_low_u64_be(*num)).into(),
@@ -95,7 +92,6 @@ impl ChainApi for TestApi {
 		})
 	}
 
-	/// Hash the extrinsic.
 	fn hash_and_length(&self, uxt: &ExtrinsicFor<Self>) -> (Self::Hash, usize) {
 		let encoded = uxt.encode();
 		(blake2_256(&encoded).into(), encoded.len())
@@ -106,50 +102,58 @@ fn uxt(transfer: Transfer) -> Extrinsic {
 	Extrinsic::Transfer(transfer, Default::default())
 }
 
+fn bench_configured(pool: Pool<TestApi>, number: u64) {
+	let mut nonce = 1;
+	let mut futures = Vec::new();
+
+	for i in 0..50 {
+		let xt = uxt(Transfer {
+			from: AccountId::from_h256(H256::from_low_u64_be(1)),
+			to: AccountId::from_h256(H256::from_low_u64_be(2)),
+			amount: 5,
+			nonce,
+		});
+
+		nonce += 1;
+
+		futures.push(pool.submit_one(&BlockId::Number(1), xt));
+	}
+
+	let res = block_on(futures::future::join_all(futures.into_iter()));
+
+	// instantly producing "blocks" and pruning all ready until no ready
+	let mut block_num = 2;
+	loop {
+		if let Some(ready) = pool.ready().next() {
+			//dbg!(ready.clone());
+			block_on(pool.prune_tags(
+				&BlockId::Number(block_num),
+				vec![to_tag(ready.data.transfer().nonce, ready.data.transfer().from.clone())],
+				vec![ready.hash],
+			)).expect("Prune failed");
+		} else {
+			break;
+		}
+
+		block_num += 1;
+	}
+
+	// pool is empty
+	assert_eq!(pool.status().ready, 0);
+	assert_eq!(pool.status().future, 0);
+}
+
 fn benchmark_main(c: &mut Criterion) {
 
     c.bench_function("sequental 50 tx", |b| {
 		b.iter(|| {
+			bench_configured(Pool::new(Default::default(), TestApi::new_with_limit(50)), 50);
+		});
+	});
 
-			let pool = Pool::new(Default::default(), TestApi::new_with_limit(50));
-			let mut nonce = 1;
-			let mut futures = Vec::new();
-
-			for i in 0..50 {
-				let xt = uxt(Transfer {
-					from: AccountId::from_h256(H256::from_low_u64_be(1)),
-					to: AccountId::from_h256(H256::from_low_u64_be(2)),
-					amount: 5,
-					nonce,
-				});
-
-				nonce += 1;
-
-				futures.push(pool.submit_one(&BlockId::Number(1), xt));
-			}
-
-			let res = block_on(futures::future::join_all(futures.into_iter()));
-
-			let mut block_num = 2;
-			// instantly pruning all ready until no ready
-			loop {
-				if let Some(ready) = pool.ready().next() {
-					//dbg!(ready.clone());
-					block_on(pool.prune_tags(
-						&BlockId::Number(block_num),
-						vec![to_tag(ready.data.transfer().nonce, ready.data.transfer().from.clone())],
-						vec![ready.hash],
-					)).expect("Prune failed");
-				} else {
-					break;
-				}
-
-				block_num += 1;
-			}
-
-			// pool is empty
-			assert_eq!(pool.status().ready, 0);
-			assert_eq!(pool.status().future, 0);
+	c.bench_function("random 100 tx", |b| {
+		b.iter(|| {
+			bench_configured(Pool::new(Default::default(), TestApi::default()), 100);
 		});
 	});
 }
