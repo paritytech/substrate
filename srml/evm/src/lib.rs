2a8603be8e1c2154c000b809384ac4b182af7f30
Wei Tang <hi@that.world>
2019-10-26 17:21:12 +0200

    srml-evm: implement balance deposit and withdraw

--- srml/evm/src/lib.rs
+++ srml/evm/src/lib.rs
@@ -25,12 +25,25 @@ mod executor;
 
 pub use crate::executor::{Account, Log};
 
-use support::{decl_module, decl_storage, decl_event};
-
+use support::{dispatch::Result, decl_module, decl_storage, decl_event};
+use support::traits::{Currency, WithdrawReason, ExistenceRequirement};
+use system::ensure_signed;
+use sr_primitives::weights::SimpleDispatchInfo;
+use sr_primitives::traits::UniqueSaturatedInto;
 use primitives::{U256, H256, H160};
 
+pub type BalanceOf<T> = <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
+
+pub trait ConvertAccountId<A> {
+	fn convert_account_id(account_id: A) -> H160;
+}
+
 /// EVM module trait
-pub trait Trait: balances::Trait {
+pub trait Trait: system::Trait {
+	/// Convert account ID to H160;
+	type ConvertAccountId: ConvertAccountId<Self::AccountId>;
+	/// Currency type for deposit and withdraw.
+	type Currency: Currency<Self::AccountId>;
 	/// The overarching event type.
 	type Event: From<Event> + Into<<Self as system::Trait>::Event>;
 }
@@ -52,6 +65,45 @@ decl_event!(
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		fn deposit_event() = default;
+
+		#[weight = SimpleDispatchInfo::FixedNormal(10_000)]
+		fn deposit_balance(origin, value: BalanceOf<T>) -> Result {
+			let sender = ensure_signed(origin)?;
+
+			T::Currency::withdraw(
+				&sender,
+				value,
+				WithdrawReason::Reserve,
+				ExistenceRequirement::KeepAlive,
+			)?;
+
+			let bvalue = U256::from(UniqueSaturatedInto::<u64>::unique_saturated_into(value));
+			let address = T::ConvertAccountId::convert_account_id(sender);
+			Accounts::mutate(&address, |account| {
+				account.balance += bvalue;
+			});
+
+			Ok(())
+		}
+
+		#[weight = SimpleDispatchInfo::FixedNormal(10_000)]
+		fn withdraw_balance(origin, value: BalanceOf<T>) -> Result {
+			let sender = ensure_signed(origin)?;
+			let address = T::ConvertAccountId::convert_account_id(sender.clone());
+			let bvalue = U256::from(UniqueSaturatedInto::<u64>::unique_saturated_into(value));
+
+			if Accounts::get(&address).balance < bvalue {
+				return Err("Not enough balance")
+			}
+
+			Accounts::mutate(&address, |account| {
+				account.balance -= bvalue;
+			});
+
+			T::Currency::deposit_creating(&sender, value);
+
+			Ok(())
+		}
 	}
 }
 
