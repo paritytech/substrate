49c8b4ab790d7f5842095602d9775fbd7e278863
Bryan Chen <xlchen1291@gmail.com>
2019-10-26 23:06:58 +1300

    fix chain_spec

--- node-template/src/service.rs
+++ node-template/src/service.rs
@@ -3,7 +3,6 @@
 use std::sync::Arc;
 use std::time::Duration;
 use substrate_client::LongestChain;
-use futures::prelude::*;
 use runtime::{self, GenesisConfig, opaque::Block, RuntimeApi};
 use substrate_service::{error::{Error as ServiceError}, AbstractService, Configuration, ServiceBuilder};
 use transaction_pool::{self, txpool::{Pool as TransactionPool}};
