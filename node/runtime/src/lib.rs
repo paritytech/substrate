76a138f88cad878478f1c5ab565ec277b870362e
Gavin Wood <gavin@parity.io>
2019-10-27 14:36:35 +0100

    runtime: Expose const params for nicks module (#3932)
    * Expose const params for nicks module
    
    * Bump runtime

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -83,8 +83,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 186,
-	impl_version: 186,
+	spec_version: 187,
+	impl_version: 187,
 	apis: RUNTIME_API_VERSIONS,
 };
 
