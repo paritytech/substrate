57c4ed98d8faff39bf467ff923351996838c5565
Bastian Köcher <git@kchr.de>
2019-10-29 14:12:50 +0100

    Catch native panics when executing the wasm runtime
    As with the native runtime, we now catch all native panics when we
    execute the wasm runtime. The panics inside the wasm runtime were
    already catched before by the wasm executor automatically, but any panic
    in the host functions could bring down the node. The recent switch to
    execute the native counterpart of the host function in `sr-io`, makes
    this change required. The native `sr-io` functions just `panic` when
    something is not provided or any other error occured.

--- core/wasm-interface/src/lib.rs
+++ core/wasm-interface/src/lib.rs
@@ -180,7 +180,7 @@ pub trait Function {
 	fn execute(
 		&self,
 		context: &mut dyn FunctionContext,
-		args: &mut dyn Iterator<Item=Value>,
+		args: &mut dyn Iterator<Item = Value>,
 	) -> Result<Option<Value>>;
 }
 
