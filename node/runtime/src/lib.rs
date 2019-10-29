5e555cf1c97dc13d5b8399812d03dcccb8797dc6
Gavin Wood <gavin@parity.io>
2019-10-29 17:03:17 +0100

    More robust punishment (#3952)
    * Introduce new option "always force new era".
    
    * Take appropriate action, even for small offences.
    
    - Deselect the offender in all circumstances
    - Ensure that deselection forces a new era
    - Ensure that forcing a new era works with the always-forcing.
    
    * Bump runtime

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -81,7 +81,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 189,
+	spec_version: 190,
 	impl_version: 190,
 	apis: RUNTIME_API_VERSIONS,
 };
