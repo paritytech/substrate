4bc54417bf7ea4b36ce0bc831dbe321c128085b2
Gavin Wood <gavin@parity.io>
2019-10-29 10:49:58 +0100

    Update srml/elections-phragmen/src/lib.rs
    Co-Authored-By: Bastian Köcher <bkchr@users.noreply.github.com>

--- srml/elections-phragmen/src/lib.rs
+++ srml/elections-phragmen/src/lib.rs
@@ -646,7 +646,7 @@ impl<T: Trait> Module<T> {
 	fn do_migrate() {
 		// old storage format.
 		let old_members: Vec<T::AccountId> = unhashed::get_raw(&<Members<T>>::hashed_key())
-			.map_or_else(Default::default, |bytes| Decode::decode(&mut &*bytes).unwrap_or_default());
+			.and_then(|bytes| Decode::decode(&mut &*bytes).ok()).unwrap_or_default();
 		let old_runners: Vec<T::AccountId> = unhashed::get_raw(&<RunnersUp<T>>::hashed_key())
 			.map_or_else(Default::default, |bytes| Decode::decode(&mut &*bytes).unwrap_or_default());
 
