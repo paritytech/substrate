8fd20a11b15b2c9519a48ea0eb6102addea83b32
Wei Tang <hi@that.world>
2019-11-01 00:12:58 +0100

    Remove extern crate alloc

--- srml/evm/src/lib.rs
+++ srml/evm/src/lib.rs
@@ -19,8 +19,6 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-extern crate alloc;
-
 mod backend;
 
 pub use crate::backend::{Account, Log, Vicinity, Backend};
