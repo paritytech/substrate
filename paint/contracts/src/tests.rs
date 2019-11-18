47963216690cec5ef5abf5ebe97eb276f42d685d
Sergei Pepyakin <sergei@parity.io>
2019-11-18 13:37:15 +0100

    srml-contracts: Add ability to read substrate's storage (#4108)
    * Add `ext_get_runtime_storage`
    
    * Implement testing for ext_get_runtime_storage
    
    * Add integration test.
    
    * Update spec version
    
    * Fix the doc
    
    * Save memory allocation.

--- paint/contracts/src/tests.rs
+++ paint/contracts/src/tests.rs
@@ -2341,3 +2341,109 @@ fn check_block_gas_limit_works() {
 		assert_eq!(check.validate(&0, &call, info, 0), Ok(Default::default()));
 	});
 }
+
+const CODE_GET_RUNTIME_STORAGE: &str = r#"
+(module
+	(import "env" "ext_get_runtime_storage"
+		(func $ext_get_runtime_storage (param i32 i32) (result i32))
+	)
+	(import "env" "ext_scratch_size" (func $ext_scratch_size (result i32)))
+	(import "env" "ext_scratch_read" (func $ext_scratch_read (param i32 i32 i32)))
+	(import "env" "ext_scratch_write" (func $ext_scratch_write (param i32 i32)))
+	(import "env" "memory" (memory 1 1))
+
+	(func (export "deploy"))
+
+	(func $assert (param i32)
+		(block $ok
+			(br_if $ok
+				(get_local 0)
+			)
+			(unreachable)
+		)
+	)
+
+	(func $call (export "call")
+		;; Load runtime storage for the first key and assert that it exists.
+		(call $assert
+			(i32.eq
+				(call $ext_get_runtime_storage
+					(i32.const 16)
+					(i32.const 4)
+				)
+				(i32.const 0)
+			)
+		)
+
+		;; assert $ext_scratch_size == 4
+		(call $assert
+			(i32.eq
+				(call $ext_scratch_size)
+				(i32.const 4)
+			)
+		)
+
+		;; copy contents of the scratch buffer into the contract's memory.
+		(call $ext_scratch_read
+			(i32.const 4)		;; Pointer in memory to the place where to copy.
+			(i32.const 0)		;; Offset from the start of the scratch buffer.
+			(i32.const 4)		;; Count of bytes to copy.
+		)
+
+		;; assert that contents of the buffer is equal to the i32 value of 0x14144020.
+		(call $assert
+			(i32.eq
+				(i32.load
+					(i32.const 4)
+				)
+				(i32.const 0x14144020)
+			)
+		)
+
+		;; Load the second key and assert that it doesn't exist.
+		(call $assert
+			(i32.eq
+				(call $ext_get_runtime_storage
+					(i32.const 20)
+					(i32.const 4)
+				)
+				(i32.const 1)
+			)
+		)
+	)
+
+	;; The first key, 4 bytes long.
+	(data (i32.const 16) "\01\02\03\04")
+	;; The second key, 4 bytes long.
+	(data (i32.const 20) "\02\03\04\05")
+)
+"#;
+
+#[test]
+fn get_runtime_storage() {
+	let (wasm, code_hash) = compile_module::<Test>(CODE_GET_RUNTIME_STORAGE).unwrap();
+	ExtBuilder::default().existential_deposit(50).build().execute_with(|| {
+		Balances::deposit_creating(&ALICE, 1_000_000);
+
+		support::storage::unhashed::put_raw(
+			&[1, 2, 3, 4],
+			0x14144020u32.to_le_bytes().to_vec().as_ref()
+		);
+
+		assert_ok!(Contract::put_code(Origin::signed(ALICE), 100_000, wasm));
+		assert_ok!(Contract::instantiate(
+			Origin::signed(ALICE),
+			100,
+			100_000,
+			code_hash.into(),
+			vec![],
+		));
+		assert_ok!(Contract::call(
+			Origin::signed(ALICE),
+			BOB,
+			0,
+			100_000,
+			vec![],
+		));
+	});
+}
