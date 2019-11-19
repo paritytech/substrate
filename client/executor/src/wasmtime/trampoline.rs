59c512c19cabc39dfebd2fe0ae52f337cfa57922
Sergey Pepyakin <sergei@parity.io>
2019-11-19 17:59:13 +0100

    Update trampoline generation code.

--- client/executor/src/wasmtime/trampoline.rs
+++ client/executor/src/wasmtime/trampoline.rs
@@ -23,6 +23,7 @@ use cranelift_codegen::{Context, binemit, ir, isa};
 use cranelift_codegen::ir::{InstBuilder, StackSlotData, StackSlotKind, TrapCode};
 use cranelift_frontend::{FunctionBuilder, FunctionBuilderContext};
 use wasmtime_jit::{CodeMemory, Compiler};
+use wasmtime_environ::CompiledFunction;
 use wasmtime_runtime::{VMContext, VMFunctionBody};
 use wasm_interface::{Function, Value, ValueType};
 use std::{cmp, panic::{self, AssertUnwindSafe}, ptr};
@@ -281,8 +282,15 @@ pub fn make_trampoline(
 		)
 		.map_err(|e| WasmError::Instantiation(format!("failed to compile trampoline: {}", e)))?;
 
+	let mut unwind_info = Vec::new();
+	context.emit_unwind_info(isa, &mut unwind_info);
+
 	let func_ref = code_memory
-		.allocate_copy_of_byte_slice(&code_buf)
+		.allocate_for_function(&CompiledFunction {
+			body: code_buf,
+			jt_offsets: context.func.jt_offsets,
+			unwind_info,
+		})
 		.map_err(|e| WasmError::Instantiation(format!("failed to allocate code memory: {}", e)))?;
 
 	Ok(func_ref.as_ptr())
