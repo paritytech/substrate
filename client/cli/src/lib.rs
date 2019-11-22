ce83a3a0773a9da7f673fbc9792de2e6dbb0a5a4
Ashley <ashley.ruglys@gmail.com>
2019-11-22 15:49:49 +0100

    Grafana integration (#3913)
    * Very WIP
    
    * record_metrics macro works
    
    * Integrate into service
    
    * Licenses and documentation
    
    * Remove unused Debugs, make respond function clearer
    
    * Conform to line widths, fix service test
    
    * Switch to storing the timestamps as millis instead
    
    * Update core/grafana-data-source/src/lib.rs
    
    Co-Authored-By: Kian Paimani <5588131+kianenigma@users.noreply.github.com>
    
    * Transform timestamps to i64 in serialization
    
    * Fix license date
    
    * Binary sort to find selection range for metrics
    
    * Obey maxDataPoints
    
    * Run a cleaning future
    
    * Newlines at EOF
    
    * Update core/service/Cargo.toml
    
    Co-Authored-By: Pierre Krieger <pierre.krieger1708@gmail.com>
    
    * Update core/grafana-data-source/src/lib.rs
    
    Co-Authored-By: Pierre Krieger <pierre.krieger1708@gmail.com>
    
    * Fix indentation
    
    * Improve select_points
    
    * Made test more accurate
    
    * Inprogress
    
    * Use the same futures version as hyper for now
    
    * Error handling
    
    * Remove dependence on hyper's tokio feature
    
    * Added target_os flag
    
    * Update Cargo.toml
    
    Co-Authored-By: Pierre Krieger <pierre.krieger1708@gmail.com>
    
    * Simplify example
    
    * Remove compat wildcard
    
    * Updated lock file
    
    * Fix indentation 😉

--- client/cli/src/lib.rs
+++ client/cli/src/lib.rs
@@ -818,9 +818,13 @@ where
 
 	let rpc_interface: &str = if cli.rpc_external { "0.0.0.0" } else { "127.0.0.1" };
 	let ws_interface: &str = if cli.ws_external { "0.0.0.0" } else { "127.0.0.1" };
+	let grafana_interface: &str = if cli.grafana_external { "0.0.0.0" } else { "127.0.0.1" };
 
 	config.rpc_http = Some(parse_address(&format!("{}:{}", rpc_interface, 9933), cli.rpc_port)?);
 	config.rpc_ws = Some(parse_address(&format!("{}:{}", ws_interface, 9944), cli.ws_port)?);
+	config.grafana_port = Some(
+		parse_address(&format!("{}:{}", grafana_interface, 9955), cli.grafana_port)?
+	);
 
 	config.rpc_ws_max_connections = cli.ws_max_connections;
 	config.rpc_cors = cli.rpc_cors.unwrap_or_else(|| if is_dev {
