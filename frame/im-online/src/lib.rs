e80e790e00325310381eb53b5c680108efd564fc
Gavin Wood <gavin@parity.io>
2019-11-27 18:56:38 +0000

    Apply suggestions from code review
    Co-Authored-By: joe petrowski <25483142+joepetrowski@users.noreply.github.com>

--- frame/im-online/src/lib.rs
+++ frame/im-online/src/lib.rs
@@ -653,7 +653,7 @@ impl<Offender: Clone> Offence<Offender> for UnresponsivenessOffence<Offender> {
 	}
 
 	fn slash_fraction(offenders: u32, validator_set_count: u32) -> Perbill {
-		// the formula is min((3 * (k - (n / 10 + 1))) / n, 1) * 0.05
+		// the formula is min((3 * (k - (n / 10 + 1))) / n, 1) * 0.07
 		// basically, 10% can be offline with no slash, but after that, it linearly climbs up to 7%
 		// when 13/30 are offline (around 5% when 1/3 are offline).
 		if let Some(threshold) = offenders.checked_sub(validator_set_count / 10 + 1) {
