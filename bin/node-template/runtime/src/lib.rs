561457e9be000c2c062ce3d3a76c98af3479ef3b
Andrew Jones <ascjones@gmail.com>
2019-12-02 19:24:25 +0000

    Fix sp-session typo (#4273)
    * Fix sp-session typo
    
    * Remove erroneous whitespaces
    
    * Don't mess with the file, editor

--- bin/node-template/runtime/src/lib.rs
+++ bin/node-template/runtime/src/lib.rs
@@ -350,7 +350,7 @@ impl_runtime_apis! {
 		}
 	}
 
-	impl sp_sesssion::SessionKeys<Block> for Runtime {
+	impl sp_session::SessionKeys<Block> for Runtime {
 		fn generate_session_keys(seed: Option<Vec<u8>>) -> Vec<u8> {
 			opaque::SessionKeys::generate(seed)
 		}
