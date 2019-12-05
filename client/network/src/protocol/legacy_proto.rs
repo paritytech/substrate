75dadcf9ad1e2af5fcbba40b8a5a1fec9d4e9366
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-12-05 14:02:48 +0100

    Move legacy_proto to protocol (#4296)
    * Move legacy_proto to protocol
    
    * Edition 2018ize legacy_proto
    
    * Some basic documentation

--- client/network/src/protocol/legacy_proto.rs
+++ client/network/src/protocol/legacy_proto.rs
@@ -14,6 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
+//! Implementation of libp2p's `NetworkBehaviour` trait that opens a single substream with the
+//! remote and then allows any communication with them.
+//!
+//! The `Protocol` struct uses `LegacyProto` in order to open substreams with the rest of the
+//! network, then performs the Substrate protocol handling on top.
+
 pub use self::behaviour::{LegacyProto, LegacyProtoOut};
 
 mod behaviour;
