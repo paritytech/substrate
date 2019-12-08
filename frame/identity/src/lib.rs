e6f449dc08649c463c7144e89130ebbbe0ad812f
Gav Wood <gavin@parity.io>
2019-12-08 17:23:39 +0100

    Minor removal

--- frame/identity/src/lib.rs
+++ frame/identity/src/lib.rs
@@ -526,7 +526,6 @@ mod tests {
 	use support::{assert_ok, assert_noop, impl_outer_origin, parameter_types, weights::Weight};
 	use primitives::H256;
 	use system::EnsureSignedBy;
-	use balances;
 	// The testing primitives are very useful for avoiding having to work with signatures
 	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
 	use sp_runtime::{
