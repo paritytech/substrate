bb33e757603f0146404a60c3d3fc725dc8451bf9
Bastian Köcher <bkchr@users.noreply.github.com>
2019-12-11 14:04:37 +0100

    Update primitives/state-machine/src/ext.rs
    Co-Authored-By: Tomasz Drwięga <tomusdrw@users.noreply.github.com>

--- primitives/state-machine/src/ext.rs
+++ primitives/state-machine/src/ext.rs
@@ -8,7 +8,6 @@
 
 // Substrate is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
