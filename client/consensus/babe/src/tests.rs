3392272fbd8127c4e470541bd46fd77915497c74
Benjamin Kampmann <ben@gnunicorn.org>
2019-12-11 08:48:23 +0100

    Fix up consensus crates for networking test changes

--- client/consensus/babe/src/tests.rs
+++ client/consensus/babe/src/tests.rs
@@ -28,11 +28,10 @@ use consensus_common::NoNetwork as DummyOracle;
 use consensus_common::import_queue::{
 	BoxBlockImport, BoxJustificationImport, BoxFinalityProofImport,
 };
-use network::test::*;
-use network::test::{Block as TestBlock, PeersClient};
-use network::config::BoxFinalityProofRequestBuilder;
+use sc_network_test::*;
+use sc_network_test::{Block as TestBlock, PeersClient};
+use sc_network::config::{BoxFinalityProofRequestBuilder, ProtocolConfig};
 use sp_runtime::{generic::DigestItem, traits::{Block as BlockT, DigestFor}};
-use network::config::ProtocolConfig;
 use tokio::runtime::current_thread;
 use client_api::BlockchainEvents;
 use test_client;
