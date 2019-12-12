9303074afb17a67e946c8e1128092aee980e94ee
Tomasz Drwięga <tomasz@parity.io>
2019-12-12 14:08:15 +0100

    Fixing: Phase 8. Tests

--- client/network/src/protocol/light_dispatch.rs
+++ client/network/src/protocol/light_dispatch.rs
@@ -686,7 +686,7 @@ pub mod tests {
 	use crate::message::{self, BlockAttributes, Direction, FromBlock, RequestId};
 	use libp2p::PeerId;
 	use super::{REQUEST_TIMEOUT, LightDispatch, LightDispatchNetwork, RequestData, StorageProof};
-	use sp_test_sp_core::{changes_trie_config, Block, Extrinsic, Header};
+	use sp_test_primitives::{changes_trie_config, Block, Extrinsic, Header};
 
 	struct DummyFetchChecker { ok: bool }
 
