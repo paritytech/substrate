7c3c8caaadafdbbc2cf42b2cc88f2468a2d08a26
Leo Arias <elopio@openzeppelin.com>
2019-12-18 00:59:32 -0600

    Fix typo in comment

--- frame/membership/src/lib.rs
+++ frame/membership/src/lib.rs
@@ -16,7 +16,7 @@
 
 //! # Membership Module
 //!
-//! Allows control of membership of a set of `AccountId`s, useful for managing membership of of a
+//! Allows control of membership of a set of `AccountId`s, useful for managing membership of a
 //! collective.
 
 // Ensure we're `no_std` when compiling for Wasm.
