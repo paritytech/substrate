adc8d505a848ac84ff88758caf3ddc92db6fc202
André Silva <andre.beat@gmail.com>
2019-12-18 13:32:09 +0000

    grandpa: fix slow gossip test (#4440)

--- client/finality-grandpa/src/communication/tests.rs
+++ client/finality-grandpa/src/communication/tests.rs
@@ -274,14 +274,23 @@ fn good_commit_leads_to_relay() {
 			let sender_id = id.clone();
 			let send_message = tester.filter_network_events(move |event| match event {
 				Event::EventStream(sender) => {
+					// Add the sending peer and send the commit
 					let _ = sender.unbounded_send(NetworkEvent::NotificationStreamOpened {
 						remote: sender_id.clone(),
 						engine_id: GRANDPA_ENGINE_ID,
 						roles: Roles::FULL,
 					});
+
 					let _ = sender.unbounded_send(NetworkEvent::NotificationsReceived {
 						remote: sender_id.clone(),
 						messages: vec![(GRANDPA_ENGINE_ID, commit_to_send.clone().into())],
+					});
+
+					// Add a random peer which will be the recipient of this message
+					let _ = sender.unbounded_send(NetworkEvent::NotificationStreamOpened {
+						remote: sc_network::PeerId::random(),
+						engine_id: GRANDPA_ENGINE_ID,
+						roles: Roles::FULL,
 					});
 
 					true
