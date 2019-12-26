48c931699144260a8be01a199536c1d6d0a3690e
Nikolay Volf <nikvolf@gmail.com>
2019-12-26 16:04:07 +0300

    Check aura slot numbers are strictly increasing (#4020)
    * initial block import handler
    
    * fix node template
    
    * fix error message
    
    * fix error message again
    
    * final fixes
    
    * fix node template again

--- bin/node-template/src/service.rs
+++ bin/node-template/src/service.rs
@@ -56,9 +56,13 @@ macro_rules! new_full_start {
 						client.clone(), &*client, select_chain
 					)?;
 
-				let import_queue = sc_consensus_aura::import_queue::<_, _, AuraPair, _>(
+				let aura_block_import = sc_consensus_aura::AuraBlockImport::<_, _, _, AuraPair>::new(
+					grandpa_block_import.clone(), client.clone(),
+				);
+
+				let import_queue = sc_consensus_aura::import_queue::<_, _, _, AuraPair, _>(
 					sc_consensus_aura::SlotDuration::get_or_compute(&*client)?,
-					Box::new(grandpa_block_import.clone()),
+					aura_block_import,
 					Some(Box::new(grandpa_block_import.clone())),
 					None,
 					client,
@@ -220,9 +224,9 @@ pub fn new_light<C: Send + Default + 'static>(config: Configuration<C, GenesisCo
 			let finality_proof_request_builder =
 				finality_proof_import.create_finality_proof_request_builder();
 
-			let import_queue = sc_consensus_aura::import_queue::<_, _, AuraPair, ()>(
+			let import_queue = sc_consensus_aura::import_queue::<_, _, _, AuraPair, ()>(
 				sc_consensus_aura::SlotDuration::get_or_compute(&*client)?,
-				Box::new(grandpa_block_import),
+				grandpa_block_import,
 				None,
 				Some(Box::new(finality_proof_import)),
 				client,
