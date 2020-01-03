e2441808516496c5cfc3c0a8652c3b90e2def75b
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2020-01-03 21:39:46 +0100

    Fix `period` of offchain transactions. (#4521)
    * Fix period of offchain transactions.
    
    * Calculate period dynamically.
    
    * Convert to u64.

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -500,7 +500,11 @@ impl frame_system::offchain::CreateTransaction<Runtime, UncheckedExtrinsic> for 
 		account: AccountId,
 		index: Index,
 	) -> Option<(Call, <UncheckedExtrinsic as traits::Extrinsic>::SignaturePayload)> {
-		let period = 1 << 8;
+		// take the biggest period possible.
+		let period = BlockHashCount::get()
+			.checked_next_power_of_two()
+			.map(|c| c / 2)
+			.unwrap_or(2) as u64;
 		let current_block = System::block_number().saturated_into::<u64>();
 		let tip = 0;
 		let extra: SignedExtra = (
