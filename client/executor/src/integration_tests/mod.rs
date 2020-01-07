0ef27467f6799c68c2d8d88da5def3f6e7171efb
Cecile Tonglet <cecile@parity.io>
2020-01-07 14:06:03 +0100

    WIP

--- client/executor/src/integration_tests/mod.rs
+++ client/executor/src/integration_tests/mod.rs
@@ -90,7 +90,7 @@ fn returning_should_work(wasm_method: WasmExecutionMethod) {
 
 #[test_case(WasmExecutionMethod::Interpreted)]
 #[cfg_attr(feature = "wasmtime", test_case(WasmExecutionMethod::Compiled))]
-#[should_panic(expected = "function missing_external do not exist")]
+#[should_panic(expected = "function missing_external does not exist")]
 fn call_not_existing_function_with_stub_enabled(wasm_method: WasmExecutionMethod) {
 	let mut ext = TestExternalities::default();
 	let mut ext = ext.ext();
@@ -108,7 +108,7 @@ fn call_not_existing_function_with_stub_enabled(wasm_method: WasmExecutionMethod
 
 #[test_case(WasmExecutionMethod::Interpreted)]
 #[cfg_attr(feature = "wasmtime", test_case(WasmExecutionMethod::Compiled))]
-#[should_panic(expected = "function yet_another_missing_external do not exist")]
+#[should_panic(expected = "function yet_another_missing_external does not exist")]
 fn call_yet_another_not_existing_function_with_stub_enabled(wasm_method: WasmExecutionMethod) {
 	let mut ext = TestExternalities::default();
 	let mut ext = ext.ext();
