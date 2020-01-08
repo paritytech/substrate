441d5ca16696913fdab841b3196af57e39ae1f10
cheme <emericchevalier.pro@gmail.com>
2020-01-08 10:39:28 +0100

    Declare transaction host function for wasm

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -908,6 +908,7 @@ pub type TestExternalities = sp_state_machine::TestExternalities<sp_core::Blake2
 #[cfg(feature = "std")]
 pub type SubstrateHostFunctions = (
 	storage::HostFunctions,
+	transaction::HostFunctions,
 	misc::HostFunctions,
 	offchain::HostFunctions,
 	crypto::HostFunctions,
