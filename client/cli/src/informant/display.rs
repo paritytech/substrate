8025361826b476f0fae9a5e1561b850c6e1de14f
Ashley <ashley.ruglys@gmail.com>
2020-01-10 12:11:58 +0100

    Add generics

--- client/cli/src/informant/display.rs
+++ client/cli/src/informant/display.rs
@@ -23,10 +23,10 @@ use sp_runtime::traits::{
 };
 use sc_service::NetworkStatus;
 use std::{convert::{TryFrom, TryInto}, fmt, time};
-use prometheus_endpoint::{create_gauge, Gauge};
+use prometheus_endpoint::{create_gauge, Gauge, U64};
 
 prometheus_endpoint::lazy_static! {
-	pub static ref SYNC_TARGET: Gauge = create_gauge(
+	pub static ref SYNC_TARGET: Gauge<U64> = create_gauge(
 		"sync_target_number",
 		"block sync target number"
 	);
@@ -74,7 +74,7 @@ impl<B: BlockT> InformantDisplay<B> {
 			(SyncState::Idle, _) => ("Idle".into(), "".into()),
 			(SyncState::Downloading, None) => (format!("Syncing{}", speed), "".into()),
 			(SyncState::Downloading, Some(n)) => {
-				SYNC_TARGET.set(n.unique_saturated_into() as i64);
+				SYNC_TARGET.set(n.unique_saturated_into() as u64);
 				(format!("Syncing{}", speed), format!(", target=#{}", n))
 			}
 		};
