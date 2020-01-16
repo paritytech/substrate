d1ebb745c48a6515b5f260d2c93066f19119b502
Fedor Sakharov <fedor.sakharov@gmail.com>
2020-01-16 18:28:54 +0300

    bring back license comment

--- primitives/trie/src/lib.rs
+++ primitives/trie/src/lib.rs
@@ -1,3 +1,5 @@
+// Copyright 2015-2020 Parity Technologies (UK) Ltd.
+// This file is part of Substrate.
 
 // Parity is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
