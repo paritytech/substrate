6f0bb91d0cde566834d32a1b59946aa1807b8399
Robin Freyler <robin.freyler@gmail.com>
2020-01-23 16:39:45 +0100

    [contracts] minor follow-up on PR #4722

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -80,8 +80,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to equal spec_version. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 204,
-	impl_version: 204,
+	spec_version: 205,
+	impl_version: 205,
 	apis: RUNTIME_API_VERSIONS,
 };
 
