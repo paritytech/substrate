c69956e48389467e6263f9120675808c007b6f67
Cecile Tonglet <cecile@parity.io>
2020-01-23 14:34:54 +0100

    WIP

--- bin/node/cli/build.rs
+++ bin/node/cli/build.rs
@@ -14,38 +14,46 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::{fs, env, path::Path};
-use sc_cli::{CoreParams, structopt::{StructOpt, clap::Shell}};
-use vergen::{ConstantsFlags, generate_cargo_keys};
-
 fn main() {
-	build_shell_completion();
-	generate_cargo_keys(ConstantsFlags::all()).expect("Failed to generate metadata files");
-
-	build_script_utils::rerun_if_git_head_changed();
+	#[cfg(feature = "cli")]
+	cli::main();
 }
 
-/// Build shell completion scripts for all known shells
-/// Full list in https://github.com/kbknapp/clap-rs/blob/e9d0562a1dc5dfe731ed7c767e6cee0af08f0cf9/src/app/parser.rs#L123
-fn build_shell_completion() {
-	for shell in &[Shell::Bash, Shell::Fish, Shell::Zsh, Shell::Elvish, Shell::PowerShell] {
-		build_completion(shell);
+#[cfg(feature = "cli")]
+mod cli {
+	use std::{fs, env, path::Path};
+	use sc_cli::{CoreParams, structopt::{StructOpt, clap::Shell}};
+	use vergen::{ConstantsFlags, generate_cargo_keys};
+
+	pub fn main() {
+		build_shell_completion();
+		generate_cargo_keys(ConstantsFlags::all()).expect("Failed to generate metadata files");
+
+		build_script_utils::rerun_if_git_head_changed();
 	}
-}
 
-/// Build the shell auto-completion for a given Shell
-fn build_completion(shell: &Shell) {
-	let outdir = match env::var_os("OUT_DIR") {
-		None => return,
-		Some(dir) => dir,
-	};
-	let path = Path::new(&outdir)
-		.parent().unwrap()
-		.parent().unwrap()
-		.parent().unwrap()
-		.join("completion-scripts");
+	/// Build shell completion scripts for all known shells
+	/// Full list in https://github.com/kbknapp/clap-rs/blob/e9d0562a1dc5dfe731ed7c767e6cee0af08f0cf9/src/app/parser.rs#L123
+	fn build_shell_completion() {
+		for shell in &[Shell::Bash, Shell::Fish, Shell::Zsh, Shell::Elvish, Shell::PowerShell] {
+			build_completion(shell);
+		}
+	}
 
-	fs::create_dir(&path).ok();
+	/// Build the shell auto-completion for a given Shell
+	fn build_completion(shell: &Shell) {
+		let outdir = match env::var_os("OUT_DIR") {
+			None => return,
+			Some(dir) => dir,
+		};
+		let path = Path::new(&outdir)
+			.parent().unwrap()
+			.parent().unwrap()
+			.parent().unwrap()
+			.join("completion-scripts");
 
-	CoreParams::clap().gen_completions("substrate-node", *shell, &path);
+		fs::create_dir(&path).ok();
+
+		CoreParams::clap().gen_completions("substrate-node", *shell, &path);
+	}
 }
