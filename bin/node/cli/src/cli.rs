7db7da3acbaea325ef9f055ee374ccce8ebe5cd6
Cecile Tonglet <cecile@parity.io>
2020-01-31 12:32:12 +0100

    Removed a few warnings (#4786)

--- bin/node/cli/src/cli.rs
+++ bin/node/cli/src/cli.rs
@@ -17,6 +17,7 @@
 use sc_cli::{SharedParams, ImportParams, RunCmd};
 use structopt::StructOpt;
 
+#[allow(missing_docs)]
 #[derive(Clone, Debug, StructOpt)]
 #[structopt(settings = &[
 	structopt::clap::AppSettings::GlobalVersion,
@@ -24,14 +25,18 @@ use structopt::StructOpt;
 	structopt::clap::AppSettings::SubcommandsNegateReqs,
 ])]
 pub struct Cli {
+	#[allow(missing_docs)]
 	#[structopt(subcommand)]
 	pub subcommand: Option<Subcommand>,
+	#[allow(missing_docs)]
 	#[structopt(flatten)]
 	pub run: RunCmd,
 }
 
+#[allow(missing_docs)]
 #[derive(Clone, Debug, StructOpt)]
 pub enum Subcommand {
+	#[allow(missing_docs)]
 	#[structopt(flatten)]
 	Base(sc_cli::Subcommand),
 	/// The custom factory subcommmand for manufacturing transactions.
