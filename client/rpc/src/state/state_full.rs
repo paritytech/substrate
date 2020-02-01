8e0cfdb63d55b696455e050e84ac3d6a3a027488
Bryan Chen <xlchen1291@gmail.com>
2020-02-01 17:04:19 +1300

    improve

--- client/rpc/src/state/state_full.rs
+++ client/rpc/src/state/state_full.rs
@@ -264,12 +264,11 @@ impl<B, E, Block, RA> StateBackend<B, E, Block, RA> for FullState<B, E, Block, R
 		Box::new(result(
 			self.block_or_best(block)
 				.and_then(|block|
-					self.client
-						.storage_keys_paged(
+					self.client.storage_keys_paged(
 						&BlockId::Hash(block), &prefix, count as usize, &start_key
-						)
-						.map(|v| v.collect())
+					)
 				)
+				.map(|v| v.collect())
 				.map_err(client_err)))
 	}
 
