93422d48119c2580f60dbf5d991d4f54ce547ee5
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-02-08 17:32:06 +0100

    Bump numbers in `wasmtime` integration tests.

--- client/executor/src/integration_tests/mod.rs
+++ client/executor/src/integration_tests/mod.rs
@@ -88,7 +88,7 @@ fn call_not_existing_function(wasm_method: WasmExecutionMethod) {
 				#[cfg(feature = "wasmtime")]
 				WasmExecutionMethod::Compiled => assert_eq!(
 					&format!("{:?}", e),
-					"Other(\"call to undefined external function with index 68\")"
+					"Other(\"call to undefined external function with index 71\")"
 				),
 			}
 		}
@@ -117,7 +117,7 @@ fn call_yet_another_not_existing_function(wasm_method: WasmExecutionMethod) {
 				#[cfg(feature = "wasmtime")]
 				WasmExecutionMethod::Compiled => assert_eq!(
 					&format!("{:?}", e),
-					"Other(\"call to undefined external function with index 69\")"
+					"Other(\"call to undefined external function with index 72\")"
 				),
 			}
 		}
