2334be69c253a6af90cb6a3b4c2f31bce1a5d5a2
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-02-10 17:39:00 +0100

    Transfer keep alive

--- frame/balances/src/benchmarking.rs
+++ frame/balances/src/benchmarking.rs
@@ -21,7 +21,7 @@ use super::*;
 use frame_system::RawOrigin;
 use sp_io::hashing::blake2_256;
 use sp_runtime::{BenchmarkResults, BenchmarkParameter};
-use sp_runtime::traits::{Benchmarking, BenchmarkingSetup, Dispatchable};
+use sp_runtime::traits::{Bounded, Benchmarking, BenchmarkingSetup, Dispatchable};
 
 use crate::Module as Balances;
 
@@ -104,10 +104,52 @@ impl<T: Trait> BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>> for
 		let e = components.iter().find(|&c| c.0 == BenchmarkParameter::E).unwrap().1;
 
 		// Give the sender account plenty of funds for transfer
-		let _ = <Balances<T> as Currency<_>>::make_free_balance_be(&user, ed.saturating_mul((e + 1).into()));
+		let _ = <Balances<T> as Currency<_>>::make_free_balance_be(&user, T::Balance::max_value());
 
 		// Give the recipient account existential deposit.
 		let _ = <Balances<T> as Currency<_>>::make_free_balance_be(&recipient, ed);
+
+		// Transfer e * existential deposit.
+		let transfer_amt = ed.saturating_mul(e.into());
+
+		// Return the `transfer` call
+		Ok((crate::Call::<T>::transfer(recipient_lookup, transfer_amt), user_origin))
+	}
+}
+
+// Benchmark `transfer_keep_alive` with the worst possible condition:
+// * The recipient account is created.
+struct TransferKeepAlive;
+impl<T: Trait> BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>> for TransferKeepAlive {
+	fn components(&self) -> Vec<(BenchmarkParameter, u32, u32)> {
+		vec![
+			// Existential Deposit Multiplier
+			(BenchmarkParameter::E, 2, 1000),
+			// User Seed
+			(BenchmarkParameter::U, 1, 1000),
+		]
+	}
+
+	fn instance(&self, components: &[(BenchmarkParameter, u32)])
+		-> Result<(crate::Call<T>, RawOrigin<T::AccountId>), &'static str>
+	{
+		// Constants
+		let ed = T::ExistentialDeposit::get();
+		
+		// Select a sender
+		let u = components.iter().find(|&c| c.0 == BenchmarkParameter::U).unwrap().1;
+		let user = account::<T>("user", u);
+		let user_origin = RawOrigin::Signed(user.clone());
+
+		// Select a recipient
+		let recipient = account::<T>("recipient", u);
+		let recipient_lookup: <T::Lookup as StaticLookup>::Source = T::Lookup::unlookup(recipient.clone());
+
+		// Get the existential deposit multiplier
+		let e = components.iter().find(|&c| c.0 == BenchmarkParameter::E).unwrap().1;
+
+		// Give the sender account plenty of funds for transfer, transfer will not kill account.
+		let _ = <Balances<T> as Currency<_>>::make_free_balance_be(&user, T::Balance::max_value());
 
 		// Transfer e * existential deposit.
 		let transfer_amt = ed.saturating_mul(e.into());
@@ -121,15 +163,16 @@ impl<T: Trait> BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>> for
 enum SelectedBenchmark {
 	Transfer,
 	TransferBestCase,
+	TransferKeepAlive,
 }
 
 // Allow us to select a benchmark from the list of available benchmarks.
-
 impl<T: Trait> BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>> for SelectedBenchmark {
 	fn components(&self) -> Vec<(BenchmarkParameter, u32, u32)> {
 		match self {
 			Self::Transfer => <Transfer as BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>>>::components(&Transfer),
 			Self::TransferBestCase => <TransferBestCase as BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>>>::components(&TransferBestCase),
+			Self::TransferKeepAlive => <TransferKeepAlive as BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>>>::components(&TransferKeepAlive),
 		}
 	}
 
@@ -139,6 +182,7 @@ impl<T: Trait> BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>> for
 		match self {
 			Self::Transfer => <Transfer as BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>>>::instance(&Transfer, components),
 			Self::TransferBestCase => <TransferBestCase as BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>>>::instance(&TransferBestCase, components),
+			Self::TransferKeepAlive => <TransferKeepAlive as BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>>>::instance(&TransferKeepAlive, components),
 		}
 	}
 }
@@ -149,6 +193,7 @@ impl<T: Trait> Benchmarking<BenchmarkResults> for Module<T> {
 		let selected_benchmark = match extrinsic.as_slice() {
 			b"transfer" => SelectedBenchmark::Transfer,
 			b"transfer_best_case" => SelectedBenchmark::TransferBestCase,
+			b"transfer_keep_alive" => SelectedBenchmark::TransferKeepAlive,
 			_ => return Err("Could not find extrinsic."),
 		};
 
