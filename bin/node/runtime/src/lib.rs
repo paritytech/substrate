ee0f98dd207013e598c95450217ca6ddcf195d1d
Wei Tang <hi@that.world>
2020-02-11 18:52:59 +0100

    pallet-evm: optional nonce parameter (#4893)
    * pallet-evm: optional nonce parameter
    
    * Consume all gases when nonce mismatches
    
    * Bump node runtime version

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -81,8 +81,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 215,
-	impl_version: 2,
+	spec_version: 216,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
 
