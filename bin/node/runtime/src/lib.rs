2290645fb253cd6629525be775cbbb6bd43658e8
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-02-12 12:11:20 +0100

    Benchmark the Balances Pallet (#4879)
    * Initial transfer bench
    
    * Add best case
    
    * Transfer keep alive
    
    * Set balance benchmarks
    
    * Bump impl
    
    * Fix text
    
    Co-authored-by: Gavin Wood <github@gavwood.com>

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -812,6 +812,7 @@ impl_runtime_apis! {
 			-> Option<Vec<BenchmarkResults>>
 		{
 			match module.as_slice() {
+				b"pallet-balances" | b"balances" => Balances::run_benchmark(extrinsic, steps, repeat).ok(),
 				b"pallet-identity" | b"identity" => Identity::run_benchmark(extrinsic, steps, repeat).ok(),
 				b"pallet-timestamp" | b"timestamp" => Timestamp::run_benchmark(extrinsic, steps, repeat).ok(),
 				_ => return None,
