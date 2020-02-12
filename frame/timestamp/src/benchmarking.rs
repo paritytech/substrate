8be0791af2010ef38f88143b8d419454145c58bb
marcio-diaz <marcio.diaz@gmail.com>
2020-02-12 10:32:34 +0100

    Add component Now.

--- frame/timestamp/src/benchmarking.rs
+++ frame/timestamp/src/benchmarking.rs
@@ -28,24 +28,27 @@ use sp_runtime::traits::{Benchmarking, BenchmarkingSetup, Dispatchable};
 struct Set;
 impl<T: Trait> BenchmarkingSetup<T, Call<T>, RawOrigin<T::AccountId>> for Set {
 	fn components(&self) -> Vec<(BenchmarkParameter, u32, u32)> {
-		vec![]
+		vec![
+			// Current time ("Now")
+			(BenchmarkParameter::N, 1, 100),
+		]
 	}
 
-	fn instance(&self, _components: &[(BenchmarkParameter, u32)])
+	fn instance(&self, components: &[(BenchmarkParameter, u32)])
 		-> Result<(Call<T>, RawOrigin<T::AccountId>), &'static str>
 	{
-		let user_origin = RawOrigin::None; 
-		let now = 1.into();
+		let user_origin = RawOrigin::None;
+		let now = components.iter().find(|&c| c.0 == BenchmarkParameter::N).unwrap().1;
 
 		// Return the `set` call
-		Ok((Call::<T>::set(now), user_origin))
+		Ok((Call::<T>::set(now.into()), user_origin))
 	}
 }
 
 selected_benchmark!(Set);
 
 impl<T: Trait> Benchmarking<BenchmarkResults> for Module<T> {
-	fn run_benchmark(extrinsic: Vec<u8>, _steps: u32, repeat: u32) -> Result<Vec<BenchmarkResults>, &'static str> {
+	fn run_benchmark(extrinsic: Vec<u8>, steps: u32, repeat: u32) -> Result<Vec<BenchmarkResults>, &'static str> {
 		// Map the input to the selected benchmark.
 		let selected_benchmark = match extrinsic.as_slice() {
 			b"set" => SelectedBenchmark::Set,
@@ -56,23 +59,45 @@ impl<T: Trait> Benchmarking<BenchmarkResults> for Module<T> {
 		sp_io::benchmarking::commit_db();
 		sp_io::benchmarking::wipe_db();
 
-		// results go here
+		let components = <SelectedBenchmark as BenchmarkingSetup<T, crate::Call<T>, RawOrigin<T::AccountId>>>::components(&selected_benchmark);
 		let mut results: Vec<BenchmarkResults> = Vec::new();
-		// Run the benchmark `repeat` times.
-		for _r in 0..repeat {
-			// Set up the externalities environment for the setup we want to benchmark.
-			let (call, caller) = <SelectedBenchmark as BenchmarkingSetup<T, Call<T>, RawOrigin<T::AccountId>>>::instance(&selected_benchmark, &[])?;
-			// Commit the externalities to the database, flushing the DB cache.
-			// This will enable worst case scenario for reading from the database.
-			sp_io::benchmarking::commit_db();
-			// Run the benchmark.
-			let start = sp_io::benchmarking::current_time();
-			call.dispatch(caller.clone().into())?;
-			let finish = sp_io::benchmarking::current_time();
-			let elapsed = finish - start;
-			results.push((vec![], elapsed));
-			// Wipe the DB back to the genesis state.
-			sp_io::benchmarking::wipe_db();
+
+		// Select the component we will be benchmarking. Each component will be benchmarked.
+		for (name, low, high) in components.iter() {
+			// Create up to `STEPS` steps for that component between high and low.
+			let step_size = ((high - low) / steps).max(1);
+			let num_of_steps = (high - low) / step_size;
+			for s in 0..num_of_steps {
+				// This is the value we will be testing for component `name`
+				let component_value = low + step_size * s;
+
+				// Select the mid value for all the other components.
+				let c: Vec<(BenchmarkParameter, u32)> = components.iter()
+					.map(|(n, l, h)|
+						(*n, if n == name { component_value } else { (h - l) / 2 + l })
+					).collect();
+
+				// Run the benchmark `repeat` times.
+				for _ in 0..repeat {
+					// Set up the externalities environment for the setup we want to benchmark.
+					let (call, caller) = <SelectedBenchmark as BenchmarkingSetup<
+						T,
+						Call<T>,
+						RawOrigin<T::AccountId>,
+					>>::instance(&selected_benchmark, &c)?;
+					// Commit the externalities to the database, flushing the DB cache.
+					// This will enable worst case scenario for reading from the database.
+					sp_io::benchmarking::commit_db();
+					// Run the benchmark.
+					let start = sp_io::benchmarking::current_time();
+					call.dispatch(caller.into())?;
+					let finish = sp_io::benchmarking::current_time();
+					let elapsed = finish - start;
+					results.push((c.clone(), elapsed));
+					// Wipe the DB back to the genesis state.
+					sp_io::benchmarking::wipe_db();
+				}
+			}
 		}
 
 		return Ok(results);
