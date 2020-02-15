819f95c0eb6bbb44a7009340761e39a77223a5a9
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-02-15 17:10:37 +0100

    datastructures -> data structures

--- bin/node-template/runtime/src/lib.rs
+++ bin/node-template/runtime/src/lib.rs
@@ -69,7 +69,7 @@ pub type DigestItem = generic::DigestItem<Hash>;
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
 /// of data like extrinsics, allowing for them to continue syncing the network through upgrades
-/// to even the core datastructures.
+/// to even the core data structures.
 pub mod opaque {
 	use super::*;
 
