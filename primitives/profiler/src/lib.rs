414feceed8bf44f6cc72e58e739c08f4a8b60856
Matt Rutherford <mattrutherford@users.noreply.github.com>
2020-02-19 14:36:43 +0000

    improve performance using quanta Clock

--- primitives/profiler/src/lib.rs
+++ primitives/profiler/src/lib.rs
@@ -2,12 +2,27 @@ use std::time::{Instant, Duration};
 use std::collections::HashMap;
 use std::thread::JoinHandle;
 use std::sync::atomic::{AtomicU64, Ordering};
+use quanta::Clock;
 use parking_lot::Mutex;
 
 pub trait Profiler: Send {
 	fn create_span(&mut self, target: String, name: String) -> u64;
 
 	fn exit_span(&self, id: u64);
+}
+
+#[derive(Debug)]
+struct EnterSpan {
+	id: u64,
+	name: String,
+	target: String,
+	time: u64,
+}
+
+#[derive(Debug)]
+struct ExitSpan {
+	id: u64,
+	time: u64,
 }
 
 enum SpanPhase {
@@ -17,6 +32,7 @@ enum SpanPhase {
 
 pub struct AsyncProfiler {
 	next_id: AtomicU64,
+	clock: Clock,
 	sender: crossbeam::channel::Sender<SpanPhase>,
 	join_handle: Option<JoinHandle<()>>,
 }
@@ -41,7 +57,7 @@ impl AsyncProfiler {
 							let enter_span = spans.pop()
 								.expect("Shouldn't be possible to exit a span already exited");
 							assert_eq!(enter_span.id, exit_span.id, "Span ids not equal {:?},{:?}", enter_span, exit_span);
-							let time = (exit_span.time - enter_span.time).as_nanos();
+							let time = exit_span.time - enter_span.time;
 							if let Err(e) = writeln!(file, "{},{},{}",
 								   enter_span.target,
 								   enter_span.name,
@@ -55,6 +71,7 @@ impl AsyncProfiler {
 		}));
 		AsyncProfiler {
 			next_id: AtomicU64::new(1),
+			clock: Clock::new(),
 			sender,
 			join_handle,
 		}
@@ -76,7 +93,7 @@ impl Profiler for AsyncProfiler {
 			id,
 			target,
 			name,
-			time: Instant::now(),
+			time: self.clock.now(),
 		};
 		if let Err(e) = self.sender.send(SpanPhase::Enter(span_datum)) {
 			eprintln!("{}", e.to_string());
@@ -85,25 +102,11 @@ impl Profiler for AsyncProfiler {
 	}
 
 	fn exit_span(&self, id: u64) {
-		let time = Instant::now();
+		let time = self.clock.now();
 		if let Err(e) = self.sender.send(SpanPhase::Exit(ExitSpan { id, time })) {
 			eprintln!("{}", e.to_string());
 		};
 	}
-}
-
-#[derive(Debug)]
-struct EnterSpan {
-	id: u64,
-	name: String,
-	target: String,
-	time: Instant,
-}
-
-#[derive(Debug)]
-struct ExitSpan {
-	id: u64,
-	time: Instant,
 }
 
 /// Profiler that immediately emits results to stdout when
@@ -111,6 +114,7 @@ struct ExitSpan {
 /// `{target},{name},{time(ns)}`
 pub struct BasicProfiler {
 	next_id: AtomicU64,
+	clock: Clock,
 	span_data: Mutex<HashMap<u64, EnterSpan>>,
 }
 
@@ -118,6 +122,7 @@ impl BasicProfiler {
 	pub fn new() -> Self {
 		BasicProfiler {
 			next_id: AtomicU64::new(1),
+			clock: Clock::new(),
 			span_data: Mutex::new(HashMap::new()),
 		}
 	}
@@ -130,7 +135,7 @@ impl Profiler for BasicProfiler {
 			id,
 			target,
 			name,
-			time: Instant::now(),
+			time: self.clock.now(),
 		};
 		self.span_data.lock().insert(id, span_datum);
 		id
@@ -139,7 +144,7 @@ impl Profiler for BasicProfiler {
 	fn exit_span(&self, id: u64) {
 		let span_datum = self.span_data.lock().remove(&id)
 			.expect("You probably passed in the wrong id; the results are invalid");
-		let time = (Instant::now() - span_datum.time).as_nanos();
+		let time = self.clock.now() - span_datum.time;
 		println!("{},{},{}",
 				 span_datum.target,
 				 span_datum.name,
