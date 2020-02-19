0f1a9ba24054f66c9c4fbe836c543b413293da2b
thiolliere <gui.thiolliere@gmail.com>
2020-02-19 16:01:52 +0900

    fix test

--- frame/balances/src/tests_composite.rs
+++ frame/balances/src/tests_composite.rs
@@ -75,7 +75,9 @@ parameter_types! {
 }
 impl pallet_transaction_payment::Trait for Test {
 	type Currency = Module<Test>;
-	type OnTransactionPayment = ();
+	type OnTransactionFeePayment = ();
+	type OnTransactionTipPayment = ();
+	type Author = ();
 	type TransactionBaseFee = TransactionBaseFee;
 	type TransactionByteFee = TransactionByteFee;
 	type WeightToFee = ConvertInto;
