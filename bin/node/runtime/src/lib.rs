35f374d4a1a0c3e1fcd4de7e5cd0dba207c6f5c7
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2020-02-21 10:42:55 +0100

    Un-deprecate ValidateUnsigned (#5003)
    * Un-deprecate ValidateUnsigned.
    
    * Bump runtime.
    
    * Bump runtime.
    
    Co-authored-by: Shawn Tabrizi <shawntabrizi@gmail.com>

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -82,7 +82,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
 	spec_version: 224,
-	impl_version: 1,
+	impl_version: 2,
 	apis: RUNTIME_API_VERSIONS,
 };
 
