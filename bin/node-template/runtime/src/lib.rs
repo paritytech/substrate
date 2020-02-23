faafb421a84abd70362c134e579b9deea53bf4fe
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-02-23 23:26:10 +0100

    Use identity lookup instead

--- bin/node-template/runtime/src/lib.rs
+++ bin/node-template/runtime/src/lib.rs
@@ -15,7 +15,7 @@ use sp_runtime::{
 	impl_opaque_keys, MultiSignature,
 };
 use sp_runtime::traits::{
-	BlakeTwo256, Block as BlockT, StaticLookup, Verify, ConvertInto, IdentifyAccount
+	BlakeTwo256, Block as BlockT, IdentityLookup, Verify, ConvertInto, IdentifyAccount
 };
 use sp_api::impl_runtime_apis;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
@@ -132,7 +132,7 @@ impl system::Trait for Runtime {
 	/// The aggregated dispatch type that is available for extrinsics.
 	type Call = Call;
 	/// The lookup mechanism to get account ID from whatever is passed in dispatchers.
-	type Lookup = System;
+	type Lookup = IdentityLookup<AccountId>;
 	/// The index type for storing how many extrinsics an account has signed.
 	type Index = Index;
 	/// The index type for blocks.
@@ -246,7 +246,7 @@ construct_runtime!(
 );
 
 /// The address format for describing accounts.
-pub type Address = <System as StaticLookup>::Source;
+pub type Address = AccountId;
 /// Block header type as expected by this runtime.
 pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
 /// Block type as expected by this runtime.
--- frame/system/src/lib.rs
+++ frame/system/src/lib.rs
@@ -1408,19 +1408,6 @@ impl<T: Trait> Lookup for ChainContext<T> {
 	}
 }
 
-impl<T: Trait> StaticLookup for Module<T> {
-	type Source = T::AccountId;
-	type Target = T::AccountId;
-
-	fn lookup(a: Self::Source) -> Result<T::AccountId, LookupError> {
-		Ok(a)
-	}
-
-	fn unlookup(a: Self::Target) -> Self::Source {
-		a
-	}
-}
-
 #[cfg(test)]
 mod tests {
 	use super::*;
