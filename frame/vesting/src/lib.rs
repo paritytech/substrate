c53cae2a49cca73597d440e2a47f75480aff36f1
Logan Saether <x@logansaether.com>
2020-02-24 18:09:54 +0100

    Update frame/vesting/src/lib.rs
    Co-Authored-By: Shawn Tabrizi <shawntabrizi@gmail.com>

--- frame/vesting/src/lib.rs
+++ frame/vesting/src/lib.rs
@@ -695,7 +695,7 @@ mod tests {
 				System::set_block_number(20);
 				assert_eq!(System::block_number(), 20);
 
-				// Account 4 has 5 * 64 unites vested by block 20.
+				// Account 4 has 5 * 64 units vested by block 20.
 				assert_eq!(Vesting::vesting_balance(&4), Some(10 * 64));
 
 				System::set_block_number(30);
