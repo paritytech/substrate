1c500b783602e611f2ea384efff2c04d63ee49df
Seun <seunlanlege@gmail.com>
2020-02-24 12:33:35 +0100

    removes use of sc_client::Client from sc_finality_grandpa

--- client/finality-grandpa/src/tests.rs
+++ client/finality-grandpa/src/tests.rs
@@ -18,10 +18,7 @@
 
 use super::*;
 use environment::HasVoted;
-use sc_network_test::{
-	Block, Hash, TestNetFactory, BlockImportAdapter, Peer,
-	PeersClient, PassThroughVerifier,
-};
+use sc_network_test::{Block, Hash, TestNetFactory, BlockImportAdapter, Peer, PeersClient, PassThroughVerifier, PeersFullClient};
 use sc_network::config::{ProtocolConfig, Roles, BoxFinalityProofRequestBuilder};
 use parking_lot::Mutex;
 use futures_timer::Delay;
@@ -60,10 +57,8 @@ type PeerData =
 	Mutex<
 		Option<
 			LinkHalf<
-				substrate_test_runtime_client::Backend,
-				substrate_test_runtime_client::Executor,
 				Block,
-				substrate_test_runtime_client::runtime::RuntimeApi,
+				PeersFullClient,
 				LongestChain<substrate_test_runtime_client::Backend, Block>
 			>
 		>
@@ -1654,6 +1649,7 @@ fn grandpa_environment_respects_voting_rules() {
 			voters: Arc::new(authority_set.current_authorities()),
 			network,
 			voting_rule,
+			_phantom: PhantomData,
 		}
 	};
 
