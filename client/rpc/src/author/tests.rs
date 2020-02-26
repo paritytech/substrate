79b7e23ecc2a33a8b33f0cc9d7c03b8d7ed5f986
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2020-02-26 17:05:32 +0100

    Fix RPC author tests

--- client/rpc/src/author/tests.rs
+++ client/rpc/src/author/tests.rs
@@ -217,7 +217,7 @@ fn should_insert_key() {
 
 	let public_keys = setup.keystore.read().keys(ED25519).unwrap();
 
-	assert!(public_keys.contains(&(ed25519::CRYPTO_ID, key_pair.public().to_raw_vec())));
+	assert!(public_keys.contains(&CryptoTypePublicPair(ed25519::CRYPTO_ID, key_pair.public().to_raw_vec())));
 }
 
 #[test]
@@ -233,8 +233,8 @@ fn should_rotate_keys() {
 	let ed25519_public_keys = setup.keystore.read().keys(ED25519).unwrap();
 	let sr25519_public_keys = setup.keystore.read().keys(SR25519).unwrap();
 
-	assert!(ed25519_public_keys.contains(&(ed25519::CRYPTO_ID, session_keys.ed25519.to_raw_vec())));
-	assert!(sr25519_public_keys.contains(&(sr25519::CRYPTO_ID, session_keys.sr25519.to_raw_vec())));
+	assert!(ed25519_public_keys.contains(&CryptoTypePublicPair(ed25519::CRYPTO_ID, session_keys.ed25519.to_raw_vec())));
+	assert!(sr25519_public_keys.contains(&CryptoTypePublicPair(sr25519::CRYPTO_ID, session_keys.sr25519.to_raw_vec())));
 }
 
 #[test]
