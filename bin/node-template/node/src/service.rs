e46240b522b09b2588d853289ae23862ffc73ce5
Ashley <ashley.ruglys@gmail.com>
2020-03-03 11:36:58 +0100

    Add more metrics to prometheus (#5034)
    * Add a few things
    
    * Add finality_grandpa_round
    
    * fix fg tests
    
    * Nitpicks
    
    * Nitpicks
    
    * Fix name of prometheus crate

--- bin/node-template/node/src/service.rs
+++ bin/node-template/node/src/service.rs
@@ -157,6 +157,7 @@ pub fn new_full(config: Configuration<GenesisConfig>)
 			on_exit: service.on_exit(),
 			telemetry_on_connect: Some(service.telemetry_on_connect_stream()),
 			voting_rule: grandpa::VotingRulesBuilder::default().build(),
+			prometheus_registry: service.prometheus_registry()
 		};
 
 		// the GRANDPA voter task is considered infallible, i.e.
