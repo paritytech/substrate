0e1ff5899add7d32274566c0a9349243d65b10ee
Gavin Wood <gavin@parity.io>
2020-03-05 13:37:59 +0100

    Adds a session getter to historical proofs (#5125)
    * Adds a session getter to historical proofs
    
    * Bump spec_version
    
    * Adds some useful trait derives to Proof

--- frame/session/src/historical.rs
+++ frame/session/src/historical.rs
@@ -27,7 +27,7 @@
 
 use sp_std::prelude::*;
 use codec::{Encode, Decode};
-use sp_runtime::KeyTypeId;
+use sp_runtime::{KeyTypeId, RuntimeDebug};
 use sp_runtime::traits::{Convert, OpaqueKeys};
 use frame_support::{decl_module, decl_storage};
 use frame_support::{Parameter, print};
@@ -258,10 +258,17 @@ impl<T: Trait> ProvingTrie<T> {
 }
 
 /// Proof of ownership of a specific key.
-#[derive(Encode, Decode, Clone)]
+#[derive(Encode, Decode, Clone, Eq, PartialEq, RuntimeDebug)]
 pub struct Proof {
 	session: SessionIndex,
 	trie_nodes: Vec<Vec<u8>>,
+}
+
+impl Proof {
+	/// Returns a session this proof was generated for.
+	pub fn session(&self) -> SessionIndex {
+		self.session
+	}
 }
 
 impl<T: Trait, D: AsRef<[u8]>> frame_support::traits::KeyOwnerProofSystem<(KeyTypeId, D)>
