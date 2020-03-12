a60a5da603f37aa05ff70ca4172d4de1fb3c4289
Scott Piriou <scott@parity.io>
2020-03-12 18:13:14 +0100

    Update try_borrow_mut's error logic

--- client/db/src/bench.rs
+++ client/db/src/bench.rs
@@ -137,9 +137,10 @@ impl<B: BlockT> StateBackend<HashFor<B>> for BenchmarkingState<B> {
 	fn storage(&self, key: &[u8]) -> Result<Option<Vec<u8>>, Self::Error> {
 		let state = self.state.borrow_mut();
 		let toto = state.as_ref().ok_or_else(state_err)?;
-		let mut usage_info = self.usage_info.try_borrow_mut().map_err(|_| "salut".to_string())?;
-		usage_info.reads.ops += 1;
-		usage_info.reads.bytes += 1;
+		self.usage_info.try_borrow_mut().map(|usage_info| {
+			usage_info.reads.ops += 1;
+			usage_info.reads.bytes += 1;
+		});
 		toto.storage(key)
 	}
 
@@ -271,9 +272,10 @@ impl<B: BlockT> StateBackend<HashFor<B>> for BenchmarkingState<B> {
 				}
 			}
 			db.write(db_transaction).map_err(|_| String::from("Error committing transaction"))?;
-			let mut usage_info = self.usage_info.try_borrow_mut().map_err(|_| "salut".to_string())?;
-			usage_info.writes.ops += 1;
-			usage_info.writes.bytes += 1;
+			self.usage_info.try_borrow_mut().map(|usage_info| {
+				usage_info.writes.ops += 1;
+				usage_info.writes.bytes += 1;
+			});
 			self.root.set(storage_root);
 		} else {
 			return Err("Trying to commit to a closed db".into())
