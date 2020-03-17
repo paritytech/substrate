7b2a46fa1065d9a23c32813f7447ec3d923fe8eb
Cecile Tonglet <cecile@parity.io>
2020-03-17 09:55:23 +0100

    WIP
    Forked at: 2afecf81ee19b8a6edb364b419190ea47c4a4a31
    Parent branch: origin/master

--- client/cli-derive/examples/basic.rs
+++ client/cli-derive/examples/basic.rs
@@ -1,12 +1,20 @@
 use sc_cli_derive::spec_factory;
-//use sc_service::{ChainSpec, RuntimeGenesis};
 
-struct ChainSpec<G, E = ()>;
-struct GenesisConfig;
-struct Extension;
+mod sc_service {
+	pub struct ChainSpec<G, E = ()>;
+	pub trait SubstrateCli<G, E = ()> {
+		fn load_spec(id: &str) -> Result<Option<ChainSpec<G, E>>, String>;
+		fn get_impl_name() -> &'static str;
+		fn get_impl_version() -> &'static str;
+	}
+}
 
-#[spec_factory]
-fn load_spec(id: &str) -> Result<Option<ChainSpec<GenesisConfig>>, String> {
+struct MyGenesisConfig;
+struct MyExtension;
+struct MyCli;
+
+#[spec_factory(cli = MyCli)]
+fn load_spec(id: &str) -> Result<Option<sc_service::ChainSpec<MyGenesisConfig>>, String> {
 	Ok(None)
 }
 
--- client/cli-derive/src/lib.rs
+++ client/cli-derive/src/lib.rs
@@ -1,24 +1,56 @@
-#![allow(unused_imports, unused_variables)]
-
 extern crate proc_macro;
 
-use proc_macro2::{Span, TokenStream};
-use proc_macro_error::{abort, abort_call_site, proc_macro_error, set_dummy};
-use quote::{quote, quote_spanned};
+use proc_macro2::{Span};
+use proc_macro_error::{abort, abort_call_site, proc_macro_error};
+use quote::{quote, ToTokens};
+use std::collections::HashMap;
 use syn;
-use syn::parse::Parse;
-use syn::parse::ParseStream;
-use syn::parse_macro_input;
-use syn::{parse_quote, Token};
+use syn::parse::Parser;
 use syn::{punctuated::Punctuated, spanned::Spanned, token::Comma, *};
 
 #[proc_macro_attribute]
 #[proc_macro_error]
 pub fn spec_factory(
-	_a: proc_macro::TokenStream,
+	a: proc_macro::TokenStream,
 	i: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-	let s: syn::ItemFn = match syn::parse(i) {
+	let attrs = match Punctuated::<ExprAssign, Token![,]>::parse_terminated.parse(a) {
+		Ok(x) => x,
+		Err(err) => abort_call_site!("could not parse attributes: {}", err),
+	};
+
+	let attrs = attrs
+		.iter()
+		.map(|x| {
+			(
+				match &*x.left {
+					Expr::Path(p) if p.path.segments.len() == 1 => {
+						p.path.segments[0].ident.to_string()
+					}
+					_ => abort!(x.left.span(), "could not parse token"),
+				},
+				(*x.right).to_token_stream(),
+			)
+		})
+		.collect::<HashMap<_, _>>();
+
+	let default_cli = quote! { Cli };
+	let cli = attrs.get("cli").unwrap_or(&default_cli);
+	let default_impl_name = std::env::var("CARGO_PKG_NAME").unwrap().to_token_stream();
+	let impl_name = attrs.get("impl_name").unwrap_or(&default_impl_name);
+	let impl_commit = std::env::var("SUBSTRATE_CLI_COMMIT").unwrap_or_default();
+	let commit_dash = if impl_commit.is_empty() { "" } else { "-" };
+	let default_impl_version = format!(
+		"{}{}{}-{}",
+		std::env::var("CARGO_PKG_VERSION").unwrap_or_default(),
+		commit_dash,
+		impl_commit,
+		std::env::var("SUBSTRATE_CLI_PLATFORM").unwrap_or_default(),
+	)
+	.to_token_stream();
+	let impl_version = attrs.get("impl_version").unwrap_or(&default_impl_version);
+
+	let s: ItemFn = match syn::parse(i) {
 		Ok(x) => x,
 		_ => abort_call_site!("this macro only works on a function"),
 	};
@@ -37,7 +69,10 @@ pub fn spec_factory(
 			})
 			.next()
 			.unwrap(),
-		Err(span) => abort!(span, "expected: Result<Option<ChainSpec<_, _>>, String>"),
+		Err(span) => abort!(
+			span,
+			"expected: Result<Option<sc_service::ChainSpec<_, _>>, String>"
+		),
 	};
 
 	let output_option = match get_type_arguments(output_ok, "Option") {
@@ -49,17 +84,17 @@ pub fn spec_factory(
 			})
 			.next()
 			.unwrap(),
-		Err(span) => abort!(span, "expected: Option<ChainSpec<_, _>>"),
+		Err(span) => abort!(span, "expected: Option<sc_service::ChainSpec<_, _>>"),
 	};
 
 	let output_chain_spec = match get_type_arguments(output_option, "ChainSpec") {
 		Ok(x) => x,
-		Err(span) => abort!(span, "expected: ChainSpec<_, _>"),
+		Err(span) => abort!(span, "expected: sc_service::ChainSpec<_, _>"),
 	};
 
 	let type_args = output_chain_spec
 		.iter()
-		.map(|x| match x {
+		.filter_map(|x| match x {
 			GenericArgument::Type(Type::Path(ty_path)) => Some(ty_path),
 			_ => None,
 		})
@@ -68,7 +103,7 @@ pub fn spec_factory(
 	if type_args.is_empty() {
 		abort!(
 			output_chain_spec.span(),
-			"could not find the G type argument of ChainSpec"
+			"could not find the G type argument of sc_service::ChainSpec"
 		);
 	}
 
@@ -76,8 +111,12 @@ pub fn spec_factory(
 	let extension = type_args.get(1);
 
 	let gen = quote! {
-		impl sc_service::SubstrateCli<#runtime_genesis, #extension> for Cli {
+		impl sc_service::SubstrateCli<#runtime_genesis, #extension> for #cli {
 			#s
+
+			fn get_impl_name() -> &'static str { #impl_name }
+
+			fn get_impl_version() -> &'static str { #impl_version }
 		}
 	};
 	gen.into()
