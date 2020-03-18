38d64a5761f35bc25d54f57c84d9eb5b77eee0bf
Cecile Tonglet <cecile@parity.io>
2020-03-18 09:45:27 +0100

    Fixed sc-cli-derive example

--- client/cli-derive/examples/basic.rs
+++ client/cli-derive/examples/basic.rs
@@ -1,30 +1,24 @@
+#![allow(unused_variables, dead_code)]
+
 use sc_cli_derive::spec_factory;
+use serde::{Deserialize, Serialize};
+use sp_runtime::BuildStorage;
+use sp_storage::Storage;
 
-mod sc_service {
-	pub struct ChainSpec<G, E = ()>;
-}
-
-mod sc_cli {
-	pub trait SubstrateCLI<G, E = ()> {
-		fn load_spec(id: &str) -> Result<Option<ChainSpec<G, E>>, String>;
-		fn get_impl_name() -> &'static str;
-		fn get_impl_version() -> &'static str;
-		fn get_support_url() -> &'static str;
-		fn get_executable_name() -> &'static str;
-		fn get_author() -> &'static str;
-		fn get_description() -> &'static str;
-		fn get_copyright_start_year() -> i32;
-	}
-}
-
+#[derive(Serialize, Deserialize)]
 struct MyGenesisConfig;
 struct MyExtension;
 struct MyCli;
 
-#[spec_factory(cli = MyCli)]
-fn load_spec(id: &str) -> Result<Option<sc_service::ChainSpec<MyGenesisConfig>>, String> {
+impl BuildStorage for MyGenesisConfig {
+	fn assimilate_storage(&self, _: &mut Storage) -> Result<(), String> {
+		unimplemented!()
+	}
+}
+
+#[spec_factory(cli = MyCli, support_url = "http://example.org", copyright_start_year = 2020)]
+fn spec_factory(id: &str) -> Result<Option<sc_service::ChainSpec<MyGenesisConfig>>, String> {
 	Ok(None)
 }
 
-fn main() {
-}
+fn main() {}
