09b64502b3a73b26eec810e8c1b84e2f9baeee35
Cecile Tonglet <cecile@parity.io>
2020-03-18 15:36:30 +0100

    Fixing stuff

--- bin/node/cli/src/browser.rs
+++ bin/node/cli/src/browser.rs
@@ -42,10 +42,10 @@ async fn start_inner(chain_spec: String, log_level: String, wasm_ext: Transport)
 		.await?;
 
 	info!("Substrate browser node");
-	info!("  version {}", config.full_version());
+	info!("  version {}", config.impl_version);
 	info!("  by Parity Technologies, 2017-2020");
-	info!("Chain specification: {}", config.expect_chain_spec().name());
-	info!("Node name: {}", config.name);
+	info!("Chain specification: {}", config.chain_spec.name());
+	info!("Node name: {}", config.network.node_name);
 	info!("Roles: {:?}", config.roles);
 
 	// Create the service. This is the most heavy initialization step.
