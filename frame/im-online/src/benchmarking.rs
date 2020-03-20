8ca3864a8c9f7728406c54f9aa545de0b20a2f83
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-03-20 08:38:16 +0100

    Update frame/im-online/src/benchmarking.rs
    Co-Authored-By: Marcio Diaz <marcio.diaz@gmail.com>

--- frame/im-online/src/benchmarking.rs
+++ frame/im-online/src/benchmarking.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Balances pallet benchmarking.
+//! I'm Online pallet benchmarking.
 
 #![cfg(feature = "runtime-benchmarks")]
 
