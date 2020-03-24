93016476691ec08a053f17dde6e6966033dc9f30
NikVolf <nikvolf@gmail.com>
2020-03-24 23:05:45 +0300

    fix test

--- client/transaction-pool/graph/src/base_pool.rs
+++ client/transaction-pool/graph/src/base_pool.rs
@@ -1079,7 +1079,7 @@ mod tests {
 				source: Source::External,
 			}),
 			"Transaction { \
-hash: 4, priority: 1000, valid_till: 64, bytes: 1, propagate: true, source: External, \
+hash: 4, priority: 1000, valid_till: 64, bytes: 1, propagate: true, source: TransactionSource::External, \
 requires: [03,02], provides: [04], data: [4]}".to_owned()
 		);
 	}
