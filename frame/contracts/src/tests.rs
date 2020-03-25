05b03e8ca0b41303b794b0115b9d7f0e6aace7f9
Alexander Theißen <alexander.theissen@parity.io>
2020-03-25 12:21:28 +0100

    contracts: Remove OnKilledAccount implementation
    Contracts now longer rely on this callback to tell them when they
    are removed. Instead, they can only self destruct  using `ext_terminate`.

--- frame/contracts/src/tests.rs
+++ frame/contracts/src/tests.rs
@@ -117,7 +117,7 @@ impl frame_system::Trait for Test {
 	type ModuleToIndex = ();
 	type AccountData = pallet_balances::AccountData<u64>;
 	type OnNewAccount = ();
-	type OnKilledAccount = Contracts;
+	type OnKilledAccount = ();
 }
 impl pallet_balances::Trait for Test {
 	type Balance = u64;
