a62448b95bb07536ea485dffd74b3f147a80b73e
Cecile Tonglet <cecile@parity.io>
2020-03-25 14:52:13 +0100

    Fix example

--- client/cli-derive/examples/basic.rs
+++ client/cli-derive/examples/basic.rs
@@ -1,6 +1,6 @@
 #![allow(unused_variables, dead_code)]
 
-use sc_cli::{CliConfiguration, ImportParams, KeystoreParams, SharedParams};
+use sc_cli::{CliConfiguration, ImportParams, KeystoreParams, SharedParams, SubstrateCLI};
 use sc_cli_derive::{spec_factory, substrate_cli_params};
 
 struct MyCli {
@@ -10,13 +10,14 @@ struct MyCli {
 }
 
 #[spec_factory(
-	cli = MyCli,
 	support_url = "http://example.org",
 	copyright_start_year = 2020,
 	impl_version = "0.1.0",
 )]
-fn spec_factory(id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
-	Err("not implemented".into())
+impl SubstrateCLI for MyCli {
+	fn spec_factory(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
+		Err("not implemented".into())
+	}
 }
 
 #[substrate_cli_params(shared_params = shared, import_params = import, keystore_params = keystore)]
