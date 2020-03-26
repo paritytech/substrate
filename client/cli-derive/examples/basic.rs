38db0bf41c7866f07deba46675ba7f6886e35f75
Cecile Tonglet <cecile@parity.io>
2020-03-26 10:07:37 +0100

    Fixed example & test of cli-derive

--- client/cli-derive/examples/basic.rs
+++ client/cli-derive/examples/basic.rs
@@ -1,7 +1,7 @@
 #![allow(unused_variables, dead_code)]
 
 use sc_cli::{CliConfiguration, ImportParams, KeystoreParams, SharedParams, SubstrateCli};
-use sc_cli_derive::{load_spec, substrate_cli_params};
+use sc_cli_derive::{substrate_cli_configuration, substrate_cli_params};
 
 struct MyCli {
 	shared: SharedParams,
@@ -9,7 +9,7 @@ struct MyCli {
 	keystore: KeystoreParams,
 }
 
-#[load_spec(
+#[substrate_cli_configuration(
 	support_url = "http://example.org",
 	copyright_start_year = 2020,
 	impl_version = "0.1.0",
