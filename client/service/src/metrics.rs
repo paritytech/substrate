419208f31e07bd19b485582da08f7b6a6818474b
Benjamin Kampmann <ben@parity.io>
2020-03-26 14:18:09 +0100

    Adding netstat tcp connection info to prometheus

--- client/service/src/metrics.rs
+++ client/service/src/metrics.rs
@@ -6,11 +6,13 @@ use sc_client::ClientInfo;
 use crate::NetworkStatus;
 use sp_transaction_pool::PoolStatus;
 use sp_runtime::traits::{NumberFor, Block, SaturatedConversion, UniqueSaturatedInto};
+use netstat2::{TcpState, ProtocolSocketInfo, iterate_sockets_info, AddressFamilyFlags, ProtocolFlags};
 
 struct PrometheusMetrics {
 	block_height_number: GaugeVec<U64>,
 	ready_transactions_number: Gauge<U64>,
 	memory_usage_bytes: Gauge<U64>,
+	netstat: GaugeVec<U64>,
 	cpu_usage_percentage: Gauge<F64>,
 	network_per_sec_bytes: GaugeVec<U64>,
 	database_cache: Gauge<U64>,
@@ -29,10 +31,12 @@ impl PrometheusMetrics {
             )
                 .const_label("name", name)
                 .const_label("version", version)
-        )?, &registry)?.set(1);
+		)?, &registry)?.set(1);
+		
         register(Gauge::<U64>::new(
             "node_roles", "The roles the node is running as",
-        )?, &registry)?.set(roles);
+		)?, &registry)?.set(roles);
+		
 		Ok(Self {
 			block_height_number: register(GaugeVec::new(
 				Opts::new("block_height_number", "Height of the chain"),
@@ -61,6 +65,10 @@ impl PrometheusMetrics {
 				Opts::new("state_db_cache_bytes", "State DB cache in bytes"),
 				&["subtype"]
 			)?, registry)?,
+			netstat: register(GaugeVec::new(
+				Opts::new("netstat_tcp", "Current TCP connections "),
+				&["status"]
+			)?, registry)?,
 		})
 	}
 }
@@ -69,6 +77,16 @@ pub struct MetricsService {
 	metrics: Option<PrometheusMetrics>,
 	system: sysinfo::System,
 	pid: Option<sysinfo::Pid>,
+}
+
+#[derive(Default)]
+pub struct ConnectionsCount {
+	listen: u64,
+	established: u64,
+	starting: u64,
+	closing: u64,
+	closed: u64,
+	other: u64
 }
 
 impl MetricsService {
@@ -96,6 +114,52 @@ impl MetricsService {
 		}
 	}
 
+	fn process_info(&mut self) -> (f32, u64) {
+		match self.pid {
+			Some(pid) => if self.system.refresh_process(pid) {
+					let proc = self.system.get_process(pid)
+						.expect("Above refresh_process succeeds, this must be Some(), qed");
+					(proc.cpu_usage(), proc.memory())
+				} else { (0.0, 0) },
+			None => (0.0, 0)
+		}
+	}
+
+	fn connections_info(&self) -> Option<ConnectionsCount> {
+		self.pid.as_ref().and_then(|pid| {
+			let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+			let proto_flags = ProtocolFlags::TCP;
+			let netstat_pid = *pid as u32;
+
+			iterate_sockets_info(af_flags, proto_flags).ok().map(|iter|
+				iter.filter_map(|r| 
+					r.ok().and_then(|s| {
+						if s.associated_pids.contains(&netstat_pid) {
+							match s.protocol_socket_info {
+								ProtocolSocketInfo::Tcp(info) => Some(info.state),
+								_ => None
+							}
+						} else {
+							None
+						}
+					})
+				).fold(ConnectionsCount::default(), |mut counter, socket_state| {
+					match socket_state {
+						TcpState::Listen => counter.listen += 1,
+						TcpState::Established => counter.established += 1,
+						TcpState::Closed => counter.closed += 1,
+						TcpState::SynSent | TcpState::SynReceived => counter.starting += 1,
+						TcpState::FinWait1 | TcpState::FinWait2 | TcpState::CloseWait
+						| TcpState::Closing | TcpState::LastAck => counter.closing += 1,
+						_ => counter.other += 1
+					}
+
+					counter
+				})
+			)
+		})
+	}
+
 	pub fn tick<T: Block>(&mut self, info: &ClientInfo<T>, txpool_status: &PoolStatus, net_status: &NetworkStatus<T>) {
 
 		let best_number = info.chain.best_number.saturated_into::<u64>();
@@ -106,16 +170,8 @@ impl MetricsService {
 		let bandwidth_upload = net_status.average_upload_per_sec;
 		let best_seen_block = net_status.best_seen_block
 			.map(|num: NumberFor<T>| num.unique_saturated_into() as u64);
+		let (cpu_usage, memory) = self.process_info();
 
-		// get cpu usage and memory usage of this process
-		let (cpu_usage, memory) = match self.pid {
-			Some(pid) => if self.system.refresh_process(pid) {
-					let proc = self.system.get_process(pid)
-						.expect("Above refresh_process succeeds, this must be Some(), qed");
-					(proc.cpu_usage(), proc.memory())
-				} else { (0.0, 0) },
-			None => (0.0, 0)
-		};
 
 		telemetry!(
 			SUBSTRATE_INFO;
@@ -145,7 +201,7 @@ impl MetricsService {
 		);
 
 		if let Some(metrics) = self.metrics.as_ref() {
-			metrics.cpu_usage_percentage.set(cpu_usage.into());
+			metrics.cpu_usage_percentage.set(cpu_usage as f64);
 			metrics.memory_usage_bytes.set(memory);
 			metrics.ready_transactions_number.set(txpool_status.ready as u64);
 
@@ -168,6 +224,15 @@ impl MetricsService {
 					metrics.state_db.with_label_values(&["pruning"]).set(pruning.as_bytes() as u64);
 				}
 				metrics.state_db.with_label_values(&["pinned"]).set(info.memory.state_db.pinned.as_bytes() as u64);
+			}
+
+			if let Some(conns) = self.connections_info() {
+				metrics.netstat.with_label_values(&["tcp_listen"]).set(conns.listen);
+				metrics.netstat.with_label_values(&["tcp_established"]).set(conns.established);
+				metrics.netstat.with_label_values(&["tcp_starting"]).set(conns.starting);
+				metrics.netstat.with_label_values(&["tcp_closing"]).set(conns.closing);
+				metrics.netstat.with_label_values(&["tcp_closed"]).set(conns.closed);
+				metrics.netstat.with_label_values(&["tcp_other"]).set(conns.other);
 			}
 		}
 
