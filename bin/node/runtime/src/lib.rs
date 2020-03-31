678150f822113f9a9851c551f67da2e635eed680
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-03-31 22:53:21 +0200

    Benchmark Utility Pallet (#5384)
    * Move tests
    
    * Move around feature cfgs
    
    * Start benchmarks
    
    * as_multi bench
    
    * finish
    
    * fix tests
    
    * Update frame/utility/src/benchmarking.rs
    
    Co-Authored-By: Marcio Diaz <marcio.diaz@gmail.com>
    
    * Update Cargo.toml
    
    * fix test
    
    Co-authored-by: Marcio Diaz <marcio.diaz@gmail.com>

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -878,15 +878,16 @@ impl_runtime_apis! {
 			let mut batches = Vec::<BenchmarkBatch>::new();
 			let params = (&pallet, &benchmark, &lowest_range_values, &highest_range_values, &steps, repeat);
 			add_benchmark!(params, batches, b"balances", Balances);
-			add_benchmark!(params, batches, b"im-online", ImOnline);
+			add_benchmark!(params, batches, b"collective", Council);
+			add_benchmark!(params, batches, b"democracy", Democracy);
 			add_benchmark!(params, batches, b"identity", Identity);
+			add_benchmark!(params, batches, b"im-online", ImOnline);
 			add_benchmark!(params, batches, b"session", SessionBench::<Runtime>);
 			add_benchmark!(params, batches, b"staking", Staking);
 			add_benchmark!(params, batches, b"timestamp", Timestamp);
 			add_benchmark!(params, batches, b"treasury", Treasury);
+			add_benchmark!(params, batches, b"utility", Utility);
 			add_benchmark!(params, batches, b"vesting", Vesting);
-			add_benchmark!(params, batches, b"democracy", Democracy);
-			add_benchmark!(params, batches, b"collective", Council);
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
 		}
