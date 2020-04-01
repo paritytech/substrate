e7cc96db78395e1532a5cc9519c4dfd93644598c
HackFisher <hackfisher@gmail.com>
2020-04-01 13:10:09 +0800

    bump runtime version

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -128,8 +128,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 240,
-	impl_version: 1,
+	spec_version: 241,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
 
