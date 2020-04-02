88b4c30528634ad9b70dcf475fc2acdef5a8c70b
marcio-diaz <marcio.diaz@gmail.com>
2020-04-02 13:27:31 +0200

    Apply review suggestion: move benchmark to own crate.

--- frame/offences/benchmarking/src/lib.rs
+++ frame/offences/benchmarking/src/lib.rs
@@ -16,32 +16,51 @@
 
 //! Offences pallet benchmarking.
 
-use super::*;
+#![cfg_attr(not(feature = "std"), no_std)]
 
 use sp_std::prelude::*;
+use sp_std::vec;
+
+use frame_system::RawOrigin;
 use frame_benchmarking::{benchmarks, account};
 use frame_support::traits::Currency;
-use frame_system::RawOrigin;
+
 use sp_runtime::{Perbill, traits::{Convert, StaticLookup}};
-use pallet_im_online::UnresponsivenessOffence;
-use pallet_staking::{Module as Staking, RewardDestination, ValidatorPrefs};
-use pallet_session::historical::IdentificationTuple;
+use sp_staking::offence::ReportOffence;
+
+use pallet_im_online::{Trait as ImOnlineTrait, UnresponsivenessOffence};
+use pallet_offences::{Trait as OffencesTrait, Module as OffencesModule};
+use pallet_staking::{
+	Module as Staking, Trait as StakingTrait, RewardDestination, ValidatorPrefs,
+	Exposure, IndividualExposure,
+};
+use pallet_session::Trait as SessionTrait;
+use pallet_session::historical::{Trait as HistoricalTrait, IdentificationTuple};
 
 const SEED: u32 = 0;
+
 const MAX_REPORTERS: u32 = 1000;
 const MAX_OFFENDERS: u32 = 1000;
 const MAX_NOMINATORS: u32 = 1000;
 
-pub trait Trait: pallet_staking::Trait + crate::Trait + pallet_im_online::Trait {}
+pub struct Module<T: Trait>(OffencesModule<T>);
 
-fn create_offender<T: frame_system::Trait + pallet_session::Trait + pallet_staking::Trait>(n: u32, nominators: u32) -> Result<T::AccountId, &'static str> {
-	let stash: T::AccountId = account("stash", n, 0);
-	let controller: T::AccountId = account("controller", n, 0);
+pub trait Trait: SessionTrait + StakingTrait + OffencesTrait + ImOnlineTrait {}
+
+fn create_offender<T: Trait>(n: u32, nominators: u32) -> Result<T::AccountId, &'static str> {
+	let stash: T::AccountId = account("stash", n, SEED);
+	let controller: T::AccountId = account("controller", n, SEED);
 	let controller_lookup: <T::Lookup as StaticLookup>::Source = T::Lookup::unlookup(controller.clone());
 	let reward_destination = RewardDestination::Staked;
-	let amount = T::Currency::minimum_balance() * 10.into();
+	let amount = T::Currency::minimum_balance();
 
-	Staking::<T>::bond(RawOrigin::Signed(stash.clone()).into(), controller_lookup.clone(), amount.clone(), reward_destination.clone())?;
+	Staking::<T>::bond(
+		RawOrigin::Signed(stash.clone()).into(),
+		controller_lookup.clone(),
+		amount.clone(),
+		reward_destination.clone(),
+	)?;
+
 	let validator_prefs = ValidatorPrefs {
 		commission: Perbill::from_percent(50),
 	};
@@ -50,29 +69,34 @@ fn create_offender<T: frame_system::Trait + pallet_session::Trait + pallet_staki
 	let mut individual_exposures = vec![];
 
 	// Create n nominators
-	for j in 0 .. nominators {
-		let nominator_stash: T::AccountId = account("nominator stash", n * MAX_NOMINATORS + j, 0);
-		let nominator_controller: T::AccountId = account("nominator controller", n * MAX_NOMINATORS + j, 0);
+	for i in 0 .. nominators {
+		let nominator_stash: T::AccountId = account("nominator stash", n * MAX_NOMINATORS + i, SEED);
+		let nominator_controller: T::AccountId = account("nominator controller", n * MAX_NOMINATORS + i, SEED);
 		let nominator_controller_lookup: <T::Lookup as StaticLookup>::Source = T::Lookup::unlookup(nominator_controller.clone());
 
-		Staking::<T>::bond(RawOrigin::Signed(nominator_stash.clone()).into(), nominator_controller_lookup.clone(), amount, reward_destination)?;
+		Staking::<T>::bond(
+			RawOrigin::Signed(nominator_stash.clone()).into(),
+			nominator_controller_lookup.clone(),
+			amount,
+			reward_destination,
+		)?;
 
 		let selected_validators: Vec<<T::Lookup as StaticLookup>::Source> = vec![controller_lookup.clone()];
 		Staking::<T>::nominate(RawOrigin::Signed(nominator_controller.clone()).into(), selected_validators)?;
 
-		individual_exposures.push(pallet_staking::IndividualExposure {
+		individual_exposures.push(IndividualExposure {
 			who: nominator_controller.clone(),
 			value: amount.clone(),
 		});
 	}
 
-	let exposure = pallet_staking::Exposure {
+	let exposure = Exposure {
 		total: amount.clone() * n.into(),
-		own: amount.clone(),
+		own: amount,
 		others: individual_exposures,
 	};
-
-	Staking::<T>::add_era_stakers(0u32.into(), stash.clone().into(), exposure);
+	let current_era = 0u32;
+	Staking::<T>::add_era_stakers(current_era.into(), stash.clone().into(), exposure);
 
 	return Ok(controller)
 }
@@ -96,10 +120,10 @@ fn make_inputs<T: Trait>(r: u32, o: u32, n: u32)
 
 	let offenders = offenders.iter()
 		.map(|id|
-			<T as pallet_session::Trait>::ValidatorIdOf::convert(id.clone())
+			<T as SessionTrait>::ValidatorIdOf::convert(id.clone())
 				.expect("failed to get validator id from account id"))
 		.map(|validator_id|
-			<T as pallet_session::historical::Trait>::FullIdentificationOf::convert(validator_id.clone())
+			<T as HistoricalTrait>::FullIdentificationOf::convert(validator_id.clone())
 			.map(|full_id| (validator_id, full_id))
 			.expect("failed to convert validator id to full identification"))
 		.collect::<Vec<IdentificationTuple<T>>>();
@@ -128,6 +152,6 @@ benchmarks! {
 
 		let (reporters, offence) = make_inputs::<T>(r, o, n)?;
 	}: {
-		let _ = <T as pallet_im_online::Trait>::ReportUnresponsiveness::report_offence(reporters, offence);
+		let _ = <T as ImOnlineTrait>::ReportUnresponsiveness::report_offence(reporters, offence);
 	}
 }
