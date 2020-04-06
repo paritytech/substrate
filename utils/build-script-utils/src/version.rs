993b86c769f43b12bfc2e558609b01f546936fa4
Cecile Tonglet <cecile@parity.io>
2020-04-06 16:29:08 +0200

    Print error

--- utils/build-script-utils/src/version.rs
+++ utils/build-script-utils/src/version.rs
@@ -30,7 +30,7 @@ pub fn generate_cargo_keys() {
 			println!("cargo:rustc-env=SUBSTRATE_CLI_IMPL_VERSION={}", get_version(sha.as_str()))
 		}
 		Ok(o) => eprintln!("cargo:warning=Git command failed with status: {}", o.status),
-		_ => eprintln!("cargo:warning=Failed to execute git command"),
+		Err(err) => eprintln!("cargo:warning=Failed to execute git command: {}", err),
 	}
 }
 
