20ef4d78c8b6e7ad9ed57ca9162fcb7d0acd8b85
kianenigma <kian.peymani@gmail.com>
2020-04-06 12:31:20 +0200

    Fix build

--- bin/node/cli/src/factory_impl.rs
+++ bin/node/cli/src/factory_impl.rs
@@ -122,7 +122,7 @@ impl RuntimeAdapter for FactoryState<Number> {
 					(*amount).into()
 				)
 			)
-		}, key, (version, genesis_hash.clone(), prior_block_hash.clone(), (), (), (), (), ()))
+		}, key, (version, genesis_hash.clone(), prior_block_hash.clone(), (), (), (), ()))
 	}
 
 	fn inherent_extrinsics(&self) -> InherentData {
