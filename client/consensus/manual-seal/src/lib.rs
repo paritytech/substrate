9d282532039d89d3c39455513d2c248df677d4c8
Joshy Orndorff <admin@joshyorndorff.com>
2020-04-07 07:51:26 -0400

    Don't auto-finalize on verification

--- client/consensus/manual-seal/src/lib.rs
+++ client/consensus/manual-seal/src/lib.rs
@@ -58,7 +58,6 @@ impl<B: BlockT> Verifier<B> for ManualSealVerifier {
 		let mut import_params = BlockImportParams::new(origin, header);
 		import_params.justification = justification;
 		import_params.body = body;
-		import_params.finalized = true;
 		import_params.fork_choice = Some(ForkChoiceStrategy::LongestChain);
 
 		Ok((import_params, None))
