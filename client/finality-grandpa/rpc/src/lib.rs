04fca7e1624b25147b36460bc5ff012e21a2c2ba
Jon Häggblad <jon.haggblad@gmail.com>
2020-04-08 09:42:28 +0200

    grandpa: return background rounds too

--- client/finality-grandpa/rpc/src/lib.rs
+++ client/finality-grandpa/rpc/src/lib.rs
@@ -16,166 +16,181 @@
 
 //! RPC API for GRANDPA.
 
-use futures::{FutureExt, TryFutureExt};
-use jsonrpc_derive::rpc;
-use jsonrpc_core::Error;
-use sc_finality_grandpa::{SharedVoterState, SharedAuthoritySet, AuthorityId, voter};
 use finality_grandpa::BlockNumberOps;
-use serde::{Serialize, Deserialize};
+use sc_finality_grandpa::{voter, AuthorityId, SharedAuthoritySet, SharedVoterState};
+
+use futures::{FutureExt, TryFutureExt};
+use jsonrpc_core::Error;
+use jsonrpc_derive::rpc;
+use log::warn;
+use serde::{Deserialize, Serialize};
 use std::{collections::HashSet, fmt::Debug};
 
 type FutureResult<T> = Box<dyn jsonrpc_core::futures::Future<Item = T, Error = Error> + Send>;
 
 #[rpc]
 pub trait GrandpaApi {
-	#[rpc(name = "grandpa_roundState")]
-	fn round_state(&self) -> FutureResult<ReportedRoundStates>;
+    #[rpc(name = "grandpa_roundState")]
+    fn round_state(&self) -> FutureResult<ReportedRoundStates>;
 }
 
 pub struct GrandpaRpcHandler<Hash, Block> {
-	// WIP: pass AuthorityId as type parameter
-	shared_voter_state: SharedVoterState<AuthorityId>,
-	shared_authority_set: SharedAuthoritySet<Hash, Block>,
+    // WIP: pass AuthorityId as type parameter
+    shared_voter_state: SharedVoterState<AuthorityId>,
+    shared_authority_set: SharedAuthoritySet<Hash, Block>,
 }
 
 impl<Hash, Block> GrandpaRpcHandler<Hash, Block> {
-	pub fn new(
-		shared_voter_state: SharedVoterState<AuthorityId>,
-		shared_authority_set: SharedAuthoritySet<Hash, Block>
-	) -> Self {
-		Self {
-			shared_voter_state,
-			shared_authority_set,
-		}
-	}
+    pub fn new(
+        shared_voter_state: SharedVoterState<AuthorityId>,
+        shared_authority_set: SharedAuthoritySet<Hash, Block>,
+    ) -> Self {
+        Self {
+            shared_voter_state,
+            shared_authority_set,
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Prevotes {
-	pub current_weight: u64,
-	pub missing: HashSet<AuthorityId>,
+    pub current_weight: u64,
+    pub missing: HashSet<AuthorityId>,
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Precommits {
-	pub current_weight: u64,
-	pub missing: HashSet<AuthorityId>,
+    pub current_weight: u64,
+    pub missing: HashSet<AuthorityId>,
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RoundState {
-	pub round: u64,
-	pub total_weight: u64,
-	pub threshold_weight: u64,
-	pub prevotes: Prevotes,
-	pub precommits: Precommits,
+    pub round: u64,
+    pub total_weight: u64,
+    pub threshold_weight: u64,
+    pub prevotes: Prevotes,
+    pub precommits: Precommits,
 }
 
 impl RoundState {
-	pub fn from(
-		round: u64,
-		round_state: voter::report::RoundState<AuthorityId>,
-		voters: HashSet<AuthorityId>
-	) -> Self {
-		let prevotes = round_state.prevote_ids;
-		let missing_prevotes = voters.difference(&prevotes).cloned().collect();
+    pub fn from(
+        round: u64,
+        round_state: &voter::report::RoundState<AuthorityId>,
+        voters: &HashSet<AuthorityId>,
+    ) -> Self {
+        let prevotes = &round_state.prevote_ids;
+        let missing_prevotes = voters.difference(&prevotes).cloned().collect();
 
-		let precommits = round_state.precommit_ids;
-		let missing_precommits = voters.difference(&precommits).cloned().collect();
+        let precommits = &round_state.precommit_ids;
+        let missing_precommits = voters.difference(&precommits).cloned().collect();
 
-		Self {
-			round,
-			total_weight: round_state.total_weight,
-			threshold_weight: round_state.threshold_weight,
-			prevotes: Prevotes {
-				current_weight: round_state.prevote_current_weight,
-				missing: missing_prevotes,
-			},
-			precommits: Precommits {
-				current_weight: round_state.precommit_current_weight,
-				missing: missing_precommits,
-			}
-		}
-	}
+        Self {
+            round,
+            total_weight: round_state.total_weight,
+            threshold_weight: round_state.threshold_weight,
+            prevotes: Prevotes {
+                current_weight: round_state.prevote_current_weight,
+                missing: missing_prevotes,
+            },
+            precommits: Precommits {
+                current_weight: round_state.precommit_current_weight,
+                missing: missing_precommits,
+            },
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ReportedRoundStates {
-	pub set_id: u64,
-	pub best: RoundState,
-	// pub background,
+    pub set_id: u64,
+    pub best: RoundState,
+    pub background: Vec<RoundState>,
 }
 
 impl ReportedRoundStates {
-	pub fn from<Hash, Block>(
-		voter_state: &SharedVoterState<AuthorityId>,
-		authority_set: &SharedAuthoritySet<Hash, Block>
-	) -> Self
-	where
-		Hash: Debug + Clone + Eq + Send + Sync + 'static,
-		Block: BlockNumberOps + Send + Sync + 'static,
-	{
-		let voter_state = voter_state.read().as_ref().map(|vs| vs.voter_state());
-		// WIP: handle unwrap of lazily instantiated VoterState
-		let voter_state = voter_state.unwrap();
+    pub fn from<Hash, Block>(
+        voter_state: &SharedVoterState<AuthorityId>,
+        authority_set: &SharedAuthoritySet<Hash, Block>,
+    ) -> Self
+    where
+        Hash: Debug + Clone + Eq + Send + Sync + 'static,
+        Block: BlockNumberOps + Send + Sync + 'static,
+    {
+        let voter_state = voter_state.read().as_ref().map(|vs| vs.voter_state());
+        // WIP: handle unwrap of lazily instantiated VoterState
+        let voter_state = voter_state.unwrap();
 
-		let current_authorities = authority_set.current_authorities();
-		let voters: HashSet<AuthorityId> = current_authorities.voters().iter().map(|p| p.0.clone()).collect();
+        let current_voters: HashSet<AuthorityId> = authority_set
+            .current_authorities()
+			.voters()
+            .iter()
+            .map(|p| p.0.clone())
+            .collect();
 
-		let set_id = voter_state.best_round.0;
-		let best = {
-			let (round, round_state) = voter_state.best_round;
-			RoundState::from(round, round_state, voters)
-		};
+        let set_id = voter_state.best_round.0;
+        if voter_state.best_round.0 != authority_set.set_id() {
+            warn!("Set ID from AuthoritySet does not match VoterState?")
+        }
 
-		Self { set_id, best }
-	}
+        let best = {
+            let (round, round_state) = voter_state.best_round;
+            RoundState::from(round, &round_state, &current_voters)
+        };
+
+        let background = voter_state
+            .background_rounds
+            .iter()
+            .map(|(round, round_state)| RoundState::from(*round, round_state, &current_voters))
+            .collect();
+
+        Self {
+            set_id,
+            best,
+            background,
+        }
+    }
 }
 
-impl<Hash, Block: Send + Sync> GrandpaApi for GrandpaRpcHandler<Hash, Block> where
-	Hash: Debug + Clone + Eq + Send + Sync + 'static,
-	Block: BlockNumberOps + Send + Sync + 'static,
+impl<Hash, Block: Send + Sync> GrandpaApi for GrandpaRpcHandler<Hash, Block>
+where
+    Hash: Debug + Clone + Eq + Send + Sync + 'static,
+    Block: BlockNumberOps + Send + Sync + 'static,
 {
-	fn round_state(&self) -> FutureResult<ReportedRoundStates> {
-		let round_states = ReportedRoundStates::from(
-			&self.shared_voter_state,
-			&self.shared_authority_set
-		);
-		let future = async move {
-			Ok(round_states)
-		}.boxed();
-		Box::new(future.compat())
-	}
+    fn round_state(&self) -> FutureResult<ReportedRoundStates> {
+        let round_states =
+            ReportedRoundStates::from(&self.shared_voter_state, &self.shared_authority_set);
+        let future = async move { Ok(round_states) }.boxed();
+        Box::new(future.compat())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	use substrate_test_runtime_client::{
-		DefaultTestClientBuilderExt,
-		TestClientBuilder,
-		TestClientBuilderExt,
-	};
-	use std::sync::Arc;
-	use jsonrpc_core::IoHandler;
+    use super::*;
+    use jsonrpc_core::IoHandler;
+    use std::sync::Arc;
+    use substrate_test_runtime_client::{
+        DefaultTestClientBuilderExt, TestClientBuilder, TestClientBuilderExt,
+    };
 
-	#[test]
-	fn round_state() {
-		let builder = TestClientBuilder::new();
-		let client = builder.build();
-		let client = Arc::new(client);
+    #[test]
+    fn round_state() {
+        let builder = TestClientBuilder::new();
+        let client = builder.build();
+        let client = Arc::new(client);
 
-		let handler = GrandpaRpcHandler {};
-		let mut io = IoHandler::new();
-		io.extend_with(GrandpaApi::to_delegate(handler));
+        let handler = GrandpaRpcHandler {};
+        let mut io = IoHandler::new();
+        io.extend_with(GrandpaApi::to_delegate(handler));
 
-		let request = r#"{"jsonrpc":"2.0","method":"grandpa_roundState","params":[],"id":1}"#;
-		let response = r#"{"jsonrpc":"2.0","result":"Hello world","id":1}"#;
+        let request = r#"{"jsonrpc":"2.0","method":"grandpa_roundState","params":[],"id":1}"#;
+        let response = r#"{"jsonrpc":"2.0","result":"Hello world","id":1}"#;
 
-		assert_eq!(Some(response.into()), io.handle_request_sync(request));
-	}
+        assert_eq!(Some(response.into()), io.handle_request_sync(request));
+    }
 }
