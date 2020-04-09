42814c01b9990c2b20b2f05f72364246771bf463
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-04-09 20:10:18 +0200

    Missed one

--- frame/generic-asset/src/lib.rs
+++ frame/generic-asset/src/lib.rs
@@ -1124,6 +1124,7 @@ impl<T: Subtrait> frame_system::Trait for ElevatedTrait<T> {
 	type Event = ();
 	type BlockHashCount = T::BlockHashCount;
 	type MaximumBlockWeight = T::MaximumBlockWeight;
+	type DbWeight = ();
 	type MaximumBlockLength = T::MaximumBlockLength;
 	type AvailableBlockRatio = T::AvailableBlockRatio;
 	type Version = T::Version;
