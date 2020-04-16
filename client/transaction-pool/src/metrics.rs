04d89dfba28023292f6cfe8826217c3297b955a3
Nikolay Volf <nikvolf@gmail.com>
2020-04-16 16:31:43 +0300

    Update client/transaction-pool/src/metrics.rs
    Co-Authored-By: Max Inden <mail@max-inden.de>

--- client/transaction-pool/src/metrics.rs
+++ client/transaction-pool/src/metrics.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Transaction pool prometheus metrics.
+//! Transaction pool Prometheus metrics.
 
 use prometheus_endpoint::{register, Counter, PrometheusError, Registry, U64};
 
