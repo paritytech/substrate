ddcd1acd772579e79a1c71bd90697eae98539381
zeke <emostov@middlebury.edu>
2020-04-17 17:40:11 -0700

    add module_id to tests under parameter_types and frame_system::Trait

--- frame/treasury/src/tests.rs
+++ frame/treasury/src/tests.rs
@@ -29,6 +29,7 @@ use sp_runtime::{
     testing::Header,
     traits::{BadOrigin, BlakeTwo256, IdentityLookup},
     Perbill,
+    ModuleId,
 };
 use std::cell::RefCell;
 
@@ -115,8 +116,10 @@ parameter_types! {
     pub const TipFindersFee: Percent = Percent::from_percent(20);
     pub const TipReportDepositBase: u64 = 1;
     pub const TipReportDepositPerByte: u64 = 1;
+    pub const TreasuryModuleId: ModuleId = ModuleId(*b"py/trsry");
 }
 impl Trait for Test {
+    type ModuleId = TreasuryModuleId;
     type Currency = pallet_balances::Module<Test>;
     type ApproveOrigin = frame_system::EnsureRoot<u64>;
     type RejectOrigin = frame_system::EnsureRoot<u64>;
