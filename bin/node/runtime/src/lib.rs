80149d00c7638771264dd3682667694c67cc6747
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-04-22 21:35:01 +0200

    Extrinsic base weight same as old `MINIMUM_WEIGHT`

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -120,6 +120,7 @@ parameter_types! {
 	pub const BlockHashCount: BlockNumber = 250;
 	/// We allow for 2 seconds of compute with a 6 second average block time.
 	pub const MaximumBlockWeight: Weight = 2_000_000_000_000;
+	pub const ExtrinsicBaseWeight: Weight = 10_000_000;
 	pub const MaximumBlockLength: u32 = 5 * 1024 * 1024;
 	pub const Version: RuntimeVersion = VERSION;
 	pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
