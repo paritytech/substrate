8b9f2fed415184fc490a24c50ec6e5e338846c95
Bastian Köcher <git@kchr.de>
2020-04-22 20:52:41 +0200

    Allow missing functions when checking the new runtime's version
    When upgrading a runtime, we check that the version of the new runtime
    is incrementing the `spec_version` or the `spec_name` changes. To do
    this, we need to initialize the runtime and call `Core_version`. When
    initializing the runtime we check all imports and if we can fulfill
    them. Up to now, we would fail when the node did not provide all the
    required host functions. In Westend this lead to older nodes rejecting
    the runtime upgrade, as the runtime version could not be extracted, but
    newer nodes with these host functions succeeded and generated a
    different state root. This pr allows missing host functions. A missing
    host function is replaced by a stub that returns an error when being
    called. As `Core_version` doesn't call any host function besides
    `malloc` and `free`, this should make sure that all nodes (with and
    without the required) host functions generate the same block. This does
    not prevent that old nodes will fail to author blocks with the new
    runtime.

--- frame/system/src/lib.rs
+++ frame/system/src/lib.rs
@@ -2099,6 +2099,7 @@ pub(crate) mod tests {
 				_: &str,
 				_: &[u8],
 				_: &mut dyn sp_externalities::Externalities,
+				_: sp_core::traits::MissingHostFunctions,
 			) -> Result<Vec<u8>, String> {
 				Ok(self.0.clone())
 			}
