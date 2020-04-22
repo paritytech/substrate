ec922353a2b0626338829c197c1a0e03aa97ccf6
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-04-22 11:25:44 +0200

    Tweak description

--- client/cli/src/params/network_params.rs
+++ client/cli/src/params/network_params.rs
@@ -85,9 +85,8 @@ pub struct NetworkParams {
 	#[structopt(flatten)]
 	pub node_key_params: NodeKeyParams,
 
-	/// Disable the yamux flow control. Enable this if the networking seems broken. This option
-	/// will be removed in the future once there is enough confidence that this feature is
-	/// properly working.
+	/// Disable the yamux flow control. This option will be removed in the future once there is
+	/// enough confidence that this feature is properly working.
 	#[structopt(long = "no-yamux-flow-control")]
 	pub no_yamux_flow_control: bool,
 }
