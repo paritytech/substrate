c295218ff79e262ac7e46da17ec48aacd36da8a0
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-04-30 12:21:36 +0200

    copy pasta

--- frame/session/benchmarking/src/mock.rs
+++ frame/session/benchmarking/src/mock.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Mock file for staking fuzzing.
+//! Mock file for session benchmarking.
 
 #![cfg(test)]
 
