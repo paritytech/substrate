55879f634126afc8522c3d46d24eb2e63fe3c756
kianenigma <kian.peymani@gmail.com>
2020-04-30 13:15:49 +0200

    Add to runtime

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -921,6 +921,7 @@ impl_runtime_apis! {
 			add_benchmark!(params, batches, b"treasury", Treasury);
 			add_benchmark!(params, batches, b"utility", Utility);
 			add_benchmark!(params, batches, b"vesting", Vesting);
+			add_benchmark!(params, batches, b"elections", Elections);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
