c986b440143400933e148178dba8aa6661b56d53
Scott Piriou <scott@parity.io>
2020-05-01 22:48:19 +0200

    Revamp export/import/revert testing

--- bin/node/cli/tests/export_import_flow.rs
+++ bin/node/cli/tests/export_import_flow.rs
@@ -17,178 +17,126 @@
 #![cfg(unix)]
 
 use assert_cmd::cargo::cargo_bin;
-use std::{process::Command, fs};
-use tempfile::tempdir;
+use std::{process::Command, fs, path::PathBuf};
+use tempfile::{tempdir, TempDir};
 use regex::Regex;
 use lazy_static::lazy_static;
 
 mod common;
 
-fn contains_error(log: &str) -> bool {
+fn contains_error(stderr: &[u8]) -> bool {
+	let log_output = String::from_utf8_lossy(stderr).to_owned();
     lazy_static! {
         static ref RE: Regex = Regex::new("Error").unwrap();
     }
-    RE.is_match(log)
+    RE.is_match(&log_output)
+}
+
+/// Helper struct to execute the export/import/revert tests.
+/// The fields are paths to a temporary directory
+struct ExportImportRevertExecutor<'a> {
+	base_path: &'a TempDir,
+	exported_blocks: &'a PathBuf,
+	db_path: &'a PathBuf,
+}
+
+/// Format options for export / import commands.
+enum FormatOpt {
+	Json,
+	Binary,
+}
+
+impl<'a> ExportImportRevertExecutor<'a> {
+	fn new(base_path: &'a TempDir, exported_blocks: &'a PathBuf, db_path: &'a PathBuf) -> Self {
+		Self {
+			base_path,
+			exported_blocks,
+			db_path,
+		}
+	}
+
+	/// Helper method to run a command.
+	fn run_block_command(&self, command: &str, format_opt: FormatOpt, expected_to_fail: bool) {
+		let arguments = match format_opt {
+			FormatOpt::Binary => vec![command, "--dev", "--pruning", "archive", "--binary", "-d"],
+			FormatOpt::Json => vec![command, "--dev", "--pruning", "archive", "-d"],
+		};
+		let output = Command::new(cargo_bin("substrate"))
+			.args(&arguments)
+			.arg(&self.base_path.path())
+			.arg(&self.exported_blocks)
+			.output()
+			.unwrap();
+
+		if expected_to_fail {
+			// Checking that we did indeed find an error.
+			assert!(contains_error(&output.stderr), "expected to error but did not error!");
+		} else {
+			// Making sure no error were logged.
+			assert!(!contains_error(&output.stderr), "expected not to error but error'd!");
+		}
+
+		// Command should never fail.
+		assert!(output.status.success());
+	}
+
+	/// Runs the `export-blocks` command.
+	fn run_export(&self, fmt_opt: FormatOpt) {
+		self.run_block_command("export-blocks", fmt_opt, false);
+
+		let metadata = fs::metadata(&self.exported_blocks).unwrap();
+		assert!(metadata.len() > 0, "file exported_blocks should not be empty");
+
+		let _ = fs::remove_dir_all(&self.db_path);
+	}
+
+	/// Runs the `import-blocks` command, asserting that an error was found or not depending on `expected_to_fail`.
+	fn run_import(&self, fmt_opt: FormatOpt, expected_to_fail: bool) {
+		self.run_block_command("import-blocks", fmt_opt, expected_to_fail)
+	}
+
+	/// Runs the `revert` command.
+	fn run_revert(&self) {
+		let output = Command::new(cargo_bin("substrate"))
+			.args(&["revert", "--dev", "--pruning", "archive", "-d"])
+			.arg(&self.base_path.path())
+			.output()
+			.unwrap();
+
+		// Reverting should not log any error.
+		assert!(!contains_error(&output.stderr));
+		// Command should never fail.
+		assert!(output.status.success());
+	}
+
+	/// Helper function that runs the whole export / import / revert flow and checks for errors.
+	fn run(&self, export_fmt: FormatOpt, import_fmt: FormatOpt, expected_to_fail: bool) {
+		self.run_export(export_fmt);
+		self.run_import(import_fmt, expected_to_fail);
+		self.run_revert();
+	}
 }
 
 #[test]
-fn export_binary_import_binary_works() {
+fn export_import_revert() {
 	let base_path = tempdir().expect("could not create a temp dir");
 	let exported_blocks = base_path.path().join("exported_blocks");
 	let db_path = base_path.path().join("db");
 
 	common::run_dev_node_for_a_while(base_path.path());
 
-	let output = Command::new(cargo_bin("substrate"))
-		.args(&["export-blocks", "--pruning", "archive", "--binary", "--dev", "-d"])
-		.arg(&base_path.path())
-		.arg(&exported_blocks)
-		.output()
-		.unwrap();
+	let executor = ExportImportRevertExecutor::new(
+		&base_path,
+		&exported_blocks,
+		&db_path,
+	);
 
-	let log_output = String::from_utf8_lossy(&output.stderr).to_owned();
-	// Making sure no error were logged.
-	assert!(!contains_error(&log_output));
-
-	assert!(output.status.success());
-
-	let metadata = fs::metadata(&exported_blocks).unwrap();
-	assert!(metadata.len() > 0, "file exported_blocks should not be empty");
-
-	let _ = fs::remove_dir_all(&db_path);
-
-	let output = Command::new(cargo_bin("substrate"))
-		.args(&["import-blocks", "--pruning", "archive", "--binary", "--dev", "-d"])
-		.arg(&base_path.path())
-		.arg(&exported_blocks)
-		.output()
-		.expect("failed to execute process");
-
-	let log_output = String::from_utf8_lossy(&output.stderr).to_owned();
-	// Making sure no error were logged.
-	assert!(!contains_error(&log_output));
-
-	assert!(output.status.success());
-}
-
-#[test]
-fn export_default_import_default_works() {
-	let base_path = tempdir().expect("could not create a temp dir");
-	let exported_blocks = base_path.path().join("exported_blocks");
-	let db_path = base_path.path().join("db");
-
-	common::run_dev_node_for_a_while(base_path.path());
-
-	let output = Command::new(cargo_bin("substrate"))
-		.args(&["export-blocks", "--pruning", "archive", "--binary", "--dev", "-d"])
-		.arg(&base_path.path())
-		.arg(&exported_blocks)
-		.output()
-		.unwrap();
-
-	let log_output = String::from_utf8_lossy(&output.stderr).to_owned();
-	// Making sure no error were logged.
-	assert!(!contains_error(&log_output));
-
-	assert!(output.status.success());
-
-	let metadata = fs::metadata(&exported_blocks).unwrap();
-	assert!(metadata.len() > 0, "file exported_blocks should not be empty");
-
-	let _ = fs::remove_dir_all(&db_path);
-
-	let output = Command::new(cargo_bin("substrate"))
-		.args(&["import-blocks", "--pruning", "archive", "--binary", "--dev", "-d"])
-		.arg(&base_path.path())
-		.arg(&exported_blocks)
-		.output()
-		.expect("failed to execute process");
-
-	let log_output = String::from_utf8_lossy(&output.stderr).to_owned();
-	// Making sure no error were logged.
-	assert!(!contains_error(&log_output));
-
-	assert!(output.status.success());
-}
-
-#[test]
-fn export_binary_import_default_works() {
-	let base_path = tempdir().expect("could not create a temp dir");
-	let exported_blocks = base_path.path().join("exported_blocks");
-	let db_path = base_path.path().join("db");
-
-	common::run_dev_node_for_a_while(base_path.path());
-
-	let output = Command::new(cargo_bin("substrate"))
-		.args(&["export-blocks", "--pruning", "archive", "--binary", "--dev", "-d"])
-		.arg(&base_path.path())
-		.arg(&exported_blocks)
-		.output()
-		.unwrap();
-
-	let log_output = String::from_utf8_lossy(&output.stderr).to_owned();
-	// Making sure no error were logged.
-	assert!(!contains_error(&log_output));
-
-	assert!(output.status.success());
-
-	let metadata = fs::metadata(&exported_blocks).unwrap();
-	assert!(metadata.len() > 0, "file exported_blocks should not be empty");
-
-	let _ = fs::remove_dir_all(&db_path);
-
-	let output = Command::new(cargo_bin("substrate"))
-		.args(&["import-blocks", "--pruning", "archive", "--dev", "-d"])
-		.arg(&base_path.path())
-		.arg(&exported_blocks)
-		.output()
-		.expect("failed to execute process");
-
-	let log_output = String::from_utf8_lossy(&output.stderr).to_owned();
-	// Making sure we found an error.
-	assert!(contains_error(&log_output));
-
-	// Exit code should still be 0.
-	assert!(output.status.success());
-}
-
-#[test]
-fn export_default_import_binary_works() {
-	let base_path = tempdir().expect("could not create a temp dir");
-	let exported_blocks = base_path.path().join("exported_blocks");
-	let db_path = base_path.path().join("db");
-
-	common::run_dev_node_for_a_while(base_path.path());
-
-	let output = Command::new(cargo_bin("substrate"))
-		.args(&["export-blocks", "--pruning", "archive", "--dev", "-d"])
-		.arg(&base_path.path())
-		.arg(&exported_blocks)
-		.output()
-		.unwrap();
-
-	let log_output = String::from_utf8_lossy(&output.stderr).to_owned();
-	// Making sure no error were logged.
-	assert!(!contains_error(&log_output));
-
-	assert!(output.status.success());
-
-	let metadata = fs::metadata(&exported_blocks).unwrap();
-	assert!(metadata.len() > 0, "file exported_blocks should not be empty");
-
-	let _ = fs::remove_dir_all(&db_path);
-
-	let output = Command::new(cargo_bin("substrate"))
-		.args(&["import-blocks", "--pruning", "archive", "--binary", "--dev", "-d"])
-		.arg(&base_path.path())
-		.arg(&exported_blocks)
-		.output()
-		.expect("failed to execute process");
-
-	let log_output = String::from_utf8_lossy(&output.stderr).to_owned();
-	// Making sure we found an error.
-	assert!(contains_error(&log_output));
-
-	// Exit code should still be 0.
-	assert!(output.status.success());
+	// Binary and binary should work.
+	executor.run(FormatOpt::Binary, FormatOpt::Binary, false);
+	// Binary and JSON should fail.
+	executor.run(FormatOpt::Binary, FormatOpt::Json, true);
+	// JSON and JSOn should work.
+	executor.run(FormatOpt::Json, FormatOpt::Json, false);
+	// JSON and binary should fail.
+	executor.run(FormatOpt::Json, FormatOpt::Binary, true);
 }
