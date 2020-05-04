dea76f499398eb7a69c49654e6d7eeebc8031608
Wei Tang <hi@that.world>
2020-05-04 08:53:37 +0200

    Fix nostd compile

--- primitives/consensus/vrf/src/schnorrkel.rs
+++ primitives/consensus/vrf/src/schnorrkel.rs
@@ -17,6 +17,7 @@
 //! Schnorrkel-based VRF.
 
 use codec::{Encode, Decode, EncodeLike};
+use sp_std::prelude::*;
 use sp_core::U512;
 use sp_runtime::RuntimeDebug;
 use sp_std::ops::{Deref, DerefMut};
