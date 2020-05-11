67db346ba50731a12c3d027fbc64deafe49ee955
Benjamin Kampmann <ben@parity.io>
2020-05-11 14:28:43 +0200

    fix formatting issue

--- client/api/src/proof_provider.rs
+++ client/api/src/proof_provider.rs
@@ -14,7 +14,9 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.//! Proof utilities
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! Proof utilities
 use sp_runtime::{
 	generic::BlockId,
 	traits::{Block as BlockT},
