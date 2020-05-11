a323dbcf2e891587c306b65835c57ec09f7d89d4
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-05-11 14:01:44 +0200

    while -> if

--- client/network/src/block_requests.rs
+++ client/network/src/block_requests.rs
@@ -704,7 +704,7 @@ where
 			}
 		}
 
-		while let Poll::Ready(Some((peer, total_handling_time))) = self.outgoing.poll_next_unpin(cx) {
+		if let Poll::Ready(Some((peer, total_handling_time))) = self.outgoing.poll_next_unpin(cx) {
 			let ev = Event::AnsweredRequest {
 				peer,
 				total_handling_time,
