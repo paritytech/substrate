a2ad3a6213b61b2aeb91c01f7d7f014ea9b6d069
Alexander Theißen <alexander.theissen@parity.io>
2020-05-12 15:16:07 +0200

    Add proper seperator between revision and file

--- .maintain/gitlab/check_line_width.sh
+++ .maintain/gitlab/check_line_width.sh
@@ -9,28 +9,28 @@ LINE_WIDTH="121"
 GOOD_LINE_WIDTH="101"
 
 
-git diff --name-only ${BASE_BRANCH}...${CI_COMMIT_SHA} \*.rs | ( while read file
+git diff --name-only ${BASE_BRANCH}...${CI_COMMIT_SHA} -- \*.rs | ( while read file
 do
   if [ ! -f ${file} ];
   then
 	echo "Skipping removed file."
-  elif git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} ${file} | grep -q "^+.\{${LINE_WIDTH}\}"
+  elif git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} | grep -q "^+.\{${LINE_WIDTH}\}"
   then
     if [ -z "${FAIL}" ]
     then
-      echo "| warning!"
-      echo "| Lines should not be longer than 120 characters."
+      echo "| error!"
+      echo "| Lines must not be longer than 120 characters."
       echo "| "
       echo "| see more https://wiki.parity.io/Substrate-Style-Guide"
       echo "|"
       FAIL="true"
     fi
     echo "| file: ${file}"
-    git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} ${file} \
+    git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} \
       | grep -n "^+.\{${LINE_WIDTH}\}"
     echo "|"
   else
-    if git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} ${file} | grep -q "^+.\{${GOOD_LINE_WIDTH}\}"
+    if git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} | grep -q "^+.\{${GOOD_LINE_WIDTH}\}"
     then
       if [ -z "${FAIL}" ]
       then
@@ -41,7 +41,7 @@ do
         echo "|"
       fi
       echo "| file: ${file}"
-      git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} ${file} \
+      git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} \
         | grep -n "^+.\{${LINE_WIDTH}\}"
       echo "|"
     fi
