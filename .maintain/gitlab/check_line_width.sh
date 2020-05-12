2cfada6f1b8748842bfb70c5df3291deceb3e136
Alexander Theißen <alexander.theissen@parity.io>
2020-05-12 15:23:51 +0200

    Do not repeat limit number in error text

--- .maintain/gitlab/check_line_width.sh
+++ .maintain/gitlab/check_line_width.sh
@@ -5,21 +5,20 @@
 set -e
 
 BASE_BRANCH="origin/master"
-LINE_WIDTH="121"
-GOOD_LINE_WIDTH="101"
-
+LINE_WIDTH="120"
+GOOD_LINE_WIDTH="100"
 
 git diff --name-only ${BASE_BRANCH}...${CI_COMMIT_SHA} -- \*.rs | ( while read file
 do
   if [ ! -f ${file} ];
   then
 	echo "Skipping removed file."
-  elif git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} | grep -q "^+.\{${LINE_WIDTH}\}"
+  elif git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} | grep -q "^+.\{$(( $LINE_WIDTH + 1 ))\}"
   then
     if [ -z "${FAIL}" ]
     then
       echo "| error!"
-      echo "| Lines must not be longer than 120 characters."
+      echo "| Lines must not be longer than ${LINE_WIDTH} characters."
       echo "| "
       echo "| see more https://wiki.parity.io/Substrate-Style-Guide"
       echo "|"
@@ -27,22 +26,22 @@ do
     fi
     echo "| file: ${file}"
     git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} \
-      | grep -n "^+.\{${LINE_WIDTH}\}"
+      | grep -n "^+.\{$(( $LINE_WIDTH + 1))\}"
     echo "|"
   else
-    if git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} | grep -q "^+.\{${GOOD_LINE_WIDTH}\}"
+    if git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} | grep -q "^+.\{$(( $GOOD_LINE_WIDTH + 1 ))\}"
     then
       if [ -z "${FAIL}" ]
       then
         echo "| warning!"
-        echo "| Lines should be longer than 100 characters only in exceptional circumstances!"
+        echo "| Lines should be longer than ${GOOD_LINE_WIDTH} characters only in exceptional circumstances!"
         echo "| "
         echo "| see more https://wiki.parity.io/Substrate-Style-Guide"
         echo "|"
       fi
       echo "| file: ${file}"
       git diff ${BASE_BRANCH}...${CI_COMMIT_SHA} -- ${file} \
-        | grep -n "^+.\{${GOOD_LINE_WIDTH}\}"
+        | grep -n "^+.\{$(( $GOOD_LINE_WIDTH + 1 ))\}"
       echo "|"
     fi
   fi
