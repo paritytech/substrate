739805945afbcbfa27b4d9fea01d6a593f4592e5
Wei Tang <hi@that.world>
2020-05-12 12:56:32 +0200

    Update frame/babe/src/lib.rs
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>

--- frame/babe/src/lib.rs
+++ frame/babe/src/lib.rs
@@ -364,7 +364,8 @@ impl<T: Trait> Module<T> {
 	}
 
 	/// Plan an epoch config change. The epoch config change is recorded and will be enacted on the
-	/// next call to `enact_epoch_change`. The config will be activated one epoch after.
+	/// next call to `enact_epoch_change`. The config will be activated one epoch after. Multiple calls to this
+	/// method will replace any existing planned config change that had not been enacted yet.
 	pub fn plan_config_change(
 		config: BabeEpochConfiguration,
 	) {
