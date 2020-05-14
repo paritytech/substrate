5a6e7659354bc47c60ef84fc902660f962c00f66
Benjamin Kampmann <ben@parity.io>
2020-05-14 11:32:15 +0200

    Put some back on GPL3+Classpath

--- client/service/src/client/light/backend.rs
+++ client/service/src/client/light/backend.rs
@@ -15,6 +15,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 //! Light client backend. Only stores headers and justifications of blocks.
 //! Everything else is requested from full nodes on demand.
 
