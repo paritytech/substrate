d71e951824e7d6d0bb267ec1b9b5e448b82692d6
Nikolay Volf <nikvolf@gmail.com>
2020-05-18 15:02:09 +0300

    Update client/basic-authorship/src/basic_authorship.rs
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- client/basic-authorship/src/basic_authorship.rs
+++ client/basic-authorship/src/basic_authorship.rs
@@ -396,7 +396,7 @@ mod tests {
 			))
 		);
 
-		let mut proposer_factory = ProposerFactory::new_test(client.clone(), txpool.clone());
+		let mut proposer_factory = ProposerFactory::new(client.clone(), txpool.clone(), None);
 
 		let cell = Mutex::new((false, time::Instant::now()));
 		let mut proposer = proposer_factory.init_with_now(
