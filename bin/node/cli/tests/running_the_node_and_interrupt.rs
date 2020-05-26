1a274a52280c9aadeac097729c1be25753ffbfd2
Gav Wood <gavin@parity.io>
2020-05-26 06:10:38 +0200

    Attempt general fix

--- bin/node/cli/tests/running_the_node_and_interrupt.rs
+++ bin/node/cli/tests/running_the_node_and_interrupt.rs
@@ -31,7 +31,7 @@ fn running_the_node_works_and_can_be_interrupted() {
 	fn run_command_and_kill(signal: Signal) {
 		let base_path = tempdir().expect("could not create a temp dir");
 		let mut cmd = Command::new(cargo_bin("substrate"))
-			.args(&["--rc1", "-d"])
+			.args(&["--dev", "-d"])
 			.arg(base_path.path())
 			.spawn()
 			.unwrap();
