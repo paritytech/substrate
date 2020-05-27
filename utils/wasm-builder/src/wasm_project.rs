352fc96f9a959dcb2f039fdf59d53a861b94e74c
Bastian Köcher <git@kchr.de>
2020-05-27 20:39:15 +0200

    Fix compilation

--- utils/wasm-builder/src/wasm_project.rs
+++ utils/wasm-builder/src/wasm_project.rs
@@ -318,7 +318,7 @@ fn find_package_by_manifest_path<'a>(
 	crate_metadata.packages
 		.iter()
 		.find(|p| p.manifest_path == manifest_path)
-		.expect("Wasm project exists in its own metadata; qed");
+		.expect("Wasm project exists in its own metadata; qed")
 }
 
 /// Get a list of enabled features for the project.
@@ -374,7 +374,7 @@ fn create_project(cargo_manifest: &Path, wasm_workspace: &Path, crate_metadata: 
 	let mut enabled_features = project_enabled_features(&cargo_manifest, &crate_metadata);
 
 	if has_runtime_wasm_feature(cargo_manifest, crate_metadata) {
-		enabled_features.push("runtime-wasm");
+		enabled_features.push("runtime-wasm".into());
 	}
 
 	write_file_if_changed(
