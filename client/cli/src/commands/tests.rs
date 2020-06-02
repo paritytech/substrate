2a4374bca915312e5100e6b7d0f56dc593c3d0b7
Seun <seunlanlege@gmail.com>
2020-06-02 07:49:20 +0100

    remove unused vars

--- client/cli/src/commands/tests.rs
+++ client/cli/src/commands/tests.rs
@@ -18,12 +18,10 @@
 
 use super::{SignCmd, VanityCmd};
 use structopt::StructOpt;
-use crate::Error;
 
 
 #[test]
 fn sign() {
-	let words = "remember fiber forum demise paper uniform squirrel feel access exclude casual effort";
 	let seed = "0xad1fb77243b536b90cfe5f0d351ab1b1ac40e3890b41dc64f766ee56340cfca5";
 
 	let sign = SignCmd::from_iter(&["sign", "--suri", seed, "--message", &seed[2..], "--password", "12345"]);
