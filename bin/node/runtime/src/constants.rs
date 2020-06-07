a588347c1f053837122abb6c1c364457d89be12a
Gav Wood <gavin@parity.io>
2020-06-07 14:24:15 +0200

    Line width

--- bin/node/runtime/src/constants.rs
+++ bin/node/runtime/src/constants.rs
@@ -25,7 +25,9 @@ pub mod currency {
 	pub const CENTS: Balance = 1_000 * MILLICENTS;    // assume this is worth about a cent.
 	pub const DOLLARS: Balance = 100 * CENTS;
 
-	pub const fn deposit(items: u32, bytes: u32) -> Balance { items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS }
+	pub const fn deposit(items: u32, bytes: u32) -> Balance {
+		items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
+	}
 }
 
 /// Time.
