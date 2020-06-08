a5bf77ded46c3a8b0c940d6b51bdf4dd19e76339
pscott <30843220+pscott@users.noreply.github.com>
2020-06-08 15:43:00 +0200

    Make all features explicit (#6267)
    * make all features explicit
    
    * Change to -feature suffix to with- prefix
    
    * Add newline at the end of the Cargo.toml file
    
    * Remove rhd feature
    
    * Remove some features from Cargo.toml
    
    * Remove test-helpers feature in tx pool
    
    * Return db_open_error("with-"..
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Rename subdb feature to with-subdb
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Remove 'strict' feature and cfg_attr
    
    * Check for with-subdb feature instead of subdb
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- client/db/src/utils.rs
+++ client/db/src/utils.rs
@@ -36,7 +36,7 @@ use crate::{DatabaseSettings, DatabaseSettingsSrc, Database, DbHash};
 
 /// Number of columns in the db. Must be the same for both full && light dbs.
 /// Otherwise RocksDb will fail to open database && check its type.
-#[cfg(any(feature = "kvdb-rocksdb", feature = "test-helpers", test))]
+#[cfg(any(feature = "with-kvdb-rocksdb", feature = "test-helpers", test))]
 pub const NUM_COLUMNS: u32 = 11;
 /// Meta column. The set of keys in the column is shared by full && light storages.
 pub const COLUMN_META: u32 = 0;
@@ -219,7 +219,7 @@ pub fn open_database<Block: BlockT>(
 	);
 
 	let db: Arc<dyn Database<DbHash>> = match &config.source {
-		#[cfg(any(feature = "kvdb-rocksdb", test))]
+		#[cfg(any(feature = "with-kvdb-rocksdb", test))]
 		DatabaseSettingsSrc::RocksDb { path, cache_size } => {
 			// first upgrade database to required version
 			crate::upgrade::upgrade_db::<Block>(&path, db_type)?;
@@ -255,27 +255,27 @@ pub fn open_database<Block: BlockT>(
 				.map_err(|err| sp_blockchain::Error::Backend(format!("{}", err)))?;
 			sp_database::as_database(db)
 		},
-		#[cfg(not(any(feature = "kvdb-rocksdb", test)))]
+		#[cfg(not(any(feature = "with-kvdb-rocksdb", test)))]
 		DatabaseSettingsSrc::RocksDb { .. } => {
-			return db_open_error("kvdb-rocksdb");
+			return db_open_error("with-kvdb-rocksdb");
 		},
-		#[cfg(feature = "subdb")]
+		#[cfg(feature = "with-subdb")]
 		DatabaseSettingsSrc::SubDb { path } => {
 			crate::subdb::open(&path, NUM_COLUMNS)
 				.map_err(|e| sp_blockchain::Error::Backend(format!("{:?}", e)))?
 		},
-		#[cfg(not(feature = "subdb"))]
+		#[cfg(not(feature = "with-subdb"))]
 		DatabaseSettingsSrc::SubDb { .. } => {
-			return db_open_error("subdb");
+			return db_open_error("with-subdb");
 		},
-		#[cfg(feature = "parity-db")]
+		#[cfg(feature = "with-parity-db")]
 		DatabaseSettingsSrc::ParityDb { path } => {
 			crate::parity_db::open(&path)
 				.map_err(|e| sp_blockchain::Error::Backend(format!("{:?}", e)))?
 		},
-		#[cfg(not(feature = "parity-db"))]
+		#[cfg(not(feature = "with-parity-db"))]
 		DatabaseSettingsSrc::ParityDb { .. } => {
-			return db_open_error("parity-db");
+			return db_open_error("with-parity-db");
 		},
 		DatabaseSettingsSrc::Custom(db) => db.clone(),
 	};
