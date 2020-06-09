5383907d4ab3139cc69b58954b98e4fab225547f
Bernhard Schuster <bernhard@parity.io>
2020-06-09 18:50:10 +0200

    address review comments

--- frame/session/src/historical/shared.rs
+++ frame/session/src/historical/shared.rs
@@ -21,17 +21,17 @@
 
 use super::*;
 
-pub(super) const PREFIX: &[u8] = b"historical";
-pub(super) const LAST_PRUNE: &[u8] = b"historical_last_prune";
+pub(super) const PREFIX: &[u8] = b"session_historical";
+pub(super) const LAST_PRUNE: &[u8] = b"session_historical_last_prune";
 
 /// Derive the key used to store the list of validators
 pub(super) fn derive_key<P: AsRef<[u8]>>(prefix: P, session_index: SessionIndex) -> Vec<u8> {
-    let prefix: &[u8] = prefix.as_ref();
-    let encoded_session_index = session_index.encode();
-    assert!(encoded_session_index.len() > 0);
-    let mut concatenated = Vec::with_capacity(prefix.len() + 1 + encoded_session_index.len());
-    concatenated.extend_from_slice(prefix);
-    concatenated.push('/' as u8);
-    concatenated.extend_from_slice(encoded_session_index.as_slice());
-    concatenated
+	let prefix: &[u8] = prefix.as_ref();
+	session_index.using_encoded(|encoded_session_index| {
+		prefix.into_iter()
+			.chain(b"/".into_iter())
+			.chain(encoded_session_index.into_iter())
+			.copied()
+			.collect::<Vec<u8>>()
+	})
 }
