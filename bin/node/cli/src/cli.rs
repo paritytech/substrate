299e8d42cdee2f039c09dc199cfdcbc8bd229933
Cecile Tonglet <cecile@parity.io>
2020-06-10 13:13:25 +0200

    Add a feature to create automatically a random temporary directory for base path & remove `Clone` (#6221)
    * Initial commit
    
    Forked at: 342caad3074076a4fde0472719f6a473df839e42
    Parent branch: origin/master
    
    * Add a feature to create automatically a temporary directory for base path
    
    * doc fix and todos
    
    * use parking_lot instead
    
    * use refcell instead since we stay in the main thread
    
    * remove Clone derives
    
    * add test
    
    * solving dependency issue
    
    * clarifying doc
    
    * conflict argument with base-path
    
    * WIP
    
    Forked at: 342caad3074076a4fde0472719f6a473df839e42
    Parent branch: origin/master
    
    * revert dep deletion
    
    * fixing test and making base_path optional
    
    * hold basepath while the service is running
    
    * fixes
    
    * Update client/cli/src/params/shared_params.rs
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Update client/service/Cargo.toml
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Update client/cli/src/commands/mod.rs
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Update client/service/src/config.rs
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * WIP
    
    Forked at: 342caad3074076a4fde0472719f6a473df839e42
    Parent branch: origin/master
    
    * improve doc
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- bin/node/cli/src/cli.rs
+++ bin/node/cli/src/cli.rs
@@ -20,7 +20,7 @@ use sc_cli::RunCmd;
 use structopt::StructOpt;
 
 /// An overarching CLI command definition.
-#[derive(Clone, Debug, StructOpt)]
+#[derive(Debug, StructOpt)]
 pub struct Cli {
 	/// Possible subcommand with parameters.
 	#[structopt(subcommand)]
@@ -31,7 +31,7 @@ pub struct Cli {
 }
 
 /// Possible subcommands of the main binary.
-#[derive(Clone, Debug, StructOpt)]
+#[derive(Debug, StructOpt)]
 pub enum Subcommand {
 	/// A set of base subcommands handled by `sc_cli`.
 	#[structopt(flatten)]
