95ea8dd66a08d1aeb57cc4e8a65bff3aa76f4bcf
Guillaume Thiolliere <gui.thiolliere@gmail.com>
2020-06-15 17:05:41 +0200

    Introduce in-origin filtering (#6318)
    * impl filter in origin
    
    * remove IsCallable usage. Breaking: utility::batch(root, calls) no longer bypass BasicCallFilter
    
    * rename BasicCallFilter -> BaseCallFilter
    
    * refactor code
    
    * Apply suggestions from code review
    
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>
    
    * remove forgotten temporar comment
    
    * better add suggestion in another PR
    
    * refactor: use Clone instead of mem::replace
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix benchmarks
    
    * Make root bypass filter in utility::batch
    
    * fix unused imports
    
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- frame/support/test/tests/decl_error.rs
+++ frame/support/test/tests/decl_error.rs
@@ -89,12 +89,14 @@ pub type BlockNumber = u64;
 pub type Index = u64;
 
 impl system::Trait for Runtime {
+	type BaseCallFilter = ();
 	type Hash = H256;
 	type Origin = Origin;
 	type BlockNumber = BlockNumber;
 	type AccountId = AccountId;
 	type Event = Event;
 	type ModuleToIndex = ModuleToIndex;
+	type Call = Call;
 }
 
 frame_support::construct_runtime!(
