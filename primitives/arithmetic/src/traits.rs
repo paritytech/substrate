fd289d41c6cdcbc2d0059d1d431e07bf848e5584
thiolliere <gui.thiolliere@gmail.com>
2020-06-18 15:58:17 +0200

    doc refactor

--- primitives/arithmetic/src/traits.rs
+++ primitives/arithmetic/src/traits.rs
@@ -79,14 +79,10 @@ pub trait AtLeast32Bit: BaseArithmetic + From<u16> + From<u32> {}
 
 impl<T: BaseArithmetic + From<u16> + From<u32>> AtLeast32Bit for T {}
 
-/// A meta trait for arithmetic.
-///
-/// Arithmetic types do all the usual stuff you'd expect numbers to do. They are guaranteed to
-/// be able to represent at least `u32` values without loss, hence the trait implies `From<u32>`
-/// Same as [`AtLeast32Bit `], but also bounded to be unsigned. 
-pub trait AtLeast32BitUnsigned: BaseArithmetic + From<u16> + From<u32> + Unsigned {}
+/// A meta trait for arithmetic.  Same as [`AtLeast32Bit `], but also bounded to be unsigned.
+pub trait AtLeast32BitUnsigned: AtLeast32Bit + Unsigned {}
 
-impl<T: BaseArithmetic + From<u16> + From<u32> + Unsigned> AtLeast32BitUnsigned for T {}
+impl<T: AtLeast32Bit + Unsigned> AtLeast32BitUnsigned for T {}
 
 /// Just like `From` except that if the source value is too big to fit into the destination type
 /// then it'll saturate the destination.
