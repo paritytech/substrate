7c7d18d84db10a52fc0db0080b9b646cc90a3426
Dan Forbes <dan@danforbes.dev>
2020-06-19 06:55:15 -0700

    Simple Docs for Atomic Swap Pallet (#6434)
    * Simple Docs for Atomic Swap Pallet
    
    * Fix copy-and-paste error

--- frame/atomic-swap/src/lib.rs
+++ frame/atomic-swap/src/lib.rs
@@ -15,7 +15,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! # Atomic swap support pallet
+//! # Atomic Swap
+//!
+//! A module for atomically sending funds.
+//!
+//! - [`atomic_swap::Trait`](./trait.Trait.html)
+//! - [`Call`](./enum.Call.html)
+//! - [`Module`](./struct.Module.html)
+//!
+//! ## Overview
+//!
+//! A module for atomically sending funds from an origin to a target. A proof
+//! is used to allow the target to approve (claim) the swap. If the swap is not
+//! claimed within a specified duration of time, the sender may cancel it.
+//!
+//! ## Interface
+//!
+//! ### Dispatchable Functions
+//!
+//! * `create_swap` - called by a sender to register a new atomic swap
+//! * `claim_swap` - called by the target to approve a swap
+//! * `cancel_swap` - may be called by a sender after a specified duration
 
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
