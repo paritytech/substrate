1e957cee42bc400fbf64f9853ec0309abba68cf5
Bastian Köcher <git@kchr.de>
2020-06-19 09:26:08 +0200

    Update sync chain info on own block import
    Before we only updated the chain info of sync when we have imported
    something using the import queue. However, if you import your own
    blocks, this is not done using the import queue and so sync is not
    updated. If we don't do this, it can lead to sync switching to "major
    sync" mode because sync is not informed about new blocks. This
    especially happens on Cumulus, where a collator is selected multiple
    times to include its block into the relay chain and thus, sync switches
    to major sync mode while the node is still building blocks.

--- client/network/src/service.rs
+++ client/network/src/service.rs
@@ -747,6 +747,12 @@ impl<B: BlockT + 'static, H: ExHashT> NetworkService<B, H> {
 			.unbounded_send(ServiceToWorkerMsg::UpdateChain);
 	}
 
+	/// Inform the network service about an own imported block.
+	pub fn own_block_imported(&self, hash: B::Hash, number: NumberFor<B>) {
+		let _ = self
+			.to_worker
+			.unbounded_send(ServiceToWorkerMsg::OwnBlockImported(hash, number));
+	}
 }
 
 impl<B: BlockT + 'static, H: ExHashT> sp_consensus::SyncOracle
@@ -813,6 +819,7 @@ enum ServiceToWorkerMsg<B: BlockT, H: ExHashT> {
 	},
 	DisconnectPeer(PeerId),
 	UpdateChain,
+	OwnBlockImported(B::Hash, NumberFor<B>),
 }
 
 /// Main network worker. Must be polled in order for the network to advance.
@@ -1143,6 +1150,8 @@ impl<B: BlockT + 'static, H: ExHashT> Future for NetworkWorker<B, H> {
 					this.network_service.user_protocol_mut().disconnect_peer(&who),
 				ServiceToWorkerMsg::UpdateChain =>
 					this.network_service.user_protocol_mut().update_chain(),
+				ServiceToWorkerMsg::OwnBlockImported(hash, number) =>
+					this.network_service.user_protocol_mut().own_block_imported(hash, number),
 			}
 		}
 
