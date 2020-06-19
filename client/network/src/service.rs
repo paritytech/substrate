6721e005a36dc99bd3b9bc6bcea426f78f401a83
Cecile Tonglet <cecile@parity.io>
2020-06-19 16:51:44 +0200

    Fix bad previous impl

--- client/network/src/service.rs
+++ client/network/src/service.rs
@@ -108,35 +108,37 @@ impl<B: BlockT + 'static, H: ExHashT> NetworkWorker<B, H> {
 	/// `worker.service()`. The `NetworkService` can be shared through the codebase.
 	pub fn new(params: Params<B, H>) -> Result<NetworkWorker<B, H>, Error> {
 		// Ensure the listen addresses are consistent with the transport.
-		for addr in &params.network_config.listen_addresses {
-			if !matches!(params.network_config.transport, TransportConfig::MemoryOnly) {
-				let invalid_addresses: Vec<_> = addr.iter()
-					.filter(|x| matches!(x, libp2p::core::multiaddr::Protocol::Memory(_)))
-					.map(|x| x.to_string())
-					.collect();
+		if !matches!(params.network_config.transport, TransportConfig::MemoryOnly) {
+			let invalid_addresses: Vec<_> = params.network_config.listen_addresses.iter()
+				.filter(|x| x.iter()
+					.any(|y| matches!(y, libp2p::core::multiaddr::Protocol::Memory(_)))
+				)
+				.map(|x| x.to_string())
+				.collect();
 
-				if !invalid_addresses.is_empty() {
-					return Err(Error::InvalidConfiguration(format!(
-						"The following network addresses are invalid because the transport is \
-						not set on {:?}: {}",
-						TransportConfig::MemoryOnly,
-						invalid_addresses.join(", "),
-					)))
-				}
-			} else {
-				let invalid_addresses: Vec<_> = addr.iter()
-					.filter(|x| !matches!(x, libp2p::core::multiaddr::Protocol::Memory(_)))
-					.map(|x| x.to_string())
-					.collect();
+			if !invalid_addresses.is_empty() {
+				return Err(Error::InvalidConfiguration(format!(
+					"The following network listen addresses are invalid because the transport \
+					is not set on {:?}: {}",
+					TransportConfig::MemoryOnly,
+					invalid_addresses.join(", "),
+				)))
+			}
+		} else {
+			let invalid_addresses: Vec<_> = params.network_config.listen_addresses.iter()
+				.filter(|x| x.iter()
+					.any(|y| !matches!(y, libp2p::core::multiaddr::Protocol::Memory(_)))
+				)
+				.map(|x| x.to_string())
+				.collect();
 
-				if !invalid_addresses.is_empty() {
-					return Err(Error::InvalidConfiguration(format!(
-						"The following network addresses are invalid because the transport is \
-						set on {:?}: {}",
-						TransportConfig::MemoryOnly,
-						invalid_addresses.join(", "),
-					)))
-				}
+			if !invalid_addresses.is_empty() {
+				return Err(Error::InvalidConfiguration(format!(
+					"The following network listen addresses are invalid because the transport \
+					is set on {:?}: {}",
+					TransportConfig::MemoryOnly,
+					invalid_addresses.join(", "),
+				)))
 			}
 		}
 
