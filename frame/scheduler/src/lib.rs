39040bc17cff1a62e6c35175bf2cbf2eda17a9a9
Bryan Chen <xlchen1291@gmail.com>
2020-06-24 11:40:51 +1200

    add notes

--- frame/scheduler/src/lib.rs
+++ frame/scheduler/src/lib.rs
@@ -28,6 +28,14 @@
 //! specified block number or at a specified period. These scheduled dispatches
 //! may be named or anonymous and may be canceled.
 //!
+//! **NOTE:** The scheduled calls will be dispatched with the default filter
+//! for the origin: namely `frame_system::Trait::BaseCallFilter` for all origin
+//! except root which will get no filter. And not the filter contained in origin
+//! use to call `fn schedule`.
+//!
+//! If a call is scheduled using proxy or whatever mecanism which adds filter,
+//! then those filter will not be used when dispatching the schedule call.
+//!
 //! ## Interface
 //!
 //! ### Dispatchable Functions
