439f09e2d0655414118b884c3e6b8207ab291032
Ashley <ashley.ruglys@gmail.com>
2020-07-02 12:57:56 +0200

    Remove `ServiceBuilderCommand` and implement the chain ops as standalone functions instead. (#6543)
    * :)
    
    * Slight tidy
    
    * Remove ServiceBuilderCommand
    
    * Remove whitespace
    
    * Keep task manager alive for check_block/import_blocks
    
    * Pass task_manager to run_until_exit
    
    * Make task_manager in run_until_exit and make subcommands async
    
    * Change the async_run fn to return a future and task manager
    
    * async_run should take a result fn
    
    * Apply suggestions from code review
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Fix spaces in export_raw_state
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- client/service/src/lib.rs
+++ client/service/src/lib.rs
@@ -23,7 +23,6 @@
 #![recursion_limit="128"]
 
 pub mod config;
-#[macro_use]
 pub mod chain_ops;
 pub mod error;
 
@@ -55,7 +54,7 @@ use sp_utils::{status_sinks, mpsc::{tracing_unbounded, TracingUnboundedReceiver,
 pub use self::error::Error;
 pub use self::builder::{
 	new_full_client, new_client,
-	ServiceBuilder, ServiceBuilderCommand, TFullClient, TLightClient, TFullBackend, TLightBackend,
+	ServiceBuilder, TFullClient, TLightClient, TFullBackend, TLightBackend,
 	TFullCallExecutor, TLightCallExecutor, RpcExtensionBuilder,
 };
 pub use config::{
@@ -79,6 +78,7 @@ pub use sc_network::config::{
 pub use sc_tracing::TracingReceiver;
 pub use task_manager::SpawnTaskHandle;
 pub use task_manager::TaskManager;
+pub use sp_consensus::import_queue::ImportQueue;
 use sc_client_api::{Backend, BlockchainEvents};
 
 const DEFAULT_PROTOCOL_ID: &str = "sup";
