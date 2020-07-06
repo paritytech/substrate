baec590bee394a0591faa63224a0f4514def706d
Scott Piriou <scott@parity.io>
2020-07-06 14:15:03 +0200

    Remove unused variable

--- bin/node/cli/tests/build_spec_works.rs
+++ bin/node/cli/tests/build_spec_works.rs
@@ -34,7 +34,6 @@ fn build_spec_works() {
 	// Make sure that the `dev` chain folder exists, but the `db` doesn't
 	assert!(base_path.path().join("chains/dev/").exists());
 	assert!(!base_path.path().join("chains/dev/db").exists());
-	let s = String::from_utf8_lossy(output.stdout.as_slice());
 
 	let _value: serde_json::Value = serde_json::from_slice(output.stdout.as_slice()).unwrap();
 }
