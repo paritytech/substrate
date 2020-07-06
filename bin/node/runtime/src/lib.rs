eb0ae88bf976b2e19e154b23d8bcabcbd6719858
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-07-06 02:28:20 +0200

    Update bin/node/runtime/src/lib.rs

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -1136,7 +1136,7 @@ impl_runtime_apis! {
 			let mut batches = Vec::<BenchmarkBatch>::new();
 			let params = (&pallet, &benchmark, &lowest_range_values, &highest_range_values, &steps, repeat, &whitelist);
 
-      add_benchmark!(params, batches, pallet_babe, Babe);
+			add_benchmark!(params, batches, pallet_babe, Babe);
 			add_benchmark!(params, batches, pallet_balances, Balances);
 			add_benchmark!(params, batches, pallet_collective, Council);
 			add_benchmark!(params, batches, pallet_democracy, Democracy);
