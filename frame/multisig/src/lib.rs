9e9223c1cbb8d43af5bdc621ac8244764a374185
Alexander Popiak <alexander.popiak@parity.io>
2020-07-08 15:34:06 +0200

    add multisig migration

--- frame/multisig/src/lib.rs
+++ frame/multisig/src/lib.rs
@@ -235,6 +235,20 @@ decl_module! {
 		/// Deposit one of this module's events by using the default implementation.
 		fn deposit_event() = default;
 
+		// Migration necessary for the Edgeware migration.
+		// Created in the [stacked filtering PR](https://github.com/paritytech/substrate/pull/6273/files#diff-9967c6d85464599e91d4626b3869c66bR209)
+		// and removed [here](https://github.com/paritytech/substrate/pull/6476/files#diff-9967c6d85464599e91d4626b3869c66bL238)
+		fn on_runtime_upgrade() -> Weight {
+			// Utility.Multisigs -> Multisig.Multisigs
+			use frame_support::migration::{StorageIterator, put_storage_value};
+			for (key, value) in StorageIterator::<
+				Multisig<T::BlockNumber, BalanceOf<T>, T::AccountId>
+			>::new(b"Utility", b"Multisigs").drain() {
+				put_storage_value(b"Multisig", b"Multisigs", &key, value);
+			}
+			1_000_000_000
+		}
+
 		/// Immediately dispatch a multi-signature call using a single approval from the caller.
 		///
 		/// The dispatch origin for this call must be _Signed_.
