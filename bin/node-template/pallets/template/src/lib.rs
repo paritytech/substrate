5433f9cf5b7f299da39d3e2548dcce59ff1b5df2
Dan Forbes <dan@danforbes.dev>
2020-07-09 12:45:33 -0700

    Import `Get` trait to fix build

--- bin/node-template/pallets/template/src/lib.rs
+++ bin/node-template/pallets/template/src/lib.rs
@@ -9,7 +9,7 @@
 /// For more guidance on Substrate FRAME, see the example pallet
 /// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
 use frame_system::ensure_signed;
 
 #[cfg(test)]
