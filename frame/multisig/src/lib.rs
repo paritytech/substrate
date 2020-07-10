bb9260808936194adbe0891ec063754392a033b5
Alexander Popiak <alexander.popiak@parity.io>
2020-07-10 12:20:39 +0200

    extract multisig migration into module

--- frame/multisig/src/lib.rs
+++ frame/multisig/src/lib.rs
@@ -59,6 +59,7 @@ use sp_runtime::{DispatchError, DispatchResult, traits::{Dispatchable, Zero}};
 
 mod tests;
 mod benchmarking;
+pub mod migration;
 
 type BalanceOf<T> = <<T as Trait>::Currency as Currency<<T as frame_system::Trait>::AccountId>>::Balance;
 
@@ -235,20 +236,8 @@ decl_module! {
 		/// Deposit one of this module's events by using the default implementation.
 		fn deposit_event() = default;
 
-		// Migration necessary for the Edgeware migration.
-		// Created in the [stacked filtering PR](https://github.com/paritytech/substrate/pull/6273/files#diff-9967c6d85464599e91d4626b3869c66bR209)
-		// and removed [here](https://github.com/paritytech/substrate/pull/6476/files#diff-9967c6d85464599e91d4626b3869c66bL238)
 		fn on_runtime_upgrade() -> Weight {
-			// Utility.Multisigs -> Multisig.Multisigs
-			use frame_support::migration::{StorageIterator, put_storage_value};
-			sp_runtime::print("🕊️  Migrating Multisigs...");
-			for (key, value) in StorageIterator::<
-				Multisig<T::BlockNumber, BalanceOf<T>, T::AccountId>
-			>::new(b"Utility", b"Multisigs").drain() {
-				put_storage_value(b"Multisig", b"Multisigs", &key, value);
-			}
-			sp_runtime::print("🕊️  Done Multisigs.");
-			1_000_000_000
+			migration::migrate_utility_to_multisigs::<T>()
 		}
 
 		/// Immediately dispatch a multi-signature call using a single approval from the caller.
