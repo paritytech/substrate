b48be7b1f6dfbb76ae425405371842d20222c780
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-07-14 01:04:38 +0200

    Update benchmarking macro for no components

--- frame/benchmarking/src/tests.rs
+++ frame/benchmarking/src/tests.rs
@@ -167,6 +167,10 @@ benchmarks!{
 	verify {
 		ensure!(m[0] == 0, "You forgot to sort!")
 	}
+
+	no_components {
+		let caller = account::<T::AccountId>("caller", 0, 0);
+	}: set_value(RawOrigin::Signed(caller), 0)
 }
 
 #[test]
@@ -242,5 +246,6 @@ fn benchmarks_generate_unit_tests() {
 		assert_ok!(test_benchmark_sort_vector::<Test>());
 		assert_err!(test_benchmark_bad_origin::<Test>(), "Bad origin");
 		assert_err!(test_benchmark_bad_verify::<Test>(), "You forgot to sort!");
+		assert_ok!(test_benchmark_no_components::<Test>());
 	});
 }
