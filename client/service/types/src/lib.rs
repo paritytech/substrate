7744a9ed4514330587f5fd60a9a0e5bccf0d96c1
Ashley Ruglys <ashley.ruglys@gmail.com>
2020-07-20 10:57:51 +0200

    Improve service types

--- client/service/types/src/lib.rs
+++ client/service/types/src/lib.rs
@@ -14,6 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
+//! Type definitions to make building a service easier.
+
+#![warn(missing_docs)]
+
 pub use sc_service as _service;
 pub use sc_finality_grandpa as _grandpa;
 pub use sc_consensus as _consensus;
@@ -23,63 +27,114 @@ pub use sc_transaction_pool as _transaction_pool;
 pub use sc_network as _network;
 pub use sp_api as _sp_api;
 
+use {
+    _service as service, _grandpa as grandpa, _consensus as consensus, _aura as aura,
+    _transaction_pool as transaction_pool, _sp_api as sp_api, _network as network,
+    _babe as babe,
+};
+
+/// Type definitions for a full client.
+pub mod full {
+    use super::*;
+
+    pub use service::TFullClient as Client;
+    pub use service::TFullBackend as Backend;
+
+    /// A GRANDPA block import.
+    pub type GrandpaBlockImport<Block, RuntimeApi, Executor, SelectChain> = grandpa::GrandpaBlockImport<
+        Backend<Block>, Block, Client<Block, RuntimeApi, Executor>, SelectChain
+    >;
+    /// A GRANDPA link. Connects the block import to the GRANDPA service.
+    pub type GrandpaLink<Block, RuntimeApi, Executor, SelectChain> = grandpa ::LinkHalf<
+        Block, Client<Block, RuntimeApi, Executor>, SelectChain
+    >;
+    /// A basic select chain implementation.
+    pub type LongestChain<Block> = consensus::LongestChain<Backend<Block>, Block>;
+    /// A basic transaction pool.
+    pub type BasicPool<Block, RuntimeApi, Executor> = transaction_pool::BasicPool<
+        transaction_pool::FullChainApi<Client<Block, RuntimeApi, Executor>, Block>, Block
+    >;
+    /// An import queue for AURA.
+    pub type AuraImportQueue<Block, RuntimeApi, Executor> = aura::AuraImportQueue<
+        Block, sp_api::TransactionFor<Client<Block, RuntimeApi, Executor>, Block>
+    >;
+    /// An import queue for BABE.
+    pub type BabeImportQueue<Block, RuntimeApi, Executor> = babe::BabeImportQueue<
+        Block, sp_api::TransactionFor<Client<Block, RuntimeApi, Executor>, Block>
+    >;
+    /// A block import for BABE. Wraps around another block import.
+    pub type BabeBlockImport<Block, RuntimeApi, Executor, BlockImport> = babe::BabeBlockImport<
+        Block, Client<Block, RuntimeApi, Executor>, BlockImport
+    >;
+}
+
+/// Type definitions for a light client.
+pub mod light {
+    use super::*;
+
+    pub use service::TLightClient as Client;
+    pub use service::TLightBackend as Backend;
+
+    /// A network fetcher for a light client.
+    pub type Fetcher<Block> = network::config::OnDemand<Block>;
+
+    /// A basic transaction pool.
+    pub type BasicPool<Block, RuntimeApi, Executor> = transaction_pool::BasicPool<
+        transaction_pool::LightChainApi<Client<Block, RuntimeApi, Executor>, Fetcher<Block>, Block>,
+        Block
+    >;
+}
+
+/// Setup the type definitions given a `Block`, `RuntimeApi`and `Executor`.
 #[macro_export]
 macro_rules! setup_types {
     ($block:ty, $runtime_api:ty, $executor:ty) => {
-        use $crate::{
-            _service as service, _grandpa as grandpa, _consensus as consensus, _aura as aura,
-            _transaction_pool as transaction_pool, _sp_api as sp_api, _network as network,
-            _babe as babe,
-        };
-
         /// Type definitions for a full client.
         pub mod full {
+            use $crate::full;
             use super::*;
 
             /// A full client.
-            pub type Client = service::TFullClient<$block, $runtime_api, $executor>;
+            pub type Client = full::Client<$block, $runtime_api, $executor>;
             /// A full backend.
-            pub type Backend = service::TFullBackend<$block>;
+            pub type Backend = full::Backend<$block>;
             /// A GRANDPA block import.
-            pub type GrandpaBlockImport<SelectChain> = grandpa::GrandpaBlockImport<
-                Backend, $block, Client, SelectChain
+            pub type GrandpaBlockImport<SelectChain> = full::GrandpaBlockImport<
+                $block, $runtime_api, $executor, SelectChain
             >;
             /// A GRANDPA link. Connects the block import to the GRANDPA service.
-            pub type GrandpaLink<SelectChain> = grandpa ::LinkHalf<
-                $block, Client, SelectChain
+            pub type GrandpaLink<SelectChain> = full::GrandpaLink<
+                $block, $runtime_api, $executor, SelectChain
             >;
             /// A basic select chain implementation.
-            pub type LongestChain = consensus::LongestChain<Backend, $block>;
+            pub type LongestChain = full::LongestChain<$block>;
             /// A basic transaction pool.
-            pub type BasicPool = transaction_pool::BasicPool<
-                transaction_pool::FullChainApi<Client, $block>, $block
-            >;
+            pub type BasicPool = $crate::BasicPool<$block, $runtime_api, $executor>;
             /// An import queue for AURA.
-            pub type AuraImportQueue = aura::AuraImportQueue<
-                $block, sp_api::TransactionFor<Client, $block>
+            pub type AuraImportQueue = full::AuraImportQueue<
+                $block, $runtime_api, $executor
             >;
             /// An import queue for BABE.
-            pub type BabeImportQueue = babe::BabeImportQueue<
-                $block, sp_api::TransactionFor<Client, $block>
+            pub type BabeImportQueue = full::BabeImportQueue<
+                $block, $runtime_api, $executor
             >;
             /// A block import for BABE. Wraps around another block import.
-            pub type BabeBlockImport<BlockImport> = babe::BabeBlockImport<
-                $block, Client, BlockImport
+            pub type BabeBlockImport<BlockImport> = full::BabeBlockImport<
+                $block, $runtime_api, $executor, BlockImport
             >;
         }
         
         /// Type definitions for a light client.
         pub mod light {
+            use $crate::light;
             use super::*;
 
             /// A light client.
-            pub type Client = service::TLightClient<$block, $runtime_api, $executor>;
+            pub type Client = light::Client<$block, $runtime_api, $executor>;
             /// A network fetcher for a light client.
-            pub type Fetcher = network::config::OnDemand<$block>;
+            pub type Fetcher = light::Fetcher<$block>;
             /// A basic transaction pool.
-            pub type BasicPool = transaction_pool::BasicPool<
-                transaction_pool::LightChainApi<Client, Fetcher, $block>, $block
-            >;
+            pub type BasicPool = light::BasicPool<$block, $runtime_api, $executor>;
         }       
     }
 }
