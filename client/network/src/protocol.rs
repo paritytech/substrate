9fe3166b185a87161fb5d8e6754e69ee4e6298a6
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-07-22 13:58:27 +0200

    Remove the legacy requests-answering protocols

--- client/network/src/protocol.rs
+++ client/network/src/protocol.rs
@@ -18,7 +18,7 @@
 
 use crate::{
 	ExHashT,
-	chain::{Client, FinalityProofProvider},
+	chain::Client,
 	config::{BoxFinalityProofRequestBuilder, ProtocolId, TransactionPool, TransactionImportFuture, TransactionImport},
 	error,
 	utils::interval
@@ -31,10 +31,6 @@ use libp2p::{Multiaddr, PeerId};
 use libp2p::core::{ConnectedPoint, connection::{ConnectionId, ListenerId}};
 use libp2p::swarm::{ProtocolsHandler, IntoProtocolsHandler};
 use libp2p::swarm::{NetworkBehaviour, NetworkBehaviourAction, PollParameters};
-use sp_core::{
-	storage::{StorageKey, PrefixedStorageKey, ChildInfo, ChildType},
-	hexdisplay::HexDisplay
-};
 use sp_consensus::{
 	BlockOrigin,
 	block_validation::BlockAnnounceValidator,
@@ -43,7 +39,7 @@ use sp_consensus::{
 use codec::{Decode, Encode};
 use sp_runtime::{generic::BlockId, ConsensusEngineId, Justification};
 use sp_runtime::traits::{
-	Block as BlockT, Header as HeaderT, NumberFor, One, Zero, CheckedSub
+	Block as BlockT, Header as HeaderT, NumberFor, Zero, CheckedSub
 };
 use sp_arithmetic::traits::SaturatedConversion;
 use message::{BlockAnnounce, Message};
@@ -51,12 +47,11 @@ use message::generic::{Message as GenericMessage, ConsensusMessage, Roles};
 use prometheus_endpoint::{Registry, Gauge, Counter, GaugeVec, HistogramVec, PrometheusError, Opts, register, U64};
 use sync::{ChainSync, SyncState};
 use std::borrow::Cow;
-use std::collections::{BTreeMap, HashMap, HashSet, VecDeque, hash_map::Entry};
+use std::collections::{HashMap, HashSet, VecDeque, hash_map::Entry};
 use std::sync::Arc;
 use std::fmt::Write;
-use std::{cmp, io, num::NonZeroUsize, pin::Pin, task::Poll, time};
+use std::{io, num::NonZeroUsize, pin::Pin, task::Poll, time};
 use log::{log, Level, trace, debug, warn, error};
-use sc_client_api::{ChangesProof, StorageProof};
 use util::LruHashSet;
 use wasm_timer::Instant;
 
@@ -90,8 +85,6 @@ pub(crate) const CURRENT_VERSION: u32 = 6;
 /// Lowest version we support
 pub(crate) const MIN_VERSION: u32 = 3;
 
-// Maximum allowed entries in `BlockResponse`
-const MAX_BLOCK_DATA_RESPONSE: u32 = 128;
 /// When light node connects to the full node and the full node is behind light node
 /// for at least `LIGHT_MAXIMAL_BLOCKS_DIFFERENCE` blocks, we consider it not useful
 /// and disconnect to free connection slot.
@@ -114,16 +107,12 @@ mod rep {
 	pub const GOOD_TRANSACTION: Rep = Rep::new(1 << 7, "Good transaction");
 	/// Reputation change when a peer sends us a bad transaction.
 	pub const BAD_TRANSACTION: Rep = Rep::new(-(1 << 12), "Bad transaction");
-	/// We sent an RPC query to the given node, but it failed.
-	pub const RPC_FAILED: Rep = Rep::new(-(1 << 12), "Remote call failed");
 	/// We received a message that failed to decode.
 	pub const BAD_MESSAGE: Rep = Rep::new(-(1 << 12), "Bad message");
 	/// We received an unexpected response.
 	pub const UNEXPECTED_RESPONSE: Rep = Rep::new_fatal("Unexpected response packet");
 	/// We received an unexpected transaction packet.
 	pub const UNEXPECTED_TRANSACTIONS: Rep = Rep::new_fatal("Unexpected transactions packet");
-	/// We received an unexpected light node request.
-	pub const UNEXPECTED_REQUEST: Rep = Rep::new_fatal("Unexpected block request packet");
 	/// Peer has different genesis.
 	pub const GENESIS_MISMATCH: Rep = Rep::new_fatal("Genesis mismatch");
 	/// Peer is on unsupported protocol version.
@@ -142,7 +131,6 @@ struct Metrics {
 	finality_proofs: GaugeVec<U64>,
 	justifications: GaugeVec<U64>,
 	propagated_transactions: Counter<U64>,
-	legacy_requests_received: Counter<U64>,
 }
 
 impl Metrics {
@@ -187,10 +175,6 @@ impl Metrics {
 			propagated_transactions: register(Counter::new(
 				"sync_propagated_transactions",
 				"Number of transactions propagated to at least one peer",
-			)?, r)?,
-			legacy_requests_received: register(Counter::new(
-				"sync_legacy_requests_received",
-				"Number of block/finality/light-client requests received on the legacy substream",
 			)?, r)?,
 		})
 	}
@@ -242,8 +226,6 @@ pub struct Protocol<B: BlockT, H: ExHashT> {
 	/// Used to report reputation changes.
 	peerset_handle: sc_peerset::PeersetHandle,
 	transaction_pool: Arc<dyn TransactionPool<H, B>>,
-	/// When asked for a proof of finality, we use this struct to build one.
-	finality_proof_provider: Option<Arc<dyn FinalityProofProvider<B>>>,
 	/// Handles opening the unique substream and sending and receiving raw messages.
 	behaviour: GenericProto,
 	/// For each legacy gossiping engine ID, the corresponding new protocol name.
@@ -381,7 +363,6 @@ impl<B: BlockT, H: ExHashT> Protocol<B, H> {
 		local_peer_id: PeerId,
 		chain: Arc<dyn Client<B>>,
 		transaction_pool: Arc<dyn TransactionPool<H, B>>,
-		finality_proof_provider: Option<Arc<dyn FinalityProofProvider<B>>>,
 		finality_proof_request_builder: Option<BoxFinalityProofRequestBuilder<B>>,
 		protocol_id: ProtocolId,
 		peerset_config: sc_peerset::PeersetConfig,
@@ -459,7 +440,6 @@ impl<B: BlockT, H: ExHashT> Protocol<B, H> {
 			sync,
 			important_peers,
 			transaction_pool,
-			finality_proof_provider,
 			peerset_handle: peerset_handle.clone(),
 			behaviour,
 			protocol_name_by_engine: HashMap::new(),
@@ -608,7 +588,6 @@ impl<B: BlockT, H: ExHashT> Protocol<B, H> {
 		match message {
 			GenericMessage::Status(_) =>
 				debug!(target: "sub-libp2p", "Received unexpected Status"),
-			GenericMessage::BlockRequest(r) => self.on_block_request(who, r),
 			GenericMessage::BlockResponse(r) => {
 				let outcome = self.on_block_response(who.clone(), r);
 				self.update_peer_info(&who);
@@ -621,27 +600,31 @@ impl<B: BlockT, H: ExHashT> Protocol<B, H> {
 			},
 			GenericMessage::Transactions(m) =>
 				self.on_transactions(who, m),
-			GenericMessage::RemoteCallRequest(request) => self.on_remote_call_request(who, request),
 			GenericMessage::RemoteCallResponse(_) =>
 				warn!(target: "sub-libp2p", "Received unexpected RemoteCallResponse"),
-			GenericMessage::RemoteReadRequest(request) =>
-				self.on_remote_read_request(who, request),
 			GenericMessage::RemoteReadResponse(_) =>
 				warn!(target: "sub-libp2p", "Received unexpected RemoteReadResponse"),
-			GenericMessage::RemoteHeaderRequest(request) =>
-				self.on_remote_header_request(who, request),
 			GenericMessage::RemoteHeaderResponse(_) =>
 				warn!(target: "sub-libp2p", "Received unexpected RemoteHeaderResponse"),
-			GenericMessage::RemoteChangesRequest(request) =>
-				self.on_remote_changes_request(who, request),
 			GenericMessage::RemoteChangesResponse(_) =>
 				warn!(target: "sub-libp2p", "Received unexpected RemoteChangesResponse"),
-			GenericMessage::FinalityProofRequest(request) =>
-				self.on_finality_proof_request(who, request),
-			GenericMessage::FinalityProofResponse(response) =>
-				return self.on_finality_proof_response(who, response),
-			GenericMessage::RemoteReadChildRequest(request) =>
-				self.on_remote_read_child_request(who, request),
+			GenericMessage::FinalityProofResponse(_) =>
+				warn!(target: "sub-libp2p", "Received unexpected FinalityProofResponse"),
+			GenericMessage::FinalityProofRequest(_) |
+			GenericMessage::BlockRequest(_) |
+			GenericMessage::RemoteReadChildRequest(_) |
+			GenericMessage::RemoteCallRequest(_) |
+			GenericMessage::RemoteReadRequest(_) |
+			GenericMessage::RemoteHeaderRequest(_) |
+			GenericMessage::RemoteChangesRequest(_) => {
+				debug!(
+					target: "sub-libp2p",
+					"Received no longer supported legacy request from {:?}",
+					who
+				);
+				self.disconnect_peer(&who);
+				self.peerset_handle.report_peer(who, rep::BAD_PROTOCOL);
+			},
 			GenericMessage::Consensus(msg) =>
 				return if self.protocol_name_by_engine.contains_key(&msg.engine_id) {
 					CustomMessageOutcome::NotificationsReceived {
@@ -679,21 +662,6 @@ impl<B: BlockT, H: ExHashT> Protocol<B, H> {
 		CustomMessageOutcome::None
 	}
 
-	fn send_message(
-		&mut self,
-		who: &PeerId,
-		message: Option<(Cow<'static, [u8]>, Vec<u8>)>,
-		legacy: Message<B>,
-	) {
-		send_message::<B>(
-			&mut self.behaviour,
-			&mut self.context_data.stats,
-			who,
-			message,
-			legacy,
-		);
-	}
-
 	fn update_peer_request(&mut self, who: &PeerId, request: &mut message::BlockRequest<B>) {
 		update_peer_request::<B, H>(&mut self.context_data.peers, who, request)
 	}
@@ -717,90 +685,6 @@ impl<B: BlockT, H: ExHashT> Protocol<B, H> {
 		} else {
 			CustomMessageOutcome::None
 		}
-	}
-
-	fn on_block_request(&mut self, peer: PeerId, request: message::BlockRequest<B>) {
-		if let Some(metrics) = &self.metrics {
-			metrics.legacy_requests_received.inc();
-		}
-
-		trace!(target: "sync", "BlockRequest {} from {}: from {:?} to {:?} max {:?} for {:?}",
-			request.id,
-			peer,
-			request.from,
-			request.to,
-			request.max,
-			request.fields,
-		);
-
-		// sending block requests to the node that is unable to serve it is considered a bad behavior
-		if !self.config.roles.is_full() {
-			trace!(target: "sync", "Peer {} is trying to sync from the light node", peer);
-			self.behaviour.disconnect_peer(&peer);
-			self.peerset_handle.report_peer(peer, rep::UNEXPECTED_REQUEST);
-			return;
-		}
-
-		let mut blocks = Vec::new();
-		let mut id = match request.from {
-			message::FromBlock::Hash(h) => BlockId::Hash(h),
-			message::FromBlock::Number(n) => BlockId::Number(n),
-		};
-		let max = cmp::min(request.max.unwrap_or(u32::max_value()), MAX_BLOCK_DATA_RESPONSE) as usize;
-		let get_header = request.fields.contains(message::BlockAttributes::HEADER);
-		let get_body = request.fields.contains(message::BlockAttributes::BODY);
-		let get_justification = request
-			.fields
-			.contains(message::BlockAttributes::JUSTIFICATION);
-		while let Some(header) = self.context_data.chain.header(id).unwrap_or(None) {
-			if blocks.len() >= max {
-				break;
-			}
-			let number = *header.number();
-			let hash = header.hash();
-			let parent_hash = *header.parent_hash();
-			let justification = if get_justification {
-				self.context_data.chain.justification(&BlockId::Hash(hash)).unwrap_or(None)
-			} else {
-				None
-			};
-			let block_data = message::generic::BlockData {
-				hash,
-				header: if get_header { Some(header) } else { None },
-				body: if get_body {
-					self.context_data
-						.chain
-						.block_body(&BlockId::Hash(hash))
-						.unwrap_or(None)
-				} else {
-					None
-				},
-				receipt: None,
-				message_queue: None,
-				justification,
-			};
-			// Stop if we don't have requested block body
-			if get_body && block_data.body.is_none() {
-				trace!(target: "sync", "Missing data for block request.");
-				break;
-			}
-			blocks.push(block_data);
-			match request.direction {
-				message::Direction::Ascending => id = BlockId::Number(number + One::one()),
-				message::Direction::Descending => {
-					if number.is_zero() {
-						break;
-					}
-					id = BlockId::Hash(parent_hash)
-				}
-			}
-		}
-		let response = message::generic::BlockResponse {
-			id: request.id,
-			blocks,
-		};
-		trace!(target: "sync", "Sending BlockResponse with {} blocks", response.blocks.len());
-		self.send_message(&peer, None, GenericMessage::BlockResponse(response))
 	}
 
 	/// Adjusts the reputation of a node.
@@ -1397,51 +1281,6 @@ impl<B: BlockT, H: ExHashT> Protocol<B, H> {
 		self.sync.on_block_finalized(&hash, *header.number())
 	}
 
-	fn on_remote_call_request(
-		&mut self,
-		who: PeerId,
-		request: message::RemoteCallRequest<B::Hash>,
-	) {
-		trace!(target: "sync", "Remote call request {} from {} ({} at {})",
-			request.id,
-			who,
-			request.method,
-			request.block
-		);
-
-		if let Some(metrics) = &self.metrics {
-			metrics.legacy_requests_received.inc();
-		}
-
-		let proof = match self.context_data.chain.execution_proof(
-			&BlockId::Hash(request.block),
-			&request.method,
-			&request.data,
-		) {
-			Ok((_, proof)) => proof,
-			Err(error) => {
-				trace!(target: "sync", "Remote call request {} from {} ({} at {}) failed with: {}",
-					request.id,
-					who,
-					request.method,
-					request.block,
-					error
-				);
-				self.peerset_handle.report_peer(who.clone(), rep::RPC_FAILED);
-				StorageProof::empty()
-			}
-		};
-
-		self.send_message(
-			&who,
-			None,
-			GenericMessage::RemoteCallResponse(message::RemoteCallResponse {
-				id: request.id,
-				proof,
-			}),
-		);
-	}
-
 	/// Request a justification for the given block.
 	///
 	/// Uses `protocol` to queue a new justification request and tries to dispatch all pending
@@ -1526,257 +1365,6 @@ impl<B: BlockT, H: ExHashT> Protocol<B, H> {
 		finalization_result: Result<(B::Hash, NumberFor<B>), ()>,
 	) {
 		self.sync.on_finality_proof_import(request_block, finalization_result)
-	}
-
-	fn on_remote_read_request(
-		&mut self,
-		who: PeerId,
-		request: message::RemoteReadRequest<B::Hash>,
-	) {
-		if let Some(metrics) = &self.metrics {
-			metrics.legacy_requests_received.inc();
-		}
-
-		if request.keys.is_empty() {
-			debug!(target: "sync", "Invalid remote read request sent by {}", who);
-			self.behaviour.disconnect_peer(&who);
-			self.peerset_handle.report_peer(who, rep::BAD_MESSAGE);
-			return;
-		}
-
-		let keys_str = || match request.keys.len() {
-			1 => HexDisplay::from(&request.keys[0]).to_string(),
-			_ => format!(
-				"{}..{}",
-				HexDisplay::from(&request.keys[0]),
-				HexDisplay::from(&request.keys[request.keys.len() - 1]),
-			),
-		};
-
-		trace!(target: "sync", "Remote read request {} from {} ({} at {})",
-			request.id, who, keys_str(), request.block);
-		let proof = match self.context_data.chain.read_proof(
-			&BlockId::Hash(request.block),
-			&mut request.keys.iter().map(AsRef::as_ref)
-		) {
-			Ok(proof) => proof,
-			Err(error) => {
-				trace!(target: "sync", "Remote read request {} from {} ({} at {}) failed with: {}",
-					request.id,
-					who,
-					keys_str(),
-					request.block,
-					error
-				);
-				StorageProof::empty()
-			}
-		};
-		self.send_message(
-			&who,
-			None,
-			GenericMessage::RemoteReadResponse(message::RemoteReadResponse {
-				id: request.id,
-				proof,
-			}),
-		);
-	}
-
-	fn on_remote_read_child_request(
-		&mut self,
-		who: PeerId,
-		request: message::RemoteReadChildRequest<B::Hash>,
-	) {
-		if let Some(metrics) = &self.metrics {
-			metrics.legacy_requests_received.inc();
-		}
-
-		if request.keys.is_empty() {
-			debug!(target: "sync", "Invalid remote child read request sent by {}", who);
-			self.behaviour.disconnect_peer(&who);
-			self.peerset_handle.report_peer(who, rep::BAD_MESSAGE);
-			return;
-		}
-
-		let keys_str = || match request.keys.len() {
-			1 => HexDisplay::from(&request.keys[0]).to_string(),
-			_ => format!(
-				"{}..{}",
-				HexDisplay::from(&request.keys[0]),
-				HexDisplay::from(&request.keys[request.keys.len() - 1]),
-			),
-		};
-
-		trace!(target: "sync", "Remote read child request {} from {} ({} {} at {})",
-			request.id, who, HexDisplay::from(&request.storage_key), keys_str(), request.block);
-		let prefixed_key = PrefixedStorageKey::new_ref(&request.storage_key);
-		let child_info = match ChildType::from_prefixed_key(prefixed_key) {
-			Some((ChildType::ParentKeyId, storage_key)) => Ok(ChildInfo::new_default(storage_key)),
-			None => Err("Invalid child storage key".into()),
-		};
-		let proof = match child_info.and_then(|child_info| self.context_data.chain.read_child_proof(
-			&BlockId::Hash(request.block),
-			&child_info,
-			&mut request.keys.iter().map(AsRef::as_ref),
-		)) {
-			Ok(proof) => proof,
-			Err(error) => {
-				trace!(target: "sync", "Remote read child request {} from {} ({} {} at {}) failed with: {}",
-					request.id,
-					who,
-					HexDisplay::from(&request.storage_key),
-					keys_str(),
-					request.block,
-					error
-				);
-				StorageProof::empty()
-			}
-		};
-		self.send_message(
-			&who,
-			None,
-			GenericMessage::RemoteReadResponse(message::RemoteReadResponse {
-				id: request.id,
-				proof,
-			}),
-		);
-	}
-
-	fn on_remote_header_request(
-		&mut self,
-		who: PeerId,
-		request: message::RemoteHeaderRequest<NumberFor<B>>,
-	) {
-		if let Some(metrics) = &self.metrics {
-			metrics.legacy_requests_received.inc();
-		}
-
-		trace!(target: "sync", "Remote header proof request {} from {} ({})",
-			request.id, who, request.block);
-		let (header, proof) = match self.context_data.chain.header_proof(&BlockId::Number(request.block)) {
-			Ok((header, proof)) => (Some(header), proof),
-			Err(error) => {
-				trace!(target: "sync", "Remote header proof request {} from {} ({}) failed with: {}",
-					request.id,
-					who,
-					request.block,
-					error
-				);
-				(Default::default(), StorageProof::empty())
-			}
-		};
-		self.send_message(
-			&who,
-			None,
-			GenericMessage::RemoteHeaderResponse(message::RemoteHeaderResponse {
-				id: request.id,
-				header,
-				proof,
-			}),
-		);
-	}
-
-	fn on_remote_changes_request(
-		&mut self,
-		who: PeerId,
-		request: message::RemoteChangesRequest<B::Hash>,
-	) {
-		if let Some(metrics) = &self.metrics {
-			metrics.legacy_requests_received.inc();
-		}
-
-		trace!(target: "sync", "Remote changes proof request {} from {} for key {} ({}..{})",
-			request.id,
-			who,
-			if let Some(sk) = request.storage_key.as_ref() {
-				format!("{} : {}", HexDisplay::from(sk), HexDisplay::from(&request.key))
-			} else {
-				HexDisplay::from(&request.key).to_string()
-			},
-			request.first,
-			request.last
-		);
-		let key = StorageKey(request.key);
-		let prefixed_key =  request.storage_key.as_ref()
-			.map(|storage_key| PrefixedStorageKey::new_ref(storage_key));
-		let (first, last, min, max) = (request.first, request.last, request.min, request.max);
-		let proof = match self.context_data.chain.key_changes_proof(
-			first,
-			last,
-			min,
-			max,
-			prefixed_key,
-			&key,
-		) {
-			Ok(proof) => proof,
-			Err(error) => {
-				trace!(target: "sync", "Remote changes proof request {} from {} for key {} ({}..{}) failed with: {}",
-					request.id,
-					who,
-					if let Some(sk) = request.storage_key.as_ref() {
-						format!("{} : {}", HexDisplay::from(sk), HexDisplay::from(&key.0))
-					} else {
-						HexDisplay::from(&key.0).to_string()
-					},
-					request.first,
-					request.last,
-					error
-				);
-				ChangesProof::<B::Header> {
-					max_block: Zero::zero(),
-					proof: vec![],
-					roots: BTreeMap::new(),
-					roots_proof: StorageProof::empty(),
-				}
-			}
-		};
-		self.send_message(
-			&who,
-			None,
-			GenericMessage::RemoteChangesResponse(message::RemoteChangesResponse {
-				id: request.id,
-				max: proof.max_block,
-				proof: proof.proof,
-				roots: proof.roots.into_iter().collect(),
-				roots_proof: proof.roots_proof,
-			}),
-		);
-	}
-
-	fn on_finality_proof_request(
-		&mut self,
-		who: PeerId,
-		request: message::FinalityProofRequest<B::Hash>,
-	) {
-		if let Some(metrics) = &self.metrics {
-			metrics.legacy_requests_received.inc();
-		}
-
-		trace!(target: "sync", "Finality proof request from {} for {}", who, request.block);
-		let finality_proof = self.finality_proof_provider.as_ref()
-			.ok_or_else(|| String::from("Finality provider is not configured"))
-			.and_then(|provider|
-				provider.prove_finality(request.block, &request.request).map_err(|e| e.to_string())
-			);
-		let finality_proof = match finality_proof {
-			Ok(finality_proof) => finality_proof,
-			Err(error) => {
-				trace!(target: "sync", "Finality proof request from {} for {} failed with: {}",
-					who,
-					request.block,
-					error
-				);
-				None
-			},
-		};
-		self.send_message(
-			&who,
-			None,
-			GenericMessage::FinalityProofResponse(message::FinalityProofResponse {
-				id: 0,
-				block: request.block,
-				proof: finality_proof,
-			}),
-		);
 	}
 
 	/// Must be called after a [`CustomMessageOutcome::FinalityProofRequest`] has been emitted,
