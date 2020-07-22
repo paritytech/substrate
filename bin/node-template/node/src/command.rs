17922fe47af9992abe0b4a74469df5d80980159b
Wei Tang <wei@that.world>
2020-07-22 21:40:11 +0200

    Better handling of stable-only build (#6569)
    * Better handling of stable-only build
    
    * Fix node template build
    
    * Fix wasm builder node-template version mismatch
    
    * Fix load_spec error
    
    * Add , in parameter
    
    * Add descrptive panic messages in tests
    
    * Add descriptive tests in node/executor benches
    
    * Fix missing compact_code_unwrap
    
    * Add missing wasm_binary_unwrap function for executor integration test
    
    * Only define import_sp_io in no_std
    
    * Small Cargo.toml styling fix
    
    * Bump wasm-builder to 2.0.0
    
    * Fix all `with_wasm_builder_from_crates` version in Substrate
    
    * Use `with_wasm_builder_from_crates` for node-template
    
    Co-authored-by: Gavin Wood <gavin@parity.io>

--- bin/node-template/node/src/command.rs
+++ bin/node-template/node/src/command.rs
@@ -49,8 +49,8 @@ impl SubstrateCli for Cli {
 
 	fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
 		Ok(match id {
-			"dev" => Box::new(chain_spec::development_config()),
-			"" | "local" => Box::new(chain_spec::local_testnet_config()),
+			"dev" => Box::new(chain_spec::development_config()?),
+			"" | "local" => Box::new(chain_spec::local_testnet_config()?),
 			path => Box::new(chain_spec::ChainSpec::from_json_file(
 				std::path::PathBuf::from(path),
 			)?),
