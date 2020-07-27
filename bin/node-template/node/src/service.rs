489d1d23db3315a668a7d1abf24221caece669bc
Ashley Ruglys <ashley.ruglys@gmail.com>
2020-07-27 10:40:51 +0200

    Use node_template_runtime::opaque::Block instead of node_template_runtime::Block

--- bin/node-template/node/src/service.rs
+++ bin/node-template/node/src/service.rs
@@ -3,7 +3,7 @@
 use std::sync::Arc;
 use std::time::Duration;
 use sc_client_api::{ExecutorProvider, RemoteBackend};
-use node_template_runtime::{self, Block, RuntimeApi};
+use node_template_runtime::{self, opaque::Block, RuntimeApi};
 use sc_service::{error::Error as ServiceError, Configuration, ServiceComponents, TaskManager};
 use sp_inherents::InherentDataProviders;
 use sc_executor::native_executor_instance;
