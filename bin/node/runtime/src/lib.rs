b06342a9ffd2f0923429736e473b46ac69c2ad9e
Alexander Theißen <alexander.theissen@parity.io>
2020-07-27 08:21:36 +0200

    Bump runtime

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -105,7 +105,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 256,
+	spec_version: 257,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
