86cba45d410532664a3a984f396c7c7b46ecf53a
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-07-29 10:48:50 +0200

    512 -> 2048

--- client/network/src/protocol/generic_proto/handler/group.rs
+++ client/network/src/protocol/generic_proto/handler/group.rs
@@ -76,7 +76,7 @@ use std::{borrow::Cow, error, io, str, sync::Arc, task::{Context, Poll}};
 /// See [`NotificationsSink::reserve_notification`] for context.
 const ASYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 8;
 /// Number of pending notifications in synchronous contexts.
-const SYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 512;
+const SYNC_NOTIFICATIONS_BUFFER_SIZE: usize = 2048;
 
 /// Implements the `IntoProtocolsHandler` trait of libp2p.
 ///
