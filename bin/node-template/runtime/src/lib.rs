24b9005df08b1e6c78e2cd3f2e15cb8dd481bce5
Joshy Orndorff <admin@joshyorndorff.com>
2020-08-05 14:41:10 -0400

    Fix line length

--- bin/node-template/runtime/src/lib.rs
+++ bin/node-template/runtime/src/lib.rs
@@ -307,7 +307,13 @@ pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signatu
 /// Extrinsic type that has already been checked.
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, Call, SignedExtra>;
 /// Executive: handles dispatch to the various modules.
-pub type Executive = frame_executive::Executive<Runtime, Block, frame_system::ChainContext<Runtime>, Runtime, AllModules>;
+pub type Executive = frame_executive::Executive<
+	Runtime,
+	Block,
+	frame_system::ChainContext<Runtime>,
+	Runtime,
+	AllModules,
+>;
 
 impl_runtime_apis! {
 	impl sp_api::Core<Block> for Runtime {
