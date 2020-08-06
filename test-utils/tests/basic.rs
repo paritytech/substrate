f0c0e7f41a95b6f7f690747db45d233a26d2fda9
Cecile Tonglet <cecile@parity.io>
2020-08-06 11:22:23 +0200

    WIP
    Forked at: 60e3a693b29789045614e2ed73126695bc8b0794
    Parent branch: origin/master

--- test-utils/tests/basic.rs
+++ test-utils/tests/basic.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use sc_service::TaskExecutor;
+use sc_service::{TaskExecutor, TaskType};
 
 #[substrate_test_utils::test]
 async fn basic_test(_: TaskExecutor) {
