f17569fa4c7fa930eab1dd07a359fde7e3019093
Joshy Orndorff <JoshOrndorff@users.noreply.github.com>
2020-08-06 15:23:31 -0400

    De-alias pallets in node template runtime (#6836)
    * dealias pallets
    
    * Restore accidentally deleted code blocks

--- bin/node-template/node/src/chain_spec.rs
+++ bin/node-template/node/src/chain_spec.rs
@@ -139,17 +139,17 @@ fn testnet_genesis(
 			code: wasm_binary.to_vec(),
 			changes_trie_config: Default::default(),
 		}),
-		balances: Some(BalancesConfig {
+		pallet_balances: Some(BalancesConfig {
 			// Configure endowed accounts with initial balance of 1 << 60.
 			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
 		}),
-		aura: Some(AuraConfig {
+		pallet_aura: Some(AuraConfig {
 			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
 		}),
-		grandpa: Some(GrandpaConfig {
+		pallet_grandpa: Some(GrandpaConfig {
 			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
 		}),
-		sudo: Some(SudoConfig {
+		pallet_sudo: Some(SudoConfig {
 			// Assign network admin rights.
 			key: root_key,
 		}),
