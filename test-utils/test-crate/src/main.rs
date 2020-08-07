badbec37d95542f7328bd98ffa0dcd2b709019cb
Cecile Tonglet <cecile@parity.io>
2020-08-07 11:20:23 +0200

    fix test

--- test-utils/test-crate/src/main.rs
+++ test-utils/test-crate/src/main.rs
@@ -16,6 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+#[cfg(test)]
 #[test_utils::test]
 async fn basic_test(_: sc_service::TaskExecutor) {
 	assert!(true);
