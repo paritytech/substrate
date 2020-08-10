9f87045deba525e0021a346d5c16193e147dbed6
RRTTI <raul@ost.com>
2020-08-10 15:46:13 +0200

    Update lib.rs
    - Include on `Assign_curator`, `accept_curator` and `unassign_curator` on Bounties Protocol Section 
    - Include curator fee and curator deposit definitions on Terminology
    - Update intro.

--- frame/treasury/src/lib.rs
+++ frame/treasury/src/lib.rs
@@ -47,12 +47,13 @@
 //! ### Bounty
 //!
 //! A Bounty Spending is a reward for a specified body of work - or specified set of objectives - that
-//! needs to be executed for a predefined Treasury amount to be paid out. A Curator is assigned to be delegated
-//! the responsibility of assigning a payout address once the specified set of objectives is completed.
+//! needs to be executed for a predefined Treasury amount to be paid out. A curator is assigned after the bounty is approved and funded by Council, to be delegated
+//! with the responsibility of assigning a payout address once the specified set of objectives is completed.
 //!
-//! After the Council has activated a bounty, it delegates the work that requires expertise to the Curator. They
-//! get to close the Active bounty. Closing the Active bounty enacts a delayed payout to the payout address. The
-//! delay allows for intervention through regular democracy.
+//! After the Council has activated a bounty, it delegates the work that requires expertise to a curator in exchange of a deposit. Once the curator accepts the bounty, they
+//! get to close the Active bounty. Closing the Active bounty enacts a delayed payout to the payout address, the curator fee and the return of the curator deposit. The
+//! delay allows for intervention through regular democracy. The Council gets to unassign the curator, resulting in a new curator election. The Council also gets to cancel 
+//! the bounty if deemed necessary before assigning a curator or once the bounty is active or payout is pending, resulting in the slash of the curator's deposit. 
 //!
 //!
 //! ### Terminology
@@ -80,9 +81,11 @@
 //! - **Proposer:** An account proposing a bounty spending.
 //! - **Curator:** An account managing the bounty and assigning a payout address receiving the reward for the completion of work.
 //! - **Deposit:** The amount held on deposit for placing a bounty proposal plus the amount held on deposit per byte within the bounty description.
+//! - **Curator deposit:** The payment from a candidate willing to curate an approved bounty. The deposit is returned when/if the bounty is completed. 
 //! - **Bounty value:** The total amount that should be paid to the Payout Address if the bounty is rewarded.
 //! - **Payout address:** The account to which the total or part of the bounty is assigned to.
 //! - **Payout Delay:** The delay period for which a bounty beneficiary needs to wait before claiming.
+//! - **Curator fee:** The reserved upfront payment for a curator for work related to the bounty.  
 //!
 //! ## Interface
 //!
@@ -106,10 +109,14 @@
 //! - `propose_bounty` - Propose a specific treasury amount to be earmarked for a predefined set of tasks and stake the required deposit.
 //! - `reject_bounty` - Reject a specific treasury amount to be earmarked for a predefined body of work.
 //! - `approve_bounty` - Accept a specific treasury amount to be earmarked for a predefined body of work.
-//! - `award_bounty` - Close and pay out the specified amount for the completed work.
-//! - `claim_bounty` - Claim a specific bounty amount from the Payout Address
-//! - `cancel_bounty` - Cancel the earmark for a specific treasury amount and close the bounty.
+//! - `assign_curator` - Assign an account to a bounty as candidate curator.
+//! - `accept_curator` - Accept a bounty assignment from the Council, setting a curator deposit.
 //! - `extend_bounty_expiry` - Extend the expiry block number of the bounty and stay active.
+//! - `award_bounty` - Close and pay out the specified amount for the completed work.
+//! - `claim_bounty` - Claim a specific bounty amount from the Payout Address.
+//! - `unassign_curator` - Unassign an accepted curator from a specific earmark.
+//! - `cancel_bounty` - Cancel the earmark for a specific treasury amount and close the bounty.
+//!
 //!
 //! ## GenesisConfig
 //!
