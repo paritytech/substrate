6b94f45a15a597f96ff88783a10dc145b08686f8
kaichao <kaichaosuna@gmail.com>
2020-08-15 10:03:39 +0800

    Update frame/node-permission/src/lib.rs
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- frame/node-permission/src/lib.rs
+++ frame/node-permission/src/lib.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! # Node permission moudule
+//! # Node permission module
 //!
 //! This module is used by the `client/service` to retrieve the current
 //! permissioned nodes.
