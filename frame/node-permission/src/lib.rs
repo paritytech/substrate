5822801c32185ec33ce0d4db51c1cb32bad1d6d9
kaichao <kaichaosuna@gmail.com>
2020-08-15 10:39:30 +0800

    Update frame/node-permission/src/lib.rs
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- frame/node-permission/src/lib.rs
+++ frame/node-permission/src/lib.rs
@@ -175,8 +175,7 @@ decl_module! {
             let remove_location = nodes.binary_search(&remove).ok().ok_or(Error::<T>::NotExist)?;
             nodes.remove(remove_location);
             let add_location = nodes.binary_search(&add).err().ok_or(Error::<T>::AlreadyJoined)?;
-            nodes[location] = add;
-            nodes.sort();
+            nodes.insert(add_location, node_id);
             Self::put_allowlist(nodes);
 
             Self::deposit_event(Event::NodesSwapped);
