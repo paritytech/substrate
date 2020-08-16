c49981c57405bce8fbbdc5a4d92f1941af21c8a8
Bryan Chen <xlchen1291@gmail.com>
2020-08-16 15:04:04 +1200

    update comments

--- frame/treasury/src/lib.rs
+++ frame/treasury/src/lib.rs
@@ -47,13 +47,18 @@
 //! ### Bounty
 //!
 //! A Bounty Spending is a reward for a specified body of work - or specified set of objectives - that
-//! needs to be executed for a predefined Treasury amount to be paid out. A curator is assigned after the bounty is approved and funded by Council, to be delegated
+//! needs to be executed for a predefined Treasury amount to be paid out. A curator is assigned after
+//! the bounty is approved and funded by Council, to be delegated
 //! with the responsibility of assigning a payout address once the specified set of objectives is completed.
 //!
-//! After the Council has activated a bounty, it delegates the work that requires expertise to a curator in exchange of a deposit. Once the curator accepts the bounty, they
-//! get to close the Active bounty. Closing the Active bounty enacts a delayed payout to the payout address, the curator fee and the return of the curator deposit. The
-//! delay allows for intervention through regular democracy. The Council gets to unassign the curator, resulting in a new curator election. The Council also gets to cancel
-//! the bounty if deemed necessary before assigning a curator or once the bounty is active or payout is pending, resulting in the slash of the curator's deposit.
+//! After the Council has activated a bounty, it delegates the work that requires expertise to a curator
+//! in exchange of a deposit. Once the curator accepts the bounty, they
+//! get to close the Active bounty. Closing the Active bounty enacts a delayed payout to the payout
+//! address, the curator fee and the return of the curator deposit. The
+//! delay allows for intervention through regular democracy. The Council gets to unassign the curator,
+//! resulting in a new curator election. The Council also gets to cancel
+//! the bounty if deemed necessary before assigning a curator or once the bounty is active or payout
+//! is pending, resulting in the slash of the curator's deposit.
 //!
 //!
 //! ### Terminology
@@ -77,12 +82,17 @@
 //!   rather than the main beneficiary.
 //!
 //! Bounty:
-//! - **Bounty spending proposal:** A proposal to reward a predefined body of work upon completion by the Treasury.
+//! - **Bounty spending proposal:** A proposal to reward a predefined body of work upon completion by
+//! the Treasury.
 //! - **Proposer:** An account proposing a bounty spending.
-//! - **Curator:** An account managing the bounty and assigning a payout address receiving the reward for the completion of work.
-//! - **Deposit:** The amount held on deposit for placing a bounty proposal plus the amount held on deposit per byte within the bounty description.
-//! - **Curator deposit:** The payment from a candidate willing to curate an approved bounty. The deposit is returned when/if the bounty is completed.
-//! - **Bounty value:** The total amount that should be paid to the Payout Address if the bounty is rewarded.
+//! - **Curator:** An account managing the bounty and assigning a payout address receiving the reward
+//! for the completion of work.
+//! - **Deposit:** The amount held on deposit for placing a bounty proposal plus the amount held on
+//! deposit per byte within the bounty description.
+//! - **Curator deposit:** The payment from a candidate willing to curate an approved bounty. The deposit
+//! is returned when/if the bounty is completed.
+//! - **Bounty value:** The total amount that should be paid to the Payout Address if the bounty is
+//! rewarded.
 //! - **Payout address:** The account to which the total or part of the bounty is assigned to.
 //! - **Payout Delay:** The delay period for which a bounty beneficiary needs to wait before claiming.
 //! - **Curator fee:** The reserved upfront payment for a curator for work related to the bounty.
@@ -106,7 +116,8 @@
 //! - `close_tip` - Close and pay out a tip.
 //!
 //! Bounty protocol:
-//! - `propose_bounty` - Propose a specific treasury amount to be earmarked for a predefined set of tasks and stake the required deposit.
+//! - `propose_bounty` - Propose a specific treasury amount to be earmarked for a predefined set of
+//! tasks and stake the required deposit.
 //! - `reject_bounty` - Reject a specific treasury amount to be earmarked for a predefined body of work.
 //! - `approve_bounty` - Accept a specific treasury amount to be earmarked for a predefined body of work.
 //! - `assign_curator` - Assign an account to a bounty as candidate curator.
