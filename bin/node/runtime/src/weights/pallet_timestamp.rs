a4bd3e70f8f590e0c52f3bff4a565dd95c705f7e
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-08-17 22:59:23 +0200

    WeightInfo for System, Timestamp, and Utility (#6868)
    * initial updates to system
    
    * fix compile
    
    * Update writer.rs
    
    * update weights
    
    * finish system weights
    
    * timestamp weights
    
    * utility weight
    
    * Fix overflow in weight calculations
    
    * add back weight notes
    
    * Update for whitelisted benchmarks
    
    * add trait bounds
    
    * Revert "add trait bounds"
    
    This reverts commit 12b08b7189aa3969f96fa19b211a370860fdb240.
    
    * Update weights for unaccounted for read

--- bin/node/runtime/src/weights/pallet_timestamp.rs
+++ bin/node/runtime/src/weights/pallet_timestamp.rs
@@ -13,10 +13,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! A list of the different weight modules for our runtime.
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 2.0.0-rc5
 
-pub mod frame_system;
-pub mod pallet_balances;
-pub mod pallet_democracy;
-pub mod pallet_timestamp;
-pub mod pallet_utility;
+#![allow(unused_parens)]
+
+use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
+
+pub struct WeightInfo;
+impl pallet_timestamp::WeightInfo for WeightInfo {
+	// WARNING! Some components were not used: ["t"]
+	fn set() -> Weight {
+		(9133000 as Weight)
+			.saturating_add(DbWeight::get().reads(2 as Weight))
+			.saturating_add(DbWeight::get().writes(1 as Weight))
+	}
+	// WARNING! Some components were not used: ["t"]
+	fn on_finalize() -> Weight {
+		(5915000 as Weight)
+	}
+}
