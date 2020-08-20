ea1c6419353d951d4f1c3a76d91ad5b6d81e79f4
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-08-20 17:06:20 +0200

    two_percent_count = one_percent_count * 2

--- frame/staking/src/lib.rs
+++ frame/staking/src/lib.rs
@@ -3148,7 +3148,7 @@ impl<T: Trait> Module<T> {
 		};
 		// Current validator count should never be zero.
 		let current_validator_count = Self::validator_count().max(1);
-		let one_percent_count = (1 + (current_validator_count - 1) / 100);
+		let one_percent_count = 1 + (current_validator_count - 1) / 100;
 		let global_average = average(&exposures);
 		// Sort exposures by balance
 		exposures.sort();
@@ -3159,8 +3159,8 @@ impl<T: Trait> Module<T> {
 		let one_percent_average = average(&exposures[0..(one_percent_count as usize)]);
 		if one_percent_average > Percent::from_percent(40) * global_average {
 			return (current_validator_count + one_percent_count).min(MaximumValidatorCount::get())
-		} else {
-			let two_percent_count = (1 + (current_validator_count - 1) / 50);
+		} else if current_validator_count > 1 {
+			let two_percent_count = one_percent_count.saturating_mul(2);
 			let two_percent_average = average(&exposures[0..(two_percent_count as usize)]);
 			// Rule 2: If the bottom 2% of validators (more precisely, 2 ceil(0.01 k) validators) have
 			// an average stake value strictly below 20% of the global average, in the next era decrease
