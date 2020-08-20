0be730881e14e69e0b55e9946bd83c2977c17338
Tomasz Drwięga <tomasz@parity.io>
2020-08-20 16:47:12 +0200

    WiP

--- frame/merkle-mountain-range/src/lib.rs
+++ frame/merkle-mountain-range/src/lib.rs
@@ -19,20 +19,41 @@
 //!
 //! ## Overview
 //!
+//! NOTE This pallet is experimental and not proven to work in production.
+//!
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use codec::{Encode, Decode};
 use frame_support::{
-	debug,
-	decl_module, decl_storage,
+	debug, decl_module, decl_storage,
+	dispatch::Parameter,
 	weights::Weight,
 };
+use sp_runtime::traits;
+use sp_std::marker::PhantomData;
 
+mod primitives;
 #[cfg(test)]
 mod tests;
 
 /// This pallet's configuration trait
 pub trait Trait: frame_system::Trait {
+	/// A hasher type for MMR.
+	///
+	/// To construct trie nodes that result in merging (bagging) two peaks, depending on the node
+	/// kind we take either:
+	/// - The node (hash) itself if it's an inner node.
+	/// - The hash of SCALE-encoding of the leaf data if it's a leaf node.
+	///
+	/// Then we create a tuple of these two hashes, SCALE-encode it (concatenate) and
+	/// hash, to obtain a new MMR inner node - the new peak.
+	type Hashing: traits::Hash;
 
+	/// Data stored in the leaf nodes.
+	///
+	/// By default every leaf node will always include a (parent) block hash and
+	/// any additional `LeafData` defined by this type.
+	type LeafData: Parameter;
 }
 
 decl_storage! {
@@ -44,11 +65,54 @@ decl_module! {
 	/// A public part of the pallet.
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 		fn on_initialize(block_number: T::BlockNumber) -> Weight {
-			debug::native::info!("Hello World from offchain workers!");
+			debug::native::info!("Hello World from MMR");
 			todo!()
 		}
 	}
 }
+
 impl<T: Trait> Module<T> {
+}
+
+#[derive(Debug, codec::Encode, codec::Decode)]
+pub enum MMRNode<H: traits::Hash, L> {
+	Leaf(L),
+	Inner(H::Output),
+}
+
+impl<H: traits::Hash, L: codec::Encode> MMRNode<H, L> {
+	/// Retrieve a hash of the node.
+	///
+	/// Depending on the node type it's going to either be a contained value for `Inner` node,
+	/// or a hash of SCALE-encoded `Leaf` data.
+	pub fn hash(&self) -> H::Output {
+		match *self {
+			MMRNode::Leaf(ref leaf) => H::hash_of(leaf),
+			MMRNode::Inner(ref hash) => hash.clone(),
+		}
+	}
+}
+
+/// A storage layer for MMR.
+pub struct MMRIndexer<H, L>(PhantomData<(H, L)>);
+
+impl<H: traits::Hash, L: codec::Codec> mmr::MMRStore<MMRNode<H, L>> for MMRIndexer<H, L> {
+	fn get_elem(&self, pos: u64) -> mmr::Result<Option<MMRNode<H, L>>> {
+		todo!()
+	}
+	fn append(&mut self, pos: u64, elems: Vec<MMRNode<H, L>>) -> mmr::Result<()> {
+		todo!()
+	}
+}
+
+/// Hasher type for MMR.
+pub struct MMRHasher<H, L>(PhantomData<(H, L)>);
+
+impl<H: traits::Hash, L: codec::Codec> mmr::Merge for MMRHasher<H, L> {
+	type Item = MMRNode<H, L>;
+
+	fn merge(left: &Self::Item, right: &Self::Item) -> Self::Item {
+		MMRNode::Inner(H::hash_of(&(left.hash(), right.hash())))
+	}
 }
 
