98acb3a1cf3f5fae75424e69fe6525f7970b6b93
Ashley Ruglys <ashley.ruglys@gmail.com>
2020-08-24 13:17:24 +0200

    Revert comments

--- client/api/src/backend.rs
+++ client/api/src/backend.rs
@@ -540,18 +540,14 @@ pub fn changes_tries_state_at_block<'a, Block: BlockT>(
 /// Provide CHT roots. These are stored on a light client and generated dynamically on a full
 /// client.
 pub trait ProvideChtRoots<Block: BlockT> {
-	/// Get headers CHT root for given block.
-	/// * On a light client: Returns None if the block is not pruned (not a part of any CHT).
-	/// * On a full client: Returns None if the block is the genesis block.
+	/// Get headers CHT root for given block. Returns None if the block is not a part of any CHT.
 	fn header_cht_root(
 		&self,
 		cht_size: NumberFor<Block>,
 		block: NumberFor<Block>,
 	) -> sp_blockchain::Result<Option<Block::Hash>>;
 
-	/// Get changes trie CHT root for given block.
-	/// * On a light client: Returns None if the block is not pruned (not a part of any CHT).
-	/// * On a full client: Returns None if the block is the genesis block.
+	/// Get changes trie CHT root for given block. Returns None if the block is not a part of any CHT.
 	fn changes_trie_cht_root(
 		&self,
 		cht_size: NumberFor<Block>,
