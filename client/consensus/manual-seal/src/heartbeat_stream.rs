115990e8c007cd97022169b5d983d96ee5018b68
Jimmy Chu <jimmychu0807@gmail.com>
2020-08-26 19:52:32 +0800

    Updated Heartbeat stream with test
    Added comments also.

--- client/consensus/manual-seal/src/heartbeat_stream.rs
+++ client/consensus/manual-seal/src/heartbeat_stream.rs
@@ -1,5 +1,22 @@
-// Library shortcuts
+// This file is part of Substrate.
 
+// Copyright (C) 2020 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! HeartbeatStream type and HeartbeatOptions used in instant seal
 use std::{pin::Pin};
 use futures::{
 	prelude::*,
@@ -9,17 +26,18 @@ use tokio::time::{Duration, Instant, Delay};
 
 use crate::rpc::{EngineCommand};
 
-// ---
-// Constant Definition
-
+/// Constants for generating default heartbeat options
 const DEFAULT_TIMEOUT: u64 = 30;
 const DEFAULT_MIN_BLOCKTIME: u64 = 1;
 const DEFAULT_FINALIZE: bool = false;
 
-// ---
+/// Heartbeat options to manage the behavior of the heartbeat stream
 pub struct HeartbeatOptions {
+	/// The amount of time passed that a new heartbeat block will be generated, in sec.
 	pub timeout: u64,
+	/// The minimum amount of time to pass before generating another block, in sec.
 	pub min_blocktime: u64,
+	/// Control whether the generated heartbeat block is finalized
 	pub finalize: bool,
 }
 
@@ -33,10 +51,16 @@ impl Default for HeartbeatOptions {
 	}
 }
 
+/// HeartbeatStream is combining transaction pool notification stream and generate heartbeat
+///   blocks when a certain time has passed without any transactions.
 pub struct HeartbeatStream<Hash> {
+	/// The transaction pool notification stream
 	pool_stream: Box<dyn Stream<Item = EngineCommand<Hash>> + Unpin + Send>,
-	delay: Delay,
-	instant: Instant,
+	/// Future to control when the next heartbeat block should be generated
+	delay_future: Delay,
+	/// To remember when the last heartbeat block is generated
+	last_heartbeat: Option<Instant>,
+	/// Heartbeat options
 	opts: HeartbeatOptions,
 }
 
@@ -45,10 +69,13 @@ impl<Hash> HeartbeatStream<Hash> {
 		pool_stream: Box<dyn Stream<Item = EngineCommand<Hash>> + Unpin + Send>,
 		opts: HeartbeatOptions
 	) -> Self {
+		if opts.min_blocktime > opts.timeout {
+			panic!("Heartbeat options `min_blocktime` value must not be larger than `timeout` value.");
+		}
 		Self {
 			pool_stream,
-			delay: tokio::time::delay_for(Duration::from_secs(opts.timeout)),
-			instant: Instant::now(),
+			delay_future: tokio::time::delay_for(Duration::from_secs(opts.timeout)),
+			last_heartbeat: None,
 			opts
 		}
 	}
@@ -62,14 +89,20 @@ impl<Hash> Stream for HeartbeatStream<Hash> {
 		let mut hbs = self.get_mut();
 		match hbs.pool_stream.poll_next_unpin(cx) {
 			Poll::Ready(Some(ec)) => {
-				// Ensure we are not producing block too frequently, based on `min_blocktime` setting
-				if Instant::now() - hbs.instant < Duration::from_secs(hbs.opts.min_blocktime) {
-					return Poll::Pending;
+				if let Some(last_heartbeat) = hbs.last_heartbeat {
+					// If the last heartbeat happened within min_blocktime time, we want to wait at least
+					//   until `min_blocktime` has passed.
+					if Instant::now() - last_heartbeat < Duration::from_secs(hbs.opts.min_blocktime) {
+						// We set `delay_future` here so those txs arrived after heartbeats doesn't have to wait
+						//   for `timeout`s to get processed, but only `min_blocktime`s.
+						hbs.delay_future = tokio::time::delay_for(Duration::from_secs(hbs.opts.min_blocktime));
+						return Poll::Pending;
+					}
 				}
 
 				// reset the timer and delay future
-				hbs.delay = tokio::time::delay_for(Duration::from_secs(hbs.opts.timeout));
-				hbs.instant = Instant::now();
+				hbs.delay_future = tokio::time::delay_for(Duration::from_secs(hbs.opts.timeout));
+				hbs.last_heartbeat = Some(Instant::now());
 				Poll::Ready(Some(ec))
 			},
 
@@ -78,15 +111,15 @@ impl<Hash> Stream for HeartbeatStream<Hash> {
 
 			Poll::Pending => {
 				// We check if the delay for heartbeat has reached
-				if let Poll::Ready(_) = hbs.delay.poll_unpin(cx) {
+				if let Poll::Ready(_) = hbs.delay_future.poll_unpin(cx) {
 					// reset the timer and delay future
-					hbs.delay = tokio::time::delay_for(Duration::from_secs(hbs.opts.timeout));
-					hbs.instant = Instant::now();
+					hbs.delay_future = tokio::time::delay_for(Duration::from_secs(hbs.opts.timeout));
+					hbs.last_heartbeat = Some(Instant::now());
 
 					return Poll::Ready(Some(EngineCommand::SealNewBlock {
 						create_empty: true, // heartbeat blocks are empty by definition
 						finalize: hbs.opts.finalize,
-						parent_hash: None, // QUESTION: no parent hash here? Is this block still conneected with the whole chain?
+						parent_hash: None,
 						sender: None,
 					}));
 				}
