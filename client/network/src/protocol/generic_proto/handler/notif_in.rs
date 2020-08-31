f14a792ed73e4915cee568f3c3dddbc8e079625c
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-08-31 10:51:02 +0200

    Also process OpenRequest and Closed

--- client/network/src/protocol/generic_proto/handler/notif_in.rs
+++ client/network/src/protocol/generic_proto/handler/notif_in.rs
@@ -37,7 +37,7 @@ use libp2p::swarm::{
 	NegotiatedSubstream,
 };
 use log::{error, warn};
-use std::{borrow::Cow, collections::VecDeque, convert::Infallible, fmt, pin::Pin, task::{Context, Poll}};
+use std::{borrow::Cow, collections::VecDeque, fmt, pin::Pin, task::{Context, Poll}};
 
 /// Implements the `IntoProtocolsHandler` trait of libp2p.
 ///
@@ -140,12 +140,21 @@ impl NotifsInHandler {
 		self.in_protocol.protocol_name()
 	}
 
-	/// Equivalent to the `poll` method of `ProtocolsHandler`, except that it only drives the
-	/// handshake process.
+	/// Equivalent to the `poll` method of `ProtocolsHandler`, except that it is guaranteed to
+	/// never generate [`NotifsInHandlerOut::Notif`].
 	///
 	/// Use this method in situations where it is not desirable to receive events but still
 	/// necessary to drive any potential incoming handshake or request.
-	pub fn poll_process(&mut self, cx: &mut Context) -> Poll<Infallible> {
+	pub fn poll_process(
+		&mut self,
+		cx: &mut Context
+	) -> Poll<
+		ProtocolsHandlerEvent<DeniedUpgrade, (), NotifsInHandlerOut, void::Void>
+	> {
+		if let Some(event) = self.events_queue.pop_front() {
+			return Poll::Ready(event)
+		}
+
 		match self.substream.as_mut().map(|s| NotificationsInSubstream::poll_process(Pin::new(s), cx)) {
 			None | Some(Poll::Pending) => {},
 			Some(Poll::Ready(Ok(v))) => match v {},
