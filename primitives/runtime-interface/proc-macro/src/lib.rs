7419129fe1acd4e06856f3665c540cf0a735b362
Benjamin Kampmann <ben@parity.io>
2020-09-07 17:31:12 +0200

    fixing formatting

--- primitives/runtime-interface/proc-macro/src/lib.rs
+++ primitives/runtime-interface/proc-macro/src/lib.rs
@@ -34,70 +34,69 @@ mod runtime_interface;
 mod utils;
 
 struct Options {
-	wasm_only: bool,
-	tracing: bool
+    wasm_only: bool,
+    tracing: bool
 }
 
 impl Options {
-	fn unpack(self) -> (bool, bool) {
-		(self.wasm_only, self.tracing)
-	}
+    fn unpack(self) -> (bool, bool) {
+        (self.wasm_only, self.tracing)
+    }
 }
 impl Default for Options {
-	fn default() -> Self {
-		Options { wasm_only: false, tracing: true }
-	}
+    fn default() -> Self {
+        Options { wasm_only: false, tracing: true }
+    }
 }
 
 impl Parse for Options {
     fn parse(input: ParseStream) -> Result<Self> {
-		let mut res = Self::default();
-		while !input.is_empty() {
-			let lookahead = input.lookahead1();
-			if lookahead.peek(runtime_interface::keywords::wasm_only) {
-				let _ = input.parse::<runtime_interface::keywords::wasm_only>();
-				res.wasm_only = true;
-			} else if lookahead.peek(runtime_interface::keywords::no_tracing) {
-				let _ = input.parse::<runtime_interface::keywords::no_tracing>();
-				res.tracing = false;
-			} else if lookahead.peek(Token![,]) {
-				let _ = input.parse::<Token![,]>();
-			} else {
-				return Err(lookahead.error())
-			}
-		}
-		Ok(res)
-	}
-
+        let mut res = Self::default();
+        while !input.is_empty() {
+            let lookahead = input.lookahead1();
+            if lookahead.peek(runtime_interface::keywords::wasm_only) {
+                let _ = input.parse::<runtime_interface::keywords::wasm_only>();
+                res.wasm_only = true;
+            } else if lookahead.peek(runtime_interface::keywords::no_tracing) {
+                let _ = input.parse::<runtime_interface::keywords::no_tracing>();
+                res.tracing = false;
+            } else if lookahead.peek(Token![,]) {
+                let _ = input.parse::<Token![,]>();
+            } else {
+                return Err(lookahead.error())
+            }
+        }
+        Ok(res)
+    }
 }
 
 #[proc_macro_attribute]
 pub fn runtime_interface(
-	attrs: proc_macro::TokenStream,
-	input: proc_macro::TokenStream,
+    attrs: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-	let trait_def = parse_macro_input!(input as ItemTrait);
-	let (wasm_only, tracing) = parse_macro_input!(attrs as Options).unpack();
+    let trait_def = parse_macro_input!(input as ItemTrait);
+    let (wasm_only, tracing) = parse_macro_input!(attrs as Options).unpack();
 
-	runtime_interface::runtime_interface_impl(trait_def, wasm_only, tracing)
-		.unwrap_or_else(|e| e.to_compile_error())
-		.into()
+    runtime_interface::runtime_interface_impl(trait_def, wasm_only, tracing)
+        .unwrap_or_else(|e| e.to_compile_error())
+        .into()
 }
 
 #[proc_macro_derive(PassByCodec)]
 pub fn pass_by_codec(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	let input = parse_macro_input!(input as DeriveInput);
-	pass_by::codec_derive_impl(input).unwrap_or_else(|e| e.to_compile_error()).into()
+    let input = parse_macro_input!(input as DeriveInput);
+    pass_by::codec_derive_impl(input).unwrap_or_else(|e| e.to_compile_error()).into()
 }
 
 #[proc_macro_derive(PassByInner)]
 pub fn pass_by_inner(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	let input = parse_macro_input!(input as DeriveInput);
-	pass_by::inner_derive_impl(input).unwrap_or_else(|e| e.to_compile_error()).into()
+    let input = parse_macro_input!(input as DeriveInput);
+    pass_by::inner_derive_impl(input).unwrap_or_else(|e| e.to_compile_error()).into()
 }
 
 #[proc_macro_derive(PassByEnum)]
 pub fn pass_by_enum(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	let input = parse_macro_input!(input as DeriveInput);
-	pass_by::enum_derive_impl(input).unwrap_or_else(|e| e.to_compile_error()).into()
+    let input = parse_macro_input!(input as DeriveInput);
+    pass_by::enum_derive_impl(input).unwrap_or_else(|e| e.to_compile_error()).into()
 }
