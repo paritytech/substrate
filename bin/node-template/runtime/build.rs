c83834bf9d30359e6ee884dff5360d12a0512bd2
Benjamin Kampmann <ben@parity.io>
2020-09-14 11:57:08 +0200

    remove commented lines

--- bin/node-template/runtime/build.rs
+++ bin/node-template/runtime/build.rs
@@ -6,7 +6,5 @@ fn main() {
 		.with_wasm_builder_from_crates("2.0.0")
 		.export_heap_base()
 		.import_memory()
-		// Uncomment the following to activate tracing in WASM
-		// .append_to_rust_flags("--cfg feature=\\\"with-tracing\\\"")
 		.build()
 }
