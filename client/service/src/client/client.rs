684dedf4f156217a11f5d3444503007dc49faead
Bastian Köcher <bkchr@users.noreply.github.com>
2020-09-16 16:07:10 +0200

    Update client/service/src/client/client.rs
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>

--- client/service/src/client/client.rs
+++ client/service/src/client/client.rs
@@ -802,6 +802,7 @@ impl<B, E, Block, RA> Client<B, E, Block, RA> where
 
 		operation.op.insert_aux(aux)?;
 
+		// we only notify when we are already synced to the tip of the chain or if this import triggers a re-org
 		if make_notifications || tree_route.is_some() {
 			if finalized {
 				operation.notify_finalized.push(hash);
