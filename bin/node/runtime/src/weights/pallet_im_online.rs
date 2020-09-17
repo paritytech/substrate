171ee7215ee1ff8541103d51456115e9531e9be5
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-09-17 14:56:50 +0200

    benchmark machine weights

--- bin/node/runtime/src/weights/pallet_im_online.rs
+++ bin/node/runtime/src/weights/pallet_im_online.rs
@@ -25,9 +25,9 @@ use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
 pub struct WeightInfo;
 impl pallet_im_online::WeightInfo for WeightInfo {
 	fn validate_unsigned_and_then_heartbeat(k: u32, e: u32, ) -> Weight {
-		(232729000 as Weight)
-			.saturating_add((231000 as Weight).saturating_mul(k as Weight))
-			.saturating_add((520000 as Weight).saturating_mul(e as Weight))
+		(139830000 as Weight)
+			.saturating_add((211000 as Weight).saturating_mul(k as Weight))
+			.saturating_add((654000 as Weight).saturating_mul(e as Weight))
 			.saturating_add(DbWeight::get().reads(4 as Weight))
 			.saturating_add(DbWeight::get().writes(1 as Weight))
 	}
--- frame/im-online/src/default_weight.rs
+++ frame/im-online/src/default_weight.rs
@@ -24,9 +24,9 @@ use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
 
 impl crate::WeightInfo for () {
 	fn validate_unsigned_and_then_heartbeat(k: u32, e: u32, ) -> Weight {
-		(232729000 as Weight)
-			.saturating_add((231000 as Weight).saturating_mul(k as Weight))
-			.saturating_add((520000 as Weight).saturating_mul(e as Weight))
+		(139830000 as Weight)
+			.saturating_add((211000 as Weight).saturating_mul(k as Weight))
+			.saturating_add((654000 as Weight).saturating_mul(e as Weight))
 			.saturating_add(DbWeight::get().reads(4 as Weight))
 			.saturating_add(DbWeight::get().writes(1 as Weight))
 	}
