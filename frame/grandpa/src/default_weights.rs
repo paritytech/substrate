d0612baa5a2881bacbedbb3fa6ae20e15d0c7679
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-09-21 13:26:19 +0200

    Update frame/grandpa/src/default_weights.rs
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>

--- frame/grandpa/src/default_weights.rs
+++ frame/grandpa/src/default_weights.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Default weights for the Babe Pallet
+//! Default weights for the GRANDPA Pallet
 //! This file was not auto-generated.
 
 use frame_support::weights::{
