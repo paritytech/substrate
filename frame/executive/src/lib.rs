f66da68ecc8b7cfdcb4dad3682e59e29de37bcd3
Gav Wood <gavin@parity.io>
2020-09-22 16:07:23 +0200

    Fixes

--- frame/executive/src/lib.rs
+++ frame/executive/src/lib.rs
@@ -738,7 +738,7 @@ mod tests {
 				header: Header {
 					parent_hash: [69u8; 32].into(),
 					number: 1,
-					state_root: hex!("23cd0e2dfd95f1529336c8983889cd7430454d04fd1e0ca0f5614839ea7d36c6").into(),
+					state_root: hex!("465a1569d309039bdf84b0479d28064ea29e6584584dc7d788904bb14489c6f6").into(),
 					extrinsics_root: hex!("03170a2e7597b7b7e3d84c05391d139a62b157e78786d8c082f29dcf4c111314").into(),
 					digest: Digest { logs: vec![], },
 				},
