abd7db2027f3766a1de74630ae7a186e79d747e0
kianenigma <kian.peymani@gmail.com>
2020-09-22 13:24:06 +0200

    Fix tests one last time

--- frame/elections-phragmen/src/lib.rs
+++ frame/elections-phragmen/src/lib.rs
@@ -2408,7 +2408,7 @@ mod tests {
 			assert_err_with_weight!(
 				Elections::remove_member(Origin::root(), 4, true),
 				Error::<Test>::InvalidReplacement,
-				Some(45302000), // only thing that matters for now is that it is NOT the full block.
+				Some(33777000), // only thing that matters for now is that it is NOT the full block.
 			);
 		});
 
@@ -2430,7 +2430,7 @@ mod tests {
 			assert_err_with_weight!(
 				Elections::remove_member(Origin::root(), 4, false),
 				Error::<Test>::InvalidReplacement,
-				Some(45302000) // only thing that matters for now is that it is NOT the full block.
+				Some(33777000) // only thing that matters for now is that it is NOT the full block.
 			);
 		});
 	}
