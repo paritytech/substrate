e4a9f92391cc0e03bf18d7b3747caf4e6546fa84
Bastian Köcher <bkchr@users.noreply.github.com>
2020-09-22 15:39:56 +0200

    Rename `ModuleToIndex` to `PalletRuntimeSetup` (#7148)
    * Rename `ModuleToIndex` to `PalletRuntimeSetup`
    
    Besides the renaming it also adds support getting the name of a pallet
    as configured in the runtime.
    
    * Rename it to `PalletInfo`
    
    * Remove accidentally added files

--- frame/scheduler/src/lib.rs
+++ frame/scheduler/src/lib.rs
@@ -735,7 +735,7 @@ mod tests {
 		type MaximumBlockLength = MaximumBlockLength;
 		type AvailableBlockRatio = AvailableBlockRatio;
 		type Version = ();
-		type ModuleToIndex = ();
+		type PalletInfo = ();
 		type AccountData = ();
 		type OnNewAccount = ();
 		type OnKilledAccount = ();
