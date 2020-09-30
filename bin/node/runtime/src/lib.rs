2898c022731e6c9ed05bc2f67ba24b075b7541e8
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-09-30 22:55:17 +0200

    Improve Payout Benchmarks in Staking (#7237)
    * add additional checks
    
    * fix wasm trap
    
    * Wasm trap?
    
    * Clean up validators before running payout
    
    * fix line width

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -436,7 +436,7 @@ parameter_types! {
 	pub const BondingDuration: pallet_staking::EraIndex = 24 * 28;
 	pub const SlashDeferDuration: pallet_staking::EraIndex = 24 * 7; // 1/4 the bonding duration.
 	pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
-	pub const MaxNominatorRewardedPerValidator: u32 = 64;
+	pub const MaxNominatorRewardedPerValidator: u32 = 256;
 	pub const ElectionLookahead: BlockNumber = EPOCH_DURATION_IN_BLOCKS / 4;
 	pub const MaxIterations: u32 = 10;
 	// 0.05%. The higher the value, the more strict solution acceptance becomes.
