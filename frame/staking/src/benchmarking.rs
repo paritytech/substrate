5b1841ead3bce98e05ed3525b74bbe9369f4976f
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-09-30 03:42:49 +0200

    fix wasm trap

--- frame/staking/src/benchmarking.rs
+++ frame/staking/src/benchmarking.rs
@@ -46,13 +46,13 @@ fn add_slashing_spans<T: Trait>(who: &T::AccountId, spans: u32) {
 }
 
 // This function generates one validator being nominated by n nominators, and returns the validator
-// stash account and the nominators' stash and controller. It also starts an era and creates pending payouts.
+// stash account. It also starts an era and creates pending payouts.
 pub fn create_validator_with_nominators<T: Trait>(
 	n: u32,
 	upper_bound: u32,
 	dead: bool,
 	destination: RewardDestination<T::AccountId>
-) -> Result<(T::AccountId, Vec<(T::AccountId, T::AccountId)>), &'static str> {
+) -> Result<T::AccountId, &'static str> {
 	let mut points_total = 0;
 	let mut points_individual = Vec::new();
 
@@ -66,18 +66,15 @@ pub fn create_validator_with_nominators<T: Trait>(
 	points_total += 10;
 	points_individual.push((v_stash.clone(), 10));
 
-	let mut nominators = Vec::new();
-
 	// Give the validator n nominators, but keep total users in the system the same.
 	for i in 0 .. upper_bound {
-		let (n_stash, n_controller) = if !dead {
+		let (_n_stash, n_controller) = if !dead {
 			create_stash_controller::<T>(u32::max_value() - i, 100, destination.clone())?
 		} else {
 			create_stash_and_dead_controller::<T>(u32::max_value() - i, 100, destination.clone())?
 		};
 		if i < n {
 			Staking::<T>::nominate(RawOrigin::Signed(n_controller.clone()).into(), vec![stash_lookup.clone()])?;
-			nominators.push((n_stash, n_controller));
 		}
 	}
 
@@ -103,7 +100,7 @@ pub fn create_validator_with_nominators<T: Trait>(
 		.saturating_mul(1000.into());
 	<ErasValidatorReward<T>>::insert(current_era, total_payout);
 
-	Ok((v_stash, nominators))
+	Ok(v_stash)
 }
 
 const USER_SEED: u32 = 999666;
@@ -285,7 +282,7 @@ benchmarks! {
 
 	payout_stakers_dead_controller {
 		let n in 1 .. T::MaxNominatorRewardedPerValidator::get() as u32;
-		let (validator, nominators) = create_validator_with_nominators::<T>(
+		let validator = create_validator_with_nominators::<T>(
 			n,
 			T::MaxNominatorRewardedPerValidator::get() as u32,
 			true,
@@ -299,10 +296,6 @@ benchmarks! {
 		let caller = whitelisted_caller();
 		let validator_controller = <Bonded<T>>::get(&validator).unwrap();
 		let balance_before = T::Currency::free_balance(&validator_controller);
-		for (_, controller) in &nominators {
-			let balance = T::Currency::free_balance(&controller);
-			assert!(balance.is_zero(), "Controller has balance, but should be dead.");
-		}
 	}: payout_stakers(RawOrigin::Signed(caller), validator.clone(), current_era)
 	verify {
 		let balance_after = T::Currency::free_balance(&validator_controller);
@@ -311,16 +304,11 @@ benchmarks! {
 			"Balance of controller {:?} should have increased after payout.",
 			validator,
 		);
-
-		for (_, controller) in &nominators {
-			let balance = T::Currency::free_balance(&controller);
-			assert!(!balance.is_zero(), "Payout not given to controller.");
-		}
 	}
 
 	payout_stakers_alive_staked {
 		let n in 1 .. T::MaxNominatorRewardedPerValidator::get() as u32;
-		let (validator, nominators) = create_validator_with_nominators::<T>(
+		let validator = create_validator_with_nominators::<T>(
 			n,
 			T::MaxNominatorRewardedPerValidator::get() as u32,
 			false,
@@ -333,11 +321,6 @@ benchmarks! {
 
 		let caller = whitelisted_caller();
 		let balance_before = T::Currency::free_balance(&validator);
-		let mut nominator_balances_before = Vec::new();
-		for (stash, _) in &nominators {
-			let balance = T::Currency::free_balance(&stash);
-			nominator_balances_before.push(balance);
-		}
 	}: payout_stakers(RawOrigin::Signed(caller), validator.clone(), current_era)
 	verify {
 		let balance_after = T::Currency::free_balance(&validator);
@@ -346,14 +329,6 @@ benchmarks! {
 			"Balance of stash {:?} should have increased after payout.",
 			validator,
 		);
-		for ((stash, _), balance_before) in nominators.iter().zip(nominator_balances_before.iter()) {
-			let balance_after = T::Currency::free_balance(&stash);
-			assert!(
-				balance_before < &balance_after,
-				"Balance of nominator stash {:?} should have increased after payout.",
-				stash,
-			);
-		}
 	}
 
 	rebond {
@@ -727,14 +702,12 @@ mod tests {
 		ExtBuilder::default().has_stakers(false).build().execute_with(|| {
 			let n = 10;
 
-			let (validator_stash, nominators) = create_validator_with_nominators::<Test>(
+			let validator_stash = create_validator_with_nominators::<Test>(
 				n,
 				<Test as Trait>::MaxNominatorRewardedPerValidator::get() as u32,
 				false,
 				RewardDestination::Staked,
 			).unwrap();
-
-			assert_eq!(nominators.len() as u32, n);
 
 			let current_era = CurrentEra::get().unwrap();
 
@@ -751,7 +724,7 @@ mod tests {
 		ExtBuilder::default().has_stakers(false).build().execute_with(|| {
 			let n = 10;
 
-			let (validator_stash, _nominators) = create_validator_with_nominators::<Test>(
+			let validator_stash = create_validator_with_nominators::<Test>(
 				n,
 				<Test as Trait>::MaxNominatorRewardedPerValidator::get() as u32,
 				false,
