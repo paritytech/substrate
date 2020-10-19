b49b666043c61891faa3b56ce7fc4d74c40ebad8
Max Inden <mail@max-inden.de>
2020-10-19 21:43:32 +0200

    client/network: Remove option to disable yamux flow control (#7358)
    With the `OnRead` flow control option yamux "send[s] window updates only
    when data is read on the receiving end" and not as soon as "a Stream's
    receive window drops to 0".
    
    Yamux flow control has proven itself. This commit removes the feature
    flag. Yamux flow control is now always enabled.

--- client/service/test/src/lib.rs
+++ client/service/test/src/lib.rs
@@ -225,7 +225,6 @@ fn node_config<G: RuntimeGenesis + 'static, E: ChainSpecExtension + Clone + 'sta
 		enable_mdns: false,
 		allow_private_ipv4: true,
 		wasm_external_transport: None,
-		use_yamux_flow_control: true,
 	};
 
 	Configuration {
