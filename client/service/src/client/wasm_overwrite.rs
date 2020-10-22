824b24e88f0a650cb4e1795e991a56c0f0ab9d8d
Andrew Plaza <aplaza@liquidthink.net>
2020-10-22 13:48:25 +0200

    Update client/service/src/client/wasm_overwrite.rs
    Co-authored-by: David <dvdplm@gmail.com>

--- client/service/src/client/wasm_overwrite.rs
+++ client/service/src/client/wasm_overwrite.rs
@@ -62,8 +62,8 @@ impl FetchRuntimeCode for WasmBlob {
 }
 
 
-/// Scrapes WASM from a folder
-/// and returns WASM from that folder if the runtime spec version matches.
+/// Scrapes WASM from a folder and returns WASM from that folder
+/// if the runtime spec version matches.
 #[derive(Clone, Debug)]
 pub struct WasmOverwrite<E> {
 	// Map of runtime spec version -> Wasm Blob
