298d0159dfa82c855315afec0cc3c58e95761c58
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-10-25 20:15:45 +0100

    support custom filename output

--- utils/frame/benchmarking-cli/src/writer.rs
+++ utils/frame/benchmarking-cli/src/writer.rs
@@ -242,8 +242,10 @@ pub fn write_results(
 	for (pallet, results) in all_results.into_iter() {
 		// Create new file: "path/to/pallet_name.rs".
 		let mut file_path = path.clone();
-		file_path.push(&pallet);
-		file_path.set_extension("rs");
+		if file_path.is_dir() {
+			file_path.push(&pallet);
+			file_path.set_extension("rs");
+		}
 
 		let hbs_data = TemplateData {
 			args: args.clone(),
