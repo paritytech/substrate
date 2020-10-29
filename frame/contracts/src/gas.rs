30ecb9b259140a860b20c662fda01b5b2c33e5fa
Alexander Theißen <alex.theissen@me.com>
2020-10-29 15:57:56 +0100

    contracts: Refactor the runtime API in order to simplify node integration (#7409)
    * contracts: Make use of existing type aliases for runtime API types
    
    * contracts: Refactor the contracts call runtime API
    
    * review: Fix comment typo
    
    Co-authored-by: Andrew Jones <ascjones@gmail.com>
    
    * Update frame/contracts/common/src/lib.rs
    
    Co-authored-by: Nikolay Volf <nikvolf@gmail.com>
    
    * Update frame/contracts/common/src/lib.rs
    
    Co-authored-by: Nikolay Volf <nikvolf@gmail.com>
    
    * Update frame/contracts/common/src/lib.rs
    
    Co-authored-by: Nikolay Volf <nikvolf@gmail.com>
    
    * Update frame/contracts/common/src/lib.rs
    
    Co-authored-by: Nikolay Volf <nikvolf@gmail.com>
    
    * Update frame/contracts/common/src/lib.rs
    
    Co-authored-by: Nikolay Volf <nikvolf@gmail.com>
    
    * Update lib.rs
    
    * review: Group crate imports
    
    Co-authored-by: Andrew Jones <ascjones@gmail.com>
    Co-authored-by: Addie Wagenknecht <addie@nortd.com>
    Co-authored-by: Nikolay Volf <nikvolf@gmail.com>

--- frame/contracts/src/gas.rs
+++ frame/contracts/src/gas.rs
@@ -14,12 +14,13 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate. If not, see <http://www.gnu.org/licenses/>.
 
-use crate::{Trait, exec::ExecError};
+use crate::Trait;
 use sp_std::marker::PhantomData;
 use sp_runtime::traits::Zero;
 use frame_support::dispatch::{
 	DispatchResultWithPostInfo, PostDispatchInfo, DispatchErrorWithPostInfo,
 };
+use pallet_contracts_primitives::ExecError;
 
 #[cfg(test)]
 use std::{any::Any, fmt::Debug};
