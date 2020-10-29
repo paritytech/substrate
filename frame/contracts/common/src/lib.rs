60b93fab7c0aa456ca3f7b2fb410e02e9b3fbc27
Alexander Theißen <alex.theissen@me.com>
2020-10-29 11:32:53 +0100

    review: Fix comment typo
    Co-authored-by: Andrew Jones <ascjones@gmail.com>

--- frame/contracts/common/src/lib.rs
+++ frame/contracts/common/src/lib.rs
@@ -61,7 +61,7 @@ bitflags! {
 	/// Flags used by a contract to customize exit behaviour.
 	#[derive(Encode, Decode)]
 	pub struct ReturnFlags: u32 {
-		/// If this bit is set all changes made by the contract exection are rolled back.
+		/// If this bit is set all changes made by the contract execution are rolled back.
 		const REVERT = 0x0000_0001;
 	}
 }
