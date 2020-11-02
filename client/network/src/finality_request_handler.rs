1a6b00eadf4893bf4fb5bf285c342910cc5695a9
Max Inden <mail@max-inden.de>
2020-11-02 17:55:23 +0100

    client/network/finality_request_handler: Log errors

--- client/network/src/finality_request_handler.rs
+++ client/network/src/finality_request_handler.rs
@@ -17,16 +17,20 @@
 //! Helper for handling (i.e. answering) finality requests from a remote peer via the
 //! [`crate::request_responses::RequestResponsesBehaviour`].
 
-use crate::request_responses::ProtocolConfig;
-use futures::channel::mpsc;
+use codec::Decode;
+use crate::chain::FinalityProofProvider;
+use crate::config::ProtocolId;
+use crate::request_responses::{IncomingRequest, ProtocolConfig};
+use crate::schema::v1::finality::{FinalityProofRequest, FinalityProofResponse};
+use futures::channel::{mpsc, oneshot};
 use futures::stream::StreamExt;
-use std::sync::{Arc};
-pub use crate::{
-	config::ProtocolId,
-	chain::{Client, FinalityProofProvider}};
-use sp_runtime::{traits::Block as BlockT};
-use crate::request_responses::IncomingRequest;
+use log::debug;
 use prost::Message;
+use sp_runtime::{traits::Block as BlockT};
+use std::sync::{Arc};
+use std::time::Duration;
+
+const LOG_TARGET: &str = "finality-request-handler";
 
 /// Generate the finality proof protocol name from chain specific protocol identifier.
 pub fn generate_protocol_name(protocol_id: ProtocolId) -> String {
@@ -45,16 +49,19 @@ pub struct FinalityRequestHandler<B> {
 
 impl<B: BlockT> FinalityRequestHandler<B> {
 	/// Create a new [`FinalityRequestHandler`].
-	pub fn new(protocol_id: ProtocolId, proof_provider: Arc<dyn FinalityProofProvider<B>>) -> (Self, ProtocolConfig){
+	pub fn new(
+		protocol_id: ProtocolId,
+		proof_provider: Arc<dyn FinalityProofProvider<B>>,
+	) -> (Self, ProtocolConfig){
+		// TODO: Likeley we want to allow more than 0 buffered requests. Rethink this value.
 		let (tx, rx) = mpsc::channel(0);
 
 		let protocol_config = ProtocolConfig {
 			name: generate_protocol_name(protocol_id).into(),
-			// TODO: Change.
-			max_request_size: 4096,
-			// TODO: Change.
-			max_response_size: 4096,
-			request_timeout: std::time::Duration::from_secs(10),
+			max_request_size: 1024 * 1024,
+			max_response_size: 1024 * 1024,
+			// TODO: What is a sane value here?
+			request_timeout: Duration::from_secs(10),
 			inbound_queue: Some(tx),
 		};
 
@@ -66,25 +73,58 @@ impl<B: BlockT> FinalityRequestHandler<B> {
 		(handler, protocol_config)
 	}
 
+	fn handle_request(
+		&self,
+		payload: Vec<u8>,
+		pending_response: oneshot::Sender<Vec<u8>>
+	) -> Result<(), HandleRequestError<B>> {
+		// Decode request.
+		let request = FinalityProofRequest::decode(&payload[..])?;
+		let block_hash = Decode::decode(&mut request.block_hash.as_ref())?;
+
+		// Proof.
+		let finality_proof = self.proof_provider.prove_finality(block_hash, &request.request)
+			.map_err(|e| HandleRequestError::ProofFinality(block_hash, e))?
+			.unwrap_or_default();
+
+		// Encode response.
+		let resp = FinalityProofResponse { proof: finality_proof };
+		let mut data = Vec::with_capacity(resp.encoded_len());
+		resp.encode(&mut data)
+			.map_err(|e| HandleRequestError::EncodeProto(block_hash, e))?;
+
+		// Respond.
+		pending_response.send(data)
+			.map_err(|_| HandleRequestError::SendResponse)
+	}
+
 	/// Run [`FinalityRequestHandler`].
 	pub async fn run(mut self) {
-		while let Some(crate::request_responses::IncomingRequest { peer, payload, pending_response }) = self.request_receiver.next().await {
-			let req = crate::schema::v1::finality::FinalityProofRequest::decode(&payload[..]).unwrap();
+		while let Some(request) = self.request_receiver.next().await {
+			let IncomingRequest { peer, payload, pending_response } = request;
 
-			let block_hash = codec::Decode::decode(&mut req.block_hash.as_ref()).unwrap();
-
-			log::trace!(target: "sync", "Finality proof request from {} for {}", peer, block_hash);
-
-			let finality_proof = self.proof_provider
-				.prove_finality(block_hash, &req.request)
-				.unwrap()
-				.unwrap_or_default();
-
-			let resp = crate::schema::v1::finality::FinalityProofResponse { proof: finality_proof };
-			let mut data = Vec::with_capacity(resp.encoded_len());
-			resp.encode(&mut data).unwrap();
-
-			pending_response.send(data).unwrap();
+			match self.handle_request(payload, pending_response) {
+				Ok(()) => debug!(target: LOG_TARGET, "Handled finality proof request from {}.", peer),
+				Err(e) => debug!(
+					target: LOG_TARGET,
+					"Failed to handle finality proof request from {}: {}",
+					peer, e,
+				),
+			}
 		}
 	}
+}
+
+#[derive(derive_more::Display, derive_more::From)]
+enum HandleRequestError<B: BlockT> {
+	#[display(fmt = "Failed to decode request: {}.", _0)]
+	DecodeProto(prost::DecodeError),
+	#[display(fmt = "Failed to encode response for {}: {}.", _0, _1)]
+	EncodeProto(B::Hash, prost::EncodeError),
+	#[display(fmt = "Failed to decode block hash: {}.", _0)]
+	DecodeScale(codec::Error),
+	#[display(fmt = "Failed to proof finality for {}: {}.", _0, _1)]
+	ProofFinality(B::Hash, sp_blockchain::Error),
+	#[display(fmt = "Failed to send response.")]
+	SendResponse,
 }
