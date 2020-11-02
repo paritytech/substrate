06dbd9893295efb8a3e177c5fa67ef7dc79f007c
Max Inden <mail@max-inden.de>
2020-11-02 19:13:29 +0100

    client/network/block_request_handler: Log errors

--- client/network/src/block_request_handler.rs
+++ client/network/src/block_request_handler.rs
@@ -17,20 +17,25 @@
 //! Helper for handling (i.e. answering) block requests from a remote peer via the
 //! [`crate::request_responses::RequestResponsesBehaviour`].
 
-use crate::request_responses::ProtocolConfig;
-use futures::channel::mpsc;
-use futures::stream::StreamExt;
-use std::sync::{Arc};
-pub use crate::{
-	protocol::{message::{self, BlockAttributes}},
-	chain::{Client, FinalityProofProvider},
-	config::ProtocolId,
-};
-use sp_runtime::{traits::Block as BlockT};
-use crate::request_responses::IncomingRequest;
-use prost::Message;
 use codec::{Encode, Decode};
-use sp_runtime::{generic::BlockId, traits::{Header, One, Zero}};
+use crate::chain::Client;
+use crate::config::ProtocolId;
+use crate::protocol::{message::BlockAttributes};
+use crate::request_responses::{IncomingRequest, ProtocolConfig};
+use crate::schema::v1::block_request::FromBlock;
+use crate::schema::v1::{BlockResponse, Direction};
+use futures::channel::{mpsc, oneshot};
+use futures::stream::StreamExt;
+use log::debug;
+use prost::Message;
+use sp_runtime::{traits::Block as BlockT, generic::BlockId, traits::{Header, One, Zero}};
+use std::cmp::min;
+use std::sync::{Arc};
+use std::time::Duration;
+
+const LOG_TARGET: &str = "block-request-handler";
+const MAX_BLOCKS_IN_RESPONSE: usize = 128;
+const MAX_BODY_BYTES: usize = 8 * 1024 * 1024;
 
 /// Generate the block protocol name from chain specific protocol identifier.
 pub fn generate_protocol_name(protocol_id: ProtocolId) -> String {
@@ -43,158 +48,160 @@ pub fn generate_protocol_name(protocol_id: ProtocolId) -> String {
 
 /// Handler for incoming block requests from a remote peer.
 pub struct BlockRequestHandler<B> {
-	// TODO: Rename?
-	chain: Arc<dyn Client<B>>,
+	client: Arc<dyn Client<B>>,
 	request_receiver: mpsc::Receiver<IncomingRequest>,
 }
 
 impl <B: BlockT> BlockRequestHandler<B> {
 	/// Create a new [`BlockRequestHandler`].
 	pub fn new(protocol_id: ProtocolId, client: Arc<dyn Client<B>>) -> (Self, ProtocolConfig) {
-		let (tx, rx) = mpsc::channel(0);
+		// TODO: Likeley we want to allow more than 0 buffered requests. Rethink this value.
+		let (tx, request_receiver) = mpsc::channel(0);
 
 		let protocol_config = ProtocolConfig {
 			name: generate_protocol_name(protocol_id).into(),
-			// TODO: Change.
-			max_request_size: 4096,
-			// TODO: Change.
-			max_response_size: 4096,
-			request_timeout: std::time::Duration::from_secs(10),
+			max_request_size: 1024 * 1024,
+			max_response_size: 16 * 1024 * 1024,
+			request_timeout: Duration::from_secs(40),
 			inbound_queue: Some(tx),
 		};
 
-		let handler = Self {
-			chain: client,
-			request_receiver: rx,
+		(Self { client, request_receiver }, protocol_config)
+	}
+
+	fn handle_request(
+		&self,
+		payload: Vec<u8>,
+		pending_response: oneshot::Sender<Vec<u8>>
+	) -> Result<(), HandleRequestError> {
+		let request = crate::schema::v1::BlockRequest::decode(&payload[..])?;
+
+		let from_block_id =	match request.from_block.ok_or(HandleRequestError::MissingFromField)? {
+			FromBlock::Hash(ref h) => {
+				let h = Decode::decode(&mut h.as_ref())?;
+				BlockId::<B>::Hash(h)
+			}
+			FromBlock::Number(ref n) => {
+				let n = Decode::decode(&mut n.as_ref())?;
+				BlockId::<B>::Number(n)
+			}
 		};
 
-		(handler, protocol_config)
+		let max_blocks = if request.max_blocks == 0 {
+			MAX_BLOCKS_IN_RESPONSE
+		} else {
+			min(request.max_blocks as usize, MAX_BLOCKS_IN_RESPONSE)
+		};
+
+		let direction = Direction::from_i32(request.direction)
+			.ok_or(HandleRequestError::ParseDirection)?;
+		let attributes = BlockAttributes::from_be_u32(request.fields)?;
+		let get_header = attributes.contains(BlockAttributes::HEADER);
+		let get_body = attributes.contains(BlockAttributes::BODY);
+		let get_justification = attributes.contains(BlockAttributes::JUSTIFICATION);
+
+		let mut blocks = Vec::new();
+		let mut block_id = from_block_id;
+
+		let mut total_size: usize = 0;
+		while let Some(header) = self.client.header(block_id).unwrap_or(None) {
+			let number = *header.number();
+			let hash = header.hash();
+			let parent_hash = *header.parent_hash();
+			let justification = if get_justification {
+				self.client.justification(&BlockId::Hash(hash))?
+			} else {
+				None
+			};
+			let is_empty_justification = justification.as_ref().map(|j| j.is_empty()).unwrap_or(false);
+
+			let body = if get_body {
+				match self.client.block_body(&BlockId::Hash(hash))? {
+					Some(mut extrinsics) => extrinsics.iter_mut()
+						.map(|extrinsic| extrinsic.encode())
+						.collect(),
+					None => {
+						log::trace!(target: "sync", "Missing data for block request.");
+						break;
+					}
+				}
+			} else {
+				Vec::new()
+			};
+
+			let block_data = crate::schema::v1::BlockData {
+				hash: hash.encode(),
+				header: if get_header {
+					header.encode()
+				} else {
+					Vec::new()
+				},
+				body,
+				receipt: Vec::new(),
+				message_queue: Vec::new(),
+				justification: justification.unwrap_or_default(),
+				is_empty_justification,
+			};
+
+			total_size += block_data.body.len();
+			blocks.push(block_data);
+
+			if blocks.len() >= max_blocks as usize || total_size > MAX_BODY_BYTES {
+				break
+			}
+
+			match direction {
+				Direction::Ascending => {
+					block_id = BlockId::Number(number + One::one())
+				}
+				Direction::Descending => {
+					if number.is_zero() {
+						break
+					}
+					block_id = BlockId::Hash(parent_hash)
+				}
+			}
+		}
+
+		let res = BlockResponse { blocks };
+
+		let mut data = Vec::with_capacity(res.encoded_len());
+		res.encode(&mut data)?;
+
+		pending_response.send(data)
+			.map_err(|_| HandleRequestError::SendResponse)
 	}
 
 	/// Run [`BlockRequestHandler`].
 	pub async fn run(mut self) {
-		while let Some(crate::request_responses::IncomingRequest { peer: _, payload, pending_response }) = self.request_receiver.next().await {
+		while let Some(request) = self.request_receiver.next().await {
+			let IncomingRequest { peer, payload, pending_response } = request;
 
-			let request = crate::schema::v1::BlockRequest::decode(&payload[..]).unwrap();
-
-
-
-			let from_block_id =	match request.from_block {
-				Some(crate::schema::v1::block_request::FromBlock::Hash(ref h)) => {
-					let h = Decode::decode(&mut h.as_ref()).unwrap();
-					BlockId::<B>::Hash(h)
-				}
-				Some(crate::schema::v1::block_request::FromBlock::Number(ref n)) => {
-					let n = Decode::decode(&mut n.as_ref()).unwrap();
-					BlockId::<B>::Number(n)
-				}
-				None => {
-					panic!();
-					// let msg = "missing `BlockRequest::from_block` field";
-					// return Err(io::Error::new(io::ErrorKind::Other, msg).into())
-				}
-			};
-
-			// let max_blocks =
-			// 	if request.max_blocks == 0 {
-			// 		self.config.max_block_data_response
-			// 	} else {
-			// 		min(request.max_blocks, self.config.max_block_data_response)
-			// 	};
-
-			let direction =
-				if request.direction == crate::schema::v1::Direction::Ascending as i32 {
-					crate::schema::v1::Direction::Ascending
-				} else if request.direction == crate::schema::v1::Direction::Descending as i32 {
-					crate::schema::v1::Direction::Descending
-				} else {
-					panic!();
-					// let msg = format!("invalid `BlockRequest::direction` value: {}", request.direction);
-					// return Err(io::Error::new(io::ErrorKind::Other, msg).into())
-				};
-
-			let attributes = BlockAttributes::from_be_u32(request.fields).unwrap();
-			let get_header = attributes.contains(BlockAttributes::HEADER);
-			let get_body = attributes.contains(BlockAttributes::BODY);
-			let get_justification = attributes.contains(BlockAttributes::JUSTIFICATION);
-
-			let mut blocks = Vec::new();
-			let mut block_id = from_block_id;
-			// TODO: Still needed?
-			// let mut total_size = 0;
-			while let Some(header) = self.chain.header(block_id).unwrap_or(None) {
-				// if blocks.len() >= max_blocks as usize
-				// 	|| (blocks.len() >= 1 && total_size > self.config.max_block_body_bytes)
-				// {
-				// 	break
-				// }
-
-				let number = *header.number();
-				let hash = header.hash();
-				let parent_hash = *header.parent_hash();
-				let justification = if get_justification {
-					self.chain.justification(&BlockId::Hash(hash)).unwrap()
-				} else {
-					None
-				};
-				let is_empty_justification = justification.as_ref().map(|j| j.is_empty()).unwrap_or(false);
-
-				let body = if get_body {
-					match self.chain.block_body(&BlockId::Hash(hash)).unwrap() {
-						Some(mut extrinsics) => extrinsics.iter_mut()
-							.map(|extrinsic| extrinsic.encode())
-							.collect(),
-						None => {
-							log::trace!(target: "sync", "Missing data for block request.");
-							break;
-						}
-					}
-				} else {
-					Vec::new()
-				};
-
-				let block_data = crate::schema::v1::BlockData {
-					hash: hash.encode(),
-					header: if get_header {
-						header.encode()
-					} else {
-						Vec::new()
-					},
-					body,
-					receipt: Vec::new(),
-					message_queue: Vec::new(),
-					justification: justification.unwrap_or_default(),
-					is_empty_justification,
-				};
-
-				// TODO: Still needed?
-				// total_size += block_data.body.len();
-				blocks.push(block_data);
-
-				match direction {
-					crate::schema::v1::Direction::Ascending => {
-						block_id = BlockId::Number(number + One::one())
-					}
-					crate::schema::v1::Direction::Descending => {
-						if number.is_zero() {
-							break
-						}
-						block_id = BlockId::Hash(parent_hash)
-					}
-				}
+			match self.handle_request(payload, pending_response) {
+				Ok(()) => debug!(target: LOG_TARGET, "Handled block request from {}.", peer),
+				Err(e) => debug!(
+					target: LOG_TARGET,
+					"Failed to handle block request from {}: {}",
+					peer, e,
+				),
 			}
-
-			let res = crate::schema::v1::BlockResponse { blocks };
-
-			let mut data = Vec::with_capacity(res.encoded_len());
-			res.encode(&mut data).unwrap();
-			// log::debug!(
-			// 	target: "sync",
-			// 	"Error encoding block response for peer {}: {}",
-			// 	peer, e
-			// )
-
-			pending_response.send(data).unwrap();
 		}
 	}
+}
+
+#[derive(derive_more::Display, derive_more::From)]
+enum HandleRequestError {
+	#[display(fmt = "Failed to decode request: {}.", _0)]
+	DecodeProto(prost::DecodeError),
+	#[display(fmt = "Failed to encode response: {}.", _0)]
+	EncodeProto(prost::EncodeError),
+	#[display(fmt = "Failed to decode block hash: {}.", _0)]
+	DecodeScale(codec::Error),
+	#[display(fmt = "Missing `BlockRequest::from_block` field.")]
+	MissingFromField,
+	#[display(fmt = "Failed to parse BlockRequest::direction.")]
+	ParseDirection,
+	Client(sp_blockchain::Error),
+	#[display(fmt = "Failed to send response.")]
+	SendResponse,
 }
