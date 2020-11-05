4cd3d47a58bbffc30bdc1bcb3b178afbd05e18ae
Bastian Köcher <git@kchr.de>
2020-11-05 16:52:10 +0100

    Added comment

--- client/basic-authorship/src/basic_authorship.rs
+++ client/basic-authorship/src/basic_authorship.rs
@@ -45,6 +45,10 @@ use sc_proposer_metrics::MetricsLink as PrometheusMetrics;
 /// Default maximum block size in bytes used by [`Proposer`].
 ///
 /// Can be overwritten by [`ProposerFactory::set_maxium_block_size`].
+///
+/// Be aware that there is also an upper packet size on what the networking code
+/// will accept. If the block doesn't fit in such a package, it can not be
+/// transferred to other nodes.
 pub const DEFAULT_MAX_BLOCK_SIZE: usize = 4 * 1024 * 1024 + 512;
 
 /// Proposer factory.
