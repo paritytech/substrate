095d0f06a42b66a92f28d097499a69e3f170d59d
Jon Häggblad <jon.haggblad@gmail.com>
2020-11-11 14:29:19 +0100

    slots: incrementally backoff claiming slots if finality lags behind (#7186)
    * babe: backoff authoring blocks when finality lags
    
    * babe: move backoff authoring params to default constructor
    
    * babe: deduplicate the test a bit
    
    * babe: set backoff constants in service
    
    * babe: use better names for backoff authoring block parameters
    
    * babe: remove last unwrap
    
    * babe: slight style tweak
    
    * babe: fix comment
    
    * slots: move backoff block authorship logic to SimpleSlotWorker
    
    * aura: append SlotInfo in on_slot
    
    * slots: use the correct types for parameters
    
    * slots: fix review comments
    
    * aura: add missing backoff authoring blocks parameters
    
    * slots: add comments for default values
    
    * slots: add additional checks in test
    
    * slots: update implementation for new master
    
    * slots: revert the change to SlotInfo
    
    * Fix review comments
    
    * slots: rework unit tests for backing off claiming slots
    
    * slots: add test for asymptotic behaviour for slot claims
    
    * slots: address review comments
    
    * slots: add test for max_interval
    
    * slots: add assertion for intervals between between claimed slots
    
    * slots: remove rustfmt directive
    
    * slots: another attempt at explaining authoring_rate
    
    * slots: up unfinalized_slack to 50 by default
    
    * slots: add tests for time to reach max_interval
    
    * slots: fix typo in comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * slots: additional tweaks to comments and info calls
    
    * slots: rename to BackoffAuthoringOnFinalizedHeadLagging
    
    * slots: make the backing off strategy generic
    
    * Apply suggestions from code review
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * slots: implement backoff trait for () for simplicity
    
    * slots: move logging inside backing off function to make it more specific
    
    * aura: add missing function parameter
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- bin/node/cli/src/service.rs
+++ bin/node/cli/src/service.rs
@@ -204,6 +204,8 @@ pub fn new_full_base(
 
 	let role = config.role.clone();
 	let force_authoring = config.force_authoring;
+	let backoff_authoring_blocks =
+		Some(sc_consensus_slots::BackoffAuthoringOnFinalizedHeadLagging::default());
 	let name = config.network.node_name.clone();
 	let enable_grandpa = !config.disable_grandpa;
 	let prometheus_registry = config.prometheus_registry().cloned();
@@ -249,6 +251,7 @@ pub fn new_full_base(
 			sync_oracle: network.clone(),
 			inherent_data_providers: inherent_data_providers.clone(),
 			force_authoring,
+			backoff_authoring_blocks,
 			babe_link,
 			can_author_with,
 		};
