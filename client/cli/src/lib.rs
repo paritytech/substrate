6416651bf5ddcf158e6bbd76b33eaf30209b5a0d
Cecile Tonglet <cecile@parity.io>
2020-11-12 15:01:53 +0100

    Fix tests

--- client/cli/src/lib.rs
+++ client/cli/src/lib.rs
@@ -284,7 +284,12 @@ mod tests {
 	#[test]
 	fn test_logger_filters() {
 		let test_pattern = "afg=debug,sync=trace,client=warn,telemetry,something-with-dash=error";
-		init_logger(&test_pattern, Default::default(), Default::default()).unwrap();
+		init_logger(
+			&test_pattern,
+			Default::default(),
+			Default::default(),
+			Default::default(),
+		).unwrap();
 
 		tracing::dispatcher::get_default(|dispatcher| {
 			let test_filter = |target, level| {
@@ -343,7 +348,12 @@ mod tests {
 	fn log_something_with_dash_target_name() {
 		if env::var("ENABLE_LOGGING").is_ok() {
 			let test_pattern = "test-target=info";
-			init_logger(&test_pattern, Default::default(), Default::default()).unwrap();
+			init_logger(
+				&test_pattern,
+				Default::default(),
+				Default::default(),
+				Default::default(),
+			).unwrap();
 
 			log::info!(target: "test-target", "{}", EXPECTED_LOG_MESSAGE);
 		}
@@ -379,7 +389,12 @@ mod tests {
 	fn prefix_in_log_lines_entrypoint() {
 		if env::var("ENABLE_LOGGING").is_ok() {
 			let test_pattern = "test-target=info";
-			init_logger(&test_pattern, Default::default(), Default::default()).unwrap();
+			init_logger(
+				&test_pattern,
+				Default::default(),
+				Default::default(),
+				Default::default(),
+			).unwrap();
 			prefix_in_log_lines_process();
 		}
 	}
