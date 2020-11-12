63d7c09d9f5c3a2cf527ad35e3977add1874a369
Bastian Köcher <git@kchr.de>
2020-11-12 19:36:09 +0100

    Update docs

--- test-utils/runtime/build.rs
+++ test-utils/runtime/build.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use wasm_builder::WasmBuilder;
+use substrate_wasm_builder::WasmBuilder;
 
 fn main() {
 	WasmBuilder::new()
