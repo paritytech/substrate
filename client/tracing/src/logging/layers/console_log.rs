39306cada4117253504c40e0dc022a328bbe50a0
Cecile Tonglet <cecile@parity.io>
2020-11-25 14:03:47 +0100

    Fix wrong path

--- client/tracing/src/logging/layers/console_log.rs
+++ client/tracing/src/logging/layers/console_log.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::event_format::{CustomFmtContext, EventFormat};
+use crate::logging::event_format::{CustomFmtContext, EventFormat};
 use std::fmt;
 use tracing::{Event, Level, Subscriber};
 use tracing_subscriber::{
