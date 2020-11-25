61ff1ccb2fce2c410f44f7673bba4523ff4a58b0
Alexander Popiak <alexander.popiak@parity.io>
2020-11-25 17:37:39 +0100

    re-add note about availability in no_std envs

--- frame/support/src/lib.rs
+++ frame/support/src/lib.rs
@@ -102,7 +102,8 @@ pub enum Never {}
 ///
 /// - Using `static` to create a static parameter type. Its value is
 ///   being provided by a static variable with the equivalent name in `UPPER_SNAKE_CASE`. An
-///   additional `set` function is provided in this case to alter the static variable. 
+///   additional `set` function is provided in this case to alter the static variable.
+///   **This is intended for testing ONLY and is ONLY available when `std` is enabled.**
 ///
 /// # Examples
 ///
