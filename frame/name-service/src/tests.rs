50d57c5dd7df5314dcd621e7d754e56fe4379659
Stanly Johnson <stanly@sovereignwallet.network>
2020-12-05 20:00:45 +0530

    set_name and bid tests

--- frame/name-service/src/tests.rs
+++ frame/name-service/src/tests.rs
@@ -23,8 +23,9 @@ use super::*;
 use super::mock::*;
 use sp_core::blake2_256;
 use frame_support::{
-	assert_ok, assert_noop,
-	traits::{OnInitialize, OnFinalize}
+	assert_ok, assert_noop, assert_err,
+	traits::{OnInitialize, OnFinalize},
+	error::BadOrigin
 };
 use pallet_balances::Error as BalancesError;
 
@@ -91,4 +92,71 @@ fn end_to_end_should_work() {
 		// Name can be bid for again.
 		assert_ok!(NameService::bid(Origin::signed(1), name_hash, 5));
 	});
+}
+
+#[test]
+fn manager_can_set_name(){
+	new_test_ext().execute_with(|| {
+		// Test data
+		let name = b"shawntabrizi";
+		let name_hash = blake2_256(name);
+
+		// name setting by manager works correctly
+		let status = NameStatus::default();
+		assert_ok!(NameService::set_name(Origin::signed(100), name_hash.clone(), status.clone()));
+		
+		let stored_data = Registration::<Test>::get(&name_hash);
+		assert_eq!(stored_data, NameStatus::default());
+
+		// non manager call should fail
+		assert_err!(NameService::set_name(Origin::signed(1), name_hash, status), BadOrigin);
+	})
+}
+
+#[test]
+fn bid_function_works(){
+	new_test_ext().execute_with(|| {
+		// Test data
+		let name = b"shawntabrizi";
+		let name_hash = blake2_256(name);
+
+		// create bid works correctly
+		assert_ok!(NameService::bid(Origin::signed(1), name_hash, 5));
+		let stored_data = Registration::<Test>::get(&name_hash);
+		assert_eq!(stored_data, NameStatus::Bidding {
+			who: 1,
+			bid_end: <mock::Test as Trait>::BiddingPeriod::get(),
+			amount: 5
+		});
+		assert_eq!(Balances::free_balance(&1), 95);
+
+		// another bid at same price should fail
+		assert_err!(NameService::bid(Origin::signed(2), name_hash, 5), Error::<Test>::InvalidBid);
+
+		// previous bidder should be able to raise bid
+		run_to_block(2);
+		assert_ok!(NameService::bid(Origin::signed(1), name_hash, 10));
+		let stored_data = Registration::<Test>::get(&name_hash);
+		assert_eq!(stored_data, NameStatus::Bidding {
+			who: 1,
+			bid_end: <mock::Test as Trait>::BiddingPeriod::get().saturating_add(2),
+			amount: 10
+		});
+		assert_eq!(Balances::free_balance(&1), 90);
+
+		// another user can outbid current bidder
+		assert_ok!(NameService::bid(Origin::signed(2), name_hash, 20));
+		let stored_data = Registration::<Test>::get(&name_hash);
+		assert_eq!(stored_data, NameStatus::Bidding {
+			who: 2,
+			bid_end: <mock::Test as Trait>::BiddingPeriod::get().saturating_add(2),
+			amount: 20
+		});
+		assert_eq!(Balances::free_balance(&1), 100);
+		assert_eq!(Balances::free_balance(&2), 180);
+
+		// cannot bid on expired item
+		run_to_block(12);
+		assert_err!(NameService::bid(Origin::signed(2), name_hash, 25), Error::<Test>::InvalidBid);
+	})
 }
