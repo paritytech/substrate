d7eee4f6581af72d947919933d36f26997fdf1cf
Denis P <denis.pisarev@parity.io>
2020-12-08 15:36:01 +0100

    CI: add a whitelist for allowed GH actions pins

---
+++ .github/allowed-actions.js
@@ -0,0 +1,7 @@
+// This is a whitelist of GitHub Actions that are approved for use in this project.
+// If a new or existing workflow file is updated to use an action or action version
+// not listed here, CI will fail.
+
+module.exports = [
+    'gaurav-nelson/github-action-markdown-link-check@95d5f3b63d2ca8e93f527396308c41e70634e2e7', // gaurav-nelson/github-action-markdown-link-check@v1.0.7
+]
