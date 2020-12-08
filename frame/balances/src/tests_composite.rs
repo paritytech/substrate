ddc57b0f77e9f57e8bcc7325c72e43bea2789e7b
thiolliere <gui.thiolliere@gmail.com>
2020-12-08 10:55:38 +0100

    fix balances test

--- frame/balances/src/tests_composite.rs
+++ frame/balances/src/tests_composite.rs
@@ -57,7 +57,7 @@ parameter_types! {
 }
 impl frame_system::Config for Test {
 	type BaseCallFilter = ();
-	type BlockWeights = ();
+	type BlockWeights = BlockWeights;
 	type BlockLength = ();
 	type DbWeight = ();
 	type Origin = Origin;
