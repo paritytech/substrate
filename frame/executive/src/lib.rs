566ad03cf8c07c3c3ab226a724b2dcb4b6dbbc9b
Sergei Shulepov <sergei@parity.io>
2020-12-15 20:07:04 +0100

    Leave some header related info for inspection (#7727)
    * Leave some system data for inspection
    
    There is not much benefit in being active when removing this data. It's
    actively harmful when one tries to read the block number in runtime
    APIs in the context of a block.
    
    * Update the expected root hash
    
    This is excepted since now we persist new members.
    
    * Revert extrinsics_root to `take`
    
    It's going away in one of the following PRs anyway
    
    * Update the state root once again
    
    * Update the comment on the storage items that are left in the storage
    
    Excluding ExtrinsicsRoot since it's going away

--- frame/executive/src/lib.rs
+++ frame/executive/src/lib.rs
@@ -753,7 +753,7 @@ mod tests {
 				header: Header {
 					parent_hash: [69u8; 32].into(),
 					number: 1,
-					state_root: hex!("6a3ad91caba5b8ac15c325a36d7568adf6a7e49321865de7527b851d870343d4").into(),
+					state_root: hex!("ba1a82a264b8007e0c04c9ea35e541593daad08b6e2bf7c0a6780a67d1c55018").into(),
 					extrinsics_root: hex!("03170a2e7597b7b7e3d84c05391d139a62b157e78786d8c082f29dcf4c111314").into(),
 					digest: Digest { logs: vec![], },
 				},
