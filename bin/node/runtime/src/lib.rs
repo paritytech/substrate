cf37341856bdfe58546d3b042ec6d3a5b0926023
Robert Habermeier <rphmeier@gmail.com>
2020-12-26 19:34:39 -0500

    add a `current_epoch` to BabeApi (#7789)
    * add a `current_epoch` to BabeApi
    
    * add current_epoch_start back again
    
    * fix node-runtime
    
    * bump spec version

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -111,7 +111,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 260,
+	spec_version: 261,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
@@ -1139,6 +1139,10 @@ impl_runtime_apis! {
 
 		fn current_epoch_start() -> sp_consensus_babe::SlotNumber {
 			Babe::current_epoch_start()
+		}
+
+		fn current_epoch() -> sp_consensus_babe::Epoch {
+			Babe::current_epoch()
 		}
 
 		fn generate_key_ownership_proof(
