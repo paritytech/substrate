9b08105b8c7106d723c4f470304ad9e2868569d9
Bastian Köcher <bkchr@users.noreply.github.com>
2021-01-04 10:03:13 +0100

    Happy new year (#7814)
    * Happy new year
    
    Updates the copyright years and fixes wrong license headers.
    
    * Fix the template
    
    * Split HEADER into HEADER-APACHE & HEADER-GPL

--- frame/utility/src/tests.rs
+++ frame/utility/src/tests.rs
@@ -1,6 +1,6 @@
 // This file is part of Substrate.
 
-// Copyright (C) 2019-2020 Parity Technologies (UK) Ltd.
+// Copyright (C) 2019-2021 Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
