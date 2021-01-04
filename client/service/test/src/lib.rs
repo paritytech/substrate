02199bc95d91e213aca4b6630a4d1ca7260e679e
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-01-04 10:41:19 +0100

    Revert "CI debugging"
    This reverts commit 5c7911c8122d3520992c96cb757dbdb71f41b1ea.

--- client/service/test/src/lib.rs
+++ client/service/test/src/lib.rs
@@ -194,7 +194,7 @@ where F: Clone + Send + 'static, L: Clone + Send +'static, U: Clone + Send + 'st
 		match self.runtime.block_on(interval) {
 			Ok(()) => unreachable!("interval always fails; qed"),
 			Err(ref err) if err.is_inner() => (),
-			Err(_) => { log::error!("Waited for too long"); panic!("Waited for too long"); },
+			Err(_) => panic!("Waited for too long"),
 		}
 	}
 }
