97736e11bd50b22699910e6aa04243fac612b4df
cheme <emericchevalier.pro@gmail.com>
2021-01-05 17:04:28 +0100

    Update client/grandpa-warp-sync/src/lib.rs
    quick fix

--- client/grandpa-warp-sync/src/lib.rs
+++ client/grandpa-warp-sync/src/lib.rs
@@ -108,7 +108,7 @@ impl<TBlock: BlockT, TBackend: Backend<TBlock>> GrandpaWarpSyncRequestHandler<TB
 	{
 		let request = Request::<TBlock>::decode(&mut &payload[..])?;
 
-		let response = sc_finality_grandpa::finality_proof::prove_warp_sync(
+		let response = sc_finality_grandpa::prove_warp_sync(
 			self.backend.blockchain(), request.begin,
 		)?;
 
