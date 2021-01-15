1709098b2d6940e580b11ab03b0c3822e8fab8a8
Andrew Jones <ascjones@gmail.com>
2021-01-15 11:16:30 +0000

    Single line import

--- frame/system/src/extensions/check_nonce.rs
+++ frame/system/src/extensions/check_nonce.rs
@@ -17,9 +17,7 @@
 
 use codec::{Encode, Decode};
 use crate::Config;
-use frame_support::{
-	weights::DispatchInfo,
-};
+use frame_support::weights::DispatchInfo;
 use sp_runtime::{
 	traits::{SignedExtension, DispatchInfoOf, Dispatchable, One},
 	transaction_validity::{
