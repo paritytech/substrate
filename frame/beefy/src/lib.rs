e1f86a49b3bcb2e4845400b578579fcbf32bce9f
Andreas Doerr <adoerr@users.noreply.github.com>
2021-01-19 18:58:28 +0100

    Update README (#58)
    * Update README
    
    * Apply suggestions from code review
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    
    * address review comments
    
    * missed a typo
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>

---
+++ frame/beefy/src/lib.rs
@@ -0,0 +1,121 @@
+// Copyright (C) 2020 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::Encode;
+
+use beefy_primitives::{AuthorityIndex, ConsensusLog, BEEFY_ENGINE_ID};
+use frame_support::{decl_module, decl_storage, Parameter};
+use sp_runtime::{
+	generic::DigestItem,
+	traits::{IsMember, Member},
+	RuntimeAppPublic,
+};
+use sp_std::prelude::*;
+
+pub trait Config: frame_system::Config {
+	/// The identifier type for an authority.
+	type AuthorityId: Member + Parameter + RuntimeAppPublic + Default;
+}
+
+decl_storage! {
+	trait Store for Module<T: Config> as Beefy {
+		/// The current list of authorities.
+		pub Authorities get(fn authorities): Vec<T::AuthorityId>;
+		/// Authorities scheduled for the next session.
+		pub NextAuthorities get(fn next_authorities): Vec<T::AuthorityId>;
+	}
+	add_extra_genesis {
+		config(authorities): Vec<T::AuthorityId>;
+		build(|config| Module::<T>::initialize_authorities(&config.authorities))
+	}
+}
+
+decl_module! {
+	pub struct Module<T: Config> for enum Call where origin: T::Origin { }
+}
+
+impl<T: Config> Module<T> {
+	fn change_authorities(new: Vec<T::AuthorityId>, queued: Vec<T::AuthorityId>) {
+		// As in GRANDPA, we don't trigger validator set change if the set actually
+		// remains the same.
+		if new != Self::authorities() {
+			<Authorities<T>>::put(&new);
+			let log: DigestItem<T::Hash> =
+				DigestItem::Consensus(BEEFY_ENGINE_ID, ConsensusLog::AuthoritiesChange(new).encode());
+			<frame_system::Module<T>>::deposit_log(log);
+		}
+
+		<NextAuthorities<T>>::put(&queued);
+	}
+
+	fn initialize_authorities(authorities: &[T::AuthorityId]) {
+		if !authorities.is_empty() {
+			assert!(
+				<Authorities<T>>::get().is_empty(),
+				"Authorities are already initialized!"
+			);
+			<Authorities<T>>::put(authorities);
+			// for consistency we initialize the next validator set as well.
+			// Note it's an assumption in the `pallet_session` as well.
+			<NextAuthorities<T>>::put(authorities);
+		}
+	}
+}
+
+impl<T: Config> sp_runtime::BoundToRuntimeAppPublic for Module<T> {
+	type Public = T::AuthorityId;
+}
+
+impl<T: Config> pallet_session::OneSessionHandler<T::AccountId> for Module<T> {
+	type Key = T::AuthorityId;
+
+	fn on_genesis_session<'a, I: 'a>(validators: I)
+	where
+		I: Iterator<Item = (&'a T::AccountId, T::AuthorityId)>,
+	{
+		let authorities = validators.map(|(_, k)| k).collect::<Vec<_>>();
+		Self::initialize_authorities(&authorities);
+	}
+
+	fn on_new_session<'a, I: 'a>(changed: bool, validators: I, queued_validators: I)
+	where
+		I: Iterator<Item = (&'a T::AccountId, T::AuthorityId)>,
+	{
+		if changed {
+			let next_authorities = validators.map(|(_, k)| k).collect::<Vec<_>>();
+			let next_queued_authorities = queued_validators.map(|(_, k)| k).collect::<Vec<_>>();
+
+			Self::change_authorities(next_authorities, next_queued_authorities);
+		}
+	}
+
+	fn on_disabled(i: usize) {
+		let log: DigestItem<T::Hash> = DigestItem::Consensus(
+			BEEFY_ENGINE_ID,
+			ConsensusLog::<T::AuthorityId>::OnDisabled(i as AuthorityIndex).encode(),
+		);
+
+		<frame_system::Module<T>>::deposit_log(log);
+	}
+}
+
+impl<T: Config> IsMember<T::AuthorityId> for Module<T> {
+	fn is_member(authority_id: &T::AuthorityId) -> bool {
+		Self::authorities().iter().any(|id| id == authority_id)
+	}
+}
