7577f98b54c8b7d1a3eb49bf76a15f942ce803c2
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2021-01-27 12:14:23 +0300

    Apply suggestions from code review
    Co-authored-by: Pierre Krieger <pierre.krieger1708@gmail.com>

--- client/network/src/bitswap.rs
+++ client/network/src/bitswap.rs
@@ -1,4 +1,4 @@
-// Copyright 2020 Parity Technologies (UK) Ltd.
+// Copyright 2021 Parity Technologies (UK) Ltd.
 // This file is part of Substrate.
 
 // Substrate is free software: you can redistribute it and/or modify
@@ -24,7 +24,7 @@ use std::collections::VecDeque;
 use std::io;
 use std::sync::Arc;
 use std::task::{Context, Poll};
-use cid::{self, Version};
+use cid::Version;
 use codec::Encode;
 use core::pin::Pin;
 use futures::Future;
@@ -212,7 +212,7 @@ impl<B: BlockT> NetworkBehaviour for Bitswap<B> {
 			HandlerEvent::ResponseSent => return,
 			HandlerEvent::Request(msg) => msg,
 		};
-		log::trace!(target: LOG_TARGET, "Received request: {:?} from {}", request, peer);
+		trace!(target: LOG_TARGET, "Received request: {:?} from {}", request, peer);
 		if self.ready_blocks.len() > MAX_RESPONSE_QUEUE {
 			debug!(target: LOG_TARGET, "Ignored request: queue is full");
 			return;
