a28b47326e0bfa8b15ecdfcfdaad74bb1080fa4c
Joshy Orndorff <admin@joshyorndorff.com>
2021-01-28 09:39:07 -0500

    caution and link to issue

--- primitives/inherents/src/lib.rs
+++ primitives/inherents/src/lib.rs
@@ -421,6 +421,9 @@ pub trait ProvideInherent {
 	/// If the inherent is not present, it will return `e`.
 	///
 	/// Err(_) indicates that this function failed and further operations should be aborted.
+	///
+	/// CAUTION: This check has a bug whe used in pallets that also provide unsigned transactions.
+	/// See https://github.com/paritytech/substrate/issues/6243 for details.
 	fn is_inherent_required(_: &InherentData) -> Result<Option<Self::Error>, Self::Error> { Ok(None) }
 
 	/// Check whether the given inherent is valid. Checking the inherent is optional and can be
