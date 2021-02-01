c21aecb2476099db205df8e3db2a0bc4a08daf30
Alexander Popiak <alexander.popiak@parity.io>
2021-02-01 15:47:56 +0100

    add doc comments for AllModules

--- frame/support/procedural/src/construct_runtime/mod.rs
+++ frame/support/procedural/src/construct_runtime/mod.rs
@@ -470,7 +470,10 @@ fn decl_all_modules<'a>(
 
 	quote!(
 		#types
+		/// All pallets included in the runtime as a nested tuple of types.
+		/// Excludes the System pallet.
 		pub type AllModules = ( #all_modules );
+		/// All pallets included in the runtime as a nested tuple of types.
 		pub type AllModulesWithSystem = ( #all_modules_with_system );
 	)
 }
