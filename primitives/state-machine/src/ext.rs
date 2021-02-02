3c96128de181a9f68664bce1d9880980dd940cd1
Andrew Plaza <andrew.plaza@parity.io>
2021-02-02 13:21:10 +0100

    remove unused trace import

--- primitives/state-machine/src/ext.rs
+++ primitives/state-machine/src/ext.rs
@@ -33,7 +33,7 @@ use sp_externalities::{
 use codec::{Decode, Encode, EncodeAppend};
 
 use sp_std::{fmt, any::{Any, TypeId}, vec::Vec, vec, boxed::Box};
-use crate::{warn, trace, log_error};
+use crate::{warn, log_error};
 #[cfg(feature = "std")]
 use crate::changes_trie::State as ChangesTrieState;
 use crate::StorageTransactionCache;
