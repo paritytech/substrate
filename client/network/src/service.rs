cf0b7a4ebe12c04fd0fa0e6f0e9c0b9b67b0470e
Robert Klotzner <eskimor@users.noreply.github.com>
2021-02-02 18:08:05 +0100

    Update client/network/src/service.rs
    Typo.
    
    Co-authored-by: Roman Borschel <romanb@users.noreply.github.com>

--- client/network/src/service.rs
+++ client/network/src/service.rs
@@ -851,7 +851,7 @@ impl<B: BlockT + 'static, H: ExHashT> NetworkService<B, H> {
 	///
 	/// Keep in mind that the connected receiver might receive a `Canceled` event in case of a
 	/// closing connection. This is expected behaviour. With `request` you would get a
-	/// `RequestFailure::Network(OutboundFailure::ConnectionClosed` in that case.
+	/// `RequestFailure::Network(OutboundFailure::ConnectionClosed)` in that case.
 	pub fn detached_request(
 		&self,
 		target: PeerId,
