6d0fe15d5166b7438c99c4b22060e9c298c5669d
Bastian Köcher <bkchr@users.noreply.github.com>
2021-02-02 16:14:35 +0100

    Update frame/aura/src/migrations.rs
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>

--- frame/aura/src/migrations.rs
+++ frame/aura/src/migrations.rs
@@ -36,7 +36,7 @@ pub trait RemoveLastTimestamp: super::Config {
 /// This storage value was removed and replaced by `CurrentSlot`. As we only remove this storage
 /// value, it is safe to call this method multiple times.
 ///
-/// This migration requires a type `T` that implements [`MigrationFromLastTimestamp`].
+/// This migration requires a type `T` that implements [`RemoveLastTimestamp`].
 pub fn remove_last_timestamp<T: RemoveLastTimestamp>() -> Weight {
 	LastTimestamp::<T>::kill();
 	T::DbWeight::get().reads(1)
