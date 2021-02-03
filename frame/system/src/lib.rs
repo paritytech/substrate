f6e6648fe4fe0d350fa354dffabbfd6903fdaa25
Shaopeng Wang <spxwang@gmail.com>
2021-02-03 13:41:51 +1300

    frame-system: Index type 'MaybeSerializeDeserialize' bound.

--- frame/system/src/lib.rs
+++ frame/system/src/lib.rs
@@ -174,7 +174,7 @@ pub mod pallet {
 		/// Account index (aka nonce) type. This stores the number of previous transactions associated
 		/// with a sender account.
 		type Index:
-			Parameter + Member + MaybeSerialize + Debug + Default + MaybeDisplay + AtLeast32Bit
+			Parameter + Member + MaybeSerializeDeserialize + Debug + Default + MaybeDisplay + AtLeast32Bit
 			+ Copy;
 
 		/// The block number type used by the runtime.
