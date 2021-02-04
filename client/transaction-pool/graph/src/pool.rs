2a47ffb620b141d0b3ef8fa1833d042b915ae185
André Silva <andrerfosilva@gmail.com>
2021-02-04 00:30:16 +0000

    client: fix transaction pool test compilation

--- client/transaction-pool/graph/src/pool.rs
+++ client/transaction-pool/graph/src/pool.rs
@@ -656,9 +656,9 @@ mod tests {
 
 		// then
 		let mut it = futures::executor::block_on_stream(stream);
-		assert_eq!(it.next(), Some(hash0));
-		assert_eq!(it.next(), Some(hash1));
-		assert_eq!(it.next(), None);
+		assert_eq!(it.next().map(|n| n.hash), Some(hash0));
+		assert_eq!(it.next().map(|n| n.hash), Some(hash1));
+		assert!(it.next().is_none());
 	}
 
 	#[test]
