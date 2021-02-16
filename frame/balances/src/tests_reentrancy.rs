7c1181ec7204914829e090f3ff93f2875d205839
RK <r.raajey@gmail.com>
2021-02-16 15:08:36 +0530

    Update frame/balances/src/tests_reentrancy.rs
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- frame/balances/src/tests_reentrancy.rs
+++ frame/balances/src/tests_reentrancy.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Test utilities
+//! Test setup for potentil reentracy and lost updates of nested mutations. 
 
 #![cfg(test)]
 
