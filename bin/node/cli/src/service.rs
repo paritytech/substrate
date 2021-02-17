743accbe3256de2fc615adcaa3ab03ebdbbb4dbd
Cecile Tonglet <cecile@parity.io>
2021-02-17 08:44:25 +0100

    Fix telemetry span not entering properly attempt 3 (#8043)
    * Fix tracing tests (#8022)
    
    * Fix tracing tests
    
    The tests were not working properly.
    
    1. Some test was setting a global subscriber, this could lead to racy
    conditions with other tests.
    
    2. A logging test called `process::exit` which is completly wrong.
    
    * Update client/tracing/src/lib.rs
    
    Co-authored-by: David <dvdplm@gmail.com>
    
    * Review comments
    
    Co-authored-by: David <dvdplm@gmail.com>
    
    * Fix tracing spans are not being forwarded to spawned task (#8009)
    
    * Fix tracing spans are not being forwarded to spawned task
    
    There is a bug that tracing spans are not forwarded to spawned task. The
    problem was that only the telemetry span was forwarded. The solution to
    this is to use the tracing provided `in_current_span` to capture the
    current active span and pass the telemetry span explictely. We will now
    always enter the span when the future is polled. This is essentially the
    same strategy as tracing is doing with its `Instrumented`, but now
    extended for our use case with having multiple spans active.
    
    * More tests
    
    * Proper test for telemetry and prefix span
    
    * WIP
    
    * Fix test (need to create & enter the span at the same time)
    
    * WIP
    
    * Remove telemtry_span from sc_service config
    
    * CLEANUP
    
    * Update comment
    
    * Incorrect indent
    
    * More meaningful name
    
    * Dedent
    
    * Naming XD
    
    * Attempt to make a more complete test
    
    * lint
    
    * Missing licenses
    
    * Remove user data
    
    * CLEANUP
    
    * Apply suggestions from code review
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * CLEANUP
    
    * Apply suggestion
    
    * Update bin/node/cli/tests/telemetry.rs
    
    Co-authored-by: David <dvdplm@gmail.com>
    
    * Wrapping lines
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    Co-authored-by: David <dvdplm@gmail.com>

--- bin/node/cli/src/service.rs
+++ bin/node/cli/src/service.rs
@@ -34,7 +34,7 @@ use sp_runtime::traits::Block as BlockT;
 use futures::prelude::*;
 use sc_client_api::{ExecutorProvider, RemoteBackend};
 use node_executor::Executor;
-use sc_telemetry::TelemetryConnectionNotifier;
+use sc_telemetry::{TelemetryConnectionNotifier, TelemetrySpan};
 
 type FullClient = sc_service::TFullClient<Block, RuntimeApi, Executor>;
 type FullBackend = sc_service::TFullBackend<Block>;
@@ -226,6 +226,9 @@ pub fn new_full_base(
 	let enable_grandpa = !config.disable_grandpa;
 	let prometheus_registry = config.prometheus_registry().cloned();
 
+	let telemetry_span = TelemetrySpan::new();
+	let _telemetry_span_entered = telemetry_span.enter();
+
 	let (_rpc_handlers, telemetry_connection_notifier) = sc_service::spawn_tasks(
 		sc_service::SpawnTasksParams {
 			config,
@@ -240,6 +243,7 @@ pub fn new_full_base(
 			remote_blockchain: None,
 			network_status_sinks: network_status_sinks.clone(),
 			system_rpc_tx,
+			telemetry_span: Some(telemetry_span.clone()),
 		},
 	)?;
 
@@ -433,6 +437,9 @@ pub fn new_light_base(mut config: Configuration) -> Result<(
 
 	let rpc_extensions = node_rpc::create_light(light_deps);
 
+	let telemetry_span = TelemetrySpan::new();
+	let _telemetry_span_entered = telemetry_span.enter();
+
 	let (rpc_handlers, telemetry_connection_notifier) =
 		sc_service::spawn_tasks(sc_service::SpawnTasksParams {
 			on_demand: Some(on_demand),
@@ -444,6 +451,7 @@ pub fn new_light_base(mut config: Configuration) -> Result<(
 			config, backend, network_status_sinks, system_rpc_tx,
 			network: network.clone(),
 			task_manager: &mut task_manager,
+			telemetry_span: Some(telemetry_span.clone()),
 		})?;
 
 	Ok((
