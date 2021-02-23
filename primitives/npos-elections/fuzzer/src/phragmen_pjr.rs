ba365292f2b0670a5b2ed1ee9195d0c5e8de9ec9
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-02-23 11:24:29 +0100

    Enable manually setting iteration parameters in single run.
    This gives us the ability to reproducably extract cases where
    honggfuzz has discovered a panic. For example:
    
    $ cargo run --release --bin phragmen_pjr -- --candidates 569 --voters 100
    Tue 23 Feb 2021 11:23:39 AM CET
       Compiling bitflags v1.2.1
       Compiling unicode-width v0.1.8
       Compiling unicode-segmentation v1.7.1
       Compiling ansi_term v0.11.0
       Compiling strsim v0.8.0
       Compiling vec_map v0.8.2
       Compiling proc-macro-error-attr v1.0.4
       Compiling proc-macro-error v1.0.4
       Compiling textwrap v0.11.0
       Compiling atty v0.2.14
       Compiling heck v0.3.2
       Compiling clap v2.33.3
       Compiling structopt-derive v0.4.14
       Compiling structopt v0.3.21
       Compiling sp-npos-elections-fuzzer v2.0.0-alpha.5 (/home/coriolinus/Documents/Projects/paritytech/substrate/primitives/npos-elections/fuzzer)
        Finished release [optimized] target(s) in 6.15s
         Running `/home/coriolinus/Documents/Projects/paritytech/substrate/target/release/phragmen_pjr -c 569 -v 100`
    thread 'main' panicked at 'unbalanced sequential phragmen must satisfy PJR', primitives/npos-elections/fuzzer/src/phragmen_pjr.rs:133:5
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    
    This is still not adequate proof that seq_phragmen is broken; it could
    very well be that our PJR checker is doing the wrong thing, or we've
    somehow missed a parameter of interest. Still, it's concerning.

--- primitives/npos-elections/fuzzer/src/phragmen_pjr.rs
+++ primitives/npos-elections/fuzzer/src/phragmen_pjr.rs
@@ -18,16 +18,34 @@
 //! Fuzzing which ensures that running unbalanced sequential phragmen always produces a result
 //! which satisfies our PJR checker.
 //!
-//! # Running
+//! ## Running a single iteration
 //!
-//! Run with `cargo hfuzz run phragmen_pjr`.
+//! Honggfuzz shuts down each individual loop iteration after a configurable time limit.
+//! It can be helpful to run a single iteration on your hardware to help benchmark how long that time
+//! limit should reasonably be. Simply run the program without the `fuzzing` configuration to run a
+//! single iteration: `cargo run --bin phragmen_pjr`.
 //!
-//! # Debugging a panic
+//! On a relatively modern machine (late 2019 era), a single iteration takes ~30 seconds with maximum
+//! parameters.
+//!
+//! ## Running
+//!
+//! Run with `HFUZZ_RUN_ARGS="-t 60" cargo hfuzz run phragmen_pjr`.
+//!
+//! Note the environment variable: by default, `cargo hfuzz` shuts down each iteration after 1 second
+//! of runtime. We significantly increase that to ensure that the fuzzing gets a chance to complete.
+//!
+//! ## Debugging a panic
 //!
 //! Once a panic is found, it can be debugged with
-//! `cargo hfuzz run-debug phragmen_pjr hfuzz_workspace/phragmen_pjr/*.fuzz`.
+//! `HFUZZ_RUN_ARGS="-t 60" cargo hfuzz run-debug phragmen_pjr hfuzz_workspace/phragmen_pjr/*.fuzz`.
+//!
 
+#[cfg(fuzzing)]
 use honggfuzz::fuzz;
+
+#[cfg(not(fuzzing))]
+use structopt::StructOpt;
 
 mod common;
 use common::{generate_random_npos_inputs, to_range};
@@ -40,42 +58,80 @@ use sp_npos_elections::{
 type AccountId = u64;
 type PerThing = sp_arithmetic::PerU16;
 
+const MIN_CANDIDATES: usize = 100;
+const MAX_CANDIDATES: usize = 1000;
+const MIN_VOTERS: usize = 100;
+const MAX_VOTERS: usize = 2000;
+
+#[cfg(fuzzing)]
 fn main() {
 	loop {
 		fuzz!(|data: (usize, usize, u64)| {
-			let (mut candidate_count, mut voter_count, seed) = data;
-			let rng = rand::rngs::SmallRng::seed_from_u64(seed);
-			candidate_count = to_range(candidate_count, 100, 1000);
-			voter_count = to_range(voter_count, 100, 2000);
-
-			let (rounds, candidates, voters) =
-				generate_random_npos_inputs(candidate_count, voter_count, rng);
-
-			// Run seq-phragmen
-			let ElectionResult {
-				winners,
-				assignments,
-			} = seq_phragmen::<AccountId, PerThing>(rounds, candidates.clone(), voters.clone(), None)
-				.expect("seq_phragmen must succeed");
-
-			// pjr_check only cares about the identity of the winner, not its balance
-			let winners = to_without_backing(winners);
-
-			// convert assignments into staked assignments
-			let assignments = assignment_ratio_to_staked(assignments, |who| {
-				let voter_idx = voters
-					.binary_search_by_key(who, |(id, _weight, _assignments)| *id)
-					.expect("voter must be present in voters list");
-				voters[voter_idx].1
-			});
-
-			let supports: Supports<AccountId> = to_supports(&winners, &assignments)
-				.expect("election result must be structurally valid");
-
-			assert!(
-				pjr_check(&supports, candidates, voters),
-				"unbalanced sequential phragmen must satisfy PJR",
-			);
+			let (candidate_count, voter_count, seed) = data;
+			iteration(candidate_count, voter_count, seed);
 		});
 	}
+}
+
+#[cfg(not(fuzzing))]
+#[derive(Debug, StructOpt)]
+struct Opt {
+	/// How many candidates participate in this election
+	#[structopt(short, long)]
+	candidates: Option<usize>,
+
+	/// How many voters participate in this election
+	#[structopt(short, long)]
+	voters: Option<usize>,
+
+	/// Random seed to use in this election
+	#[structopt(long)]
+	seed: Option<u64>,
+}
+
+#[cfg(not(fuzzing))]
+fn main() {
+	let opt = Opt::from_args();
+	// candidates and voters by default use the maxima, which turn out to be one less than
+	// the constant.
+	iteration(
+		opt.candidates.unwrap_or(MAX_CANDIDATES-1),
+		opt.voters.unwrap_or(MAX_VOTERS-1),
+		opt.seed.unwrap_or_default(),
+	);
+}
+
+fn iteration(mut candidate_count: usize, mut voter_count: usize, seed: u64) {
+	let rng = rand::rngs::SmallRng::seed_from_u64(seed);
+	candidate_count = to_range(candidate_count, MIN_CANDIDATES, MAX_CANDIDATES);
+	voter_count = to_range(voter_count, MIN_VOTERS, MAX_VOTERS);
+
+	let (rounds, candidates, voters) =
+		generate_random_npos_inputs(candidate_count, voter_count, rng);
+
+	// Run seq-phragmen
+	let ElectionResult {
+		winners,
+		assignments,
+	} = seq_phragmen::<AccountId, PerThing>(rounds, candidates.clone(), voters.clone(), None)
+		.expect("seq_phragmen must succeed");
+
+	// pjr_check only cares about the identity of the winner, not its balance
+	let winners = to_without_backing(winners);
+
+	// convert assignments into staked assignments
+	let assignments = assignment_ratio_to_staked(assignments, |who| {
+		let voter_idx = voters
+			.binary_search_by_key(who, |(id, _weight, _assignments)| *id)
+			.expect("voter must be present in voters list");
+		voters[voter_idx].1
+	});
+
+	let supports: Supports<AccountId> = to_supports(&winners, &assignments)
+		.expect("election result must be structurally valid");
+
+	assert!(
+		pjr_check(&supports, candidates, voters),
+		"unbalanced sequential phragmen must satisfy PJR",
+	);
 }
