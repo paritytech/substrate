44ad00d96cb3f7b174a8b6a461093fa7196ce27a
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-03-02 11:56:51 -0400

    Make Benchmark Output Analysis Function Configurable (#8228)
    * Integrate `output-analysis`
    
    * fix test
    
    * use default
    
    * cargo run --release --features=runtime-benchmarks --manifest-path=bin/node/cli/Cargo.toml -- benchmark --chain=dev --steps=50 --repeat=20 --pallet=frame_system --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --output=./frame/system/src/weights.rs --template=./.maintain/frame-weight-template.hbs
    
    * Update frame/system/src/weights.rs
    
    * cargo run --release --features runtime-benchmarks --manifest-path bin/node/cli/Cargo.toml -- benchmark --chain=dev --steps=50 --repeat=20 --pallet=frame_system --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --output=./frame/system/src/weights.rs --template=./.maintain/frame-weight-template.hbs --output-analysis=max
    
    * Update frame/system/src/weights.rs
    
    * dont discard value_dist and model
    
    * feedback
    
    Co-authored-by: Parity Benchmarking Bot <admin@parity.io>

--- frame/benchmarking/src/lib.rs
+++ frame/benchmarking/src/lib.rs
@@ -26,7 +26,7 @@ mod analysis;
 
 pub use utils::*;
 #[cfg(feature = "std")]
-pub use analysis::{Analysis, BenchmarkSelector, RegressionModel};
+pub use analysis::{Analysis, BenchmarkSelector, RegressionModel, AnalysisChoice};
 #[doc(hidden)]
 pub use sp_io::storage::root as storage_root;
 #[doc(hidden)]
