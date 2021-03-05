84ead6f12578101b2ac59b173cf1543fdd550875
JesseAbram <jesseabramowitz@hotmail.com>
2021-03-05 15:03:27 +0100

    change reduced weight to used weight

--- frame/executive/src/lib.rs
+++ frame/executive/src/lib.rs
@@ -375,10 +375,10 @@ where
 		let mut remaining_weight = max_weight.saturating_sub(weight.total());
 
 		if remaining_weight > 0 {
-			let mut reduced_weight = <frame_system::Module<System> as OnIdle<System::BlockNumber>>::on_idle(block_number, remaining_weight);
-			remaining_weight = remaining_weight.saturating_sub(reduced_weight);
-			reduced_weight = <AllModules as OnIdle<System::BlockNumber>>::on_idle(block_number, remaining_weight).saturating_add(reduced_weight);
-			<frame_system::Module::<System>>::register_extra_weight_unchecked(reduced_weight, DispatchClass::Mandatory);
+			let mut used_weight = <frame_system::Module<System> as OnIdle<System::BlockNumber>>::on_idle(block_number, remaining_weight);
+			remaining_weight = remaining_weight.saturating_sub(used_weight);
+			used_weight = <AllModules as OnIdle<System::BlockNumber>>::on_idle(block_number, remaining_weight).saturating_add(used_weight);
+			<frame_system::Module::<System>>::register_extra_weight_unchecked(used_weight, DispatchClass::Mandatory);
 		}
 
 		<frame_system::Module<System> as OnFinalize<System::BlockNumber>>::on_finalize(block_number);
