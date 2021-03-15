cd749f91e9d87cfb1e61a9257d0ffed6e553e536
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2021-03-15 07:17:56 +0100

    Update frame/system/src/lib.rs
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- frame/system/src/lib.rs
+++ frame/system/src/lib.rs
@@ -1451,7 +1451,7 @@ impl<T: Config> Module<T> {
 				Err(err) => {
 					log::trace!(
 						target: "runtime::system", 
-						"failed extrinsic at {:?} => {:?}", 
+						"Extrinsic failed at block({}): {:?}", 
 						Self::block_number(), 
 						err,
 					);
