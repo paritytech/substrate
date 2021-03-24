f9fe546b5dca5e820f67d9bd2daa405b6f4ab734
Shaun Wang <spxwang@gmail.com>
2021-03-24 21:20:10 +1300

    Update metadata type alias.

--- frame/vesting/src/lib.rs
+++ frame/vesting/src/lib.rs
@@ -182,7 +182,7 @@ pub mod pallet {
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	#[pallet::metadata(T::AccountId = "AccountId", T::Balance = "Balance")]
+	#[pallet::metadata(T::AccountId = "AccountId", BalanceOf<T> = "Balance")]
 	pub enum Event<T: Config> {
 		/// The amount vested has been updated. This could indicate more funds are available. The
 		/// balance given is the amount which is left unvested (and thus locked).
