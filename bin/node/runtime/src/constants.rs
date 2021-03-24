4b34e4e4d57f858f146bc5e5e83b5cc4db1a009f
Joshy Orndorff <admin@joshyorndorff.com>
2021-03-24 15:35:40 -0400

    Revert over-aggressive search-n-replace

--- bin/node/runtime/src/constants.rs
+++ bin/node/runtime/src/constants.rs
@@ -59,11 +59,11 @@ pub mod time {
 	// 1 in 4 blocks (on average, not counting collisions) will be primary BABE blocks.
 	pub const PRIMARY_PROBABILITY: (u64, u64) = (1, 4);
 
-	pub const EPOCH_DURATION_IN_SLOTS: BlockNumber = 10 * MINUTES;
+	pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 10 * MINUTES;
 	pub const EPOCH_DURATION_IN_SLOTS: u64 = {
 		const SLOT_FILL_RATE: f64 = MILLISECS_PER_BLOCK as f64 / SLOT_DURATION as f64;
 
-		(EPOCH_DURATION_IN_SLOTS as f64 * SLOT_FILL_RATE) as u64
+		(EPOCH_DURATION_IN_BLOCKS as f64 * SLOT_FILL_RATE) as u64
 	};
 
 	// These time units are defined in number of blocks.
