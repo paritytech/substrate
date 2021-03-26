b7f99df04c2ae69ab35888af9ec51fb4e0eeebba
Jimmy Chu <jimmychu0807@gmail.com>
2021-03-27 07:38:41 +0800

    update cargo version in node-tpl to v3 and spec_version
    Signed-off-by: Jimmy Chu <jimmychu0807@gmail.com>

--- bin/node-template/runtime/src/lib.rs
+++ bin/node-template/runtime/src/lib.rs
@@ -96,7 +96,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node-template"),
 	impl_name: create_runtime_str!("node-template"),
 	authoring_version: 1,
-	spec_version: 1,
+	// TODO: adding reason on setting spec_version to 100.
+	spec_version: 100,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
