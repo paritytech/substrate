52bd96ab3d1a2ee4acdf2d3b1ad599ffbee4e1b8
Jimmy Chu <jimmychu0807@gmail.com>
2021-03-27 19:30:43 +0800

    Updated pallet-template to v3 and sync back files in current devhub substrate-node-template
    Signed-off-by: Jimmy Chu <jimmychu0807@gmail.com>

---
+++ bin/node-template/scripts/docker_run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This script meant to be run on Unix/Linux based systems
+set -e
+
+echo "*** Start Substrate node template ***"
+
+cd $(dirname ${BASH_SOURCE[0]})/..
+
+docker-compose down --remove-orphans
+docker-compose run --rm --service-ports dev $@
