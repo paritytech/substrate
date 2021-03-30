f1cd0d099329e8d328a6fd7afdae70aab7dc6e3a
Alexander Popiak <alexander.popiak@parity.io>
2021-03-30 16:28:41 +0200

    add note about changing slot duration

--- bin/node-template/runtime/src/lib.rs
+++ bin/node-template/runtime/src/lib.rs
@@ -117,6 +117,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 /// Change this to adjust the block time.
 pub const MILLISECS_PER_BLOCK: u64 = 6000;
 
+// NOTE: Currently it is not possible to change the slot duration after the chain has started.
+//       Attempting to do so will brick block production.
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 
 // Time is measured by number of blocks.
