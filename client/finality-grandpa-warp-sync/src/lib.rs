fb8b68fbc54d6687008a6c9a080011f66edae5dd
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-04-02 16:40:05 +0200

    Another tweak to GrandPa warp sync (#8514)
    * Another tweak to GrandPa warp sync
    
    * Rename to WarpSyncFragment
    
    * Ensure proof is minimal

--- client/finality-grandpa-warp-sync/src/lib.rs
+++ client/finality-grandpa-warp-sync/src/lib.rs
@@ -31,7 +31,7 @@ use sc_finality_grandpa::SharedAuthoritySet;
 
 mod proof;
 
-pub use proof::{AuthoritySetChangeProof, WarpSyncProof};
+pub use proof::{WarpSyncFragment, WarpSyncProof};
 
 /// Generates the appropriate [`RequestResponseConfig`] for a given chain configuration.
 pub fn request_response_config_for_chain<TBlock: BlockT, TBackend: Backend<TBlock> + 'static>(
