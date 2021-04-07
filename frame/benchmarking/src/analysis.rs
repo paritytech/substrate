37e69235507a77456d415a557b3e8bad8e5908d3
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-04-07 21:59:40 +0200

    integrate proof_size into benchmarking pipeline

--- frame/benchmarking/src/analysis.rs
+++ frame/benchmarking/src/analysis.rs
@@ -38,6 +38,7 @@ pub enum BenchmarkSelector {
 	StorageRootTime,
 	Reads,
 	Writes,
+	ProofSize,
 }
 
 #[derive(Debug)]
@@ -86,6 +87,7 @@ impl Analysis {
 				BenchmarkSelector::StorageRootTime => result.storage_root_time,
 				BenchmarkSelector::Reads => result.reads.into(),
 				BenchmarkSelector::Writes => result.writes.into(),
+				BenchmarkSelector::ProofSize => result.proof_size.into(),
 			}
 		).collect();
 
@@ -126,6 +128,7 @@ impl Analysis {
 						BenchmarkSelector::StorageRootTime => result.storage_root_time,
 						BenchmarkSelector::Reads => result.reads.into(),
 						BenchmarkSelector::Writes => result.writes.into(),
+						BenchmarkSelector::ProofSize => result.proof_size.into(),
 					};
 					(result.components[i].1, data)
 				})
@@ -190,6 +193,7 @@ impl Analysis {
 					BenchmarkSelector::StorageRootTime => result.storage_root_time,
 					BenchmarkSelector::Reads => result.reads.into(),
 					BenchmarkSelector::Writes => result.writes.into(),
+					BenchmarkSelector::ProofSize => result.proof_size.into(),
 				})
 		}
 
--- frame/benchmarking/src/utils.rs
+++ frame/benchmarking/src/utils.rs
@@ -62,6 +62,7 @@ pub struct BenchmarkResults {
 	pub repeat_reads: u32,
 	pub writes: u32,
 	pub repeat_writes: u32,
+	pub proof_size: u32,
 }
 
 /// Configuration used to setup and run runtime benchmarks.
@@ -163,11 +164,10 @@ pub trait Benchmarking {
 		self.set_whitelist(whitelist);
 	}
 
-	/// Get current estimated proof size
+	/// Get current estimated proof size.
 	fn proof_size(&self) -> Option<u32> {
 		self.proof_size()
 	}
-
 }
 
 /// The pallet benchmarking trait.
