85d6a358bd7e9f0fa44f0e6da4ab70812767b050
huythanhcere <huythanh@cere.io>
2021-04-07 19:09:20 +0700

    feature(eth-chain-bridge): Fix comment and prepair for PR

--- frame/chainbridge/src/lib.rs
+++ frame/chainbridge/src/lib.rs
@@ -619,7 +619,6 @@ impl<T: Trait> EnsureOrigin<T::Origin> for EnsureBridge<T> {
         })
     }
 
-    #[cfg(feature = "runtime-benchmarks")]
 	fn successful_origin() -> T::Origin {
 		T::Origin::from(system::RawOrigin::Signed(Default::default()))
 	}
