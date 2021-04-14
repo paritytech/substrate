d04f074dd7a34133491507d7e3d541d10fc86f37
Andrew Jones <ascjones@gmail.com>
2021-04-14 16:04:34 +0100

    Update frame/balances/src/tests_composite.rs
    Co-authored-by: Guillaume Thiolliere <gui.thiolliere@gmail.com>

--- frame/balances/src/tests_composite.rs
+++ frame/balances/src/tests_composite.rs
@@ -60,7 +60,7 @@ impl frame_system::Config for Test {
 	type Origin = Origin;
 	type Index = u64;
 	type BlockNumber = u64;
-	type Call = CallWithDispatchInfo;
+	type Call = Call;
 	type Hash = H256;
 	type Hashing = ::sp_runtime::traits::BlakeTwo256;
 	type AccountId = u64;
