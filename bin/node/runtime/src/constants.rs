8398f9840e035be8c30c35717ae28918a963b312
Andrei Navoichyk <andrei@cere.io>
2021-04-15 14:20:41 +0300

    updated dollar

--- bin/node/runtime/src/constants.rs
+++ bin/node/runtime/src/constants.rs
@@ -21,7 +21,7 @@
 pub mod currency {
 	use node_primitives::Balance;
 
-	pub const MILLICENTS: Balance = 1_000_000_000;
+	pub const MILLICENTS: Balance = 100_000;
 	pub const CENTS: Balance = 1_000 * MILLICENTS;    // assume this is worth about a cent.
 	pub const DOLLARS: Balance = 100 * CENTS;
 
