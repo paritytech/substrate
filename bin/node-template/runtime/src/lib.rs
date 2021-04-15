2cd88cdb4e68f6cb7a7566df35a905653f740858
Andrei Navoichyk <andrei@cere.io>
2021-04-15 14:19:29 +0300

    Revert "updating block time"
    This reverts commit 3a9f6880

--- bin/node-template/runtime/src/lib.rs
+++ bin/node-template/runtime/src/lib.rs
@@ -101,7 +101,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	transaction_version: 1,
 };
 
-pub const MILLISECS_PER_BLOCK: u64 = 12000;
+pub const MILLISECS_PER_BLOCK: u64 = 6000;
 
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 
