b391959d0902e79425599a29382422a3f4d73312
Andrei Navoichyk <andrei@cere.io>
2021-04-22 15:12:38 +0300

    updated Burn permill

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -614,7 +614,7 @@ parameter_types! {
 	pub const ProposalBond: Permill = Permill::from_percent(5);
 	pub const ProposalBondMinimum: Balance = 1 * DOLLARS;
 	pub const SpendPeriod: BlockNumber = 1 * DAYS;
-	pub const Burn: Permill = Permill::from_percent(50);
+	pub const Burn: Permill = Permill::from_percent(0);
 	pub const TipCountdown: BlockNumber = 1 * DAYS;
 	pub const TipFindersFee: Percent = Percent::from_percent(20);
 	pub const TipReportDepositBase: Balance = 1 * DOLLARS;
