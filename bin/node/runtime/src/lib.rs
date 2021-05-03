0205a20b4462354db2adbc65d1ca19181057f8ec
Andrei Navoichyk <andrei@cere.io>
2021-05-03 13:30:28 +0300

    set transactions fee ratio to 50 on 50

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -135,11 +135,11 @@ pub struct DealWithFees;
 impl OnUnbalanced<NegativeImbalance> for DealWithFees {
 	fn on_unbalanceds<B>(mut fees_then_tips: impl Iterator<Item=NegativeImbalance>) {
 		if let Some(fees) = fees_then_tips.next() {
-			// for fees, 80% to treasury, 20% to author
-			let mut split = fees.ration(80, 20);
+			// for fees, 50% to treasury, 50% to author
+			let mut split = fees.ration(50, 50);
 			if let Some(tips) = fees_then_tips.next() {
-				// for tips, if any, 80% to treasury, 20% to author (though this can be anything)
-				tips.ration_merge_into(80, 20, &mut split);
+				// for tips, if any, 50% to treasury, 50% to author (though this can be anything)
+				tips.ration_merge_into(50, 50, &mut split);
 			}
 			Treasury::on_unbalanced(split.0);
 			Author::on_unbalanced(split.1);
