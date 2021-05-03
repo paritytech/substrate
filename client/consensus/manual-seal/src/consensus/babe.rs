ec180313e410915ed5e319358628260f9d1f3b53
Bastian Köcher <bkchr@users.noreply.github.com>
2021-05-03 16:39:25 +0200

    Rework inherent data client side (#8526)
    * Lol
    
    * Yeah
    
    * Moare
    
    * adaasda
    
    * Convert AURA to new pallet macro
    
    * AURA: Switch to `CurrentSlot` instead of `LastTimestamp`
    
    This switches AURA to use `CurrentSlot` instead of `LastTimestamp`.
    
    * Add missing file
    
    * Update frame/aura/src/migrations.rs
    
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>
    
    * Remove the runtime side provide inherent code
    
    * Use correct weight
    
    * Add TODO
    
    * Remove the Inherent from AURA
    
    * :facepalm:
    
    * Remove unused stuff
    
    * Update primitives authorship
    
    * Fix babe inherent data provider
    
    * Fix consensus-uncles
    
    * Fix BABE
    
    * Do some further changes to authorship primitives... :D
    
    * More work
    
    * Make it compile the happy path
    
    * Make it async!
    
    * Take hash
    
    * More stuff
    
    * Hacks
    
    * Revert "Hacks"
    
    This reverts commit cfffad88668cfdebf632a59c4fbfada001ef8251.
    
    * Fix
    
    * Make `execute_block` return the final block header
    
    * Move Aura digest stuff
    
    * Make it possible to disable equivocation checking
    
    * Fix fix fix
    
    * Some refactorings
    
    * Comment
    
    * Fixes fixes fixes
    
    * More cleanups
    
    * Some love
    
    * Better love
    
    * Make slot duration being exposed as `Duration` to the outside
    
    * Some slot info love
    
    * Add `build_aura_worker` utility function
    
    * Copy copy copy
    
    * Some stuff
    
    * Start fixing pow
    
    * Fix pow
    
    * Remove some bounds
    
    * More work
    
    * Make grandpa work
    
    * Make slots use `async_trait`
    
    * Introduce `SharedData`
    
    * Add test and fix bugs
    
    * Switch to `SharedData`
    
    * Make grandpa tests working
    
    * More Babe work
    
    * Make grandpa work
    
    * Introduce `SharedData`
    
    * Add test and fix bugs
    
    * Switch to `SharedData`
    
    * Make grandpa tests working
    
    * More Babe work
    
    * Make it async
    
    * Fix fix
    
    * Use `async_trait` in sc-consensus-slots
    
    This makes the code a little bit easier to read and also expresses that
    there can always only be one call at a time to `on_slot`.
    
    * Make grandpa tests compile
    
    * More Babe tests work
    
    * Fix network test
    
    * Start fixing service test
    
    * Finish service-test
    
    * Fix sc-consensus-aura
    
    * Fix fix fix
    
    * More fixes
    
    * Make everything compile *yeah*
    
    * Make manual-seal compile
    
    * More fixes
    
    * Start fixing Aura
    
    * Fix Aura tests
    
    * Fix Babe tests
    
    * Make everything compile
    
    * Move code around and switch to async_trait
    
    * Fix Babe
    
    * Docs docs docs
    
    * Move to FRAME
    
    * Fix fix fix
    
    * Make everything compile
    
    * Last cleanups
    
    * Fix integration test
    
    * Change slot usage of the timestamp
    
    * We really need to switch to `impl-trait-for-tuples`
    
    * Update primitives/inherents/src/lib.rs
    
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>
    
    * Update primitives/inherents/src/lib.rs
    
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>
    
    * Update primitives/inherents/src/lib.rs
    
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>
    
    * Some extra logging
    
    * Remove dbg!
    
    * Update primitives/consensus/common/src/import_queue/basic_queue.rs
    
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>
    
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>

--- client/consensus/manual-seal/src/consensus/babe.rs
+++ client/consensus/manual-seal/src/consensus/babe.rs
@@ -24,8 +24,8 @@ use codec::Encode;
 use std::{borrow::Cow, sync::{Arc, atomic}, time::SystemTime};
 use sc_client_api::AuxStore;
 use sc_consensus_babe::{
-	Config, Epoch, authorship, CompatibleDigestItem, BabeIntermediate,
-	register_babe_inherent_data_provider, INTERMEDIATE_KEY, find_pre_digest,
+	Config, Epoch, authorship, CompatibleDigestItem, BabeIntermediate, INTERMEDIATE_KEY,
+	find_pre_digest,
 };
 use sc_consensus_epochs::{SharedEpochChanges, descendent_query, ViableEpochDescriptor, EpochHeader};
 use sp_keystore::SyncCryptoStorePtr;
@@ -38,12 +38,12 @@ use sp_consensus_babe::{
 	BabeApi, inherents::BabeInherentData, ConsensusLog, BABE_ENGINE_ID, AuthorityId,
 	digests::{PreDigest, SecondaryPlainPreDigest, NextEpochDescriptor}, BabeAuthorityWeight,
 };
-use sp_inherents::{InherentDataProviders, InherentData, ProvideInherentData, InherentIdentifier};
+use sp_inherents::{InherentData, InherentDataProvider, InherentIdentifier};
 use sp_runtime::{
 	traits::{DigestItemFor, DigestFor, Block as BlockT, Zero, Header},
 	generic::{Digest, BlockId},
 };
-use sp_timestamp::{InherentType, InherentError, INHERENT_IDENTIFIER, TimestampInherentData};
+use sp_timestamp::{InherentType, INHERENT_IDENTIFIER, TimestampInherentData};
 
 /// Provides BABE-compatible predigests and BlockImportParams.
 /// Intended for use with BABE runtimes.
@@ -73,7 +73,6 @@ impl<B, C> BabeConsensusDataProvider<B, C>
 	pub fn new(
 		client: Arc<C>,
 		keystore: SyncCryptoStorePtr,
-		provider: &InherentDataProviders,
 		epoch_changes: SharedEpochChanges<B, Epoch>,
 		authorities: Vec<(AuthorityId, BabeAuthorityWeight)>,
 	) -> Result<Self, Error> {
@@ -82,10 +81,6 @@ impl<B, C> BabeConsensusDataProvider<B, C>
 		}
 
 		let config = Config::get_or_compute(&*client)?;
-		let timestamp_provider = SlotTimestampProvider::new(client.clone())?;
-
-		provider.register_provider(timestamp_provider)?;
-		register_babe_inherent_data_provider(provider, config.slot_duration())?;
 
 		Ok(Self {
 			config,
@@ -131,7 +126,8 @@ impl<B, C> ConsensusDataProvider<B> for BabeConsensusDataProvider<B, C>
 	type Transaction = TransactionFor<C, B>;
 
 	fn create_digest(&self, parent: &B::Header, inherents: &InherentData) -> Result<DigestFor<B>, Error> {
-		let slot = inherents.babe_inherent_data()?;
+		let slot = inherents.babe_inherent_data()?
+			.ok_or_else(|| Error::StringError("No babe inherent data".into()))?;
 		let epoch = self.epoch(parent, slot)?;
 
 		// this is a dev node environment, we should always be able to claim a slot.
@@ -194,7 +190,8 @@ impl<B, C> ConsensusDataProvider<B> for BabeConsensusDataProvider<B, C>
 		params: &mut BlockImportParams<B, Self::Transaction>,
 		inherents: &InherentData
 	) -> Result<(), Error> {
-		let slot = inherents.babe_inherent_data()?;
+		let slot = inherents.babe_inherent_data()?
+			.ok_or_else(|| Error::StringError("No babe inherent data".into()))?;
 		let epoch_changes = self.epoch_changes.shared_data();
 		let mut epoch_descriptor = epoch_changes
 			.epoch_descriptor_for_child_of(
@@ -216,7 +213,9 @@ impl<B, C> ConsensusDataProvider<B> for BabeConsensusDataProvider<B, C>
 
 		if !has_authority {
 			log::info!(target: "manual-seal", "authority not found");
-			let slot = *inherents.timestamp_inherent_data()? / self.config.slot_duration;
+			let timestamp = inherents.timestamp_inherent_data()?
+				.ok_or_else(|| Error::StringError("No timestamp inherent data".into()))?;
+			let slot = *timestamp / self.config.slot_duration;
 			// manually hard code epoch descriptor
 			epoch_descriptor = match epoch_descriptor {
 				ViableEpochDescriptor::Signaled(identifier, _header) => {
@@ -243,14 +242,14 @@ impl<B, C> ConsensusDataProvider<B> for BabeConsensusDataProvider<B, C>
 
 /// Provide duration since unix epoch in millisecond for timestamp inherent.
 /// Mocks the timestamp inherent to always produce the timestamp for the next babe slot.
-struct SlotTimestampProvider {
+pub struct SlotTimestampProvider {
 	time: atomic::AtomicU64,
 	slot_duration: u64
 }
 
 impl SlotTimestampProvider {
-	/// create a new mocked time stamp provider.
-	fn new<B, C>(client: Arc<C>) -> Result<Self, Error>
+	/// Create a new mocked time stamp provider.
+	pub fn new<B, C>(client: Arc<C>) -> Result<Self, Error>
 		where
 			B: BlockT,
 			C: AuxStore + HeaderBackend<B> + ProvideRuntimeApi<B>,
@@ -281,11 +280,8 @@ impl SlotTimestampProvider {
 	}
 }
 
-impl ProvideInherentData for SlotTimestampProvider {
-	fn inherent_identifier(&self) -> &'static InherentIdentifier {
-		&INHERENT_IDENTIFIER
-	}
-
+#[async_trait::async_trait]
+impl InherentDataProvider for SlotTimestampProvider {
 	fn provide_inherent_data(&self, inherent_data: &mut InherentData) -> Result<(), sp_inherents::Error> {
 		// we update the time here.
 		let duration: InherentType = self.time.fetch_add(
@@ -296,7 +292,11 @@ impl ProvideInherentData for SlotTimestampProvider {
 		Ok(())
 	}
 
-	fn error_to_string(&self, error: &[u8]) -> Option<String> {
-		InherentError::try_from(&INHERENT_IDENTIFIER, error).map(|e| format!("{:?}", e))
+	async fn try_handle_error(
+		&self,
+		_: &InherentIdentifier,
+		_: &[u8],
+	) -> Option<Result<(), sp_inherents::Error>> {
+		None
 	}
 }
