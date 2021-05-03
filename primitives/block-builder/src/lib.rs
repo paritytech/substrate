f28ca5011d083e8abfde0e08b2734c0807ca7f4d
André Silva <andrerfosilva@gmail.com>
2021-05-03 16:46:17 +0100

    primitives: remove random_seed from BlockBuilderApi

--- primitives/block-builder/src/lib.rs
+++ primitives/block-builder/src/lib.rs
@@ -19,9 +19,8 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use sp_inherents::{CheckInherentsResult, InherentData};
 use sp_runtime::{traits::Block as BlockT, ApplyExtrinsicResult};
-
-use sp_inherents::{InherentData, CheckInherentsResult};
 
 sp_api::decl_runtime_apis! {
 	/// The `BlockBuilder` api trait that provides the required functionality for building a block.
@@ -32,16 +31,17 @@ sp_api::decl_runtime_apis! {
 		/// Returns an inclusion outcome which specifies if this extrinsic is included in
 		/// this block or not.
 		fn apply_extrinsic(extrinsic: <Block as BlockT>::Extrinsic) -> ApplyExtrinsicResult;
+
 		/// Finish the current block.
 		#[renamed("finalise_block", 3)]
 		fn finalize_block() -> <Block as BlockT>::Header;
+
 		/// Generate inherent extrinsics. The inherent data will vary from chain to chain.
 		fn inherent_extrinsics(
 			inherent: InherentData,
 		) -> sp_std::vec::Vec<<Block as BlockT>::Extrinsic>;
+
 		/// Check that the inherents are valid. The inherent data will vary from chain to chain.
 		fn check_inherents(block: Block, data: InherentData) -> CheckInherentsResult;
-		/// Generate a random seed.
-		fn random_seed() -> <Block as BlockT>::Hash;
 	}
 }
