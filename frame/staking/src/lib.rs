600c1f26818c33678bc2bd9a0a828c21a934a5fb
Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
2021-05-03 04:53:09 -0300

    Remove Offence delay (#8414)
    * Removed can_report api from OnOffenceHandler
    
    * Removed DeferredOffences and create a storage migration
    
    * Removed missing comments
    
    * Mock set_deferred_offences and deferred_offences methods
    
    * OnOffenceHandler::on_offence always succeed
    
    * Fix benchmark tests
    
    * Fix runtime-benchmark cfg methods
    
    * Removed 'applied' attribute from Offence event
    
    * refactor deprecated deferred offences getter
    
    * Validate if offences are submited after on_runtime_upgrade
    
    * update changelog
    
    * Remove empty lines
    
    * Fix remove_deferred_storage weights
    
    * Remove Offence::on_runtime_upgrade benchmark
    
    * Revert CHANGELOG.md update
    
    * Deprecate DeferredOffenceOf type
    
    * Update copyright
    
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>
    
    * Add migration logs
    
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>
    
    * Fix migration log
    
    * Remove unused import
    
    * Add migration tests
    
    * rustfmt
    
    * use generate_storage_alias! macro
    
    * Refactor should_resubmit_deferred_offences test
    
    * Replace spaces by tabs
    
    * Refactor should_resubmit_deferred_offences test
    
    * Removed WeightSoftLimit
    
    * Removed WeightSoftLimit from tests and mocks
    
    * Remove unused imports
    
    * Apply suggestions from code review
    
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- frame/staking/src/lib.rs
+++ frame/staking/src/lib.rs
@@ -2770,11 +2770,7 @@ where
 		>],
 		slash_fraction: &[Perbill],
 		slash_session: SessionIndex,
-	) -> Result<Weight, ()> {
-		if !Self::can_report() {
-			return Err(());
-		}
-
+	) -> Weight {
 		let reward_proportion = SlashRewardFraction::get();
 		let mut consumed_weight: Weight = 0;
 		let mut add_db_reads_writes = |reads, writes| {
@@ -2786,7 +2782,7 @@ where
 			add_db_reads_writes(1, 0);
 			if active_era.is_none() {
 				// this offence need not be re-submitted.
-				return Ok(consumed_weight)
+				return consumed_weight
 			}
 			active_era.expect("value checked not to be `None`; qed").index
 		};
@@ -2811,7 +2807,7 @@ where
 			match eras.iter().rev().filter(|&&(_, ref sesh)| sesh <= &slash_session).next() {
 				Some(&(ref slash_era, _)) => *slash_era,
 				// before bonding period. defensive - should be filtered out.
-				None => return Ok(consumed_weight),
+				None => return consumed_weight,
 			}
 		};
 
@@ -2879,12 +2875,7 @@ where
 			}
 		}
 
-		Ok(consumed_weight)
-	}
-
-	fn can_report() -> bool {
-		// TODO: https://github.com/paritytech/substrate/issues/8343
-		true
+		consumed_weight
 	}
 }
 
