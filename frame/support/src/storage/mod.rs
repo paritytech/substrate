c6c01cc1f0eb85a008d20beb70c19d19d1fca398
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-05-06 16:19:04 +0200

    Add `BoundedBTreeSet`
    Part of https://github.com/paritytech/substrate/issues/8719

--- frame/support/src/storage/mod.rs
+++ frame/support/src/storage/mod.rs
@@ -29,6 +29,7 @@ pub use sp_runtime::TransactionOutcome;
 
 pub mod unhashed;
 pub mod hashed;
+pub mod bounded_btree_set;
 pub mod bounded_vec;
 pub mod child;
 #[doc(hidden)]
@@ -817,6 +818,7 @@ mod private {
 	impl<T: Encode> Sealed for Vec<T> {}
 	impl<Hash: Encode> Sealed for Digest<Hash> {}
 	impl<T: BoundedVecValue, S: Get<u32>> Sealed for BoundedVec<T, S> {}
+	impl<T, S> Sealed for bounded_btree_set::BoundedBTreeSet<T, S> {}
 }
 
 impl<T: Encode> StorageAppend<T> for Vec<T> {}
