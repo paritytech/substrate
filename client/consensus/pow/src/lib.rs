441b08ef89e3e0621a8b2677517572672c85aff0
Wei Tang <wei@that.world>
2021-05-07 23:32:32 +0200

    pow: fix docs on mining worker

--- client/consensus/pow/src/lib.rs
+++ client/consensus/pow/src/lib.rs
@@ -21,7 +21,15 @@
 //! To use this engine, you can need to have a struct that implements
 //! `PowAlgorithm`. After that, pass an instance of the struct, along
 //! with other necessary client references to `import_queue` to setup
-//! the queue. Use the `start_mine` function for basic CPU mining.
+//! the queue.
+//!
+//! This library also comes with an async miner worker, which can be
+//! started via the `start_miner_worker` function. It returns a worker
+//! handle together with a future. The future must be pulled. Through
+//! the worker handle, you can pull the metadata needed to start the
+//! mining process via `MiningWorker::metadata`, and then do the actual
+//! mining on a standalone thread. Finally, when a seal is found, call
+//! `MiningWorker::submit` to build the block.
 //!
 //! The auxiliary storage for PoW engine only stores the total difficulty.
 //! For other storage requirements for particular PoW algorithm (such as
