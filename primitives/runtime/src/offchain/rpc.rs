2f59d4d524b8577e6b391025b4b2ed85e5ef872e
Nacho Pal <ig.pal.san@gmail.com>
2021-05-08 19:23:41 +0800

    offchain rpc call added

--- primitives/runtime/src/offchain/rpc.rs
+++ primitives/runtime/src/offchain/rpc.rs
@@ -17,54 +17,96 @@
 
 //! A high-level helpers for making RPC calls from Offchain Workers.
 //!
-//! `sp-io` crate exposes a low level methods to make and control RPC requests
-//! available only for Offchain Workers. Those might be hard to use
-//! and usually that level of control is not really necessary.
+//! Example:
+//! ```rust,no_run
+//! fn offchain_worker(block_number: T::BlockNumber) {
+//! 	let request = rpc::Request {
+//! 		jsonrpc: JSONRPC,
+//! 		id: 1,
+//! 		method: "chain_getFinalizedHead",
+//! 		params: Vec::new(),
+//! 		timeout: TIMEOUT_PERIOD,
+//! 		url: RPC_REQUEST_URL
+//! 	};
+//!
+//! 	let rpc_response = request.send();
+//!
+//! 	match rpc_response {
+//! 		Ok(response) => {
+//! 			if !response.result.is_empty() {
+//! 				log::info!("Rpc call result: {:?}", str::from_utf8(&response.result).unwrap());
+//! 			} else {
+//! 				log::error!("Rpc call error: code: {:?}, message: {:?}",
+//! 								response.error.code,
+//! 								str::from_utf8(&response.error.message).unwrap()
+//! 							);
+//! 			}
+//! 		},
+//! 		Err(e) => {
+//! 			log::error!("Rpc call error: {:?}", e);
+//! 		}
+//! 	}
+//! }
+//! ```
 
-// use frame_support::{debug};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use serde::{Deserialize, Deserializer};
 use crate::{RuntimeDebug, offchain::Duration};
 use serde_json::json;
 use sp_std::{vec::Vec, str};
-// use sp_core::RuntimeDebug;
-// use crate::offchain::Duration;
 use super::http;
 
 /// A rpc call error
 #[derive(Clone, PartialEq, Eq, RuntimeDebug)]
 pub enum Error {
+	/// Response can not be deserialized
 	Deserializing,
+	/// Http error
 	Http(http::Error)
 }
 
+/// A rpc result type
 pub type RpcResult = Result<Response, Error>;
 
+/// A rpc call error field
 #[derive(Deserialize, Default, RuntimeDebug)]
 pub struct RpcError {
+	/// error code
 	pub code: i32,
+	/// error message
 	#[serde(deserialize_with = "de_string_to_bytes")]
 	pub message: Vec<u8>
 }
 
+/// A rpc call response
 #[derive(Deserialize, Default, RuntimeDebug)]
 pub struct Response {
+	/// rpc response jsonrpc
 	#[serde(deserialize_with = "de_string_to_bytes")]
 	pub jsonrpc: Vec<u8>,
+	/// rpc response result
 	#[serde(default)]
 	#[serde(deserialize_with = "de_string_to_bytes")]
 	pub result: Vec<u8>,
+	/// rpc response error struct
 	#[serde(default)]
-	// #[serde(deserialize_with = "de_string_to_bytes")]
 	pub error: RpcError,
+	/// rpc response id
 	pub id: u32
 }
 
+/// A rpc call request
 pub struct Request<'a> {
+	/// rpc request jsonrpc
 	pub jsonrpc: &'a str,
+	/// rpc request id
 	pub id: u32,
+	/// rpc request method
 	pub method: &'a str,
+	/// rpc request params
 	pub params: Vec<&'a str>,
+	/// rpc request timeout
 	pub timeout: u64,
+	/// rpc request url
 	pub url: &'a str
 }
 
@@ -77,6 +119,10 @@ where
 }
 
 impl<'a> Request<'a> {
+	/// Send the request and return a Rpc result.
+	///
+	/// Err is returned in case there is a Http
+	/// or deserialize error.
 	pub fn send(&self) -> RpcResult {
 		let request_body = json!({
 			"jsonrpc": self.jsonrpc,
@@ -85,22 +131,13 @@ impl<'a> Request<'a> {
 			"params": self.params
 		});
 
-		// debug::info!("========================= BODY CONSTRUCT ==================== {:?}", serde_json::to_string(&request_body).unwrap());
-
 		let mut body: Vec<&[u8]> = Vec::new();
-		// let request_body_vec: Vec<u8> = serde_json::to_vec(&request_body).unwrap();
 		let request_body_slice: &[u8] = &(serde_json::to_vec(&request_body).unwrap())[..];
-		// let request_body_slice: &[u8] = &v[..];
 		body.push(request_body_slice);
 
 		let post_request = http::Request::post(self.url, body);
 
 		let timeout = sp_io::offchain::timestamp().add(Duration::from_millis(self.timeout));
-
-		// let pending = post_request
-		// 	.add_header("Content-Type", "application/json;charset=utf-8")
-		// 	.deadline(timeout)
-		// 	.send().map_err(|_| http::Error::IoError);
 
 		let pending = match post_request
 			.add_header("Content-Type", "application/json;charset=utf-8")
@@ -109,9 +146,6 @@ impl<'a> Request<'a> {
 				Ok(result) => result,
 				Err(e) => return Err(Error::Http(e)),
 		};
-
-		// let response = pending.try_wait(timeout)
-		// 	.map_err(|_| http::Error::DeadlineReached);
 
 		let response = match pending.try_wait(timeout).map_err(|_| http::Error::DeadlineReached) {
 			Ok(result) =>
@@ -122,23 +156,13 @@ impl<'a> Request<'a> {
 			Err(e) => return Err(Error::Http(e)),
 		};
 
-		// let b = match response {
-		// 	Err(e) => return Err(Error::Http(e)),
-		// };
-
 		if response.code != 200 {
 			log::warn!("Unexpected status code: {}", response.code);
 			return Err(Error::Http(http::Error::Unknown));
 		}
 
 		let response_body_bytes = response.body().collect::<Vec<u8>>();
-
-		let response_body_str = str::from_utf8(&response_body_bytes).map_err(|_| Error::Deserializing)?;
-
-		log::info!("=================== RESPONSE RAW ================== {:?}", response_body_str);
-
-
-		let rpc_response: Response = serde_json::from_str(&response_body_str).map_err(|_| Error::Deserializing)?;
+		let rpc_response: Response = serde_json::from_slice(&response_body_bytes).map_err(|_| Error::Deserializing)?;
 
 		Ok(rpc_response)
 	}
