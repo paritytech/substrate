1bb2fb0f2679903527c0abce62bd73ece6737c2c
Stanly Johnson <stanlyjohnson@outlook.com>
2021-05-17 15:55:45 +0530

    fix metadata

--- frame/scheduler/src/lib.rs
+++ frame/scheduler/src/lib.rs
@@ -185,7 +185,7 @@ pub mod pallet {
 	/// Events type.
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	#[pallet::metadata(T::BlockNumber = "BlockNumber")]
+	#[pallet::metadata(T::BlockNumber = "BlockNumber", TaskAddress<T::BlockNumber> = "TaskAddress<BlockNumber>")]
 	pub enum Event<T: Config> {
 		/// Scheduled some task. \[when, index\]
 		Scheduled(T::BlockNumber, u32),
