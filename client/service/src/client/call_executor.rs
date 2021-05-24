9e8ce24352710a229de24642d8fdaa9cf78fa305
Bastian Köcher <info@kchr.de>
2021-05-25 00:28:15 +0200

    Introduce `code_substitute`
    This introduces a new field `code_substitute` into the chain spec. This
    can be used to substitute the on-chain wasm starting from a given block
    until there is another wasm on chain (determined through the
    `spec_version`). This can be used to fix broken on chain wasm runtimes.

--- client/service/src/client/call_executor.rs
+++ client/service/src/client/call_executor.rs
@@ -32,33 +32,41 @@ use sp_core::{
 };
 use sp_api::{ProofRecorder, InitializeBlock, StorageTransactionCache};
 use sc_client_api::{backend, call_executor::CallExecutor};
-use super::{client::ClientConfig, wasm_override::WasmOverride};
+use super::{client::ClientConfig, wasm_override::WasmOverride, wasm_substitutes::WasmSubstitutes};
 
 /// Call executor that executes methods locally, querying all required
 /// data from local backend.
-pub struct LocalCallExecutor<B, E> {
+pub struct LocalCallExecutor<Block: BlockT, B, E> {
 	backend: Arc<B>,
 	executor: E,
 	wasm_override: Option<WasmOverride<E>>,
+	wasm_substitutes: WasmSubstitutes<Block, E, B>,
 	spawn_handle: Box<dyn SpawnNamed>,
-	client_config: ClientConfig,
+	client_config: ClientConfig<Block>,
 }
 
-impl<B, E> LocalCallExecutor<B, E>
+impl<Block: BlockT, B, E> LocalCallExecutor<Block, B, E>
 where
-	E: CodeExecutor + RuntimeInfo + Clone + 'static
+	E: CodeExecutor + RuntimeInfo + Clone + 'static,
+	B: backend::Backend<Block>,
 {
 	/// Creates new instance of local call executor.
 	pub fn new(
 		backend: Arc<B>,
 		executor: E,
 		spawn_handle: Box<dyn SpawnNamed>,
-		client_config: ClientConfig,
+		client_config: ClientConfig<Block>,
 	) -> sp_blockchain::Result<Self> {
 		let wasm_override = client_config.wasm_runtime_overrides
 			.as_ref()
 			.map(|p| WasmOverride::new(p.clone(), executor.clone()))
 			.transpose()?;
+
+		let wasm_substitutes = WasmSubstitutes::new(
+			client_config.wasm_runtime_substitutes.clone(),
+			executor.clone(),
+			backend.clone(),
+		)?;
 
 		Ok(LocalCallExecutor {
 			backend,
@@ -66,13 +74,14 @@ where
 			wasm_override,
 			spawn_handle,
 			client_config,
+			wasm_substitutes,
 		})
 	}
 
 	/// Check if local runtime code overrides are enabled and one is available
 	/// for the given `BlockId`. If yes, return it; otherwise return the same
 	/// `RuntimeCode` instance that was passed.
-	fn check_override<'a, Block>(
+	fn check_override<'a>(
 		&'a self,
 		onchain_code: RuntimeCode<'a>,
 		id: &BlockId<Block>,
@@ -81,16 +90,16 @@ where
 		Block: BlockT,
 		B: backend::Backend<Block>,
 	{
+		let spec = self.runtime_version(id)?.spec_version;
 		let code = if let Some(d) = self.wasm_override
 			.as_ref()
-			.map::<sp_blockchain::Result<Option<RuntimeCode>>, _>(|o| {
-				let spec = self.runtime_version(id)?.spec_version;
-				Ok(o.get(&spec, onchain_code.heap_pages))
-			})
-			.transpose()?
+			.map(|o| o.get(&spec, onchain_code.heap_pages))
 			.flatten() {
 			log::debug!(target: "wasm_overrides", "using WASM override for block {}", id);
 			d
+		} else if let Some(s) = self.wasm_substitutes.get(spec, onchain_code.heap_pages, id) {
+			log::debug!(target: "wasm_substitutes", "Using WASM substitute for block {:?}", id);
+			s
 		} else {
 			log::debug!(
 				target: "wasm_overrides",
@@ -104,7 +113,7 @@ where
 	}
 }
 
-impl<B, E> Clone for LocalCallExecutor<B, E> where E: Clone {
+impl<Block: BlockT, B, E> Clone for LocalCallExecutor<Block, B, E> where E: Clone {
 	fn clone(&self) -> Self {
 		LocalCallExecutor {
 			backend: self.backend.clone(),
@@ -112,11 +121,12 @@ impl<B, E> Clone for LocalCallExecutor<B, E> where E: Clone {
 			wasm_override: self.wasm_override.clone(),
 			spawn_handle: self.spawn_handle.clone(),
 			client_config: self.client_config.clone(),
+			wasm_substitutes: self.wasm_substitutes.clone(),
 		}
 	}
 }
 
-impl<B, E, Block> CallExecutor<Block> for LocalCallExecutor<B, E>
+impl<B, E, Block> CallExecutor<Block> for LocalCallExecutor<Block, B, E>
 where
 	B: backend::Backend<Block>,
 	E: CodeExecutor + RuntimeInfo + Clone + 'static,
@@ -314,7 +324,7 @@ where
 	}
 }
 
-impl<B, E, Block> sp_version::GetRuntimeVersion<Block> for LocalCallExecutor<B, E>
+impl<Block, B, E> sp_version::GetRuntimeVersion<Block> for LocalCallExecutor<Block, B, E>
 	where
 		B: backend::Backend<Block>,
 		E: CodeExecutor + RuntimeInfo + Clone + 'static,
@@ -357,11 +367,7 @@ mod tests {
 
 		// wasm_runtime_overrides is `None` here because we construct the
 		// LocalCallExecutor directly later on
-		let client_config = ClientConfig {
-			offchain_worker_enabled: false,
-			offchain_indexing_api: false,
-			wasm_runtime_overrides: None,
-		};
+		let client_config = ClientConfig::default();
 
 		// client is used for the convenience of creating and inserting the genesis block.
 		let _client = substrate_test_runtime_client::client::new_with_backend::<
@@ -383,10 +389,15 @@ mod tests {
 
 		let call_executor = LocalCallExecutor {
 			backend: backend.clone(),
-			executor,
+			executor: executor.clone(),
 			wasm_override: Some(overrides),
 			spawn_handle: Box::new(TaskExecutor::new()),
 			client_config,
+			wasm_substitutes: WasmSubstitutes::new(
+				Default::default(),
+				executor.clone(),
+				backend.clone(),
+			).unwrap(),
 		};
 
 		let check = call_executor.check_override(onchain_code, &BlockId::Number(Default::default()))
