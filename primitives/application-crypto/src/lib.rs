73623e9933d50648e0e7fe90b6171a8e45d7f5a2
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-05-25 17:36:36 -0400

    remove app crypto for now

--- primitives/application-crypto/src/lib.rs
+++ primitives/application-crypto/src/lib.rs
@@ -200,7 +200,6 @@ macro_rules! app_crypto_public_full_crypto {
 				$crate::codec::Encode,
 				$crate::codec::Decode,
 				$crate::RuntimeDebug,
-				$crate::max_encoded_len::MaxEncodedLen,
 			)]
 			#[derive(Hash)]
 			pub struct Public($public);
