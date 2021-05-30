fb6f88af86f105aaefd6ed0b0ea3cc85390b9923
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-05-30 05:18:53 -0400

    update mock

--- frame/babe/src/mock.rs
+++ frame/babe/src/mock.rs
@@ -151,10 +151,11 @@ impl pallet_timestamp::Config for Test {
 
 parameter_types! {
 	pub const ExistentialDeposit: u128 = 1;
+	pub const MaxLocks: u32 = 10;
 }
 
 impl pallet_balances::Config for Test {
-	type MaxLocks = ();
+	type MaxLocks = MaxLocks;
 	type Balance = u128;
 	type DustRemoval = ();
 	type Event = Event;
