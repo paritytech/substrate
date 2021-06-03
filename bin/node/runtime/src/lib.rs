0c059ee1d8bfc36a68c449b230aed490e7cb7e7b
arkpar <arkady.paronyan@gmail.com>
2021-06-03 17:13:45 +0200

    Fixed node runtime build

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -1118,6 +1118,7 @@ impl pallet_transaction_storage::Config for Runtime {
 	type Event = Event;
 	type Currency = Balances;
 	type Call = Call;
+	type FeeDestination = ();
 	type WeightInfo = pallet_transaction_storage::weights::SubstrateWeight<Runtime>;
 }
 
