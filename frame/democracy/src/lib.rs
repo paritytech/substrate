23231d12ccc186807c3f913240e04116313f2e54
Andrew Jones <ascjones@gmail.com>
2021-06-10 16:23:06 +0100

    Remove metadata attributes

--- frame/democracy/src/lib.rs
+++ frame/democracy/src/lib.rs
@@ -488,13 +488,6 @@ pub mod pallet {
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	#[pallet::metadata(
-		T::AccountId = "AccountId",
-		Vec<T::AccountId> = "Vec<AccountId>",
-		BalanceOf<T> = "Balance",
-		T::BlockNumber = "BlockNumber",
-		T::Hash = "Hash",
-	)]
 	pub enum Event<T: Config> {
 		/// A motion has been proposed by a public account. \[proposal_index, deposit\]
 		Proposed(PropIndex, BalanceOf<T>),
