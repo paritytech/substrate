77158e9a1d35a2f9e7e7b1707440c976bff9f47d
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-06-12 00:48:51 +0100

    Update lib.rs

--- frame/staking/src/lib.rs
+++ frame/staking/src/lib.rs
@@ -1320,12 +1320,16 @@ pub mod pallet {
 		}
 
 		fn integrity_test() {
-			assert!(
-				T::SlashDeferDuration::get() < T::BondingDuration::get() || T::BondingDuration::get() == 0,
-				"As per documentation, slash defer duration ({}) should be less than bonding duration ({}).",
-				T::SlashDeferDuration::get(),
-				T::BondingDuration::get(),
-			)
+			sp_std::if_std! {
+				sp_io::TestExternalities::new_empty().execute_with(||
+					assert!(
+						T::SlashDeferDuration::get() < T::BondingDuration::get() || T::BondingDuration::get() == 0,
+						"As per documentation, slash defer duration ({}) should be less than bonding duration ({}).",
+						T::SlashDeferDuration::get(),
+						T::BondingDuration::get(),
+					)
+				);
+			}
 		}
 	}
 
