c3e9fcf11c043c245d938d65b34f02152f7a3caf
Guillaume Thiolliere <gui.thiolliere@gmail.com>
2021-06-16 11:09:24 +0200

    Do not run pallet_ui test with conditional-storage feature (#9122)
    * do not run pallet_ui test with conditional-compilation feature
    
    * fix

--- frame/support/test/tests/pallet_ui/storage_info_unsatisfied_nmap.rs
+++ frame/support/test/tests/pallet_ui/storage_info_unsatisfied_nmap.rs
@@ -1,28 +1,27 @@
-// #[frame_support::pallet]
-// mod pallet {
-// 	use frame_support::pallet_prelude::{Hooks, StorageNMap, Twox64Concat, NMapKey};
-// 	use frame_system::pallet_prelude::BlockNumberFor;
+#[frame_support::pallet]
+mod pallet {
+	use frame_support::pallet_prelude::{Hooks, StorageNMap, Twox64Concat, NMapKey};
+	use frame_system::pallet_prelude::BlockNumberFor;
 
-// 	#[pallet::config]
-// 	pub trait Config: frame_system::Config {}
+	#[pallet::config]
+	pub trait Config: frame_system::Config {}
 
-// 	#[pallet::pallet]
-// 	#[pallet::generate_storage_info]
-// 	pub struct Pallet<T>(core::marker::PhantomData<T>);
+	#[pallet::pallet]
+	#[pallet::generate_storage_info]
+	pub struct Pallet<T>(core::marker::PhantomData<T>);
 
-// 	#[pallet::hooks]
-// 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-// 	#[pallet::call]
-// 	impl<T: Config> Pallet<T> {}
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {}
 
-// 	#[derive(codec::Encode, codec::Decode)]
-// 	struct Bar;
+	#[derive(codec::Encode, codec::Decode)]
+	struct Bar;
 
-// 	#[pallet::storage]
-// 	type Foo<T> = StorageNMap<_, NMapKey<Twox64Concat, Bar>, u32>;
-// }
+	#[pallet::storage]
+	type Foo<T> = StorageNMap<_, NMapKey<Twox64Concat, Bar>, u32>;
+}
 
 fn main() {
-	compile_error!("Temporarily disabled due to test flakiness");
 }
