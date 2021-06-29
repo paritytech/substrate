f1e7fe7add9b8cc27007975c9e1efe3893cec545
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-06-29 15:01:40 +0200

    start sketching out adjustable thresholds

--- frame/staking/src/lib.rs
+++ frame/staking/src/lib.rs
@@ -960,6 +960,59 @@ pub mod pallet {
 
 		/// Weight information for extrinsics in this pallet.
 		type WeightInfo: WeightInfo;
+
+		/// The list of thresholds separating the various voter bags.
+		///
+		/// Voters are separated into unsorted bags according to their vote weight. This specifies
+		/// the thresholds separating the bags. A voter's bag is the largest bag for which the
+		/// voter's weight is less than or equal to its upper threshold.
+		///
+		/// When voters are iterated, higher bags are iterated completely before lower bags. This
+		/// that iteration is _semi-sorted_: voters of higher weight tend to come before voters of
+		/// lower weight, but peer voters within a particular bag are sorted in insertion order.
+		///
+		/// # Expressing the constant
+		///
+		/// This constant must be sorted in strictly increasing order. Duplicate items are not
+		/// permitted.
+		///
+		/// There is an implied upper limit of `VoteWeight::MAX`; that value does not need to be
+		/// specified within the bag. For any two threshold lists, if one ends with
+		/// `VoteWeight::MAX`, the other one does not, and they are otherwise equal, the two lists
+		/// will behave identically.
+		///
+		/// # Calculation
+		///
+		/// It is recommended to generate the set of thresholds in a geometric series, such that
+		/// there exists some constant ratio such that `threshold[k + 1] == (threshold[k] *
+		/// constant_ratio).max(threshold[k] + 1)` for all `k`.
+		///
+		/// Given the desire to compute `N` bags, the constant ratio can be computed with
+		/// `exp(ln(VoterBags::MAX) / N)`.
+		///
+		/// # Examples
+		///
+		/// - If `VoterBagThresholds::get().is_empty()`, then all voters are put into the same bag,
+		///   and iteration is strictly in insertion order.
+		/// - If `VoterBagThresholds::get().len() == 64`, and the thresholds are determined
+		///   according to the procedure given above, then the constant ratio is equal to 2.
+		/// - If `VoterBagThresholds::get().len() == 200`, and the thresholds are determined
+		///   according to the procedure given above, then the constant ratio is approximately equal
+		///   to 1.248.
+		/// - If the threshold list begins `[1, 2, 3, ...]`, then a voter with weight 0 or 1 will
+		///   fall into bag 0, a voter with weight 2 will fall into bag 1, etc.
+		///
+		/// # Migration
+		///
+		/// In the event that this list ever changes, the `VoterList` data structure will need to be
+		/// regenerated.
+		#[pallet::constant]
+		type VoterBagThresholds: Get<&'static [VoteWeight]>;
+
+		/// A type sufficient to distinguish between all voter bags.
+		///
+		/// For 256 bags or fewer, `u8` suffices.
+		type BagIdx: Copy + Eq + Ord + codec::Codec + MaxEncodedLen;
 	}
 
 	#[pallet::extra_constants]
