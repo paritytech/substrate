cb598671aa6d55f8013223c315c8a8893b24da3f
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-07-02 11:36:29 +0200

    WIP: implement make_ratio macro

--- frame/staking/bag-thresholds/src/lib.rs
+++ frame/staking/bag-thresholds/src/lib.rs
@@ -17,6 +17,11 @@
 
 //! Proc macros to generate voter bag thresholds and associated items.
 
+use proc_macro::TokenStream;
+
+mod common;
+mod make_ratio_impl;
+
 /// Calculate an appropriate constant ratio between thresholds.
 ///
 /// This macro can be thought of as a function with signature
@@ -25,12 +30,13 @@
 /// pub const fn make_ratio(n: usize, VoteWeight: Type, existential_weight: VoteWeight) -> f64;
 /// ```
 ///
-/// - The argument `n` is how many divisions we're partitioning `VoteWeight` into.
+/// - The argument `n` is how many divisions we're partitioning `VoteWeight` into. It may be any
+///   expression which is evaluable in a const context.
 /// - `VoteWeight` is the type of the vote weight, and should typically be a typedef. It must have a
 ///   `::MAX` attribute available.
 /// - `existential_weight` is the weight below which it's not worth examining a voter. Typically,
 ///   this will be the result of some calculation involving the existential deposit for a chain's
-///   balance type.
+///   balance type. It may be any expression which is evaluable in a const context.
 ///
 /// # Example:
 ///
@@ -44,8 +50,8 @@
 ///
 /// The constant ratio is calculated per `exp(ln(VoteWeight::MAX - existential_weight) / n).
 #[proc_macro]
-pub fn make_ratio(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	todo!()
+pub fn make_ratio(input: TokenStream) -> TokenStream {
+	make_ratio_impl::make_ratio(input)
 }
 
 /// Make a constant array of threshold values suitable for use as voter bag thresholds.
@@ -56,12 +62,13 @@ pub fn make_ratio(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 /// pub const fn make_thresholds(n: usize, VoteWeight: Type, existential_weight: VoteWeight) -> [VoteWeight; n];
 /// ```
 ///
-/// - The argument `n` is how many divisions we're partitioning `VoteWeight` into.
+/// - The argument `n` is how many divisions we're partitioning `VoteWeight` into. It may be any
+///   expression which is evaluable in a const context.
 /// - `VoteWeight` is the type of the vote weight, and should typically be a typedef. It must have a
 ///   `::MAX` attribute available.
 /// - `existential_weight` is the weight below which it's not worth examining a voter. Typically,
 ///   this will be the result of some calculation involving the existential deposit for a chain's
-///   balance type.
+///   balance type. It may be any expression which is evaluable in a const context.
 ///
 /// The output has these properties:
 ///
@@ -86,7 +93,7 @@ pub fn make_ratio(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 /// const THRESHOLDS: [u64; 200] = &make_thresholds!(200, u64, 0);
 /// ```
 #[proc_macro]
-pub fn make_thresholds(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn make_thresholds(input: TokenStream) -> TokenStream {
 	todo!()
 }
 
@@ -110,6 +117,11 @@ pub fn make_thresholds(input: proc_macro::TokenStream) -> proc_macro::TokenStrea
 /// It is intended to be used to simply edit a thresholds list, inserting some items and removing
 /// others, without needing to express the entire list.
 ///
+/// Note that due to macro limitations, `inserting` and `removing` must be array literals.
+///
+/// Note that `removing` overrides `inserting`: if a value appears in both lists, it does not appear
+/// in the output.
+///
 /// # Example:
 ///
 /// ```
@@ -117,6 +129,6 @@ pub fn make_thresholds(input: proc_macro::TokenStream) -> proc_macro::TokenStrea
 /// const THRESHOLDS: &[u64] = &edit_thresholds!(make_thresholds!(200, u64, 0), [12345, 54321], []);
 /// ```
 #[proc_macro]
-pub fn edit_thresholds(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn edit_thresholds(input: TokenStream) -> TokenStream {
 	todo!()
 }
