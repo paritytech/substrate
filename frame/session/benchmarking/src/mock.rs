b73fd7f2c46d4699739467b8c7b5d843e50b3f6e
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-07-05 14:56:12 +0200

    add VoterBagThresholds to some configs

--- frame/session/benchmarking/src/mock.rs
+++ frame/session/benchmarking/src/mock.rs
@@ -185,6 +185,7 @@ impl pallet_staking::Config for Test {
 	type ElectionProvider = onchain::OnChainSequentialPhragmen<Self>;
 	type GenesisElectionProvider = Self::ElectionProvider;
 	type WeightInfo = ();
+	type VoterBagThresholds = ();
 }
 
 impl crate::Config for Test {}
