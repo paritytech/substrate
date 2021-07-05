586eefc02305775341c3fb292abce3bcc4d96052
Leonardo Custodio <leonardo@custodio.me>
2021-07-05 10:30:31 -0300

    Typo

--- frame/election-provider-multi-phase/src/lib.rs
+++ frame/election-provider-multi-phase/src/lib.rs
@@ -897,7 +897,7 @@ pub mod pallet {
 		#[pallet::weight(T::DbWeight::get().reads_writes(1, 1))]
 		pub fn set_emergency_election_result(
 			origin: OriginFor<T>,
-			supports: Supports<T::AcountId>,
+			supports: Supports<T::AccountId>,
 		) -> DispatchResult {
 			T::ForceOrigin::ensure_origin(origin)?;
 			ensure!(Self::current_phase().is_emergency(), <Error<T>>::CallNotAllowed);
