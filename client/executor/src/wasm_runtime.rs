2eca696dafe79fb0a709b34f06d54b0a85c28c3a
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2021-07-08 13:57:23 +0200

    Update client/executor/src/wasm_runtime.rs

--- client/executor/src/wasm_runtime.rs
+++ client/executor/src/wasm_runtime.rs
@@ -223,13 +223,6 @@ impl RuntimeCache {
 		let code_hash = &runtime_code.hash;
 		let heap_pages = runtime_code.heap_pages.unwrap_or(default_heap_pages);
 
-		log::trace!(
-			target: "wasm-runtime",
-			"with_instance: using {} heap pages in {:?} context",
-			heap_pages,
-			runtime_code.context,
-		);
-
 		let mut runtimes = self.runtimes.lock(); // this must be released prior to calling f
 		let pos = runtimes.iter().position(|r| r.as_ref().map_or(
 			false,
