e3e99461da2133d03da9666e21f0ca433a53ffc8
Keith Yeung <kungfukeith11@gmail.com>
2021-07-17 21:51:24 -0700

    Fix unresolved crate

--- frame/society/src/lib.rs
+++ frame/society/src/lib.rs
@@ -1222,7 +1222,7 @@ impl<T: Config> EnsureOrigin<T::Origin> for EnsureFounder<T> {
 	#[cfg(feature = "runtime-benchmarks")]
 	fn successful_origin() -> T::Origin {
 		let founder = Founder::<T>::get().expect("society founder should exist");
-		T::Origin::from(system::RawOrigin::Signed(founder))
+		T::Origin::from(frame_system::RawOrigin::Signed(founder))
 	}
 }
 
