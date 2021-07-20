f31dff7f1885e04e5b166175f95ddd786c952f43
Alexander Popiak <alexander.popiak@parity.io>
2021-07-20 16:55:54 +0200

    Balance to Asset Balance Conversion (#9076)
    * add BalanceConversion trait and implementation
    
    * derive some useful traits on Imbalance
    
    * Update frame/support/src/traits/tokens/fungibles/imbalance.rs
    
    Co-authored-by: Xiliang Chen <xlchen1291@gmail.com>
    
    * make BalanceConversion error type configurable
    
    * add RuntimeDebug import and derive traits on other Imbalance
    
    * formatting
    
    * move BalanceConversion trait to frame-support
    
    * add necessary trait import
    
    Co-authored-by: Xiliang Chen <xlchen1291@gmail.com>

--- frame/support/src/traits/tokens/fungibles/imbalance.rs
+++ frame/support/src/traits/tokens/fungibles/imbalance.rs
@@ -20,7 +20,7 @@
 
 use super::*;
 use sp_std::marker::PhantomData;
-use sp_runtime::traits::Zero;
+use sp_runtime::{RuntimeDebug, traits::Zero};
 use super::fungibles::{AssetId, Balance};
 use super::balanced::Balanced;
 use crate::traits::misc::{TryDrop, SameOrOther};
@@ -37,6 +37,7 @@ pub trait HandleImbalanceDrop<AssetId, Balance> {
 ///
 /// Importantly, it has a special `Drop` impl, and cannot be created outside of this module.
 #[must_use]
+#[derive(RuntimeDebug, Eq, PartialEq)]
 pub struct Imbalance<
 	A: AssetId,
 	B: Balance,
