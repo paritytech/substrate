865e37c22d44998d8852aeec38f53cddc7bbfb9c
Falco Hirschenberger <falco.hirschenberger@gmail.com>
2021-07-23 13:54:18 +0200

    Don't return misleading result with PostInfo in proxy-pallet
    also autoformatting two unrelated files
    
    fixes #9421

--- frame/staking/src/lib.rs
+++ frame/staking/src/lib.rs
@@ -2365,8 +2365,7 @@ impl<T: Config> Pallet<T> {
 		})?;
 
 		let controller = Self::bonded(&validator_stash).ok_or_else(|| {
-			Error::<T>::NotStash
-				.with_weight(T::WeightInfo::payout_stakers_alive_staked(0))
+			Error::<T>::NotStash.with_weight(T::WeightInfo::payout_stakers_alive_staked(0))
 		})?;
 		let mut ledger = <Ledger<T>>::get(&controller).ok_or(Error::<T>::NotController)?;
 
