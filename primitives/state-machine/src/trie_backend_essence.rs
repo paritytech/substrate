40185b6f5d9d823e0d08b95df66a0cfd8475f4bd
cheme <emericchevalier.pro@gmail.com>
2021-07-23 09:13:17 +0200

    correct rust fmt

--- primitives/state-machine/src/trie_backend_essence.rs
+++ primitives/state-machine/src/trie_backend_essence.rs
@@ -133,7 +133,7 @@ where
 		#[cfg(feature = "std")]
 		{
 			if let Some(result) = self.cache.read().child_root.get(child_info.storage_key()) {
-				return Ok(result.clone());
+				return Ok(result.clone())
 			}
 		}
 
@@ -165,7 +165,7 @@ where
 		let mut hash = H::Out::default();
 
 		if child_root.len() != hash.as_ref().len() {
-			return Err(format!("Invalid child storage hash at {:?}", child_info.storage_key()));
+			return Err(format!("Invalid child storage hash at {:?}", child_info.storage_key()))
 		}
 		// note: child_root and hash must be same size, panics otherwise.
 		hash.as_mut().copy_from_slice(&child_root[..]);
@@ -261,7 +261,7 @@ where
 
 				&child_root
 			} else {
-				return Ok(true);
+				return Ok(true)
 			}
 		} else {
 			&self.root
@@ -284,8 +284,8 @@ where
 				Ok(v) => v.unwrap_or_else(|| empty_child_trie_root::<Layout<H>>().encode()),
 				Err(e) => {
 					debug!(target: "trie", "Error while iterating child storage: {}", e);
-					return;
-				}
+					return
+				},
 			};
 			child_root.as_mut().copy_from_slice(&root_vec);
 			&child_root
@@ -317,8 +317,8 @@ where
 			Ok(v) => v.unwrap_or_else(|| empty_child_trie_root::<Layout<H>>().encode()),
 			Err(e) => {
 				debug!(target: "trie", "Error while iterating child storage: {}", e);
-				return;
-			}
+				return
+			},
 		};
 		let mut root = H::Out::default();
 		root.as_mut().copy_from_slice(&root_vec);
@@ -374,7 +374,7 @@ where
 				debug_assert!(key.starts_with(prefix));
 
 				if !f(key, value) {
-					return Ok(false);
+					return Ok(false)
 				}
 			}
 
@@ -389,9 +389,8 @@ where
 		};
 		match result {
 			Ok(completed) => Ok(completed),
-			Err(e) if matches!(*e, TrieError::IncompleteDatabase(_)) && allow_missing_nodes => {
-				Ok(false)
-			}
+			Err(e) if matches!(*e, TrieError::IncompleteDatabase(_)) && allow_missing_nodes =>
+				Ok(false),
 			Err(e) => Err(format!("TrieDB iteration error: {}", e)),
 		}
 	}
@@ -446,7 +445,7 @@ impl<'a, S: 'a + TrieBackendStorage<H>, H: Hasher> hash_db::HashDB<H, DBValue>
 				Err(e) => {
 					warn!(target: "trie", "Failed to read from DB: {}", e);
 					None
-				}
+				},
 			}
 		}
 	}
@@ -529,14 +528,14 @@ impl<S: TrieBackendStorage<H>, H: Hasher> hash_db::AsHashDB<H, DBValue>
 impl<S: TrieBackendStorage<H>, H: Hasher> hash_db::HashDB<H, DBValue> for TrieBackendEssence<S, H> {
 	fn get(&self, key: &H::Out, prefix: Prefix) -> Option<DBValue> {
 		if *key == self.empty {
-			return Some([0u8].to_vec());
+			return Some([0u8].to_vec())
 		}
 		match self.storage.get(&key, prefix) {
 			Ok(x) => x,
 			Err(e) => {
 				warn!(target: "trie", "Failed to read from DB: {}", e);
 				None
-			}
+			},
 		}
 	}
 
