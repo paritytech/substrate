d94bf34cb3305f2876045543771544fa548ed7a0
Squirrel <gilescope@gmail.com>
2021-07-26 14:18:27 +0100

    post rustfmt whitespace fixup (#9436)
    Taking best bits of rustfmt's format_strings
    
    Co-authored-by: Alexander Popiak <alexander.popiak@gmail.com>

--- frame/staking/reward-curve/src/lib.rs
+++ frame/staking/reward-curve/src/lib.rs
@@ -353,8 +353,8 @@ fn generate_piecewise_linear(points: Vec<(u32, u32)>) -> TokenStream2 {
 	for (x, y) in points {
 		let error = || {
 			panic!(
-				"Generated reward curve approximation doesn't fit into [0, 1] -> [0, 1] \
-			because of point:
+				"Generated reward curve approximation doesn't fit into [0, 1] -> [0, 1] because \
+				 of point:
 			x = {:07} per million
 			y = {:07} per million",
 				x, y
