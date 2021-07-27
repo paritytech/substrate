5f7a245b323d9950a2cad9e378fed89d466d7361
ebalo55 <ebalo55@users.noreply.github.com>
2021-07-27 19:39:49 +0200

    melodity beats inclusion

--- primitives/core/src/crypto.rs
+++ primitives/core/src/crypto.rs
@@ -576,6 +576,8 @@ ss58_address_format!(
 		(47, "reserved47", "Reserved for future use (47).")
 	NeatcoinAccount =>
 		(48, "neatcoin", "Neatcoin mainnet, standard account (*25519).")
+	NeatcoinAccount =>
+		(57, "beats", "Melodity Beats Mainnet, standard account (*25519).")
 	HydraDXAccount =>
 		(63, "hydradx", "HydraDX standard account (*25519).")
 	AventusAccount =>
