8956089130dd4f37c8d982a8019f8dbdd2ea8156
Andrew Jones <ascjones@gmail.com>
2021-08-03 13:42:07 +0100

    Update primitives/runtime/src/generic/unchecked_extrinsic.rs
    Co-authored-by: Keith Yeung <kungfukeith11@gmail.com>

--- primitives/runtime/src/generic/unchecked_extrinsic.rs
+++ primitives/runtime/src/generic/unchecked_extrinsic.rs
@@ -366,7 +366,7 @@ mod tests {
 
 	// NOTE: this is demonstration. One can simply use `()` for testing.
 	#[derive(
-		Debug, Encode, Decode, Clone, Eq, PartialEq, Ord, PartialOrd, scale_info::TypeInfo,
+		Debug, Encode, Decode, Clone, Eq, PartialEq, Ord, PartialOrd, TypeInfo,
 	)]
 	struct TestExtra;
 	impl SignedExtension for TestExtra {
