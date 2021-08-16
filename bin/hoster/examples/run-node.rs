4976294ae41355fa6008ecd517e767f4b4a73201
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-08-16 14:10:06 +0200

    Add switchable consensus

--- bin/hoster/examples/run-node.rs
+++ bin/hoster/examples/run-node.rs
@@ -1,12 +1,12 @@
-use hoster::run;
-
 fn main() {
-	run::<
+	hoster::Builder::<
 		node_runtime::Block,
 		node_runtime::RuntimeApi,
 		node_executor::Executor,
 		node_runtime::GenesisConfig,
 		node_cli::chain_spec::Extensions,
-	>()
+	>::new()
+	.with_babe_consensus()
+	.run()
 	.unwrap();
 }
--- bin/hoster/src/lib.rs
+++ bin/hoster/src/lib.rs
@@ -8,6 +8,8 @@ use sc_network::Event;
 use sc_service::error::Error as ServiceError;
 use sc_telemetry::TelemetryWorker;
 use sp_api::ConstructRuntimeApi;
+use sp_consensus::SlotData;
+use sp_consensus_aura::sr25519::{AuthorityId as AuraId, AuthorityPair as AuraPair};
 use sp_runtime::traits::{Block as BlockT, Header as HeaderT};
 use sp_transaction_pool::runtime_api::TaggedTransactionQueue;
 use std::{str::FromStr, sync::Arc};
@@ -15,7 +17,7 @@ use std::{str::FromStr, sync::Arc};
 type StateBackend<Block> =
 	sc_client_db::SyncingCachingState<sc_client_db::RefTrackingState<Block>, Block>;
 
-fn new_full<Block, RuntimeApi, Executor>(
+fn new_full_babe<Block, RuntimeApi, Executor>(
 	mut config: sc_service::Configuration,
 ) -> Result<sc_service::TaskManager, ServiceError>
 where
@@ -166,7 +168,7 @@ where
 		telemetry: telemetry.as_mut(),
 	})?;
 
-	if let sc_service::config::Role::Authority { .. } = &role {
+	if role.is_authority() {
 		let proposer = sc_basic_authorship::ProposerFactory::new(
 			task_manager.spawn_handle(),
 			client.clone(),
@@ -224,6 +226,275 @@ where
 
 		let babe = sc_consensus_babe::start_babe(babe_config)?;
 		task_manager.spawn_essential_handle().spawn_blocking("babe-proposer", babe);
+	}
+
+	// Spawn authority discovery module.
+	if role.is_authority() {
+		let authority_discovery_role =
+			sc_authority_discovery::Role::PublishAndDiscover(keystore_container.keystore());
+		let dht_event_stream =
+			network.event_stream("authority-discovery").filter_map(|e| async move {
+				match e {
+					Event::Dht(e) => Some(e),
+					_ => None,
+				}
+			});
+		let (authority_discovery_worker, _service) =
+			sc_authority_discovery::new_worker_and_service_with_config(
+				sc_authority_discovery::WorkerConfig {
+					publish_non_global_ips: auth_disc_publish_non_global_ips,
+					..Default::default()
+				},
+				client.clone(),
+				network.clone(),
+				Box::pin(dht_event_stream),
+				authority_discovery_role,
+				prometheus_registry.clone(),
+			);
+
+		task_manager
+			.spawn_handle()
+			.spawn("authority-discovery-worker", authority_discovery_worker.run());
+	}
+
+	// if the node isn't actively participating in consensus then it doesn't
+	// need a keystore, regardless of which protocol we use below.
+	let keystore =
+		if role.is_authority() { Some(keystore_container.sync_keystore()) } else { None };
+
+	if enable_grandpa {
+		let config = sc_finality_grandpa::Config {
+			// FIXME #1578 make this available through chainspec
+			gossip_duration: std::time::Duration::from_millis(333),
+			justification_period: 512,
+			name: Some(name),
+			observer_enabled: false,
+			keystore,
+			local_role: role,
+			telemetry: telemetry.as_ref().map(|x| x.handle()),
+		};
+
+		// start the full GRANDPA voter
+		// NOTE: non-authorities could run the GRANDPA observer protocol, but at
+		// this point the full voter should provide better guarantees of block
+		// and vote data availability than the observer. The observer has not
+		// been tested extensively yet and having most nodes in a network run it
+		// could lead to finality stalls.
+		let grandpa_config = sc_finality_grandpa::GrandpaParams {
+			config,
+			link: grandpa_link,
+			network: network.clone(),
+			telemetry: telemetry.as_ref().map(|x| x.handle()),
+			voting_rule: sc_finality_grandpa::VotingRulesBuilder::default().build(),
+			prometheus_registry,
+			shared_voter_state: sc_finality_grandpa::SharedVoterState::empty(),
+		};
+
+		// the GRANDPA voter task is considered infallible, i.e.
+		// if it fails we take down the service with it.
+		task_manager.spawn_essential_handle().spawn_blocking(
+			"grandpa-voter",
+			sc_finality_grandpa::run_grandpa_voter(grandpa_config)?,
+		);
+	}
+
+	network_starter.start_network();
+
+	Ok(task_manager)
+}
+
+fn new_full_aura<Block, RuntimeApi, Executor>(
+	mut config: sc_service::Configuration,
+) -> Result<sc_service::TaskManager, ServiceError>
+where
+	Block: BlockT + std::marker::Unpin,
+	<Block as BlockT>::Hash: FromStr,
+	<<Block as BlockT>::Header as HeaderT>::Number: AsPrimitive<usize>,
+	Executor: NativeExecutionDispatch + 'static,
+	RuntimeApi: ConstructRuntimeApi<Block, sc_service::TFullClient<Block, RuntimeApi, Executor>>
+		+ Send
+		+ Sync
+		+ 'static,
+	<RuntimeApi as ConstructRuntimeApi<
+		Block,
+		sc_service::TFullClient<Block, RuntimeApi, Executor>,
+	>>::RuntimeApi: TaggedTransactionQueue<Block>
+		+ sp_consensus_aura::AuraApi<Block, AuraId>
+		+ sp_block_builder::BlockBuilder<Block>
+		+ sp_api::ApiExt<Block, StateBackend = StateBackend<Block>>
+		+ sc_finality_grandpa::GrandpaApi<Block>
+		+ sp_offchain::OffchainWorkerApi<Block>
+		+ sp_api::Metadata<Block>
+		+ sp_session::SessionKeys<Block>
+		+ sp_authority_discovery::AuthorityDiscoveryApi<Block>,
+{
+	let telemetry = config
+		.telemetry_endpoints
+		.clone()
+		.filter(|x| !x.is_empty())
+		.map(|endpoints| -> Result<_, sc_telemetry::Error> {
+			let worker = TelemetryWorker::new(16)?;
+			let telemetry = worker.handle().new_telemetry(endpoints);
+			Ok((worker, telemetry))
+		})
+		.transpose()?;
+
+	let (client, backend, keystore_container, mut task_manager) =
+		sc_service::new_full_parts::<Block, RuntimeApi, Executor>(
+			&config,
+			telemetry.as_ref().map(|(_, telemetry)| telemetry.handle()),
+		)?;
+	let client = Arc::new(client);
+
+	let mut telemetry = telemetry.map(|(worker, telemetry)| {
+		task_manager.spawn_handle().spawn("telemetry", worker.run());
+		telemetry
+	});
+
+	let select_chain = sc_consensus::LongestChain::new(backend.clone());
+
+	let transaction_pool = sc_transaction_pool::BasicPool::new_full(
+		config.transaction_pool.clone(),
+		config.role.is_authority().into(),
+		config.prometheus_registry(),
+		task_manager.spawn_essential_handle(),
+		client.clone(),
+	);
+
+	let (grandpa_block_import, grandpa_link) = sc_finality_grandpa::block_import(
+		client.clone(),
+		&(client.clone() as Arc<_>),
+		select_chain.clone(),
+		telemetry.as_ref().map(|x| x.handle()),
+	)?;
+
+	let slot_duration = sc_consensus_aura::slot_duration(&*client)?.slot_duration();
+
+	let import_queue = sc_consensus_aura::import_queue::<AuraPair, _, _, _, _, _, _>(
+		sc_consensus_aura::ImportQueueParams {
+			block_import: grandpa_block_import.clone(),
+			justification_import: Some(Box::new(grandpa_block_import.clone())),
+			client: client.clone(),
+			create_inherent_data_providers: move |_, ()| async move {
+				let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
+
+				let slot =
+					sp_consensus_aura::inherents::InherentDataProvider::from_timestamp_and_duration(
+						*timestamp,
+						slot_duration,
+					);
+
+				Ok((timestamp, slot))
+			},
+			spawner: &task_manager.spawn_essential_handle(),
+			can_author_with: sp_consensus::CanAuthorWithNativeVersion::new(
+				client.executor().clone(),
+			),
+			registry: config.prometheus_registry(),
+			check_for_equivocation: Default::default(),
+			telemetry: telemetry.as_ref().map(|x| x.handle()),
+		},
+	)?;
+
+	let auth_disc_publish_non_global_ips = config.network.allow_non_globals_in_dht;
+
+	config.network.extra_sets.push(sc_finality_grandpa::grandpa_peers_set_config());
+	let warp_sync = Arc::new(sc_finality_grandpa::warp_proof::NetworkProvider::new(
+		backend.clone(),
+		grandpa_link.shared_authority_set().clone(),
+	));
+
+	let (network, system_rpc_tx, network_starter) =
+		sc_service::build_network(sc_service::BuildNetworkParams {
+			config: &config,
+			client: client.clone(),
+			transaction_pool: transaction_pool.clone(),
+			spawn_handle: task_manager.spawn_handle(),
+			import_queue,
+			on_demand: None,
+			block_announce_validator_builder: None,
+			warp_sync: Some(warp_sync),
+		})?;
+
+	if config.offchain_worker.enabled {
+		sc_service::build_offchain_workers(
+			&config,
+			task_manager.spawn_handle(),
+			client.clone(),
+			network.clone(),
+		);
+	}
+
+	let role = config.role.clone();
+	let force_authoring = config.force_authoring;
+	let backoff_authoring_blocks =
+		Some(sc_consensus_slots::BackoffAuthoringOnFinalizedHeadLagging::default());
+	let name = config.network.node_name.clone();
+	let enable_grandpa = !config.disable_grandpa;
+	let prometheus_registry = config.prometheus_registry().cloned();
+
+	sc_service::spawn_tasks(sc_service::SpawnTasksParams {
+		config,
+		backend: backend.clone(),
+		client: client.clone(),
+		keystore: keystore_container.sync_keystore(),
+		network: network.clone(),
+		rpc_extensions_builder: Box::new(|_, _| Ok(())),
+		transaction_pool: transaction_pool.clone(),
+		task_manager: &mut task_manager,
+		on_demand: None,
+		remote_blockchain: None,
+		system_rpc_tx,
+		telemetry: telemetry.as_mut(),
+	})?;
+
+	if role.is_authority() {
+		let proposer = sc_basic_authorship::ProposerFactory::new(
+			task_manager.spawn_handle(),
+			client.clone(),
+			transaction_pool.clone(),
+			prometheus_registry.as_ref(),
+			telemetry.as_ref().map(|x| x.handle()),
+		);
+
+		let can_author_with =
+			sp_consensus::CanAuthorWithNativeVersion::new(client.executor().clone());
+
+		let slot_duration = sc_consensus_aura::slot_duration(&*client)?;
+		let raw_slot_duration = slot_duration.slot_duration();
+		let aura = sc_consensus_aura::start_aura::<AuraPair, _, _, _, _, _, _, _, _, _, _, _>(
+			sc_consensus_aura::StartAuraParams {
+				slot_duration,
+				client: client.clone(),
+				select_chain,
+				block_import: grandpa_block_import,
+				proposer_factory: proposer,
+				create_inherent_data_providers: move |_, ()| async move {
+					let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
+
+					let slot =
+						sp_consensus_aura::inherents::InherentDataProvider::from_timestamp_and_duration(
+							*timestamp,
+							raw_slot_duration,
+						);
+
+					Ok((timestamp, slot))
+				},
+				force_authoring,
+				backoff_authoring_blocks,
+				keystore: keystore_container.sync_keystore(),
+				can_author_with,
+				sync_oracle: network.clone(),
+				justification_sync_link: network.clone(),
+				block_proposal_slot_portion: SlotProportion::new(2f32 / 3f32),
+				max_block_proposal_slot_portion: None,
+				telemetry: telemetry.as_ref().map(|x| x.handle()),
+			},
+		)?;
+
+		// the AURA authoring task is considered essential, i.e. if it
+		// fails we take down the service with it.
+		task_manager.spawn_essential_handle().spawn_blocking("aura", aura);
 	}
 
 	// Spawn authority discovery module.
@@ -360,38 +631,114 @@ where
 	}
 }
 
-pub fn run<Block, RuntimeApi, Executor, GenesisConfig, Extension>() -> Result<(), sc_cli::Error>
-where
-	Block: BlockT + std::marker::Unpin,
-	<Block as BlockT>::Hash: FromStr,
-	<<Block as BlockT>::Header as HeaderT>::Number: AsPrimitive<usize>,
-	Executor: NativeExecutionDispatch + 'static,
-	RuntimeApi: ConstructRuntimeApi<Block, sc_service::TFullClient<Block, RuntimeApi, Executor>>
-		+ Send
-		+ Sync
-		+ 'static,
-	<RuntimeApi as ConstructRuntimeApi<
-		Block,
-		sc_service::TFullClient<Block, RuntimeApi, Executor>,
-	>>::RuntimeApi: TaggedTransactionQueue<Block>
-		+ sp_consensus_babe::BabeApi<Block>
-		+ sp_block_builder::BlockBuilder<Block>
-		+ sp_api::ApiExt<Block, StateBackend = StateBackend<Block>>
-		+ sc_finality_grandpa::GrandpaApi<Block>
-		+ sp_offchain::OffchainWorkerApi<Block>
-		+ sp_api::Metadata<Block>
-		+ sp_session::SessionKeys<Block>
-		+ sp_authority_discovery::AuthorityDiscoveryApi<Block>,
-	GenesisConfig: sc_chain_spec::RuntimeGenesis + 'static,
-	Extension:
-		sp_runtime::DeserializeOwned + Send + Sync + sc_service::ChainSpecExtension + 'static,
+pub struct Builder<Block, RuntimeApi, Executor, GenesisConfig, Extension> {
+	_phantom: std::marker::PhantomData<(Block, RuntimeApi, Executor, GenesisConfig, Extension)>,
+}
+
+impl<Block, RuntimeApi, Executor, GenesisConfig, Extension>
+	Builder<Block, RuntimeApi, Executor, GenesisConfig, Extension>
 {
-	let cli = Cli::<GenesisConfig, Extension>::from_args();
+	pub fn new() -> Self {
+		Self { _phantom: Default::default() }
+	}
 
-	let runner = cli.create_runner(&cli.run)?;
-	runner.run_node_until_exit(|config| async move {
-		new_full::<Block, RuntimeApi, Executor>(config)
-	})?;
+	pub fn with_babe_consensus(
+		self,
+	) -> BabeBuilder<Block, RuntimeApi, Executor, GenesisConfig, Extension> {
+		BabeBuilder { _phantom: self._phantom }
+	}
 
-	Ok(())
+	pub fn with_aura_consensus(
+		self,
+	) -> AuraBuilder<Block, RuntimeApi, Executor, GenesisConfig, Extension> {
+		AuraBuilder { _phantom: self._phantom }
+	}
+}
+
+pub struct BabeBuilder<Block, RuntimeApi, Executor, GenesisConfig, Extension> {
+	_phantom: std::marker::PhantomData<(Block, RuntimeApi, Executor, GenesisConfig, Extension)>,
+}
+
+impl<Block, RuntimeApi, Executor, GenesisConfig, Extension>
+	BabeBuilder<Block, RuntimeApi, Executor, GenesisConfig, Extension>
+{
+	pub fn run(self) -> Result<(), sc_cli::Error>
+	where
+		Block: BlockT + std::marker::Unpin,
+		<Block as BlockT>::Hash: FromStr,
+		<<Block as BlockT>::Header as HeaderT>::Number: AsPrimitive<usize>,
+		Executor: NativeExecutionDispatch + 'static,
+		RuntimeApi: ConstructRuntimeApi<Block, sc_service::TFullClient<Block, RuntimeApi, Executor>>
+			+ Send
+			+ Sync
+			+ 'static,
+		<RuntimeApi as ConstructRuntimeApi<
+			Block,
+			sc_service::TFullClient<Block, RuntimeApi, Executor>,
+		>>::RuntimeApi: TaggedTransactionQueue<Block>
+			+ sp_consensus_babe::BabeApi<Block>
+			+ sp_block_builder::BlockBuilder<Block>
+			+ sp_api::ApiExt<Block, StateBackend = StateBackend<Block>>
+			+ sc_finality_grandpa::GrandpaApi<Block>
+			+ sp_offchain::OffchainWorkerApi<Block>
+			+ sp_api::Metadata<Block>
+			+ sp_session::SessionKeys<Block>
+			+ sp_authority_discovery::AuthorityDiscoveryApi<Block>,
+		GenesisConfig: sc_chain_spec::RuntimeGenesis + 'static,
+		Extension:
+			sp_runtime::DeserializeOwned + Send + Sync + sc_service::ChainSpecExtension + 'static,
+	{
+		let cli = Cli::<GenesisConfig, Extension>::from_args();
+
+		let runner = cli.create_runner(&cli.run)?;
+		runner.run_node_until_exit(|config| async move {
+			new_full_babe::<Block, RuntimeApi, Executor>(config)
+		})?;
+
+		Ok(())
+	}
+}
+
+pub struct AuraBuilder<Block, RuntimeApi, Executor, GenesisConfig, Extension> {
+	_phantom: std::marker::PhantomData<(Block, RuntimeApi, Executor, GenesisConfig, Extension)>,
+}
+
+impl<Block, RuntimeApi, Executor, GenesisConfig, Extension>
+	AuraBuilder<Block, RuntimeApi, Executor, GenesisConfig, Extension>
+{
+	pub fn run(self) -> Result<(), sc_cli::Error>
+	where
+		Block: BlockT + std::marker::Unpin,
+		<Block as BlockT>::Hash: FromStr,
+		<<Block as BlockT>::Header as HeaderT>::Number: AsPrimitive<usize>,
+		Executor: NativeExecutionDispatch + 'static,
+		RuntimeApi: ConstructRuntimeApi<Block, sc_service::TFullClient<Block, RuntimeApi, Executor>>
+			+ Send
+			+ Sync
+			+ 'static,
+		<RuntimeApi as ConstructRuntimeApi<
+			Block,
+			sc_service::TFullClient<Block, RuntimeApi, Executor>,
+		>>::RuntimeApi: TaggedTransactionQueue<Block>
+			+ sp_consensus_aura::AuraApi<Block, AuraId>
+			+ sp_block_builder::BlockBuilder<Block>
+			+ sp_api::ApiExt<Block, StateBackend = StateBackend<Block>>
+			+ sc_finality_grandpa::GrandpaApi<Block>
+			+ sp_offchain::OffchainWorkerApi<Block>
+			+ sp_api::Metadata<Block>
+			+ sp_session::SessionKeys<Block>
+			+ sp_authority_discovery::AuthorityDiscoveryApi<Block>,
+		GenesisConfig: sc_chain_spec::RuntimeGenesis + 'static,
+		Extension:
+			sp_runtime::DeserializeOwned + Send + Sync + sc_service::ChainSpecExtension + 'static,
+	{
+		let cli = Cli::<GenesisConfig, Extension>::from_args();
+
+		let runner = cli.create_runner(&cli.run)?;
+		runner.run_node_until_exit(|config| async move {
+			new_full_aura::<Block, RuntimeApi, Executor>(config)
+		})?;
+
+		Ok(())
+	}
 }
