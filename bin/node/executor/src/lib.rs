80059d22551ec2975f629f8754e6ee8bb5712592
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-08-17 12:36:34 +0200

    Fix minor node-executor error

--- bin/node/executor/src/lib.rs
+++ bin/node/executor/src/lib.rs
@@ -18,7 +18,7 @@
 //! A `CodeExecutor` specialization which uses natively compiled runtime when the wasm to be
 //! executed is equivalent to the natively compiled code.
 
-pub use sc_executor::NativeExecutor;
+pub use sc_executor::NativeElseWasmExecutor;
 
 // Declare an instance of the native executor named `ExecutorDispatch`. Include the wasm binary as
 // the equivalent wasm code.
