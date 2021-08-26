6ef2484ad6a7f849641c1a72328d5d47ecdef177
Mateusz Nowakowski <mat13mn@gmail.com>
2021-08-26 13:18:46 +0200

    disable record_proof_works test

--- primitives/api/test/tests/runtime_calls.rs
+++ primitives/api/test/tests/runtime_calls.rs
@@ -157,6 +157,9 @@ fn initialize_block_is_skipped() {
 }
 
 #[test]
+#[ignore]
+/// that test should be easy to allign once we have
+/// encrypted extrinscs intended to  be executed in the same block as submitted
 fn record_proof_works() {
 	let (client, longest_chain) = TestClientBuilder::new()
 		.set_execution_strategy(ExecutionStrategy::Both)
