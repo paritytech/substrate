8721729ffed1a65d4f93443ac04e35839e7ed883
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2021-09-02 19:28:36 +0200

    bring back CORS hack to work with polkadot UI

--- client/service/src/builder.rs
+++ client/service/src/builder.rs
@@ -659,7 +659,7 @@ fn init_telemetry<TBl: BlockT, TCl: BlockBackend<TBl>>(
 // Maciej: This is very WIP, mocking the original `gen_handler`. All of the `jsonrpsee`
 // specific logic should be merged back to `gen_handler` down the road.
 fn gen_rpc_module<TBl, TBackend, TCl, TExPool>(
-	deny_unsafe: DenyUnsafe,
+	_deny_unsafe: DenyUnsafe,
 	spawn_handle: SpawnTaskHandle,
 	client: Arc<TCl>,
 	on_demand: Option<Arc<OnDemand<TBl>>>,
@@ -692,6 +692,9 @@ where
 	TBl::Header: Unpin,
 {
 	const UNIQUE_METHOD_NAMES_PROOF: &str = "Method names are unique; qed";
+
+	// TODO(niklasad1): fix CORS.
+	let deny_unsafe = DenyUnsafe::No;
 
 	let system_info = sc_rpc::system::SystemInfo {
 		chain_name: config.chain_spec.name().into(),
