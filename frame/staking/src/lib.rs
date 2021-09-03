b83afc337e98114d7acc1759068efb9e3a5a86de
emostov <32168567+emostov@users.noreply.github.com>
2021-09-03 13:44:24 +0200

    comment

--- frame/staking/src/lib.rs
+++ frame/staking/src/lib.rs
@@ -732,7 +732,7 @@ enum Releases {
 	V5_0_0, // blockable validators.
 	V6_0_0, // removal of all storage associated with offchain phragmen.
 	V7_0_0, // keep track of number of nominators / validators in map
-	V8_0_0, // Populate `SortedListProvider` with the assumption that it is pallet-bags-list
+	V8_0_0, // populate `SortedListProvider`.
 }
 
 impl Default for Releases {
