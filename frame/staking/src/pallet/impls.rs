dba68757f23b59ef46fdbbb413dfcd3979a1d8ff
emostov <32168567+emostov@users.noreply.github.com>
2021-09-03 20:27:24 +0200

    use nominator map comment

--- frame/staking/src/pallet/impls.rs
+++ frame/staking/src/pallet/impls.rs
@@ -1242,7 +1242,8 @@ impl<T: Config> VoteWeightProvider<T::AccountId> for Pallet<T> {
 }
 
 /// A simple voter list implementation that does not require any additional pallets. Note, this
-/// does not provided nominators in sorted ordered.
+/// does not provided nominators in sorted ordered. If you desire nominators in a sorted order take
+/// a look at [`pallet-bags-list].
 pub struct UseNominatorsMap<T>(sp_std::marker::PhantomData<T>);
 impl<T: Config> SortedListProvider<T::AccountId> for UseNominatorsMap<T> {
 	type Error = ();
