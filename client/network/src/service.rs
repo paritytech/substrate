b409c7099407e7cdc0fcdb89e8e2fdf7f8256ccb
Chevdor <chevdor@users.noreply.github.com>
2021-09-07 12:18:57 +0200

    Cherry Pick of PR #9676 + #9391 (#9704)
    * RUSTSEC-2021-0076 bump libsecp256k1 (#9391)
    
    * RUSTSEC-2021-0076 bump libsecp256k1
    
    libsecp256k1 allows overflowing signatures
    https://rustsec.org/advisories/RUSTSEC-2021-0076
    
    Changes were made to conform to libsecp256k1 version differences.
    
    Closes #9356
    
    * parse_standard_slice() -> parse_overflowing_slice()
    
    * Added v2 host function for ecdsa_verify
    
    * Add feature tag over helpers
    
    * Added ecdsa_verify v2 to test runner
    
    * PR feedback
    
    - Spaces -> tabs
    - renamed two helper functions
    
    * Fixed imports after rebasing
    
    * Bump rest of libsecp256k1 (and libp2p)
    
    libp2p also uses libsecp256k1 so it is required to be bumped too, along
    with all the version difference changes.
    
    * Add version2 for ecdsa pubkey recovery
    
    * libp2p rebase master fixes
    
    * Fix test panic when non Behaviour event is returned
    
    * Update bin/node/browser-testing/Cargo.toml
    
    * Update primitives/core/src/ecdsa.rs
    
    * Update primitives/core/src/ecdsa.rs
    
    * Update Cargo.lock
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Use coherent prost crate  versions (#9676)
    
    * Bump node-browser-testing deps on prost
    
    Co-authored-by: Trevor Arjeski <72849114+trevor-crypto@users.noreply.github.com>
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    Co-authored-by: Andreas Doerr <adoerr@users.noreply.github.com>

--- client/network/src/service.rs
+++ client/network/src/service.rs
@@ -1623,7 +1623,7 @@ impl<B: BlockT + 'static, H: ExHashT> Future for NetworkWorker<B, H> {
 			}
 
 			// Process the next action coming from the network.
-			let next_event = this.network_service.next_event();
+			let next_event = this.network_service.select_next_some();
 			futures::pin_mut!(next_event);
 			let poll_value = next_event.poll_unpin(cx);
 
@@ -1911,14 +1911,14 @@ impl<B: BlockT + 'static, H: ExHashT> Future for NetworkWorker<B, H> {
 						}
 					}
 				},
-				Poll::Ready(SwarmEvent::NewListenAddr(addr)) => {
-					trace!(target: "sub-libp2p", "Libp2p => NewListenAddr({})", addr);
+				Poll::Ready(SwarmEvent::NewListenAddr { address, .. }) => {
+					trace!(target: "sub-libp2p", "Libp2p => NewListenAddr({})", address);
 					if let Some(metrics) = this.metrics.as_ref() {
 						metrics.listeners_local_addresses.inc();
 					}
 				},
-				Poll::Ready(SwarmEvent::ExpiredListenAddr(addr)) => {
-					info!(target: "sub-libp2p", "📪 No longer listening on {}", addr);
+				Poll::Ready(SwarmEvent::ExpiredListenAddr { address, .. }) => {
+					info!(target: "sub-libp2p", "📪 No longer listening on {}", address);
 					if let Some(metrics) = this.metrics.as_ref() {
 						metrics.listeners_local_addresses.dec();
 					}
@@ -2000,9 +2000,8 @@ impl<B: BlockT + 'static, H: ExHashT> Future for NetworkWorker<B, H> {
 					}
 				},
 				Poll::Ready(SwarmEvent::UnknownPeerUnreachableAddr { address, error }) =>
-					trace!(target: "sub-libp2p", "Libp2p => UnknownPeerUnreachableAddr({}): {}",
-						address, error),
-				Poll::Ready(SwarmEvent::ListenerClosed { reason, addresses }) => {
+					trace!(target: "sub-libp2p", "Libp2p => UnknownPeerUnreachableAddr({}): {}", address, error),
+				Poll::Ready(SwarmEvent::ListenerClosed { reason, addresses, .. }) => {
 					if let Some(metrics) = this.metrics.as_ref() {
 						metrics.listeners_local_addresses.sub(addresses.len() as u64);
 					}
@@ -2021,7 +2020,7 @@ impl<B: BlockT + 'static, H: ExHashT> Future for NetworkWorker<B, H> {
 						),
 					}
 				},
-				Poll::Ready(SwarmEvent::ListenerError { error }) => {
+				Poll::Ready(SwarmEvent::ListenerError { error, .. }) => {
 					debug!(target: "sub-libp2p", "Libp2p => ListenerError: {}", error);
 					if let Some(metrics) = this.metrics.as_ref() {
 						metrics.listeners_errors_total.inc();
