965b4515a7ce1a743c637d80135557ee9e122fbf
koushiro <koushiro.cqx@gmail.com>
2021-09-07 12:33:51 +0800

    Fix
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- frame/membership/src/migrations/v4.rs
+++ frame/membership/src/migrations/v4.rs
@@ -80,7 +80,7 @@ pub fn migrate<T: frame_system::Config, P: GetStorageVersion + PalletInfoAccess,
 /// [`frame_support::traits::OnRuntimeUpgrade::pre_upgrade`] for further testing.
 ///
 /// Panics if anything goes wrong.
-pub fn pre_migrate<P: GetStorageVersion + 'static, N: AsRef<str>>(
+pub fn pre_migrate<P: GetStorageVersion, N: AsRef<str>>(
 	old_pallet_name: N,
 	new_pallet_name: N,
 ) {
