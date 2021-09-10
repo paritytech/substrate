a4197348767bbd3cd0d4f0952eb597917048e70a
Bastian Köcher <info@kchr.de>
2021-09-10 18:43:44 +0200

    Remove warnings

--- client/service/src/task_manager/mod.rs
+++ client/service/src/task_manager/mod.rs
@@ -22,10 +22,9 @@ use crate::{config::TaskType, Error};
 use exit_future::Signal;
 use futures::{
 	future::{join_all, pending, select, try_join_all, BoxFuture, Either},
-	sink::SinkExt,
 	Future, FutureExt, StreamExt,
 };
-use log::{debug, error};
+use log::debug;
 use prometheus_endpoint::{
 	exponential_buckets, register, CounterVec, HistogramOpts, HistogramVec, Opts, PrometheusError,
 	Registry, U64,
