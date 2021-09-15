c1e06c62f396b3db1b9f9ee4836878659aea5956
Andrew Jones <ascjones@gmail.com>
2021-09-15 09:46:35 +0100

    Fix collective errors

--- frame/collective/src/lib.rs
+++ frame/collective/src/lib.rs
@@ -126,6 +126,7 @@ impl DefaultVote for MoreThanMajorityThenPrimeDefaultVote {
 
 /// Origin for the collective module.
 #[derive(PartialEq, Eq, Clone, RuntimeDebug, Encode, Decode, TypeInfo)]
+#[scale_info(skip_type_params(I))]
 pub enum RawOrigin<AccountId, I> {
 	/// It has been condoned by a given number of members of the collective from a given total.
 	Members(MemberCount, MemberCount),
