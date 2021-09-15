44b6259bf849b4dfc5580d2fbb195303f4eb2d65
Andrew Jones <ascjones@gmail.com>
2021-09-15 09:30:28 +0100

    Remove pallet event metadata

--- frame/collective/src/lib.rs
+++ frame/collective/src/lib.rs
@@ -275,7 +275,6 @@ pub mod pallet {
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	#[pallet::metadata(T::AccountId = "AccountId", T::Hash = "Hash")]
 	pub enum Event<T: Config<I>, I: 'static = ()> {
 		/// A motion (given hash) has been proposed (by given account) with a threshold (given
 		/// `MemberCount`).
