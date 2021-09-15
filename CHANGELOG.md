56ac146468bf270e92a3f875498620f020fa60e3
Andrei Navoichyk <andrei@cere.io>
2021-09-15 12:57:48 +0300

    Revert "Merge pull request #77 from Cerebellum-Network/feature/update-nightly-version"
    This reverts commit f21284d3e59bd8702878d6021b57e665333c15a3, reversing
    changes made to 6dc9435ff988bb4bd44410727bf4baa2077603bd.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,7 +11,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Added dynamic block interval
 - Added validation for resource Id in chainbridge frame.
-- Updated nightly version to 2021-05-21
 
 ## [2.3.2]
 
--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-rustup install nightly-2021-05-21
+rustup install nightly-2021-05-07
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2021-05-21
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-05-07
