c03b51323a6fbca3f78a2b943a46512a1680bb2a
Karthikeyan <karthikeyan@cere.io>
2021-09-16 13:05:25 +0530

    Updated to use dockerhub

--- scripts/extract-wasm.sh
+++ scripts/extract-wasm.sh
@@ -2,7 +2,7 @@
 
 export dir_path_to="${1:-./}"
 export image_tag="${2:-latest}"
-docker pull 338287888375.dkr.ecr.us-west-2.amazonaws.com/pos-network-node:"$image_tag"
-id=$(docker create "338287888375.dkr.ecr.us-west-2.amazonaws.com/pos-network-node:$image_tag")
-docker cp "${id}":/home/cere/wasm-file "$dir_path_to"
+docker pull docker pull cerebellumnetwork/pos-node:"$image_tag"
+id=$(docker create "cerebellumnetwork/pos-node:$image_tag")
+docker cp "${id}":/home/cere/node-runtime-artifacts "$dir_path_to"
 docker rm -v "$id"
