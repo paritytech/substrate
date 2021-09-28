a327619608575321a76b936324162483c45ec1dd
Giles Cope <gilescope@gmail.com>
2021-09-28 10:23:31 +0100

    updated msg

--- client/cli/src/runner.rs
+++ client/cli/src/runner.rs
@@ -198,7 +198,7 @@ impl<C: SubstrateCli> Runner<C> {
 /// (and hint to people who haven't set up logging how to do it)
 pub fn print_node_infos<C: SubstrateCli>(config: &Configuration) {	
 	if log::max_level() == log::LevelFilter::Off {
-		println!("( for logging set env var RUST_LOG=info )");
+		println!("Logging disabled  (to enable typically set env var RUST_LOG=info )");
 	}
 	info!("{}", C::impl_name());
 	info!("✌️  version {}", C::impl_version());
