2229b338bc63526f51c59e0c4520b8dc49b37a7b
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-09-29 01:22:07 -0400

    simple trait to inspect metadata

--- frame/support/src/traits/tokens/fungibles.rs
+++ frame/support/src/traits/tokens/fungibles.rs
@@ -65,6 +65,18 @@ pub trait Inspect<AccountId> {
 	) -> WithdrawConsequence<Self::Balance>;
 }
 
+/// Trait for reading metadata from a fungible asset.
+pub trait InspectMetadata<AccountId>: Inspect<AccountId> {
+	/// Return the name of an asset.
+	fn name(asset: &Self::AssetId) -> Vec<u8>;
+
+	/// Return the symbol of an asset.
+	fn symbol(asset: &Self::AssetId) -> Vec<u8>;
+
+	/// Return the decimals of an asset.
+	fn decimals(asset: &Self::AssetId) -> u8;
+}
+
 /// Trait for providing a set of named fungible assets which can be created and destroyed.
 pub trait Mutate<AccountId>: Inspect<AccountId> {
 	/// Attempt to increase the `asset` balance of `who` by `amount`.
