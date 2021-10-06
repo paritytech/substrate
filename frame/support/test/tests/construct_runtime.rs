1411f7b030e4e20c63ff4881afe1fe3c11b2bc2f
thiolliere <gui.thiolliere@gmail.com>
2021-10-06 16:55:06 +0200

    refactor test for more understandable flow

--- frame/support/test/tests/construct_runtime.rs
+++ frame/support/test/tests/construct_runtime.rs
@@ -327,11 +327,12 @@ mod origin_test {
 		assert_eq!(Origin::from(super::nested::module3::Origin).filter_call(&rejected_call), false);
 
 		let mut origin = Origin::from(Some(0));
-
 		origin.add_filter(|c| matches!(c, Call::Module3(_)));
 		assert_eq!(origin.filter_call(&accepted_call), false);
 		assert_eq!(origin.filter_call(&rejected_call), false);
 
+		// Now test for root origin and filters:
+		let mut origin = Origin::from(Some(0));
 		origin.set_caller_from(Origin::root());
 		assert!(matches!(origin.caller, OriginCaller::system(super::system::RawOrigin::Root)));
 
@@ -342,7 +343,7 @@ mod origin_test {
 		origin.set_caller_from(Origin::from(Some(0)));
 
 		// Back to another signed origin, the filtered are now effective again
-		assert_eq!(origin.filter_call(&accepted_call), false);
+		assert_eq!(origin.filter_call(&accepted_call), true);
 		assert_eq!(origin.filter_call(&rejected_call), false);
 
 		origin.set_caller_from(Origin::root());
