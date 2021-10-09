d9ec48f7b56c7f61238b76f3d5afe5b5222e4224
david <wizdave97@gmail.com>
2021-10-09 14:22:21 +0100

    updated events of a couple pallets

--- frame/balances/src/tests.rs
+++ frame/balances/src/tests.rs
@@ -444,7 +444,7 @@ macro_rules! decl_tests {
 				let _ = Balances::withdraw(
 					&2, 11, WithdrawReasons::TRANSFER, ExistenceRequirement::KeepAlive
 				);
-				System::assert_last_event(Event::Balances(crate::Event::Withdraw(2, 11)));
+				System::assert_last_event(Event::Balances(crate::Event::Withdraw{who: 2, amount: 11}));
 				assert_eq!(Balances::free_balance(2), 100);
 				assert_eq!(<TotalIssuance<$test>>::get(), 100);
 			});
@@ -505,7 +505,7 @@ macro_rules! decl_tests {
 				assert_ok!(Balances::reserve(&1, 110));
 				assert_ok!(Balances::repatriate_reserved(&1, &2, 41, Status::Free), 0);
 				System::assert_last_event(
-					Event::Balances(crate::Event::ReserveRepatriated(1, 2, 41, Status::Free))
+					Event::Balances(crate::Event::ReserveRepatriated{from: 1, to: 2, balance: 41, destination_status: Status::Free})
 				);
 				assert_eq!(Balances::reserved_balance(1), 69);
 				assert_eq!(Balances::free_balance(1), 0);
@@ -724,18 +724,18 @@ macro_rules! decl_tests {
 					System::set_block_number(2);
 					assert_ok!(Balances::reserve(&1, 10));
 
-					System::assert_last_event(Event::Balances(crate::Event::Reserved(1, 10)));
+					System::assert_last_event(Event::Balances(crate::Event::Reserved{who: 1, value: 10}));
 
 					System::set_block_number(3);
 					assert!(Balances::unreserve(&1, 5).is_zero());
 
-					System::assert_last_event(Event::Balances(crate::Event::Unreserved(1, 5)));
+					System::assert_last_event(Event::Balances(crate::Event::Unreserved{who: 1, value: 5}));
 
 					System::set_block_number(4);
 					assert_eq!(Balances::unreserve(&1, 6), 1);
 
 					// should only unreserve 5
-					System::assert_last_event(Event::Balances(crate::Event::Unreserved(1, 5)));
+					System::assert_last_event(Event::Balances(crate::Event::Unreserved{who: 1, value: 5}));
 				});
 		}
 
@@ -751,8 +751,8 @@ macro_rules! decl_tests {
 						events(),
 						[
 							Event::System(system::Event::NewAccount(1)),
-							Event::Balances(crate::Event::Endowed(1, 100)),
-							Event::Balances(crate::Event::BalanceSet(1, 100, 0)),
+							Event::Balances(crate::Event::Endowed{account: 1, free_balance: 100}),
+							Event::Balances(crate::Event::BalanceSet{who: 1, free: 100, reserved: 0}),
 						]
 					);
 
@@ -763,8 +763,8 @@ macro_rules! decl_tests {
 						events(),
 						[
 							Event::System(system::Event::KilledAccount(1)),
-							Event::Balances(crate::Event::DustLost(1, 99)),
-							Event::Balances(crate::Event::Slashed(1, 1)),
+							Event::Balances(crate::Event::DustLost{account: 1, balance: 99}),
+							Event::Balances(crate::Event::Slashed{who: 1, amount_slashed: 1}),
 						]
 					);
 				});
@@ -782,8 +782,8 @@ macro_rules! decl_tests {
 						events(),
 						[
 							Event::System(system::Event::NewAccount(1)),
-							Event::Balances(crate::Event::Endowed(1, 100)),
-							Event::Balances(crate::Event::BalanceSet(1, 100, 0)),
+							Event::Balances(crate::Event::Endowed{account: 1, free_balance: 100}),
+							Event::Balances(crate::Event::BalanceSet{who: 1, free: 100, reserved: 0}),
 						]
 					);
 
@@ -794,7 +794,7 @@ macro_rules! decl_tests {
 						events(),
 						[
 							Event::System(system::Event::KilledAccount(1)),
-							Event::Balances(crate::Event::Slashed(1, 100)),
+							Event::Balances(crate::Event::Slashed{who: 1, amount_slashed: 100}),
 						]
 					);
 				});
@@ -814,7 +814,7 @@ macro_rules! decl_tests {
 					assert_eq!(Balances::slash(&1, 900), (NegativeImbalance::new(900), 0));
 					// Account is still alive
 					assert!(System::account_exists(&1));
-					System::assert_last_event(Event::Balances(crate::Event::Slashed(1, 900)));
+					System::assert_last_event(Event::Balances(crate::Event::Slashed{who: 1, amount_slashed: 900}));
 
 					// SCENARIO: Slash will kill account because not enough balance left.
 					assert_ok!(Balances::set_balance(Origin::root(), 1, 1_000, 0));
