00629de9b7aae97e041b21b081f469eb834169a7
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2021-10-14 12:44:07 +0100

    make the ws buffer size configurable (#10013)
    * make the ws buffer size configurable
    
    * Update client/cli/src/commands/run_cmd.rs
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    
    * Update client/cli/src/commands/run_cmd.rs
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    
    * Update client/cli/src/commands/run_cmd.rs
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Final touches
    
    * Apply suggestions from code review
    
    * fix bench
    
    * remove in buffer
    
    * Apply suggestions from code review
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- client/service/test/src/lib.rs
+++ client/service/test/src/lib.rs
@@ -261,6 +261,7 @@ fn node_config<
 		rpc_cors: None,
 		rpc_methods: Default::default(),
 		rpc_max_payload: None,
+		ws_max_out_buffer_capacity: None,
 		prometheus_config: None,
 		telemetry_endpoints: None,
 		default_heap_pages: None,
