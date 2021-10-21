d35f273b7d67b1b85a9e72973cab13c5c156c1d3
cheme <emericchevalier.pro@gmail.com>
2021-10-21 11:00:15 +0200

    Patch to delay runtime switch.

--- primitives/version/src/lib.rs
+++ primitives/version/src/lib.rs
@@ -217,12 +217,7 @@ impl RuntimeVersion {
 
 	/// Returns state version to use for update.
 	pub fn state_version(&self) -> StateVersion {
-		let core_api_id = sp_runtime::hashing::blake2_64(b"Core");
-		if self.has_api_with(&core_api_id, |v| v >= 4) {
-			StateVersion::V1
-		} else {
-			StateVersion::V0
-		}
+		StateVersion::V0
 	}
 }
 
