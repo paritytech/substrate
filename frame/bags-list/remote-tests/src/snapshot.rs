86f92965fca54411b1564f81bbb9912fcb735bab
David Palm <dvdplm@gmail.com>
2021-11-10 13:27:53 +0100

    Fmt

--- frame/bags-list/remote-tests/src/snapshot.rs
+++ frame/bags-list/remote-tests/src/snapshot.rs
@@ -18,7 +18,7 @@
 
 use frame_support::traits::PalletInfoAccess;
 use remote_externalities::{Builder, Mode, OnlineConfig};
-use sp_runtime::{DeserializeOwned, traits::Block as BlockT};
+use sp_runtime::{traits::Block as BlockT, DeserializeOwned};
 
 /// Execute create a snapshot from pallet-staking.
 pub async fn execute<Runtime: crate::RuntimeT, Block: BlockT + DeserializeOwned>(
