221499b4bee7c0c01d7499ec03fc25a8b0ada738
thiolliere <gui.thiolliere@gmail.com>
2021-11-17 11:59:05 +0900

    add comment about constraint

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -1253,6 +1253,8 @@ construct_runtime!(
 		Utility: pallet_utility,
 		Babe: pallet_babe,
 		Timestamp: pallet_timestamp,
+		// Authorship must be before session in order to note author in the correct session and era
+		// for im-online and staking.
 		Authorship: pallet_authorship,
 		Indices: pallet_indices,
 		Balances: pallet_balances,
