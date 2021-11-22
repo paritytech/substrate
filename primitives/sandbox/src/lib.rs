67e804c4042511effb484696617b8e0d92ee93f8
thiolliere <gui.thiolliere@gmail.com>
2021-11-22 17:59:50 +0900

    fix some doc link

--- primitives/sandbox/src/lib.rs
+++ primitives/sandbox/src/lib.rs
@@ -89,7 +89,7 @@ pub type HostFuncType<T> = fn(&mut T, &[Value]) -> Result<ReturnValue, HostError
 /// will be used by the guest module.
 ///
 /// The memory can't be directly accessed by supervisor, but only
-/// through designated functions [`get`](Memory::get) and [`set`](Memory::set).
+/// through designated functions [`get`](SandboxMemory::get) and [`set`](SandboxMemory::set).
 pub trait SandboxMemory: Sized + Clone {
 	/// Construct a new linear memory instance.
 	///
