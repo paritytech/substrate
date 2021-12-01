d07414f01bc5aaa8c32ff64249468518a1e487b3
Guillaume Thiolliere <gui.thiolliere@gmail.com>
2021-12-01 16:38:03 +0900

    Update frame/bags-list/remote-tests/src/snapshot.rs

--- frame/bags-list/remote-tests/src/snapshot.rs
+++ frame/bags-list/remote-tests/src/snapshot.rs
@@ -39,8 +39,10 @@ pub async fn execute<Runtime: crate::RuntimeT, Block: BlockT + DeserializeOwned>
 		}))
 		.inject_hashed_prefix(&<pallet_staking::Bonded<Runtime>>::prefix_hash())
 		.inject_hashed_prefix(&<pallet_staking::Ledger<Runtime>>::prefix_hash())
-		.inject_hashed_prefix(&<pallet_staking::Validators<Runtime>>::prefix_hash())
-		.inject_hashed_prefix(&<pallet_staking::Nominators<Runtime>>::prefix_hash())
+		.inject_hashed_prefix(&<pallet_staking::Validators<Runtime>>::map_storage_final_prefix())
+		.inject_hashed_prefix(&<pallet_staking::Nominators<Runtime>>::map_storage_final_prefix())
+		.inject_hashed_key(&<pallet_staking::Validators<Runtime>>::counter_storage_final_key())
+		.inject_hashed_key(&<pallet_staking::Nominators<Runtime>>::counter_storage_final_key())
 		.build()
 		.await
 		.unwrap();
