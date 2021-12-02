80df1fb19e38103f1dcf0c9d0b143f1dcbfd6576
Gavin Wood <gavin@parity.io>
2021-12-02 17:13:45 +0100

    Update frame/authorship/src/lib.rs
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- frame/authorship/src/lib.rs
+++ frame/authorship/src/lib.rs
@@ -311,8 +311,6 @@ impl<T: Config> Pallet<T> {
 		let digest = <frame_system::Pallet<T>>::digest();
 		let pre_runtime_digests = digest.logs.iter().filter_map(|d| d.as_pre_runtime());
 		T::FindAuthor::find_author(pre_runtime_digests).map(|a| { <Author<T>>::put(&a); a })
-			None
-		}
 	}
 
 	fn verify_and_import_uncles(new_uncles: Vec<T::Header>) -> dispatch::DispatchResult {
