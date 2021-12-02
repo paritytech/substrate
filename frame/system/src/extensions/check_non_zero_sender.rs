51a725b94875d4b3dc655943ed2c121ed968e3c5
Gav Wood <gavin@parity.io>
2021-12-02 15:29:22 -0500

    Formatting

--- frame/system/src/extensions/check_non_zero_sender.rs
+++ frame/system/src/extensions/check_non_zero_sender.rs
@@ -16,14 +16,16 @@
 // limitations under the License.
 
 use crate::Config;
-use sp_std::{marker::PhantomData, prelude::*};
 use codec::{Decode, Encode};
-use scale_info::TypeInfo;
-use sp_runtime::traits::{DispatchInfoOf, Dispatchable, SignedExtension};
-use sp_runtime::transaction_validity::{
-	InvalidTransaction, TransactionValidity, TransactionValidityError, ValidTransaction,
-};
 use frame_support::weights::DispatchInfo;
+use scale_info::TypeInfo;
+use sp_runtime::{
+	traits::{DispatchInfoOf, Dispatchable, SignedExtension},
+	transaction_validity::{
+		InvalidTransaction, TransactionValidity, TransactionValidityError, ValidTransaction,
+	},
+};
+use sp_std::{marker::PhantomData, prelude::*};
 
 /// Check to ensure that the sender is not the zero address.
 #[derive(Encode, Decode, Clone, Eq, PartialEq, TypeInfo)]
