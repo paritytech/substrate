7610cded3d725149b8b8ae74e859cde5fdffa661
Jan Bujak <jan@parity.io>
2021-12-06 09:17:54 +0000

    Fix `sc-executor` tests' compilation

--- client/executor/src/native_executor.rs
+++ client/executor/src/native_executor.rs
@@ -650,7 +650,7 @@ mod tests {
 			8,
 		);
 
-		fn extract_host_functions<H>(_: &WasmExecutor<H>) -> Vec<&'static dyn Function>
+		fn extract_host_functions<H>(_: &WasmExecutor<H>) -> Vec<&'static dyn sp_wasm_interface::Function>
 		where
 			H: HostFunctions,
 		{
