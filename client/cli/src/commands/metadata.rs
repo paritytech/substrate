f3a045f4dfeb764e76790242169f38054118e8d6
Bastian Köcher <bkchr@users.noreply.github.com>
2021-12-13 22:38:58 +0100

    Update client/cli/src/commands/metadata.rs
    Co-authored-by: David <dvdplm@gmail.com>

--- client/cli/src/commands/metadata.rs
+++ client/cli/src/commands/metadata.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Key related CLI utilities
+//! Metadata related CLI utilities
 
 use super::MetadataDump;
 use crate::{CliConfiguration, Error, SharedParams};
