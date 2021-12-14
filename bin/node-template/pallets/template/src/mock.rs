fed71adc3090e743f0ec945f01a093e3bffbdf45
Damilare <dakinlose@teamapt.com>
2021-12-14 22:03:27 +0100

    remove warning to fix CI issue refractor for pallet preimage

--- bin/node-template/pallets/template/src/mock.rs
+++ bin/node-template/pallets/template/src/mock.rs
@@ -1,5 +1,5 @@
 use crate as pallet_template;
-use frame_support::{parameter_types, traits::{ConstU16, ConstU64}};
+use frame_support::{traits::{ConstU16, ConstU64}};
 use frame_system as system;
 use sp_core::H256;
 use sp_runtime::{
