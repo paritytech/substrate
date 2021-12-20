5a6a7b2a6e40060b7177fac5b187d5a53c783b22
Adrian Catangiu <adrian@parity.io>
2021-12-20 14:58:24 +0200

    Update client/beefy/src/lib.rs
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>

--- client/beefy/src/lib.rs
+++ client/beefy/src/lib.rs
@@ -48,8 +48,9 @@ pub(crate) mod beefy_protocol_name {
 	/// Old names for the notifications protocol, used for backward compatibility.
 	pub(crate) const LEGACY_NAMES: [&'static str; 1] = ["/paritytech/beefy/1"];
 
-	/// Name of the notifications protocol used by Beefy.
-	/// Must be registered towards the networking in order for Beefy to properly function.
+	/// Name of the notifications protocol used by BEEFY.
+	///
+	/// Must be registered towards the networking in order for BEEFY to properly function.
 	pub fn standard_name<Hash: std::fmt::Display>(
 		genesis_hash: &Hash,
 		chain_spec: &Box<dyn ChainSpec>,
