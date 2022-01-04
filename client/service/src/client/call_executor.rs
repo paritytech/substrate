823581c4adc2cf9ab6948f5337bbb3cc5cb43cc8
Bastian Köcher <bkchr@users.noreply.github.com>
2022-01-04 20:48:43 +0100

    Use storage transaction when running with proof recorder (#10581)
    This will enable parachains to re-import a block without re-executing it.

--- client/service/src/client/call_executor.rs
+++ client/service/src/client/call_executor.rs
@@ -246,8 +246,10 @@ where
 					&runtime_code,
 					self.spawn_handle.clone(),
 				)
+				.with_storage_transaction_cache(
+					storage_transaction_cache.as_mut().map(|c| &mut **c),
+				)
 				.set_parent_hash(at_hash);
-				// TODO: https://github.com/paritytech/substrate/issues/4455
 				state_machine.execute_using_consensus_failure_handler(
 					execution_manager,
 					native_call.map(|n| || (n)().map_err(|e| Box::new(e) as Box<_>)),
