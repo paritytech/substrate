57f090550163945b57203ed9c4ac88168148d07c
Bastian Köcher <info@kchr.de>
2022-01-13 23:25:50 +0100

    Remove some last `AccountId32::default()` leftovers
    As we removed `Default` of account id, we have overseen some last bits. This pr removes these last
    bits.

--- primitives/core/src/crypto.rs
+++ primitives/core/src/crypto.rs
@@ -478,9 +478,7 @@ pub trait Public: ByteArray + Derive + CryptoType + PartialEq + Eq + Clone + Sen
 }
 
 /// An opaque 32-byte cryptographic identifier.
-#[derive(
-	Clone, Eq, PartialEq, Ord, PartialOrd, Default, Encode, Decode, MaxEncodedLen, TypeInfo,
-)]
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Encode, Decode, MaxEncodedLen, TypeInfo)]
 #[cfg_attr(feature = "std", derive(Hash))]
 pub struct AccountId32([u8; 32]);
 
@@ -541,9 +539,9 @@ impl<'a> sp_std::convert::TryFrom<&'a [u8]> for AccountId32 {
 	type Error = ();
 	fn try_from(x: &'a [u8]) -> Result<AccountId32, ()> {
 		if x.len() == 32 {
-			let mut r = AccountId32::default();
-			r.0.copy_from_slice(x);
-			Ok(r)
+			let mut data = [0; 32];
+			data.copy_from_slice(x);
+			Ok(AccountId32(data))
 		} else {
 			Err(())
 		}
