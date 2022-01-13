3d7abab552e91e4784104b800fcae3a2f8f3ae7e
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-01-13 14:14:10 +0100

    Add without_storage_info where needed
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/collective/src/lib.rs
+++ frame/collective/src/lib.rs
@@ -172,6 +172,7 @@ pub mod pallet {
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	#[pallet::storage_version(STORAGE_VERSION)]
+	#[pallet::without_storage_info]
 	pub struct Pallet<T, I = ()>(PhantomData<(T, I)>);
 
 	#[pallet::config]
--- frame/scored-pool/src/lib.rs
+++ frame/scored-pool/src/lib.rs
@@ -129,6 +129,7 @@ pub mod pallet {
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
+	#[pallet::without_storage_info]
 	pub struct Pallet<T, I = ()>(_);
 
 	#[pallet::config]
