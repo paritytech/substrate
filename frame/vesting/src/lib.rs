0cbed694192000a05d28c930c7cc9c3c2b3b6de0
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-01-13 13:54:33 +0100

    Remove generate_storage_info
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/vesting/src/lib.rs
+++ frame/vesting/src/lib.rs
@@ -225,7 +225,6 @@ pub mod pallet {
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
-	#[pallet::generate_storage_info]
 	pub struct Pallet<T>(_);
 
 	#[pallet::genesis_config]
