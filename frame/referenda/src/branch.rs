9318b09fa85491158086dd46c99c9455445ad6de
Shawn Tabrizi <shawntabrizi@gmail.com>
2022-01-31 04:29:12 +0000

    fix license

--- frame/referenda/src/branch.rs
+++ frame/referenda/src/branch.rs
@@ -7,7 +7,9 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// 	http://www.apache.org/licenses/LICENSE-2.0sp_runtime::{DispatchResult, traits::One}asp_runtime::{DispatchResult, traits::AtLeast32BitUnsigned} in writing, software
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
