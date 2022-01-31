fd5f1791a87ef70f156e7de3ce0c8398aa775295
Andrei Navoichyk <andrei@cere.io>
2022-01-31 16:46:01 +0300

    updated params

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -115,7 +115,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 276,
+	spec_version: 277,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
@@ -429,11 +429,11 @@ impl pallet_session::historical::Trait for Runtime {
 pallet_staking_reward_curve::build! {
 	const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
 		min_inflation: 0_000_100,
-		max_inflation: 0_002_555,
-		ideal_stake: 0_200_000,
+		max_inflation: 0_013_000,
+		ideal_stake: 0_100_000,
 		falloff: 0_050_000,
-		max_piece_count: 350,
-		test_precision: 0_185_000,
+		max_piece_count: 100,
+		test_precision: 0_050_000,
 	);
 }
 
