7cb0e76b847a49e2a9227e0c039bcbb335c83309
Alexander Gryaznov <hi@agryaznov.com>
2022-02-04 20:13:00 +0100

    contracts: `is_contract(address)` and `caller_is_origin()` are added to API (#10789)
    * is_contract() and caller_is_origin() added to Ext API
    
    * is_contract() exposed in wasm runtime.rs
    
    * + test for is_contract()
    
    * + seal_is_contract benchmark
    
    * caller_is_origin() exposed to wasm/runtime.rs and covered by a test
    
    * + seal_caller_is_origin benchmark
    
    * Update frame/contracts/src/exec.rs
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Update frame/contracts/src/exec.rs
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Update frame/contracts/src/exec.rs
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Update frame/contracts/src/wasm/runtime.rs
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Update frame/contracts/src/wasm/runtime.rs
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Update frame/contracts/src/wasm/runtime.rs
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Update frame/contracts/src/exec.rs
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * identation fix for benchmark macroses; test cosmetic improvement
    
    * benchmark fix
    
    * + is_contract() wasm test
    
    * + caller_is_origin() wasm test
    
    * Apply suggestions from code review
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * is_contract() to borrow param instead of taking ownership
    
    * phrasing improved
    
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>
    
    * fixed wasm tests according to @athei feedback
    
    * dead code warnings suppressed by unstable-interface attributes
    
    * cargo run --quiet --profile=production  --features=runtime-benchmarks --manifest-path=bin/node/cli/Cargo.toml -- benchmark --chain=dev --steps=50 --repeat=20 --pallet=pallet_contracts --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --output=./frame/contracts/src/weights.rs --template=./.maintain/frame-weight-template.hbs
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>
    Co-authored-by: Parity Bot <admin@parity.io>

--- frame/contracts/src/schedule.rs
+++ frame/contracts/src/schedule.rs
@@ -256,6 +256,12 @@ pub struct HostFnWeights<T: Config> {
 	/// Weight of calling `seal_caller`.
 	pub caller: Weight,
 
+	/// Weight of calling `seal_is_contract`.
+	pub is_contract: Weight,
+
+	/// Weight of calling `seal_caller_is_origin`.
+	pub caller_is_origin: Weight,
+
 	/// Weight of calling `seal_address`.
 	pub address: Weight,
 
@@ -571,6 +577,8 @@ impl<T: Config> Default for HostFnWeights<T> {
 	fn default() -> Self {
 		Self {
 			caller: cost_batched!(seal_caller),
+			is_contract: cost_batched!(seal_is_contract),
+			caller_is_origin: cost_batched!(seal_caller_is_origin),
 			address: cost_batched!(seal_address),
 			gas_left: cost_batched!(seal_gas_left),
 			balance: cost_batched!(seal_balance),
