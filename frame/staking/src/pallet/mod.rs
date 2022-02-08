703ba255c50f9f87442c3d29d9ad783119bcc6ce
Ross Bulat <ross@jkrbinvestments.com>
2022-02-08 19:57:30 +0000

    commission-changed-event-and-deposit

--- frame/staking/src/pallet/mod.rs
+++ frame/staking/src/pallet/mod.rs
@@ -609,6 +609,8 @@ pub mod pallet {
 		Chilled(T::AccountId),
 		/// The stakers' rewards are getting paid. \[era_index, validator_stash\]
 		PayoutStarted(EraIndex, T::AccountId),
+		/// A validator has updated their commission
+		CommissionChanged(T::AccountId, Perbill),
 	}
 
 	#[pallet::error]
@@ -982,7 +984,13 @@ pub mod pallet {
 			}
 
 			Self::do_remove_nominator(stash);
-			Self::do_add_validator(stash, prefs);
+			Self::do_add_validator(stash, prefs.clone());
+
+			Self::deposit_event(Event::CommissionChanged(
+				ledger.stash, 
+				prefs.commission
+			));
+
 			Ok(())
 		}
 
