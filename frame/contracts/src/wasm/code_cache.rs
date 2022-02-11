665fc06def5a5aaba8c2661597f2add7bc3991ce
Yarik Bratashchuk <yarik.bratashchuk@gmail.com>
2022-02-11 09:46:51 +0200

    Upgradable contracts using `set_code` function (#10690)
    * poc logic
    
    * set_code_hash impl, tests, benchmark
    
    * Address @xgreenx's comments
    
    * Move func defs closer to set_storage
    
    * Check if code exists
    
    - increment/decrement codehash refcount
    
    * Document error for non-existing code hash
    
    * Revert unrelated change
    
    * Changes due to @athei's review
    
    * Fix error handling
    
    - comment errors: ReturnCodes
    - update mock ext implementation
    - return Error::CodeNotFound when no code for such hash
    
    * Emit ContractCodeUpdated when setting new code_hash
    
    * Address @athei's comments
    
    * Move related defs to the bottom
    
    * Minor comment update
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Improve docs
    
    * Improve docs
    
    * Update frame/contracts/src/wasm/runtime.rs
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Refactor set_code_hash test
    
    * Minor change to benchmark
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Minor change to benchmark
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Minor comment refactor
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    
    * Address @HCastano's comments
    
    * Update seal_set_code_hash comment
    
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>
    
    * Move set_code_hash after delegate_call
    
    * Move function to the bottom
    
    * Moved and changed banchmark, added verify block
    
    * Bring back previous benchmark
    
    * Remove skip_meta for seal_set_code_hash
    
    * Bring back skip_meta for seal_set_storage_per_new_kb
    
    * Apply weights
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    Co-authored-by: Hernando Castano <HCastano@users.noreply.github.com>

--- frame/contracts/src/wasm/code_cache.rs
+++ frame/contracts/src/wasm/code_cache.rs
@@ -117,6 +117,22 @@ pub fn decrement_refcount<T: Config>(code_hash: CodeHash<T>) -> Result<(), Dispa
 	Ok(())
 }
 
+/// Increment the refcount of a code in-storage by one.
+///
+/// # Errors
+///
+/// [`Error::CodeNotFound`] is returned if the specified `code_hash` does not exist.
+pub fn increment_refcount<T: Config>(code_hash: CodeHash<T>) -> Result<(), DispatchError> {
+	<OwnerInfoOf<T>>::mutate(code_hash, |existing| -> Result<(), DispatchError> {
+		if let Some(info) = existing {
+			info.refcount = info.refcount.saturating_add(1);
+			Ok(())
+		} else {
+			Err(Error::<T>::CodeNotFound.into())
+		}
+	})
+}
+
 /// Try to remove code together with all associated information.
 pub fn try_remove<T: Config>(origin: &T::AccountId, code_hash: CodeHash<T>) -> DispatchResult {
 	<OwnerInfoOf<T>>::try_mutate_exists(&code_hash, |existing| {
