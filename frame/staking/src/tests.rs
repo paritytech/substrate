518fca421b38cd54eb36e1d8229892270ffdb42d
emostov <32168567+emostov@users.noreply.github.com>
2022-03-01 17:50:20 -0800

    Allow trailing commas for `bounded_vec!`

--- frame/staking/src/tests.rs
+++ frame/staking/src/tests.rs
@@ -1480,7 +1480,7 @@ fn rebond_is_fifo() {
 				active: 300,
 				unlocking: bounded_vec![
 					UnlockChunk { value: 400, era: 2 + 3 },
-					UnlockChunk { value: 300, era: 3 + 3 }
+					UnlockChunk { value: 300, era: 3 + 3 },
 				],
 				claimed_rewards: vec![],
 			})
@@ -1499,7 +1499,7 @@ fn rebond_is_fifo() {
 				unlocking: bounded_vec![
 					UnlockChunk { value: 400, era: 2 + 3 },
 					UnlockChunk { value: 300, era: 3 + 3 },
-					UnlockChunk { value: 200, era: 4 + 3 }
+					UnlockChunk { value: 200, era: 4 + 3 },
 				],
 				claimed_rewards: vec![],
 			})
@@ -1515,7 +1515,7 @@ fn rebond_is_fifo() {
 				active: 500,
 				unlocking: bounded_vec![
 					UnlockChunk { value: 400, era: 2 + 3 },
-					UnlockChunk { value: 100, era: 3 + 3 }
+					UnlockChunk { value: 100, era: 3 + 3 },
 				],
 				claimed_rewards: vec![],
 			})
