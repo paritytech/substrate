db68fcd1663b6f3a4013e3d308eb6767a8f4f888
Shawn Tabrizi <shawntabrizi@gmail.com>
2022-03-02 17:28:14 -0400

    more ui fixes

--- frame/support/test/tests/pallet_ui/hooks_invalid_item.rs
+++ frame/support/test/tests/pallet_ui/hooks_invalid_item.rs
@@ -1,6 +1,6 @@
 #[frame_support::pallet]
 mod pallet {
-	use frame_support::pallet_prelude::{Hooks, Weight};
+	use frame_support::pallet_prelude::Hooks;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {}
