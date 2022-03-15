e4c0d05052b535b07536905219db6f090acf87fa
Nathan Whitaker <nathan.whitaker01@gmail.com>
2022-03-14 20:03:22 -0400

    Reduce penalty for sending duplicate block request

--- client/network/src/block_request_handler.rs
+++ client/network/src/block_request_handler.rs
@@ -53,7 +53,7 @@ mod rep {
 	use super::ReputationChange as Rep;
 
 	/// Reputation change when a peer sent us the same request multiple times.
-	pub const SAME_REQUEST: Rep = Rep::new_fatal("Same block request multiple times");
+	pub const SAME_REQUEST: Rep = Rep::new(-(1 << 29), "Same block request multiple times");
 }
 
 /// Generates a [`ProtocolConfig`] for the block request protocol, refusing incoming requests.
