cf69667d603943badcf7a17e67cbb0a727a65ac0
Georges <georges.dib@gmail.com>
2022-03-16 21:27:19 +0000

    Moving `NposSolution` to frame (#11031)
    * Move `sp-npos-elections-solution-type`
    to `frame-election-provider-support`
    First stab at it, will need to amend some more stuff
    
    * Fixing tests
    
    * Fixing tests
    
    * Fixing cargo.toml for std configuration
    
    * fmt
    
    * Committing suggested changes
    renaming, and re exporting macro.
    
    * Removing unneeded imports
    
    * Move `NposSolution` to frame
    
    * Removing `npos_election` dependencies
    Implementing _fpes better
    
    * some feedback for moving NPoSSolution to frame
    
    * fmt
    
    * more formatting
    
    * Fixed some imports and fmt
    
    * Fixing docs
    
    Co-authored-by: kianenigma <kian@parity.io>

--- primitives/npos-elections/src/lib.rs
+++ primitives/npos-elections/src/lib.rs
@@ -100,26 +100,16 @@ pub mod pjr;
 pub mod reduce;
 pub mod traits;
 
-pub use assignments::{Assignment, IndexAssignment, IndexAssignmentOf, StakedAssignment};
+pub use assignments::{Assignment, StakedAssignment};
 pub use balancing::*;
 pub use helpers::*;
 pub use phragmen::*;
 pub use phragmms::*;
 pub use pjr::*;
 pub use reduce::reduce;
-pub use traits::{IdentifierT, NposSolution, PerThing128, __OrInvalidIndex};
+pub use traits::{IdentifierT, PerThing128};
 
-// re-export for the solution macro, with the dependencies of the macro.
-#[doc(hidden)]
-pub use codec;
-#[doc(hidden)]
-pub use scale_info;
-#[doc(hidden)]
-pub use sp_arithmetic;
-#[doc(hidden)]
-pub use sp_std;
-
-/// The errors that might occur in the this crate and solution-type.
+/// The errors that might occur in this crate and `frame-election-provider-solution-type`.
 #[derive(Eq, PartialEq, RuntimeDebug)]
 pub enum Error {
 	/// While going from solution indices to ratio, the weight of all the edges has gone above the
