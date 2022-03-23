096f205d82303a889cf566792262e8ffe61bb7fb
Georges <georges.dib@gmail.com>
2022-03-23 09:14:44 +0000

    Implementing `MaxEncodedLen` for `generate_solution_type` (#11032)
    * Move `sp-npos-elections-solution-type`
    to `frame-election-provider-support`
    First stab at it, will need to amend some more stuff
    
    * Fixing tests
    
    * Fixing tests
    
    * Fixing cargo.toml for std configuration
    
    * Implementing `MaxEncodedLen`
    on `generate_solution_type`
    
    * Full implementation of `max_encoded_len`
    
    * Fixing implementation bug
    adding some comments and documentation
    
    * fmt
    
    * Committing suggested changes
    renaming, and re exporting macro.
    
    * Removing unneeded imports
    
    * Implementing `MaxEncodedLen`
    on `generate_solution_type`
    
    * Full implementation of `max_encoded_len`
    
    * Fixing implementation bug
    adding some comments and documentation
    
    * Move `NposSolution` to frame
    
    * Implementing `MaxEncodedLen`
    on `generate_solution_type`
    
    * Full implementation of `max_encoded_len`
    
    * Fixing implementation bug
    adding some comments and documentation
    
    * Fixing test
    
    * Removing unneeded dependencies
    
    * `VoterSnapshotPerBlock` -> `MaxElectingVoters`
    
    * rename `SizeBound` to `MaxVoters`
    
    * Removing TODO and change bound
    
    * renaming `size_bound` to `max_voters`
    
    * Enabling tests for `solution-type`
    These got dropped off after the crate was moved from `sp_npos_elections`
    
    * Adding tests for `MaxEncodedLen` of solution_type
    
    * Better rustdocs. Better indenting and comments.
    Removing unneeded imports in tests.

--- frame/election-provider-support/solution-type/tests/ui/fail/missing_size_bound.rs
+++ frame/election-provider-support/solution-type/tests/ui/fail/missing_size_bound.rs
@@ -1,10 +1,10 @@
 use frame_election_provider_solution_type::generate_solution_type;
 
 generate_solution_type!(pub struct TestSolution::<
-	u16,
+	VoterIndex = u16,
 	TargetIndex = u8,
 	Accuracy = Perbill,
-	MaxVoters = ConstU32::<10>,
+	ConstU32::<10>,
 >(8));
 
 fn main() {}
