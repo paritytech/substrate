3610b9a1b28360f6035e947995f2866fce96d8d5
Georges Dib <georges.dib@gmail.com>
2022-04-05 22:25:46 +0100

    Some more attempts at `mms`

--- primitives/npos-elections/src/mms.rs
+++ primitives/npos-elections/src/mms.rs
@@ -19,10 +19,14 @@
 //!
 //! This is algorithm 1 from the paper: <https://arxiv.org/pdf/2004.12990.pdf>
 //! This is explained a bit further in issue <https://github.com/paritytech/substrate/issues/6639>
+//!
+//! This is also further explained in [The Maximin Support Method: An Extension of the D'Hondt Method to Approval-Based Multiwinner Elections](https://arxiv.org/abs/1609.05370).
 
 use crate::{
-	balance, setup_inputs, ElectionResult, ExtendedBalance, IdentifierT, PerThing128, VoteWeight,
+	balance, setup_inputs, Assignment, ElectionResult, ExtendedBalance, IdentifierT, PerThing128,
+	VoteWeight,
 };
+use core::ops::Add;
 
 /// Execute the mms method.
 ///
@@ -45,20 +49,41 @@ use crate::{
 /// assignments, `assignment.distribution.map(|p| p.deconstruct()).sum()` fails to fit inside
 /// `UpperOf<P>`. A user of this crate may statically assert that this can never happen and safely
 /// `expect` this to return `Ok`.
-pub fn mms<AccountId: IdentifierT, P: PerThing128>(
+pub fn mms<AccountId: IdentifierT, P: PerThing128 + Add<Output = P>>(
 	to_elect: usize,
 	candidates: Vec<AccountId>,
 	voters: Vec<(AccountId, VoteWeight, impl IntoIterator<Item = AccountId>)>,
 	iterations: usize,
 	tolerance: ExtendedBalance,
 ) -> Result<ElectionResult<AccountId, P>, crate::Error> {
-	//let (candidates, mut voters) = setup_inputs(candidates, voters);
-
 	let mut winners = vec![];
-	for round in 0..to_elect {
-		for candidate in candidates {
-            winners.push(candidate);
-			balance(&mut voters, iterations, tolerance);
+	for _round in 0..to_elect {
+		let mut max_support = P::zero();
+		let mut winner;
+		for candidate in &candidates {
+			let mut augmented_winners = winners.clone();
+			augmented_winners.push(candidate);
+			let (_, mut used_voters) = setup_inputs(augmented_winners, voters);
+			let mut support = P::zero();
+			balance(&mut used_voters, iterations, tolerance);
+			for Assignment { who, distribution } in
+				used_voters.into_iter().filter_map(|v| v.into_assignment::<P>())
+			{
+				for (c, weight_extended) in distribution.into_iter() {
+					if c == candidate {
+						support = support.add(weight_extended);
+					}
+				}
+			}
+
+			if support > max_support {
+				max_support = support;
+				winner = candidate;
+			}
+		}
+
+		if !max_support.is_zero() {
+			winners.push(winner);
 		}
 	}
 	Err(crate::Error::SolutionWeightOverflow)
