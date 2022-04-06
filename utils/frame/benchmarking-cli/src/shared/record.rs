6466c4e89ba6fa990b1957ec5cc188ce040c50cc
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-04-06 18:53:36 +0200

    Review fixes
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- utils/frame/benchmarking-cli/src/shared/record.rs
+++ utils/frame/benchmarking-cli/src/shared/record.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Defines the [`BenchRecord`] and calculates [`super::Stats`] from it.
+//! Defines the [`BenchRecord`] and its facilities for computing [`super::Stats`].
 
 use sc_cli::Result;
 use sc_service::Configuration;
