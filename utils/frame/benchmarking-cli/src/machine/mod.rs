0e8bfa2cb766e823c9e3b006a6db8f212b784049
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-04-11 13:54:25 +0200

    Use sc-sysinfo
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- utils/frame/benchmarking-cli/src/machine/mod.rs
+++ utils/frame/benchmarking-cli/src/machine/mod.rs
@@ -15,40 +15,49 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Contains [`MachineCmd`] as entry point for the node
+//! Contains the [`MachineCmd`] as entry point for the node
 //! and the core benchmarking logic.
 
 use sc_cli::{CliConfiguration, Result, SharedParams};
-use sp_core::{blake2_256, sr25519, Pair};
+use sc_sysinfo::gather_hwbench;
+use sp_core::{sr25519, Pair};
 use sp_io::crypto::sr25519_verify;
 use sp_std::prelude::*;
 
 use clap::Parser;
 use log::info;
-use prettytable::{cell, row, table, Table};
+use prettytable::{cell, row, table};
 use rand::RngCore;
 use std::{
 	fmt::Debug,
 	time::{Duration, Instant},
 };
-use thousands::Separable;
+use tempfile::tempdir;
 
 use crate::shared::new_rng;
 
 /// Command to benchmark the hardware.
 ///
-/// The implementation of this command is currently a place holder until
-/// <https://github.com/paritytech/substrate/pull/11062> is merged.
+/// Runs multiple benchmarks and prints their output to console.
+/// Can be used to gauge if the hardware is fast enough to keep up with a chain's requirements.
+/// This command must be integrated by the client since the client can set compiler flags
+/// which influence the results.
 ///
 /// The output or `benchmark machine --dev` looks like this:
 /// ```text
-/// +----------------+------------------+------------+----------+------------+
-/// | Function       | Input len [byte] | Iterations | Time [s] | Per second |
-/// +----------------+------------------+------------+----------+------------+
-/// | BLAKE2-256     | 32               | 10,000,000 | 2.061    | 4,851,958  |
-/// +----------------+------------------+------------+----------+------------+
-/// | SR25519 verify | 32               | 20,000     | 1.489    | 13,429     |
-/// +----------------+------------------+------------+----------+------------+
+/// +----------+----------------+-------+------+
+/// | Category | Function       | Score | Unit |
+/// +----------+----------------+-------+------+
+/// | Memory   | Copy           | 9082  | MB/s |
+/// +----------+----------------+-------+------+
+/// | Disk     | Seq Write      | 724   | MB/s |
+/// +----------+----------------+-------+------+
+/// | Disk     | Rnd Write      | 362   | MB/s |
+/// +----------+----------------+-------+------+
+/// | CPU      | BLAKE2-256     | 816   | MB/s |
+/// +----------+----------------+-------+------+
+/// | CPU      | SR25519 Verify | 13.52 | K/s  |
+/// +----------+----------------+-------+------+
 /// ```
 #[derive(Debug, Parser)]
 pub struct MachineCmd {
@@ -56,49 +65,38 @@ pub struct MachineCmd {
 	#[clap(flatten)]
 	pub shared_params: SharedParams,
 
-	/// Iterations of the hash function.
-	#[clap(long, default_value = "10000000")]
-	pub hash_reps: u64,
-
 	/// Iterations of the verification function.
 	#[clap(long, default_value = "20000")]
 	pub verify_reps: u64,
 }
 
 impl MachineCmd {
-	/// Execute the benchmark and log the results.
+	/// Execute the benchmark and print the results.
 	pub fn run(&self) -> Result<()> {
+		info!("Running machine benchmarks...");
+		let tmp_dir = tempdir()?;
+		let info = gather_hwbench(Some(tmp_dir.path()));
+
 		// Use a table for nicer console output.
-		let mut table =
-			table!(["Function", "Input len [byte]", "Iterations", "Time [s]", "Per second"]);
+		let mut table = table!(
+			["Category", "Function", "Score", "Unit"],
+			["Memory", "Copy", info.memory_memcpy_score, "MB/s"],
+			["Disk", "Seq Write", info.disk_sequential_write_score.unwrap_or_default(), "MB/s"],
+			["Disk", "Rnd Write", info.disk_random_write_score.unwrap_or_default(), "MB/s"],
+			["CPU", "BLAKE2-256", info.cpu_hashrate_score, "MB/s"]
+		);
 
-		let input_len = 32_usize;
-		// Benchmark the BLAKE2-256 hashing speed.
-		let name = "BLAKE2-256";
-		let duration = Self::blake2_speed(self.hash_reps, input_len);
-		Self::put_col(&mut table, name, self.hash_reps, input_len, duration);
-
-		// Benchmark the speed of SR2519 verification.
-		let name = "SR25519 verify";
-		let duration = Self::sr_verify_speed(self.verify_reps, input_len);
-		Self::put_col(&mut table, name, self.verify_reps, input_len, duration);
+		// Additionally measure the speed of SR25519::Verify for 32 byte input.
+		let input_len = 32;
+		let took = Self::sr_verify_speed(self.verify_reps, input_len as usize);
+		let speed = (self.verify_reps as f64 / took.as_secs_f64()) / 1000.0;
+		table.add_row(row!["CPU", "SR25519 Verify", format!("{:.2}", speed), "K/s"]);
 
 		info!("\n{}", table);
 		Ok(())
 	}
 
-	/// Recursively hashes a value `reps` many times.
-	fn blake2_speed(reps: u64, input_len: usize) -> Duration {
-		let mut data = vec![0u8; input_len];
-
-		let start = Instant::now();
-		for _ in 0..reps {
-			data = blake2_256(&data).into();
-		}
-		start.elapsed()
-	}
-
-	/// Verify the signature of different random data `reps` many times.
+	/// Verify signatures of different random data `reps` many times.
 	fn sr_verify_speed(reps: u64, input_len: usize) -> Duration {
 		let pair = sr25519::Pair::from_string("//Alice", None).unwrap();
 
@@ -121,19 +119,6 @@ impl MachineCmd {
 		}
 
 		start.elapsed()
-	}
-
-	/// Format the data and put it as column into the table.
-	fn put_col(table: &mut Table, name: &str, reps: u64, size: usize, d: Duration) {
-		let d = d.as_secs_f32();
-		let s = (reps as f32 / d) as u64;
-		table.add_row(row![
-			name,
-			size,
-			reps.separate_with_commas(),
-			format!("{:.3}", d),
-			s.separate_with_commas()
-		]);
 	}
 }
 
