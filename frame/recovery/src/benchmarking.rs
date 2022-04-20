0042a97b5c1948f834bbb4ff0f0f25cff8a39e54
Ayevbeosa Iyamu <ayevbeosa.j@gmail.com>
2022-04-20 18:46:25 +0100

    Set vec to be mut

--- frame/recovery/src/benchmarking.rs
+++ frame/recovery/src/benchmarking.rs
@@ -48,7 +48,7 @@ fn get_total_deposit<T: Config>(
 
 fn generate_friends<T: Config>(num: u32) -> Vec<<T as frame_system::Config>::AccountId> {
 	// Create friends
-	let friends = (0..num).map(|x| account("friend", i, SEED)).collect::<Vec<_>>();
+	let mut friends = (0..num).map(|x| account("friend", x, SEED)).collect::<Vec<_>>();
 	// Sort
 	friends.sort();
 
