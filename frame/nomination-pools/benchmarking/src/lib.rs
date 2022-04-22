f77dfc879d76c05b486b3bcdb2590768fa5bd14f
emostov <32168567+emostov@users.noreply.github.com>
2022-04-21 17:49:41 -0700

    Fix asserts to work in more scenarios

--- frame/nomination-pools/benchmarking/src/lib.rs
+++ frame/nomination-pools/benchmarking/src/lib.rs
@@ -247,8 +247,8 @@ frame_benchmarking::benchmarks! {
 
 	}: bond_extra(Origin::Signed(scenario.creator1.clone()), BondExtra::FreeBalance(extra))
 	verify {
-		assert_eq!(
-			T::StakingInterface::active_stake(&scenario.origin1).unwrap(),
+		assert!(
+			T::StakingInterface::active_stake(&scenario.origin1).unwrap() >
 			scenario.dest_weight
 		);
 	}
@@ -267,8 +267,8 @@ frame_benchmarking::benchmarks! {
 
 	}: bond_extra(Origin::Signed(scenario.creator1.clone()), BondExtra::Rewards)
 	verify {
-		assert_eq!(
-			T::StakingInterface::active_stake(&scenario.origin1).unwrap(),
+		assert!(
+			T::StakingInterface::active_stake(&scenario.origin1).unwrap() >
 			scenario.dest_weight
 		);
 	}
