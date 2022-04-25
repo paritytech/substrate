14d965bc3fed43b911666f4482efbf367bec1823
tgmichel <telmo@purestake.com>
2022-04-25 21:07:26 +0200

    Add `chain-info` Subcommand (#11250)
    * Add `blockchain-info` Subcommand
    
    * Update comment
    
    * Cleanup
    
    * Cleanup
    
    * Use `sync_run`
    
    * Use `sc_client_db` utility fns instead service backend
    
    * Use service `Backend` builder
    
    * Impl `From<sp_blockchain::Info>`
    
    * Rename to `chain-info`
    
    * fmt
    
    * Copyright year
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Expose `DatabaseParams`
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- bin/node-template/node/src/command.rs
+++ bin/node-template/node/src/command.rs
@@ -162,6 +162,10 @@ pub fn run() -> sc_cli::Result<()> {
 		Some(Subcommand::TryRuntime) => Err("TryRuntime wasn't enabled when building the node. \
 				You can enable it with `--features try-runtime`."
 			.into()),
+		Some(Subcommand::ChainInfo(cmd)) => {
+			let runner = cli.create_runner(cmd)?;
+			runner.sync_run(|config| cmd.run::<Block>(&config))
+		},
 		None => {
 			let runner = cli.create_runner(&cli.run)?;
 			runner.run_node_until_exit(|config| async move {
