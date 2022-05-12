6a1417d551f90df8907f373430e55f5ac48ec9c2
Jan Bujak <jan@parity.io>
2022-05-12 09:08:39 +0000

    Make sure the commit hash is always of the same length in `impl_version`

--- utils/build-script-utils/src/version.rs
+++ utils/build-script-utils/src/version.rs
@@ -22,7 +22,7 @@ pub fn generate_cargo_keys() {
 	let commit = if let Ok(hash) = std::env::var("SUBSTRATE_CLI_GIT_COMMIT_HASH") {
 		Cow::from(hash.trim().to_owned())
 	} else {
-		match Command::new("git").args(&["rev-parse", "--short", "HEAD"]).output() {
+		match Command::new("git").args(&["rev-parse", "--short=11", "HEAD"]).output() {
 			Ok(o) if o.status.success() => {
 				let sha = String::from_utf8_lossy(&o.stdout).trim().to_owned();
 				Cow::from(sha)
