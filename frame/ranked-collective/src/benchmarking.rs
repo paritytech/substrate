bf125a88a9f39436509a3d24d92884f2e3e4b041
Gav Wood <gavin@parity.io>
2022-05-18 17:14:09 +0200

    benchmarks

--- frame/ranked-collective/src/benchmarking.rs
+++ frame/ranked-collective/src/benchmarking.rs
@@ -24,7 +24,7 @@ use sp_runtime::traits::Bounded;
 use sp_std::mem::size_of;
 
 use frame_benchmarking::{account, benchmarks_instance_pallet, whitelisted_caller};
-use frame_support::dispatch::UnfilteredDispatchable;
+use frame_support::{assert_ok, dispatch::UnfilteredDispatchable};
 use frame_system::{Call as SystemCall, Pallet as System, RawOrigin as SystemOrigin};
 
 const SEED: u32 = 0;
@@ -37,7 +37,7 @@ fn assert_last_event<T: Config<I>, I: 'static>(generic_event: <T as Config<I>>::
 
 fn make_member<T: Config<I>, I: 'static>(rank: Rank) -> T::AccountId {
 	let who = account::<T::AccountId>("member", MemberCount::<T, I>::get().0, SEED);
-	Pallet::<T, I>::add_member(T::AdminOrigin::successful_origin(), who.clone(), rank);
+	assert_ok!(Pallet::<T, I>::add_member(T::AdminOrigin::successful_origin(), who.clone(), rank));
 	who
 }
 
@@ -79,6 +79,48 @@ benchmarks_instance_pallet! {
 	verify {
 		assert_eq!(Members::<T, I>::get(&who).unwrap().rank, rank);
 		assert_last_event::<T, I>(Event::RankChanged { who, rank }.into());
+	}
+
+	vote {
+		let rank = 1;
+		let caller: T::AccountId = whitelisted_caller();
+		Pallet::<T, I>::add_member(T::AdminOrigin::successful_origin(), caller.clone(), rank);
+		// Create a poll
+		let class = T::Polls::classes().into_iter().next().expect("Must always be at least one class");
+		let poll = T::Polls::create_ongoing(class).expect("Must always be able to create a poll");
+
+		// Vote once.
+		assert_ok!(Pallet::<T, I>::vote(SystemOrigin::Signed(caller.clone()).into(), poll, true));
+	}: _(SystemOrigin::Signed(caller.clone()), poll, false)
+	verify {
+		let tally = Tally::from_parts(0, 1);
+		let ev = Event::Voted { who: caller, poll, vote: VoteRecord::Nay(1), tally };
+		assert_last_event::<T, I>(ev.into());
+	}
+
+	cleanup_poll {
+		let rank = 1;
+		let n in 1 .. 100;
+
+		dbg!(n);
+		// Create a poll
+		let class = T::Polls::classes().into_iter().next().expect("Must always be at least one class");
+		let poll = T::Polls::create_ongoing(class).expect("Must always be able to create a poll");
+
+		// Vote in the poll by each of `n` members
+		for i in 0..n {
+			let who = make_member::<T, I>(rank);
+			assert_ok!(Pallet::<T, I>::vote(SystemOrigin::Signed(who).into(), poll, true));
+		}
+
+		// End the poll.
+		T::Polls::end_ongoing(poll, false).expect("Must always be able to end a poll");
+
+		assert_eq!(Voting::<T, I>::iter_prefix(poll).count(), n as usize);
+		dbg!(Voting::<T, I>::iter_prefix(poll).count());
+	}: _(SystemOrigin::Signed(whitelisted_caller()), poll, n)
+	verify {
+		assert_eq!(Voting::<T, I>::iter().count(), 0);
 	}
 
 	impl_benchmark_test_suite!(Collective, crate::tests::new_test_ext(), crate::tests::Test);
