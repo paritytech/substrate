a6afb06c11180ae4dd0f3d58c3b38ad7c8cf6cf8
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2022-05-23 13:12:17 +0100

    Update frame/nomination-pools/src/lib.rs
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/nomination-pools/src/lib.rs
+++ frame/nomination-pools/src/lib.rs
@@ -1854,7 +1854,7 @@ pub mod pallet {
 
 		/// Nominate on behalf of the pool.
 		///
-		/// The dispatch origin of this call must be signed by the pool nominator or the the pool
+		/// The dispatch origin of this call must be signed by the pool nominator or the pool
 		/// root role.
 		///
 		/// This directly forward the call to the staking pallet, on behalf of the pool bonded
