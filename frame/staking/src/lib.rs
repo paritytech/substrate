274a334b17e8c6ef48115ce6455a300496ea1507
Shawn Tabrizi <shawntabrizi@gmail.com>
2022-06-06 15:28:13 +0100

    fmt

--- frame/staking/src/lib.rs
+++ frame/staking/src/lib.rs
@@ -778,8 +778,8 @@ impl<Balance: Default> EraPayout<Balance> for () {
 /// Adaptor to turn a `PiecewiseLinear` curve definition into an `EraPayout` impl, used for
 /// backwards compatibility.
 pub struct ConvertCurve<T>(sp_std::marker::PhantomData<T>);
-impl<Balance: AtLeast32BitUnsigned + Clone, T: Get<&'static PiecewiseLinear>>
-	EraPayout<Balance> for ConvertCurve<T>
+impl<Balance: AtLeast32BitUnsigned + Clone, T: Get<&'static PiecewiseLinear>> EraPayout<Balance>
+	for ConvertCurve<T>
 {
 	fn era_payout(
 		total_staked: Balance,
