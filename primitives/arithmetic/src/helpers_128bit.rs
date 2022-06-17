61f99d96f6c409b700d06d1f5de976fa6b9fe36e
Boluwatife Bakre <tifebakre@yahoo.co.uk>
2022-06-17 04:57:29 +0100

    Remove multiply_by_rational  (#11598)
    * Removed multiply_by_rational
    Replaced with multiply_by_rational_with_rounding
    
    * fixes
    
    * Test Fixes
    
    * nightly fmt
    
    * Test Fix
    
    * Fixed fuzzer.

--- primitives/arithmetic/src/helpers_128bit.rs
+++ primitives/arithmetic/src/helpers_128bit.rs
@@ -22,12 +22,7 @@
 //! multiplication implementation provided there.
 
 use crate::{biguint, Rounding};
-use num_traits::Zero;
-use sp_std::{
-	cmp::{max, min},
-	convert::TryInto,
-	mem,
-};
+use sp_std::cmp::{max, min};
 
 /// Helper gcd function used in Rational128 implementation.
 pub fn gcd(a: u128, b: u128) -> u128 {
@@ -59,65 +54,6 @@ pub fn to_big_uint(x: u128) -> biguint::BigUint {
 	let mut n = biguint::BigUint::from_limbs(&[xhh, xhl, xlh, xll]);
 	n.lstrip();
 	n
-}
-
-/// Safely and accurately compute `a * b / c`. The approach is:
-///   - Simply try `a * b / c`.
-///   - Else, convert them both into big numbers and re-try. `Err` is returned if the result cannot
-///     be safely casted back to u128.
-///
-/// Invariant: c must be greater than or equal to 1.
-pub fn multiply_by_rational(mut a: u128, mut b: u128, mut c: u128) -> Result<u128, &'static str> {
-	if a.is_zero() || b.is_zero() {
-		return Ok(Zero::zero())
-	}
-	c = c.max(1);
-
-	// a and b are interchangeable by definition in this function. It always helps to assume the
-	// bigger of which is being multiplied by a `0 < b/c < 1`. Hence, a should be the bigger and
-	// b the smaller one.
-	if b > a {
-		mem::swap(&mut a, &mut b);
-	}
-
-	// Attempt to perform the division first
-	if a % c == 0 {
-		a /= c;
-		c = 1;
-	} else if b % c == 0 {
-		b /= c;
-		c = 1;
-	}
-
-	if let Some(x) = a.checked_mul(b) {
-		// This is the safest way to go. Try it.
-		Ok(x / c)
-	} else {
-		let a_num = to_big_uint(a);
-		let b_num = to_big_uint(b);
-		let c_num = to_big_uint(c);
-
-		let mut ab = a_num * b_num;
-		ab.lstrip();
-		let mut q = if c_num.len() == 1 {
-			// PROOF: if `c_num.len() == 1` then `c` fits in one limb.
-			ab.div_unit(c as biguint::Single)
-		} else {
-			// PROOF: both `ab` and `c` cannot have leading zero limbs; if length of `c` is 1,
-			// the previous branch would handle. Also, if ab for sure has a bigger size than
-			// c, because `a.checked_mul(b)` has failed, hence ab must be at least one limb
-			// bigger than c. In this case, returning zero is defensive-only and div should
-			// always return Some.
-			let (mut q, r) = ab.div(&c_num, true).unwrap_or((Zero::zero(), Zero::zero()));
-			let r: u128 = r.try_into().expect("reminder of div by c is always less than c; qed");
-			if r > (c / 2) {
-				q = q.add(&to_big_uint(1));
-			}
-			q
-		};
-		q.lstrip();
-		q.try_into().map_err(|_| "result cannot fit in u128")
-	}
 }
 
 mod double128 {
@@ -247,7 +183,7 @@ mod double128 {
 }
 
 /// Returns `a * b / c` and `(a * b) % c` (wrapping to 128 bits) or `None` in the case of
-/// overflow.
+/// overflow and c = 0.
 pub const fn multiply_by_rational_with_rounding(
 	a: u128,
 	b: u128,
@@ -256,7 +192,7 @@ pub const fn multiply_by_rational_with_rounding(
 ) -> Option<u128> {
 	use double128::Double128;
 	if c == 0 {
-		panic!("attempt to divide by zero")
+		return None
 	}
 	let (result, remainder) = Double128::product_of(a, b).div(c);
 	let mut result: u128 = match result.try_into_u128() {
@@ -361,7 +297,7 @@ mod tests {
 			let b = random_u128(i + (1 << 30));
 			let c = random_u128(i + (1 << 31));
 			let x = mulrat(a, b, c, NearestPrefDown);
-			let y = multiply_by_rational(a, b, c).ok();
+			let y = multiply_by_rational_with_rounding(a, b, c, Rounding::NearestPrefDown);
 			assert_eq!(x.is_some(), y.is_some());
 			let x = x.unwrap_or(0);
 			let y = y.unwrap_or(0);
