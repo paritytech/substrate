54746ca3074f2e298b7878410644d85a80d4a346
Szegoo <sakacszergej@gmail.com>
2022-06-21 12:53:59 +0200

    fix

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -714,6 +714,7 @@ impl pallet_election_provider_multi_phase::Config for Runtime {
 	type MaxElectingVoters = MaxElectingVoters;
 	type BenchmarkingConfig = ElectionProviderBenchmarkConfig;
 	type WeightInfo = pallet_election_provider_multi_phase::weights::SubstrateWeight<Self>;
+	type EmergencyPunishmentMultiple = ConstU32<2>;
 }
 
 parameter_types! {
