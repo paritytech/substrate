51311d18c7cf91d89cd8d1c59c58516fa3b068e6
kianenigma <kian@parity.io>
2022-06-27 23:17:15 +0200

    pushed

--- frame/system/src/lib.rs
+++ frame/system/src/lib.rs
@@ -197,6 +197,7 @@ impl<MaxNormal: Get<u32>, MaxOverflow: Get<u32>> ConsumerLimits for (MaxNormal, 
 pub mod pallet {
 	use crate::{self as frame_system, pallet_prelude::*, *};
 	use frame_support::pallet_prelude::*;
+	use sp_runtime::DispatchErrorWithPostInfo;
 
 	/// System configuration trait. Implemented by runtime.
 	#[pallet::config]
@@ -375,8 +376,11 @@ pub mod pallet {
 				Ok(_) => Ok(().into()),
 				Err(_) => {
 					// roughly same as a 4 byte remark since perbill is u32.
-					Ok(Some(T::SystemWeightInfo::remark(4u32)).into())
-				}
+					Err(DispatchErrorWithPostInfo {
+						post_info: Some(T::SystemWeightInfo::remark(4u32)).into(),
+						error: DispatchError::BadOrigin,
+					})
+				},
 			}
 		}
 
