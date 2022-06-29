e0fada55a9889acb2ede33a9614df442176e779b
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2022-06-29 17:39:56 +0100

    Refund weight in `system::fillBlock` (#11754)
    * fix
    
    * pushed
    
    * node: fix fee multiplier test
    
    Co-authored-by: André Silva <andrerfosilva@gmail.com>

--- bin/node/executor/tests/fees.rs
+++ bin/node/executor/tests/fees.rs
@@ -58,8 +58,10 @@ fn fee_multiplier_increases_and_decreases_on_big_weight() {
 			},
 			CheckedExtrinsic {
 				signed: Some((charlie(), signed_extra(0, 0))),
-				function: Call::System(frame_system::Call::fill_block {
-					ratio: Perbill::from_percent(60),
+				function: Call::Sudo(pallet_sudo::Call::sudo {
+					call: Box::new(Call::System(frame_system::Call::fill_block {
+						ratio: Perbill::from_percent(60),
+					})),
 				}),
 			},
 		],
