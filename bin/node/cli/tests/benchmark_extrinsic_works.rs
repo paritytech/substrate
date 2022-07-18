f603a06e21336c74b2c8cc283aa5b58c6d4bc94a
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-07-18 17:40:42 +0200

    Fix test
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- bin/node/cli/tests/benchmark_extrinsic_works.rs
+++ bin/node/cli/tests/benchmark_extrinsic_works.rs
@@ -18,6 +18,7 @@
 
 use assert_cmd::cargo::cargo_bin;
 use std::process::Command;
+use tempfile::tempdir;
 
 /// Tests that the `benchmark extrinsic` command works for
 /// remark and transfer_keep_alive within the substrate dev runtime.
@@ -32,7 +33,9 @@ fn benchmark_extrinsic(pallet: &str, extrinsic: &str) {
 	let base_dir = tempdir().expect("could not create a temp dir");
 
 	let status = Command::new(cargo_bin("substrate"))
-		.args(&["benchmark", "extrinsic", "--dev"].arg("-d").arg(base_dir.path()))
+		.args(&["benchmark", "extrinsic", "--dev"])
+		.arg("-d")
+		.arg(base_dir.path())
 		.args(&["--pallet", pallet, "--extrinsic", extrinsic])
 		// Run with low repeats for faster execution.
 		.args(["--warmup=10", "--repeat=10", "--max-ext-per-block=10"])
