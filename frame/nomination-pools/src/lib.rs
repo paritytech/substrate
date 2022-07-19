178bfd4706cbf92557f1a875e59191806a5156df
wirednkod <wirednkod@gmail.com>
2022-07-19 10:24:29 +0200

    Add era parameter to Event::Unbonded

--- frame/nomination-pools/src/lib.rs
+++ frame/nomination-pools/src/lib.rs
@@ -1349,6 +1349,7 @@ pub mod pallet {
 		///   pool.
 		/// - `points` is the number of points that are issued as a result of `balance` being
 		/// dissolved into the corresponding unbonding pool.
+		/// - `era` is the era that the Unbond Event took place.
 		///
 		/// In the absence of slashing, these values will match. In the presence of slashing, the
 		/// number of points that are issued in the unbonding pool will be less than the amount
@@ -1358,6 +1359,7 @@ pub mod pallet {
 			pool_id: PoolId,
 			balance: BalanceOf<T>,
 			points: BalanceOf<T>,
+			era: u32,
 		},
 		/// A member has withdrawn from their pool.
 		///
@@ -1683,6 +1685,7 @@ pub mod pallet {
 				pool_id: member.pool_id,
 				points: points_unbonded,
 				balance: unbonding_balance,
+				era: unbond_era,
 			});
 
 			// Now that we know everything has worked write the items to storage.
