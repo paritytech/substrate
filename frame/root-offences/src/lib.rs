4e56c3017612320715b7712a904ab4fb539734b6
Szegoo <sakacszergej@gmail.com>
2022-08-01 10:40:43 +0200

    addidtional cleaning

--- frame/root-offences/src/lib.rs
+++ frame/root-offences/src/lib.rs
@@ -64,8 +64,6 @@ pub mod pallet {
 			FullIdentification = Exposure<<T as frame_system::Config>::AccountId, BalanceOf<T>>,
 			FullIdentificationOf = ExposureOf<T>,
 		>,
-		T::SessionHandler: pallet_session::SessionHandler<<T as frame_system::Config>::AccountId>,
-		T::SessionManager: pallet_session::SessionManager<<T as frame_system::Config>::AccountId>,
 		T::ValidatorIdOf: Convert<
 			<T as frame_system::Config>::AccountId,
 			Option<<T as frame_system::Config>::AccountId>,
