6bfab6496aeb70590cb9b88892a15ea781f4965a
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2022-08-01 11:51:21 +0200

    feat(rpc middleware): use custom time buckets

--- client/rpc-servers/src/middleware.rs
+++ client/rpc-servers/src/middleware.rs
@@ -24,6 +24,10 @@ use prometheus_endpoint::{
 	U64,
 };
 
+/// Histogram time buckets in seconds.
+const HISTOGRAM_BUCKETS: [f64; 12] =
+	[5.0, 25.0, 100.0, 500.0, 1_000.0, 2_500.0, 10_000.0, 25_000.0, 100_000.0, 1_000_000.0, 10_000_000.0];
+
 /// Metrics for RPC middleware storing information about the number of requests started/completed,
 /// calls started/completed and their timings.
 #[derive(Debug, Clone)]
@@ -74,7 +78,8 @@ impl RpcMetrics {
 						HistogramOpts::new(
 							"substrate_rpc_calls_time",
 							"Total time [μs] of processed RPC calls",
-						),
+						)
+						.buckets(HISTOGRAM_BUCKETS.to_vec()),
 						&["protocol", "method"],
 					)?,
 					metrics_registry,
