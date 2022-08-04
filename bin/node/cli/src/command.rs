88e3ef18f9afa820de197cdd2405acb0c92edc8b
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-08-05 00:07:06 +0200

    WIP
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- bin/node/cli/src/command.rs
+++ bin/node/cli/src/command.rs
@@ -16,7 +16,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#[allow(unused)] // @ggwpez FIXME
+#![allow(unused)] // @ggwpez FIXME
+
 use super::benchmarking::{inherent_benchmark_data, RemarkBuilder, TransferKeepAliveBuilder};
 use crate::{
 	chain_spec, service,
