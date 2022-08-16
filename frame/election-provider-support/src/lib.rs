907708de191e618cb9b2e757d5b9602d5cd02a4b
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-08-16 14:45:57 +0200

    Use unimplemented macro instead
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/election-provider-support/src/lib.rs
+++ frame/election-provider-support/src/lib.rs
@@ -519,7 +519,9 @@ pub trait SortedListProvider<AccountId> {
 	/// If `who` changes by the returned amount they are guaranteed to have a worst case change
 	/// in their list position.
 	#[cfg(feature = "runtime-benchmarks")]
-	fn score_update_worst_case(_who: &AccountId, _is_increase: bool) -> Self::Score;
+	fn score_update_worst_case(_who: &AccountId, _is_increase: bool) -> Self::Score {
+		unimplemented!();
+	}
 }
 
 /// Something that can provide the `Score` of an account. Similar to [`ElectionProvider`] and
