e0bf7c7c653910509448b5a5ddf99dd5993434b1
Szegoo <sakacszergej@gmail.com>
2022-08-20 16:42:53 +0300

    new test

--- frame/root-offences/src/tests.rs
+++ frame/root-offences/src/tests.rs
@@ -35,3 +35,27 @@ fn create_offence_works_given_root_origin() {
 		assert_eq!(Balances::free_balance(21), 1000);
 	})
 }
+
+#[test]
+fn create_offence_wont_slash_non_active_validators() {
+	ExtBuilder::default().build_and_execute(|| {
+		start_session(1);
+
+		assert_eq!(active_era(), 0);
+		assert_eq!(current_era(), 0);
+
+		// 31 is not an active validator.
+		assert_eq!(Balances::free_balance(31), 500);
+
+		let offenders = [(31, Perbill::from_percent(20)), (11, Perbill::from_percent(20))].to_vec();
+		assert_ok!(RootOffences::create_offence(Origin::root(), offenders.clone()));
+
+		System::assert_last_event(Event::CreatedOffence { offenders, unapplied_slash: 0 }.into());
+
+		// so 31 didn't get slashed.
+		assert_eq!(Balances::free_balance(31), 500);
+
+		// but 11 is an active validator so he got slashed.
+		assert_eq!(Balances::free_balance(11), 800);
+	})
+}
