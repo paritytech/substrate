7c9c97ad6353b030a56079c8ed5a7ca08ae49244
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2022-08-30 11:10:40 +0200

    Update lib.rs

--- frame/uniques/src/lib.rs
+++ frame/uniques/src/lib.rs
@@ -908,7 +908,8 @@ pub mod pallet {
 
 		/// Approve an item to be transferred by a delegated third-party account.
 		///
-		/// The origin must be Signed and the owner of the `item` or the admin of the collection.
+		/// The origin must conform to `ForceOrigin` or must be `Signed` and the sender must be
+		/// either the owner of the `item` or the admin of the collection.
 		///
 		/// - `collection`: The collection of the item to be approved for delegated transfer.
 		/// - `item`: The item of the item to be approved for delegated transfer.
