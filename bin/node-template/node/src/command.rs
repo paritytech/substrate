6152bc9c52d7fe7cf5ee870507a3212f32956ebb
Stephen Shelton <steve@brewcraft.org>
2022-09-01 11:23:43 -0600

    fmt

--- bin/node-template/node/src/command.rs
+++ bin/node-template/node/src/command.rs
@@ -139,7 +139,13 @@ pub fn run() -> sc_cli::Result<()> {
 						let PartialComponents { client, .. } = service::new_partial(&config)?;
 						let ext_builder = RemarkBuilder::new(client.clone());
 
-						cmd.run(config, client, inherent_benchmark_data()?, Vec::new(), &ext_builder)
+						cmd.run(
+							config,
+							client,
+							inherent_benchmark_data()?,
+							Vec::new(),
+							&ext_builder,
+						)
 					},
 					BenchmarkCmd::Extrinsic(cmd) => {
 						let PartialComponents { client, .. } = service::new_partial(&config)?;
--- bin/node/cli/src/command.rs
+++ bin/node/cli/src/command.rs
@@ -129,7 +129,13 @@ pub fn run() -> Result<()> {
 						let PartialComponents { client, .. } = new_partial(&config)?;
 						let ext_builder = RemarkBuilder::new(client.clone());
 
-						cmd.run(config, client, inherent_benchmark_data()?, Vec::new(), &ext_builder)
+						cmd.run(
+							config,
+							client,
+							inherent_benchmark_data()?,
+							Vec::new(),
+							&ext_builder,
+						)
 					},
 					BenchmarkCmd::Extrinsic(cmd) => {
 						let PartialComponents { client, .. } = service::new_partial(&config)?;
