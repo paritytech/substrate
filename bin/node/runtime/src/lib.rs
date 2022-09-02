cfcbf7fec2fc0d1b0ff9889beafd8f17bdfa3299
Maksim Ramanenkau <cbnhrpo@gmail.com>
2022-09-02 15:56:02 +0400

    Keep old constant value

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -587,8 +587,7 @@ parameter_types! {
 	pub const CandidacyBond: Balance = 100 * DOLLARS;
 	// 1 storage item created, key size is 32 bytes, value size is 16+16.
 	pub const VotingBondBase: Balance = deposit(1, 64);
-	// additional data per vote is 32 bytes (account id).
-	pub const VotingBondFactor: Balance = deposit(0, 32);
+	pub const VotingBondFactor: Balance = 1 * DOLLARS;
 	pub const TermDuration: BlockNumber = 7 * DAYS;
 	pub const DesiredMembers: u32 = 13;
 	pub const DesiredRunnersUp: u32 = 20;
