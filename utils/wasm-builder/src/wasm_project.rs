aea0a41d89cc77d610eaf3e0e15c173f7ad96a42
Alexander Theißen <alex.theissen@me.com>
2022-09-02 16:23:58 +0200

    Use wasm-opt on runtime

--- utils/wasm-builder/src/wasm_project.rs
+++ utils/wasm-builder/src/wasm_project.rs
@@ -669,7 +669,9 @@ fn compact_wasm_file(
 			"{}.compact.wasm",
 			wasm_binary_name.clone().unwrap_or_else(|| default_wasm_binary_name.clone()),
 		));
-		wasm_gc::garbage_collect_file(&wasm_file, &wasm_compact_file)
+		wasm_opt::OptimizationOptions::new_opt_level_4()
+			.zero_filled_memory(true)
+			.run(&wasm_file, Option::<&str>::None, &wasm_compact_file, Option::<&str>::None)
 			.expect("Failed to compact generated WASM binary.");
 		Some(WasmBinary(wasm_compact_file))
 	} else {
