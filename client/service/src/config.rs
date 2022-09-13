197acd01d642bd8b7035b1aa925f1d322b5c1744
Bastian Köcher <info@kchr.de>
2022-09-13 07:12:42 +0100

    Update client/service/src/config.rs
    Co-authored-by: Koute <koute@users.noreply.github.com>

--- client/service/src/config.rs
+++ client/service/src/config.rs
@@ -262,7 +262,7 @@ impl Default for RpcMethods {
 	}
 }
 
-#[static_init::dynamic(drop)]
+#[static_init::dynamic(drop,lazy)]
 static mut BASE_PATH_TEMP: Option<TempDir> = None;
 
 /// The base path that is used for everything that needs to be write on disk to run a node.
