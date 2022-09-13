96f3a2129588361ac540da55519a4595a542beef
linning <linningde25@gmail.com>
2022-09-13 18:40:23 +0800

    make clippy happy
    Signed-off-by: linning <linningde25@gmail.com>

--- frame/support/src/traits/hooks.rs
+++ frame/support/src/traits/hooks.rs
@@ -18,10 +18,11 @@
 //! Traits for hooking tasks to events in a blockchain's lifecycle.
 
 use crate::weights::Weight;
-use codec::{Decode, Encode};
 use impl_trait_for_tuples::impl_for_tuples;
 use sp_runtime::traits::AtLeast32BitUnsigned;
 use sp_std::prelude::*;
+#[cfg(feature = "try-runtime")]
+use codec::{Decode, Encode};
 
 /// The block initialization trait.
 ///
