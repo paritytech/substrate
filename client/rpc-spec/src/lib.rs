eb120ad9b4fdd2cd42e6cbba3c31b201aaebf9e2
Alexandru Vasile <60601340+lexnv@users.noreply.github.com>
2022-09-14 18:58:31 +0300

    Update client/rpc-spec/src/lib.rs
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>

--- client/rpc-spec/src/lib.rs
+++ client/rpc-spec/src/lib.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! JSON-RPC interface exposed by blockchain nodes in the Substrate ecosystem.
+//! Substrate JSON-RPC interface v2.
 //!
 //! Specification [document](https://paritytech.github.io/json-rpc-interface-spec/).
 
