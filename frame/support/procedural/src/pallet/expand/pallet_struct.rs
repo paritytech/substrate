975f3713cae66c69dd0365bc1ba77110b5bd0028
Sam Johnson <sam@durosoft.com>
2022-09-15 11:59:21 -0400

    remove unneeded use
    Co-authored-by: Bastian Köcher <info@kchr.de>

--- frame/support/procedural/src/pallet/expand/pallet_struct.rs
+++ frame/support/procedural/src/pallet/expand/pallet_struct.rs
@@ -176,7 +176,7 @@ pub fn expand_pallet_struct(def: &mut Def) -> proc_macro2::TokenStream {
 		use #frame_support::traits::{StorageInfoTrait, TrackedStorageKey, WhitelistedStorageKeys};
 		impl<#type_impl_gen> WhitelistedStorageKeys for #pallet_ident<#type_use_gen> #storages_where_clauses {
 			fn whitelisted_storage_keys() -> #frame_support::sp_std::vec::Vec<TrackedStorageKey> {
-				use #frame_support::sp_std::{vec, vec::Vec};
+				use #frame_support::sp_std::vec;
 				vec![#(
 					TrackedStorageKey::new(#whitelisted_storage_idents::<#type_use_gen>::hashed_key().to_vec())
 				),*]
