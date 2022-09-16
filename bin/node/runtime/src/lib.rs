ac570e46fc39c60e183b3a7409d857cdb8cb2836
Maksim Ramanenkau <cbnhrpo@gmail.com>
2022-09-16 12:42:43 +0400

    Code review

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -119,8 +119,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 284,
-	impl_version: 1,
+	spec_version: 285,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
 };
