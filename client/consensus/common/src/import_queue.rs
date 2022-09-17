aa7efc8d747ac9927296f71f13a6b9bbc5165192
Szegoo <sakacszergej@gmail.com>
2022-09-17 20:16:44 +0200

    docs

--- client/consensus/common/src/import_queue.rs
+++ client/consensus/common/src/import_queue.rs
@@ -62,7 +62,7 @@ pub type BoxBlockImport<B, Transaction> =
 pub type BoxJustificationImport<B> =
 	Box<dyn JustificationImport<B, Error = ConsensusError> + Send + Sync>;
 
-/// Maps to the Origin used by the network.
+/// Maps to the RuntimeOrigin used by the network.
 pub type RuntimeOrigin = libp2p::PeerId;
 
 /// Block data used by the queue.
