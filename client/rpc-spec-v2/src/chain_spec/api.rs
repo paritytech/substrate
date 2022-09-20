de5a7f60a5144d79e3641ac7fe9d2019aef35931
Alexandru Vasile <60601340+lexnv@users.noreply.github.com>
2022-09-20 18:18:03 +0300

    rpc: Implement `chainSpec` RPC API (#12261)
    * rpc/chain_spec: Add traits for `chainSpec` API
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    
    * rpc/chain_spec: Implement `chainSpec` RPC methods
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    
    * rpc/chain_spec: Add tests
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    
    * bin/node: Enable `chainSpec` API
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    
    * rpc/chain_spec: Assume `genesis_hash` as non-empty
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    
    * Update client/rpc-spec/Cargo.toml
    
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>
    
    * Update client/rpc-spec/src/lib.rs
    
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>
    
    * client/rpc_spec: Rename crate to `rpc_spec_v2`
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    
    * rpc-servers: Remove the `version` field from `rpc_methods`
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    
    * rpc/chain_spec: Fix copyright years
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>

---
+++ client/rpc-spec-v2/src/chain_spec/api.rs
@@ -0,0 +1,53 @@
+// This file is part of Substrate.
+
+// Copyright (C) 2022 Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! API trait of the chain spec.
+
+use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+use sc_chain_spec::Properties;
+
+#[rpc(client, server)]
+pub trait ChainSpecApi {
+	/// Get the chain name, as present in the chain specification.
+	///
+	/// # Unstable
+	///
+	/// This method is unstable and subject to change in the future.
+	#[method(name = "chainSpec_unstable_chainName")]
+	fn chain_spec_unstable_chain_name(&self) -> RpcResult<String>;
+
+	/// Get the chain's genesis hash.
+	///
+	/// # Unstable
+	///
+	/// This method is unstable and subject to change in the future.
+	#[method(name = "chainSpec_unstable_genesisHash")]
+	fn chain_spec_unstable_genesis_hash(&self) -> RpcResult<String>;
+
+	/// Get the properties of the chain, as present in the chain specification.
+	///
+	/// # Note
+	///
+	/// The json whitespaces are not guaranteed to persist.
+	///
+	/// # Unstable
+	///
+	/// This method is unstable and subject to change in the future.
+	#[method(name = "chainSpec_unstable_properties")]
+	fn chain_spec_unstable_properties(&self) -> RpcResult<Properties>;
+}
