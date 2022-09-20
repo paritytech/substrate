bdc0538d6150f00e432a032f8b2f47e15f7b70f8
Jegor Sidorenko <jegor@parity.io>
2022-09-20 14:40:22 +0300

    Update target balance

--- frame/nfts/src/benchmarking.rs
+++ frame/nfts/src/benchmarking.rs
@@ -505,6 +505,7 @@ benchmarks_instance_pallet! {
 		let target: T::AccountId = account("target", 0, SEED);
 		let target_lookup = T::Lookup::unlookup(target.clone());
 		let origin = SystemOrigin::Signed(caller.clone());
+		T::Currency::make_free_balance_be(&target, DepositBalanceOf::<T, I>::max_value());
 		Nfts::<T, I>::transfer(origin.clone().into(), collection, item2, target_lookup)?;
 		Nfts::<T, I>::create_swap(origin.clone().into(), collection, item1, collection, item2, Some(price), Some(duration))?;
 	}: _(SystemOrigin::Signed(target.clone()), collection, item2, collection, item1, Some(price.clone()))
