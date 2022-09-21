3f3fdda7b2e82d42f130b6eb92472857658718ff
Roman Useinov <roman.useinov@gmail.com>
2022-09-21 15:08:52 +0200

    reinstate encode/decode

--- frame/support/src/traits/hooks.rs
+++ frame/support/src/traits/hooks.rs
@@ -22,6 +22,9 @@ use impl_trait_for_tuples::impl_for_tuples;
 use sp_runtime::traits::AtLeast32BitUnsigned;
 use sp_std::prelude::*;
 
+#[cfg(feature = "try-runtime")]
+use codec::{Decode, Encode};
+
 /// The block initialization trait.
 ///
 /// Implementing this lets you express what should happen for your pallet when the block is
