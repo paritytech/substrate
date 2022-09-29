8563a312a8998caec4498b78183ea19ac1335d75
Jegor Sidorenko <jegor@parity.io>
2022-09-29 14:30:04 +0300

    Fix traits

--- frame/support/src/traits/tokens/nonfungible_v2.rs
+++ frame/support/src/traits/tokens/nonfungible_v2.rs
@@ -24,7 +24,7 @@
 //! For an NFT API which has dual-level namespacing, the traits in `nonfungibles` are better to
 //! use.
 
-use super::nonfungibles;
+use super::nonfungibles_v2 as nonfungibles;
 use crate::{dispatch::DispatchResult, traits::Get};
 use codec::{Decode, Encode};
 use sp_runtime::TokenError;
@@ -74,11 +74,11 @@ pub trait InspectEnumerable<AccountId>: Inspect<AccountId> {
 
 /// Trait for providing an interface for NFT-like items which may be minted, burned and/or have
 /// attributes set on them.
-pub trait Mutate<AccountId>: Inspect<AccountId> {
+pub trait Mutate<AccountId, ItemConfig>: Inspect<AccountId> {
 	/// Mint some `item` to be owned by `who`.
 	///
 	/// By default, this is not a supported operation.
-	fn mint_into(_item: &Self::ItemId, _who: &AccountId) -> DispatchResult {
+	fn mint_into(_item: &Self::ItemId, _who: &AccountId, _config: &ItemConfig) -> DispatchResult {
 		Err(TokenError::Unsupported.into())
 	}
 
@@ -158,26 +158,37 @@ impl<
 }
 
 impl<
-		F: nonfungibles::Mutate<AccountId>,
+		F: nonfungibles::Mutate<AccountId, ItemConfig>,
 		A: Get<<F as nonfungibles::Inspect<AccountId>>::CollectionId>,
 		AccountId,
-	> Mutate<AccountId> for ItemOf<F, A, AccountId>
+		ItemConfig,
+	> Mutate<AccountId, ItemConfig> for ItemOf<F, A, AccountId>
 {
-	fn mint_into(item: &Self::ItemId, who: &AccountId) -> DispatchResult {
-		<F as nonfungibles::Mutate<AccountId>>::mint_into(&A::get(), item, who)
+	fn mint_into(item: &Self::ItemId, who: &AccountId, config: &ItemConfig) -> DispatchResult {
+		<F as nonfungibles::Mutate<AccountId, ItemConfig>>::mint_into(&A::get(), item, who, config)
 	}
 	fn burn(item: &Self::ItemId, maybe_check_owner: Option<&AccountId>) -> DispatchResult {
-		<F as nonfungibles::Mutate<AccountId>>::burn(&A::get(), item, maybe_check_owner)
+		<F as nonfungibles::Mutate<AccountId, ItemConfig>>::burn(&A::get(), item, maybe_check_owner)
 	}
 	fn set_attribute(item: &Self::ItemId, key: &[u8], value: &[u8]) -> DispatchResult {
-		<F as nonfungibles::Mutate<AccountId>>::set_attribute(&A::get(), item, key, value)
+		<F as nonfungibles::Mutate<AccountId, ItemConfig>>::set_attribute(
+			&A::get(),
+			item,
+			key,
+			value,
+		)
 	}
 	fn set_typed_attribute<K: Encode, V: Encode>(
 		item: &Self::ItemId,
 		key: &K,
 		value: &V,
 	) -> DispatchResult {
-		<F as nonfungibles::Mutate<AccountId>>::set_typed_attribute(&A::get(), item, key, value)
+		<F as nonfungibles::Mutate<AccountId, ItemConfig>>::set_typed_attribute(
+			&A::get(),
+			item,
+			key,
+			value,
+		)
 	}
 }
 
