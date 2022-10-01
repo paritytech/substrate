83c339d8cad7f895766f5957f90c1d2ed3baa2a2
Sam Johnson <sam@durosoft.com>
2022-10-01 19:11:43 +0000

    add docs for pallet_macro_stub

--- frame/support/procedural/src/lib.rs
+++ frame/support/procedural/src/lib.rs
@@ -585,6 +585,8 @@ pub fn storage_alias(_: TokenStream, input: TokenStream) -> TokenStream {
 		.into()
 }
 
+/// Used internally to decorate pallet attribute macro stubs when they are erroneously used
+/// outsof a pallet module
 fn pallet_macro_stub() -> TokenStream {
 	quote!(compile_error!("This attribute can only be used within a `pallet` module")).into()
 }
