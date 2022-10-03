3914be7f961a170bb7e824d959e32df907362f52
Michal <devdanco@gmail.com>
2022-10-03 14:57:47 +0200

    remove orml deps

--- frame/support/src/traits/tokens/currency.rs
+++ frame/support/src/traits/tokens/currency.rs
@@ -267,6 +267,50 @@ pub trait MultiTokenCurrency<AccountId> {
 	) -> SignedImbalance<Self::Balance, Self::PositiveImbalance>;
 }
 
+/// A currency whose accounts can have liquidity restrictions.
+pub trait MultiTokenLockableCurrency<AccountId>: MultiTokenCurrency<AccountId> {
+	/// The quantity used to denote time; usually just a `BlockNumber`.
+	type Moment;
+
+	/// The maximum number of locks a user should have on their account.
+	type MaxLocks: Get<u32>;
+
+	/// Create a new balance lock on account `who`.
+	///
+	/// If the new lock is valid (i.e. not already expired), it will push the
+	/// struct to the `Locks` vec in storage. Note that you can lock more funds
+	/// than a user has.
+	///
+	/// If the lock `id` already exists, this will update it.
+	fn set_lock(
+		currency_id: Self::CurrencyId,
+		id: LockIdentifier,
+		who: &AccountId,
+		amount: Self::Balance,
+		reasons: WithdrawReasons,
+	);
+
+	/// Changes a balance lock (selected by `id`) so that it becomes less liquid
+	/// in all parameters or creates a new one if it does not exist.
+	///
+	/// Calling `extend_lock` on an existing lock `id` differs from `set_lock`
+	/// in that it applies the most severe constraints of the two, while
+	/// `set_lock` replaces the lock with the new parameters. As in,
+	/// `extend_lock` will set:
+	/// - maximum `amount`
+	/// - bitwise mask of all `reasons`
+	fn extend_lock(
+		currency_id: Self::CurrencyId,
+		id: LockIdentifier,
+		who: &AccountId,
+		amount: Self::Balance,
+		reasons: WithdrawReasons,
+	);
+
+	/// Remove an existing lock.
+	fn remove_lock(currency_id: Self::CurrencyId, id: LockIdentifier, who: &AccountId);
+}
+
 /// Abstraction over a fungible assets system.
 pub trait Currency<AccountId> {
 	/// The balance of an account.
