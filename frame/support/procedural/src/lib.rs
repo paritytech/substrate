7456d8532cf8636527fe17225290a772426ff179
Sam Johnson <sam@durosoft.com>
2022-10-04 14:10:54 -0400

    add additional note about storage prefix
    Co-authored-by: Squirrel <gilescope@gmail.com>

--- frame/support/procedural/src/lib.rs
+++ frame/support/procedural/src/lib.rs
@@ -1017,7 +1017,8 @@ pub fn getter(_: TokenStream, _: TokenStream) -> TokenStream {
 }
 
 /// The optional attribute `#[pallet::storage_prefix = "SomeName"]` allows you to define the
-/// storage prefix to use.
+/// storage prefix to use. This is helpful if you wish to rename the storage 
+/// field but don't want to perform a migration.
 ///
 /// E.g:
 ///
