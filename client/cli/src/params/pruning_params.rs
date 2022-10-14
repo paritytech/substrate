a5fb1ccfe732dd706ad61c3df08e489a81ec462c
Alexandru Vasile <alexandru.vasile@parity.io>
2022-10-14 13:24:06 +0000

    client/cli: Add delayed pruning mode and make it default
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- client/cli/src/params/pruning_params.rs
+++ client/cli/src/params/pruning_params.rs
@@ -30,12 +30,16 @@ pub struct PruningParams {
 	/// or for all of the canonical blocks (i.e 'archive-canonical').
 	#[clap(alias = "pruning", long, value_name = "PRUNING_MODE")]
 	pub state_pruning: Option<String>,
-	/// Specify the blocks pruning mode, a number of blocks to keep or 'archive'.
+	/// Specify the blocks pruning mode.
 	///
-	/// Default is to keep all finalized blocks.
-	/// otherwise, all blocks can be kept (i.e 'archive'),
-	/// or for all canonical blocks (i.e 'archive-canonical'),
-	/// or for the last N blocks (i.e a number).
+	/// The options are as follows:
+	///   'delayed'            Pruning of blocks is delayed for a sensible amount of time to
+	///                        satisfy the RPC Spec V2.
+	///   'archive'            Keep all blocks.
+	///   'archive-canonical'  Keep all finalized (canonical) blocks.
+	///   [number]             Keep the last N finalized (canonical) blocks.
+	///
+	/// Default is the 'delayed' option.
 	///
 	/// NOTE: only finalized blocks are subject for removal!
 	#[clap(alias = "keep-blocks", long, value_name = "COUNT")]
@@ -66,6 +70,7 @@ impl PruningParams {
 			Some(bp) => match bp.as_str() {
 				"archive" => Ok(BlocksPruning::KeepAll),
 				"archive-canonical" => Ok(BlocksPruning::KeepFinalized),
+				"delayed" => Ok(BlocksPruning::Delayed(crate::DELAYED_PRUNING)),
 				bc => bc
 					.parse()
 					.map_err(|_| {
