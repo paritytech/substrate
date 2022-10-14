3ea38b8f91ebbe9b7afef06d933a0b3f0d2c5807
Alexander Theißen <alex.theissen@me.com>
2022-10-14 12:20:42 +0200

    Remove dward sections

--- utils/wasm-builder/src/wasm_project.rs
+++ utils/wasm-builder/src/wasm_project.rs
@@ -668,7 +668,9 @@ fn compact_wasm_file(
 	let (wasm_compact_path, wasm_compact_compressed_path) = if profile.wants_compact() {
 		let wasm_compact_path = project.join(format!("{}.compact.wasm", out_name,));
 		wasm_opt::OptimizationOptions::new_opt_level_0()
+			.mvp_features_only()
 			.debug_info(true)
+			.add_pass(wasm_opt::Pass::StripDwarf)
 			.run(&in_path, &wasm_compact_path)
 			.expect("Failed to compact generated WASM binary.");
 
