ed851bf80a4abc7e24c6b98804eacd76fb9d1f83
Maksim Ramanenkau <cbnhrpo@gmail.com>
2022-10-17 16:04:25 +0300

    Updat rustc version according to the Polkadot release

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-rustup install stable-2021-11-01
+rustup install nightly-2021-11-07
 
-rustup target add wasm32-unknown-unknown --toolchain stable-2021-11-01
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-11-07
