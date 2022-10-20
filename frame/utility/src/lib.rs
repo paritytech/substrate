f003754dfd6932c223ebcc79854d4f7f2abb0242
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2022-10-20 21:32:27 +0200

    Update frame/utility/src/lib.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- frame/utility/src/lib.rs
+++ frame/utility/src/lib.rs
@@ -291,7 +291,7 @@ pub mod pallet {
 		/// Send a batch of dispatch calls and atomically execute them.
 		/// The whole transaction will rollback and fail if any of the calls failed.
 		///
-		/// May be called from either the root or a signed origin.
+		/// May be called from any origin except `None`.
 		///
 		/// - `calls`: The calls to be dispatched from the same origin. The number of call must not
 		///   exceed the constant: `batched_calls_limit` (available in constant metadata).
