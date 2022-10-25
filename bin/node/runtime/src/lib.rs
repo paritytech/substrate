c9a9829599a46396f93d822f39add10a4db38f2b
Maksim Ramanenkau <cbnhrpo@gmail.com>
2022-10-25 11:27:28 +0300

    Set runtime state version to 0

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -132,7 +132,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
-	state_version: 1,
+	state_version: 0,
 };
 
 /// The BABE epoch configuration at genesis.
