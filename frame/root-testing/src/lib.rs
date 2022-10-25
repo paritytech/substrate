b8e7c21ba8e81b31ef6b59f65383998a030eb1df
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2022-10-25 15:51:39 +0200

    Update frame/root-testing/src/lib.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- frame/root-testing/src/lib.rs
+++ frame/root-testing/src/lib.rs
@@ -19,7 +19,7 @@
 //!
 //! Pallet that contains extrinsics that can be usefull in testing.
 //!
-//! NOTE: This pallet should only be used for testing purposes.
+//! NOTE: This pallet should only be used for testing purposes and should not be used in production runtimes!
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
