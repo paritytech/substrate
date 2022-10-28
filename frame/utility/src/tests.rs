bf0f4f221c7032a22f436ed2483b0abf7418714c
Szegoo <sakacszergej@gmail.com>
2022-10-28 15:23:11 +0200

    fix

--- frame/utility/src/tests.rs
+++ frame/utility/src/tests.rs
@@ -195,7 +195,6 @@ parameter_types! {
 	pub const MultisigDepositBase: u64 = 1;
 	pub const MultisigDepositFactor: u64 = 1;
 	pub const MaxSignatories: u32 = 3;
-	pub const MaxSignatories: u16 = 3;
 	pub const MotionDuration: BlockNumber = MOTION_DURATION_IN_BLOCKS;
 	pub const MaxProposals: u32 = 100;
 	pub const MaxMembers: u32 = 100;
@@ -211,7 +210,6 @@ impl pallet_collective::Config<CouncilCollective> for Test {
 	type MaxMembers = MaxMembers;
 	type DefaultVote = pallet_collective::PrimeDefaultVote;
 	type WeightInfo = ();
-	pub const MaxSignatories: u32 = 3;
 }
 
 impl example::Config for Test {}
