f81880b45761b4b1d04af6e36bb5c6c443fbe439
Jan Bujak <jan@parity.io>
2022-11-08 05:37:54 +0000

    Remove `RuntimeTasks::{spawn, join}` host functions

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -40,7 +40,7 @@ use sp_core::{
 	hexdisplay::HexDisplay,
 	offchain::{OffchainDbExt, OffchainWorkerExt, TransactionPoolExt},
 	storage::ChildInfo,
-	traits::{RuntimeSpawnExt, TaskExecutorExt},
+	traits::TaskExecutorExt,
 };
 #[cfg(feature = "std")]
 use sp_keystore::{KeystoreExt, SyncCryptoStore};
@@ -1657,35 +1657,6 @@ pub trait Sandbox {
 	}
 }
 
-/// Wasm host functions for managing tasks.
-///
-/// This is DEPRECATED!
-#[cfg(not(target_arch = "wasm32"))] // Leave the host functions for compatibility.
-#[runtime_interface(wasm_only)]
-pub trait RuntimeTasks {
-	/// Wasm host function for spawning task.
-	fn spawn(dispatcher_ref: u32, entry: u32, payload: Vec<u8>) -> u64 {
-		sp_externalities::with_externalities(|mut ext| {
-			let runtime_spawn = ext
-				.extension::<RuntimeSpawnExt>()
-				.expect("Cannot spawn without dynamic runtime dispatcher (RuntimeSpawnExt)");
-			runtime_spawn.spawn_call(dispatcher_ref, entry, payload)
-		})
-		.expect("`RuntimeTasks::spawn`: called outside of externalities context")
-	}
-
-	/// Wasm host function for joining a task.
-	fn join(handle: u64) -> Vec<u8> {
-		sp_externalities::with_externalities(|mut ext| {
-			let runtime_spawn = ext
-				.extension::<RuntimeSpawnExt>()
-				.expect("Cannot join without dynamic runtime dispatcher (RuntimeSpawnExt)");
-			runtime_spawn.join(handle)
-		})
-		.expect("`RuntimeTasks::join`: called outside of externalities context")
-	}
-}
-
 /// Allocator used by Substrate when executing the Wasm runtime.
 #[cfg(all(target_arch = "wasm32", not(feature = "std")))]
 struct WasmAllocator;
@@ -1764,7 +1735,6 @@ pub type SubstrateHostFunctions = (
 	sandbox::HostFunctions,
 	crate::trie::HostFunctions,
 	offchain_index::HostFunctions,
-	runtime_tasks::HostFunctions,
 	transaction_index::HostFunctions,
 );
 
