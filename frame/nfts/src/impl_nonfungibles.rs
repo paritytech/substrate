d7a1675414f5d878a21c23db205aba3a118d7c68
Jegor Sidorenko <jegor@parity.io>
2022-11-18 10:10:11 +0200

    Use KeyPrefixIterator instead of Box

--- frame/nfts/src/impl_nonfungibles.rs
+++ frame/nfts/src/impl_nonfungibles.rs
@@ -20,6 +20,7 @@
 use super::*;
 use frame_support::{
 	ensure,
+	storage::KeyPrefixIterator,
 	traits::{tokens::nonfungibles_v2::*, Get},
 	BoundedSlice,
 };
@@ -186,25 +187,31 @@ impl<T: Config<I>, I: 'static> Transfer<T::AccountId> for Pallet<T, I> {
 }
 
 impl<T: Config<I>, I: 'static> InspectEnumerable<T::AccountId> for Pallet<T, I> {
+	type CollectionsIterator = KeyPrefixIterator<<T as Config<I>>::CollectionId>;
+	type ItemsIterator = KeyPrefixIterator<<T as Config<I>>::ItemId>;
+	type OwnedIterator =
+		KeyPrefixIterator<(<T as Config<I>>::CollectionId, <T as Config<I>>::ItemId)>;
+	type OwnedInCollectionIterator = KeyPrefixIterator<<T as Config<I>>::ItemId>;
+
 	/// Returns an iterator of the collections in existence.
 	///
 	/// NOTE: iterating this list invokes a storage read per item.
-	fn collections() -> Box<dyn Iterator<Item = Self::CollectionId>> {
-		Box::new(CollectionMetadataOf::<T, I>::iter_keys())
+	fn collections() -> Self::CollectionsIterator {
+		Collection::<T, I>::iter_keys()
 	}
 
 	/// Returns an iterator of the items of a `collection` in existence.
 	///
 	/// NOTE: iterating this list invokes a storage read per item.
-	fn items(collection: &Self::CollectionId) -> Box<dyn Iterator<Item = Self::ItemId>> {
-		Box::new(ItemMetadataOf::<T, I>::iter_key_prefix(collection))
+	fn items(collection: &Self::CollectionId) -> Self::ItemsIterator {
+		Item::<T, I>::iter_key_prefix(collection)
 	}
 
 	/// Returns an iterator of the items of all collections owned by `who`.
 	///
 	/// NOTE: iterating this list invokes a storage read per item.
-	fn owned(who: &T::AccountId) -> Box<dyn Iterator<Item = (Self::CollectionId, Self::ItemId)>> {
-		Box::new(Account::<T, I>::iter_key_prefix((who,)))
+	fn owned(who: &T::AccountId) -> Self::OwnedIterator {
+		Account::<T, I>::iter_key_prefix((who,))
 	}
 
 	/// Returns an iterator of the items of `collection` owned by `who`.
@@ -213,7 +220,7 @@ impl<T: Config<I>, I: 'static> InspectEnumerable<T::AccountId> for Pallet<T, I> 
 	fn owned_in_collection(
 		collection: &Self::CollectionId,
 		who: &T::AccountId,
-	) -> Box<dyn Iterator<Item = Self::ItemId>> {
-		Box::new(Account::<T, I>::iter_key_prefix((who, collection)))
+	) -> Self::OwnedInCollectionIterator {
+		Account::<T, I>::iter_key_prefix((who, collection))
 	}
 }
