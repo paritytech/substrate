aa9d230ca1c4fa5c3d5511a8aafea1ff92841f71
Alexandru Vasile <alexandru.vasile@parity.io>
2022-11-21 14:41:40 +0000

    archive/tests: Test `archive_unstable_header`
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- client/rpc-spec-v2/src/archive/tests.rs
+++ client/rpc-spec-v2/src/archive/tests.rs
@@ -1,5 +1,8 @@
 use super::*;
-use jsonrpsee::{types::EmptyParams, RpcModule};
+use codec::Encode;
+use jsonrpsee::{
+	core::server::rpc_module::Subscription as RpcSubscription, types::EmptyParams, RpcModule,
+};
 use sc_block_builder::BlockBuilderProvider;
 use sp_consensus::BlockOrigin;
 use sp_core::{hexdisplay::HexDisplay, testing::TaskExecutor};
@@ -8,6 +11,16 @@ use substrate_test_runtime_client::{prelude::*, Backend, Client, ClientBlockImpo
 
 type Block = substrate_test_runtime_client::runtime::Block;
 const CHAIN_GENESIS: [u8; 32] = [0; 32];
+const INVALID_HASH: [u8; 32] = [1; 32];
+
+async fn get_next_event<T: serde::de::DeserializeOwned>(sub: &mut RpcSubscription) -> T {
+	let (event, _sub_id) = tokio::time::timeout(std::time::Duration::from_secs(1), sub.next())
+		.await
+		.unwrap()
+		.unwrap()
+		.unwrap();
+	event
+}
 
 async fn setup_api(
 ) -> (Arc<Client<Backend>>, RpcModule<Archive<Backend, Block, Client<Backend>>>, Block) {
@@ -32,4 +45,26 @@ async fn get_genesis() {
 	let genesis: String =
 		api.call("archive_unstable_genesisHash", EmptyParams::new()).await.unwrap();
 	assert_eq!(genesis, format!("0x{}", HexDisplay::from(&CHAIN_GENESIS)));
+}
+
+#[tokio::test]
+async fn get_header() {
+	let (_client, api, block) = setup_api().await;
+
+	let block_hash = format!("{:?}", block.header.hash());
+	let invalid_hash = format!("0x{:?}", HexDisplay::from(&INVALID_HASH));
+
+	// Invalid block hash.
+	let mut sub = api.subscribe("archive_unstable_header", [&invalid_hash]).await.unwrap();
+	let event: ArchiveEvent<String> = get_next_event(&mut sub).await;
+	assert_eq!(event, ArchiveEvent::Inaccessible);
+
+	// Valid block hash.
+	let mut sub = api.subscribe("archive_unstable_header", [&block_hash]).await.unwrap();
+	let event: ArchiveEvent<String> = get_next_event(&mut sub).await;
+	let expected = {
+		let result = format!("0x{}", HexDisplay::from(&block.header.encode()));
+		ArchiveEvent::Done(ArchiveResult { result })
+	};
+	assert_eq!(event, expected);
 }
