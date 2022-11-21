11b41c1058830be0350773935ec1b7876258c7e8
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2022-11-21 20:22:23 +0100

    Revert "epoch duration reduced"
    This reverts commit 7c75b698d219f9615f14b14c9152a87c6c55c70b.

--- bin/node/runtime/src/constants.rs
+++ bin/node/runtime/src/constants.rs
@@ -63,7 +63,7 @@ pub mod time {
 
 	// NOTE: Currently it is not possible to change the epoch duration after the chain has started.
 	//       Attempting to do so will brick block production.
-	pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 1 * MINUTES / 2;
+	pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 10 * MINUTES;
 	pub const EPOCH_DURATION_IN_SLOTS: u64 = {
 		const SLOT_FILL_RATE: f64 = MILLISECS_PER_BLOCK as f64 / SLOT_DURATION as f64;
 
