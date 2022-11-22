4c1f695a4b0232d73a75afc1f7772d615dfcf5f5
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2022-11-22 09:13:17 +0100

    versioning test

--- utils/build-script-utils/src/version.rs
+++ utils/build-script-utils/src/version.rs
@@ -32,6 +32,7 @@ pub fn generate_cargo_keys() {
 			},
 			Ok(o) => {
 				println!("cargo:warning=Git command failed with status: {}", o.status);
+				println!("cargo:warning=Git command failed with status: {}", o.status);
 				Cow::from("unknown")
 			},
 			Err(err) => {
