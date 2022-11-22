2054fbf6c5828346b637d2d49f29d18d605599c1
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2022-11-22 10:38:27 +0100

    build util debug messages added

--- utils/build-script-utils/src/version.rs
+++ utils/build-script-utils/src/version.rs
@@ -41,6 +41,7 @@ pub fn generate_cargo_keys() {
 		}
 	};
 
+	println!("cargo:warning=WARNING rustc:env=SUBSTRATE_CLI_IMPL_VERSION={}", get_version(&commit));
 	println!("cargo:rustc-env=SUBSTRATE_CLI_IMPL_VERSION={}", get_version(&commit))
 }
 
