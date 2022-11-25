3e9182366f31ee93473a43d2afedf1357e2df7a8
joe petrowski <25483142+joepetrowski@users.noreply.github.com>
2022-11-25 15:52:26 +0100

    Allow Alliance Fellows to Give Up Voting Rights (#12730)
    * allow fellows to abdicate voting rights
    
    * rename founders to founding fellows, give equal power
    
    * Drop FoundingFellow role and veto call (#12762)
    
    * drop FoundingFellow role
    
    * drop veto call
    
    * Storage migration to remove founder role (#12766)
    
    * storage migration to remove founder role
    
    * skip migration if no members
    
    * truncate the final fellows set if overflows
    
    * change log - action order
    
    * MemberAbdicated -> FellowAbdicated
    
    Co-authored-by: Muharem Ismailov <ismailov.m.h@gmail.com>

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -1574,8 +1574,7 @@ impl pallet_collective::Config<AllianceCollective> for Runtime {
 }
 
 parameter_types! {
-	pub const MaxFounders: u32 = 10;
-	pub const MaxFellows: u32 = AllianceMaxMembers::get() - MaxFounders::get();
+	pub const MaxFellows: u32 = AllianceMaxMembers::get();
 	pub const MaxAllies: u32 = 100;
 	pub const AllyDeposit: Balance = 10 * DOLLARS;
 	pub const RetirementPeriod: BlockNumber = ALLIANCE_MOTION_DURATION_IN_BLOCKS + (1 * DAYS);
@@ -1606,7 +1605,6 @@ impl pallet_alliance::Config for Runtime {
 	type IdentityVerifier = ();
 	type ProposalProvider = AllianceProposalProvider;
 	type MaxProposals = AllianceMaxProposals;
-	type MaxFounders = MaxFounders;
 	type MaxFellows = MaxFellows;
 	type MaxAllies = MaxAllies;
 	type MaxUnscrupulousItems = ConstU32<100>;
