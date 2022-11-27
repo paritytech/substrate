52e64306076a9b27213d8c2c3f505a211772b304
Gav <gavin@parity.io>
2022-11-27 21:31:20 +0000

    Fixes

--- bin/node/testing/src/genesis.rs
+++ bin/node/testing/src/genesis.rs
@@ -20,9 +20,9 @@
 
 use crate::keyring::*;
 use kitchensink_runtime::{
-	constants::currency::*, wasm_binary_unwrap, AccountId, AssetsConfig, BabeConfig,
-	BalancesConfig, GenesisConfig, GrandpaConfig, IndicesConfig, SessionConfig, SocietyConfig,
-	StakerStatus, StakingConfig, SystemConfig, BABE_GENESIS_EPOCH_CONFIG,
+	constants::currency::*, wasm_binary_unwrap, AccountId, BabeConfig, BalancesConfig,
+	GenesisConfig, GrandpaConfig, IndicesConfig, SessionConfig, SocietyConfig, StakerStatus,
+	StakingConfig, SystemConfig, BABE_GENESIS_EPOCH_CONFIG,
 };
 use sp_keyring::{Ed25519Keyring, Sr25519Keyring};
 use sp_runtime::Perbill;
