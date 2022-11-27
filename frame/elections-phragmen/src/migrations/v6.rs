359cac173b13cbb8270683542c1fb38386e47f2d
Sergej Sakac <sakacszergej@gmail.com>
2022-11-27 10:22:56 +0100

    complete migrations

--- frame/elections-phragmen/src/migrations/v6.rs
+++ frame/elections-phragmen/src/migrations/v6.rs
@@ -15,9 +15,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::{BalanceOf, Config, Pallet, MAXIMUM_VOTE};
+use crate::{BalanceOf, Config, Pallet, SeatHolder, Weight, MAXIMUM_VOTE};
 use codec::{Decode, Encode};
-use frame_support::{traits::ConstU32, BoundedVec, RuntimeDebug, Twox64Concat};
+use frame_support::{pallet_prelude::StorageVersion, traits::ConstU32, BoundedVec, RuntimeDebug};
+use sp_core::Get;
+use sp_runtime::Saturating;
 use sp_std::prelude::*;
 
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug, PartialEq)]
@@ -27,18 +29,95 @@ struct DeprecatedVoter<AccountId, Balance> {
 	deposit: Balance,
 }
 
-#[frame_support::storage_alias]
-type Voting<T: Config> = StorageMap<
-	Pallet<T>,
-	Twox64Concat,
-	<T as frame_system::Config>::AccountId,
-	crate::Voter<<T as frame_system::Config>::AccountId, BalanceOf<T>>,
->;
+pub fn apply<T: Config>() -> Weight {
+	let storage_version = StorageVersion::get::<Pallet<T>>();
+	log::info!(
+		target: "runtime::elections-phragmen",
+		"Running migration for elections-phragmen with storage version {:?}",
+		storage_version,
+	);
 
-pub fn migrate_voting<T: Config>() {
-	Voting::<T>::translate::<DeprecatedVoter<T::AccountId, BalanceOf<T>>, _>(|_, voter| {
+	if storage_version <= 5 {
+		let weight = migrate_voting::<T>();
+		weight.saturating_add(migrate_members::<T>());
+		weight.saturating_add(migrate_runners_up::<T>());
+		weight.saturating_add(migrate_candidates::<T>());
+
+		StorageVersion::new(6).put::<Pallet<T>>();
+
+		weight
+	} else {
+		log::warn!(
+			target: "runtime::elections-phragmen",
+			"Attempted to apply migration to V6 but failed because storage version is {:?}",
+			storage_version,
+		);
+		Weight::zero()
+	}
+}
+
+pub fn migrate_voting<T: Config>() -> Weight {
+	let mut translated = 0u64;
+	crate::Voting::<T>::translate::<DeprecatedVoter<T::AccountId, BalanceOf<T>>, _>(|_, voter| {
+		translated.saturating_inc();
 		let bounded_votes: BoundedVec<T::AccountId, ConstU32<{ MAXIMUM_VOTE as u32 }>> =
 			voter.votes.try_into().unwrap();
 		Some(crate::Voter { votes: bounded_votes, stake: voter.stake, deposit: voter.deposit })
 	});
+	T::DbWeight::get().reads_writes(translated, translated)
+}
+
+pub fn migrate_members<T: Config>() -> Weight {
+	let mut translated = 0u64;
+	let _ = crate::Members::<T>::translate::<Vec<SeatHolder<T::AccountId, BalanceOf<T>>>, _>(
+		|maybe_members| {
+			translated.saturating_inc();
+			maybe_members.map(|members| {
+				let bounded_members: BoundedVec<
+					SeatHolder<T::AccountId, BalanceOf<T>>,
+					T::DesiredMembers,
+				> = members.try_into().unwrap();
+				bounded_members
+			})
+		},
+	);
+	T::DbWeight::get().reads_writes(translated, translated)
+}
+
+pub fn migrate_runners_up<T: Config>() -> Weight {
+	let mut translated = 0u64;
+	let _ = crate::RunnersUp::<T>::translate::<Vec<SeatHolder<T::AccountId, BalanceOf<T>>>, _>(
+		|maybe_runners_up| {
+			translated.saturating_inc();
+			maybe_runners_up.map(|runners_up| {
+				let bounded_runners_up: BoundedVec<
+					SeatHolder<T::AccountId, BalanceOf<T>>,
+					T::DesiredRunnersUp,
+				> = runners_up.try_into().unwrap();
+				bounded_runners_up
+			})
+		},
+	);
+	T::DbWeight::get().reads_writes(translated, translated)
+}
+
+pub fn migrate_candidates<T: Config>() -> Weight {
+	let mut translated = 0u64;
+	let _ = crate::Candidates::<T>::translate::<Vec<(T::AccountId, BalanceOf<T>)>, _>(
+		|maybe_candidates| {
+			translated.saturating_inc();
+			maybe_candidates.map(|candidates| {
+				let bounded_candidates: BoundedVec<(T::AccountId, BalanceOf<T>), T::MaxCandidates> =
+					candidates.try_into().unwrap();
+				bounded_candidates
+			})
+		},
+	);
+	T::DbWeight::get().reads_writes(translated, translated)
+}
+
+pub fn post_migration<T: Config>() {
+	log::info!("post-migration elections-phragmen");
+
+	assert_eq!(StorageVersion::get::<crate::Pallet<T>>(), 6);
 }
