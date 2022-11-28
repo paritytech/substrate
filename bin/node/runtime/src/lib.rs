a0e00dc87cc1c2e52281bff3db61a4d4cc1aa983
joe petrowski <25483142+joepetrowski@users.noreply.github.com>
2022-11-28 19:51:59 +0100

    Remove Default, HasCompact, and TypeInfo trait bounds on AssetId (#12740)
    * Remove Default, HasCompact, and TypeInfo trait bounds on AssetId
    
    * don't use default in benchmarking
    
    * add helper trait
    
    * add helper to assets tx payment test
    
    * docs fixes
    
    * i'm confused
    
    * aha, cargo
    
    * move affected dispatchable calls into new indices
    
    * Helper -> BenchmarkHelper
    
    * benchmark use of helper
    
    * actually, don't break every call interface
    
    * use into on AssetIdParameter
    
    * Remove From from AssetIdParameter and use it in BenchmarkHelper
    
    * include from
    
    Co-authored-by: parity-processbot <>

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -1440,6 +1440,7 @@ impl pallet_assets::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type Balance = u128;
 	type AssetId = u32;
+	type AssetIdParameter = codec::Compact<u32>;
 	type Currency = Balances;
 	type CreateOrigin = AsEnsureOriginWithArg<EnsureSigned<AccountId>>;
 	type ForceOrigin = EnsureRoot<AccountId>;
@@ -1453,6 +1454,8 @@ impl pallet_assets::Config for Runtime {
 	type Extra = ();
 	type WeightInfo = pallet_assets::weights::SubstrateWeight<Runtime>;
 	type RemoveItemsLimit = ConstU32<1000>;
+	#[cfg(feature = "runtime-benchmarks")]
+	type BenchmarkHelper = ();
 }
 
 parameter_types! {
