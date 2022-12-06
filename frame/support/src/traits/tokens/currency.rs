469103463652cad170398f41bffaa6d16172e0e4
Anthony Alaribe <anthonyalaribe@gmail.com>
2022-12-06 13:17:42 +0200

    reexport LockIdentifier and LockableCurrency from support/traits

--- frame/support/src/traits/tokens/currency.rs
+++ frame/support/src/traits/tokens/currency.rs
@@ -32,8 +32,7 @@ use sp_std::fmt::Debug;
 mod reservable;
 pub use reservable::{NamedReservableCurrency, ReservableCurrency};
 mod lockable;
-
-pub use lockable::VestingSchedule;
+pub use lockable::{LockIdentifier, LockableCurrency, VestingSchedule};
 
 /// Abstraction over a fungible assets system.
 pub trait Currency<AccountId> {
