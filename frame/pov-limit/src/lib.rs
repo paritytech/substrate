24ac38a21c3a49c57ece60db69d2896da9137e17
Sergej Sakac <sakacszergej@gmail.com>
2022-12-06 19:32:20 +0100

    update docs

--- frame/pov-limit/src/lib.rs
+++ frame/pov-limit/src/lib.rs
@@ -17,8 +17,8 @@
 
 //! # Pov Limit Pallet
 //!
-//! Pallet that consumes up to a specified weight of a block on `on_initialize`.
-//! The weight consumed is set as a percentage as a config parameter.
+//! Pallet that controls compute usage and PoV size usage. The parameters for
+//! the usage are configured by root and are stored as `StorageValue`s.
 //!
 //! NOTE: This is only meant to be used for testing.
 
