d06359cab8e6eb78d625b7520b37496252139a22
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2022-12-08 16:12:43 +0100

    non-trivial usages of haeder(block_id) refactored
    This may required introduction of dedicated function:
    header_for_block_num

--- client/finality-grandpa/src/warp_proof.rs
+++ client/finality-grandpa/src/warp_proof.rs
@@ -116,8 +116,12 @@ impl<Block: BlockT> WarpSyncProof<Block> {
 		let set_changes = set_changes.iter_from(begin_number).ok_or(Error::MissingData)?;
 
 		for (_, last_block) in set_changes {
-			let header = blockchain.header(BlockId::Number(*last_block))?.expect(
-				"header number comes from previously applied set changes; must exist in db; qed.",
+			let hash = blockchain.hash(*last_block)?.expect(
+				"header number comes from previously applied set changes; corresponding hash must exist in db; qed.",
+			);
+
+			let header = blockchain.header(hash)?.expect(
+				"header for given hash obtained successfully from db exists in db; qed.",
 			);
 
 			// the last block in a set is the one that triggers a change to the next set,
