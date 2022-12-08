6085dd04ec396dd23c5201dbbae7c3c04a0e2658
Davide Galassi <davxy@datawok.net>
2022-12-08 17:52:53 +0100

    Fix missing symbol inclusion

--- frame/grandpa/src/migrations/v4.rs
+++ frame/grandpa/src/migrations/v4.rs
@@ -15,6 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use crate::LOG_TARGET;
 use frame_support::{
 	traits::{Get, StorageVersion},
 	weights::Weight,
