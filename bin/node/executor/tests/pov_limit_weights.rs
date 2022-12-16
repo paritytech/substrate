6f1a430f469ffe27a2d273a8120909d75f525a4a
Sergej Sakac <sakacszergej@gmail.com>
2022-12-16 20:01:02 +0100

    test

--- bin/node/executor/tests/pov_limit_weights.rs
+++ bin/node/executor/tests/pov_limit_weights.rs
@@ -15,5 +15,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use frame_support::traits::Hooks;
+use kitchensink_runtime::{PovLimit, Runtime, System};
+use pallet_pov_limit::WeightInfo;
+
+pub mod common;
+use self::common::*;
+
 #[test]
-fn expected_weight_same_as_actual() {}
+fn expected_weight_same_as_actual() {
+	let mut t = new_test_ext(compact_code_unwrap());
+
+	let expected_weight = <Runtime as pallet_pov_limit::Config>::WeightInfo::on_idle();
+
+	t.execute_with(|| {
+		let weight = System::block_weight().total();
+		let max_weight = <Runtime as frame_system::Config>::BlockWeights::get().max_block;
+		let remaining_weight = max_weight.saturating_sub(weight);
+
+		let actual_weight = PovLimit::on_idle(System::block_number(), remaining_weight);
+
+		let ref_time_delta =
+			i128::abs(actual_weight.ref_time() as i128 - expected_weight.ref_time() as i128);
+		assert!(ref_time_delta < 1_000_000);
+	});
+}
