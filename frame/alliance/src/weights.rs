f44bbe26e87c013960a1a9bb5baacba6b6ef71f5
muharem <ismailov.m.h@gmail.com>
2022-12-22 11:28:53 +0100

    Alliance: remove unused weight func

--- frame/alliance/src/weights.rs
+++ frame/alliance/src/weights.rs
@@ -30,7 +30,6 @@ use sp_std::marker::PhantomData;
 pub trait WeightInfo {
 	fn propose_proposed(b: u32, m: u32, p: u32, ) -> Weight;
 	fn vote(m: u32, ) -> Weight;
-	fn veto(p: u32, ) -> Weight;
 	fn close_early_disapproved(m: u32, p: u32, ) -> Weight;
 	fn close_early_approved(b: u32, m: u32, p: u32, ) -> Weight;
 	fn close_disapproved(m: u32, p: u32, ) -> Weight;
@@ -84,19 +83,6 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 			.saturating_add(Weight::from_ref_time(43_152 as u64).saturating_mul(m as u64))
 			.saturating_add(T::DbWeight::get().reads(2 as u64))
 			.saturating_add(T::DbWeight::get().writes(1 as u64))
-	}
-	// Storage: Alliance Members (r:1 w:0)
-	// Storage: AllianceMotion ProposalOf (r:1 w:1)
-	// Storage: AllianceMotion Proposals (r:1 w:1)
-	// Storage: AllianceMotion Voting (r:0 w:1)
-	/// The range of component `p` is `[1, 100]`.
-	fn veto(p: u32, ) -> Weight {
-		// Minimum execution time: 23_000 nanoseconds.
-		Weight::from_ref_time(26_045_752 as u64)
-			// Standard Error: 2_154
-			.saturating_add(Weight::from_ref_time(61_220 as u64).saturating_mul(p as u64))
-			.saturating_add(T::DbWeight::get().reads(3 as u64))
-			.saturating_add(T::DbWeight::get().writes(3 as u64))
 	}
 	// Storage: Alliance Members (r:1 w:0)
 	// Storage: AllianceMotion Voting (r:1 w:1)
@@ -362,19 +348,6 @@ impl WeightInfo for () {
 			.saturating_add(Weight::from_ref_time(43_152 as u64).saturating_mul(m as u64))
 			.saturating_add(RocksDbWeight::get().reads(2 as u64))
 			.saturating_add(RocksDbWeight::get().writes(1 as u64))
-	}
-	// Storage: Alliance Members (r:1 w:0)
-	// Storage: AllianceMotion ProposalOf (r:1 w:1)
-	// Storage: AllianceMotion Proposals (r:1 w:1)
-	// Storage: AllianceMotion Voting (r:0 w:1)
-	/// The range of component `p` is `[1, 100]`.
-	fn veto(p: u32, ) -> Weight {
-		// Minimum execution time: 23_000 nanoseconds.
-		Weight::from_ref_time(26_045_752 as u64)
-			// Standard Error: 2_154
-			.saturating_add(Weight::from_ref_time(61_220 as u64).saturating_mul(p as u64))
-			.saturating_add(RocksDbWeight::get().reads(3 as u64))
-			.saturating_add(RocksDbWeight::get().writes(3 as u64))
 	}
 	// Storage: Alliance Members (r:1 w:0)
 	// Storage: AllianceMotion Voting (r:1 w:1)
