3d2452706e250eda3e670e0a07a8d48c53f2ccce
Luke Schoen <ltfschoen@gmail.com>
2023-01-03 07:19:16 +0000

    remove switching on and off the stopping of execution errors showing since not required

--- bin/node-template/scripts/docker_run.sh
+++ bin/node-template/scripts/docker_run.sh
@@ -3,12 +3,10 @@
 set -e
 
 printf "*** Start Substrate node template ***\n"
-
 SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 PARENT_DIR=$(dirname "$SCRIPT_DIR")
 mkdir -p "$PARENT_DIR/.local"
 mkdir -p "$PARENT_DIR/.cargo/registry"
-
 cd $(dirname ${BASH_SOURCE[0]})/..
 
 printf "Searching for docker-compose executable...\n"
@@ -17,35 +15,22 @@ if ! [ -x "$(command -v docker-compose)" ]; then
     printf "Skipping docker-compose since executable is not installed.\n"
 else
     printf "Detected docker-compose executable.\n"
-
-    # turn off errors temporarily to continue even if subcommand `docker-compose version` not exist
-    set +e
-
     exit_code=$(docker-compose version; status=$?; echo $status)
     # get the last character captured by $? since it may also include command output first
     last_char=${exit_code: -1}
 
     # prefix last char with random character incase last char is 0 but not an exit code
     if [[ "x${last_char}" == "x0" && "$(docker-compose version)" =~ " 1." ]]; then
-
-        # turn on errors again
-        set -e
-
         printf "Detected legacy docker-compose version 1.x. Using Compose File Format 1.\n"
         docker-compose -f docker-compose-legacy.yml down --remove-orphans
         docker-compose -f docker-compose-legacy.yml run --rm --service-ports dev $@
         exit
     fi
 
-    set +e
-
     exit_code=$(docker-compose compose version; status=$?; echo $status)
     last_char=${exit_code: -1}
 
     if [[ "x${last_char}" == "x0" && "$(docker-compose compose version)" =~ " v2." ]]; then
-
-        set -e
-
         printf "Detected legacy docker-compose version 2.x. Using Compose File Format 2+.\n"
         docker-compose compose down --remove-orphans
         docker-compose compose run --rm --service-ports dev $@
@@ -59,16 +44,10 @@ if ! [ -x "$(command -v docker)" ]; then
     printf "Skipping docker since docker executable is not installed.\n"
 else
     printf "Detected docker executable.\n"
-
-    set +e
-
     exit_code=$(docker compose version; status=$?; echo $status)
     last_char=${exit_code: -1}
 
     if [[ "x${last_char}" == "x0" ]]; then
-
-        set -e
-
         printf "Detected docker compose subcommand.\n"
         docker compose down --remove-orphans
         docker compose run --rm --service-ports dev $@
