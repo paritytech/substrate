e22166fa0d40342d3b373aeca0a33abc782b152c
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2023-01-04 06:00:10 +0800

    Enrich TooManySiblingBlocks error info (#13052)

--- client/state-db/src/lib.rs
+++ client/state-db/src/lib.rs
@@ -134,7 +134,7 @@ pub enum StateDbError {
 	/// Invalid pruning mode specified. Contains expected mode.
 	IncompatiblePruningModes { stored: PruningMode, requested: PruningMode },
 	/// Too many unfinalized sibling blocks inserted.
-	TooManySiblingBlocks,
+	TooManySiblingBlocks { number: u64 },
 	/// Trying to insert existing block.
 	BlockAlreadyExists,
 	/// Invalid metadata
@@ -184,7 +184,8 @@ impl fmt::Debug for StateDbError {
 				"Incompatible pruning modes [stored: {:?}; requested: {:?}]",
 				stored, requested
 			),
-			Self::TooManySiblingBlocks => write!(f, "Too many sibling blocks inserted"),
+			Self::TooManySiblingBlocks { number } =>
+				write!(f, "Too many sibling blocks at #{number} inserted"),
 			Self::BlockAlreadyExists => write!(f, "Block already exists"),
 			Self::Metadata(message) => write!(f, "Invalid metadata: {}", message),
 			Self::BlockUnavailable =>
