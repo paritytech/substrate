3efaf03fb849bb8c74f5d52e7190f8040ec4d727
Luke Schoen <ltfschoen@gmail.com>
2023-01-03 10:58:08 +0000

    wip: remove old docker-compose.yml

--- bin/node-template/scripts/docker_run.sh
+++ bin/node-template/scripts/docker_run.sh
@@ -32,8 +32,8 @@ else
 
     if [[ "x${last_char}" == "x0" && "$(docker-compose compose version)" =~ " v2." ]]; then
         printf "Detected legacy docker-compose version 2.x. Using Compose File Format 2+.\n"
-        docker-compose compose down --remove-orphans
-        docker-compose compose run --rm --service-ports dev $@
+        docker-compose compose -f docker-compose-legacy.yml down --remove-orphans
+        docker-compose compose -f docker-compose-legacy.yml run --rm --service-ports dev $@
         exit
     fi
 
@@ -49,8 +49,9 @@ else
 
     if [[ "x${last_char}" == "x0" ]]; then
         printf "Detected docker compose subcommand.\n"
-        docker compose down --remove-orphans
-        docker compose run --rm --service-ports dev $@
+        # TOD0 modify the docker-compose-legacy.yml file to inject `services:` on first line
+        docker compose -f docker-compose-legacy.yml down --remove-orphans
+        docker compose -f docker-compose-legacy.yml run --rm --service-ports dev $@
     else
         printf "Skipping docker since docker executable subcommand not supported.\n"
     fi
