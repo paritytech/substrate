2d282173d17b8247e7f634ba9fafb1a2a8555843
Roman Useinov <roman.useinov@gmail.com>
2023-01-09 15:03:03 +0100

    Update frame/staking/src/pallet/mod.rs
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- frame/staking/src/pallet/mod.rs
+++ frame/staking/src/pallet/mod.rs
@@ -253,9 +253,6 @@ pub mod pallet {
 
 		/// Something that listens to staking updates and performs actions based on the data it
 		/// receives.
-		/// NOTE: Once we have more implementors of EventListener - this could be done similarly to
-		/// `OnRuntimeUpdate` - just a tuple of all sorts of pallets implementing the trait that all
-		/// react to the interface methods being called or ignore them.
 		type EventListener: sp_staking::OnStakingUpdate<Self::AccountId, BalanceOf<Self>>;
 
 		/// The maximum number of `unlocking` chunks a [`StakingLedger`] can
