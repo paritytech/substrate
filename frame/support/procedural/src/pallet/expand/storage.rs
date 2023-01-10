d00654be79d3696c42867c43ee1c1e2e70ce78e8
alexgparity <115470171+alexgparity@users.noreply.github.com>
2023-01-10 12:05:33 +0100

    Update frame/support/procedural/src/pallet/expand/storage.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- frame/support/procedural/src/pallet/expand/storage.rs
+++ frame/support/procedural/src/pallet/expand/storage.rs
@@ -535,7 +535,7 @@ pub fn expand_storages(def: &mut Def) -> proc_macro2::TokenStream {
 							<T as #frame_system::Config>::PalletInfo
 							as #frame_support::traits::PalletInfo
 						>::name::<Pallet<#type_use_gen>>()
-							.expect("Every active pallet has a name in the runtime; qed. Have you added the pallet into construct_runtime! ?")
+							.expect("No name found for the pallet in the runtime! This usually means that the pallet wasn't added to `construct_runtime!`.")
 					}
 					const STORAGE_PREFIX: &'static str = #counter_prefix_struct_const;
 				}
