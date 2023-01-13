48fe1dac525aaa2512146bf8cfbdce45d77fb9bc
Sam Johnson <sam@durosoft.com>
2023-01-12 21:38:43 -0500

    simplify extrinsic call keyword parsing
    Co-authored-by: Keith Yeung <kungfukeith11@gmail.com>

--- frame/support/procedural/src/benchmark.rs
+++ frame/support/procedural/src/benchmark.rs
@@ -171,7 +171,7 @@ impl BenchmarkDef {
 				Stmt::Semi(Expr::Call(expr_call), _semi) => { // #[extrinsic_call] case
 					(&expr_call.attrs).iter().enumerate().find_map(|(k, attr)| {
 						let Some(segment) = attr.path.segments.last() else { return None; };
-						let Ok(_) = syn::parse::<keywords::extrinsic_call>(segment.ident.to_token_stream().into()) else { return None; };
+						let _: keywords::extrinsic_call = syn::parse(segment.ident.to_token_stream().into()).ok()?;
 						let mut expr_call = expr_call.clone();
 
 						// consume #[extrinsic_call] tokens
