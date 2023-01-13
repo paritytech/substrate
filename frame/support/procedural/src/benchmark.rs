14ef35fc3d5f0a30813069fad0d2c6c3dee8b520
Sam Johnson <sam@durosoft.com>
2023-01-13 00:32:50 -0500

    nit

--- frame/support/procedural/src/benchmark.rs
+++ frame/support/procedural/src/benchmark.rs
@@ -169,7 +169,7 @@ impl BenchmarkDef {
 		let maybe_call_def = item_fn.block.stmts.iter().enumerate().find_map(|(i, child)| {
 			match child {
 				Stmt::Semi(Expr::Call(expr_call), _semi) => { // #[extrinsic_call] case
-					(&expr_call.attrs).iter().enumerate().find_map(|(k, attr)| {
+					expr_call.attrs.iter().enumerate().find_map(|(k, attr)| {
 						let segment = attr.path.segments.last()?;
 						let _: keywords::extrinsic_call = syn::parse(segment.ident.to_token_stream().into()).ok()?;
 						let mut expr_call = expr_call.clone();
