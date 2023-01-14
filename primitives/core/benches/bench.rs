00320fd90964ac2c1cb84cc875096688d3bf2b26
koushiro <koushiro.cqx@gmail.com>
2023-01-14 11:06:01 +0800

    update criterion to v0.4.0

--- primitives/core/benches/bench.rs
+++ primitives/core/benches/bench.rs
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[macro_use]
-extern crate criterion;
-
-use criterion::{black_box, Bencher, BenchmarkId, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Bencher, BenchmarkId, Criterion};
 use sp_core::{
 	crypto::Pair as _,
 	hashing::{blake2_128, twox_128},
