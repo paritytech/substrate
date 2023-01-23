32bb34a7a07029f178a25e8d6c11db6093f67ba0
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-01-23 16:49:18 +0100

    Fix usage
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/system/src/extensions/check_weight.rs
+++ frame/system/src/extensions/check_weight.rs
@@ -135,7 +135,7 @@ where
 
 	// add the weight. If class is unlimited, use saturating add instead of checked one.
 	if limit_per_class.max_total.is_none() && limit_per_class.reserved.is_none() {
-		all_weight.saturating_accrue(extrinsic_weight, info.class)
+		all_weight.accrue(extrinsic_weight, info.class)
 	} else {
 		all_weight
 			.checked_accrue(extrinsic_weight, info.class)
@@ -229,7 +229,7 @@ where
 		let unspent = post_info.calc_unspent(info);
 		if unspent.any_gt(Weight::zero()) {
 			crate::BlockWeight::<T>::mutate(|current_weight| {
-				current_weight.saturating_reduce(unspent, info.class);
+				current_weight.reduce(unspent, info.class);
 			})
 		}
 
