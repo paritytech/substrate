8b4331c27bba6d9f178b8be093e39153350b9be8
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2023-01-25 20:59:19 -0300

    reduce exec time of fast-unstake benchmarks (#13120)
    * reduce exec time of fast-unstake benchmarks
    
    * fix test
    
    * fmt
    
    * fix patch the tests
    
    * fix patch the tests
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * add batch size as well
    
    * update some benches to be better
    
    * fix one last test
    
    * fix
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * reduce time even more
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * fix tests
    
    * nit
    
    * remove
    
    * improve the weight calc further
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * fix benchmarks
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * update
    
    * fix
    
    * fix
    
    * fmt
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * lots of changes again...
    
    * smaller input
    
    * update
    
    * fmt
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * cleanup
    
    * small simplification
    
    * fmt
    
    * reduce exec time a bit
    
    * fix
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * test
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * increase again
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    * review comments
    
    * fmt
    
    * fix
    
    * ".git/.scripts/commands/bench/bench.sh" pallet dev pallet_fast_unstake
    
    Co-authored-by: command-bot <>

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -591,10 +591,13 @@ impl pallet_staking::Config for Runtime {
 impl pallet_fast_unstake::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type ControlOrigin = frame_system::EnsureRoot<AccountId>;
-	type BatchSize = ConstU32<128>;
+	type BatchSize = ConstU32<64>;
 	type Deposit = ConstU128<{ DOLLARS }>;
 	type Currency = Balances;
 	type Staking = Staking;
+	type MaxErasToCheckPerBlock = ConstU32<1>;
+	#[cfg(feature = "runtime-benchmarks")]
+	type MaxBackersPerValidator = MaxNominatorRewardedPerValidator;
 	type WeightInfo = ();
 }
 
