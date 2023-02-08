df8a1f1356a9848a8bae5e6c50f7f1a8a3a54ccb
Achim Schneider <achim@parity.io>
2023-02-08 08:40:46 +0100

    serialize_uncomrpessed

--- primitives/arkworks/src/utils.rs
+++ primitives/arkworks/src/utils.rs
@@ -4,6 +4,6 @@ use ark_std::{io::Cursor, vec, vec::Vec};
 pub fn serialize_result(result: impl CanonicalSerialize) -> Vec<u8> {
 	let mut serialized_result = vec![0u8; result.serialized_size(Compress::No)];
 	let mut cursor = Cursor::new(&mut serialized_result[..]);
-	result.serialize_compressed(&mut cursor).unwrap();
+	result.serialize_uncompressed(&mut cursor).unwrap();
 	serialized_result
 }
