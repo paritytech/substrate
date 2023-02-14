a773e7ff834eda99e1af0ece6b6b36e26cc94b5d
Sam Johnson <sam@durosoft.com>
2023-02-13 20:15:58 -0500

    add UI tests covering complex path for BenchmarkResult, BenchmarkError

---
+++ frame/support/test/tests/benchmark_ui/pass/valid_complex_path_benchmark_result.rs
@@ -0,0 +1,17 @@
+use frame_benchmarking::v2::*;
+#[allow(unused_imports)]
+use frame_support_test::Config;
+
+#[benchmarks]
+mod benches {
+	use super::*;
+
+	#[benchmark]
+	fn bench() -> frame_benchmarking::v2::BenchmarkResult<()> {
+		#[block]
+		{}
+		Ok(())
+	}
+}
+
+fn main() {}
