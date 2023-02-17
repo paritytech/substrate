c5c23d17e2a34bc1c40301a68493463842bf391b
Dmitry Markin <dmitry@markin.tech>
2023-02-17 11:52:41 +0300

    Add comment that we must call `poll_flush` on out substream even if no data was written

--- client/network/src/protocol/notifications/handler.rs
+++ client/network/src/protocol/notifications/handler.rs
@@ -782,6 +782,9 @@ impl ConnectionHandler for NotifsHandler {
 		// performed before the code paths that can produce `Ready` (with some rare exceptions).
 		// Importantly, however, the flush is performed *after* notifications are queued with
 		// `Sink::start_send`.
+		// Note that we must call `poll_flush` on all substreams and not only on those we
+		// have called `Sink::start_send` on, because `NotificationsOutSubstream::poll_flush`
+		// also reports the substream termination (even if no data was written into it).
 		for protocol_index in 0..self.protocols.len() {
 			match &mut self.protocols[protocol_index].state {
 				State::Open { out_substream: out_substream @ Some(_), .. } => {
