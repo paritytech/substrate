bdb9a79192fdcc818dbc0fb7a0f290afa8673780
Alisher A. Khassanov <a.khssnv@gmail.com>
2023-02-17 17:03:53 +0600

    Update spec_version

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -129,7 +129,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 302,
+	spec_version: 30200,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
