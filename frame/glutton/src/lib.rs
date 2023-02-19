f944813952d72004c5e92216099f1569490779ee
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-02-19 14:00:40 +0100

    Revert "Update frame/glutton/src/lib.rs"
    This reverts commit 98a237afd27de3deb15ba381871e12f71a9d71b1.
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/glutton/src/lib.rs
+++ frame/glutton/src/lib.rs
@@ -121,7 +121,10 @@ pub mod pallet {
 			let proof_size_limit = Storage::<T>::get().mul_floor(meter.remaining().proof_size());
 			let computation_weight_limit =
 				Compute::<T>::get().mul_floor(meter.remaining().ref_time());
-			meter.check_accrue(Weight::from_parts(computation_weight_limit, proof_size_limit));
+			let mut meter = WeightMeter::from_limit(Weight::from_parts(
+				computation_weight_limit,
+				proof_size_limit,
+			));
 
 			Self::waste_at_most_proof_size(&mut meter);
 			Self::waste_at_most_ref_time(&mut meter);
