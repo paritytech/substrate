8d8117269207f59ef639dd1058a4179c444c8678
Vivek Pandya <vivekvpandya@gmail.com>
2023-02-19 19:35:28 +0530

    Fix remaining files

--- client/network/sync/src/warp_request_handler.rs
+++ client/network/sync/src/warp_request_handler.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 Parity Technologies (UK) Ltd.
+// Copyright Parity Technologies (UK) Ltd.
 // This file is part of Substrate.
 
 // Substrate is free software: you can redistribute it and/or modify
