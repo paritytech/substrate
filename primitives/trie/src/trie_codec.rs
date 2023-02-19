74bd6ca21ba4f74228db3b95e62699f15f419ca3
Vivek Pandya <vivekvpandya@gmail.com>
2023-02-19 10:45:37 +0530

    Remove years from copy right header

--- primitives/trie/src/trie_codec.rs
+++ primitives/trie/src/trie_codec.rs
@@ -1,6 +1,6 @@
 // This file is part of Substrate.
 
-// Copyright (C) 2021-2023 Parity Technologies (UK) Ltd.
+// Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
