ef63fdfc7b026264535f762691a607ce37b1713d
Ross Bulat <ross@parity.io>
2023-02-21 18:26:52 +0700

    fix test

--- frame/nomination-pools/src/tests.rs
+++ frame/nomination-pools/src/tests.rs
@@ -2131,7 +2131,7 @@ mod unbond {
 				assert_ok!(Pools::unbond(RuntimeOrigin::signed(20), 20, 15));
 				assert_eq!(PoolMembers::<Runtime>::get(20).unwrap().active_points(), 0);
 				assert_eq!(PoolMembers::<Runtime>::get(20).unwrap().unbonding_points(), 20);
-				assert_eq!(ClaimPermissions::<Runtime>::get(20), ClaimPermission::Permissioned);
+				assert_eq!(ClaimPermissions::<Runtime>::get(20), ClaimPermission::PermissionlessAll);
 			})
 	}
 
