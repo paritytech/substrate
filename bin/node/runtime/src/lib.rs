4cc19ccf0498e1aa12cbcf0f236fbc5362e324a3
Ross Bulat <ross@parity.io>
2023-02-23 14:14:09 +0700

    formatting

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -2009,7 +2009,7 @@ impl_runtime_apis! {
 	}
 
 	impl pallet_staking_runtime_api::StakingApi<Block, Balance> for Runtime {
-	fn inflation_rate() -> Perquintill {
+		fn inflation_rate() -> Perquintill {
 			Staking::api_inflation_rate(IDEAL_STAKE_BASE, FALLOFF)
 		}
 		fn nominations_quota(balance: Balance) -> u32 {
