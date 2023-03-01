98550eb2fe77e9ae96d4122cff4d586a9edfa95f
Ankan <ankan.anurag@gmail.com>
2023-03-01 01:56:57 +0100

    add max exposure page count to staking interface

--- frame/fast-unstake/src/benchmarking.rs
+++ frame/fast-unstake/src/benchmarking.rs
@@ -74,7 +74,7 @@ fn setup_staking<T: Config>(v: u32, until: EraIndex) {
 		.collect::<Vec<_>>();
 
 	for era in 0..=until {
-		let others = (0..T::MaxExposurePageSize::get())
+		let others = (0..T::Staking::max_exposure_page_size())
 			.map(|s| {
 				let who = frame_benchmarking::account::<T::AccountId>("nominator", era, s);
 				let value = ed;
