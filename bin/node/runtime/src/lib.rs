59d70c974b7cc282a61b04429951e25b7908bd4d
Gav <gavin@parity.io>
2023-03-05 16:07:29 +0000

    Fixes

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -1601,6 +1601,7 @@ impl pallet_core_fellowship::Config for Runtime {
 	type Members = RankedCollective;
 	type Balance = Balance;
 	type ParamsOrigin = frame_system::EnsureRoot<AccountId>;
+	type InductOrigin = pallet_core_fellowship::EnsureInducted<Runtime, (), 1>;
 	type ProofOrigin = frame_system::EnsureRootWithSuccess<AccountId, ConstU16<9>>;
 	type PromoteOrigin = frame_system::EnsureRootWithSuccess<AccountId, ConstU16<9>>;
 }
