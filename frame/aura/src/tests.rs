fc0649a1352109a1ea7656117a63682569bec649
Vivek Pandya <vivekvpandya@gmail.com>
2023-03-10 09:31:34 +0530

    Use Hooks instead of OnInitialize trait in aura pallet

--- frame/aura/src/tests.rs
+++ frame/aura/src/tests.rs
@@ -21,7 +21,7 @@
 
 use crate::mock::{new_test_ext, Aura, MockDisabledValidators, System};
 use codec::Encode;
-use frame_support::traits::OnInitialize;
+use frame_support::traits::Hooks;
 use sp_consensus_aura::{Slot, AURA_ENGINE_ID};
 use sp_runtime::{Digest, DigestItem};
 
