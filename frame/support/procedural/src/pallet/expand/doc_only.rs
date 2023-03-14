09ec374d5c7baa216fa93892a066fdd932b22953
Sam Johnson <sam@durosoft.com>
2023-03-14 17:04:05 -0400

    remove unused variables

--- frame/support/procedural/src/pallet/expand/doc_only.rs
+++ frame/support/procedural/src/pallet/expand/doc_only.rs
@@ -21,10 +21,7 @@ use crate::pallet::Def;
 
 pub fn expand_doc_only(def: &mut Def) -> proc_macro2::TokenStream {
 	let storage_names = &def.storages.iter().map(|storage| &storage.ident).collect::<Vec<_>>();
-	let storage_viss = &def.storages.iter().map(|storage| &storage.vis).collect::<Vec<_>>();
 	let storage_docs = &def.storages.iter().map(|storage| &storage.docs).collect::<Vec<_>>();
-	let storage_generics =
-		&def.storages.iter().map(|storage| &storage.named_generics).collect::<Vec<_>>();
 	let dispatchables = {
 		if let Some(call_def) = &def.call {
 			let type_impl_generics = def.type_impl_generics(Span::call_site());
