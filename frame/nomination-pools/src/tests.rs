bbea2d2647272f1327cfc414d87d9cbb5b202109
Ross Bulat <ross@parity.io>
2023-03-14 12:23:04 +0700

    add last_recorded_total_payouts_needs_commission

--- frame/nomination-pools/src/tests.rs
+++ frame/nomination-pools/src/tests.rs
@@ -6500,6 +6500,59 @@ mod commission {
 	}
 
 	#[test]
+	fn last_recorded_total_payouts_needs_commission() {
+		ExtBuilder::default().build_and_execute(|| {
+			let pool_id = 1;
+
+			// Given:
+
+			// Set initial commission of pool 1 to 10%.
+			assert_ok!(Pools::set_commission(
+				RuntimeOrigin::signed(900),
+				pool_id,
+				Some((Perbill::from_percent(10), 2)),
+			));
+			assert_eq!(
+				pool_events_since_last_call(),
+				vec![
+					Event::Created { depositor: 10, pool_id: 1 },
+					Event::Bonded { member: 10, pool_id: 1, bonded: 10, joined: true },
+					Event::PoolCommissionUpdated {
+						pool_id: 1,
+						current: Some((Perbill::from_percent(10), 2))
+					},
+				]
+			);
+
+			// When:
+
+			// The pool earns 100 points
+			assert_ok!(Balances::mutate_account(&default_reward_account(), |a| a.free += 100));
+
+			// Claim payout:
+			assert_ok!(Pools::claim_payout(RuntimeOrigin::signed(10)));
+
+			// Claim commission:
+			assert_ok!(Pools::claim_commission(RuntimeOrigin::signed(900), pool_id));
+
+			// Then:
+
+			assert_eq!(
+				RewardPools::<Runtime>::get(1).unwrap().last_recorded_total_payouts,
+				90 + 10
+			);
+
+			assert_eq!(
+				pool_events_since_last_call(),
+				vec![
+					Event::PaidOut { member: 10, pool_id: 1, payout: 90 },
+					Event::PoolCommissionClaimed { pool_id: 1, commission: 0 }
+				]
+			);
+		})
+	}
+
+	#[test]
 	fn do_reward_payout_with_100_percent_commission() {
 		ExtBuilder::default().build_and_execute(|| {
 			// turn off GlobalMaxCommission for this test.
