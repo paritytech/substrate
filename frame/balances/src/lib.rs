fc5ff3d52bcd026c9f0198f611d1f8e076c24faa
Gav <gavin@parity.io>
2023-03-14 08:09:20 +0000

    Fixes

--- frame/balances/src/lib.rs
+++ frame/balances/src/lib.rs
@@ -186,7 +186,7 @@ use sp_runtime::{
 		AtLeast32BitUnsigned, Bounded, CheckedAdd, CheckedSub, MaybeSerializeDeserialize,
 		Saturating, StaticLookup, Zero,
 	},
-	ArithmeticError, DispatchError, FixedPointOperand, RuntimeDebug,
+	ArithmeticError, DispatchError, FixedPointOperand, Perbill, RuntimeDebug,
 };
 use sp_std::{cmp, fmt::Debug, mem, prelude::*, result};
 pub use types::{AccountData, BalanceLock, DustCleaner, IdAmount, Reasons, ReserveData};
@@ -692,7 +692,8 @@ pub mod pallet {
 					upgrade_count.saturating_inc();
 				}
 			}
-			if Perbill::from_rational(upgrade_count, who.len()) >= Perbill::from_percent(90) {
+			let proportion_upgraded = Perbill::from_rational(upgrade_count, who.len() as u32);
+			if proportion_upgraded >= Perbill::from_percent(90) {
 				Ok(Pays::No.into())
 			} else {
 				Ok(Pays::Yes.into())
