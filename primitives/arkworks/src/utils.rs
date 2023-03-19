2b1cd004f9f3f7cb1b0513c794f9ea781bb75ef1
Achim Schneider <achim@parity.io>
2023-03-19 14:15:58 +0100

    vec without capacity

--- primitives/arkworks/src/utils.rs
+++ primitives/arkworks/src/utils.rs
@@ -26,8 +26,7 @@ where
 	T: CanonicalDeserialize + Sized,
 {
 	let mut cursor = Cursor::new(bytes.to_vec());
-	let length = bytes.len() / 8;
-	let mut result = Vec::with_capacity(length as usize);
+	let mut result = Vec::new();
 	for _ in 0..length {
 		result.push(T::deserialize_uncompressed_unchecked(&mut cursor)?);
 	}
