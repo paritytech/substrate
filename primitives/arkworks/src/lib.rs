db922ca3edc5610ce1d3d012b66bcc452a74b0a8
Achim Schneider <achim@parity.io>
2023-03-20 09:37:07 +0100

    add error

--- primitives/arkworks/src/lib.rs
+++ primitives/arkworks/src/lib.rs
@@ -6,3 +6,14 @@ pub mod bw6_761;
 pub mod ed_on_bls12_377;
 pub mod ed_on_bls12_381;
 mod utils;
+
+/// Error computing an elliptic curve pairing
+#[derive(Encode, Decode, Debug)]
+pub enum PairingError {
+	#[codec(index = 1)]
+	InternalPanic,
+	#[codec(index = 2)]
+	FinalExpInverse,
+	#[codec(index = 3)]
+	MillerLoopCofactor,
+}
