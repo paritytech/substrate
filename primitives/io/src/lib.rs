c282f6244630674dc390b4891f2e41b95bfd346c
Achim Schneider <achim@parity.io>
2023-03-20 10:53:20 +0100

    import PairingError

--- primitives/io/src/lib.rs
+++ primitives/io/src/lib.rs
@@ -1129,12 +1129,12 @@ pub trait EllipticCurves {
 	fn bls12_381_multi_miller_loop(
 		a: Vec<Vec<u8>>,
 		b: Vec<Vec<u8>>,
-	) -> Result<Vec<u8>, PairingError> {
+	) -> Result<Vec<u8>, sp_arkworks::PairingError> {
 		sp_arkworks::bls12_381::multi_miller_loop(a, b)
 	}
 
 	/// Compute a final exponentiation on bls12_381
-	fn bls12_381_final_exponentiation(f12: Vec<u8>) -> Result<Vec<u8>, PairingError> {
+	fn bls12_381_final_exponentiation(f12: Vec<u8>) -> Result<Vec<u8>, sp_arkworks::PairingError> {
 		sp_arkworks::bls12_381::final_exponentiation(f12)
 	}
 
@@ -1172,12 +1172,12 @@ pub trait EllipticCurves {
 	fn bls12_377_multi_miller_loop(
 		a: Vec<Vec<u8>>,
 		b: Vec<Vec<u8>>,
-	) -> Result<Vec<u8>, PairingError> {
+	) -> Result<Vec<u8>, sp_arkworks::PairingError> {
 		sp_arkworks::bls12_377::multi_miller_loop(a, b)
 	}
 
 	/// Compute a final exponentiation for bls12_377
-	fn bls12_377_final_exponentiation(f12: Vec<u8>) -> Result<Vec<u8>, PairingError> {
+	fn bls12_377_final_exponentiation(f12: Vec<u8>) -> Result<Vec<u8>, sp_arkworks::PairingError> {
 		sp_arkworks::bls12_377::final_exponentiation(f12)
 	}
 
@@ -1215,12 +1215,12 @@ pub trait EllipticCurves {
 	fn bw6_761_multi_miller_loop(
 		a: Vec<Vec<u8>>,
 		b: Vec<Vec<u8>>,
-	) -> Result<Vec<u8>, PairingError> {
+	) -> Result<Vec<u8>, sp_arkworks::PairingError> {
 		sp_arkworks::bw6_761::multi_miller_loop(a, b)
 	}
 
 	/// Compute a final exponentiation on bw6_761
-	fn bw6_761_final_exponentiation(f12: Vec<u8>) -> Result<Vec<u8>, PairingError> {
+	fn bw6_761_final_exponentiation(f12: Vec<u8>) -> Result<Vec<u8>, sp_arkworks::PairingError> {
 		sp_arkworks::bw6_761::final_exponentiation(f12)
 	}
 
