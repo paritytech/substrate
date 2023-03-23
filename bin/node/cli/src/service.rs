8263aa407ca1957c40aa965b226689b04827b9dc
arkpar <arkady.paronyan@gmail.com>
2023-03-23 12:54:54 +0100

    Build fix

--- bin/node/cli/src/service.rs
+++ bin/node/cli/src/service.rs
@@ -282,11 +282,11 @@ pub fn new_partial(
 		(rpc_extensions_builder, shared_voter_state2)
 	};
 
-	let statement_store = sc_statement_store::Store::new(
+	let statement_store = Arc::new(sc_statement_store::Store::new(
 		config.database.path().unwrap(),
 		client.clone(),
 		config.prometheus_registry(),
-	)?;
+	)?);
 
 	Ok(sc_service::PartialComponents {
 		client,
