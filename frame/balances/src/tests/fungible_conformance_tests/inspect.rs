9e3dc4ecdaa44d9031bd971b4edbbf1f06642875
Liam Aharon <liam.aharon@hotmail.com>
2023-03-28 16:50:40 +0400

    wrap inspect tests in a macro

--- frame/balances/src/tests/fungible_conformance_tests/inspect.rs
+++ frame/balances/src/tests/fungible_conformance_tests/inspect.rs
@@ -20,118 +20,120 @@
 //! TODO: Decouple these tests from balances and abstract them into a macro so that they can be
 //! used by any pallet that implements the `fungible` Inspect trait.
 
-use super::*;
-use frame_support::traits::tokens::{
-	Fortitude::{Force, Polite},
-	Preservation::{Expendable, Preserve, Protect},
-};
-use fungible::{Inspect, Mutate, MutateFreeze};
+macro_rules! fungible_inspect_conformance_tests {
+	($ext_builder:ident, $pallet:ident) => {
+		use super::*;
+		use frame_support::traits::tokens::{
+			Fortitude::{Force, Polite},
+			Preservation::{Expendable, Preserve, Protect},
+		};
+		use fungible::{Inspect, Mutate, MutateFreeze};
 
-#[test]
-fn total_issuance_works() {
-	ExtBuilder::default().build_and_execute_with(|| {
-		assert_eq!(Balances::total_issuance(), 0);
-		Balances::set_balance(&1, 100);
-		assert_eq!(Balances::total_issuance(), 100);
-		Balances::set_balance(&2, 100);
-		assert_eq!(Balances::total_issuance(), 200);
-		Balances::set_balance(&2, 101);
-		assert_eq!(Balances::total_issuance(), 201);
-	});
+		#[test]
+		fn total_issuance_works() {
+			$ext_builder::default().build_and_execute_with(|| {
+				assert_eq!($pallet::total_issuance(), 0);
+				$pallet::set_balance(&1, 100);
+				assert_eq!($pallet::total_issuance(), 100);
+				$pallet::set_balance(&2, 100);
+				assert_eq!($pallet::total_issuance(), 200);
+				$pallet::set_balance(&2, 101);
+				assert_eq!($pallet::total_issuance(), 201);
+			});
+		}
+
+		#[test]
+		fn active_issuance_works() {
+			$ext_builder::default().existential_deposit(10).build_and_execute_with(|| {
+				// TODO: Is there an official definition for what it means for issuance to be
+				// 'controlled by the system'? If there is no offical definition, then I think it
+				// cannot be tested in a generic way and we should remove this test.
+				assert!(true);
+			});
+		}
+
+		#[test]
+		fn minimum_balance_works() {
+			$ext_builder::default().existential_deposit(5).build_and_execute_with(|| {
+				assert_eq!($pallet::minimum_balance(), 5);
+			});
+			$ext_builder::default().existential_deposit(10).build_and_execute_with(|| {
+				assert_eq!($pallet::minimum_balance(), 10);
+			});
+		}
+
+		#[test]
+		fn total_balance_works() {
+			$ext_builder::default().existential_deposit(10).build_and_execute_with(|| {
+				assert_eq!($pallet::total_balance(&1), 0);
+				$pallet::set_balance(&1, 100);
+				assert_eq!($pallet::total_balance(&1), 100);
+				$pallet::set_balance(&2, 100);
+				assert_eq!($pallet::total_balance(&2), 100);
+				$pallet::set_balance(&2, 101);
+				assert_eq!($pallet::total_balance(&2), 101);
+			});
+		}
+
+		// TODO: Properly understand Preservation and Fortitude enums, and ensure every permutation
+		// is thoroughly tested.
+		#[test]
+		fn reducible_balance_basic_works() {
+			$ext_builder::default().existential_deposit(10).build_and_execute_with(|| {
+				$pallet::set_balance(&1, 100);
+				assert_eq!($pallet::reducible_balance(&1, Expendable, Polite), 100);
+				assert_eq!($pallet::reducible_balance(&1, Protect, Polite), 90);
+				assert_eq!($pallet::reducible_balance(&1, Preserve, Polite), 90);
+				assert_eq!($pallet::reducible_balance(&1, Expendable, Force), 100);
+				assert_eq!($pallet::reducible_balance(&1, Protect, Force), 90);
+				assert_eq!($pallet::reducible_balance(&1, Preserve, Force), 90);
+			});
+		}
+
+		#[test]
+		fn reducible_balance_other_provide_works() {
+			$ext_builder::default().existential_deposit(10).build_and_execute_with(|| {
+				$pallet::set_balance(&1, 100);
+				System::inc_providers(&1);
+				assert_eq!($pallet::reducible_balance(&1, Expendable, Polite), 100);
+				assert_eq!($pallet::reducible_balance(&1, Protect, Polite), 100);
+				assert_eq!($pallet::reducible_balance(&1, Preserve, Polite), 90);
+				assert_eq!($pallet::reducible_balance(&1, Expendable, Force), 100);
+				assert_eq!($pallet::reducible_balance(&1, Protect, Force), 100);
+				assert_eq!($pallet::reducible_balance(&1, Preserve, Force), 90);
+			});
+		}
+
+		#[test]
+		fn reducible_balance_frozen_works() {
+			$ext_builder::default().existential_deposit(10).build_and_execute_with(|| {
+				$pallet::set_balance(&1, 100);
+				assert_ok!($pallet::set_freeze(&TestId::Foo, &1, 50));
+				assert_eq!($pallet::reducible_balance(&1, Expendable, Polite), 50);
+				assert_eq!($pallet::reducible_balance(&1, Protect, Polite), 50);
+				assert_eq!($pallet::reducible_balance(&1, Preserve, Polite), 50);
+				assert_eq!($pallet::reducible_balance(&1, Expendable, Force), 90);
+				assert_eq!($pallet::reducible_balance(&1, Protect, Force), 90);
+				assert_eq!($pallet::reducible_balance(&1, Preserve, Force), 90);
+			});
+		}
+
+		#[test]
+		fn can_deposit_works() {
+			$ext_builder::default().build_and_execute_with(|| {
+				// TODO
+				assert!(true);
+			});
+		}
+
+		#[test]
+		fn can_deposit_withdraw_works() {
+			$ext_builder::default().build_and_execute_with(|| {
+				// TODO
+				assert!(true);
+			});
+		}
+	};
 }
 
-#[test]
-fn active_issuance_works() {
-	ExtBuilder::default().existential_deposit(10).build_and_execute_with(|| {
-		// fungible docs on active_issuance:
-		// "The total amount of issuance in the system excluding those which are controlled by
-		// the system."
-		//
-		// TODO: What does it mean for some issuance to be 'controlled by the system'?
-		// If there is no offical definition, then I think it cannot be tested in a generic way and
-		// we should remove this test.
-		assert!(true);
-	});
-}
-
-#[test]
-fn minimum_balance_works() {
-	ExtBuilder::default().existential_deposit(5).build_and_execute_with(|| {
-		assert_eq!(Balances::minimum_balance(), 5);
-	});
-	ExtBuilder::default().existential_deposit(10).build_and_execute_with(|| {
-		assert_eq!(Balances::minimum_balance(), 10);
-	});
-}
-
-#[test]
-fn total_balance_works() {
-	ExtBuilder::default().existential_deposit(10).build_and_execute_with(|| {
-		assert_eq!(Balances::total_balance(&1), 0);
-		Balances::set_balance(&1, 100);
-		assert_eq!(Balances::total_balance(&1), 100);
-		Balances::set_balance(&2, 100);
-		assert_eq!(Balances::total_balance(&2), 100);
-		Balances::set_balance(&2, 101);
-		assert_eq!(Balances::total_balance(&2), 101);
-	});
-}
-
-// TODO: Properly understand Preservation and Fortitude enums, and ensure every permutation is
-// thoroughly tested.
-#[test]
-fn reducible_balance_basic_works() {
-	ExtBuilder::default().existential_deposit(10).build_and_execute_with(|| {
-		Balances::set_balance(&1, 100);
-		assert_eq!(Balances::reducible_balance(&1, Expendable, Polite), 100);
-		assert_eq!(Balances::reducible_balance(&1, Protect, Polite), 90);
-		assert_eq!(Balances::reducible_balance(&1, Preserve, Polite), 90);
-		assert_eq!(Balances::reducible_balance(&1, Expendable, Force), 100);
-		assert_eq!(Balances::reducible_balance(&1, Protect, Force), 90);
-		assert_eq!(Balances::reducible_balance(&1, Preserve, Force), 90);
-	});
-}
-
-#[test]
-fn reducible_balance_other_provide_works() {
-	ExtBuilder::default().existential_deposit(10).build_and_execute_with(|| {
-		Balances::set_balance(&1, 100);
-		System::inc_providers(&1);
-		assert_eq!(Balances::reducible_balance(&1, Expendable, Polite), 100);
-		assert_eq!(Balances::reducible_balance(&1, Protect, Polite), 100);
-		assert_eq!(Balances::reducible_balance(&1, Preserve, Polite), 90);
-		assert_eq!(Balances::reducible_balance(&1, Expendable, Force), 100);
-		assert_eq!(Balances::reducible_balance(&1, Protect, Force), 100);
-		assert_eq!(Balances::reducible_balance(&1, Preserve, Force), 90);
-	});
-}
-
-#[test]
-fn reducible_balance_frozen_works() {
-	ExtBuilder::default().existential_deposit(10).build_and_execute_with(|| {
-		Balances::set_balance(&1, 100);
-		assert_ok!(Balances::set_freeze(&TestId::Foo, &1, 50));
-		assert_eq!(Balances::reducible_balance(&1, Expendable, Polite), 50);
-		assert_eq!(Balances::reducible_balance(&1, Protect, Polite), 50);
-		assert_eq!(Balances::reducible_balance(&1, Preserve, Polite), 50);
-		assert_eq!(Balances::reducible_balance(&1, Expendable, Force), 90);
-		assert_eq!(Balances::reducible_balance(&1, Protect, Force), 90);
-		assert_eq!(Balances::reducible_balance(&1, Preserve, Force), 90);
-	});
-}
-
-#[test]
-fn can_deposit_works() {
-	ExtBuilder::default().build_and_execute_with(|| {
-		// TODO
-		assert!(true);
-	});
-}
-
-#[test]
-fn can_deposit_withdraw_works() {
-	ExtBuilder::default().build_and_execute_with(|| {
-		// TODO
-		assert!(true);
-	});
-}
+fungible_inspect_conformance_tests!(ExtBuilder, Balances);
