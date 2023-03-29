b65d1d5f2fc98ab5acd0310d1df4b6ecdfb23ed7
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-03-29 16:44:13 +0200

    Cleanup safe-mode benches
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/tx-pause/src/benchmarking.rs
+++ frame/tx-pause/src/benchmarking.rs
@@ -23,35 +23,31 @@ use frame_benchmarking::benchmarks;
 
 benchmarks! {
 	pause {
-		let full_name: FullNameOf<T> = (name::<T>(b"SomePalletName"), name::<T>(b"SomePalletName"));
-		let origin = T::PauseOrigin::try_successful_origin().expect("Tx-pause pallet does not make sense without pause origin");
+		let origin = T::PauseOrigin::try_successful_origin()
+			.expect("Tx-pause pallet is not usable without pause origin");
+		let full_name = name::<T>();
 
 	}: _<T::RuntimeOrigin>(origin, full_name.clone())
 	verify {
-		assert!(TxPause::<T>::paused_calls(full_name.clone()).is_some())
+		assert!(TxPause::<T>::paused_calls(full_name).is_some())
 	}
 
   unpause {
-		let full_name: FullNameOf<T> = (name::<T>(b"SomePalletName"), name::<T>(b"SomePalletName"));
-		let pause_origin = T::PauseOrigin::try_successful_origin().expect("Tx-pause pallet does not make sense without pause origin");
-
-		TxPause::<T>::pause(
-			pause_origin,
-			full_name.clone(),
-			)?;
-
-		let unpause_origin = T::UnpauseOrigin::try_successful_origin().expect("Tx-pause pallet does not make sense without un-pause origin");
-		// let call = Call::<T>::unpause { pallet_name: pallet_name.clone(), maybe_call_name: maybe_call_name.clone() };
+		let unpause_origin = T::UnpauseOrigin::try_successful_origin()
+			.expect("Tx-pause pallet is not usable without pause origin");
+		let full_name = name::<T>();
+		TxPause::<T>::do_pause(full_name.clone()).unwrap();
 
 		}: _<T::RuntimeOrigin>(unpause_origin, full_name.clone())
 	verify {
-		assert!(TxPause::<T>::paused_calls(full_name.clone()).is_none())
-
+		assert!(TxPause::<T>::paused_calls(full_name).is_none())
 	}
 
 	impl_benchmark_test_suite!(TxPause, crate::mock::new_test_ext(), crate::mock::Test);
 }
 
-pub fn name<T: Config>(bytes: &[u8]) -> BoundedVec<u8, T::MaxNameLen> {
-	bytes.to_vec().try_into().unwrap()
+/// Longest possible name.
+fn name<T: Config>() -> FullNameOf<T> {
+	let max_len = T::MaxNameLen::get() as usize;
+	(vec![1; max_len].try_into().unwrap(), vec![1; max_len].try_into().unwrap())
 }
