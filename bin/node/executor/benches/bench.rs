7054f42abf5562b20fead6c26d3d1769c8558440
yjhmelody <yjh465402634@gmail.com>
2023-03-30 22:01:36 +0800

    fix bench

--- bin/node/executor/benches/bench.rs
+++ bin/node/executor/benches/bench.rs
@@ -191,9 +191,9 @@ fn bench_execute_block(c: &mut Criterion) {
 	for strategy in execution_methods {
 		group.bench_function(format!("{:?}", strategy), |b| {
 			let genesis_config = node_testing::genesis::config(Some(compact_code_unwrap()));
-			let (use_native, wasm_method) = match strategy {
-				ExecutionMethod::Native => (true, WasmExecutionMethod::default()),
-				ExecutionMethod::Wasm(wasm_method) => (false, wasm_method),
+			let use_native = match strategy {
+				ExecutionMethod::Native => true,
+				ExecutionMethod::Wasm(..) => false,
 			};
 
 			let executor =
