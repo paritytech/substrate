149b6db9661d1e70414e06cea1be05ac8d7d1c5c
Alexander Gryaznov <hi@agryaznov.com>
2023-04-04 21:33:34 +0300

    update links to ink!

--- frame/contracts/src/lib.rs
+++ frame/contracts/src/lib.rs
@@ -76,7 +76,7 @@
 //! The Contract module is a work in progress. The following examples show how this Contract module
 //! can be used to instantiate and call contracts.
 //!
-//! * [`ink`](https://github.com/paritytech/ink) is
+//! * [`ink!`](https://use.ink) is
 //! an [`eDSL`](https://wiki.haskell.org/Embedded_domain_specific_language) that enables writing
 //! WebAssembly based smart contracts in the Rust programming language.
 
