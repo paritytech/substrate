ee95acf39c4b47be07f317bcc13905ff7e637e19
AlexD10S <alexfraga10@gmail.com>
2023-04-04 17:25:16 +0100

    use benchmarking v2 syntax to generate the weights

--- bin/node-template/pallets/template/src/lib.rs
+++ bin/node-template/pallets/template/src/lib.rs
@@ -69,7 +69,7 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::call_index(0)]
-		#[pallet::weight(T::WeightInfo::do_something(*something))]
+		#[pallet::weight(T::WeightInfo::do_something())]
 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
