5b508e9263456b5d22afffbe58d9057666545d03
Gonçalo Pestana <g6pestana@gmail.com>
2023-04-05 14:14:46 +0100

    Mention `on_idle` round-robin logic to `trait Hooks` cargo doc (#13797)
    * Adds on_idle round-robin logic to trait Hooks docs
    
    * Makes the docs more concise
    
    * Update frame/support/src/traits/hooks.rs
    
    Co-authored-by: Bastian Köcher <git@kchr.de>
    
    * fmt
    
    ---------
    
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- frame/support/src/traits/hooks.rs
+++ frame/support/src/traits/hooks.rs
@@ -227,10 +227,15 @@ pub trait Hooks<BlockNumber> {
 	fn on_finalize(_n: BlockNumber) {}
 
 	/// This will be run when the block is being finalized (before `on_finalize`).
-	/// Implement to have something happen using the remaining weight.
-	/// Will not fire if the remaining weight is 0.
-	/// Return the weight used, the hook will subtract it from current weight used
-	/// and pass the result to the next `on_idle` hook if it exists.
+	///
+	/// Implement to have something happen using the remaining weight. Will not fire if the
+	/// remaining weight is 0.
+	///
+	/// Each pallet's `on_idle` is chosen to be the first to execute in a round-robin fashion
+	/// indexed by the block number.
+	///
+	/// Return the weight used, the caller will use this to calculate the remaining weight and then
+	/// call the next pallet `on_idle` hook if there is still weight left.
 	fn on_idle(_n: BlockNumber, _remaining_weight: Weight) -> Weight {
 		Weight::zero()
 	}
