17849938f040218ca3afb54b729663a0824775af
Liam Aharon <liam.aharon@hotmail.com>
2023-04-06 19:11:19 +1000

    Update frame/support/src/remove_pallet.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- frame/support/src/remove_pallet.rs
+++ frame/support/src/remove_pallet.rs
@@ -1,4 +1,4 @@
-// Copyright 2017-2023 Parity Technologies (UK) Ltd.
+// Copyright Parity Technologies (UK) Ltd.
 // This file is part of Substrate.
 
 // Polkadot is free software: you can redistribute it and/or modify
