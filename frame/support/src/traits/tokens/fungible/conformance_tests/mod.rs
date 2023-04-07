7544e0f26ceddda6631f4eb25e2f73fb5f790fa8
Liam Aharon <liam.aharon@hotmail.com>
2023-04-07 11:32:38 +0400

    remove balanced stub

--- frame/support/src/traits/tokens/fungible/conformance_tests/mod.rs
+++ frame/support/src/traits/tokens/fungible/conformance_tests/mod.rs
@@ -1,7 +1,1 @@
-/// Temporary module to hold the conformance tests for the Fungible trait,
-/// until it is abstracted into pallet-agnostic macro/s and moved outside
-/// of the balances module.
-///
-/// Open question: Do we need conformance tests for 'unbalanced' traits?
-pub mod balanced;
 pub mod inspect_mutate;
