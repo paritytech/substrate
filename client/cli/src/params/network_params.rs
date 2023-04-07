a924e996f9fd282108ac93bc2489ba54f2410daf
Aaro Altonen <48052676+altonen@users.noreply.github.com>
2023-04-07 10:30:58 +0300

    Update client/cli/src/params/network_params.rs

--- client/cli/src/params/network_params.rs
+++ client/cli/src/params/network_params.rs
@@ -150,7 +150,7 @@ pub struct NetworkParams {
 	/// Maximum number of blocks per request.
 	///
 	/// Try reducing this number from the default value if you have a slow network connection
-	/// and observe block request timing out.
+	/// and observe block requests timing out.
 	#[arg(long, value_name = "COUNT", default_value_t = 64)]
 	pub max_blocks_per_request: u32,
 }
