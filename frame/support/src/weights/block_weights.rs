ad0aac0a17ba889f0457d46e0566a0d5bd96e71a
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-04-11 09:51:44 +0000

    Re-run weights on bm2
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/support/src/weights/block_weights.rs
+++ frame/support/src/weights/block_weights.rs
@@ -16,8 +16,8 @@
 // limitations under the License.
 
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
-//! DATE: 2023-03-15 (Y/M/D)
-//! HOSTNAME: `bm3`, CPU: `Intel(R) Core(TM) i7-7700K CPU @ 4.20GHz`
+//! DATE: 2023-04-06 (Y/M/D)
+//! HOSTNAME: `bm2`, CPU: `Intel(R) Core(TM) i7-7700K CPU @ 4.20GHz`
 //!
 //! SHORT-NAME: `block`, LONG-NAME: `BlockExecution`, RUNTIME: `Development`
 //! WARMUPS: `10`, REPEAT: `100`
@@ -44,17 +44,17 @@ parameter_types! {
 	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
 	///
 	/// Stats nanoseconds:
-	///   Min, Max: 402_748, 458_228
-	///   Average:  412_772
-	///   Median:   406_151
-	///   Std-Dev:  13480.33
+	///   Min, Max: 386_820, 419_676
+	///   Average:  392_184
+	///   Median:   389_668
+	///   Std-Dev:  5285.57
 	///
 	/// Percentiles nanoseconds:
-	///   99th: 450_080
-	///   95th: 445_111
-	///   75th: 414_170
+	///   99th: 406_316
+	///   95th: 399_697
+	///   75th: 396_532
 	pub const BlockExecutionWeight: Weight =
-		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(412_772), 0);
+		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(392_184), 0);
 }
 
 #[cfg(test)]
