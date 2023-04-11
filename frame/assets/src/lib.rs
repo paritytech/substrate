348dfefcdd498a62f9577b4f15434b3c4cdc6954
muharem <ismailov.m.h@gmail.com>
2023-04-11 17:26:49 +0200

    storage version back to 1

--- frame/assets/src/lib.rs
+++ frame/assets/src/lib.rs
@@ -203,7 +203,7 @@ pub mod pallet {
 	use frame_system::pallet_prelude::*;
 
 	/// The current storage version.
-	const STORAGE_VERSION: StorageVersion = StorageVersion::new(2);
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
 
 	#[pallet::pallet]
 	#[pallet::storage_version(STORAGE_VERSION)]
