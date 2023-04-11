880aeb00e8aa48d7409af2d723ff1c1dfff7ab3b
Liam Aharon <liam.aharon@hotmail.com>
2023-04-11 10:40:39 +0400

    add test doc comments

--- frame/support/src/traits/tokens/fungible/conformance_tests/inspect_mutate.rs
+++ frame/support/src/traits/tokens/fungible/conformance_tests/inspect_mutate.rs
@@ -25,10 +25,16 @@ use core::fmt::Debug;
 use sp_arithmetic::traits::AtLeast8BitUnsigned;
 use sp_runtime::traits::{Bounded, Zero};
 
-//
-// mint_into
-//
-
+/// Test the `mint_into` function for successful token minting.
+///
+/// This test checks the `mint_into` function in the `Mutate` trait implementation for type `T`.
+/// It ensures that account balances and total issuance values are updated correctly after minting
+/// tokens into two distinct accounts.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn mint_into_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -57,6 +63,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + amount_0 + amount_1);
 }
 
+/// Test the `mint_into` function for overflow prevention.
+///
+/// This test ensures that minting tokens beyond the maximum balance value for an account
+/// returns an error and does not change the account balance or total issuance values.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn mint_into_overflow<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -83,6 +98,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + amount);
 }
 
+/// Test the `mint_into` function for handling balances below the minimum value.
+///
+/// This test verifies that minting tokens below the minimum balance for an account
+/// returns an error and has no impact on the account balance or total issuance values.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn mint_into_below_minimum<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -109,10 +133,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance);
 }
 
-//
-// burn_from
-//
-
+/// Test the `burn_from` function for successfully burning an exact amount of tokens.
+///
+/// This test checks that the `burn_from` function with `Precision::Exact` correctly
+/// reduces the account balance and total issuance values by the burned amount.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate` for `AccountId`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn burn_from_exact_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -143,6 +172,16 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + initial_balance - amount_to_burn);
 }
 
+/// Test the `burn_from` function for successfully burning tokens with a best-effort approach.
+///
+/// This test verifies that the `burn_from` function with `Precision::BestEffort` correctly
+/// reduces the account balance and total issuance values by the reducible balance when
+/// attempting to burn an amount greater than the reducible balance.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate` for `AccountId`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn burn_from_best_effort_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -178,6 +217,16 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + initial_balance - reducible_balance);
 }
 
+/// Test the `burn_from` function for handling insufficient funds with `Precision::Exact`.
+///
+/// This test verifies that burning an amount greater than the account's balance with
+/// `Precision::Exact` returns an error and does not change the account balance or total issuance
+/// values.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn burn_from_exact_insufficient_funds<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -204,10 +253,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance);
 }
 
-//
-// restore
-//
-
+/// Test the `restore` function for successful restoration.
+///
+/// This test verifies that restoring an amount into each account updates their balances and the
+/// total issuance values correctly.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn restore_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -236,6 +290,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + amount_0 + amount_1);
 }
 
+/// Test the `restore` function for handling balance overflow.
+///
+/// This test verifies that restoring an amount beyond the maximum balance returns an error and
+/// does not change the account balance or total issuance values.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn restore_overflow<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -260,6 +323,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + amount);
 }
 
+/// Test the `restore` function for handling restoration below the minimum balance.
+///
+/// This test verifies that restoring an amount below the minimum balance returns an error and
+/// does not change the account balance or total issuance values.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn restore_below_minimum<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -286,10 +358,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance);
 }
 
-//
-// shelve tests
-//
-
+/// Test the `shelve` function for successful shelving.
+///
+/// This test verifies that shelving an amount from an account reduces the account balance and
+/// total issuance values by the shelved amount.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn shelve_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -319,6 +396,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + initial_balance - amount_to_shelve);
 }
 
+/// Test the `shelve` function for handling insufficient funds.
+///
+/// This test verifies that attempting to shelve an amount greater than the account's balance
+/// returns an error and does not change the account balance or total issuance values.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn shelve_insufficient_funds<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -344,10 +430,16 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + initial_balance);
 }
 
-//
-// transfer
-//
-
+/// Test the `transfer` function for a successful transfer.
+///
+/// This test verifies that transferring an amount between two accounts with
+/// `Preservation::Expendable` updates the account balances and maintains the total issuance and
+/// active issuance values.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn transfer_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -377,6 +469,17 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + initial_balance * 2.into());
 }
 
+/// Test the `transfer` function with `Preservation::Expendable` for transferring the entire
+/// balance.
+///
+/// This test verifies that transferring the entire balance from one account to another with
+/// `Preservation::Expendable` updates the account balances and maintains the total issuance and
+/// active issuance values.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn transfer_expendable_all<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -407,6 +510,23 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + initial_balance * 2.into());
 }
 
+/// Test the transfer function with Preservation::Expendable for transferring amounts that leaves
+/// an account with less than the minimum balance.
+///
+/// This test verifies that when transferring an amount using Preservation::Expendable and an
+/// account will be left with less than the minimum balance, the account balances are updated, dust
+/// is collected properly depending on whether a dust_trap exists, and the total issuance and active
+/// issuance values remain consistent.
+///
+/// # Parameters
+///
+/// - dust_trap: An optional account identifier to which dust will be collected. If None, dust will
+///   be removed from the total and active issuance.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn transfer_expendable_dust<T, AccountId>(dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -470,6 +590,17 @@ where
 	}
 }
 
+/// Test the `transfer` function with `Preservation::Protect` and `Preservation::Preserve` for
+/// transferring the entire balance.
+///
+/// This test verifies that attempting to transfer the entire balance with `Preservation::Protect`
+/// or `Preservation::Preserve` returns an error, and the account balances, total issuance, and
+/// active issuance values remain unchanged.
+///
+/// # Type Parameters
+///
+/// - `T`: Implements `Mutate<AccountId>`.
+/// - `AccountId`: Account identifier implementing `AtLeast8BitUnsigned`.
 pub fn transfer_protect_preserve<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -521,10 +652,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + initial_balance * 2.into());
 }
 
-//
-// set_balance
-//
-
+/// Test the set_balance function for successful minting.
+///
+/// This test verifies that minting a balance using set_balance updates the account balance, total
+/// issuance, and active issuance correctly.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn set_balance_mint_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -552,6 +688,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + expected_new);
 }
 
+/// Test the set_balance function for successful burning.
+///
+/// This test verifies that burning a balance using set_balance updates the account balance, total
+/// issuance, and active issuance correctly.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn set_balance_burn_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -579,10 +724,15 @@ where
 	assert_eq!(T::active_issuance(), initial_active_issuance + expected_new);
 }
 
-//
-// can_deposit
-//
-
+/// Test the can_deposit function for returning a success value.
+///
+/// This test verifies that the can_deposit function returns DepositConsequence::Success when
+/// depositing a reasonable amount.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn can_deposit_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -600,6 +750,15 @@ where
 	assert_eq!(ret, DepositConsequence::Success);
 }
 
+/// Test the can_deposit function for returning a minimum balance error.
+///
+/// This test verifies that the can_deposit function returns DepositConsequence::BelowMinimum when
+/// depositing below the minimum balance.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn can_deposit_below_minimum<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -620,6 +779,15 @@ where
 	assert_eq!(ret, DepositConsequence::BelowMinimum);
 }
 
+/// Test the can_deposit function for returning an overflow error.
+///
+/// This test verifies that the can_deposit function returns DepositConsequence::Overflow when
+/// depositing an amount that would cause an overflow.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn can_deposit_overflow<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -637,10 +805,15 @@ where
 	assert_eq!(ret, DepositConsequence::Overflow);
 }
 
-//
-// can_withdraw
-//
-
+/// Test the can_withdraw function for returning a success value.
+///
+/// This test verifies that the can_withdraw function returns WithdrawConsequence::Success when
+/// withdrawing a reasonable amount.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn can_withdraw_success<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -658,6 +831,15 @@ where
 	assert_eq!(ret, WithdrawConsequence::Success);
 }
 
+/// Test the can_withdraw function for withdrawal resulting in a reduced balance of zero.
+///
+/// This test verifies that the can_withdraw function returns WithdrawConsequence::ReducedToZero
+/// when withdrawing an amount that would reduce the account balance below the minimum balance.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn can_withdraw_reduced_to_zero<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -677,6 +859,15 @@ where
 	assert_eq!(ret, WithdrawConsequence::ReducedToZero(T::minimum_balance() - 1.into()));
 }
 
+/// Test the can_withdraw function for returning a low balance error.
+///
+/// This test verifies that the can_withdraw function returns WithdrawConsequence::BalanceLow when
+/// withdrawing an amount that would result in an account balance below the current balance.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn can_withdraw_balance_low<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -698,10 +889,15 @@ where
 	assert_eq!(ret, WithdrawConsequence::BalanceLow);
 }
 
-//
-// reducible_balance
-//
-
+/// Test the reducible_balance function with Preservation::Expendable.
+///
+/// This test verifies that the reducible_balance function returns the full account balance when
+/// using Preservation::Expendable.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn reducible_balance_expendable<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
@@ -717,6 +913,15 @@ where
 	assert_eq!(ret, initial_balance);
 }
 
+/// Test the reducible_balance function with Preservation::Protect and Preservation::Preserve.
+///
+/// This test verifies that the reducible_balance function returns the account balance minus the
+/// minimum balance when using either Preservation::Protect or Preservation::Preserve.
+///
+/// # Type Parameters
+///
+/// - T: Implements Mutate<AccountId>.
+/// - AccountId: Account identifier implementing AtLeast8BitUnsigned.
 pub fn reducible_balance_protect_preserve<T, AccountId>(_dust_trap: Option<AccountId>)
 where
 	T: Mutate<AccountId>,
