44a9f6af98e59b05600f42901d6578eb30a3bfa3
William Freudenberger <w.freude@icloud.com>
2023-04-12 08:22:23 +0200

    tests: update balance mock

--- frame/asset-rate/src/mock.rs
+++ frame/asset-rate/src/mock.rs
@@ -77,6 +77,10 @@ impl pallet_balances::Config for Test {
 	type MaxLocks = ();
 	type MaxReserves = ();
 	type ReserveIdentifier = [u8; 8];
+	type HoldIdentifier = ();
+	type FreezeIdentifier = ();
+	type MaxHolds = ();
+	type MaxFreezes = ();
 }
 
 impl pallet_asset_rate::Config for Test {
