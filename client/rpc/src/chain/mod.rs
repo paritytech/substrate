73a4a0e3c41e8e7c88a910138e99fe66d75d1e9f
yjhmelody <yjh465402634@gmail.com>
2023-04-18 15:26:25 +0800

    chore(docs): improve some comments

--- client/rpc/src/chain/mod.rs
+++ client/rpc/src/chain/mod.rs
@@ -59,10 +59,10 @@ where
 		}
 	}
 
-	/// Get header of a relay chain block.
+	/// Get header of a chain block.
 	fn header(&self, hash: Option<Block::Hash>) -> Result<Option<Block::Header>, Error>;
 
-	/// Get header and body of a relay chain block.
+	/// Get header and body of a chain block.
 	fn block(&self, hash: Option<Block::Hash>) -> Result<Option<SignedBlock<Block>>, Error>;
 
 	/// Get hash of the n-th block in the canon chain.
