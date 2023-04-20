c6cd8d98b7aa8f4111090b952c362428913f7f47
Juan Girini <juangirini@gmail.com>
2023-04-20 16:50:59 +0200

    contracts: fix failing wasm test

--- frame/contracts/src/wasm/mod.rs
+++ frame/contracts/src/wasm/mod.rs
@@ -2991,11 +2991,11 @@ mod tests {
 		const CODE_CALLER_IS_ROOT: &str = r#"
 ;; This runs `caller_is_root` check on zero account address
 (module
-	(import "seal0" "seal_caller_is_root" (func $seal_caller_is_root (result i32)))
+	(import "seal0" "caller_is_root" (func $caller_is_root (result i32)))
 	(import "seal0" "seal_return" (func $seal_return (param i32 i32 i32)))
 	(import "env" "memory" (memory 1 1))
 
-	;; [0, 4) here the return code of the `seal_caller_is_root` will be stored
+	;; [0, 4) here the return code of the `caller_is_root` will be stored
 	;; we initialize it with non-zero value to be sure that it's being overwritten below
 	(data (i32.const 0) "\10\10\10\10")
 
@@ -3004,9 +3004,9 @@ mod tests {
 	(func (export "call")
 		(i32.store
 			(i32.const 0)
-			(call $seal_caller_is_root)
+			(call $caller_is_root)
 		)
-		;; exit with success and take `seal_caller_is_root` return code to the output buffer
+		;; exit with success and take `caller_is_root` return code to the output buffer
 		(call $seal_return (i32.const 0) (i32.const 0) (i32.const 4))
 	)
 )
