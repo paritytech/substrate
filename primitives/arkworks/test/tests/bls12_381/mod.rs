fad84e07441a577e8f1d4da05f391dc0998a95ba
Achim Schneider <achim@parity.io>
2023-04-22 13:37:49 +0200

    fix tests

--- primitives/arkworks/test/tests/bls12_381/mod.rs
+++ primitives/arkworks/test/tests/bls12_381/mod.rs
@@ -1,14 +1,16 @@
+#![cfg_attr(not(feature = "std"), no_std)]
 use ark_algebra_test_templates::*;
-use ark_ff::{fields::Field, UniformRand, Zero};
+use ark_ff::{fields::Field, One, Zero};
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Compress, Validate};
-use ark_std::{rand::Rng, test_rng, vec, vec::Vec, One};
-use sp_ark_models::{AffineRepr, CurveGroup, Group};
+use ark_std::{rand::Rng, test_rng, vec, vec::Vec, UniformRand};
+use sp_ark_models::{pairing::PairingOutput, AffineRepr, CurveGroup, Group};
 
-use sp_ark_bls12_381::{
+use crate::{
 	fq::Fq, fq2::Fq2, fr::Fr, G1Affine as G1Affine_Host, G1Projective as G1Projective_Host,
 	G2Affine as G2Affine_Host, G2Projective as G2Projective_Host, HostFunctions,
 };
 
+#[derive(PartialEq, Eq)]
 pub struct Host {}
 
 impl HostFunctions for Host {
@@ -32,19 +34,19 @@ impl HostFunctions for Host {
 	}
 }
 
+test_group!(g1; crate::G1Projective<super::Host>; sw);
+test_group!(g2; crate::G2Projective<super::Host>; sw);
+test_group!(pairing_output; PairingOutput<crate::Bls12_381<super::Host>>; msm);
+test_pairing!(ark_pairing; crate::Bls12_381<super::Host>);
+
 type G1Projective = G1Projective_Host<Host>;
 type G1Affine = G1Affine_Host<Host>;
 type G2Projective = G2Projective_Host<Host>;
 type G2Affine = G2Affine_Host<Host>;
 
-test_group!(g1; sp_ark_bls12_381::G1Projective<super::Host>; sw);
-test_group!(g2; sp_ark_bls12_381::G2Projective<super::Host>; sw);
-test_group!(pairing_output; ark_ec::pairing::PairingOutput<sp_ark_bls12_381::Bls12_381<super::Host>>; msm);
-test_pairing!(pairing; sp_ark_bls12_381::Bls12_381<super::Host>);
-
 #[test]
 fn test_g1_endomorphism_beta() {
-	assert!(sp_ark_bls12_381::g1::BETA.pow(&[3u64]).is_one());
+	assert!(crate::g1::BETA.pow([3u64]).is_one());
 }
 
 #[test]
@@ -62,7 +64,7 @@ fn test_g1_subgroup_non_membership_via_endomorphism() {
 		let greatest = rng.gen();
 
 		if let Some(p) = G1Affine::get_point_from_x_unchecked(x, greatest) {
-			if !<sp_ark_models::short_weierstrass::Projective<sp_ark_bls12_381::g1::Config<Host>> as ark_std::Zero>::is_zero(&p.mul_bigint(Fr::characteristic())) {
+			if !<sp_ark_models::short_weierstrass::Projective<crate::g1::Config<Host>> as ark_std::Zero>::is_zero(&p.mul_bigint(Fr::characteristic())) {
                 assert!(!p.is_in_correct_subgroup_assuming_on_curve());
                 return;
             }
@@ -85,7 +87,7 @@ fn test_g2_subgroup_non_membership_via_endomorphism() {
 		let greatest = rng.gen();
 
 		if let Some(p) = G2Affine::get_point_from_x_unchecked(x, greatest) {
-			if !<sp_ark_models::short_weierstrass::Projective<sp_ark_bls12_381::g2::Config::<Host>> as ark_std::Zero>::is_zero(&p.mul_bigint(Fr::characteristic())) {
+			if !<sp_ark_models::short_weierstrass::Projective<crate::g2::Config::<Host>> as ark_std::Zero>::is_zero(&p.mul_bigint(Fr::characteristic())) {
                 assert!(!p.is_in_correct_subgroup_assuming_on_curve());
                 return;
             }
@@ -126,7 +128,7 @@ macro_rules! test_vectors {
 #[test]
 fn g1_compressed_valid_test_vectors() {
 	let bytes: &'static [u8] = include_bytes!("g1_compressed_valid_test_vectors.dat");
-	test_vectors!(G1Projective, G1Affine, Compress::No, bytes);
+	test_vectors!(G1Projective, G1Affine, Compress::Yes, bytes);
 }
 
 #[test]
@@ -138,7 +140,7 @@ fn g1_uncompressed_valid_test_vectors() {
 #[test]
 fn g2_compressed_valid_test_vectors() {
 	let bytes: &'static [u8] = include_bytes!("g2_compressed_valid_test_vectors.dat");
-	test_vectors!(G2Projective, G2Affine, Compress::No, bytes);
+	test_vectors!(G2Projective, G2Affine, Compress::Yes, bytes);
 }
 
 #[test]
