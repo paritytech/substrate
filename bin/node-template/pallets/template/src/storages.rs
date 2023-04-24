2f9a178fa1ab25c60b6d5cec69f4508a9b2ef6ad
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2023-04-24 09:43:13 +0530

    Moves template palet back

--- bin/node-template/pallets/template/src/storages.rs
+++
@@ -1,9 +0,0 @@
-use frame_support::pallet_macros::*;
-
-#[export_section]
-mod storages {
-
-    #[pallet::storage]
-	#[pallet::getter(fn something2)]
-	pub type Something2<T> = StorageValue<_, u32>;
-}
