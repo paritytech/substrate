ead8ba3d8ea2e43d3bc9ff5c17d45cacf4cd113a
PG Herveou <pgherveou@gmail.com>
2023-04-25 23:03:41 +0200

    Apply suggestions from code review
    Co-authored-by: Sasha Gryaznov <hi@agryaznov.com>

--- frame/contracts/src/tests.rs
+++ frame/contracts/src/tests.rs
@@ -2984,7 +2984,7 @@ fn sr25519_verify() {
 }
 
 #[test]
-fn test_failed_charge_should_roll_back_call() {
+fn failed_deposit_charge_should_roll_back_call() {
 	let (wasm_caller, _) = compile_module::<Test>("call_runtime_and_call").unwrap();
 	let (wasm_callee, _) = compile_module::<Test>("store").unwrap();
 
