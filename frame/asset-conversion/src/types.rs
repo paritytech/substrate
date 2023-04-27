fb5236857d1eb922b294e5a356941883b7498ffe
Giles Cope <gilescope@gmail.com>
2023-04-27 10:03:41 +0100

    remove unused imports

--- frame/asset-conversion/src/types.rs
+++ frame/asset-conversion/src/types.rs
@@ -16,8 +16,8 @@
 // limitations under the License.
 
 use super::*;
-use core::{fmt::Display, marker::PhantomData};
-use sp_std::{cmp::Ordering, fmt::Formatter};
+use core::marker::PhantomData;
+use sp_std::cmp::Ordering;
 
 use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::traits::fungibles::Inspect;
