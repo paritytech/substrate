814176e71ebed4f9d3f6f586056a17b11665661c
Sam Johnson <sam@durosoft.com>
2023-04-27 22:14:09 -0400

    fix derive_impl_attr_args_parsing test

--- frame/support/procedural/src/derive_impl.rs
+++ frame/support/procedural/src/derive_impl.rs
@@ -48,16 +48,19 @@ impl ToTokens for DeriveImplAttrArgs {
 
 #[test]
 fn test_derive_impl_attr_args_parsing() {
-	parse2::<DeriveImplAttrArgs>(quote!(some::path::TestDefaultConfig, some::path::DefaultConfig))
-		.unwrap();
 	parse2::<DeriveImplAttrArgs>(quote!(
-		frame_system::preludes::testing::TestDefaultConfig,
-		DefaultConfig
+		some::path::TestDefaultConfig as some::path::DefaultConfig
 	))
 	.unwrap();
-	parse2::<DeriveImplAttrArgs>(quote!(Something, some::path::DefaultConfig)).unwrap();
-	parse2::<DeriveImplAttrArgs>(quote!(Something, DefaultConfig)).unwrap();
-	assert!(parse2::<DeriveImplAttrArgs>(quote!(DefaultConfig)).is_err());
+	parse2::<DeriveImplAttrArgs>(quote!(
+		frame_system::preludes::testing::TestDefaultConfig as DefaultConfig
+	))
+	.unwrap();
+	parse2::<DeriveImplAttrArgs>(quote!(Something as some::path::DefaultConfig)).unwrap();
+	parse2::<DeriveImplAttrArgs>(quote!(Something as DefaultConfig)).unwrap();
+	parse2::<DeriveImplAttrArgs>(quote!(DefaultConfig)).unwrap();
+	assert!(parse2::<DeriveImplAttrArgs>(quote!()).is_err());
+	assert!(parse2::<DeriveImplAttrArgs>(quote!(Config Config)).is_err());
 }
 
 /// Gets the [`Ident`] representation of the given [`ImplItem`], if one exists. Otherwise
