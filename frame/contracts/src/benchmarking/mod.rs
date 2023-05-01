25235f60345a550ae5063093cec1c5b9a55dbfab
Juan Girini <juangirini@gmail.com>
2023-05-01 17:12:48 +0200

    contracts: fix caller_is_root benchmarking

--- frame/contracts/src/benchmarking/mod.rs
+++ frame/contracts/src/benchmarking/mod.rs
@@ -537,9 +537,22 @@ benchmarks! {
 	#[pov_mode = Measured]
 	seal_caller_is_root {
 		let r in 0 .. API_BENCHMARK_RUNS;
-		let instance = Contract::<T>::new(WasmModule::getter(
-			"seal0", "caller_is_root", r
-		), vec![])?;
+
+		let code = WasmModule::<T>::from(ModuleDefinition {
+			memory: Some(ImportedMemory::max::<T>()),
+			imported_functions: vec![ImportedFunction {
+				module: "seal0",
+				name: "caller_is_root",
+				params: vec![],
+				return_type: Some(ValueType::I32),
+			}],
+			call_body: Some(body::repeated(r, &[
+				Instruction::Call(0),
+				Instruction::Drop,
+			])),
+			.. Default::default()
+		});
+		let instance = Contract::<T>::new(code, vec![])?;
 		let origin = RawOrigin::Root;
 	}: call(origin, instance.addr, 0u32.into(), Weight::MAX, None, vec![])
 
