d8b0012f1291bec9c49f4f8d4e01efc24644e476
Ross Bulat <ross@parity.io>
2023-05-09 12:54:53 +0800

    import

--- frame/session/benchmarking/src/lib.rs
+++ frame/session/benchmarking/src/lib.rs
@@ -34,7 +34,7 @@ use frame_support::{
 use frame_system::RawOrigin;
 use pallet_session::{historical::Pallet as Historical, Pallet as Session, *};
 use pallet_staking::{
-	benchmarking::create_validator_with_nominators, testing_utils::create_validators,
+	benchmarking::create_validator_with_nominators, testing_utils::create_validators, RewardDestination
 };
 
 const MAX_VALIDATORS: u32 = 1000;
