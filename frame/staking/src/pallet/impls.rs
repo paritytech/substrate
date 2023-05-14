75f70bc86adfb8eac3c24143b7c855c8eea499e7
Ross Bulat <ross@parity.io>
2023-05-14 23:08:57 +0700

    fmt

--- frame/staking/src/pallet/impls.rs
+++ frame/staking/src/pallet/impls.rs
@@ -1014,10 +1014,11 @@ impl<T: Config> Pallet<T> {
 					println!("stake is more than ideal.");
 					let curve_base = ideal_interest * ideal_stake - min_inflation;
 					println!("curve base: {:?}", curve_base);
-					let curve_power= (staked_as_percent - ideal_stake).int_div(falloff);
+					let curve_power = (staked_as_percent - ideal_stake).int_div(falloff);
 					println!("{:?} divided by {:?}", staked_as_percent - ideal_stake, falloff);
 					println!("curve power: {:?}", curve_power);
-					let curve_multiplier = Perquintill::from_percent(1) / Perquintill::from_percent(2u64.pow(curve_power as u32));
+					let curve_multiplier = Perquintill::from_percent(1) /
+						Perquintill::from_percent(2u64.pow(curve_power as u32));
 					println!("curve multiplier: {:?}", curve_multiplier);
 					println!("final curve: {:?} * {:?}", curve_base, curve_multiplier);
 					curve_base.saturating_mul(curve_multiplier)
