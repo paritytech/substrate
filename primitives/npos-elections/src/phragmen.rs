18cb506e8c2a3fb80d75ea4ac25b3f3faa0e89b5
Bernhard Schuster <bernhard@ahoi.io>
2023-05-15 11:08:25 +0200

    import vec

--- primitives/npos-elections/src/phragmen.rs
+++ primitives/npos-elections/src/phragmen.rs
@@ -30,6 +30,7 @@ use sp_arithmetic::{
 	Rational128, Rounding,
 };
 use sp_std::prelude::*;
+use sp_std::vec;
 
 /// The denominator used for loads. Since votes are collected as u64, the smallest ratio that we
 /// might collect is `1/approval_stake` where approval stake is the sum of votes. Hence, some number
