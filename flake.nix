b8c4f7c38fa7770637960bc75757f63602c8dc22
André Silva <andrerfosilva@gmail.com>
2023-05-17 23:02:20 +0100

    improve flake.nix

--- flake.nix
+++ flake.nix
@@ -13,60 +13,40 @@
   };
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     let
-      per_system = flake-utils.lib.eachDefaultSystem (system:
+      outputs = flake-utils.lib.eachDefaultSystem (system:
         let
           overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs {
             inherit system overlays;
           };
-          rust-native-build-inputs = with pkgs; [ clang pkg-config ];
-          rust-src = pkgs.lib.cleanSourceWith {
-            src = pkgs.lib.cleanSource ./.;
-            filter = pkgs.nix-gitignore.gitignoreFilterPure
+          substrate-src = with pkgs; lib.cleanSourceWith {
+            src = lib.cleanSource ./.;
+            filter = nix-gitignore.gitignoreFilterPure
               (name: type:
-                (
-                  (type == "regular" && pkgs.lib.strings.hasSuffix ".nix" name)
-                  == false
-                  &&
-                  (type == "directory" && ".github" == name) == false
-                )
+                !(type == "regular" && lib.strings.hasSuffix ".nix" name
+                  ||
+                  type == "directory" && ".github" == name)
               )
               [ ./.gitignore ] ./.;
           };
+          rust-platform = with pkgs; makeRustPlatform {
+            cargo = rust-bin.beta.latest.default;
+            rustc = rust-bin.beta.latest.default;
+          };
           rust-env = with pkgs; {
-            LD_LIBRARY_PATH = pkgs.lib.strings.makeLibraryPath [
-              pkgs.stdenv.cc.cc.lib
-              pkgs.llvmPackages.libclang.lib
-            ];
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-            PROTOC = "${pkgs.protobuf}/bin/protoc";
-            ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib";
+            PROTOC = "${lib.makeBinPath [ protobuf ]}/protoc";
+            ROCKSDB_LIB_DIR = lib.makeLibraryPath [ rocksdb ];
           };
-
-          darwin = pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
-            frameworks.Security
-          ]);
-
-          rust-libs = {
-            buildInputs = with pkgs; [ openssl ] ++ darwin;
-            nativeBuildInputs = rust-native-build-inputs;
-            doCheck = false;
-          };
-          rust-deps = pkgs.makeRustPlatform {
-            inherit pkgs;
-            cargo = pkgs.rust-bin.beta.latest.default;
-            rustc = pkgs.rust-bin.beta.latest.default;
-          };
-          subkey = with pkgs; rust-deps.buildRustPackage (rust-libs // rust-env // rec {
+          subkey = rust-platform.buildRustPackage (rust-env // {
             name = "subkey";
-            src = rust-src;
+            src = substrate-src;
             cargoLock = {
               lockFile = ./Cargo.lock;
             };
+            nativeBuildInputs = [ rust-platform.bindgenHook ];
             doCheck = false;
-            cargoBuildFlags = "--package ${name}";
+            buildAndTestSubdir = "bin/utils/subkey";
           });
-
         in
         {
           packages = {
@@ -75,9 +55,9 @@
         }
       );
     in
-    per_system // {
-      overlays = final: prev: {
-        subkey = per_system.packages.${prev.system}.subkey;
+    outputs // {
+      overlays.default = final: prev: {
+        subkey = outputs.packages.${prev.system}.subkey;
       };
     };
 }
