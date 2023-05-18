fcd2ee997dca21cf9c4f5dd6b4d403e58ace9f18
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2023-05-18 13:10:52 +0200

    helper removed

--- primitives/genesis-builder/src/lib.rs
+++ primitives/genesis-builder/src/lib.rs
@@ -17,5 +17,27 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub mod api;
-pub mod helper;
+//! Substrate genesis config builder
+//!
+//! This Runtime API allows to construct `GenesisConfig`, in particular:
+//! - serialize the runtime default `GenesisConfig` struct into json format,
+//! - put the GenesisConfig struct into the storage. Internally this operation calls
+//!   `GenesisBuild::build` function
+//! for all runtime pallets, which is typically provided by pallet's author.
+//! - deserialize the GenesisConfig from given json blob and put GenesisConfig into the state
+//!   storage. Allows to build
+//! customized configuration.
+//!
+//! Providing externalities with empty storage and putting GenesisConfig into storage allows to
+//! catch and build the raw storage of `GenesisConfig` which is the foundation for genesis block.
+
+sp_api::decl_runtime_apis! {
+	/// API to interact with GenesisConfig for the runtime
+	pub trait GenesisBuilder {
+		/// Instantiate default `GenesisConfig` and serializes it to json blob.
+		fn default_genesis_config_as_json() -> sp_std::vec::Vec<u8>;
+
+		/// Deserialize the `GenesisConfig` from given json blob and put it into the storage.
+		fn build_genesis_config_from_json(json: sp_std::vec::Vec<u8>);
+	}
+}
