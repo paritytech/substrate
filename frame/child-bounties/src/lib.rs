25c79215a2381e09697009c1b8f6774ea707943b
Liam Aharon <liam.aharon@hotmail.com>
2023-05-18 16:56:11 +0400

    init child bounties version to zero

--- frame/child-bounties/src/lib.rs
+++ frame/child-bounties/src/lib.rs
@@ -130,7 +130,7 @@ pub mod pallet {
 	use super::*;
 
 	/// The current storage version.
-	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
 
 	#[pallet::pallet]
 	#[pallet::storage_version(STORAGE_VERSION)]
