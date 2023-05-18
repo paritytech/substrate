42fbea2b6b974946c78b2c5846a67e604ce6fd48
Liam Aharon <liam.aharon@hotmail.com>
2023-05-18 16:36:03 +0400

    add childbounties storage version

--- frame/child-bounties/src/lib.rs
+++ frame/child-bounties/src/lib.rs
@@ -129,7 +129,11 @@ pub mod pallet {
 
 	use super::*;
 
+	/// The current storage version.
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
 	#[pallet::pallet]
+	#[pallet::storage_version(STORAGE_VERSION)]
 	pub struct Pallet<T>(_);
 
 	#[pallet::config]
