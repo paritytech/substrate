69e1d940970f4319efcc6ab54d43cd6c5a35cda9
Achim Schneider <achim@parity.io>
2023-05-22 07:07:03 +0200

    reorgainze tests

--- primitives/arkworks/src/ed_on_bls12_377.rs
+++ primitives/arkworks/src/ed_on_bls12_377.rs
@@ -32,3 +32,24 @@ pub fn mul_projective(base: Vec<u8>, scalar: Vec<u8>) -> Result<Vec<u8>, ()> {
 pub fn msm(bases: Vec<u8>, scalars: Vec<u8>) -> Result<Vec<u8>, ()> {
 	msm_te_generic::<EdwardsConfig>(bases, scalars)
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use ark_algebra_test_templates::*;
+	use sp_ark_ed_on_bls12_377::{EdwardsProjective as EdwardsProjectiveHost, HostFunctions};
+
+	struct Host {}
+
+	impl HostFunctions for Host {
+		fn ed_on_bls12_377_msm(bases: Vec<u8>, scalars: Vec<u8>) -> Result<Vec<u8>, ()> {
+			sp_io::elliptic_curves::ed_on_bls12_377_msm(bases, scalars)
+		}
+		fn ed_on_bls12_377_mul_projective(base: Vec<u8>, scalar: Vec<u8>) -> Result<Vec<u8>, ()> {
+			sp_io::elliptic_curves::ed_on_bls12_377_mul_projective(base, scalar)
+		}
+	}
+
+	type EdwardsProjective = EdwardsProjectiveHost<Host>;
+	test_group!(te; EdwardsProjective; te);
+}
--- primitives/arkworks/src/ed_on_bls12_381_bandersnatch.rs
+++ primitives/arkworks/src/ed_on_bls12_381_bandersnatch.rs
@@ -44,3 +44,47 @@ pub fn te_msm(bases: Vec<u8>, scalars: Vec<u8>) -> Result<Vec<u8>, ()> {
 pub fn sw_msm(bases: Vec<u8>, scalars: Vec<u8>) -> Result<Vec<u8>, ()> {
 	msm_sw_generic::<BandersnatchConfig>(bases, scalars)
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use ark_algebra_test_templates::*;
+	use sp_ark_ed_on_bls12_381_bandersnatch::{
+		EdwardsProjective as EdwardsProjectiveHost, HostFunctions, SWProjective as SWProjectiveHost,
+	};
+
+	pub struct Host {}
+
+	impl HostFunctions for Host {
+		fn ed_on_bls12_381_bandersnatch_te_msm(
+			bases: Vec<u8>,
+			scalars: Vec<u8>,
+		) -> Result<Vec<u8>, ()> {
+			sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_te_msm(bases, scalars)
+		}
+		fn ed_on_bls12_381_bandersnatch_sw_msm(
+			bases: Vec<u8>,
+			scalars: Vec<u8>,
+		) -> Result<Vec<u8>, ()> {
+			sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_sw_msm(bases, scalars)
+		}
+		fn ed_on_bls12_381_bandersnatch_te_mul_projective(
+			base: Vec<u8>,
+			scalar: Vec<u8>,
+		) -> Result<Vec<u8>, ()> {
+			sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_te_mul_projective(base, scalar)
+		}
+		fn ed_on_bls12_381_bandersnatch_sw_mul_projective(
+			base: Vec<u8>,
+			scalar: Vec<u8>,
+		) -> Result<Vec<u8>, ()> {
+			sp_io::elliptic_curves::ed_on_bls12_381_bandersnatch_sw_mul_projective(base, scalar)
+		}
+	}
+
+	type EdwardsProjective = EdwardsProjectiveHost<Host>;
+	type SWProjective = SWProjectiveHost<Host>;
+
+	test_group!(sw; SWProjective; sw);
+	test_group!(te; EdwardsProjective; te);
+}
