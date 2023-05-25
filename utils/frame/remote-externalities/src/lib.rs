fab41e0d48c601d4d036e21c2af3e7a18ca296c0
s5k0651 <suraj410401@gmail.com>
2023-05-26 04:26:33 +0530

    Add tests

--- utils/frame/remote-externalities/src/lib.rs
+++ utils/frame/remote-externalities/src/lib.rs
@@ -354,7 +354,7 @@ where
 		// number of parts to divide the prefix into
 		const NUM_PARTS: usize = 4;
 
-		let chunks = Self::split_storage_key_prefix(prefix, NUM_PARTS);
+		let chunks = split_prefix::split_storage_key_prefix(prefix, NUM_PARTS);
 		let tasks = chunks
 			.into_iter()
 			.map(|(start_key, end_key)| self.rpc_get_keys_paged_range(start_key, end_key, at))
@@ -369,52 +369,6 @@ where
 			.collect::<Vec<_>>();
 
 		Ok(all_keys)
-	}
-
-	// This function splits the prefix into `num_parts` equal chunks of ranges represented
-	// by pairs of (start_key, end_key) where start_key is the starting key of each chunk
-	// and end_key is the ending key of each chunk.
-	fn split_storage_key_prefix(
-		prefix: StorageKey,
-		num_parts: usize,
-	) -> Vec<(StorageKey, StorageKey)> {
-		let mut start_key = prefix.clone();
-		let mut chunks = vec![];
-
-		// loops `num_parts` times, each time calling the `get_next_wrapping_index` function
-		// with the current `start_key` value, and pushing the result as a tuple
-		// of `(start_key, end_key)` onto the `chunks` vector. The `start_key` value is then
-		// updated to the `end_key` value
-		for _ in 0..num_parts {
-			let end_key = StorageKey(Self::get_next_wrapping_index(&start_key.0));
-			chunks.push((start_key.clone(), end_key.clone()));
-			start_key = end_key;
-		}
-
-		chunks
-	}
-
-	// This function takes a byte slice `key` as input and returns the next wrapping index
-	// of `key`. It iterates over key in reverse order, incrementing each byte by 1
-	// and setting carry to true if the incremented byte overflows.
-	fn get_next_wrapping_index(key: &[u8]) -> Vec<u8> {
-		let mut next_index = key.to_vec();
-		let mut carry = true;
-
-		for i in (0..key.len()).rev() {
-			if carry {
-				next_index[i] = key[i].wrapping_add(1);
-				carry = next_index[i] == 0;
-			} else {
-				break
-			}
-		}
-
-		if carry {
-			next_index.insert(0, 1);
-		}
-
-		next_index
 	}
 
 	/// Retrieves all the storage keys between `start_key` (inclusive) and `end_key` (exclusive),
@@ -1121,6 +1075,55 @@ where
 	}
 }
 
+mod split_prefix {
+	use crate::StorageKey;
+	// This function splits the prefix into `num_parts` equal chunks of ranges represented
+	// by pairs of (start_key, end_key) where start_key is the starting key of each chunk
+	// and end_key is the ending key of each chunk.
+	pub fn split_storage_key_prefix(
+		prefix: StorageKey,
+		num_parts: usize,
+	) -> Vec<(StorageKey, StorageKey)> {
+		let mut start_key = prefix.clone();
+		let mut chunks = vec![];
+
+		// loops `num_parts` times, each time calling the `get_next_wrapping_index` function
+		// with the current `start_key` value, and pushing the result as a tuple
+		// of `(start_key, end_key)` onto the `chunks` vector. The `start_key` value is then
+		// updated to the `end_key` value
+		for _ in 0..num_parts {
+			let end_key = StorageKey(get_next_wrapping_index(&start_key.0));
+			chunks.push((start_key.clone(), end_key.clone()));
+			start_key = end_key;
+		}
+
+		chunks
+	}
+
+	// This function takes a byte slice `key` as input and returns the next wrapping index
+	// of `key`. It iterates over key in reverse order, incrementing each byte by 1
+	// and setting carry to true if the incremented byte overflows.
+	pub fn get_next_wrapping_index(key: &[u8]) -> Vec<u8> {
+		let mut next_index = key.to_vec();
+		let mut carry = true;
+
+		for i in (0..key.len()).rev() {
+			if carry {
+				next_index[i] = key[i].wrapping_add(1);
+				carry = next_index[i] == 0;
+			} else {
+				break
+			}
+		}
+
+		if carry {
+			next_index.insert(0, 1);
+		}
+
+		next_index
+	}
+}
+
 #[cfg(test)]
 mod test_prelude {
 	use tracing_subscriber::EnvFilter;
@@ -1432,5 +1435,51 @@ mod remote_tests {
 			.await
 			.unwrap()
 			.execute_with(|| {});
+	}
+
+	#[test]
+	fn test_split_storage_key_prefix() {
+		// Test case 1: prefix = [0, 0, 0, 0], num_parts = 5
+		let prefix = StorageKey(vec![0, 0, 0, 0]);
+		let num_parts = 5;
+		let result = split_prefix::split_storage_key_prefix(prefix, num_parts);
+		assert_eq!(
+			result,
+			vec![
+				(StorageKey(vec![0, 0, 0, 0]), StorageKey(vec![0, 0, 0, 1])),
+				(StorageKey(vec![0, 0, 0, 1]), StorageKey(vec![0, 0, 0, 2])),
+				(StorageKey(vec![0, 0, 0, 2]), StorageKey(vec![0, 0, 0, 3])),
+				(StorageKey(vec![0, 0, 0, 3]), StorageKey(vec![0, 0, 0, 4])),
+				(StorageKey(vec![0, 0, 0, 4]), StorageKey(vec![0, 0, 0, 5])),
+			]
+		);
+
+		// Test case 2: prefix = [0, 0, 0], num_parts = 4
+		let prefix = StorageKey(vec![0, 0, 0]);
+		let num_parts = 4;
+		let result = split_prefix::split_storage_key_prefix(prefix, num_parts);
+		assert_eq!(
+			result,
+			vec![
+				(StorageKey(vec![0, 0, 0]), StorageKey(vec![0, 0, 1])),
+				(StorageKey(vec![0, 0, 1]), StorageKey(vec![0, 0, 2])),
+				(StorageKey(vec![0, 0, 2]), StorageKey(vec![0, 0, 3])),
+				(StorageKey(vec![0, 0, 3]), StorageKey(vec![0, 0, 4])),
+			]
+		);
+
+		// Test case 3: prefix = [1, 2, 3], num_parts = 4
+		let prefix = StorageKey(vec![1, 2, 3]);
+		let num_parts = 4;
+		let result = split_prefix::split_storage_key_prefix(prefix, num_parts);
+		assert_eq!(
+			result,
+			vec![
+				(StorageKey(vec![1, 2, 3]), StorageKey(vec![1, 2, 4])),
+				(StorageKey(vec![1, 2, 4]), StorageKey(vec![1, 2, 5])),
+				(StorageKey(vec![1, 2, 5]), StorageKey(vec![1, 2, 6])),
+				(StorageKey(vec![1, 2, 6]), StorageKey(vec![1, 2, 7])),
+			]
+		);
 	}
 }
