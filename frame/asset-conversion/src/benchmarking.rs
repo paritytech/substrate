43adad4c1b8ce4b54f5b44441fc74e7b38bf3642
Giles Cope <gilescope@gmail.com>
2023-06-03 11:33:44 +0100

    Remove unrequired check
    The get_pool_account function no longer relies on a `_trucating` function,
    and there is a test in place
    should someone alter the function to create pool collisions.

--- frame/asset-conversion/src/benchmarking.rs
+++ frame/asset-conversion/src/benchmarking.rs
@@ -17,8 +17,6 @@
 
 //! Asset Conversion pallet benchmarking.
 
-#![cfg(feature = "runtime-benchmarks")]
-
 use super::*;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
 use frame_support::{
