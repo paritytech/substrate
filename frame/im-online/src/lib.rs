dcc7b38fa3e53876a0e1d5c76b6c3ef64c157c76
Anton Kaliaev <anton.kalyaev@gmail.com>
2023-06-05 16:24:46 +0300

    change storage value type to `()`
    https://github.com/paritytech/substrate/pull/14251#discussion_r1214268931

--- frame/im-online/src/lib.rs
+++ frame/im-online/src/lib.rs
@@ -344,7 +344,7 @@ pub mod pallet {
 	#[pallet::storage]
 	#[pallet::getter(fn received_heartbeats)]
 	pub(crate) type ReceivedHeartbeats<T: Config> =
-		StorageDoubleMap<_, Twox64Concat, SessionIndex, Twox64Concat, AuthIndex, bool>;
+		StorageDoubleMap<_, Twox64Concat, SessionIndex, Twox64Concat, AuthIndex, ()>;
 
 	/// For each session index, we keep a mapping of `ValidatorId<T>` to the
 	/// number of blocks authored by the given authority.
@@ -401,7 +401,7 @@ pub mod pallet {
 			if let (false, Some(public)) = (exists, public) {
 				Self::deposit_event(Event::<T>::HeartbeatReceived { authority_id: public.clone() });
 
-				ReceivedHeartbeats::<T>::insert(&current_session, &heartbeat.authority_index, true);
+				ReceivedHeartbeats::<T>::insert(&current_session, &heartbeat.authority_index, ());
 
 				Ok(())
 			} else if exists {
