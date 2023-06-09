61fc91b6f1211a2b0c9e5256b2b25e583214e9b8
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2023-06-09 10:27:57 +0530

    some fixes for pallet-timestamp compilation

--- frame/timestamp/src/mock.rs
+++ frame/timestamp/src/mock.rs
@@ -59,7 +59,7 @@ impl frame_system::Config for Test {
 	type Hashing = BlakeTwo256;
 	type AccountId = u64;
 	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
+	type Block = Block;
 	type RuntimeEvent = RuntimeEvent;
 	type BlockHashCount = ConstU64<250>;
 	type Version = ();
