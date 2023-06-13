9935e92464ad9a1e8544f3dff056319404a4acce
Alexander Gryaznov <hi@agryaznov.com>
2023-06-13 17:58:50 +0300

    deposits repaid non pro rata

--- frame/contracts/src/migration/v12.rs
+++ frame/contracts/src/migration/v12.rs
@@ -15,21 +15,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Move the `determinism` field to `CodeInfo` (ex. `OwnerInfo`), remove `CodeStorage` and repay
-//! deposits.
+//! Move `OwnerInfo` to `CodeInfo`, add `determinism` field to the latter, clear `CodeStorage` and
+//! repay deposits.
 
 use crate::{
 	migration::{IsFinished, Migrate},
+	storage::meter::Diff,
 	weights::WeightInfo,
 	AccountIdOf, BalanceOf, CodeHash, Config, Determinism, Pallet, Weight, LOG_TARGET,
 };
-
 use codec::{Decode, Encode};
 use frame_support::{
-	codec, pallet_prelude::*, storage_alias, BoundedVec, DefaultNoBound, Identity,
+	codec, pallet_prelude::*, storage_alias, traits::ReservableCurrency, BoundedVec,
+	DefaultNoBound, Identity,
 };
+use scale_info::prelude::format;
 #[cfg(feature = "try-runtime")]
 use sp_runtime::TryRuntimeError;
+use sp_runtime::{traits::Zero, Saturating};
 use sp_std::{marker::PhantomData, prelude::*};
 
 mod old {
@@ -61,8 +64,7 @@ mod old {
 	}
 
 	#[storage_alias]
-	pub type OwnerInfoOf<T: Config> =
-		StorageMap<Pallet<T>, Twox64Concat, CodeHash<T>, OwnerInfo<T>>;
+	pub type OwnerInfoOf<T: Config> = StorageMap<Pallet<T>, Identity, CodeHash<T>, OwnerInfo<T>>;
 
 	#[storage_alias]
 	pub type CodeStorage<T: Config> =
@@ -79,20 +81,13 @@ pub struct CodeInfo<T: Config> {
 	determinism: Determinism,
 }
 
+type CodeVec<T> = BoundedVec<u8, <T as Config>::MaxCodeLen>;
+
 #[storage_alias]
 pub type CodeInfoOf<T: Config> = StorageMap<Pallet<T>, Twox64Concat, CodeHash<T>, CodeInfo<T>>;
 
-#[cfg(feature = "runtime-benchmarks")]
-pub fn store_old_owner_info<T: Config>(len: usize) {
-	use sp_runtime::traits::Hash;
-	let info = old::OnwerInfo {
-		owner: Default::default(),
-		deposit: BalanceOf::<T>::max(),
-		refcount: u64::MAX,
-	};
-	let hash = T::Hashing::hash(&info.owner.into());
-	old::OwnerInfoOf::<T>::insert(hash, info);
-}
+#[storage_alias]
+pub type PristineCode<T: Config> = StorageMap<Pallet<T>, Identity, CodeHash<T>, CodeVec<T>>;
 
 #[derive(Encode, Decode, MaxEncodedLen, DefaultNoBound)]
 pub struct Migration<T: Config> {
@@ -108,56 +103,67 @@ impl<T: Config> Migrate for Migration<T> {
 	}
 
 	fn step(&mut self) -> (IsFinished, Weight) {
-			log::debug!(target: LOG_TARGET, "In THE STEP!");
 		let mut iter = if let Some(last_key) = self.last_key.take() {
-			old::OwnerInfoOf::<T>::iter_from(last_key.to_vec())
+			PristineCode::<T>::iter_from(last_key.to_vec())
 		} else {
-			old::OwnerInfoOf::<T>::iter()
+			PristineCode::<T>::iter()
 		};
-		log::debug!(target: LOG_TARGET, "BEFORE start iterating!");
-		if let Some((key, old)) = iter.next() {
-			log::debug!(target: LOG_TARGET, "Migrating OwnerInfo for code_hash {:?}", key);
-			let module = old::CodeStorage::<T>::take(key).unwrap_or_else(|| {
-				log::error!(
-					target: LOG_TARGET,
-					"No PrefabWasmModule found for code_hash: {:?}",
-					key
-				);
-				panic!();
-			});
-			let freed_bytes = module.encode().len();
+		if let Some((hash, code)) = iter.next() {
+			let old = old::OwnerInfoOf::<T>::take(hash)
+				.expect(format!("OwnerInfo for code_hash {:?} not found!", hash).as_str());
+
+			log::debug!(target: LOG_TARGET, "Migrating OwnerInfo for code_hash {:?}", hash);
+
+			let module = old::CodeStorage::<T>::take(hash)
+				.expect(format!("No PrefabWasmModule found for code_hash: {:?}", hash).as_str());
+
+			// We print this to measure the impact of the migration.
+			// Storage removed: deleted PrefabWasmModule's encoded len.
+			// Storage added: determinism field encoded len (as all other CodeInfo fields are the
+			// same as in the deleted OwnerInfo)
 			log::debug!(
 				target: LOG_TARGET,
-				"Freed 1 storage item with size (in bytes): {:?}",
-				freed_bytes
+				"Storage removed: 1 item, {} bytes",
+				module.encoded_size().saturating_sub(Determinism::max_encoded_len())
 			);
 
-			// TODO: remove this, as calculated determinism added bytes
-			let freed_bytes = old.encode().len();
+			let deposit =
+				Diff { bytes_added: code.len() as u32, items_added: 2, ..Default::default() }
+					.update_contract::<T>(None)
+					.charge_or_zero();
 
 			let info = CodeInfo {
 				determinism: module.determinism,
 				owner: old.owner,
-				deposit: old.deposit,
+				deposit,
 				refcount: old.refcount,
 			};
 
-			let occupied_bytes = info.encode().len() - freed_bytes;
-
-			CodeInfoOf::<T>::insert(key, info);
-			log::debug!(
-				target: LOG_TARGET,
-				"Occupied more (for storing determinism) bytes: {:?}",
-				occupied_bytes
-			);
+			let amount = old.deposit.saturating_sub(info.deposit);
+			if !amount.is_zero() {
+				T::Currency::unreserve(&info.owner, amount);
+				log::debug!(
+					target: LOG_TARGET,
+					"Storage deposit unlocked: {:?} Balance, to: {:?}",
+					&amount,
+					&info.owner
+				);
+			} else {
+				log::warn!(
+					target: LOG_TARGET,
+					"new deposit: {:?}, old deposit: {:?}",
+					&info.deposit,
+					&old.deposit
+				);
+			}
+			CodeInfoOf::<T>::insert(hash, info);
 
 			self.last_key = Some(iter.last_raw_key().to_vec().try_into().unwrap());
 
-			// TODO: repay deposit
 			// TODO: write benchmark and pass corrent len instead of freed_bytes
-			(IsFinished::No, T::WeightInfo::v12_migration_step(freed_bytes as u32))
+			(IsFinished::No, T::WeightInfo::v12_migration_step(42))
 		} else {
-			log::debug!(target: LOG_TARGET, "No more owner_info to migrate");
+			log::debug!(target: LOG_TARGET, "No more OwnerInfo to migrate");
 			(IsFinished::Yes, T::WeightInfo::v12_migration_step(0))
 		}
 	}
@@ -165,7 +171,7 @@ impl<T: Config> Migrate for Migration<T> {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade_step() -> Result<Vec<u8>, TryRuntimeError> {
 		let len = 5;
-		log::debug!(target: LOG_TARGET, "Taking sample of {} owner infos", len);
+		log::debug!(target: LOG_TARGET, "Taking sample of {} OwnerInfo(s)", len);
 		let sample: Vec<_> = old::OwnerInfoOf::<T>::iter()
 			.take(len)
 			.map(|(k, v)| {
@@ -181,22 +187,64 @@ impl<T: Config> Migrate for Migration<T> {
 			})
 			.collect();
 
-		Ok(sample.encode())
+		let storage: u32 = old::CodeStorage::<T>::iter().map(|(_k, v)| v.encoded_size() as u32).sum();
+		let mut deposit: BalanceOf<T> = Default::default();
+		old::OwnerInfoOf::<T>::iter().for_each(|(_k, v)| deposit += v.deposit);
+
+		Ok((sample, deposit, storage).encode())
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade_step(state: Vec<u8>) -> Result<(), TryRuntimeError> {
-		let sample = <Vec<(CodeHash<T>, CodeInfo<T>)> as Decode>::decode(&mut &state[..]).unwrap();
+		let state = <(Vec<(CodeHash<T>, CodeInfo<T>)>, BalanceOf<T>, u32) as Decode>::decode(
+			&mut &state[..],
+		)
+		.unwrap();
 
-		log::debug!(target: LOG_TARGET, "Validating sample of {} code infos", sample.len());
-		for (code_hash, old) in sample {
-			let info = CodeInfoOf::<T>::get(&code_hash).unwrap();
-
+		log::debug!(target: LOG_TARGET, "Validating state of {} Codeinfo(s)", state.0.len());
+		for (hash, old) in state.0 {
+			let info = CodeInfoOf::<T>::get(&hash)
+				.expect(format!("CodeInfo for code_hash {:?} not found!", hash).as_str());
 			ensure!(info.determinism == old.determinism, "invalid determinism");
 			ensure!(info.owner == old.owner, "invalid owner");
-			ensure!(info.deposit == old.deposit, "invalid deposit");
+//			ensure!(info.deposit == old.deposit, "invalid deposit");
 			ensure!(info.refcount == old.refcount, "invalid refcount");
 		}
+
+		if let Some((k, _)) = old::CodeStorage::<T>::iter().next() {
+			log::warn!(
+				target: LOG_TARGET,
+				"CodeStorage is still NOT empty, found code_hash: {:?}",
+				k
+			);
+		} else {
+			log::debug!(target: LOG_TARGET, "CodeStorage is empty.");
+		}
+		if let Some((k, _)) = old::OwnerInfoOf::<T>::iter().next() {
+			log::warn!(
+				target: LOG_TARGET,
+				"OwnerInfoOf is still NOT empty, found code_hash: {:?}",
+				k
+			);
+		} else {
+			log::debug!(target: LOG_TARGET, "OwnerInfoOf is empty.");
+		}
+
+		let mut deposit: BalanceOf<T> = Default::default();
+		let mut bytes = 0u32;
+		CodeInfoOf::<T>::iter().for_each(|(_k, v)| {
+			deposit += v.deposit;
+			bytes += 1;
+		});
+		let old_deposit = state.1;
+		let storage = state.2;
+
+		log::info!(target: LOG_TARGET, "Storage freed, bytes: {}", storage.saturating_sub(bytes));
+		log::info!(
+			target: LOG_TARGET,
+			"Deposits returned, total: {:?} Balance",
+			old_deposit.saturating_sub(deposit)
+		);
 
 		Ok(())
 	}
