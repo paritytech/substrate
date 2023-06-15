51b2f0ed6af8dd4facb18f1a489e192fd0673f7b
Anton <anton.kalyaev@gmail.com>
2023-06-15 13:42:36 +0400

    [frame/im-online] remove network state from heartbeats (#14251)
    * [frame/im-online] remove `external_addresses` from heartbeats
    
    Users should use DHT for discovering new nodes. The reason for adding external addresses was
    unstable work of authority discovery (see https://github.com/paritytech/substrate/issues/2719),
    which is now stable. Hence we can safely remove `external_addresses`.
    
    Refs https://github.com/paritytech/polkadot/issues/7181
    
    * remove unused import
    
    * run benchmark
    
    * remove external_addresses from offchain NetworkState
    
    * add missing fn to TestNetwork
    
    * Revert "run benchmark"
    
    This reverts commit a282042c2d6bf8bae2c383f6e2699c3fe2970a3d.
    
    * update weights
    
    * address @bkchr comments
    
    * remove duplicate fn
    
    * cleanup benchmarking.rs
    
    * fix executor tests
    
    * remove peer_id from hearbeat as well
    
    https://github.com/paritytech/substrate/pull/14251#discussion_r1210887220
    
    * remove MaxPeerDataEncodingSize
    
    * change storage value type to `()`
    
    https://github.com/paritytech/substrate/pull/14251#discussion_r1214268931
    
    * scaffold storage migration
    
    * no need to check the type actually
    
    * remove unnecessary types from v0 mod
    
    * add a test for migration
    
    * expose Config types
    
    + pre_upgrade and post_upgrade working fn
    
    * fix test
    
    * replace dummy type with ConstU32
    
    * add some comments to migration test
    
    * fix comment
    
    * respond to @bkchr comments
    
    * use BoundedOpaqueNetworkState::default
    
    intead of using default for each field

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -328,8 +328,9 @@ impl InstanceFilter<RuntimeCall> for ProxyType {
 					RuntimeCall::Elections(..) |
 					RuntimeCall::Treasury(..)
 			),
-			ProxyType::Staking =>
-				matches!(c, RuntimeCall::Staking(..) | RuntimeCall::FastUnstake(..)),
+			ProxyType::Staking => {
+				matches!(c, RuntimeCall::Staking(..) | RuntimeCall::FastUnstake(..))
+			},
 		}
 	}
 	fn is_superset(&self, o: &Self) -> bool {
@@ -1261,7 +1262,6 @@ parameter_types! {
 	pub const MaxAuthorities: u32 = 100;
 	pub const MaxKeys: u32 = 10_000;
 	pub const MaxPeerInHeartbeats: u32 = 10_000;
-	pub const MaxPeerDataEncodingSize: u32 = 1_000;
 }
 
 impl<LocalCall> frame_system::offchain::CreateSignedTransaction<LocalCall> for Runtime
@@ -1329,7 +1329,6 @@ impl pallet_im_online::Config for Runtime {
 	type WeightInfo = pallet_im_online::weights::SubstrateWeight<Runtime>;
 	type MaxKeys = MaxKeys;
 	type MaxPeerInHeartbeats = MaxPeerInHeartbeats;
-	type MaxPeerDataEncodingSize = MaxPeerDataEncodingSize;
 }
 
 impl pallet_offences::Config for Runtime {
