19490ae9c33a34249c747671516ecf65418d03c1
Jegor Sidorenko <jegor@parity.io>
2023-06-16 15:41:42 +0200

    One more time..

--- bin/node/cli/src/service.rs
+++ bin/node/cli/src/service.rs
@@ -98,7 +98,7 @@ pub fn create_extrinsic(
 		)),
 		frame_system::CheckNonce::<kitchensink_runtime::Runtime>::from(nonce),
 		frame_system::CheckWeight::<kitchensink_runtime::Runtime>::new(),
-		pallet_asset_tx_payment::ChargeAssetTxPayment::<kitchensink_runtime::Runtime>::from(
+		pallet_asset_conversion_tx_payment::ChargeAssetTxPayment::<kitchensink_runtime::Runtime>::from(
 			tip, None,
 		),
 	);
