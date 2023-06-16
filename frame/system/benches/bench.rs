79424ae985331f2ae55a64addaba55a67d923917
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2023-06-16 14:25:40 +0200

    fmt

--- frame/system/benches/bench.rs
+++ frame/system/benches/bench.rs
@@ -94,7 +94,10 @@ impl module::Config for Runtime {
 }
 
 fn new_test_ext() -> sp_io::TestExternalities {
-	frame_system::GenesisConfig::<Runtime>::default().build_storage().unwrap().into()
+	frame_system::GenesisConfig::<Runtime>::default()
+		.build_storage()
+		.unwrap()
+		.into()
 }
 
 fn deposit_events(n: usize) {
