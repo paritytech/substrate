b431bd489717b7ab397485cba2a980b5c90e7b5e
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2023-06-17 12:06:31 +0200

    change default rpc message buffer

--- client/cli/src/config.rs
+++ client/cli/src/config.rs
@@ -57,7 +57,7 @@ pub const RPC_DEFAULT_MAX_RESPONSE_SIZE_MB: u32 = 15;
 pub const RPC_DEFAULT_MAX_CONNECTIONS: u32 = 100;
 /// The default number of messages the RPC server
 /// is allowed to keep in memory per connection.
-pub const RPC_DEFAULT_MESSAGE_CAPACITY_PER_CONN: u32 = 16;
+pub const RPC_DEFAULT_MESSAGE_CAPACITY_PER_CONN: u32 = 1024;
 
 /// Default configuration values used by Substrate
 ///
