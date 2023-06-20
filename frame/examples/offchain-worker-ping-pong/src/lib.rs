01af46e9972a70e0cc25703971b216b93df34e61
Bruno Galvao <brunopgalvao@gmail.com>
2023-06-20 18:11:44 -0400

    remove markdown-link-check-disable

--- frame/examples/offchain-worker-ping-pong/src/lib.rs
+++ frame/examples/offchain-worker-ping-pong/src/lib.rs
@@ -15,7 +15,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! <!-- markdown-link-check-disable -->
 //! # Offchain Worker Example Pallet
 //!
 //! The Ping-Pong Offchain Worker Example: A simple pallet demonstrating
