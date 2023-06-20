7a2c272bd9a4e133f60215a9b7fc0aa79898c9b9
pgherveou <pgherveou@gmail.com>
2023-06-20 11:56:56 +0200

    Add explicit error, if try-runtime runs a noop migration

--- frame/contracts/src/migration.rs
+++ frame/contracts/src/migration.rs
@@ -306,6 +306,11 @@ impl<T: Config, const TEST_ALL_STEPS: bool> OnRuntimeUpgrade for Migration<T, TE
 		let storage_version = <Pallet<T>>::on_chain_storage_version();
 		let target_version = <Pallet<T>>::current_storage_version();
 
+		ensure!(
+			storage_version != target_version,
+			"No upgrade: Please remove this migration from your runtime upgrade configuration."
+		);
+
 		log::debug!(
 			target: LOG_TARGET,
 			"Requested migration of {} from {:?}(on-chain storage version) to {:?}(current storage version)",
