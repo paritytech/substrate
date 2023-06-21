aee26b99d68a62b4cd0da0b76a0e2daff5a27848
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2023-06-21 11:53:03 +0200

    Update frame/src/lib.rs
    Co-authored-by: Ben Greenberg <13892277+hummusonrails@users.noreply.github.com>

--- frame/src/lib.rs
+++ frame/src/lib.rs
@@ -16,7 +16,7 @@
 //! Substrate is at a very high level composed of two parts:
 //!
 //! 1. A *runtime* which represents the state transition function of a blockchain, and is encoded
-//! as a wasm blob.
+//! as a Wasm blob.
 //! 2. A client' who's primary purpose is to execute the given runtime.
 //!
 //! FRAME is the primary framework to build a runtime.
