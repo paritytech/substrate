81d125fe2a938ba336716c8b1d5e0faff0487d50
Jegor Sidorenko <5252494+jsidorenko@users.noreply.github.com>
2023-06-23 21:17:52 +0200

    Pay tx fee with assets by using the asset conversion pallet (#14340)
    * Pay tx by swapping the assets
    
    * Change liquidity structure
    
    * Uncomment the event
    
    * Update frame/transaction-payment/asset-tx-payment/src/payment.rs
    
    Co-authored-by: Squirrel <gilescope@gmail.com>
    
    * New approach
    
    * Fix bounds
    
    * Clearer version
    
    * Change IsType with Into and From
    
    * Enable event
    
    * Check ED + fix the logic
    
    * Add temp comments
    
    * Rework the refund
    
    * Clean up
    
    * Improve readability
    
    * Getting closer
    
    * fix
    
    * Use fungible instead of Currency
    
    * Test account without ed
    
    * Final push
    
    * Fixed
    
    * Rename to pallet-asset-conversion-tx-payment
    
    * Bring back the old pallet
    
    * Update versions
    
    * Update docs
    
    * Update readme
    
    * Wrong readme updated
    
    * Revert back doc change
    
    * Fix import
    
    * Fix kitchensink
    
    * Fix
    
    * One more time..
    
    * Wait pls
    
    * Update frame/asset-conversion/src/lib.rs
    
    Co-authored-by: Squirrel <gilescope@gmail.com>
    
    * Update frame/support/src/traits/tokens/fungibles/regular.rs
    
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    
    * Update docs/comments
    
    * Docs improvement
    
    * Update frame/transaction-payment/asset-conversion-tx-payment/src/lib.rs
    
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    
    * Update frame/transaction-payment/asset-conversion-tx-payment/src/lib.rs
    
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    
    * Update frame/transaction-payment/asset-conversion-tx-payment/src/lib.rs
    
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    
    * Update frame/transaction-payment/asset-conversion-tx-payment/src/lib.rs
    
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    
    * Update frame/transaction-payment/asset-conversion-tx-payment/src/lib.rs
    
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    
    * Payed -> paid
    
    * Docs
    
    * Update frame/transaction-payment/asset-conversion-tx-payment/README.md
    
    Co-authored-by: Muharem Ismailov <ismailov.m.h@gmail.com>
    
    * Rewrite docs
    
    * Try to clean the deps
    
    * Add debug assert
    
    * Return back frame-benchmarking
    
    * Update cargo
    
    * Update frame/transaction-payment/asset-conversion-tx-payment/src/mock.rs
    
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    
    * Rename
    
    * clearer error message
    
    * Docs for Pay by Swap (#14445)
    
    * docs
    
    * better error name
    
    * more comments
    
    * more docs on swap trait
    
    * Fix compile errors
    
    * Another fix
    
    * Refactoring
    
    * Update frame/transaction-payment/asset-conversion-tx-payment/src/payment.rs
    
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    
    * Emit an error if we fail to swap the refund back
    
    * Add integrity_test
    
    * Update frame/asset-conversion/src/lib.rs
    
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>
    
    * Fmt
    
    * Use defensive_ok_or
    
    * child PR: Tidy swap event (#14441)
    
    * Dedup raising swap event
    
    * use expect rather than unwrap
    
    * Additional checks for future defence.
    
    * cargo fmt
    
    * Update frame/asset-conversion/src/lib.rs
    
    Co-authored-by: Jegor Sidorenko <5252494+jsidorenko@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jegor Sidorenko <5252494+jsidorenko@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Squirrel <gilescope@gmail.com>
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    Co-authored-by: Muharem Ismailov <ismailov.m.h@gmail.com>
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- bin/node/testing/src/keyring.rs
+++ bin/node/testing/src/keyring.rs
@@ -77,7 +77,7 @@ pub fn signed_extra(nonce: Index, extra_fee: Balance) -> SignedExtra {
 		frame_system::CheckEra::from(Era::mortal(256, 0)),
 		frame_system::CheckNonce::from(nonce),
 		frame_system::CheckWeight::new(),
-		pallet_asset_tx_payment::ChargeAssetTxPayment::from(extra_fee, None),
+		pallet_asset_conversion_tx_payment::ChargeAssetTxPayment::from(extra_fee, None),
 	)
 }
 
