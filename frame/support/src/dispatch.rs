ea990a6beee406fc717d4061d7220915a0e3cb35
bragov4ik <bragov4ik@hotmail.com>
2023-06-27 15:51:35 +0300

    add with_pays_fee

--- frame/support/src/dispatch.rs
+++ frame/support/src/dispatch.rs
@@ -328,7 +328,7 @@ impl sp_runtime::traits::Printable for PostDispatchInfo {
 }
 
 /// Allows easy conversion from `DispatchError` to `DispatchErrorWithPostInfo` for dispatchables
-/// that want to return a custom a posterior weight on error.
+/// that want to return a custom posterior weight or fee payment decision on error.
 pub trait WithPostDispatchInfo {
 	/// Call this on your modules custom errors type in order to return a custom weight on error.
 	///
@@ -339,6 +339,7 @@ pub trait WithPostDispatchInfo {
 	/// ensure!(who == me, Error::<T>::NotMe.with_weight(200_000));
 	/// ```
 	fn with_weight(self, actual_weight: Weight) -> DispatchErrorWithPostInfo;
+	fn with_pays_fee(self, pays_fee: Pays) -> DispatchErrorWithPostInfo;
 }
 
 impl<T> WithPostDispatchInfo for T
@@ -351,6 +352,12 @@ where
 				actual_weight: Some(actual_weight),
 				pays_fee: Default::default(),
 			},
+			error: self.into(),
+		}
+	}
+	fn with_pays_fee(self, pays_fee: Pays) -> DispatchErrorWithPostInfo {
+		DispatchErrorWithPostInfo {
+			post_info: PostDispatchInfo { actual_weight: None, pays_fee },
 			error: self.into(),
 		}
 	}
