0171ef1704a8364a78ff7e686a54149f73e0b071
Ross Bulat <ross@parity.io>
2023-06-27 11:43:10 +0100

    rm clone

--- frame/staking/src/benchmarking.rs
+++ frame/staking/src/benchmarking.rs
@@ -481,7 +481,7 @@ benchmarks! {
 		Payees::<T>::remove(&stash);
 		assert!(!Payees::<T>::contains_key(&stash));
 		whitelist_account!(controller);
-	}: _(RawOrigin::Signed(stash.clone()), controller.clone())
+	}: _(RawOrigin::Signed(stash.clone()), controller)
 	verify {
 		assert_eq!(Payees::<T>::get(&stash), PayeeDestination::Compound);
 	}
