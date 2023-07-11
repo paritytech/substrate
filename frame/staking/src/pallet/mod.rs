a8cc2af0c69abb50a6116f0265e654469054ef6d
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-07-11 16:32:16 +0200

    Fix other pallets
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/staking/src/pallet/mod.rs
+++ frame/staking/src/pallet/mod.rs
@@ -794,16 +794,12 @@ pub mod pallet {
 					<T::GenesisElectionProvider as ElectionProviderBase>::MaxWinners::get()
 			);
 
-			sp_std::if_std! {
-				sp_io::TestExternalities::new_empty().execute_with(||
-					assert!(
-						T::SlashDeferDuration::get() < T::BondingDuration::get() || T::BondingDuration::get() == 0,
-						"As per documentation, slash defer duration ({}) should be less than bonding duration ({}).",
-						T::SlashDeferDuration::get(),
-						T::BondingDuration::get(),
-					)
-				);
-			}
+			assert!(
+				T::SlashDeferDuration::get() < T::BondingDuration::get() || T::BondingDuration::get() == 0,
+				"As per documentation, slash defer duration ({}) should be less than bonding duration ({}).",
+				T::SlashDeferDuration::get(),
+				T::BondingDuration::get(),
+			)
 		}
 
 		#[cfg(feature = "try-runtime")]
