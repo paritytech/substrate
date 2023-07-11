5ba88f2f43d10a323ca2972e09854028c7a62078
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-07-11 16:33:38 +0200

    Update docs
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- frame/support/src/traits/hooks.rs
+++ frame/support/src/traits/hooks.rs
@@ -248,7 +248,7 @@ impl OnRuntimeUpgrade for Tuple {
 pub trait IntegrityTest {
 	/// Run integrity test.
 	///
-	/// The test is executed in read-only externalities.
+	/// The test is executed in test externalities.
 	fn integrity_test() {}
 }
 
@@ -355,7 +355,7 @@ pub trait Hooks<BlockNumber> {
 
 	/// Run integrity test.
 	///
-	/// The test is executed in read-only externalities.
+	/// The test is executed in test externalities.
 	fn integrity_test() {}
 }
 
