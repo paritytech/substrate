3987695b4438bfe9991978a1cfaab3bb3d0da189
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2023-07-12 15:40:23 +0200

    client/rpc: test fixed (new rt api)

--- client/rpc/src/state/tests.rs
+++ client/rpc/src/state/tests.rs
@@ -518,7 +518,7 @@ async fn should_return_runtime_version() {
 		[\"0x37e397fc7c91f5e4\",2],[\"0xd2bc9897eed08f15\",3],[\"0x40fe3ad401f8959a\",6],\
 		[\"0xbc9d89904f5b923f\",1],[\"0xc6e9a76309f39b09\",2],[\"0xdd718d5cc53262d4\",1],\
 		[\"0xcbca25e39f142387\",2],[\"0xf78b278be53f454c\",2],[\"0xab3c0572291feb8b\",1],\
-		[\"0xed99c5acb25eedf5\",3]],\"transactionVersion\":1,\"stateVersion\":1}";
+		[\"0xed99c5acb25eedf5\",3],[\"0xfbc577b9d747efd6\",1]],\"transactionVersion\":1,\"stateVersion\":1}";
 
 	let runtime_version = api.runtime_version(None.into()).unwrap();
 	let serialized = serde_json::to_string(&runtime_version).unwrap();
