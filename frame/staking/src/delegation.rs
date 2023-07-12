15a13b73c3787529a25b3fd8c8dea55c180d8850
Ankan <ankan.anurag@gmail.com>
2023-07-13 01:34:39 +0200

    delegation interface

--- frame/staking/src/delegation.rs
+++ frame/staking/src/delegation.rs
@@ -15,13 +15,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 //! An implementation of a delegation system for nominators.
 //!
 //! This allows an account (called delegator) to delegate their funds to another account
 //! (delegatee). Multiple delegators can delegate to the same delegatee. The delegatee is then able
 //! to use the funds of all delegators to nominate a set of validators.
 
+use std::ops::Sub;
+use codec::{FullCodec, MaxEncodedLen};
+use scale_info::TypeInfo;
+use frame_support::dispatch::DispatchResult;
+use sp_runtime::Saturating;
+use crate::{BalanceOf, Config};
 
 /// A ledger of a delegator.
 ///
@@ -35,3 +40,26 @@ pub struct DelegationLedger<T: Config> {
 	pub pending_slash: BalanceOf<T>,
 }
 
+/// A generic representation of a delegation apis exposed by staking pallet for use by other runtime
+/// pallets.
+pub trait DelegationInterface {
+	/// AccountId type used by the runtime.
+	type AccountId: Clone + sp_std::fmt::Debug;
+
+	/// Balance type used by the runtime.
+	type Balance: Sub<Output = Self::Balance>
+	+ Ord
+	+ PartialEq
+	+ Default
+	+ Copy
+	+ MaxEncodedLen
+	+ FullCodec
+	+ TypeInfo
+	+ Saturating;
+
+	/// Delegate some funds or add to an existing delegation.
+	// TODO(ank4n): No restriction to number of delegations per delegator?
+	fn delegate(delegator: Self::AccountId, delegatee: Self::AccountId, value: Self::Balance) -> DispatchResult;
+	/// Remove delegation of some or all funds.
+	fn remove_delegate(delegator: Self::AccountId, delegatee: Self::AccountId, value: Self::Balance) -> DispatchResult;
+}
