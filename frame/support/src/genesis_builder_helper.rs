91920469e23ae0d6a7ce4a90404c3b163110c208
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2023-07-12 15:39:15 +0200

    doc fixed

--- frame/support/src/genesis_builder_helper.rs
+++ frame/support/src/genesis_builder_helper.rs
@@ -24,7 +24,7 @@ use sp_genesis_builder::Result as BuildResult;
 use sp_runtime::format_runtime_string;
 
 /// Get the default `GenesisConfig` as a JSON blob. For more info refer to
-/// [`sp_genesis_builder::GenesisBuilder::get_default_as_json`]
+/// [`sp_genesis_builder::GenesisBuilder::create_default_config`]
 pub fn create_default_config<GC: BuildGenesisConfig>() -> sp_std::vec::Vec<u8> {
 	serde_json::to_string(&GC::default())
 		.expect("serialization to json is expected to work. qed.")
@@ -32,7 +32,7 @@ pub fn create_default_config<GC: BuildGenesisConfig>() -> sp_std::vec::Vec<u8> {
 }
 
 /// Patch default `GenesisConfig` using given JSON patch and store it in the storage. For more
-/// info refer to [`sp_genesis_builder::GenesisBuilder::build_config_no_defaults`].
+/// info refer to [`sp_genesis_builder::GenesisBuilder::build_config`].
 pub fn build_config<GC: BuildGenesisConfig>(json: sp_std::vec::Vec<u8>) -> BuildResult {
 	let gc = serde_json::from_slice::<GC>(&json)
 		.map_err(|e| format_runtime_string!("Invalid JSON blob: {}", e))?;
