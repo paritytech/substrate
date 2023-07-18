759406e06a9c8fbae3f8363ea85c483c47e906e0
Davide Galassi <davxy@datawok.net>
2023-07-18 18:58:12 +0200

    Rename bandersnatch testing app crypto id

--- primitives/core/src/testing.rs
+++ primitives/core/src/testing.rs
@@ -26,7 +26,7 @@ pub const SR25519: KeyTypeId = KeyTypeId(*b"sr25");
 /// Key type for generic ECDSA key.
 pub const ECDSA: KeyTypeId = KeyTypeId(*b"ecds");
 /// Key type for generic Bandersnatch key.
-pub const BANDERSNATCH: KeyTypeId = KeyTypeId(*b"bb12");
+pub const BANDERSNATCH: KeyTypeId = KeyTypeId(*b"band");
 /// Key type for generic BLS12-377 key.
 pub const BLS377: KeyTypeId = KeyTypeId(*b"bls7");
 /// Key type for generic BLS12-381 key.
