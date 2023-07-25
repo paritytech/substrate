2a89afa4556b87e091d86f95406687bcacf7f2c1
Sacha Lansky <sacha@parity.io>
2023-07-25 13:52:53 +0200

    add missing doc for buy_sell module

--- frame/nfts/src/features/buy_sell.rs
+++ frame/nfts/src/features/buy_sell.rs
@@ -15,6 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! This module contains helper functions to perform the buy and sell functionalities of the NFTs pallet.
+
 use crate::*;
 use frame_support::{
 	pallet_prelude::*,
