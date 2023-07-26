6982788354fc7953abde2ac29190317a62544269
Alexandru Vasile <alexandru.vasile@parity.io>
2023-07-26 15:03:22 +0300

    chainHead/events: Remove network config
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- client/rpc-spec-v2/src/chain_head/chain_head.rs
+++ client/rpc-spec-v2/src/chain_head/chain_head.rs
@@ -23,7 +23,7 @@ use crate::{
 		api::ChainHeadApiServer,
 		chain_head_follow::ChainHeadFollower,
 		error::Error as ChainHeadRpcError,
-		event::{ChainHeadEvent, ChainHeadResult, ErrorEvent, FollowEvent, NetworkConfig},
+		event::{ChainHeadEvent, ChainHeadResult, ErrorEvent, FollowEvent},
 		hex_string,
 		subscription::{SubscriptionManagement, SubscriptionManagementError},
 	},
@@ -205,7 +205,6 @@ where
 		mut sink: SubscriptionSink,
 		follow_subscription: String,
 		hash: Block::Hash,
-		_network_config: Option<NetworkConfig>,
 	) -> SubscriptionResult {
 		let client = self.client.clone();
 		let subscriptions = self.subscriptions.clone();
@@ -294,7 +293,6 @@ where
 		hash: Block::Hash,
 		items: Vec<StorageQuery<String>>,
 		child_trie: Option<String>,
-		_network_config: Option<NetworkConfig>,
 	) -> SubscriptionResult {
 		// Gain control over parameter parsing and returned error.
 		let items = items
@@ -362,7 +360,6 @@ where
 		hash: Block::Hash,
 		function: String,
 		call_parameters: String,
-		_network_config: Option<NetworkConfig>,
 	) -> SubscriptionResult {
 		let call_parameters = Bytes::from(parse_hex_param(&mut sink, call_parameters)?);
 
--- client/rpc-spec-v2/src/chain_head/event.rs
+++ client/rpc-spec-v2/src/chain_head/event.rs
@@ -21,27 +21,6 @@
 use serde::{ser::SerializeStruct, Deserialize, Serialize, Serializer};
 use sp_api::ApiError;
 use sp_version::RuntimeVersion;
-use std::num::NonZeroUsize;
-
-/// The network config parameter is used when a function
-/// needs to request the information from its peers.
-///
-/// These values can be tweaked depending on the urgency of the JSON-RPC function call.
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct NetworkConfig {
-	/// The total number of peers from which the information is requested.
-	total_attempts: u64,
-	/// The maximum number of requests to perform in parallel.
-	///
-	/// # Note
-	///
-	/// A zero value is illegal.
-	max_parallel: NonZeroUsize,
-	/// The time, in milliseconds, after which a single requests towards one peer
-	/// is considered unsuccessful.
-	timeout_ms: u64,
-}
 
 /// The operation could not be processed due to an error.
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -537,22 +516,6 @@ mod tests {
 
 		let event_dec: ChainHeadEvent<String> = serde_json::from_str(exp).unwrap();
 		assert_eq!(event_dec, event);
-	}
-
-	#[test]
-	fn chain_head_network_config() {
-		let conf = NetworkConfig {
-			total_attempts: 1,
-			max_parallel: NonZeroUsize::new(2).expect("Non zero number; qed"),
-			timeout_ms: 3,
-		};
-
-		let ser = serde_json::to_string(&conf).unwrap();
-		let exp = r#"{"totalAttempts":1,"maxParallel":2,"timeoutMs":3}"#;
-		assert_eq!(ser, exp);
-
-		let conf_dec: NetworkConfig = serde_json::from_str(exp).unwrap();
-		assert_eq!(conf_dec, conf);
 	}
 
 	#[test]
