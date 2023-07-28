7874eb1b6fb12637534ff336d686bb454868cda9
Sergej Sakac <sakacszergej@gmail.com>
2023-07-28 09:34:40 -0700

    fmt

--- frame/referenda/src/lib.rs
+++ frame/referenda/src/lib.rs
@@ -1301,7 +1301,8 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
 	///
 	/// General assertions:
 	///
-	/// * [`ReferendumCount`] must always be equal to the number of referenda in [`ReferendumInfoFor`].
+	/// * [`ReferendumCount`] must always be equal to the number of referenda in
+	///   [`ReferendumInfoFor`].
 	/// * Referendum indices in [`MetadataOf`] must also be stored in [`ReferendumInfoFor`].
 	#[cfg(any(feature = "try-runtime", test))]
 	fn do_try_state() -> Result<(), sp_runtime::TryRuntimeError> {
