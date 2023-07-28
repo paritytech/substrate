3fac946a1d22213f3eb4c5437b0393e16818d3a1
pgherveou <pgherveou@gmail.com>
2023-07-28 23:18:27 +0200

    add more docs

--- frame/contracts/build.rs
+++ frame/contracts/build.rs
@@ -60,7 +60,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		"
          pub mod codegen {{
 		  use crate::NoopMigration;
+		  /// The latest migration version, pulled from the latest migration file.
 		  pub const LATEST_MIGRATION_VERSION: u16 = {version};
+		  /// The Migration Steps used for benchmarking, the migration framework.
 		  pub type BenchMigrations = (NoopMigration<{}>, NoopMigration<{}>);
 		}}",
 		version - 1,
