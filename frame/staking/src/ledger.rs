fceeaa9aea9630ed4e4b8d3bf752be9c47fd68de
Gonçalo Pestana <g6pestana@gmail.com>
2023-08-01 10:01:09 +0200

    Update frame/staking/src/ledger.rs
    Co-authored-by: Liam Aharon <liam.aharon@hotmail.com>

--- frame/staking/src/ledger.rs
+++ frame/staking/src/ledger.rs
@@ -66,7 +66,7 @@ impl<T: Config> StakingLedger<T> {
 
 	/// Returns a new instance of a staking ledger.
 	///
-	/// The `Ledger` storage is not mutated. In order to store, `fn update` must be called on
+	/// The [`Ledger`] storage is not mutated. In order to store, [`StakingLedger::update`] must be called on
 	/// the returned staking ledger.
 	///
 	/// Note: as the controller accounts are being deprecated, the stash account is the same as the
