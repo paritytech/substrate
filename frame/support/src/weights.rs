ece92d6b30ee7f9fbda7a84bee76f8ff6d2c92a1
Doordashcon <jesse.chejieh@gmail.com>
2023-08-09 10:41:11 +0100

    Remove deprecated old weight items (#14706)
    * remove old weights frame-support
    
    * ".git/.scripts/commands/fmt/fmt.sh"
    
    ---------
    
    Co-authored-by: command-bot <>

--- frame/support/src/weights.rs
+++ frame/support/src/weights.rs
@@ -22,7 +22,6 @@ mod extrinsic_weights;
 mod paritydb_weights;
 mod rocksdb_weights;
 
-use crate::dispatch;
 pub use sp_weights::*;
 
 /// These constants are specific to FRAME, and the current implementation of its various components.
@@ -37,71 +36,4 @@ pub mod constants {
 	pub use super::{
 		paritydb_weights::constants::ParityDbWeight, rocksdb_weights::constants::RocksDbWeight,
 	};
-}
-
-#[deprecated = "Function has moved to `frame_support::dispatch`"]
-pub fn extract_actual_pays_fee(
-	res: &dispatch::DispatchResultWithPostInfo,
-	info: &dispatch::DispatchInfo,
-) -> dispatch::Pays {
-	dispatch::extract_actual_pays_fee(res, info)
-}
-#[deprecated = "Function has moved to `frame_support::dispatch`"]
-pub fn extract_actual_weight(
-	res: &dispatch::DispatchResultWithPostInfo,
-	info: &dispatch::DispatchInfo,
-) -> Weight {
-	dispatch::extract_actual_weight(res, info)
-}
-#[deprecated = "Trait has moved to `frame_support::dispatch`"]
-pub trait ClassifyDispatch<T>: dispatch::ClassifyDispatch<T> {
-	fn classify_dispatch(&self, target: T) -> dispatch::DispatchClass {
-		<Self as dispatch::ClassifyDispatch<T>>::classify_dispatch(self, target)
-	}
-}
-#[deprecated = "Enum has moved to `frame_support::dispatch`"]
-pub type DispatchClass = dispatch::DispatchClass;
-#[deprecated = "Struct has moved to `frame_support::dispatch`"]
-pub type DispatchInfo = dispatch::DispatchInfo;
-#[deprecated = "Trait has moved to `frame_support::dispatch`"]
-pub trait GetDispatchInfo: dispatch::GetDispatchInfo {
-	fn get_dispatch_info(&self) -> dispatch::DispatchInfo {
-		<Self as dispatch::GetDispatchInfo>::get_dispatch_info(self)
-	}
-}
-#[deprecated = "Trait has moved to `frame_support::dispatch`"]
-pub trait OneOrMany<T>: dispatch::OneOrMany<T> {
-	fn into_iter(self) -> Self::Iter
-	where
-		Self: Sized,
-	{
-		<Self as dispatch::OneOrMany<T>>::into_iter(self)
-	}
-}
-#[deprecated = "Enum has moved to `frame_support::dispatch`"]
-pub type Pays = dispatch::Pays;
-#[deprecated = "Trait has moved to `frame_support::dispatch`"]
-pub trait PaysFee<T>: dispatch::PaysFee<T> {
-	fn pays_fee(&self, target: T) -> dispatch::Pays {
-		<Self as dispatch::PaysFee<T>>::pays_fee(self, target)
-	}
-}
-#[deprecated = "Struct has moved to `frame_support::dispatch`"]
-pub type PerDispatchClass<T> = dispatch::PerDispatchClass<T>;
-#[deprecated = "Struct has moved to `frame_support::dispatch`"]
-pub type PostDispatchInfo = dispatch::PostDispatchInfo;
-#[deprecated = "Trait has moved to `frame_support::dispatch`"]
-pub trait WeighData<T>: dispatch::WeighData<T> {
-	fn weigh_data(&self, target: T) -> Weight {
-		<Self as dispatch::WeighData<T>>::weigh_data(self, target)
-	}
-}
-#[deprecated = "Trait has moved to `frame_support::dispatch`"]
-pub trait WithPostDispatchInfo: dispatch::WithPostDispatchInfo {
-	fn with_weight(self, actual_weight: Weight) -> dispatch::DispatchErrorWithPostInfo
-	where
-		Self: Sized,
-	{
-		<Self as dispatch::WithPostDispatchInfo>::with_weight(self, actual_weight)
-	}
 }
