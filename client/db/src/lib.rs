ccc5ab7bf602f7ad40341fd1a784d2e6783e25d5
Liam Aharon <liam.aharon@hotmail.com>
2023-08-11 17:52:24 +0700

    kick ci

--- client/db/src/lib.rs
+++ client/db/src/lib.rs
@@ -1,4 +1,4 @@
-// This file is part of Substrate.
+/ This file is part of Substrate.
 
 // Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
