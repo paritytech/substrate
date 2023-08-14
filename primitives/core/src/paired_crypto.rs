2a6283a75c926929363f2695fb2781afe62c2b5b
Skalman <skalman@riseup.net>
2023-08-14 10:15:06 -0400

    - implement paired crypto `Public` as tuple of two `Public`s - unsucessfuly try to implement ByteArray for paired crypto Public

--- primitives/core/src/paired_crypto.rs
+++ primitives/core/src/paired_crypto.rs
@@ -34,6 +34,7 @@ use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
 use sp_runtime_interface::pass_by::PassByInner;
 use sp_std::{convert::TryFrom, marker::PhantomData, ops::Deref};
 
+
 /// ECDSA and BLS-377 specialized types
 pub mod ecdsa_n_bls377 {
 	use crate::crypto::{CryptoTypeId};
@@ -46,7 +47,7 @@ pub mod ecdsa_n_bls377 {
 	#[cfg(feature = "full_crypto")]
 	//pub type Pair = super::Pair<ecdsa:Pair, bls377:Pair>;
 	/// BLS12-377 public key.
-	pub type Public = super::Public<ecdsa::Public, ed25519::Public>;
+	pub type Public = super::Public<ecdsa::Public, ed25519::Public, 32, 32>;
 	// /// BLS12-377 signature.
 	//pub type Signature = super::Signature<ecdsa:Signature, bls377:Signature>;
 
@@ -63,47 +64,61 @@ pub mod ecdsa_n_bls377 {
 #[cfg(feature = "full_crypto")]
 //type Seed = [u8; SECRET_KEY_SERIALIZED_SIZE];
 
-pub trait PublicKeyBound: TraitPublic + sp_std::hash::Hash + ByteArray {}
+pub trait PublicKeyBound: TraitPublic + sp_std::hash::Hash + ByteArray  {}
 /// A public key.
 #[derive(Clone, Encode, Decode, MaxEncodedLen, TypeInfo, PartialEq, Eq, PartialOrd, Ord)]
 #[scale_info(skip_type_params(T))]
-pub struct PublicWithInner<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_PLUS_RIGHT_SIZE: usize> {
-    inner: [u8; LEFT_PLUS_RIGHT_SIZE],
- 	_phantom: PhantomData<fn() -> (LeftPublic, RightPublic)>,
+pub struct Public<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_LEN: usize, const RIGHT_LEN: usize,> (LeftPublic, RightPublic);
+
+#[cfg(feature = "full_crypto")]
+impl<LeftPublic: PublicKeyBound + UncheckedFrom<[u8; LEFT_LEN]>, RightPublic: PublicKeyBound + UncheckedFrom<[u8; RIGHT_LEN]>, const LEFT_LEN: usize, const RIGHT_LEN: usize> sp_std::hash::Hash for Public<LeftPublic, RightPublic, LEFT_LEN, RIGHT_LEN> {
+  	fn hash<H: sp_std::hash::Hasher>(&self, state: &mut H) {
+ 	    self.0.hash(state);
+	    self.1.hash(state);
+	}
 }
 
-pub struct Public<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound> {
-    left_public : LeftPublic,
-    right_public: RightPublic,
+impl<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_LEN: usize, const RIGHT_LEN: usize> ByteArray  for Public<LeftPublic, RightPublic, LEFT_LEN, RIGHT_LEN> where for<'a>  Public<LeftPublic, RightPublic, LEFT_LEN, RIGHT_LEN>: TryFrom<&'a [u8], Error = ()> + AsRef<[u8]> + AsMut<[u8]>  {
+     const LEN: usize = LeftPublic::LEN + RightPublic::LEN;
 }
 
-// #[cfg(feature = "full_crypto")]
-// impl<T1: PublicKeyBound, T2: PublicKeyBound> sp_std::hash::Hash for Public<T1,T2> {
-//  	fn hash<H: sp_std::hash::Hasher>(&self, state: &mut H) {
-//  		self.left_public.hash(state);
-//         self.right_public.hash(state);
-//  	}
-// }
+impl<'a,LeftPublic: PublicKeyBound + UncheckedFrom<[u8; LEFT_LEN]>, RightPublic: PublicKeyBound + UncheckedFrom<[u8; RIGHT_LEN]>, const LEFT_LEN: usize, const RIGHT_LEN: usize>  TryFrom<&'a[u8]> for Public<LeftPublic, RightPublic, LEFT_LEN, RIGHT_LEN> {
+    type Error = ();
 
-// impl<T1: TraitPublic, T2: TraitPublic> ByteArray for Public<T1,T2> {
-//  	const LEN: usize = T1::LEN + T2::LEN;
-// }
+    fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
+ 		if data.len() != 	LEFT_LEN + RIGHT_LEN {
+			return Err(())
+		}
+	
+ 		let mut r0 = [0u8;  LEFT_LEN];
+ 		let mut r1 = [0u8; RIGHT_LEN];
+		r0.copy_from_slice(&data[0..LEFT_LEN]);  
+ 		r1.copy_from_slice(&data[LEFT_LEN..RIGHT_LEN]);
+		Ok(Self(LeftPublic::unchecked_from(r0),RightPublic::unchecked_from(r1)))
+    }
+}
 
-// impl<T1: PublicKeyBound, T2: PublicKeyBound> TryFrom<&[u8]> for Public<T1,T2> {
-//  	type Error = ();
-
-//  	fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
-// // 		if data.len() != PUBLIC_KEY_SERIALIZED_SIZE {
-// // 			return Err(())
-// // 		}
-// // 		let mut r = [0u8; PUBLIC_KEY_SERIALIZED_SIZE];
-// // 		r.copy_from_slice(data);
-// // 		Ok(Self::unchecked_from(r))
-//  	}
-// }
-
-// impl<T1,T2> AsMut<[u8]> for Public<T1,T2> {
+// impl<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_LEN: usize, const RIGHT_LEN: usize> AsMut<[u8]> for Public<LeftPublic, RightPublic, LEFT_LEN, RIGHT_LEN> {
 //  	fn as_mut(&mut self) -> &mut [u8] {
-//  		&mut self.inner[..]
+//  	 &mut [self.0.as_mut(), self.1.as_mut()].concat()
 //  	}
+// }
+
+// impl<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_LEN: usize, const RIGHT_LEN: usize, const RIGHT_PLUS_LEFT_LEN: usize> AsRef<[u8; RIGHT_PLUS_LEFT_LEN]> for Public<LeftPublic, RightPublic, LEFT_LEN, RIGHT_LEN> where for<'a>  Public<LeftPublic, RightPublic, LEFT_LEN, RIGHT_LEN>: TryFrom<&'a [u8], Error = ()> {
+//     fn as_ref(&self) -> &[u8; RIGHT_PLUS_LEFT_LEN] {
+// 	let mut r = [0u8; RIGHT_PLUS_LEFT_LEN];
+// 	r.copy_from_slice(self.0.as_ref());
+// 	r[LeftPublic::LEN..].copy_from_slice(self.1.as_ref());
+// 	&r
+//     }
+// }
+
+// impl<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_LEN: usize, const RIGHT_LEN: usize,> AsRef<[u8]> for Public<LeftPublic, RightPublic, LEFT_LEN, RIGHT_LEN> {
+//     fn as_ref(&self) -> &[u8] {
+// 	//let mut r : Vec<u8> = vec![0u8; LEFT_LEN + RIGHT_LEN];
+// 	//r.copy_from_slice(self.0.as_ref(), LeftPublic::LEN);
+// 	//r[LeftPublic::LEN..].copy_from_slice(self.1.as_ref(), RightPublic::LEN);
+// 	let mut r :Vec<u8> = [self.0.as_ref(), self.1.as_ref()].concat();
+// 	&r[..]
+//     }
 // }
