185c000a505c8950c3d62cecfc8a209ec2cf3412
Liam Aharon <liam.aharon@hotmail.com>
2023-08-16 17:09:15 +0700

    kick ci

--- bin/node/cli/bin/main.rs
+++ bin/node/cli/bin/main.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Substrate Node CLI..
+//! Substrate Node CLI
 
 #![warn(missing_docs)]
 
