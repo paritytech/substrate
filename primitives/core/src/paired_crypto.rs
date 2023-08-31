4e7fb6329565cb1dca8231da766332300de74807
Skalman <skalman@riseup.net>
2023-08-31 10:11:56 -0400

    - Implement trait `Pair` for `paired_crypto::Pair` - Implement a pair of seeds for paired crypto scheme.

--- primitives/core/src/paired_crypto.rs
+++ primitives/core/src/paired_crypto.rs
@@ -44,7 +44,7 @@ pub mod ecdsa_n_bls377 {
 
 	/// BLS12-377 key pair.
 	#[cfg(feature = "full_crypto")]
-	pub type Pair = super::Pair<ecdsa::Pair, ed25519::Pair>;
+	pub type Pair = super::Pair<ecdsa::Pair, ed25519::Pair, 64, 64>;
 	/// BLS12-377 public key.
 	pub type Public = super::Public<ecdsa::Public, ed25519::Public, 64>;
 	// /// BLS12-377 signature.
@@ -66,14 +66,26 @@ pub mod ecdsa_n_bls377 {
 
 }
 
+#[cfg(feature = "full_crypto")]
+const SECURE_SEED_LEN: usize = 32;
+
+#[cfg(feature = "full_crypto")]
+const DOUBLE_SEED_LEN: usize = SECURE_SEED_LEN * 2;
+
 /// A secret seed.
 ///
 /// It's not called a "secret key" because ring doesn't expose the secret keys
 /// of the key pair (yeah, dumb); as such we're forced to remember the seed manually if we
 /// will need it later (such as for HDKD).
 #[cfg(feature = "full_crypto")]
-type Seed<const LEFT_PLUS_RIGHT_LEN: usize> = [u8; {LEFT_PLUS_RIGHT_LEN}];
+pub trait SeedBound:  Default + AsRef<[u8]> + AsMut<[u8]> + Clone {}
 
+#[cfg(feature = "full_crypto")]
+pub struct Seed<LeftSeed: SeedBound, RightSeed: SeedBound, const LEFT_PLUS_RIGHT_LEN: usize,> {
+    left: LeftSeed,
+    right: RightSeed,
+    inner: [u8; LEFT_PLUS_RIGHT_LEN],
+}
 //pub trait Public: ByteArray + Derive + CryptoType + PartialEq + Eq + Clone + Send + Sync {}
 pub trait PublicKeyBound: TraitPublic + Sized + Derive + sp_std::hash::Hash + ByteArray + for<'a> TryFrom<&'a[u8]> + AsMut<[u8]> + CryptoType {}
 
@@ -120,8 +132,8 @@ impl<'a,LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_PLUS
   		if data.len() != 	LEFT_PLUS_RIGHT_LEN {
  			return Err(())
  		}
-         	 let mut left : LeftPublic = data[0..LeftPublic::LEN].try_into().unwrap();
-	 let mut right : RightPublic = data[LeftPublic::LEN..LEFT_PLUS_RIGHT_LEN].try_into().unwrap();
+         	 let mut left : LeftPublic = data[0..LeftPublic::LEN].try_into()?;
+	 let mut right : RightPublic = data[LeftPublic::LEN..LEFT_PLUS_RIGHT_LEN].try_into()?;
 
 	 let mut inner = [0u8; LEFT_PLUS_RIGHT_LEN];
 	 Ok(Public { left, right, inner })
@@ -167,9 +179,9 @@ impl<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_PLUS_RI
 }
 
 #[cfg(feature = "full_crypto")]
-impl<LeftPair: TraitPair, RightPair: TraitPair, LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_PLUS_RIGHT_PUBLIC_LEN: usize,> From<Pair<LeftPair, RightPair>> for Public<LeftPublic, RightPublic, LEFT_PLUS_RIGHT_PUBLIC_LEN> where Pair<LeftPair, RightPair> : TraitPair<Public = Public<LeftPublic, RightPublic, LEFT_PLUS_RIGHT_PUBLIC_LEN>>,
+impl<LeftPair: TraitPair, RightPair: TraitPair, LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_PLUS_RIGHT_PUBLIC_LEN: usize, const SIGNATURE_LEN: usize> From<Pair<LeftPair, RightPair, LEFT_PLUS_RIGHT_PUBLIC_LEN, SIGNATURE_LEN>> for Public<LeftPublic, RightPublic, LEFT_PLUS_RIGHT_PUBLIC_LEN> where Pair<LeftPair, RightPair, LEFT_PLUS_RIGHT_PUBLIC_LEN, SIGNATURE_LEN> : TraitPair<Public = Public<LeftPublic, RightPublic, LEFT_PLUS_RIGHT_PUBLIC_LEN>>,
     {
-	fn from(x: Pair<LeftPair, RightPair>) -> Self {
+	fn from(x: Pair<LeftPair, RightPair, LEFT_PLUS_RIGHT_PUBLIC_LEN, SIGNATURE_LEN>) -> Self {
 		x.public()
 	}
 }
@@ -179,8 +191,7 @@ impl<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_PLUS_RI
  	 let mut left : LeftPublic = data[0..LeftPublic::LEN].try_into().unwrap();
 	 let mut right : RightPublic = data[LeftPublic::LEN..LEFT_PLUS_RIGHT_LEN].try_into().unwrap();
 
-	 let mut inner = [0u8; LEFT_PLUS_RIGHT_LEN];
-	 Public { left, right, inner }
+	 Public { left: left, right: right, inner: data }
 	}
 
 }
@@ -248,8 +259,9 @@ impl<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_PLUS_RI
 impl<LeftPublic: PublicKeyBound, RightPublic: PublicKeyBound, const LEFT_PLUS_RIGHT_PUBLIC_LEN: usize,> CryptoType for Public<LeftPublic, RightPublic, LEFT_PLUS_RIGHT_PUBLIC_LEN> 
 {}
 
+
 //pub trait Public: ByteArray + Derive + CryptoType + PartialEq + Eq + Clone + Send + Sync {}
-pub trait SignatureBound: sp_std::hash::Hash + for<'a> TryFrom<&'a[u8]> {}
+pub trait SignatureBound: sp_std::hash::Hash + for<'a> TryFrom<&'a[u8]> + AsRef<[u8]> {}
 
 /// A pair of signatures of different types
 #[derive(Encode, Decode, MaxEncodedLen, TypeInfo, PartialEq, Eq)]
@@ -281,10 +293,13 @@ impl<'a,LeftSignature: SignatureBound, RightSignature: SignatureBound, const LEF
   		if data.len() != 	LEFT_PLUS_RIGHT_LEN {
  			return Err(())
  		}
-         	 let Ok(mut left)  = data[0..Self::LEFT_SIGNATURE_LEN].try_into() else { panic!("invalid signature") };
-	 let Ok(mut right) = data[Self::LEFT_SIGNATURE_LEN..LEFT_PLUS_RIGHT_LEN].try_into() else { panic!("invalid signature") };
+         	 let Ok(mut left) : Result<LeftSignature, _> = data[0..Self::LEFT_SIGNATURE_LEN].try_into() else { panic!("invalid signature") };
+	 let Ok(mut right) : Result<RightSignature, _>= data[Self::LEFT_SIGNATURE_LEN..LEFT_PLUS_RIGHT_LEN].try_into() else { panic!("invalid signature") };
 
-	 let mut inner = [0u8; LEFT_PLUS_RIGHT_LEN];
+	     let mut inner = [0u8; LEFT_PLUS_RIGHT_LEN];
+         inner.copy_from_slice(left.as_ref());
+ 	     inner[Self::LEFT_SIGNATURE_LEN..].copy_from_slice(right.as_ref());
+
 	 Ok(Signature { left, right, inner })
 
      }
@@ -318,15 +333,17 @@ impl<LeftSignature: SignatureBound, RightSignature: SignatureBound, const LEFT_P
 }
 
 #[cfg(feature = "std")]
-impl<'de, LeftSignature: SignatureBound, RightSignature: SignatureBound, const LEFT_PLUS_RIGHT_LEN: usize,> Deserialize<'de> for Signature<LeftSignature, RightSignature, LEFT_PLUS_RIGHT_LEN> {
+impl<'de,LeftSignature: SignatureBound, RightSignature: SignatureBound, const LEFT_PLUS_RIGHT_LEN: usize,> Deserialize<'de> for Signature<LeftSignature, RightSignature, LEFT_PLUS_RIGHT_LEN> where
+Signature<LeftSignature, RightSignature, LEFT_PLUS_RIGHT_LEN>: for<'a> TryFrom<&'a[u8]>{
 	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
 	where
 		D: Deserializer<'de>,
 	{
 		let signature_hex = array_bytes::hex2bytes(&String::deserialize(deserializer)?)
 			.map_err(|e| de::Error::custom(format!("{:?}", e)))?;
-		Signature::try_from(signature_hex.as_ref())
-			.map_err(|e| de::Error::custom(format!("{:?}", e)))
+        let sighex_ref = signature_hex.as_ref();
+		Signature::try_from(sighex_ref)
+			.map_err(|e| de::Error::custom(format!("Error in converting deserialized data into signature")))
 	}
 }
 
@@ -337,7 +354,7 @@ impl<LeftSignature: SignatureBound, RightSignature: SignatureBound, const LEFT_P
 	}
 }
 
-impl<LeftSignature: SignatureBound, RightSignature: SignatureBound, const LEFT_PLUS_RIGHT_LEN: usize,> sp_std::fmt::Debug for Signature<LeftSignature, RightSignature, LEFT_PLUS_RIGHT_LEN> {
+impl<LeftSignature: SignatureBound, RightSignature: SignatureBound, const LEFT_PLUS_RIGHT_LEN: usize,> sp_std::fmt::Debug for Signature<LeftSignature, RightSignature, LEFT_PLUS_RIGHT_LEN> where [u8;LEFT_PLUS_RIGHT_LEN]: crate::hexdisplay::AsBytesRef{
 	#[cfg(feature = "std")]
 	fn fmt(&self, f: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
 		write!(f, "{}", crate::hexdisplay::HexDisplay::from(&self.inner))
@@ -351,8 +368,8 @@ impl<LeftSignature: SignatureBound, RightSignature: SignatureBound, const LEFT_P
 
 impl<LeftSignature: SignatureBound, RightSignature: SignatureBound,  const LEFT_PLUS_RIGHT_LEN: usize,> UncheckedFrom<[u8; LEFT_PLUS_RIGHT_LEN]> for Signature<LeftSignature, RightSignature,LEFT_PLUS_RIGHT_LEN> where Signature<LeftSignature, RightSignature, LEFT_PLUS_RIGHT_LEN>: SignaturePair {
 	fn unchecked_from(data: [u8; LEFT_PLUS_RIGHT_LEN]) -> Self {
-        let mut left : LeftSignature = data[0..Self::LEFT_SIGNATURE_LEN].try_into().unwrap();
-	 let mut right : RightSignature = data[Self::LEFT_SIGNATURE_LEN..LEFT_PLUS_RIGHT_LEN].try_into().unwrap();
+        let Ok(mut left) = data[0..Self::LEFT_SIGNATURE_LEN].try_into() else { panic!("invalid signature") };
+	 let Ok(mut right) = data[Self::LEFT_SIGNATURE_LEN..LEFT_PLUS_RIGHT_LEN].try_into() else { panic!("invalid signature") };
 
 	 let mut inner = [0u8; LEFT_PLUS_RIGHT_LEN];
 	 Signature { left, right, inner }
@@ -362,7 +379,8 @@ impl<LeftSignature: SignatureBound, RightSignature: SignatureBound,  const LEFT_
 /// A key pair.
 #[cfg(feature = "full_crypto")]
 #[derive(Clone)]
-pub struct Pair<LeftPair: TraitPair, RightPair: TraitPair,> {
+pub struct Pair<LeftPair: TraitPair, RightPair: TraitPair, const PUBLIC_KEY_LEN: usize, const SIGNATURE_LEN: usize>
+{
     left: LeftPair,
     right: RightPair,
     
@@ -383,64 +401,87 @@ trait HardJunctionId {
 }
 
 #[cfg(feature = "full_crypto")]
-impl<LeftPair: TraitPair, RightPair: TraitPair> Pair<LeftPair, RightPair> {
+impl<LeftPair: TraitPair, RightPair: TraitPair, const PUBLIC_KEY_LEN: usize, const SIGNATURE_LEN: usize> Pair<LeftPair, RightPair, PUBLIC_KEY_LEN, SIGNATURE_LEN> {
 }
 
 #[cfg(feature = "full_crypto")]
-impl<LeftPair: TraitPair, RightPair: TraitPair> TraitPair for Pair<LeftPair, RightPair> where
-    Pair<LeftPair, RightPair>: DoublePair + CryptoType,
+impl<LeftPair: TraitPair, RightPair: TraitPair, const PUBLIC_KEY_LEN: usize, const SIGNATURE_LEN: usize> TraitPair for Pair<LeftPair, RightPair, PUBLIC_KEY_LEN, SIGNATURE_LEN> where
+    Pair<LeftPair, RightPair, PUBLIC_KEY_LEN, SIGNATURE_LEN>: DoublePair + CryptoType,
     LeftPair::Signature: SignatureBound,
     RightPair::Signature: SignatureBound,
+Public<LeftPair::Public, RightPair::Public, PUBLIC_KEY_LEN>: CryptoType,
+Signature<LeftPair::Signature, RightPair::Signature, SIGNATURE_LEN>: SignaturePair,
+    LeftPair::Seed: SeedBound,
+    RightPair::Seed: SeedBound,
+Seed<LeftPair::Seed, RightPair::Seed, DOUBLE_SEED_LEN>: SeedBound,
 {
-	type Seed = (LeftPair::Seed, RightPair::Seed);
-	type Public = Public<LeftPair::Public, RightPair::Public, {<Self as DoublePair>::PUBLIC_KEY_LEN}>;
-	type Signature = Signature<LeftPair::Signature, RightPair::Signature, { Self::SIGNATURE_LEN }>;
+	type Seed = Seed<LeftPair::Seed, RightPair::Seed, DOUBLE_SEED_LEN>;
+	type Public = Public<LeftPair::Public, RightPair::Public, PUBLIC_KEY_LEN>;
+	type Signature = Signature<LeftPair::Signature, RightPair::Signature, SIGNATURE_LEN>;
 
 	fn from_seed_slice(seed_slice: &[u8]) -> Result<Self, SecretStringError> {
 		if seed_slice.len() != Self::RIGHT_SEED_LEN + Self::LEFT_SEED_LEN {
 			return Err(SecretStringError::InvalidSeedLength)
 		}
-		let left = LeftPair::from_seed_slice(seed_slice[0..Self::LEFT_SEED_LEN])?;
-        let right =	RightPair::from_seed_slice(seed_slice[Self::LEFT_SEED_LEN..Self::RIGHT_SEED_LEN + Self::LEFT_SEED_LEN])?;
+		let left = LeftPair::from_seed_slice(&seed_slice[0..Self::LEFT_SEED_LEN])?;
+        let right =	RightPair::from_seed_slice(&seed_slice[Self::LEFT_SEED_LEN..Self::RIGHT_SEED_LEN + Self::LEFT_SEED_LEN])?;
 		Ok(Pair { left, right })
 	}
 
-	// fn derive<Iter: Iterator<Item = DeriveJunction>>(
-	// 	&self,
-	// 	path: Iter,
-	// 	_seed: Option<Seed>,
-	// ) -> Result<(Self, Option<Seed>), DeriveError> {
-	// 	let mut acc: [u8; SECRET_KEY_SERIALIZED_SIZE] =
-	// 		self.0.secret.to_bytes().try_into().expect(
-	// 			"Secret key serializer returns a vector of SECRET_KEY_SERIALIZED_SIZE size",
-	// 		);
-	// 	for j in path {
-	// 		match j {
-	// 			DeriveJunction::Soft(_cc) => return Err(DeriveError::SoftKeyInPath),
-	// 			DeriveJunction::Hard(cc) => acc = derive_hard_junction::<T>(&acc, &cc),
-	// 		}
-	// 	}
-	// 	Ok((Self::from_seed(&acc), Some(acc)))
-	// }
+	fn derive<Iter: Iterator<Item = DeriveJunction>>(
+	 	&self,
+	 	path: Iter,
+	 	seed: Option<Self::Seed>,
+	) -> Result<(Self, Option<Self::Seed>), DeriveError> {
 
-	// fn public(&self) -> Self::Public {
-	// 	let mut raw = [0u8; PUBLIC_KEY_SERIALIZED_SIZE];
-	// 	let pk = DoublePublicKeyScheme::into_double_public_key(&self.0).to_bytes();
-	// 	raw.copy_from_slice(pk.as_slice());
-	// 	Self::Public::unchecked_from(raw)
-	// }
+        let seed_left_right = match seed {
+            Some(seed) => (Some(seed.left), Some(seed.right)),
+                //(LeftPair::from_seed_slice(&seed).ok().map(|p|p.to_raw_vec()), RightPair::from_seed_slice(&seed).ok().map(|p| p.to_raw_vec())),
+                None => (None, None),
+        };
 
-	// fn sign(&self, message: &[u8]) -> Self::Signature {
-	// 	let mut mutable_self = self.clone();
-	// 	let r: [u8; SIGNATURE_SERIALIZED_SIZE] =
+        let left_path: Vec<_> = path.map(|p|p.clone()).collect();
+        let right_path = left_path.clone();
+		let derived_left = self.left.derive(left_path.into_iter(), seed_left_right.0)?;
+        let derived_right = self.right.derive(right_path.into_iter(), seed_left_right.1)?;
+
+            let optional_seed = match (derived_left.1, derived_right.1) {
+                (Some(seed_left), Some(seed_right)) => {
+                    let mut inner = [0u8; DOUBLE_SEED_LEN];
+                    inner.copy_from_slice(seed_left.as_ref());
+ 	                inner[SECURE_SEED_LEN..].copy_from_slice(seed_right.as_ref());
+                    Some(Self::Seed{left: seed_left, right: seed_right, inner: inner})},
+                _ => None,
+
+                };
+        //     Some(seed) => (LeftPair::from_seed_slice(&seed).ok().map(|p|p.to_raw_vec()), RightPair::from_seed_slice(&seed).ok().map(|p| p.to_raw_vec())),
+        //     None => (None, None),
+        // };
+
+	 	Ok((Self{left: derived_left.0, right: derived_right.0}, optional_seed))
+	 }
+
+	fn public(&self) -> Self::Public {
+	   let mut raw = [0u8; PUBLIC_KEY_LEN];
+		let left_pub = self.left.public();
+        let right_pub  = self.right.public();	// 	let pk = DoublePublicKeyScheme::into_double_public_key(&self.0).to_bytes();
+        raw.copy_from_slice(left_pub.as_ref());
+ 	    raw[LeftPair::Public::LEN..].copy_from_slice(right_pub.as_ref());
+	    Self::Public::unchecked_from(raw)
+        //Public{left: left_pub, right: right_pub, inner: raw}
+	}
+
+	fn sign(&self, message: &[u8]) -> Self::Signature {
+	    let mut mutable_self = self.clone();
+	    let r: [u8; SIGNATURE_LEN] = [0u8; SIGNATURE_LEN];
 	// 		DoublePublicKeyScheme::sign(&mut mutable_self.0, &Message::new(b"", message))
 	// 			.to_bytes()
 	// 			.try_into()
 	// 			.expect("Signature serializer returns vectors of SIGNATURE_SERIALIZED_SIZE size");
-	// 	Self::Signature::unchecked_from(r)
-	// }
+	    Self::Signature::unchecked_from(r)
+	}
 
-	// fn verify<M: AsRef<[u8]>>(sig: &Self::Signature, message: M, pubkey: &Self::Public) -> bool {
+	fn verify<M: AsRef<[u8]>>(sig: &Self::Signature, message: M, pubkey: &Self::Public) -> bool {
 	// 	let pubkey_array: [u8; PUBLIC_KEY_SERIALIZED_SIZE] =
 	// 		match <[u8; PUBLIC_KEY_SERIALIZED_SIZE]>::try_from(pubkey.as_ref()) {
 	// 			Ok(pk) => pk,
@@ -460,15 +501,12 @@ impl<LeftPair: TraitPair, RightPair: TraitPair> TraitPair for Pair<LeftPair, Rig
 	// 		Err(_) => return false,
 	// 	};
 
-	// 	sig.verify(&Message::new(b"", message.as_ref()), &public_key)
-	// }
+	    // 	sig.verify(&Message::new(b"", message.as_ref()), &public_key)
+        return false;
+	}
 
-	// /// Get the seed for this key.
-	// fn to_raw_vec(&self) -> Vec<u8> {
-	// 	self.0
-	// 		.secret
-	// 		.to_bytes()
-	// 		.try_into()
-	// 		.expect("Secret key serializer returns a vector of SECRET_KEY_SERIALIZED_SIZE size")
-	// }
+	/// Get the seed for this key.
+	fn to_raw_vec(&self) -> Vec<u8> {
+		[self.left.to_raw_vec(), self.left.to_raw_vec()].concat()
+	}
 }
